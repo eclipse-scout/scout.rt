0a532344b7e0d1b30220c0bd17bcf2dbd6373fad
Ivan Motsch <ivan.motsch@bsiag.com>
2010-12-01 16:58:21 +0000

    Contributed by Christoh Schwank

--- org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/form/fields/treebox/AbstractTreeBoxFilterBox.java
+++ org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/form/fields/treebox/AbstractTreeBoxFilterBox.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -252,7 +252,7 @@ public abstract class AbstractTreeBoxFilterBox extends AbstractGroupBox {
 
     @Override
     protected double getConfiguredGridWeightX() {
-      return 0;
+      return 1;
     }
 
     @Override
