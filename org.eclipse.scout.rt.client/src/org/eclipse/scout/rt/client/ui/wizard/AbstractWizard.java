95a8fa6f33a701762e3112be2768e4bc7e399548
Hannes Müller <Hannes.Mueller@bsiag.com>
2010-12-22 08:34:05 +0000

    check added to eliminate npe in startWizard

--- org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/wizard/AbstractWizard.java
+++ org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/wizard/AbstractWizard.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -870,7 +870,7 @@ public abstract class AbstractWizard extends AbstractPropertyObserver implements
     setCloseTypeInternal(CloseType.Unknown);
     createContainerForm();
     execStart();
-    if (!m_containerForm.isFormOpen()) {
+    if (m_containerForm != null && !m_containerForm.isFormOpen()) {
       m_containerForm.startWizard();
     }
   }
