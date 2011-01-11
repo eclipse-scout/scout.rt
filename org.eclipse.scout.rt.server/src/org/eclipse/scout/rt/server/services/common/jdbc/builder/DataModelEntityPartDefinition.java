ebd1a86df4b74dcea3b9eeb5c6e44ad516c2ea39
Ivan Motsch <ivan.motsch@bsiag.com>
2011-01-11 12:12:13 +0000

    enhanced javadoc no auto select clause, it's too different from where clause

--- org.eclipse.scout.rt.server/src/org/eclipse/scout/rt/server/services/common/jdbc/builder/DataModelEntityPartDefinition.java
+++ org.eclipse.scout.rt.server/src/org/eclipse/scout/rt/server/services/common/jdbc/builder/DataModelEntityPartDefinition.java
@@ -11,8 +11,6 @@
 package org.eclipse.scout.rt.server.services.common.jdbc.builder;
 
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.eclipse.scout.commons.exception.ProcessingException;
 import org.eclipse.scout.rt.shared.data.form.fields.composer.ComposerEntityNodeData;
@@ -27,37 +25,55 @@ public class DataModelEntityPartDefinition {
   private final String m_selectClause;
 
   /**
-   * @param whereClause
-   *          is the clause used to find data by constraining the where clause. This is normally something like EXISTS (
-   *          SELECT 1 FROM ... &lt;whereParts/&gt; ... &lt;groupBy&gt; ... &lt;havingParts/&gt; ... &lt;/groupBy&gt;
-   *          <p>
-   *          This adds a default selectClause iff it has the exact form EXISTS ( SELECT 1 FROM ...)
-   *          <p>
-   *          Then the EXISTS is omitted and the 1 is replaced by &lt;column/&gt;.
+   * see {@link #DataModelEntityPartDefinition(Class, String, String)}
    */
   public DataModelEntityPartDefinition(Class<? extends IDataModelEntity> entityType, String whereClause) {
-    m_entityType = entityType;
-    m_whereClause = whereClause;
-    String selectClause = null;
-    if (whereClause != null) {
-      Matcher m = Pattern.compile("EXISTS\\s*\\(\\s*SELECT\\s+1\\s+FROM").matcher(whereClause.toUpperCase());
-      if (m.find() && m.start() == 0) {
-        String template = "( SELECT <column/> FROM " + whereClause.substring(m.end()).trim();
-        selectClause = template;
-      }
-    }
-    m_selectClause = selectClause;
+    this(entityType, whereClause, null);
   }
 
   /**
    * @param whereClause
-   *          is the clause used to find data by constraining the where clause. This is normally something like EXISTS (
-   *          SELECT 1...
+   *          is the clause used to find data by constraining the where clause. This is normally something like
+   * 
+   *          <pre>
+   * EXISTS (   *          SELECT 1 FROM ... &lt;whereParts/&gt; ... &lt;groupBy&gt; ... &lt;havingParts/&gt; ... &lt;/groupBy&gt;)
+   * </pre>
+   *          <p>
+   *          whereParts and havingParts is replaced in the build process by the corresponding attributes with/without
+   *          aggregation.
    * @param selectClause
    *          is the clause to add n attributes of an entity to the base entity.
    *          This parameter is the join clause used to join the additional entity to the
    *          base entity in the select part. It must have the form (SELECT <column/> FROM ... ) or a similar form
-   *          containing a <column/> tag.
+   *          containing a <column/> tag and a @parent.TableAlias@.PRIMARY_KEY_ID
+   *          <p>
+   *          Example for a Person-Contact-Entity-join where-clause:
+   * 
+   *          <pre>
+   * @parent.Person@.PERSON_ID!=0
+   * AND EXISTS (
+   *   SELECT 1 " +
+   *   FROM MY_CONTACT @Contact@
+   *   WHERE @Contact@.PERSON_ID=@parent.Person@.PERSON_ID
+   *   &lt;whereParts/&gt;
+   *   &lt;groupBy&gt;
+   *     GROUP BY @parent.Person@.PERSON_ID
+   *     HAVING 1=1
+   *     &lt;havingParts/&gt;
+   *   &lt;/groupBy&gt;
+   * )
+   * </pre>
+   *          <p>
+   *          Example for a Person-Contact-Entity-join select-clause:
+   * 
+   *          <pre>
+   * SELECT &lt;column/&gt;
+   * FROM MY_CONTACT @Contact@
+   * WHERE @Contact@.PERSON_FK=@parent.Person@.PERSON_ID
+   * &lt;whereParts/&gt;
+   * &lt;groupBy/&gt;
+   * </pre>
+   *          <p>
    */
   public DataModelEntityPartDefinition(Class<? extends IDataModelEntity> entityType, String whereClause, String selectClause) {
     m_entityType = entityType;
@@ -66,7 +82,7 @@ public class DataModelEntityPartDefinition {
       m_selectClause = selectClause;
     }
     else {
-      throw new IllegalArgumentException("select clause must be of form (SELECT <column/> FROM ... ) or a similar that contains a <column/> tag");
+      throw new IllegalArgumentException("select clause must be of form SELECT <column/> FROM ... or a similar that contains a <column/> tag");
     }
   }
 
