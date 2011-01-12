c1207ab4bf94f771bf3efd988f18a330d66d230b
Ivan Motsch <ivan.motsch@bsiag.com>
2011-01-12 08:29:34 +0000

    fixed handling of root url without trailing '/' contributed by Philippe Jacot, BSI AG

--- org.eclipse.scout.rt.server/src/org/eclipse/scout/rt/server/ResourceServlet.java
+++ org.eclipse.scout.rt.server/src/org/eclipse/scout/rt/server/ResourceServlet.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -86,8 +86,14 @@ public class ResourceServlet extends HttpServletEx {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-    if (!writeStaticResource(req, res)) {
-      res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    String pathInfo = req.getPathInfo();
+    if (pathInfo == null || pathInfo.equals("")) {
+      res.sendRedirect("/");
+    }
+    else {
+      if (!writeStaticResource(req, res)) {
+        res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      }
     }
   }
 
