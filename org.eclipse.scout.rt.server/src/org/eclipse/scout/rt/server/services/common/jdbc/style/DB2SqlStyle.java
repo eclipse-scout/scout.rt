5008751adbd17f0c57c22d415a2fd9416e26d9f1
Stephan Leicht <stephan.leicht@bsiag.com>
2011-01-17 14:01:46 +0000

    Binding of BLOB and CLOB parameters was wrong. New code since over 1/2 year in production at customer-site. contributed by Alexandra Junghans, BSI AG

--- org.eclipse.scout.rt.server/src/org/eclipse/scout/rt/server/services/common/jdbc/style/DB2SqlStyle.java
+++ org.eclipse.scout.rt.server/src/org/eclipse/scout/rt/server/services/common/jdbc/style/DB2SqlStyle.java
@@ -10,18 +10,25 @@
  ******************************************************************************/
 package org.eclipse.scout.rt.server.services.common.jdbc.style;
 
-import java.sql.Blob;
-import java.sql.Clob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Types;
 
 import org.eclipse.scout.rt.server.services.common.jdbc.SqlBind;
 
 public class DB2SqlStyle extends AbstractSqlStyle {
 
   private static final long serialVersionUID = 1L;
+
+  @Override
+  public String getConcatOp() {
+    return "||";
+  }
+
+  @Override
+  public String getLikeWildcard() {
+    return "%";
+  }
 
   @Override
   protected int getMaxListSize() {
@@ -45,78 +52,7 @@ public class DB2SqlStyle extends AbstractSqlStyle {
 
   @Override
   public void writeBind(PreparedStatement ps, int jdbcBindIndex, SqlBind bind) throws SQLException {
-    switch (bind.getSqlType()) {
-      case Types.CLOB: {
-        if (bind.getValue() instanceof Clob) {
-          super.writeBind(ps, jdbcBindIndex, bind);
-        }
-        else {
-          String s = (String) bind.getValue();
-          /*
-           * Reflect:
-           * CLOB newClob=CLOB.createTemporary(conn,false,oracle.sql.CLOB.DURATION_SESSION);
-           * (a) newClob.putString(1,s);
-           * (b) newClob.setString(1,s);
-           */
-          Clob clob;
-          try {
-            Class clobClass;
-            clobClass = Class.forName("com.ibm.db2.app.CLOB", true, ps.getClass().getClassLoader());
-            clob = (Clob) clobClass.getMethod("createTemporary", new Class[]{Connection.class, boolean.class, int.class}).invoke(null, new Object[]{ps.getConnection(), false, clobClass.getField("DURATION_SESSION").get(null)});
-          }
-          catch (Throwable t) {
-            SQLException ex = new SQLException("bind clob on jdbcIndex " + jdbcBindIndex);
-            ex.initCause(t);
-            throw ex;
-          }
-          //
-          try {
-            clob.getClass().getMethod("putString", new Class[]{long.class, String.class}).invoke(clob, new Object[]{1, s});
-          }
-          catch (Throwable t) {
-            clob.setString(1, s);
-          }
-          ps.setClob(jdbcBindIndex, clob);
-        }
-        break;
-      }
-      case Types.BLOB: {
-        if (bind.getValue() instanceof Blob) {
-          super.writeBind(ps, jdbcBindIndex, bind);
-        }
-        else {
-          byte[] data = (byte[]) bind.getValue();
-          /*
-           * Reflect:
-           * BLOB newBlob=BLOB.createTemporary(conn,false,oracle.sql.BLOB.DURATION_SESSION);
-           * (a) newBlob.putBytes(1,data);
-           * (b) newBlob.setBytes(1,data);
-           */
-          Blob blob;
-          try {
-            Class blobClass = Class.forName("com.ibm.db2.app.BLOB", true, ps.getClass().getClassLoader());
-            blob = (Blob) blobClass.getMethod("createTemporary", new Class[]{Connection.class, boolean.class, int.class}).invoke(null, new Object[]{ps.getConnection(), false, blobClass.getField("DURATION_SESSION").get(null)});
-          }
-          catch (Throwable t) {
-            SQLException ex = new SQLException("bind blob on jdbcIndex " + jdbcBindIndex);
-            ex.initCause(t);
-            throw ex;
-          }
-          //
-          try {
-            blob.getClass().getMethod("putBytes", new Class[]{long.class, byte[].class}).invoke(blob, new Object[]{1, data});
-          }
-          catch (Throwable t) {
-            blob.setBytes(1, data);
-          }
-          ps.setBlob(jdbcBindIndex, blob);
-        }
-        break;
-      }
-      default: {
-        super.writeBind(ps, jdbcBindIndex, bind);
-      }
-    }
+    super.writeBind(ps, jdbcBindIndex, bind);
   }
 
   @Override
