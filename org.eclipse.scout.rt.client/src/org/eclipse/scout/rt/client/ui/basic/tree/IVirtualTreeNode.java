2331331726ac55c64a958907467d4ac916acd452
Ivan Motsch <ivan.motsch@bsiag.com>
2011-02-09 16:04:59 +0000

    bsi ticket 98883

--- org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/basic/tree/IVirtualTreeNode.java
+++ org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/basic/tree/IVirtualTreeNode.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -12,6 +12,7 @@ package org.eclipse.scout.rt.client.ui.basic.tree;
 
 import org.eclipse.scout.rt.client.ui.desktop.outline.IOutline;
 import org.eclipse.scout.rt.client.ui.desktop.outline.pages.IPage;
+import org.eclipse.scout.rt.client.ui.desktop.outline.pages.VirtualPage;
 
 /**
  * A virtual node is a marker tree node used to optimize performance in large trees.
@@ -19,5 +20,16 @@ import org.eclipse.scout.rt.client.ui.desktop.outline.pages.IPage;
  * It is used mainly in the {@link IPage}, {@link IOutline} area with {@link VirtualPage}s
  */
 public interface IVirtualTreeNode extends ITreeNode {
+  /**
+   * @return the real node if the virtual node has been resolved, null otherwise
+   *         This value is used when a (old) reference to a {@link IVirtualTreeNode} is interested in the real node to
+   *         update its
+   *         reference
+   */
+  ITreeNode getResolvedNode();
 
+  /**
+   * Set the real node of this virtual node.
+   */
+  void setResolvedNode(ITreeNode resolvedNode);
 }
