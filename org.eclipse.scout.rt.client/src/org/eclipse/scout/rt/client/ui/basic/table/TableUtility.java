14ee2d8d2f2d727e815e5497b5e75403b3645b0a
Ivan Motsch <ivan.motsch@bsiag.com>
2011-02-16 14:51:40 +0000

    extracted csv export to TableUtility and handle html texts

--- org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/basic/table/TableUtility.java
+++ org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/basic/table/TableUtility.java
@@ -12,15 +12,26 @@ package org.eclipse.scout.rt.client.ui.basic.table;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.Map;
 
 import org.eclipse.scout.commons.ConfigurationUtility;
+import org.eclipse.scout.commons.HTMLUtility;
+import org.eclipse.scout.commons.TypeCastUtility;
 import org.eclipse.scout.commons.exception.ProcessingException;
 import org.eclipse.scout.commons.logger.IScoutLogger;
 import org.eclipse.scout.commons.logger.ScoutLogManager;
 import org.eclipse.scout.rt.client.ui.basic.cell.Cell;
+import org.eclipse.scout.rt.client.ui.basic.table.columns.IBigDecimalColumn;
+import org.eclipse.scout.rt.client.ui.basic.table.columns.IBooleanColumn;
 import org.eclipse.scout.rt.client.ui.basic.table.columns.IColumn;
+import org.eclipse.scout.rt.client.ui.basic.table.columns.IDateColumn;
+import org.eclipse.scout.rt.client.ui.basic.table.columns.IDoubleColumn;
+import org.eclipse.scout.rt.client.ui.basic.table.columns.IIntegerColumn;
+import org.eclipse.scout.rt.client.ui.basic.table.columns.ILongColumn;
 import org.eclipse.scout.rt.client.ui.basic.table.columns.ISmartColumn;
+import org.eclipse.scout.rt.client.ui.basic.table.columns.ITimeColumn;
 import org.eclipse.scout.rt.shared.services.common.exceptionhandler.IExceptionHandlerService;
 import org.eclipse.scout.rt.shared.services.lookup.LocalLookupCall;
 import org.eclipse.scout.rt.shared.services.lookup.LookupCall;
@@ -157,4 +168,138 @@ public final class TableUtility {
       row.setRowChanging(false);
     }
   }
+
+  /**
+   * @return matrix[rowCount][columnCount] with cell as CSV cells
+   *         <p>
+   *         The returned csv will be:
+   *         <ul>
+   *         <li>Optional row: column names (Strings)</li>
+   *         <li>Optional row: column types (Classes)</li>
+   *         <li>Optional row: column formats (Strings)</li>
+   *         <li>Data row (Objects)</li>
+   *         <li>Data row (Objects)</li>
+   *         <li>...</li>
+   *         </ul>
+   *         <p>
+   *         valid exported csv types include:
+   *         <ul>
+   *         <li>java.lang.String</li>
+   *         <li>java.lang.Long</li>
+   *         <li>java.lang.Integer</li>
+   *         <li>java.lang.Float</li>
+   *         <li>java.lang.Double</li>
+   *         <li>java.lang.Boolean</li>
+   *         <li>java.util.Date</li>
+   *         </ul>
+   */
+  public static Object[][] exportRowsAsCSV(ITableRow[] rows, IColumn<?>[] columns, boolean includeLineForColumnNames, boolean includeLineForColumnTypes, boolean includeLineForColumnFormats) {
+    int nr = rows.length;
+    Object[][] a = new Object[nr + (includeLineForColumnNames ? 1 : 0) + (includeLineForColumnTypes ? 1 : 0) + (includeLineForColumnFormats ? 1 : 0)][columns.length];
+    for (int c = 0; c < columns.length; c++) {
+      IColumn<?> col = columns[c];
+      Class<?> type;
+      boolean byValue;
+      String format;
+      if (col instanceof IDateColumn) {
+        if (((IDateColumn) col).isHasTime()) {
+          type = Timestamp.class;
+          byValue = true;
+          format = ((IDateColumn) col).getFormat();
+        }
+        else {
+          type = Date.class;
+          byValue = true;
+          format = ((IDateColumn) col).getFormat();
+        }
+      }
+      else if (col instanceof IDoubleColumn) {
+        type = Double.class;
+        byValue = true;
+        format = ((IDoubleColumn) col).getFormat();
+      }
+      else if (col instanceof IIntegerColumn) {
+        type = Integer.class;
+        byValue = true;
+        format = ((IIntegerColumn) col).getFormat();
+      }
+      else if (col instanceof ILongColumn) {
+        type = Long.class;
+        byValue = true;
+        format = ((ILongColumn) col).getFormat();
+      }
+      else if (col instanceof IBigDecimalColumn) {
+        type = Long.class;
+        byValue = true;
+        format = ((IBigDecimalColumn) col).getFormat();
+      }
+      else if (col instanceof ISmartColumn<?>) {
+        type = String.class;
+        byValue = false;
+        format = null;
+      }
+      else if (col instanceof ITimeColumn) {
+        type = Date.class;
+        byValue = true;
+        format = ((ITimeColumn) col).getFormat();
+      }
+      else if (col instanceof IBooleanColumn) {
+        type = Boolean.class;
+        byValue = false;
+        format = null;
+      }
+      else {
+        type = String.class;
+        byValue = false;
+        format = null;
+      }
+      //
+      int csvRowIndex = 0;
+      if (includeLineForColumnNames) {
+        a[csvRowIndex][c] = columns[c].getHeaderCell().getText();
+        csvRowIndex++;
+      }
+      if (includeLineForColumnTypes) {
+        a[csvRowIndex][c] = type;
+        csvRowIndex++;
+      }
+      if (includeLineForColumnFormats) {
+        a[csvRowIndex][c] = format;
+        csvRowIndex++;
+      }
+      for (int r = 0; r < nr; r++) {
+        if (byValue) {
+          if (type == Timestamp.class) {
+            a[csvRowIndex][c] = TypeCastUtility.castValue(columns[c].getValue(rows[r]), Timestamp.class);
+          }
+          else {
+            a[csvRowIndex][c] = columns[c].getValue(rows[r]);
+          }
+        }
+        else {
+          String text = columns[c].getDisplayText(rows[r]);
+          //special intercept for boolean
+          if (type == Boolean.class) {
+            Boolean b = TypeCastUtility.castValue(columns[c].getValue(rows[r]), Boolean.class);
+            if (b != null && b.booleanValue()) {
+              //make sure there is a text
+              if (text == null || text.trim().length() == 0) {
+                text = "X";
+              }
+            }
+          }
+          //special intercept for html
+          if (type == String.class) {
+            if (text != null && text.startsWith("<html")) {
+              text = HTMLUtility.getPlainText(text);
+            }
+          }
+          a[csvRowIndex][c] = text;
+        }
+        csvRowIndex++;
+      }
+    }
+    return a;
+  }
+
 }
