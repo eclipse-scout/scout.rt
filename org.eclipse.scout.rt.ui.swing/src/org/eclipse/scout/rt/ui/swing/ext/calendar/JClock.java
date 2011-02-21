9b4e54da56bdcae7f215c074eb8305b38d0b90b7
Ivan Motsch <ivan.motsch@bsiag.com>
2011-02-21 15:52:26 +0000

    XXX->TODO

--- org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/ext/calendar/JClock.java
+++ org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/ext/calendar/JClock.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -51,7 +51,7 @@ public class JClock extends JComponent {
     setMaximumSize(d);
     setForeground(new Color(0x67A8CE));
     setOpaque(false);
-    //XXX add to synth L&F config xml
+    //TODO [awe] add to synth L&F config xml
     setName("Synth.Clock");
   }
 
