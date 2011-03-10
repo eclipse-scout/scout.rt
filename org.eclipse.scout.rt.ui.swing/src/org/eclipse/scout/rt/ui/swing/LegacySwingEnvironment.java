78d25913890345253a28aabb9f7ea72e13693398
Ivan Motsch <ivan.motsch@bsiag.com>
2011-03-10 14:17:44 +0000

    removed empty comments

--- org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/LegacySwingEnvironment.java
+++ org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/LegacySwingEnvironment.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -29,9 +29,6 @@ import org.eclipse.scout.rt.ui.swing.form.fields.tablefield.LegacySwingScoutTabl
 import org.eclipse.scout.rt.ui.swing.window.desktop.ISwingScoutRootFrame;
 import org.eclipse.scout.rt.ui.swing.window.desktop.LegacySwingScoutRootFrame;
 
-/**
- *
- */
 public class LegacySwingEnvironment extends AbstractSwingEnvironment {
   /*
    * (non-Javadoc)
