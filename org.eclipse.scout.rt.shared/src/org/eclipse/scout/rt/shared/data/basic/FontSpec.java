6d46ad7fc22cb1445f54fc74cb4acdeed7c31e03
Ivan Motsch <ivan.motsch@bsiag.com>
2011-03-14 09:34:13 +0000

    fixed obvious bug in parsing font spec with null name

--- org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/data/basic/FontSpec.java
+++ org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/data/basic/FontSpec.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -109,6 +109,10 @@ public class FontSpec implements java.io.Serializable {
             f.m_name = s;
           }
         }
+      }
+      //if name is "null" set it to null
+      if (f.m_name != null && f.m_name.equalsIgnoreCase("null")) {
+        f.m_name = null;
       }
       return f;
     }
