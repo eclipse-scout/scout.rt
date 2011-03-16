44a525c59fd8e3b5f9cef173de439941078e956e
Hannes Müller <Hannes.Mueller@bsiag.com>
2011-03-16 14:03:11 +0000

    registerPageForm improved: create better / distinct id for pages this is important to put/get the correct searchFilter from the cache

--- org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/AbstractMemoryPolicy.java
+++ org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/AbstractMemoryPolicy.java
@@ -10,8 +10,12 @@
  ******************************************************************************/
 package org.eclipse.scout.rt.client;
 
+import java.util.Date;
 import java.util.WeakHashMap;
+import java.util.zip.CRC32;
 
+import org.eclipse.scout.commons.beans.FastBeanInfo;
+import org.eclipse.scout.commons.beans.FastPropertyDescriptor;
 import org.eclipse.scout.commons.exception.ProcessingException;
 import org.eclipse.scout.commons.logger.IScoutLogger;
 import org.eclipse.scout.commons.logger.ScoutLogManager;
@@ -94,11 +98,60 @@ public class AbstractMemoryPolicy implements IMemoryPolicy {
    * @return the identifier for the page form
    */
   protected String registerPageForm(IPage p, IForm f) {
-    String id = p.getClass().getName() + (p.getBookmarkIdentifier() != null ? "/" + p.getBookmarkIdentifier() : "") + "/" + f.getClass().getName();
+    String id = createUniqueIdForPage(p, f);
     m_formToIdentifierMap.put(f, id);
     f.removeFormListener(m_formListener);
     f.addFormListener(m_formListener);
     return id;
+  }
+
+  protected String createUniqueIdForPage(IPage p, IForm f) {
+    if (p == null) {
+      return null;
+    }
+    StringBuilder builder = new StringBuilder();
+    createIdForPage(builder, p, f);
+    IPage page = p.getParentPage();
+    while (page != null) {
+      createIdForPage(builder, page, null);
+      page = page.getParentPage();
+    }
+    CRC32 crc = new CRC32();
+    crc.update(builder.toString().getBytes());
+    return "" + crc.getValue();
+  }
+
+  private void createIdForPage(StringBuilder b, IPage page, IForm form) {
+    b.append("/");
+    b.append(page.getClass().getName());
+    if (page.getBookmarkIdentifier() != null) {
+      b.append("/");
+      b.append(page.getBookmarkIdentifier());
+    }
+    if (form != null) {
+      b.append("/");
+      b.append(form.getClass().getName());
+    }
+    FastBeanInfo pi = new FastBeanInfo(page.getClass(), page.getClass().getSuperclass());
+    for (FastPropertyDescriptor prop : pi.getPropertyDescriptors()) {
+      if (prop.getReadMethod() != null &&
+          (Date.class.isAssignableFrom(prop.getPropertyType()) ||
+              Number.class.isAssignableFrom(prop.getPropertyType()) ||
+              String.class.isAssignableFrom(prop.getPropertyType()) ||
+              long.class.isAssignableFrom(prop.getPropertyType()))) {
+        // only accept Numbers, Strings or Dates
+        try {
+          b.append("/");
+          b.append(prop.getName());
+          b.append("=");
+          b.append(prop.getReadMethod().invoke(page, new Object[0]));
+        }
+        catch (Exception e) {
+          e.printStackTrace();
+          // nop - ignore this property
+        }
+      }
+    }
   }
 
   protected void handlePageFormEvent(FormEvent e, String pageFormIdentifier) throws ProcessingException {
