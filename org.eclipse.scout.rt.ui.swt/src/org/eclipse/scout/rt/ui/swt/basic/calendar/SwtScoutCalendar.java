025a288fb2f1a35deb3e787752d39534cba5d0ed
Andreas Hoegger <Andreas.Hoegger@bsiag.com>
2011-04-08 11:15:55 +0000

    Ticket 100'846 CalendarField

--- org.eclipse.scout.rt.ui.swt/src/org/eclipse/scout/rt/ui/swt/basic/calendar/SwtScoutCalendar.java
+++ org.eclipse.scout.rt.ui.swt/src/org/eclipse/scout/rt/ui/swt/basic/calendar/SwtScoutCalendar.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -383,7 +383,7 @@ public class SwtScoutCalendar extends SwtCalendar {
                   //refreshLayout();
                 }
               };
-              m_field.getEnvironment().invokeSwtLater(r);
+              m_field.getEnvironment().invokeScoutLater(r, 2345);
               //end notify
             }
           }
