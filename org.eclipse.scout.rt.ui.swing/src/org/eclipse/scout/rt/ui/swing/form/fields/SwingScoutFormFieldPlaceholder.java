b71a6ecca4dcb72152de3303400320f0d5029b7a
Ivan Motsch <ivan.motsch@bsiag.com>
2011-05-09 10:57:06 +0000

    made a proper field composite for placeholder field for missing ui components, contributed by Beat Schwarzentrueb, BSI AG, bsi ticket 101951

--- org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/form/fields/SwingScoutFormFieldPlaceholder.java
+++ org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/form/fields/SwingScoutFormFieldPlaceholder.java
@@ -4,78 +4,39 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
 package org.eclipse.scout.rt.ui.swing.form.fields;
 
-import javax.swing.JComponent;
-import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 import org.eclipse.scout.rt.client.ui.form.fields.IFormField;
-import org.eclipse.scout.rt.ui.swing.ISwingEnvironment;
+import org.eclipse.scout.rt.ui.swing.LogicalGridLayout;
 import org.eclipse.scout.rt.ui.swing.ext.BorderLayoutEx;
+import org.eclipse.scout.rt.ui.swing.ext.JLabelEx;
 import org.eclipse.scout.rt.ui.swing.ext.JPanelEx;
-import org.eclipse.scout.rt.ui.swing.ext.JStatusLabelEx;
 
 /**
  * used for field models that have no ui implementation
  */
-public class SwingScoutFormFieldPlaceholder implements ISwingScoutFormField<IFormField> {
-  private IFormField m_scoutObject;
-  private ISwingEnvironment m_env;
-  private JComponent m_swingContainer;
-  private JStatusLabelEx m_swingStatusLabel;
-
-  public SwingScoutFormFieldPlaceholder() {
-  }
+public class SwingScoutFormFieldPlaceholder extends SwingScoutFieldComposite<IFormField> implements ISwingScoutFormField<IFormField> {
 
   @Override
-  public void createField(IFormField model, ISwingEnvironment environment) {
-    m_scoutObject = model;
-    m_env = environment;
-    m_swingStatusLabel = getSwingEnvironment().createStatusLabel();
-    m_swingStatusLabel.setText("Placeholder for " + model.getClass().getSimpleName());
-    m_swingContainer = new JPanelEx(new BorderLayoutEx(0, 0));
-    m_swingContainer.add(BorderLayoutEx.CENTER, m_swingStatusLabel);
+  protected void initializeSwing() {
+    super.initializeSwing();
+    //
+    JLabelEx label = new JLabelEx();
+    label.setHorizontalAlignment(SwingConstants.CENTER);
+    label.setVerticalAlignment(SwingConstants.CENTER);
+    label.setText("Placeholder for " + getScoutObject().getClass().getSimpleName());
+    JPanelEx container = new JPanelEx(new BorderLayoutEx(0, 0));
+    container.add(BorderLayoutEx.CENTER, label);
+    //
+    setSwingContainer(container);
+    setSwingField(label);
+    // layout
+    getSwingContainer().setLayout(new LogicalGridLayout(getSwingEnvironment(), 1, 0));
   }
-
-  @Override
-  public JComponent getSwingContainer() {
-    return m_swingContainer;
-  }
-
-  @Override
-  public JStatusLabelEx getSwingLabel() {
-    return m_swingStatusLabel;
-  }
-
-  public JLabel getSwingStatus() {
-    return null;
-  }
-
-  @Override
-  public JComponent getSwingField() {
-    return null;
-  }
-
-  @Override
-  public IFormField getScoutObject() {
-    return m_scoutObject;
-  }
-
-  @Override
-  public ISwingEnvironment getSwingEnvironment() {
-    return m_env;
-  }
-
-  @Override
-  public void connectToScout() {
-  }
-
-  @Override
-  public void disconnectFromScout() {
-  }
-
 }
