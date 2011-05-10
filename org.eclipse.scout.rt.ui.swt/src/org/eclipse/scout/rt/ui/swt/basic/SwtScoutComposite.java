f41b0bdccbe618e2569cd2443f10785829bf6b6d
Andreas Hoegger <Andreas.Hoegger@bsiag.com>
2011-05-10 10:17:01 +0000

    Ticket 101'979

--- org.eclipse.scout.rt.ui.swt/src/org/eclipse/scout/rt/ui/swt/basic/SwtScoutComposite.java
+++ org.eclipse.scout.rt.ui.swt/src/org/eclipse/scout/rt/ui/swt/basic/SwtScoutComposite.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -401,13 +401,8 @@ public abstract class SwtScoutComposite<T extends IPropertyObserver> implements 
             case SWT.CR:
               return;
           }
-          //XXX needed?
-          /*
-          if (getSwtInputVerifierLock().isReleased()) {
-            m_timestamp = event.time;
-            event.doit = handleSwtInputVerifier();
-          }
-          */
+          m_timestamp = event.time;
+          event.doit = handleSwtInputVerifier();
           break;
         case SWT.FocusIn:
           handleSwtFocusGained();
