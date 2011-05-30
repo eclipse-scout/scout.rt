da02e12430dcf8328f6b62b906c054dda336e5a4
Ivan Motsch <ivan.motsch@bsiag.com>
2011-05-30 12:32:27 +0000

    Added explicit instead of implicit session argument to IClientNotificationConsumerService observer, added possibility to add global observer

--- org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/services/common/clientnotification/IClientNotificationConsumerService.java
+++ org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/services/common/clientnotification/IClientNotificationConsumerService.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -33,9 +33,41 @@ public interface IClientNotificationConsumerService extends IService {
   /**
    * A consumer of client notifications can add a listener to this service. The
    * listener is notified immediately on new notifications, in whatever Thread
+   * 
+   * @deprecated use {@link #addClientNotificationConsumerListener(IClientSession, IClientNotificationConsumerListener)}
+   *             instead. Services that register in their {@link IService#initializeService()} phase normally use
+   *             {@link #addGlobalClientNotificationConsumerListener(IClientNotificationConsumerListener)}
    */
+  @Deprecated
   void addClientNotificationConsumerListener(IClientNotificationConsumerListener listener);
 
+  /**
+   * @deprecated use
+   *             {@link #removeClientNotificationConsumerListener(IClientSession, IClientNotificationConsumerListener)}
+   *             instead
+   */
+  @Deprecated
   void removeClientNotificationConsumerListener(IClientNotificationConsumerListener listener);
+
+  /**
+   * A consumer of client notifications can add a listener on this service. The
+   * listener is notified immediately on new notifications, in whatever thread
+   */
+  void addClientNotificationConsumerListener(IClientSession session, IClientNotificationConsumerListener listener);
+
+  void removeClientNotificationConsumerListener(IClientSession session, IClientNotificationConsumerListener listener);
+
+  /**
+   * A consumer of client notifications can add a global listener on this service. The
+   * listener is notified immediately on new notifications, in whatever thread.
+   * A global listener is notified on any client session, this can be used to attach generic functionality for any
+   * session.
+   * Note that this does not imply that a client notification is broadcast to any client session. It simply means that
+   * whenever a client session receives a notification,
+   * this listener is informed.
+   */
+  void addGlobalClientNotificationConsumerListener(IClientNotificationConsumerListener listener);
+
+  void removeGlobalClientNotificationConsumerListener(IClientNotificationConsumerListener listener);
 
 }
