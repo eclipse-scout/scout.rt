ba662dee90962da58bc8e9bcfdd6c9c3f8208a87
Ivan Motsch <ivan.motsch@bsiag.com>
2011-06-15 06:37:40 +0000

    Security improvement: unmarshalling on form field data verifies if runtime value is compatible with generics type

--- org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/data/form/fields/AbstractValueFieldData.java
+++ org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/data/form/fields/AbstractValueFieldData.java
@@ -4,12 +4,14 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
 package org.eclipse.scout.rt.shared.data.form.fields;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
 
 import org.eclipse.scout.commons.TypeCastUtility;
@@ -36,6 +38,20 @@ public abstract class AbstractValueFieldData<T> extends AbstractFormFieldData im
   @SuppressWarnings("unchecked")
   public Class<T> getHolderType() {
     return TypeCastUtility.getGenericsParameterClass(getClass(), IHolder.class);
+  }
+
+  /**
+   * readObject is implemented to validate potential security attacks that invalidated the value type
+   */
+  private void readObject(ObjectInputStream s) throws IOException, ClassNotFoundException {
+    s.defaultReadObject();
+    //verify if valueSet and the type of the value are valid and consistent
+    if (m_value == null) {
+      return;
+    }
+    if (!getHolderType().isAssignableFrom(m_value.getClass())) {
+      throw new SecurityException("value is of inconsistent type; potential value corruption attack");
+    }
   }
 
 }
