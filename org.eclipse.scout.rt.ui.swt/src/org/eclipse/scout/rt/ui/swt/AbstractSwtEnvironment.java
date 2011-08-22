b3d29588c66cef2a323484ecb9d71f77edc9fee2
Stephan Leicht <stephan.leicht@bsiag.com>
2011-08-22 11:14:54 +0000

    22.08.2011 dwi BSI ticket #105'026, #104'976 Problem: - Failed to display corrupt HTML pages (e.g. if missing closing quote in style definition) - In HTML editor, no cleanup (auto-correction) should be applied to the given HTML. That is because if the user did some modifications in the HTML source and reloads the HTML in the editor anew, unwanted auto-corrections would be applied. Solution: - Accomplish consistency concerning HTML styling (cleanup) in between of Swing and SWT in regard of ScoutHtmlField and ScoutHmtEditor.   In more detail, this entails the following:   - Before passing the HTML to the respective widget (ScoutHtmlEditor, ScoutHtmlField), Abstract[Swing|Swt]Environment#styleHtmlText() is called to cleanup the given HTML.   - In case of HTML editor mode, no modifications are applied to the given HTML in both, Swing and SWT, respectively.   - However, in non-editor mode, some intelligence is applied to the HTML to ensure proper display of the HTML document.     - In Swing, cleanup of HTML structure and CSS definitions is done. That is crucial as Swing HTML viewer has some problems with some CSS constructs.     - In SWT, OS default browser is used. That is why cleanup of CSS is not necessary as done by browser itself.       But, because the HTML is provided as file to the browser, proper encoding and charset must be set. - Failsafe: if HTML text cannot be parsed for cleanup, the raw HTML text is used instead of an exception thrown. - Consolidation of HTML cleanup functionality in HTMLUtility for plain and simple use that is applicable for both, SWT and Swing, respectively. That is why various methods in HtmlUtility are removed. Plug-Ins affected: - org.eclipse.scout.commons (HTMLUtility - org.eclipse.scout.rt.client (AbstractHtmlField) - org.eclipse.scout.rt.ui.swing (AbstractSwingEnvironment, SwingScoutHtmlField, SwingScoutMailField) - org.eclipse.scout.rt.ui.swing.bsi.fragment (SwingScoutHtmlEditor) - org.eclipse.scout.rt.ui.swt (AbstractSwtEnvironment, SwtScoutHtmlField) - org.eclipse.scout.rt.ui.swt.bsi.fragment (SwtScoutHtmlEditor) Migration Swing / SWT: - In HTMLUtility, the following methods are removed.   - HTMLUtility#parseDocument(String) to be replaced by HTMLUtility.toHtmlDocument(String)   - HTMLUtility#formatDocument(String) to be replaced by HTMLUtility.toHtmlText(String)   - HTMLUtility#cleanupDocument(HTMLDocument, String, int) to be replaced by HTMLUtility.cleanupHtml(String, boolean,boolean, DefaultFont)   - HTMLUtility#wellformDocument(HTMLDocument, String, int) to be replaced by HTMLUtility.cleanupHtml(String, boolean,boolean, DefaultFont) Migration SWT: - In AbstractSwtEnvironment, the following methods are removed as not required anymore because logic is encapsulated in HTMLUtility.   - AbstractSwtEnvironment#styleHtmlText(Control, String)   - AbstractSwtEnvironment#createCSS(Control)   - AbstractSwtEnvironment#createHtmlDocument(String, String)

--- org.eclipse.scout.rt.ui.swt/src/org/eclipse/scout/rt/ui/swt/AbstractSwtEnvironment.java
+++ org.eclipse.scout.rt.ui.swt/src/org/eclipse/scout/rt/ui/swt/AbstractSwtEnvironment.java
@@ -609,6 +609,11 @@ public abstract class AbstractSwtEnvironment extends AbstractPropertyObserver im
 
   @Override
   public String styleHtmlText(ISwtScoutFormField<?> uiComposite, String rawHtml) {
+    if (rawHtml == null) {
+      rawHtml = "";
+    }
+    String cleanHtml = rawHtml;
+
     if (uiComposite.getScoutObject() instanceof IHtmlField) {
       IHtmlField htmlField = (IHtmlField) uiComposite.getScoutObject();
       if (htmlField.isHtmlEditor()) {
@@ -618,20 +623,20 @@ public abstract class AbstractSwtEnvironment extends AbstractPropertyObserver im
          * provided. Otherwise, if the user did some modifications in the HTML source and reloads the HTML in the editor anew,
          * unwanted auto-corrections would be applied.
          */
-        if (!StringUtility.hasText(rawHtml)) {
-          rawHtml = "<html><head></head><body></body></html>";
-  }
-    }
+        if (!StringUtility.hasText(cleanHtml)) {
+          cleanHtml = "<html><head></head><body></body></html>";
+        }
+      }
       else {
         /*
          * Because @{link SwtScoutHtmlField} is file based, it is crucial to set the content-type and charset appropriately.
          * Also, the CSS needs not to be cleaned as the native browser is used.
          */
-        rawHtml = HTMLUtility.cleanupHtml(rawHtml, true, false, createDefaultFontSettings(uiComposite.getSwtField()));
-        }
+        cleanHtml = HTMLUtility.cleanupHtml(cleanHtml, true, false, createDefaultFontSettings(uiComposite.getSwtField()));
       }
-      return rawHtml;
     }
+    return cleanHtml;
+  }
 
   /**
    * Get SWT specific default font settings
@@ -639,6 +644,7 @@ public abstract class AbstractSwtEnvironment extends AbstractPropertyObserver im
   protected DefaultFont createDefaultFontSettings(Control control) {
     DefaultFont defaultFont = new DefaultFont();
     defaultFont.setSize(12);
+    defaultFont.setSizeUnit("px");
     defaultFont.setForegroundColor(0x000000);
     defaultFont.setFamily("sans-serif");
 
