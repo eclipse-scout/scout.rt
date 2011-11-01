e2c962bc95babc085b7703f401dda0e42ff60b80
Matthias Villiger <mvi@bsiag.com>
2011-11-01 09:18:19 +0000

    Bugzilla 361816: "Replace current NLS support with Text Provider Services": abstract implementation for documentation text providers added (to be used in SDK as default super type for doc text services).

---
+++ org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/services/common/text/AbstractDynamicNlsDocumentationTextProviderService.java
@@ -0,0 +1,8 @@
+package org.eclipse.scout.rt.shared.services.common.text;
+
+/**
+ * Abstract implementation for documentation text provider services.
+ */
+public abstract class AbstractDynamicNlsDocumentationTextProviderService extends AbstractDynamicNlsTextProviderService implements IDocumentationTextProviderService {
+
+}
