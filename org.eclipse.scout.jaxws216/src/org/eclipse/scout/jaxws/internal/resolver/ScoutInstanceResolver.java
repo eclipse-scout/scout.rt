5620156727cb8247433f8fcc7426133cbbe92472
Daniel Wiehl <daniel.wiehl@bsiag.com>
2012-03-20 22:12:16 +0000

    20.03.2012 dwi Bugzilla: 374837 Problem: WebServiceContext cannot be injected into port type with @Resource annotation. That would be necessary to access message context and security information relative to a request being served. Solution: Fixed instance resolver to respect JAX-WS resource injection. Migration: None

--- org.eclipse.scout.jaxws216/src/org/eclipse/scout/jaxws/internal/resolver/ScoutInstanceResolver.java
+++ org.eclipse.scout.jaxws216/src/org/eclipse/scout/jaxws/internal/resolver/ScoutInstanceResolver.java
@@ -36,49 +36,68 @@ import org.eclipse.scout.rt.server.ServerJob;
 import org.eclipse.scout.rt.server.ThreadContext;
 
 import com.sun.xml.internal.ws.api.message.Packet;
-import com.sun.xml.internal.ws.api.server.InstanceResolver;
 import com.sun.xml.internal.ws.api.server.Invoker;
 import com.sun.xml.internal.ws.api.server.WSEndpoint;
 import com.sun.xml.internal.ws.api.server.WSWebServiceContext;
+import com.sun.xml.internal.ws.server.AbstractMultiInstanceResolver;
 
 /**
- * This resolver intercepts webservice requests prior to being propagated to the webservice implementation in order to
- * bind the call to a context.
+ * This resolver intercepts webservice requests prior to being propagated to the port type in order to
+ * bind the call to a {@link IServerSession} context.
  */
-public class ScoutInstanceResolver extends InstanceResolver<Object> {
+public class ScoutInstanceResolver<T> extends AbstractMultiInstanceResolver<T> {
 
   private static final IScoutLogger LOG = ScoutLogManager.getLogger(ScoutInstanceResolver.class);
 
-  private Class<?> m_portTypeClass;
   private IServerSessionFactory m_sessionFactory;
 
-  public ScoutInstanceResolver(Class portTypeClass) {
-    m_portTypeClass = portTypeClass;
-    if (m_portTypeClass == null) {
+  public ScoutInstanceResolver(Class<T> portTypeClass) {
+    super(portTypeClass);
+    if (portTypeClass == null) {
       throw new WebServiceException("No port type class configured in sun-jaxws.xml");
-    }
-
-    ScoutWebService annotation = m_portTypeClass.getAnnotation(ScoutWebService.class);
-    if (annotation == null) {
-      return;
-    }
-    try {
-      m_sessionFactory = annotation.sessionFactory().newInstance();
-    }
-    catch (Exception e) {
-      LOG.error("Error occured while creating session factory.", e);
     }
   }
 
   @Override
-  public Object resolve(Packet packet) {
+  public void start(WSWebServiceContext context, WSEndpoint endpoint) {
+    m_sessionFactory = createSessionFactory(clazz);
+    super.start(context, endpoint);
+  }
+
+  @Override
+  public T resolve(Packet packet) {
+    return super.create(); // creates a new port type instance, injects the @{WebServiceContext} and invokes the method annotated with @{link PostConstruct}
+  }
+
+  protected IServerSessionFactory createSessionFactory(Class<?> portTypeClass) {
+    ScoutWebService annotation = portTypeClass.getAnnotation(ScoutWebService.class);
+    if (annotation == null) {
+      return null;
+    }
     try {
-      return m_portTypeClass.newInstance();
+      return annotation.sessionFactory().newInstance();
     }
     catch (Exception e) {
-      LOG.error("Could not create instance of port type '" + m_portTypeClass.getName() + "'");
+      LOG.error("Error occured while creating session factory.", e);
     }
     return null;
+  }
+
+  protected IServerSession getSession(MessageContext context) {
+    if (m_sessionFactory == null) {
+      return null;
+    }
+    // Prefer cached session over creating a new one.
+    // However, the session is only considered if created by the same type of factory.
+    // This is to ensure a proper session context which is what the user is expecting.
+    IServerSession contextSession = ContextHelper.getContextSession(context);
+    Class<? extends IServerSessionFactory> contextSessionFactory = ContextHelper.getContextSessionFactoryClass(context);
+    if (contextSession == null || !CompareUtility.equals(m_sessionFactory.getClass(), contextSessionFactory)) {
+      // create a new session
+      return SessionHelper.createNewServerSession(m_sessionFactory);
+    }
+    // cached session
+    return contextSession;
   }
 
   @Override
@@ -86,18 +105,25 @@ public class ScoutInstanceResolver extends InstanceResolver<Object> {
     return new P_Invoker();
   }
 
-  private class P_Invoker extends Invoker {
+  protected class P_Invoker extends Invoker {
 
-    private WSWebServiceContext m_serviceContext;
+    protected WSWebServiceContext m_context;
 
     @Override
-    public void start(WSWebServiceContext serviceContext, WSEndpoint endpoint) {
-      m_serviceContext = serviceContext;
+    public void start(final WSWebServiceContext context, final WSEndpoint endpoint) {
+      m_context = context;
+      ScoutInstanceResolver.this.start(context, endpoint);
+    }
+
+    @Override
+    public void dispose() {
+      m_context = null;
+      ScoutInstanceResolver.this.dispose();
     }
 
     @Override
     public Object invoke(final Packet packet, final Method method, final Object... aobj) throws InvocationTargetException, IllegalAccessException {
-      final Object portType = resolve(packet);
+      final T portType = ScoutInstanceResolver.this.resolve(packet);
       if (portType == null) {
         throw new WebServiceException("No port type found");
       }
@@ -112,25 +138,8 @@ public class ScoutInstanceResolver extends InstanceResolver<Object> {
       if (subject == null) {
         throw new WebServiceException("Webservice request was NOT dispatched due to security reasons: request must run on behalf of a subject context.");
       }
-
-      IServerSession serverSession = null;
-      if (m_sessionFactory != null) {
-        MessageContext context = m_serviceContext.getMessageContext();
-        // Prefer cached session over creating a new one.
-        // However, the session is only considered if created by the same type of factory.
-        // This is to ensure a proper session context which is what the user is expecting.
-        IServerSession contextSession = ContextHelper.getContextSession(context);
-        Class<? extends IServerSessionFactory> contextSessionFactory = ContextHelper.getContextSessionFactoryClass(context);
-        if (contextSession == null || !CompareUtility.equals(m_sessionFactory.getClass(), contextSessionFactory)) {
-          // create a new session
-          serverSession = SessionHelper.createNewServerSession(m_sessionFactory);
-        }
-        else {
-          // cached session
-          serverSession = contextSession;
-        }
-      }
-      if (serverSession == null) {
+      IServerSession session = getSession(m_context.getMessageContext());
+      if (session == null) {
         LOG.warn("Webservice request is not run in a session context as no server session is configured.");
         return method.invoke(portType, aobj);
       }
@@ -141,7 +150,7 @@ public class ScoutInstanceResolver extends InstanceResolver<Object> {
         final Holder<IllegalAccessException> illegalAccessExceptionHolder = new Holder<IllegalAccessException>(IllegalAccessException.class);
         final Holder<RuntimeException> runtimeExceptionHolder = new Holder<RuntimeException>(RuntimeException.class);
         // run server job
-        ServerJob serverJob = new ServerJob("Server Job", serverSession, subject) {
+        ServerJob serverJob = new ServerJob("Tx", session, subject) {
 
           @Override
           protected IStatus runTransaction(IProgressMonitor monitor) throws Exception {
