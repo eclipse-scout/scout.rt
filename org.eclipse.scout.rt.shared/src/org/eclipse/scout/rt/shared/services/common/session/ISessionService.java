3859e57435337525f51e819e300e8844681c27ad
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2012-04-25 11:46:13 +0000

    Bug 377552 -Accessing session from shared plugin not possible
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=377552
    
    Added some java doc.

--- org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/services/common/session/ISessionService.java
+++ org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/services/common/session/ISessionService.java
@@ -19,5 +19,13 @@ import org.eclipse.scout.service.IService2;
  */
 @Priority(-3)
 public interface ISessionService extends IService2 {
+
+  /**
+   * Returns the session which is assigned to the current client respectively server job.
+   * <p>
+   * It's recommended to use this service only if you don't have direct access to the session. Rather use
+   * ClientJob.getCurrentSession() respectively ServerJob.getCurrentSession().
+   * </p>
+   */
   ISession getCurrentSession();
 }
