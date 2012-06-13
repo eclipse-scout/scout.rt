3a524676905b3a9e8560f26b9c5fd3a2a60435dd
Stephan Leicht <stephan.leicht@bsiag.com>
2012-06-13 16:33:29 +0000

    ASSIGNED - bug 382519: [RAP] extract theme to a separate plugin https://bugs.eclipse.org/bugs/show_bug.cgi?id=382519

--- org.eclipse.scout.rt.ui.rap.theme.rayo/src/org/eclipse/scout/rt/ui/rap/rayo/Activator.java
+++ org.eclipse.scout.rt.ui.rap.theme.rayo/src/org/eclipse/scout/rt/ui/rap/rayo/Activator.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.scout.rt.ui.rap.rayo;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -8,43 +18,26 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.scout.rt.ui.rap.rayo"; //$NON-NLS-1$
+  public static final String PLUGIN_ID = "org.eclipse.scout.rt.ui.rap.rayo"; //$NON-NLS-1$
 
-	// The shared instance
-	private static Activator plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
+  private static Activator m_plugin;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
+  public Activator() {
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+  public static Activator getDefault() {
+    return m_plugin;
+  }
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
+  @Override
+  public void start(BundleContext context) throws Exception {
+    super.start(context);
+    m_plugin = this;
+  }
 
+  @Override
+  public void stop(BundleContext context) throws Exception {
+    m_plugin = null;
+    super.stop(context);
+  }
 }
