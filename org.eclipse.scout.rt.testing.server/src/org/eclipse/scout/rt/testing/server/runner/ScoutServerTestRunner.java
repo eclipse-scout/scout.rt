f5840f5f7f7d0984b32a043471471779a8b79ca0
Judith Gull <jgu@bsiag.com>
2012-07-23 14:10:45 +0000

    testrunner should be compatible with junit 4.5 in eclipse 3.5

--- org.eclipse.scout.rt.testing.server/src/org/eclipse/scout/rt/testing/server/runner/ScoutServerTestRunner.java
+++ org.eclipse.scout.rt.testing.server/src/org/eclipse/scout/rt/testing/server/runner/ScoutServerTestRunner.java
@@ -14,6 +14,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.security.auth.Subject;
@@ -105,7 +106,9 @@ public class ScoutServerTestRunner extends BlockJUnit4ClassRunner {
       throw e;
     }
     catch (Exception e) {
-      throw new InitializationError(e);
+      List<Throwable> errors = new ArrayList<Throwable>();
+      errors.add(e);
+      throw new InitializationError(errors);
     }
   }
 
