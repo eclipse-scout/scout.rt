638825f8eebe1e534d5e9cc77106d7975b976e4c
Ivan Motsch <ivan.motsch@bsiag.com>
2012-08-15 13:38:52 +0000

    bug 387283

--- org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/window/desktop/layout/CellSplit.java
+++ org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/window/desktop/layout/CellSplit.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -50,6 +50,9 @@ class CellSplit {
   }
 
   public void setLocation(int loc) {
+    if (loc == m_location) {
+      return;
+    }
     m_location = loc;
     if (m_strategy != null) {
       m_strategy.setSplitLocation(m_row, m_col, loc);
