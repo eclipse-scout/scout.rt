6936c7c4d4e4a23d198744d4c631a6d39dda5b8e
Andreas Bur <andi.bur@gmail.com>
2012-08-17 14:57:37 +0000

    bug 354120: AbstractTable: Cell icon not considered when wrapping text https://bugs.eclipse.org/bugs/show_bug.cgi?id=354120
    merged with branch 3.8

--- org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/SwingLayoutUtility.java
+++ org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/SwingLayoutUtility.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -100,7 +100,7 @@ public final class SwingLayoutUtility {
         availTextWidth = viewR.width;
       }
       else {
-        availTextWidth = viewR.width - (iconR.width + gap);
+        availTextWidth = viewR.width - (((icon == null) ? 0 : icon.getIconWidth()) + gap);
       }
       v.setSize(availTextWidth, 0);
       //PATCH END
@@ -166,7 +166,7 @@ public final class SwingLayoutUtility {
         getSize(c, MIN),
         getSize(c, PREF),
         getSize(c, MAX)
-        };
+    };
     // validation
     if (d[MIN].width > d[PREF].width) {
       d[MIN].width = d[PREF].width;
