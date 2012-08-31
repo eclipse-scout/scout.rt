f13b6647d2000526d6008d788cfd056790132b6a
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2012-08-31 08:34:57 +0000

    Bug 388452 -SetCustomProperty, getCustomProperty missing on ITable, ITree, IAction
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=388452

--- org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/basic/tree/AbstractTree.java
+++ org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/basic/tree/AbstractTree.java
@@ -556,6 +556,21 @@ public abstract class AbstractTree extends AbstractPropertyObserver implements I
   }
 
   @Override
+  public Object getProperty(String name) {
+    return propertySupport.getProperty(name);
+  }
+
+  @Override
+  public void setProperty(String name, Object value) {
+    propertySupport.setProperty(name, value);
+  }
+
+  @Override
+  public boolean hasProperty(String name) {
+    return propertySupport.hasProperty(name);
+  }
+
+  @Override
   public String getTitle() {
     return propertySupport.getPropertyString(PROP_TITLE);
   }
--- org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/form/fields/IFormField.java
+++ org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/form/fields/IFormField.java
@@ -380,9 +380,30 @@ public interface IFormField extends IPropertyObserver {
 
   void setLabelSuppressed(boolean b);
 
+  /**
+   * @deprecated Use {@link #getProperty(String)} instead.
+   */
+  @Deprecated
   Object getCustomProperty(String propName);
 
+  /**
+   * @deprecated Use {@link #setProperty(String, Object)} instead.
+   */
+  @Deprecated
   void setCustomProperty(String propName, Object o);
+
+  Object getProperty(String name);
+
+  /**
+   * With this method it's possible to set (custom) properties.
+   * <p>
+   * <b>Important: </b> Although this method is intended to be used for custom properties, it's actually possible to
+   * change main properties as well. Keep in mind that directly changing main properties may result in unexpected
+   * behavior, so do it only if you really know what you are doing. Rather use the officially provided api instead. <br>
+   * Example for an unexpected behavior: setVisible() does not only set the property PROP_VISIBLE but also executes
+   * additional code. This code would NOT be executed by directly setting the property PROP_VISIBLE with setProperty().
+   */
+  void setProperty(String name, Object value);
 
   boolean isInitialized();
 
