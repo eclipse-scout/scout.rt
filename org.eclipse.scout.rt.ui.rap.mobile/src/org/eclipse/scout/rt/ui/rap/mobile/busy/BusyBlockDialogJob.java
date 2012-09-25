1d99b464dd5f1d97b47b2d5df9dbf269b76de795
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2012-09-25 16:49:29 +0000

    [Bug 378151] Rap: Support for mobile devices
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=378151
    
    Fixed NullPointerException which occured if a part got closed right before the busy block dialog was supposed to be shown.

--- org.eclipse.scout.rt.ui.rap.mobile/src/org/eclipse/scout/rt/ui/rap/mobile/busy/BusyBlockDialogJob.java
+++ org.eclipse.scout.rt.ui.rap.mobile/src/org/eclipse/scout/rt/ui/rap/mobile/busy/BusyBlockDialogJob.java
@@ -10,25 +10,22 @@
  ******************************************************************************/
 package org.eclipse.scout.rt.ui.rap.mobile.busy;
 
-import java.util.List;
-
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.scout.commons.holders.Holder;
 import org.eclipse.scout.commons.logger.IScoutLogger;
 import org.eclipse.scout.commons.logger.ScoutLogManager;
 import org.eclipse.scout.rt.client.busy.BusyJob;
 import org.eclipse.scout.rt.ui.rap.busy.RwtBusyHandler;
-import org.eclipse.scout.rt.ui.rap.window.IRwtScoutPart;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
 public class BusyBlockDialogJob extends BusyJob {
   private static final IScoutLogger LOG = ScoutLogManager.getLogger(BusyBlockDialogJob.class);
-  private final List<IRwtScoutPart> m_parts;
 
-  public BusyBlockDialogJob(String name, RwtBusyHandler handler, List<IRwtScoutPart> parts) {
+  public BusyBlockDialogJob(String name, RwtBusyHandler handler) {
     super(name, handler);
     setSystem(true);
-    m_parts = parts;
   }
 
   @Override
@@ -43,20 +40,17 @@ public class BusyBlockDialogJob extends BusyJob {
 
   @Override
   protected void runBlocking(final IProgressMonitor monitor) {
-    if (m_parts == null || m_parts.size() == 0) {
-      return;
-    }
     final Display display = getBusyHandler().getDisplay();
     final Holder<BusyBlockDialog> dialogHolder = new Holder<BusyBlockDialog>(BusyBlockDialog.class);
     try {
       display.syncExec(new Runnable() {
         @Override
         public void run() {
-          IRwtScoutPart activePart = m_parts.get(0);
-          if (activePart.getUiContainer() == null || activePart.getUiContainer().isDisposed()) {
+          Shell parentShell = getBusyHandler().getUiEnvironment().getParentShellIgnoringPopups(SWT.SYSTEM_MODAL | SWT.APPLICATION_MODAL | SWT.MODELESS);
+          if (parentShell == null || parentShell.isDisposed()) {
             return;
           }
-          BusyBlockDialog dialog = new BusyBlockDialog(activePart.getUiContainer().getShell(), getBusyHandler().getUiEnvironment(), monitor);
+          BusyBlockDialog dialog = new BusyBlockDialog(parentShell, getBusyHandler().getUiEnvironment(), monitor);
           dialog.open();
           dialogHolder.setValue(dialog);
         }
@@ -66,7 +60,7 @@ public class BusyBlockDialogJob extends BusyJob {
       //
     }
     finally {
-      display.syncExec(new Runnable() {
+      display.asyncExec(new Runnable() {
         @Override
         public void run() {
           BusyBlockDialog dialog = dialogHolder.getValue();
