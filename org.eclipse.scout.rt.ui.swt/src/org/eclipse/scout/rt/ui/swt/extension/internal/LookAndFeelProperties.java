23ea35964761c2fe46038e56cfcdffe5b7c144a5
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2012-09-27 15:02:42 +0000

    Bug 387972 -SWT: Multiselect in tables by moving over table with pressed mouse
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=387972

--- org.eclipse.scout.rt.ui.swt/src/org/eclipse/scout/rt/ui/swt/extension/internal/LookAndFeelProperties.java
+++ org.eclipse.scout.rt.ui.swt/src/org/eclipse/scout/rt/ui/swt/extension/internal/LookAndFeelProperties.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -12,13 +12,16 @@ package org.eclipse.scout.rt.ui.swt.extension.internal;
 
 import java.util.HashMap;
 
+import org.eclipse.scout.commons.logger.IScoutLogger;
+import org.eclipse.scout.commons.logger.ScoutLogManager;
 import org.eclipse.scout.rt.ui.swt.extension.ILookAndFeelProperties;
 
 public class LookAndFeelProperties implements ILookAndFeelProperties {
+  private static IScoutLogger LOG = ScoutLogManager.getLogger(LookAndFeelProperties.class);
 
+  private String m_contributor;
   private int m_scope;
-
-  HashMap<String, Object> m_values = new HashMap<String, Object>();
+  private HashMap<String, String> m_values = new HashMap<String, String>();
 
   @Override
   public int getScope() {
@@ -29,40 +32,51 @@ public class LookAndFeelProperties implements ILookAndFeelProperties {
     m_scope = scope;
   }
 
-  public void setPropertyInt(String name, int value) {
-    m_values.put(name, value);
-  }
-
   @Override
   public int getPropertyInt(String name) {
-    Object object = m_values.get(name);
-    if (object != null) {
-      return ((Integer) object).intValue();
+    String value = m_values.get(name);
+    if (value == null) {
+      return 0;
     }
-    return 0;
+
+    try {
+      return Integer.parseInt(value);
+    }
+    catch (NumberFormatException e) {
+      LOG.error("Could not parse extension look and feel of contributor '" + m_contributor + "' property name='" + name + "' value='" + value + "'");
+      return 0;
+    }
   }
 
   public void setPropertyString(String name, String value) {
     m_values.put(name, value);
   }
 
+  public void setContributor(String contributor) {
+    m_contributor = contributor;
+  }
+
+  public String getContributor() {
+    return m_contributor;
+  }
+
   @Override
-  public String getPropertyString(String name) {
-    Object object = m_values.get(name);
-    if (object != null) {
-      return (String) object;
+  public boolean getPropertyBool(String name) {
+    String value = m_values.get(name);
+    if (value == null) {
+      return false;
     }
-    return null;
+
+    return Boolean.parseBoolean(value);
   }
 
   public void setProperty(String name, String value) {
     m_values.put(name, value);
-    // if(name.equals(PROP_COLOR_FOREGROUND_DISABLED)){
-    // setPropertyString(name, value);
-    // }else{
-    // setPropertyInt(name, Integer.parseInt(value));
-    // }
+  }
 
+  @Override
+  public String getPropertyString(String name) {
+    return m_values.get(name);
   }
 
 }
