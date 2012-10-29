34d97976fa8f51a77e6d2e6d84faebd34a8e7607
Stephan Leicht Vogt <stephan.leicht@bsiag.com>
2012-10-29 10:30:46 +0100

    new scout.rt.shared test fragment

---
+++ org.eclipse.scout.rt.shared.test/src/org/eclipse/scout/rt/shared/services/lookup/LookupRowTest.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2010,2012 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.shared.services.lookup;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LookupRowTest {
+
+  @Test
+  public void testSerializeDeserialize() throws Exception {
+    LookupRow row = new LookupRow("key", "text");
+
+    ByteArrayOutputStream bout = new ByteArrayOutputStream();
+    ObjectOutputStream oout = new ObjectOutputStream(bout);
+    oout.writeObject(row);
+    oout.close();
+
+    byte[] serializedData = bout.toByteArray();
+
+    ByteArrayInputStream bin = new ByteArrayInputStream(serializedData);
+    ObjectInputStream oin = new ObjectInputStream(bin);
+    Object obj = oin.readObject();
+    oin.close();
+
+    Assert.assertNotNull(obj);
+    Assert.assertTrue(obj instanceof LookupRow);
+    LookupRow deserializedRow = (LookupRow) obj;
+    Assert.assertEquals("key", deserializedRow.getKey());
+    Assert.assertEquals("text", deserializedRow.getText());
+  }
+}
