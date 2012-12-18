eclipse-scout/scout.rt

commit 22ec38143d9a8638f37acbb4e3c1e0743deb5522

resolved some warnings

org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/servicetunnel/LenientPermissionWrapper.java
@@ -64,12 +64,12 @@ public class LenientPermissionWrapper implements Serializable {
 
   private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
     //no call to defaultReadObject
+    final BundleObjectInputStream bundleIn = (in instanceof BundleObjectInputStream ? (BundleObjectInputStream) in : null);
     try {
       ObjectInputStream.GetField gfields = in.readFields();
       m_className = (String) gfields.get("m_className", (String) null);
       byte[] data = (byte[]) gfields.get("m_permission", (byte[]) null);
       //
-      final BundleObjectInputStream bundleIn = (in instanceof BundleObjectInputStream ? (BundleObjectInputStream) in : null);
       ObjectInputStream localIn = new ObjectInputStream(new ByteArrayInputStream(data)) {
         @Override
         protected Class<?> resolveClass(ObjectStreamClass desc) throws IOException, ClassNotFoundException {
@@ -85,6 +85,11 @@ public class LenientPermissionWrapper implements Serializable {
     }
     catch (Throwable t) {
       LOG.warn("cannot deserialize permission", t);
+    }
+    finally {
+      if (bundleIn != null) {
+        bundleIn.close();
+      }
     }
   }
 

org.eclipse.scout.commons.test/src/org/eclipse/scout/commons/MailUtilityTest.java
@@ -20,6 +20,7 @@ import org.junit.Test;
 /**
  *
  */
+@SuppressWarnings("restriction")
 public class MailUtilityTest {
 
   /**

org.eclipse.scout.commons.test/src/org/eclipse/scout/commons/TypeCastUtilityTest.java
@@ -25,10 +25,10 @@ public class TypeCastUtilityTest extends Assert {
       handler.setLevel(Level.FINEST);
     }
     */
-    Class<?> T;
+    Class<?> genericT;
     //
-    T = TypeCastUtility.getGenericsParameterClass(LongArrayHolder.class, IHolder.class, 0);
-    assertEquals(Long[].class, T);
+    genericT = TypeCastUtility.getGenericsParameterClass(LongArrayHolder.class, IHolder.class, 0);
+    assertEquals(Long[].class, genericT);
     //
     try {
       TypeCastUtility.getGenericsParameterClass(X0.class, X0.class, 0);
@@ -44,14 +44,14 @@ public class TypeCastUtilityTest extends Assert {
     catch (IllegalArgumentException t) {
       //nop
     }
-    T = TypeCastUtility.getGenericsParameterClass(X2.class, X0.class, 0);
-    assertEquals(Data2.class, T);
+    genericT = TypeCastUtility.getGenericsParameterClass(X2.class, X0.class, 0);
+    assertEquals(Data2.class, genericT);
     //
-    T = TypeCastUtility.getGenericsParameterClass(Y3.class, Y0.class, 0);
-    assertEquals(Data3.class, T);
+    genericT = TypeCastUtility.getGenericsParameterClass(Y3.class, Y0.class, 0);
+    assertEquals(Data3.class, genericT);
     //
-    T = TypeCastUtility.getGenericsParameterClass(Y3.class, Y0.class, 1);
-    assertEquals(Flavor3.class, T);
+    genericT = TypeCastUtility.getGenericsParameterClass(Y3.class, Y0.class, 1);
+    assertEquals(Flavor3.class, genericT);
   }
 
   static class Data0 {

org.eclipse.scout.commons/src/org/eclipse/scout/commons/CharsetSafeMimeMessage.java
@@ -20,6 +20,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeUtility;
 
+@SuppressWarnings("restriction")
 public class CharsetSafeMimeMessage extends MimeMessage {
   private String m_charset;
 

org.eclipse.scout.commons/src/org/eclipse/scout/commons/FileUtility.java
@@ -35,6 +35,7 @@ import java.util.zip.ZipEntry;
  * @since 1.0
  */
 public final class FileUtility {
+  static final int KILO_BYTE = 1024;
 
   private FileUtility() {
   }
@@ -339,7 +340,7 @@ public final class FileUtility {
           output = new FileOutputStream(dest).getChannel();
 
           try {
-            int maxCount = (mbCount * 1024 * 1024) - (32 * 1024);
+            int maxCount = (mbCount * KILO_BYTE * KILO_BYTE) - (32 * KILO_BYTE);
             long size = input.size();
             long position = 0;
             while (position < size) {

org.eclipse.scout.commons/src/org/eclipse/scout/commons/MailUtility.java
@@ -50,6 +50,7 @@ import org.eclipse.scout.commons.exception.ProcessingException;
 import org.eclipse.scout.commons.logger.IScoutLogger;
 import org.eclipse.scout.commons.logger.ScoutLogManager;
 
+@SuppressWarnings("restriction")
 public final class MailUtility {
 
   public static final IScoutLogger LOG = ScoutLogManager.getLogger(MailUtility.class);

org.eclipse.scout.commons/src/org/eclipse/scout/commons/RFCWrapperPart.java
@@ -20,6 +20,7 @@ import javax.mail.MessagingException;
 import javax.mail.Multipart;
 import javax.mail.Part;
 
+@SuppressWarnings("restriction")
 public class RFCWrapperPart implements Part {
   private final Part m_rfcPart;
   private final String m_fileName;

org.eclipse.scout.commons/src/org/eclipse/scout/commons/beans/BasicPropertySupport.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -19,6 +19,7 @@ import java.util.ConcurrentModificationException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.scout.commons.CompareUtility;
@@ -39,19 +40,19 @@ public class BasicPropertySupport implements IEventListenerSource {
 
   public static final int DEFAULT_INT_VALUE = 0;
   public static final int DEFAULT_DOUBLE_VALUE = 0;
-  public static final Integer DEFAULT_INT = new Integer(DEFAULT_INT_VALUE);
-  public static final Double DEFAULT_DOUBLE = new Double(DEFAULT_DOUBLE_VALUE);
+  public static final Integer DEFAULT_INT = Integer.valueOf(DEFAULT_INT_VALUE);
+  public static final Double DEFAULT_DOUBLE = Double.valueOf(DEFAULT_DOUBLE_VALUE);
   public static final long DEFAULT_LONG_VALUE = DEFAULT_INT_VALUE;
-  public static final Long DEFAULT_LONG = new Long(DEFAULT_LONG_VALUE);
-  private static final Boolean DEFAULT_BOOL = new Boolean(false);
-  private HashMap<String, Object> m_props = new HashMap<String, Object>();
+  public static final Long DEFAULT_LONG = Long.valueOf(DEFAULT_LONG_VALUE);
+  private static final Boolean DEFAULT_BOOL = Boolean.FALSE;
+  private Map<String, Object> m_props = new HashMap<String, Object>();
   private Object m_source;
   // observer
   private Object m_listenerLock = new Object();
-  private ArrayList<Object> m_listeners;
-  private HashMap<String, ArrayList<Object>> m_childListeners;
+  private List<Object> m_listeners;
+  private Map<String, List<Object>> m_childListeners;
   private int m_propertiesChanging;
-  private ArrayList<PropertyChangeEvent> m_propertyEventBuffer;
+  private List<PropertyChangeEvent> m_propertyEventBuffer;
 
   public BasicPropertySupport(Object sourceBean) {
     m_source = sourceBean;
@@ -74,7 +75,7 @@ public class BasicPropertySupport implements IEventListenerSource {
         }
       }
       if (m_childListeners != null) {
-        for (Map.Entry<String, ArrayList<Object>> e : m_childListeners.entrySet()) {
+        for (Map.Entry<String, List<Object>> e : m_childListeners.entrySet()) {
           String context = e.getKey();
           for (Object o : e.getValue()) {
             if (o instanceof WeakReference) {
@@ -141,7 +142,7 @@ public class BasicPropertySupport implements IEventListenerSource {
   }
 
   public void setPropertyInt(String name, int i) {
-    setProperty(name, new Integer(i), DEFAULT_INT);
+    setProperty(name, Integer.valueOf(i), DEFAULT_INT);
   }
 
   public int getPropertyInt(String name) {
@@ -159,7 +160,7 @@ public class BasicPropertySupport implements IEventListenerSource {
   }
 
   public void setPropertyLong(String name, long i) {
-    setProperty(name, new Long(i), DEFAULT_LONG);
+    setProperty(name, Long.valueOf(i), DEFAULT_LONG);
   }
 
   public long getPropertyLong(String name) {
@@ -168,7 +169,7 @@ public class BasicPropertySupport implements IEventListenerSource {
   }
 
   public boolean setPropertyBool(String name, boolean b) {
-    return setProperty(name, new Boolean(b), DEFAULT_BOOL);
+    return setProperty(name, Boolean.valueOf(b), DEFAULT_BOOL);
   }
 
   public boolean getPropertyBool(String name) {
@@ -270,7 +271,7 @@ public class BasicPropertySupport implements IEventListenerSource {
       synchronized (m_listenerLock) {
         removeFromListNoLock(m_listeners, listener);
         if (m_childListeners != null) {
-          for (ArrayList childList : m_childListeners.values()) {
+          for (List childList : m_childListeners.values()) {
             removeFromListNoLock(childList, listener);
           }
         }
@@ -281,9 +282,9 @@ public class BasicPropertySupport implements IEventListenerSource {
   public void addPropertyChangeListener(String propertyName, PropertyChangeListener listener) {
     synchronized (m_listenerLock) {
       if (m_childListeners == null) {
-        m_childListeners = new HashMap<String, ArrayList<Object>>();
+        m_childListeners = new HashMap<String, List<Object>>();
       }
-      ArrayList<Object> children = m_childListeners.get(propertyName);
+      List<Object> children = m_childListeners.get(propertyName);
       if (children == null) {
         children = new ArrayList<Object>();
         m_childListeners.put(propertyName, children);
@@ -295,7 +296,7 @@ public class BasicPropertySupport implements IEventListenerSource {
   public void removePropertyChangeListener(String propertyName, PropertyChangeListener listener) {
     synchronized (m_listenerLock) {
       if (m_childListeners != null) {
-        ArrayList childList = m_childListeners.get(propertyName);
+        List childList = m_childListeners.get(propertyName);
         if (childList != null) {
           removeFromListNoLock(childList, listener);
         }
@@ -303,7 +304,7 @@ public class BasicPropertySupport implements IEventListenerSource {
     }
   }
 
-  private void removeFromListNoLock(ArrayList listeners, PropertyChangeListener listener) {
+  private void removeFromListNoLock(List listeners, PropertyChangeListener listener) {
     if (listeners == null) {
       return;
     }
@@ -321,8 +322,8 @@ public class BasicPropertySupport implements IEventListenerSource {
     else {
       listeners.remove(listener);
     }
-    if (listeners.size() == 0) {
-      listeners.trimToSize();
+    if (listeners.size() == 0 && listeners instanceof ArrayList) {
+      ((ArrayList) listeners).trimToSize();
     }
   }
 
@@ -334,7 +335,7 @@ public class BasicPropertySupport implements IEventListenerSource {
     if (oldValue == newValue) {
       return;
     }
-    firePropertyChangeImpl(propertyName, new Integer(oldValue), new Integer(newValue));
+    firePropertyChangeImpl(propertyName, Integer.valueOf(oldValue), Integer.valueOf(newValue));
   }
 
   public void firePropertyChange(String propertyName, boolean oldValue, boolean newValue) {
@@ -352,8 +353,8 @@ public class BasicPropertySupport implements IEventListenerSource {
   }
 
   private void firePropertyChangeImpl(String propertyName, Object oldValue, Object newValue) {
-    ArrayList l = m_listeners;
-    HashMap m = m_childListeners;
+    List l = m_listeners;
+    Map m = m_childListeners;
     if ((l != null && l.size() > 0) || (m != null && m.size() > 0)) {
       PropertyChangeEvent e = new PropertyChangeEvent(m_source, propertyName, oldValue, newValue);
       firePropertyChangeImpl(e);
@@ -389,7 +390,7 @@ public class BasicPropertySupport implements IEventListenerSource {
         }
         String propertyName = e.getPropertyName();
         if (propertyName != null && m_childListeners != null) {
-          ArrayList childListeners = m_childListeners.get(propertyName);
+          List childListeners = m_childListeners.get(propertyName);
           if (childListeners != null) {
             for (Object o : childListeners) {
               if (o instanceof WeakReference) {
@@ -441,8 +442,8 @@ public class BasicPropertySupport implements IEventListenerSource {
 
   public boolean hasListeners(String propertyName) {
     synchronized (m_listenerLock) {
-      ArrayList l0 = m_listeners;
-      ArrayList l1 = null;
+      List l0 = m_listeners;
+      List l1 = null;
       if (propertyName != null) {
         l1 = m_childListeners.get(propertyName);
       }

commit 0ad3fc1a7d3cb5a96c5d8283816eaf4d1c694a85

Error introduced by 22ec38143d9a8638f37acbb4e3c1e0743deb5522 resolved.

org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/servicetunnel/LenientPermissionWrapper.java
@@ -62,18 +62,17 @@ public class LenientPermissionWrapper implements Serializable {
     out.writeFields();
   }
 
-  private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+  private void readObject(final ObjectInputStream in) throws IOException, ClassNotFoundException {
     //no call to defaultReadObject
-    final BundleObjectInputStream bundleIn = (in instanceof BundleObjectInputStream ? (BundleObjectInputStream) in : null);
     try {
       ObjectInputStream.GetField gfields = in.readFields();
       m_className = (String) gfields.get("m_className", (String) null);
       byte[] data = (byte[]) gfields.get("m_permission", (byte[]) null);
-      //
+
       ObjectInputStream localIn = new ObjectInputStream(new ByteArrayInputStream(data)) {
         @Override
         protected Class<?> resolveClass(ObjectStreamClass desc) throws IOException, ClassNotFoundException {
-          return bundleIn != null ? bundleIn.resolveClass(desc) : Class.forName(desc.getName());
+          return (in instanceof BundleObjectInputStream) ? ((BundleObjectInputStream) in).resolveClass(desc) : Class.forName(desc.getName());
         }
       };
       try {
@@ -86,11 +85,5 @@ public class LenientPermissionWrapper implements Serializable {
     catch (Throwable t) {
       LOG.warn("cannot deserialize permission", t);
     }
-    finally {
-      if (bundleIn != null) {
-        bundleIn.close();
-      }
-    }
   }
-
 }