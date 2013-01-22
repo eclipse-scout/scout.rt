85bc5d860e0589c7928c1d42075fd7569c2b335e
Judtih Gull <jgu@bsiag.com>
2013-01-22 18:00:03 +0100

    updates javadoc for date fields

--- org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/form/fields/datefield/AbstractUTCDateField.java
+++ org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/form/fields/datefield/AbstractUTCDateField.java
@@ -12,26 +12,13 @@ package org.eclipse.scout.rt.client.ui.form.fields.datefield;
 
 import java.util.Date;
 
+import org.eclipse.scout.commons.UTCDate;
 import org.eclipse.scout.commons.annotations.FormData;
 import org.eclipse.scout.commons.annotations.FormData.SdkCommand;
 import org.eclipse.scout.rt.shared.data.form.fields.AbstractUTCFieldData;
 
 /**
- * A Value field for date (and time) values dependent on time zone.
- * <p>
- * It uses {@link org.eclipse.scout.rt.shared.data.form.fields.AbstractUTCFieldData AbstractUTCFieldData} with a
- * {@link org.eclipse.scout.commons.UTCDate UTCDate} in order to provide a time zone dependent date object as field
- * data.
- * </p>
- * <p>
- * <strong>Note:</strong> By default, all objects of type {@link java.util.Date} - except
- * {@link org.eclipse.scout.commons.UTCDate UTCDate} - are converted to
- * {@link org.eclipse.scout.rt.shared.servicetunnel.StaticDate StaticDate} during serialization and converted back to
- * <code>Date</code> objects during de-serialization in order to be independent of time zone and daylight saving time.
- * </p>
- * 
- * @see org.eclipse.scout.rt.client.ui.form.fields.datefield.AbstractDateField AbstractDateField
- * @see org.eclipse.scout.rt.shared.servicetunnel.ServiceTunnelObjectReplacer ServiceTunnelObjectReplacer
+ * Date field with override on {@link #getHolderType()} to {@link UTCDate}
  */
 @FormData(value = AbstractUTCFieldData.class, sdkCommand = SdkCommand.USE)
 public abstract class AbstractUTCDateField extends AbstractDateField implements IUTCDateField {
