b7a923fcf665b5926d324a0cfa8d6538b2388c0f
Andreas Bur <andi.bur@gmail.com>
2013-03-15 09:40:09 +0000

    bug 403171: Fix and improve bean-based table field data generation https://bugs.eclipse.org/bugs/show_bug.cgi?id=403171

--- org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/data/form/fields/tablefield/AbstractTableFieldBeanData.java
+++ org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/data/form/fields/tablefield/AbstractTableFieldBeanData.java
@@ -20,6 +20,18 @@ import org.eclipse.scout.rt.shared.data.form.fields.AbstractFormFieldData;
 /**
  * Form field data for table fields. This class uses {@link AbstractTableRowData} beans for storing the table's
  * contents.
+ * <p/>
+ * {@link #isValueSet()} does not behave the same as on {@link AbstractTableFieldData}. It does not track any
+ * modifications of table row properties or their row states. The following methods may change the result of
+ * {@link #isValueSet()}:
+ * <ul>
+ * <li>{@link #addRow()}</li>
+ * <li>{@link #addRow(int)}</li>
+ * <li>{@link #setRows(AbstractTableRowData[])}</li>
+ * <li>{@link #removeRow(AbstractTableRowData)}</li>
+ * <li>{@link #removeRow(int)}</li>
+ * <li>{@link #clearRows()}</li>
+ * </ul>
  * 
  * @since 3.8.2
  */
