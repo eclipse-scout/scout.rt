23cf3d8ac570296bfd43d18da56f5406247eca79
Reto Aschwanden <ras@bsiag.com>
2013-05-16 09:24:15 +0000

    bug 408209: RwtScoutSmartField does not always update scout model in case text is deleted
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=408209
    
    (cherry picked from commit 91198176c826f68349f67de3f6448cc552285344)
    
    Change-Id: I0ee9826d11ce09afe60865ab60873dde28e219e8
    Reviewed-on: https://git.eclipse.org/r/13005
    Tested-by: Hudson CI
    Reviewed-by: Stephan Leicht Vogt <stephan.leicht@bsiag.com>
    IP-Clean: Stephan Leicht Vogt <stephan.leicht@bsiag.com>

--- org.eclipse.scout.rt.ui.rap/src/org/eclipse/scout/rt/ui/rap/form/fields/smartfield/RwtScoutSmartField.java
+++ org.eclipse.scout.rt.ui.rap/src/org/eclipse/scout/rt/ui/rap/form/fields/smartfield/RwtScoutSmartField.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 BSI Business Systems Integration AG.
+ * Copyright (c) 2011,2013 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -555,7 +555,7 @@ public class RwtScoutSmartField extends RwtScoutValueFieldComposite<ISmartField<
     if (getUpdateUiFromScoutLock().isAcquired()) {
       return;
     }
-    if (getUiField().isVisible() && getUiField().isFocusControl()) {
+    if (getUiField().isVisible()) {
       String text = getUiField().getText();
       if (text == null || text.length() == 0) {
         // allow empty field without proposal
