b1eeb319a384796d881df985c5be75388d81b04f
Ken Lee <kle@bsiag.com>
2013-08-19 20:04:58 +0200

    Bug 387625 - SWT: Switching to an outline results in closing of table page
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=387625
    
    Removes workaround introduced in bug 406764 since E4 bug 385618 has been fixed.
    
    (cherry picked from commit 886466d15c8a0fa4b42f8c0ce4c1203c4f712a38)
    
    Change-Id: If8623c1a9120903fcc751f485498fdbe70a0b7ae
    Reviewed-on: https://git.eclipse.org/r/15616
    Tested-by: Hudson CI
    Reviewed-by: Ken Lee <kle@bsiag.com>
    IP-Clean: Ken Lee <kle@bsiag.com>

--- org.eclipse.scout.rt.ui.swt/src/org/eclipse/scout/rt/ui/swt/AbstractSwtEnvironment.java
+++ org.eclipse.scout.rt.ui.swt/src/org/eclipse/scout/rt/ui/swt/AbstractSwtEnvironment.java
@@ -24,7 +24,6 @@ import java.util.Map.Entry;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.scout.commons.CompareUtility;
@@ -136,17 +135,6 @@ import org.osgi.framework.Version;
  */
 public abstract class AbstractSwtEnvironment extends AbstractPropertyObserver implements ISwtEnvironment {
   private static IScoutLogger LOG = ScoutLogManager.getLogger(AbstractSwtEnvironment.class);
-  private static boolean IS_E4 = false;
-
-  static {
-    Bundle bundle = Platform.getBundle("org.eclipse.platform");
-    if (bundle != null) {
-      Object bundleVersion = bundle.getHeaders().get("Bundle-Version");
-      if (bundleVersion instanceof String) {
-        IS_E4 = CompareUtility.compareTo((String) bundleVersion, "4.0") >= 0;
-      }
-    }
-  }
 
   private final Bundle m_applicationBundle;
 
@@ -1134,19 +1122,7 @@ public abstract class AbstractSwtEnvironment extends AbstractPropertyObserver im
     }
     final ISwtScoutPart part = m_openForms.remove(form);
     if (part != null && part.getForm().equals(form)) {
-      // Workaround for bugzilla 385618 & 408741
-      if (IS_E4) {
-        getDisplay().asyncExec(new Runnable() {
-          @Override
-          public void run() {
-            closePart(part);
-          }
-        });
-      }
-      // Workaround end
-      else {
-        closePart(part);
-      }
+      closePart(part);
     }
   }
 
