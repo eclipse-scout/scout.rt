074045a47e118ee0f407b99e07510a8a0e3bf674
Matthias Nick <Matthias.Nick@bsiag.com>
2013-09-05 12:37:04 +0200

    bug 371090: NullPointerException in SwingUtilities2Wrapper.drawStringUnderlineCharAt with Java 7 https://bugs.eclipse.org/bugs/show_bug.cgi?id=371090
    added deprecated statement
    
    Change-Id: I2e1ae0d376c6e490258a1501c3fdcdba14bd4870
    
    
    Change-Id: I2e1ae0d376c6e490258a1501c3fdcdba14bd4870
    Signed-off-by: Matthias Nick <Matthias.Nick@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/16153
    Tested-by: Hudson CI
    IP-Clean: Judith Gull <jgu@bsiag.com>
    Reviewed-by: Judith Gull <jgu@bsiag.com>

--- org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/internal/SwingUtilities2Wrapper.java
+++ org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/internal/SwingUtilities2Wrapper.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -20,8 +20,11 @@ import javax.swing.SwingUtilities;
  * Reflect wrapper to SwingUtilities2 in order to prevent incompatibility issues
  * when upgrading to higher java versions before 1.6
  * com.sun.java.swing.SwingUtilities2 with 1.6 sun.swing.SwingUtilities2
+ * 
+ * @deprecated Will be removed in Release 3.11
  */
 @SuppressWarnings("unchecked")
+@Deprecated
 public final class SwingUtilities2Wrapper {
   private static Class SwingUtilities2Class;
   private static Method drawStringUnderlineCharAtMethod;
