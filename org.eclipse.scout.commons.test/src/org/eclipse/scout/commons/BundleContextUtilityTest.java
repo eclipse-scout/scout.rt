84253a658c8297a5de85b1f8d4f0adda33b6b075
Judith Gull <jgu@bsiag.com>
2013-09-17 17:35:11 +0200

    Bug 408304 - make swing plugin work without OSGI
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=408304
    
    Activator is always startet when running tests in ci
    
    Change-Id: I87334aa904a83358b66ce5c55741b03bc4bd4cde
    Reviewed-on: https://git.eclipse.org/r/16510
    Tested-by: Hudson CI
    Reviewed-by: Judith Gull <jgu@bsiag.com>
    IP-Clean: Judith Gull <jgu@bsiag.com>

--- org.eclipse.scout.commons.test/src/org/eclipse/scout/commons/BundleContextUtilityTest.java
+++ org.eclipse.scout.commons.test/src/org/eclipse/scout/commons/BundleContextUtilityTest.java
@@ -1,15 +1,7 @@
 package org.eclipse.scout.commons;
 
-import java.lang.reflect.Field;
-
-import org.easymock.EasyMock;
-import org.eclipse.core.runtime.Plugin;
-import org.eclipse.scout.commons.internal.Activator;
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
 
 /**
  * Tests for class {@link BundleContextUtility}.
@@ -19,59 +11,15 @@ import org.osgi.framework.BundleContext;
  */
 public class BundleContextUtilityTest {
 
-  Activator activator = new Activator();
-
-  @Test
-  public void testGetBundleContextProperty_NoActivator() {
-    Assert.assertNull(BundleContextUtility.getProperty("foo"));
-  }
-
-  @Test
-  public void testGetBundleContextProperty_WithActivator() throws Exception {
-    Bundle bundle = EasyMock.createMock(Bundle.class);
-    BundleContext bundleContext = EasyMock.createMock(BundleContext.class);
-
-    // The first two calls are required because of the static initializer
-    // block of SwingUtility
-    bundle.getBundleContext();
-    EasyMock.expectLastCall().andReturn(bundleContext);
-    EasyMock.expect(bundleContext.getProperty("scout.ui.layout.resetBoundsOnInvalidate")).andReturn("true");
-
-    bundle.getBundleContext();
-    EasyMock.expectLastCall().andReturn(bundleContext);
-    EasyMock.expect(bundleContext.getProperty("scout.ui.verifyInputOnWindowClosed")).andReturn("false");
-
-    bundle.getBundleContext();
-    EasyMock.expectLastCall().andReturn(bundleContext);
-    EasyMock.expect(bundleContext.getProperty("foo")).andReturn("bar");
-    EasyMock.replay(bundleContext, bundle);
-
-    // Prepare the Activator so it looks like it has been initialized by the
-    // OSGI framework.
-    setPlugin(activator);
-    Field f = Plugin.class.getDeclaredField("bundle");
-    f.setAccessible(true);
-    f.set(activator, bundle);
-
-    Assert.assertEquals("bar", BundleContextUtility.getProperty("foo"));
-  }
-
-  private void setPlugin(Activator activator) throws Exception {
-    Field f = Activator.class.getDeclaredField("m_plugin");
-    f.setAccessible(true);
-    f.set(Activator.class, activator);
-  }
-
   /**
-   * Reset the Activator after each test method, so the order in which the
-   * methods are executed does not matter. Otherwise the test <code>testGetBundleContextProperty_NoActivator</code>
-   * could fail.
-   * 
-   * @throws Exception
+   * Test for {@link BundleContextUtility#parseBooleanProperty(String, boolean)} tycho-surefire-plugin tests are always
+   * executed in an OSGi environment. Therefore plain junit tests are not
+   * included here.
    */
-  @After
-  public void tearDown() throws Exception {
-    setPlugin(null);
+  @Test
+  public void testGetBundleContextProperty_WithActivator() {
+    final String testProperty = "osgi.install.area";
+    String property = BundleContextUtility.getProperty(testProperty);
+    Assert.assertNotNull(property);
   }
-
 }
