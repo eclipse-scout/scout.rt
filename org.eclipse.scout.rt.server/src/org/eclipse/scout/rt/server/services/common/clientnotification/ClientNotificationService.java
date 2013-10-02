5870ad325479701acbfd82bb4dc4ae20dd912ab8
Christoph Schwank <christoph.schwank@bsiag.com>
2013-10-02 15:31:57 +0200

    NEW - bug 418501: export ClientNotificationService to allow project to extend the service https://bugs.eclipse.org/bugs/show_bug.cgi?id=418501
    Change-Id: I5e93ee2a70f7c3728ba06fd07bbce636937fad8f
    Reviewed-on: https://git.eclipse.org/r/16960
    Tested-by: Hudson CI
    Reviewed-by: Christoph Schwank <christoph.schwank@bsiag.com>
    IP-Clean: Christoph Schwank <christoph.schwank@bsiag.com>

--- org.eclipse.scout.rt.server/src/org/eclipse/scout/rt/server/services/common/clientnotification/ClientNotificationService.java
+++ org.eclipse.scout.rt.server/src/org/eclipse/scout/rt/server/services/common/clientnotification/ClientNotificationService.java
@@ -4,17 +4,15 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
-package org.eclipse.scout.rt.server.services.common.clientnotification.internal;
+package org.eclipse.scout.rt.server.services.common.clientnotification;
 
 import org.eclipse.scout.commons.logger.IScoutLogger;
 import org.eclipse.scout.commons.logger.ScoutLogManager;
-import org.eclipse.scout.rt.server.services.common.clientnotification.IClientNotificationFilter;
-import org.eclipse.scout.rt.server.services.common.clientnotification.IClientNotificationQueueListener;
-import org.eclipse.scout.rt.server.services.common.clientnotification.IClientNotificationService;
+import org.eclipse.scout.rt.server.services.common.clientnotification.internal.ClientNotificationQueue;
 import org.eclipse.scout.rt.shared.services.common.clientnotification.IClientNotification;
 import org.eclipse.scout.rt.shared.servicetunnel.RemoteServiceAccessDenied;
 import org.eclipse.scout.service.AbstractService;
