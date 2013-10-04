5f7f4e762527c52f489b8c71df4b2ae09e46e9ba
Ken Lee <kle@bsiag.com>
2013-10-04 17:10:21 +0200

    Bug 417584 - CBI Build: Create release branch for Kepler SR2
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=417584
    
    Updates OfficialVersion to 3.9.2
    
    Change-Id: Ia99a269a2657a7ffd03ecd9ffb1028022605c76b
    Reviewed-on: https://git.eclipse.org/r/17035
    Tested-by: Hudson CI
    Reviewed-by: Ken Lee <kle@bsiag.com>
    IP-Clean: Ken Lee <kle@bsiag.com>

--- org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/OfficialVersion.java
+++ org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/OfficialVersion.java
@@ -17,7 +17,7 @@ public final class OfficialVersion {
   private OfficialVersion() {
   }
 
-  public static final String VERSION = "3.9.1";
+  public static final String VERSION = "3.9.2";
 
   public static final String COPYRIGHT = "Scout " + VERSION + ", &copy; BSI Business Systems Integration AG " + 2001 + "," + Calendar.getInstance().get(Calendar.YEAR) + " EPL";
 
