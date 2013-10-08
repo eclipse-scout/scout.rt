18b33ddb72e5127221ef4c22ec8c4dd18b307956
Bruno Koeferli <bruno.koeferli@bsiag.com>
2013-10-08 13:44:39 +0200

    Bug 418907 - add support for removeCustomColumnValue(String columnId) to AbstractTableRowData
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=418907
    
    Change-Id: I17044f075771cd66306c0485c8f2ed5c6436137f
    Reviewed-on: https://git.eclipse.org/r/17157
    Tested-by: Bruno  K??ferli <bruno.koeferli@bsiag.com>
    Reviewed-by: Andi Bur <andi.bur@gmail.com>
    IP-Clean: Andi Bur <andi.bur@gmail.com>

---
+++ org.eclipse.scout.rt.shared.test/src/org/eclipse/scout/rt/shared/data/basic/table/AbstractTableRowDataTest.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2010 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.shared.data.basic.table;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AbstractTableRowDataTest {
+
+  private static final String TEST_COLUMN_ID = "testColumnId";
+
+  private TestingTableRowData m_rowData;
+
+  @Before
+  public void before() {
+    m_rowData = new TestingTableRowData();
+  }
+
+  @Test
+  public void testSetCustomColumnValue() {
+    assertNull(m_rowData.getCustomColumnValues());
+    assertNull(m_rowData.getCustomColumnValue(TEST_COLUMN_ID));
+
+    String value = "TEST";
+    m_rowData.setCustomColumnValue(TEST_COLUMN_ID, value);
+    assertNotNull(m_rowData.getCustomColumnValues());
+    assertEquals(value, m_rowData.getCustomColumnValue(TEST_COLUMN_ID));
+  }
+
+  @Test
+  public void testRemoveCustomColumnValue() {
+    assertNull(m_rowData.getCustomColumnValues());
+    assertNull(m_rowData.removeCustomColumnValue(TEST_COLUMN_ID));
+    assertNull(m_rowData.getCustomColumnValues());
+
+    String value = "TEST";
+    m_rowData.setCustomColumnValue(TEST_COLUMN_ID, value);
+    assertNotNull(m_rowData.getCustomColumnValues());
+    assertEquals(value, m_rowData.removeCustomColumnValue(TEST_COLUMN_ID));
+    assertNull(m_rowData.getCustomColumnValues());
+  }
+
+  private static class TestingTableRowData extends AbstractTableRowData {
+    private static final long serialVersionUID = 1L;
+  }
+}
--- org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/data/basic/table/AbstractTableRowData.java
+++ org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/data/basic/table/AbstractTableRowData.java
@@ -103,16 +103,14 @@ public abstract class AbstractTableRowData implements Serializable {
 
   /**
    * Sets a custom column value for the given <code>columnId</code>. If <code>value</code> is <code>null</code>, the
-   * custom column entry is removed from the map.
+   * custom column entry is removed by {@link #removeCustomColumnValue(String)}.
    * 
    * @param columnId
    * @param value
    */
   public void setCustomColumnValue(String columnId, Object value) {
     if (value == null) {
-      if (m_customColumnValues != null) {
-        m_customColumnValues.remove(columnId);
-      }
+      removeCustomColumnValue(columnId);
       return;
     }
 
@@ -120,5 +118,28 @@ public abstract class AbstractTableRowData implements Serializable {
       m_customColumnValues = new HashMap<String, Object>();
     }
     m_customColumnValues.put(columnId, value);
+  }
+
+  /**
+   * Removes the custom column value from the map.
+   * <p>
+   * Returns the custom column value to which the map previously associated the <code>columnId</code>, or
+   * <code>null</code> if the map contained no mapping for the <code>columnId</code>.
+   * </p>
+   * 
+   * @param columnId
+   *          columnId whose mapping is to be removed
+   * @return the previous custom column value associated with <code>columnId</code>, or <code>null</code> if there was
+   *         no mapping for <code>columnId</code>.
+   */
+  public Object removeCustomColumnValue(String columnId) {
+    if (m_customColumnValues == null) {
+      return null;
+    }
+    Object value = m_customColumnValues.remove(columnId);
+    if (m_customColumnValues.isEmpty()) {
+      m_customColumnValues = null;
+    }
+    return value;
   }
 }
