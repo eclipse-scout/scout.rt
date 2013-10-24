e4bcdf89ec62b9bd1dcd28b5ffe9e0d0c53bf34a
Matthias Nick <Matthias.Nick@bsiag.com>
2013-10-24 14:53:13 +0200

    bug 420267: SWING mnemonics on menus not working properly
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=420267
    
    Mnemonic working on lowercase letters as well
    
    Change-Id: I601d2bcfae4f36c03383b636a27e60562c980193
    Signed-off-by: Matthias Nick <Matthias.Nick@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/17726
    Tested-by: Hudson CI
    Reviewed-by: Ken Lee <kle@bsiag.com>
    IP-Clean: Ken Lee <kle@bsiag.com>

--- org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/action/SwingScoutAction.java
+++ org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/action/SwingScoutAction.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -119,6 +119,7 @@ public class SwingScoutAction<T extends IAction> extends SwingScoutComposite<T> 
 
   private void setMnemonicFromScout(char ch) {
     if (ch != 0x0) {
+      ch = Character.toUpperCase(ch); //mnemonics in Swing must be upper case
       getSwingAction().putValue(Action.MNEMONIC_KEY, new Integer(ch));
     }
   }
