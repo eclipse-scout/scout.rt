b5507065ae195141c942599a324932666716df6c
Stephan Leicht Vogt <stephan.leicht@bsiag.com>
2013-11-25 09:26:46 +0000

    bug 422444: [Commons] Base64Utility decode uses havyweight String Regex function https://bugs.eclipse.org/bugs/show_bug.cgi?id=422444
    Havyweight replaceAll function replaced with lightweight if-call.

--- org.eclipse.scout.commons/src/org/eclipse/scout/commons/Base64Utility.java
+++ org.eclipse.scout.commons/src/org/eclipse/scout/commons/Base64Utility.java
@@ -4,13 +4,21 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
 package org.eclipse.scout.commons;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.eclipse.scout.commons.logger.IScoutLogger;
+import org.eclipse.scout.commons.logger.ScoutLogManager;
+
 public final class Base64Utility {
+  private static final IScoutLogger LOG = ScoutLogManager.getLogger(Base64Utility.class);
 
   private Base64Utility() {
   }
@@ -71,32 +79,64 @@ public final class Base64Utility {
    * @return A <code>byte[]</code> containing the decoded data block.
    */
   public static byte[] decode(String string) {
-    string = string.replaceAll("[^A-Za-z0-9+/=]", "");
-    int length = string.length();
+    int length = string == null ? 0 : string.length();
     if (length == 0) {
       return new byte[0];
     }
-    int pad = (string.charAt(length - 2) == '=') ? 2 :
-        (string.charAt(length - 1) == '=') ? 1 : 0;
-    int size = length * 3 / 4 - pad;
-    byte[] buffer = new byte[size];
-    int block;
-    int i = 0;
-    int index = 0;
-    while (i < length) {
-      block = (ALPHABET.indexOf(string.charAt(i++)) & 0xff) << 18 |
-          (ALPHABET.indexOf(string.charAt(i++)) & 0xff) << 12 |
-          (ALPHABET.indexOf(string.charAt(i++)) & 0xff) << 6 |
-          (ALPHABET.indexOf(string.charAt(i++)) & 0xff);
-      buffer[index++] = (byte) (block >>> 16);
-      if (index < size) {
-        buffer[index++] = (byte) ((block >>> 8) & 0xff);
-      }
-      if (index < size) {
-        buffer[index++] = (byte) (block & 0xff);
+    P_Base64InputStream is = new P_Base64InputStream(string);
+    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+    int c1, c2, c3, c4;
+    try {
+      c1 = is.read();
+      c2 = is.read();
+      c3 = is.read();
+      c4 = is.read();
+      while (c1 >= 0 || c2 >= 0 || c3 >= 0 || c4 >= 0) {
+        int block;
+        block = (ALPHABET.indexOf((char) c1) & 0xff) << 18 |
+            (ALPHABET.indexOf((char) c2) & 0xff) << 12 |
+            (ALPHABET.indexOf((char) c3) & 0xff) << 6 |
+            (ALPHABET.indexOf((char) c4) & 0xff);
+        buffer.write((byte) (block >>> 16));
+        if (c3 != -1) {
+          buffer.write((byte) ((block >>> 8) & 0xff));
+        }
+        if (c4 != -1) {
+          buffer.write((byte) (block & 0xff));
+        }
+        c1 = is.read();
+        c2 = is.read();
+        c3 = is.read();
+        c4 = is.read();
       }
     }
-    return buffer;
+    catch (IOException e) {
+      LOG.error("IOException in Base64Utility.decode()", e);
+      return new byte[0];
+    }
+    return buffer.toByteArray();
   }
 
+  private static class P_Base64InputStream extends InputStream {
+    private final String m_buffer;
+    private final int m_count;
+    private int m_pos = 0;
+
+    public P_Base64InputStream(String base64String) {
+      m_buffer = base64String;
+      m_count = base64String.length();
+    }
+
+    @Override
+    public int read() throws IOException {
+      while (m_pos < m_count) {
+        char ch = m_buffer.charAt(m_pos++);
+        if ((ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch == '+') || (ch == '/')) {
+          return (ch & 0xFF);
+        }
+      }
+      return -1;
+    }
+  }
 }
