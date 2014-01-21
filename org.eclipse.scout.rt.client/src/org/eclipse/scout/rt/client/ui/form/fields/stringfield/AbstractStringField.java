a4319de5be19994005702bc166451009bf30641a
Adrian Moser <amo@bsiag.com>
2014-01-21 15:04:17 +0100

    Revert "Bug 381318 - Add Javadoc to getConfigured-Properties and exec-Operations"
    This reverts commit ba05603800fea75743bef051404687d477244c15.

--- org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/form/fields/stringfield/AbstractStringField.java
+++ org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/form/fields/stringfield/AbstractStringField.java
@@ -63,10 +63,6 @@ public abstract class AbstractStringField extends AbstractBasicField<String> imp
     return false;
   }
 
-  /**
-   * @return true if all characters should be transformed to lower case when typing text into the string field. default
-   *         is false.
-   */
   @ConfigProperty(ConfigProperty.BOOLEAN)
   @Order(280)
   protected boolean getConfiguredFormatLower() {
@@ -86,10 +82,6 @@ public abstract class AbstractStringField extends AbstractBasicField<String> imp
     return false;
   }
 
-  /**
-   * @return true if all characters should be transformed to upper case when typing text into the string field. default
-   *         is false.
-   */
   @ConfigProperty(ConfigProperty.BOOLEAN)
   @Order(300)
   protected boolean getConfiguredFormatUpper() {
