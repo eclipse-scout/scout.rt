b57223557a00401e7824011e041743f171a86938
Ivan Motsch <ivan.motsch@bsiag.com>
2014-02-18 13:14:15 +0100

    cleanup bench.js

--- org.eclipse.scout.rt.ui.html/org.eclipse.scout.ui.html/resources/widgets/Bench.js
+++ org.eclipse.scout.rt.ui.html/org.eclipse.scout.ui.html/resources/widgets/Bench.js
@@ -12,32 +12,3 @@ Scout.Desktop.Bench = function (scout, $desktop) {
   this.$div = $desktopBench;
 };
 
-//
-// desktop table namespace and element
-//
-
-Scout.Desktop.Table = function (scout, $bench) {
-  //create container
-  var $desktopTable = $bench.appendDiv('DesktopTable');
-  $desktopTable.appendDiv('DesktopTableHeader');
-  $desktopTable.appendDiv('DesktopTableData');
-  $desktopTable.appendDiv('DesktopTableFooter');
-
-  var $tableControl = $desktopTable.appendDiv('DesktopTableControl');
-  $tableControl.appendDiv('ControlResizeTop');
-  $tableControl.appendDiv('ControlResizeBottom');
-  $tableControl.appendDiv('ControlGraph');
-  $tableControl.appendDiv('ControlChart');
-  $tableControl.appendDiv('ControlMap');
-  $tableControl.appendDiv('ControlOrg');
-  $tableControl.appendDiv('ControlLabel');
-
-  var $tableControlInfo = $tableControl.appendDiv('ControlInfo');
-  $tableControlInfo.appendDiv('ControlInfoSelect');
-  $tableControlInfo.appendDiv('ControlInfoFilter');
-  $tableControlInfo.appendDiv('ControlInfoMore');
-  $tableControlInfo.appendDiv('ControlInfoLoad');
-
-  // set this for later usage
-  this.$div = $desktopTable;
-};
