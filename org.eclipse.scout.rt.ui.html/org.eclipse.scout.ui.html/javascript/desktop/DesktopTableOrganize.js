935ebe668137218dc714035f2625fb6c6d0351bb
Christian Rusche <christian.rusche@bsi-software.com>
2014-02-21 17:50:03 +0100

    json ui: load data model

--- org.eclipse.scout.rt.ui.html/org.eclipse.scout.ui.html/javascript/desktop/DesktopTableOrganize.js
+++ org.eclipse.scout.rt.ui.html/org.eclipse.scout.ui.html/javascript/desktop/DesktopTableOrganize.js
@@ -1,5 +1,9 @@
 // SCOUT GUI
 // (c) Copyright 2013-2014, BSI Business Systems Integration AG
 
-Scout.DesktopTableOrganize = function ($controlContainer, columns, data) {
+Scout.DesktopTableOrganize = function (scout, $controlContainer, model, columns, data) {
+  var response = scout.syncAjax('dataModel', model.outlineId, {"nodeId": model.nodeId});
+  var dataModel = response.events[0].dataModel;
+  log(dataModel);
+
 };
