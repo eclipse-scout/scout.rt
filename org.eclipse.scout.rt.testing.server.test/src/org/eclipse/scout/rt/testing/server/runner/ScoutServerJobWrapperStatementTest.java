776ba6fbcc580df91d37b6cde3910782b4ede55f
Adrian Moser <amo@bsiag.com>
2014-03-18 15:32:58 +0100

    Bug 355194 - Server Test framework: test throwing exception not always marked as failed
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=355194
    
    Change-Id: I314864da12944d9052c3c2cca85893f814216406
    Reviewed-on: https://git.eclipse.org/r/23542
    Tested-by: Hudson CI
    Reviewed-by: Adrian Moser <amo@bsiag.com>
    IP-Clean: Adrian Moser <amo@bsiag.com>
    Tested-by: Adrian Moser <amo@bsiag.com>

---
+++ org.eclipse.scout.rt.testing.server.test/src/org/eclipse/scout/rt/testing/server/runner/ScoutServerJobWrapperStatementTest.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2010 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.testing.server.runner;
+
+import org.eclipse.scout.commons.exception.ProcessingException;
+import org.eclipse.scout.commons.exception.VetoException;
+import org.eclipse.scout.rt.server.ServerJob;
+import org.eclipse.scout.rt.testing.shared.DevTestMarker;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.model.Statement;
+
+/**
+ *
+ */
+@RunWith(ScoutServerTestRunner.class)
+@DevTestMarker
+public class ScoutServerJobWrapperStatementTest {
+
+  @Test(expected = ProcessingException.class)
+  public void testEvaluate1() throws Throwable {
+    ScoutServerJobWrapperStatement statement = new ScoutServerJobWrapperStatement(ServerJob.getCurrentSession(), ServerJob.getCurrentSession().getSubject(), new Statement() {
+      @Override
+      public void evaluate() throws Throwable {
+        throw new ProcessingException("TestException");
+      }
+    });
+    statement.evaluate();
+  }
+
+  @Test(expected = Throwable.class)
+  public void testEvaluate2() throws Throwable {
+    ScoutServerJobWrapperStatement statement = new ScoutServerJobWrapperStatement(ServerJob.getCurrentSession(), ServerJob.getCurrentSession().getSubject(), new Statement() {
+      @Override
+      public void evaluate() throws Throwable {
+        throw new Throwable("Test");
+      }
+    });
+    statement.evaluate();
+  }
+
+  @Test(expected = VetoException.class)
+  public void testEvaluate3() throws Exception {
+    throw new VetoException("Test");
+  }
+
+  @Test(expected = ProcessingException.class)
+  public void testEvaluate4() throws Exception {
+    throw new ProcessingException("Test");
+  }
+
+}
