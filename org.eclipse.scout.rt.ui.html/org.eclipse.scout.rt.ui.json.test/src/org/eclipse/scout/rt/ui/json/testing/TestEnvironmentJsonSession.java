7e50dbacad61e2d2b6c4e0317427f27903b32020
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2014-03-20 10:24:58 +0100

    JsonUi: Temporary removed dependency to testenvironment
    Reason: the plugin is not available in the target platform. We need to
    fix that first.
    Change-Id: I2c158b9f82f5128091e2af83f97d629b0a93aa93

--- org.eclipse.scout.rt.ui.html/org.eclipse.scout.rt.ui.json.test/src/org/eclipse/scout/rt/ui/json/testing/TestEnvironmentJsonSession.java
+++ org.eclipse.scout.rt.ui.html/org.eclipse.scout.rt.ui.json.test/src/org/eclipse/scout/rt/ui/json/testing/TestEnvironmentJsonSession.java
@@ -1,42 +1,42 @@
-/*******************************************************************************
- * Copyright (c) 2010 BSI Business Systems Integration AG.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     BSI Business Systems Integration AG - initial API and implementation
- ******************************************************************************/
-package org.eclipse.scout.rt.ui.json.testing;
-
-import java.util.Locale;
-
-import javax.security.auth.Subject;
-
-import org.eclipse.scout.commons.security.SimplePrincipal;
-import org.eclipse.scout.rt.client.IClientSession;
-import org.eclipse.scout.rt.client.testenvironment.TestEnvironmentClientSession;
-import org.eclipse.scout.rt.shared.ui.UserAgent;
-import org.eclipse.scout.rt.ui.json.AbstractJsonSession;
-
-public class TestEnvironmentJsonSession extends AbstractJsonSession {
-
-  @Override
-  protected Class<? extends IClientSession> clientSessionClass() {
-    return null;
-  }
-
-  @Override
-  protected IClientSession createClientSession(UserAgent userAgent, Subject subject, Locale locale) {
-    return TestEnvironmentClientSession.get();
-  }
-
-  @Override
-  protected Subject initSubject() {
-    Subject subject = new Subject();
-    subject.getPrincipals().add(new SimplePrincipal("tester"));
-    return subject;
-  }
-
-}
+///*******************************************************************************
+// * Copyright (c) 2010 BSI Business Systems Integration AG.
+// * All rights reserved. This program and the accompanying materials
+// * are made available under the terms of the Eclipse Public License v1.0
+// * which accompanies this distribution, and is available at
+// * http://www.eclipse.org/legal/epl-v10.html
+// *
+// * Contributors:
+// *     BSI Business Systems Integration AG - initial API and implementation
+// ******************************************************************************/
+//package org.eclipse.scout.rt.ui.json.testing;
+//
+//import java.util.Locale;
+//
+//import javax.security.auth.Subject;
+//
+//import org.eclipse.scout.commons.security.SimplePrincipal;
+//import org.eclipse.scout.rt.client.IClientSession;
+//import org.eclipse.scout.rt.client.testenvironment.TestEnvironmentClientSession;
+//import org.eclipse.scout.rt.shared.ui.UserAgent;
+//import org.eclipse.scout.rt.ui.json.AbstractJsonSession;
+//
+//public class TestEnvironmentJsonSession extends AbstractJsonSession {
+//
+//  @Override
+//  protected Class<? extends IClientSession> clientSessionClass() {
+//    return null;
+//  }
+//
+//  @Override
+//  protected IClientSession createClientSession(UserAgent userAgent, Subject subject, Locale locale) {
+//    return TestEnvironmentClientSession.get();
+//  }
+//
+//  @Override
+//  protected Subject initSubject() {
+//    Subject subject = new Subject();
+//    subject.getPrincipals().add(new SimplePrincipal("tester"));
+//    return subject;
+//  }
+//
+//}
