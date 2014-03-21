1e7fe442b39168cfaf48f7f55af8f88f5b786ce1
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2014-03-21 09:50:28 +0100

    JsonUi: Added tests for desktopTree

--- org.eclipse.scout.rt.ui.html/org.eclipse.scout.rt.ui.json.test/src/org/eclipse/scout/rt/ui/json/JsonDesktopTreeTest.java
+++ org.eclipse.scout.rt.ui.html/org.eclipse.scout.rt.ui.json.test/src/org/eclipse/scout/rt/ui/json/JsonDesktopTreeTest.java
@@ -1,106 +1,106 @@
-///*******************************************************************************
-// * Copyright (c) 2010 BSI Business Systems Integration AG.
-// * All rights reserved. This program and the accompanying materials
-// * are made available under the terms of the Eclipse Public License v1.0
-// * which accompanies this distribution, and is available at
-// * http://www.eclipse.org/legal/epl-v10.html
-// *
-// * Contributors:
-// *     BSI Business Systems Integration AG - initial API and implementation
-// ******************************************************************************/
-//package org.eclipse.scout.rt.ui.json;
-//
-//import static org.eclipse.scout.rt.ui.json.JsonDesktopTreeTestUtil.createJsonDesktopTreeWithMocks;
-//import static org.eclipse.scout.rt.ui.json.JsonDesktopTreeTestUtil.createJsonSelectedEvent;
-//import static org.eclipse.scout.rt.ui.json.testing.JsonTestUtility.extractEventsFromResponse;
-//
-//import java.util.LinkedList;
-//import java.util.List;
-//
-//import org.eclipse.scout.commons.exception.ProcessingException;
-//import org.eclipse.scout.rt.client.ui.basic.tree.ITreeNode;
-//import org.eclipse.scout.rt.client.ui.desktop.outline.IOutline;
-//import org.eclipse.scout.rt.client.ui.desktop.outline.pages.IPage;
-//import org.eclipse.scout.rt.ui.json.desktop.fixtures.NodePage;
-//import org.eclipse.scout.rt.ui.json.desktop.fixtures.Outline;
-//import org.eclipse.scout.rt.ui.json.desktop.fixtures.OutlineWithOneNode;
-//import org.eclipse.scout.testing.client.runner.ScoutClientTestRunner;
-//import org.json.JSONException;
-//import org.json.JSONObject;
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//
-//@RunWith(ScoutClientTestRunner.class)
-//public class JsonDesktopTreeTest {
-//
-//  /**
-//   * Tests whether the model node gets correctly selected
-//   */
-//  @Test
-//  public void testSelectionEvent() throws ProcessingException, JSONException {
-//    IOutline outline = new OutlineWithOneNode();
-//    ITreeNode node = outline.getRootNode().getChildNode(0);
-//    Assert.assertFalse(node.isSelectedNode());
-//
-//    JsonDesktopTree jsonDesktopTree = createJsonDesktopTreeWithMocks(outline);
-//
-//    JsonEvent event = createJsonSelectedEvent(jsonDesktopTree.getOrCreatedNodeId(node));
-//    jsonDesktopTree.handleUiEvent(event, new JsonResponse());
-//
-//    Assert.assertTrue(node.isSelectedNode());
-//  }
-//
-//  /**
-//   * Response must not contain the selection event if the selection was triggered by the request
-//   */
-//  @Test
-//  public void testIgnorableSelectionEvent() throws ProcessingException, JSONException {
-//    IOutline outline = new OutlineWithOneNode();
-//    ITreeNode node = outline.getRootNode().getChildNode(0);
-//
-//    JsonDesktopTree jsonDesktopTree = createJsonDesktopTreeWithMocks(outline);
-//
-//    JsonEvent event = createJsonSelectedEvent(jsonDesktopTree.getOrCreatedNodeId(node));
-//    jsonDesktopTree.handleUiEvent(event, new JsonResponse());
-//
-//    List<JSONObject> responseEvents = extractEventsFromResponse(jsonDesktopTree.getJsonSession().currentJsonResponse(), JsonDesktopTree.EVENT_NODES_SELECTED);
-//    Assert.assertTrue(responseEvents.size() == 0);
-//  }
-//
-//  /**
-//   * If the selection event triggers the selection of another node, the selection event must not be ignored.
-//   */
-//  @Test
-//  public void testIgnorableSelectionEvent2() throws ProcessingException, JSONException {
-//    List<IPage> pages = new LinkedList<>();
-//    final NodePage firstNode = new NodePage();
-//    NodePage secondNode = new NodePage() {
-//      @Override
-//      protected void execPageActivated() throws ProcessingException {
-//        getOutline().selectNode(firstNode);
-//      }
-//    };
-//
-//    pages.add(firstNode);
-//    pages.add(secondNode);
-//    IOutline outline = new Outline(pages);
-//
-//    JsonDesktopTree jsonDesktopTree = createJsonDesktopTreeWithMocks(outline);
-//    JsonEvent event = createJsonSelectedEvent(jsonDesktopTree.getOrCreatedNodeId(secondNode));
-//
-//    Assert.assertFalse(firstNode.isSelectedNode());
-//    Assert.assertFalse(secondNode.isSelectedNode());
-//
-//    jsonDesktopTree.handleUiEvent(event, new JsonResponse());
-//
-//    Assert.assertTrue(firstNode.isSelectedNode());
-//    Assert.assertFalse(secondNode.isSelectedNode());
-//
-//    List<JSONObject> responseEvents = extractEventsFromResponse(jsonDesktopTree.getJsonSession().currentJsonResponse(), JsonDesktopTree.EVENT_NODES_SELECTED);
-//    Assert.assertTrue(responseEvents.size() == 1);
-//
-//    List<ITreeNode> treeNodes = jsonDesktopTree.extractTreeNodes(responseEvents.get(0));
-//    Assert.assertEquals(firstNode, treeNodes.get(0));
-//  }
-//}
+/*******************************************************************************
+ * Copyright (c) 2010 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.ui.json;
+
+import static org.eclipse.scout.rt.ui.json.JsonDesktopTreeTestUtil.createJsonDesktopTreeWithMocks;
+import static org.eclipse.scout.rt.ui.json.JsonDesktopTreeTestUtil.createJsonSelectedEvent;
+import static org.eclipse.scout.rt.ui.json.testing.JsonTestUtility.extractEventsFromResponse;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.scout.commons.exception.ProcessingException;
+import org.eclipse.scout.rt.client.ui.basic.tree.ITreeNode;
+import org.eclipse.scout.rt.client.ui.desktop.outline.IOutline;
+import org.eclipse.scout.rt.client.ui.desktop.outline.pages.IPage;
+import org.eclipse.scout.rt.ui.json.desktop.fixtures.NodePage;
+import org.eclipse.scout.rt.ui.json.desktop.fixtures.Outline;
+import org.eclipse.scout.rt.ui.json.desktop.fixtures.OutlineWithOneNode;
+import org.eclipse.scout.testing.client.runner.ScoutClientTestRunner;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(ScoutClientTestRunner.class)
+public class JsonDesktopTreeTest {
+
+  /**
+   * Tests whether the model node gets correctly selected
+   */
+  @Test
+  public void testSelectionEvent() throws ProcessingException, JSONException {
+    IOutline outline = new OutlineWithOneNode();
+    ITreeNode node = outline.getRootNode().getChildNode(0);
+    Assert.assertFalse(node.isSelectedNode());
+
+    JsonDesktopTree jsonDesktopTree = createJsonDesktopTreeWithMocks(outline);
+
+    JsonEvent event = createJsonSelectedEvent(jsonDesktopTree.getOrCreatedNodeId(node));
+    jsonDesktopTree.handleUiEvent(event, new JsonResponse());
+
+    Assert.assertTrue(node.isSelectedNode());
+  }
+
+  /**
+   * Response must not contain the selection event if the selection was triggered by the request
+   */
+  @Test
+  public void testIgnorableSelectionEvent() throws ProcessingException, JSONException {
+    IOutline outline = new OutlineWithOneNode();
+    ITreeNode node = outline.getRootNode().getChildNode(0);
+
+    JsonDesktopTree jsonDesktopTree = createJsonDesktopTreeWithMocks(outline);
+
+    JsonEvent event = createJsonSelectedEvent(jsonDesktopTree.getOrCreatedNodeId(node));
+    jsonDesktopTree.handleUiEvent(event, new JsonResponse());
+
+    List<JSONObject> responseEvents = extractEventsFromResponse(jsonDesktopTree.getJsonSession().currentJsonResponse(), JsonDesktopTree.EVENT_NODES_SELECTED);
+    Assert.assertTrue(responseEvents.size() == 0);
+  }
+
+  /**
+   * If the selection event triggers the selection of another node, the selection event must not be ignored.
+   */
+  @Test
+  public void testIgnorableSelectionEvent2() throws ProcessingException, JSONException {
+    List<IPage> pages = new LinkedList<>();
+    final NodePage firstNode = new NodePage();
+    NodePage secondNode = new NodePage() {
+      @Override
+      protected void execPageActivated() throws ProcessingException {
+        getOutline().selectNode(firstNode);
+      }
+    };
+
+    pages.add(firstNode);
+    pages.add(secondNode);
+    IOutline outline = new Outline(pages);
+
+    JsonDesktopTree jsonDesktopTree = createJsonDesktopTreeWithMocks(outline);
+    JsonEvent event = createJsonSelectedEvent(jsonDesktopTree.getOrCreatedNodeId(secondNode));
+
+    Assert.assertFalse(firstNode.isSelectedNode());
+    Assert.assertFalse(secondNode.isSelectedNode());
+
+    jsonDesktopTree.handleUiEvent(event, new JsonResponse());
+
+    Assert.assertTrue(firstNode.isSelectedNode());
+    Assert.assertFalse(secondNode.isSelectedNode());
+
+    List<JSONObject> responseEvents = extractEventsFromResponse(jsonDesktopTree.getJsonSession().currentJsonResponse(), JsonDesktopTree.EVENT_NODES_SELECTED);
+    Assert.assertTrue(responseEvents.size() == 1);
+
+    List<ITreeNode> treeNodes = jsonDesktopTree.extractTreeNodes(responseEvents.get(0));
+    Assert.assertEquals(firstNode, treeNodes.get(0));
+  }
+}
