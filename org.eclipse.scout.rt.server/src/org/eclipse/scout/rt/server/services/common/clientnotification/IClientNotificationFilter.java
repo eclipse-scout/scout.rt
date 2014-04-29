da3727f523b4ae4464a1a761bf1e6196362e0e57
Judith Gull <jgu@bsiag.com>
2014-04-29 14:21:03 +0200

    Bug 422070 - ClientNotifications are not sent to client, if an exception occurs
    javadoc
    
    Change-Id: I7e1f72ca6f8fe8b96c0229d0a1d41b9c37ef391c
    Reviewed-on: https://git.eclipse.org/r/25724
    Tested-by: Hudson CI
    Reviewed-by: Judith Gull <jgu@bsiag.com>

--- org.eclipse.scout.rt.server/src/org/eclipse/scout/rt/server/services/common/clientnotification/IClientNotificationFilter.java
+++ org.eclipse.scout.rt.server/src/org/eclipse/scout/rt/server/services/common/clientnotification/IClientNotificationFilter.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -56,6 +56,8 @@ public interface IClientNotificationFilter extends Serializable {
    *         <p>
    *         This method is used on the server side, so you can get the server session with
    *         ThreadContext.getServerSession()
+   *         <p>
+   *         This method needs to be idempotent. It is not allowed to change the filters own state.
    */
   boolean accept();
 }
