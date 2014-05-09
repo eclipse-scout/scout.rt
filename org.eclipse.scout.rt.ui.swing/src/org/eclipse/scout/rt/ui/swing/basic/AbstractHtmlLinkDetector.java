738fa7581a482d93cb3f78c6fffd2a13461dc7b4
Matthias Nick <Matthias.Nick@bsiag.com>
2014-05-09 16:01:28 +0200

    bug 434396: Swing: Detection of HTML links in table does not work always
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=434396
    
    Consider an indent if it is set
    
    Change-Id: Ic6538b9e1dfeeacd9e55cec3468ccf9929cc7329
    Signed-off-by: Matthias Nick <Matthias.Nick@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/26207
    Tested-by: Hudson CI
    Reviewed-by: Judith Gull <jgu@bsiag.com>

---
+++ org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/basic/AbstractHtmlLinkDetector.java
@@ -0,0 +1,154 @@
+/*******************************************************************************
+ * Copyright (c) 2014 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.ui.swing.basic;
+
+import java.awt.Component;
+import java.awt.Insets;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.Element;
+import javax.swing.text.Position.Bias;
+import javax.swing.text.View;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLDocument;
+
+/**
+ * Abstract class to detect a hyperlink in a JComponent (e.g. JTable or JTree)
+ * 
+ * @since 4.0-RC1
+ */
+public abstract class AbstractHtmlLinkDetector<T extends JComponent> {
+  private URL m_hyperlink;
+  private T m_container;
+
+  /**
+   * Detects a hyperlink in the container (e.g. JTree oder JTable) for the give mouse position
+   *
+   * @return <code>true</code> if hyperlink detected, <code>false</code> otherwise
+   */
+  public boolean detect(T container, Point p) {
+    m_hyperlink = null;
+    m_container = container;
+
+    try {
+      Component c = getComponent(p);
+      if (c instanceof JComponent) {
+        View v = (View) ((JComponent) c).getClientProperty(javax.swing.plaf.basic.BasicHTML.propertyKey);
+        if (v != null) {
+          HTMLDocument doc = (HTMLDocument) v.getDocument();
+          //v is the renderer, the first child is the html element
+          v = v.getView(0);
+          if (v != null && doc != null) {
+            Rectangle cellRectangle = getCellRectangle(p);
+            Rectangle contentSize = calculateContentSize((JComponent) c, cellRectangle);
+
+            Point relativeMousePosition = new Point(p.x - cellRectangle.x, p.y - cellRectangle.y);
+            if (!contentSize.contains(relativeMousePosition)) {
+              return false; //mouse has not entered the contentSize rectangle
+            }
+
+            v.setSize(contentSize.width, contentSize.height);
+            int pos = v.viewToModel(relativeMousePosition.x, relativeMousePosition.y, contentSize, new Bias[1]);
+            if (pos >= 0) {
+              Element elem = doc.getCharacterElement(pos);
+              if (elem != null) {
+                AttributeSet set = (AttributeSet) elem.getAttributes().getAttribute(HTML.Tag.A);
+                if (set != null) {
+                  String s = (String) set.getAttribute(HTML.Attribute.HREF);
+                  if (s != null) {
+                    try {
+                      m_hyperlink = new URL(s);
+                      return true;
+                    }
+                    catch (MalformedURLException mfue) {
+                      m_hyperlink = new URL(new URL("http://local"), s);
+                      return true;
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    catch (Throwable t) {
+      //nop
+    }
+
+    return false;
+  }
+
+  /**
+   * returns the cell's rectangle for the given position
+   *
+   * @since 4.0-RC1
+   */
+  protected abstract Rectangle getCellRectangle(Point p);
+
+  /**
+   * returns the component inside the container for the given position
+   *
+   * @since 4.0-RC1
+   */
+  protected abstract Component getComponent(Point p);
+
+  /**
+   * Calculates the area which can really be used inside the cellRectangle
+   *
+   * @since 4.0-RC1
+   */
+  protected Rectangle calculateContentSize(JComponent c, Rectangle cellRectangle) {
+    Insets cInsets = calculateInsets(c);
+    Rectangle contentSize = new Rectangle(0, 0, cellRectangle.width - (cInsets.left + cInsets.right), cellRectangle.height - (cInsets.top + cInsets.bottom));
+    contentSize.x += cInsets.left;
+    contentSize.y += cInsets.top;
+    return contentSize;
+  }
+
+  /**
+   * Calculates the insets of the component.
+   *
+   * @since 4.0-RC1
+   */
+  protected Insets calculateInsets(JComponent c) {
+    Insets i = c.getInsets();
+    if (c instanceof JLabel) {
+      JLabel label = (JLabel) c;
+      if (label.getIcon() != null) {
+        i.left += label.getIcon().getIconWidth() + label.getIconTextGap();
+      }
+    }
+    return i;
+  }
+
+  /**
+   * @return URL
+   *         If the url is not a valid external url (for example a local url "test/abc") then the prefix http://local/
+   *         is used.
+   */
+  public URL getHyperlink() {
+    return m_hyperlink;
+  }
+
+  /**
+   * returns the container (e.g. JTree or JTable)
+   */
+  public T getContainer() {
+    return m_container;
+  }
+}
--- org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/basic/table/TableHtmlLinkDetector.java
+++ org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/basic/table/TableHtmlLinkDetector.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -13,88 +13,47 @@ package org.eclipse.scout.rt.ui.swing.basic.table;
 import java.awt.Component;
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.net.MalformedURLException;
-import java.net.URL;
 
-import javax.swing.JComponent;
 import javax.swing.JTable;
-import javax.swing.text.AttributeSet;
-import javax.swing.text.Element;
-import javax.swing.text.Position.Bias;
-import javax.swing.text.View;
-import javax.swing.text.html.HTML;
-import javax.swing.text.html.HTMLDocument;
+
+import org.eclipse.scout.rt.ui.swing.basic.AbstractHtmlLinkDetector;
 
 /**
- *
+ * Detects a hyperlink in a JTable
  */
-public class TableHtmlLinkDetector {
-  private int m_rowIndex;
-  private int m_columnIndex;
-  private URL m_hyperlink;
+public class TableHtmlLinkDetector extends AbstractHtmlLinkDetector<JTable> {
+  private int m_rowIndex = -1;
+  private int m_columnIndex = -1;
 
+  @Override
   public boolean detect(JTable table, Point p) {
-    m_rowIndex = -1;
-    m_columnIndex = -1;
-    m_hyperlink = null;
-    //
-    int row = table.rowAtPoint(p);
-    int col = table.columnAtPoint(p);
-    if (row >= 0 && col >= 0) {
-      try {
-        Component c = table.prepareRenderer(table.getCellRenderer(row, col), row, col);
-        if (c instanceof JComponent) {
-          View v = (View) ((JComponent) c).getClientProperty(javax.swing.plaf.basic.BasicHTML.propertyKey);
-          if (v != null) {
-            HTMLDocument doc = (HTMLDocument) v.getDocument();
-            //v is the renderer, the first child is the html element
-            v = v.getView(0);
-            if (v != null && doc != null) {
-              Rectangle r = table.getCellRect(row, col, false);
-              Rectangle allocation = new Rectangle(0, 0, r.width, r.height);
-              v.setSize(r.width, r.height);
-              int pos = v.viewToModel(p.x - r.x, p.y - r.y, allocation, new Bias[1]);
-              if (pos >= 0) {
-                Element elem = doc.getCharacterElement(pos);
-                if (elem != null) {
-                  AttributeSet set = (AttributeSet) elem.getAttributes().getAttribute(HTML.Tag.A);
-                  if (set != null) {
-                    String s = (String) set.getAttribute(HTML.Attribute.HREF);
-                    if (s != null) {
-                      try {
-                        m_hyperlink = new URL(s);
-                        m_rowIndex = row;
-                        m_columnIndex = col;
-                        return true;
-                      }
-                      catch (MalformedURLException mfue) {
-                        m_hyperlink = new URL(new URL("http://local"), s);
-                        m_rowIndex = row;
-                        m_columnIndex = col;
-                        return true;
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-      catch (Throwable t) {
-        //nop
-      }
+    boolean found = super.detect(table, p);
+    if (found) {
+      m_rowIndex = getRow(p);
+      m_columnIndex = getColumn(p);
     }
-    return false;
+    return found;
   }
 
-  /**
-   * @return URL
-   *         If the url is not a valid external url (for example a local url "test/abc") then the prefix http://local/
-   *         is used.
-   */
-  public URL getHyperlink() {
-    return m_hyperlink;
+  @Override
+  protected Rectangle getCellRectangle(Point p) {
+    return getContainer().getCellRect(getRow(p), getColumn(p), false);
+  }
+
+  @Override
+  protected Component getComponent(Point p) {
+    int row = getRow(p);
+    int col = getColumn(p);
+    Component c = getContainer().prepareRenderer(getContainer().getCellRenderer(row, col), row, col);
+    return c;
+  }
+
+  private int getRow(Point p) {
+    return getContainer().rowAtPoint(p);
+  }
+
+  private int getColumn(Point p) {
+    return getContainer().columnAtPoint(p);
   }
 
   public int getRowIndex() {
