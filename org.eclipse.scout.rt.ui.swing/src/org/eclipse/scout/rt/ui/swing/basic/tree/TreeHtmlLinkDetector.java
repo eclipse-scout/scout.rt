c5e2e072e1258bdbcb4fd2b90b73c9f98f299c9f
Matthias Nick <Matthias.Nick@bsiag.com>
2014-05-14 15:40:25 +0200

    bug 434396: Swing: Detection of HTML links in table does not work always
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=434396
    
    Consider an indent if it is set. Backport of commit
    738fa7581a482d93cb3f78c6fffd2a13461dc7b4
    
    Change-Id: Ib1238b9e1dfeeacd9e55cbc3468ccf9929cc7a29
    Signed-off-by: Matthias Nick <Matthias.Nick@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/26522
    Tested-by: Hudson CI

--- org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/basic/tree/TreeHtmlLinkDetector.java
+++ org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/basic/tree/TreeHtmlLinkDetector.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -13,90 +13,48 @@ package org.eclipse.scout.rt.ui.swing.basic.tree;
 import java.awt.Component;
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.net.MalformedURLException;
-import java.net.URL;
 
-import javax.swing.JComponent;
 import javax.swing.JTree;
-import javax.swing.text.AttributeSet;
-import javax.swing.text.Element;
-import javax.swing.text.View;
-import javax.swing.text.Position.Bias;
-import javax.swing.text.html.HTML;
-import javax.swing.text.html.HTMLDocument;
 import javax.swing.tree.TreeCellRenderer;
 import javax.swing.tree.TreePath;
 
+import org.eclipse.scout.rt.ui.swing.basic.AbstractHtmlLinkDetector;
+
 /**
- *
+ * Detects a hyperlink in a JTree
  */
-public class TreeHtmlLinkDetector {
+public class TreeHtmlLinkDetector extends AbstractHtmlLinkDetector<JTree> {
   private TreePath m_treePath;
-  private URL m_hyperlink;
 
-  public boolean detect(JTree tree, Point p) {
-    m_treePath = null;
-    m_hyperlink = null;
-    //
-    TreePath path = tree.getPathForLocation(p.x, p.y);
-    if (path != null && path.getPathCount() > 0) {
-      try {
-        TreeCellRenderer renderer = tree.getCellRenderer();
-        Component c = renderer.getTreeCellRendererComponent(tree, path.getLastPathComponent(), tree.isPathSelected(path), tree.isExpanded(path), tree.getModel().isLeaf(path.getLastPathComponent()), tree.getRowForPath(path), true);
-        if (c instanceof JComponent) {
-          View v = (View) ((JComponent) c).getClientProperty(javax.swing.plaf.basic.BasicHTML.propertyKey);
-          if (v != null) {
-            HTMLDocument doc = (HTMLDocument) v.getDocument();
-            //v is the renderer, the first child is the html element
-            v = v.getView(0);
-            if (v != null && doc != null) {
-              Rectangle r = tree.getPathBounds(path);
-              Rectangle allocation = new Rectangle(0, 0, r.width, r.height);
-              v.setSize(r.width, r.height);
-              int pos = v.viewToModel(p.x - r.x, p.y - r.y, allocation, new Bias[1]);
-              if (pos >= 0) {
-                Element elem = doc.getCharacterElement(pos);
-                if (elem != null) {
-                  AttributeSet set = (AttributeSet) elem.getAttributes().getAttribute(HTML.Tag.A);
-                  if (set != null) {
-                    String s = (String) set.getAttribute(HTML.Attribute.HREF);
-                    if (s != null) {
-                      try {
-                        m_hyperlink = new URL(s);
-                        m_treePath = path;
-                        return true;
-                      }
-                      catch (MalformedURLException mfue) {
-                        m_hyperlink = new URL(new URL("http://local"), s);
-                        m_treePath = path;
-                        return true;
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-      catch (Throwable t) {
-        //nop
-      }
+  @Override
+  public boolean detect(JTree container, Point p) {
+    boolean found = super.detect(container, p);
+    if (found) {
+      m_treePath = getPath(p);
     }
-    return false;
-  }
-
-  /**
-   * @return URL
-   *         If the url is not a valid external url (for example a local url "test/abc") then the prefix http://local/
-   *         is used.
-   */
-  public URL getHyperlink() {
-    return m_hyperlink;
+    return found;
   }
 
   public TreePath getTreePath() {
     return m_treePath;
+  }
+
+  @Override
+  protected Rectangle getCellRectangle(Point p) {
+    return getContainer().getPathBounds(getPath(p));
+  }
+
+  @Override
+  protected Component getComponent(Point p) {
+    JTree tree = getContainer();
+    TreePath path = getPath(p);
+    TreeCellRenderer renderer = getContainer().getCellRenderer();
+    Component c = renderer.getTreeCellRendererComponent(tree, path.getLastPathComponent(), tree.isPathSelected(path), tree.isExpanded(path), tree.getModel().isLeaf(path.getLastPathComponent()), tree.getRowForPath(path), true);
+    return c;
+  }
+
+  private TreePath getPath(Point p) {
+    return getContainer().getPathForLocation(p.x, p.y);
   }
 
 }
