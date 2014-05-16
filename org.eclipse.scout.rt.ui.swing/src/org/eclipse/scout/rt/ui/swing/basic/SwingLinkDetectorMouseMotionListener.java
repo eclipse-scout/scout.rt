3ae65b35f2016bce646b11137135497f8448f456
Matthias Nick <Matthias.Nick@bsiag.com>
2014-05-16 07:02:29 +0000

    Bug 381662 - [SWING] Cursor appearance does not change to hand on links in HTML formatted tablecells
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=381662
    Add cursor when link detected. Backport of commit 38759cf9797f401c66b94efe577cf531ce157816.

---
+++ org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/basic/SwingLinkDetectorMouseMotionListener.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2014 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.ui.swing.basic;
+
+import java.awt.Cursor;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import javax.swing.JComponent;
+
+/**
+ * MouseMotionListener for a JTable or JTree which detects a click on a hyperlink
+ * 
+ * @since 4.0.0-RC1 (backported)
+ */
+public class SwingLinkDetectorMouseMotionListener<T extends JComponent> extends MouseMotionAdapter {
+  private final AbstractHtmlLinkDetector<T> m_detector;
+
+  public SwingLinkDetectorMouseMotionListener(AbstractHtmlLinkDetector<T> detector) {
+    m_detector = detector;
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public void mouseMoved(MouseEvent e) {
+    if (m_detector.detect((T) e.getComponent(), e.getPoint())) {
+      e.getComponent().setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+    }
+    else {
+      e.getComponent().setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+    }
+  }
+}
