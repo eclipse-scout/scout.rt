d5da625088f3ca4b55f64af57200f16508f33e49
Stephan Leicht Vogt <stephan.leicht@bsiag.com>
2014-06-05 15:06:57 +0200

    add missing files from last merge
    Change-Id: If1c7e3ebbf85e95e64059710d130ac7909e672c1

---
+++ org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/action/menu/TableMenuType.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2010 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.client.ui.action.menu;
+
+/**
+ * All possible menus types of a table menu. This menu types are used of {@link AbstractMenu#getConfiguredMenuTypes()}
+ * method.
+ */
+public enum TableMenuType implements IMenuType {
+  EmptySpace,
+  SingleSelection,
+  MultiSelection,
+  Header
+}
