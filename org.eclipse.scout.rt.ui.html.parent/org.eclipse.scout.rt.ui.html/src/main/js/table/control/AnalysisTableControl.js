4f397fda81bc7a3c9d5ba40964a4caae79f7e454
Christian Rusche <christian.rusche@bsi-software.com>
2014-06-10 08:32:20 +0200

    html ui: venn, set selection

--- org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/table/control/AnalysisTableControl.js
+++ org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/table/control/AnalysisTableControl.js
@@ -14,9 +14,9 @@ scout.AnalysisTableControl.prototype._render = function($parent) {
   $commandContainer.appendDiv('', 'command delete', 'Kriterium verwerfen').click(removeCriteria);
   $commandContainer.appendDiv('', 'separator', '');
   $commandContainer.appendDiv('', 'command union', 'Ansicht wechseln').click(switchShow);
-  $commandContainer.appendDiv('', 'command union', 'Vereinigungsmenge');
-  $commandContainer.appendDiv('', 'command distinct', 'Schnittmenge');
-  $commandContainer.appendDiv('', 'separator', 'Durch klicken bei gedrückter Umstelltaste ...');
+  $commandContainer.appendDiv('', 'command union', 'Vereinigungsmenge').click(setUnion);
+  $commandContainer.appendDiv('', 'command distinct', 'Schnittmenge').click(setIntersect);
+  $commandContainer.appendDiv('', 'separator', 'Durch klicken mit der rechten Maustaste...');
   $commandContainer.appendDiv('', 'command union', 'Simulator').click(simulateServer);
 
   // svg container
@@ -25,6 +25,8 @@ scout.AnalysisTableControl.prototype._render = function($parent) {
       .attrSVG('viewBox', '0 0 500 340')
       .attrSVG('preserveAspectRatio', 'xMinYMin');
 
+  var $vennDefs = $vennContainer.appendSVG('defs', '', 'venn-defs');
+
   $vennContainer.appendSVG('rect', '', 'venn-all')
     .attr('x', 5).attr('y', 15)
     .attr('width', 490).attr('height', 300)
@@ -32,7 +34,7 @@ scout.AnalysisTableControl.prototype._render = function($parent) {
 
   // $criteria = circle per criteria; count calculated by server
   var $criteria = [],
-    show = true,
+    show = false,
     count = {};
   count.total = undefined;
 
@@ -99,6 +101,9 @@ scout.AnalysisTableControl.prototype._render = function($parent) {
    $('text', $vennContainer)
      .animateSVG('opacity', 0, 100, $.removeThis);
 
+   // remove intersect elements
+   $('.venn-set', $vennContainer).remove();
+
    // show count all data
    if (count.total) {
      $vennContainer.appendSVG('text', '', 'venn-all-text', count.total + ' DatensÃ¤tze')
@@ -109,139 +114,180 @@ scout.AnalysisTableControl.prototype._render = function($parent) {
    var x, x0, x1, x2, y, y0, y1, y2, r0, r1, r2;
    var ret, d, d01, d02, d12;
    var alpha, beta;
+   var intersec = [];
 
-   if ($criteria.length === 1 && count.total && !show) {
+   // move circle, draw text and set
+   if ($criteria.length === 1) {
      // that is easy...
-     r0 = calcR(count['0'], MAX_R);
+     if (count.total && !show) {
+       r0 = calcR(count['0'], MAX_R);
+     } else {
+       r0 = MAX_R * 0.9;
+     }
+
      x0 = 0;
      y0 = 0;
 
      moveCircle($criteria[0], r0, x0, y0);
-     drawText(count['0'], 0, 0);
+     drawText(count['0'], x0, y0);
+     drawSetOuter(r0, x0, y0);
+     drawSetMain('0', r0, x0, y0);
+   } else if ($criteria.length === 2) {
+     if (count.total && !show) {
+       // calculate size of circles
+       r0 = calcR(count['0'], MAX_R);
+       r1 = calcR(count['1'], MAX_R);
 
-   } else if ($criteria.length === 1) {
-     // ... and simple
-     moveCircle($criteria[0], MAX_R * 0.9, 0, 0);
-     drawText(count['0'], 0, 0);
+       // find distance
+       ret = findD(count['0'], count['1'], count['01'], r0, r1);
+       r0 = ret.ra;
+       r1 = ret.rb;
+       d = ret.d;
 
-   } else if ($criteria.length === 2&& count.total && !show) {
-     // calculate size of circles
-     r0 = calcR(count['0'], MAX_R);
-     r1 = calcR(count['1'], MAX_R);
+       // find balance
+       x1 = d / ((r1*r1) / (r0*r0) + 1);
+       x0 = x1 - d;
+     } else {
+       r0 = MAX_R * 0.9;
+       r1 = MAX_R * 0.9;
 
-     // find distance
-     ret = findD(count['0'], count['1'], count['01'], r0, r1);
-     r0 = ret.ra;
-     r1 = ret.rb;
-     d = ret.d;
+       x0 = -MAX_R * 0.6;
+       x1 = MAX_R * 0.6;
+     }
 
-     // find balance
-     x1 = d / ((r1*r1) / (r0*r0) + 1);
-     x0 = x1 - d;
      y0 = 0;
+     y1 = 0;
 
      // move and resize circle
      moveCircle($criteria[0], r0, x0, y0);
-     moveCircle($criteria[1], r1, x1, y0);
+     moveCircle($criteria[1], r1, x1, y1);
 
      // draw text
-     if (count['0'] == count['01']) {
-       drawText(count['0'], x0, y0);
-       drawText(count['1'], x1 - (d - r1 - r0) / 2, y0);
-     } else if (count['1'] == count['01']) {
-       drawText(count['0'], x0 + (d - r0 - r1) / 2, y0);
-       drawText(count['1'], x1, y0);
-     } else if (count['01'] === 0) {
-       drawText(count['0'], x0, y0);
-       drawText(count['1'], x1, y0);
+     if (count.total && !show) {
+       if (count['0'] == count['01']) {
+         drawText(count['0'], x0, y0);
+         drawText(count['1'], x1 - (d - r1 - r0) / 2, y1);
+       } else if (count['1'] == count['01']) {
+         drawText(count['0'], x0 + (d - r0 - r1) / 2, y0);
+         drawText(count['1'], x1, y1);
+       } else if (count['01'] === 0) {
+         drawText(count['0'], x0, y0);
+         drawText(count['1'], x1, y1);
+       } else {
+         drawText(count['0'], x0 + (d - r0 - r1) / 2, y0);
+         drawText(count['1'], x1 - (d - r0 - r1) / 2, y1);
+         drawText(count['01'], x0 + r0 + (d - r0 - r1) / 2, y0);
+       }
      } else {
-       drawText(count['0'], x0 + (d - r0 - r1) / 2, y0);
-       drawText(count['1'], x1 - (d - r0 - r1) / 2, y0);
-       drawText(count['01'], x0 + r0 + (d - r0 - r1) / 2, y0);
+       drawText(count['0'], -MAX_R * 0.8, 0);
+       drawText(count['1'], +MAX_R * 0.8, 0);
+       drawText(count['01'], 0, 0);
      }
 
-   } else if ($criteria.length === 2) {
-     // 2 circles
-     moveCircle($criteria[0], MAX_R * 0.9, -MAX_R * 0.6, 0);
-     moveCircle($criteria[1], MAX_R * 0.9, +MAX_R * 0.6, 0);
+     drawSetOuter(r0, x0, y0, r1, x1, y1);
+     drawSetMain('0', r0, x0, y0, r1, x1, y1);
+     drawSetMain('1', r1, x1, y1, r0, x0, y0);
+     drawSetIntersect('01', r0, x0, y0, r1, x1, y1);
+   } else if ($criteria.length === 3) {
+     if (count.total && !show) {
 
-     drawText(count['0'], -MAX_R * 0.8, 0);
-     drawText(count['1'], +MAX_R * 0.8, 0);
-     drawText(count['01'], 0, 0);
+       // calculate size of circles
+       r0 = calcR(count['0'], MAX_R * 0.9);
+       r1 = calcR(count['1'], MAX_R * 0.9);
+       r2 = calcR(count['2'], MAX_R * 0.9);
 
-   } else if ($criteria.length === 3 && count.total && !show) {
-     // calculate size of circles
-     r0 = calcR(count['0'], MAX_R * 0.9);
-     r1 = calcR(count['1'], MAX_R * 0.9);
-     r2 = calcR(count['2'], MAX_R * 0.9);
+       // find distance
+       ret = findD(count['0'], count['1'], count['01'], r0, r1);
+       r0 = ret.ra;
+       r1 = ret.rb;
+       d01 = ret.d;
 
-     // find distance
-     ret = findD(count['0'], count['1'], count['01'], r0, r1);
-     r0 = ret.ra;
-     r1 = ret.rb;
-     d01 = ret.d;
+       ret = findD(count['0'], count['2'], count['02'], r0, r2);
+       r0 = ret.ra;
+       r2 = ret.rb;
+       d02 = ret.d;
 
-     ret = findD(count['0'], count['2'], count['02'], r0, r2);
-     r0 = ret.ra;
-     r2 = ret.rb;
-     d02 = ret.d;
+       ret = findD(count['1'], count['2'], count['12'], r1, r2);
+       r1 = ret.ra;
+       r2 = ret.rb;
+       d12 = ret.d;
 
-     ret = findD(count['1'], count['2'], count['12'], r1, r2);
-     r1 = ret.ra;
-     r2 = ret.rb;
-     d12 = ret.d;
+       // find balance, start with 1
+       x1 = d01 / ((r1*r1) / (r0*r0) + 1);
+       y1 = 0;
 
-     // find balance, start with 1
-     x1 = d01 / ((r1*r1) / (r0*r0) + 1);
-     y1 = 0;
+       // 0 is simple ;)
+       x0 = x1 - d01;
+       y0 = 0;
 
-     // 0 is simple ;)
-     x0 = x1 - d01;
-     y0 = 0;
+       //  fit 2 with 1
+       if (d12 > d01 + d02 || count['12'] === 0) {
+         x2 = x0 - d02;
+         y2 = y0;
+       } else  if (d12 < d01 - d02 || d12 < d02 - d01) {
+         x2 = x0 + d02;
+         y2 = y0;
+       } else  {
+         alpha = Math.acos((d02*d02 + d01*d01 - d12*d12) / (2 * d02 * d01));
+         beta = Math.acos((d12*d12 + d01*d01 - d02*d02) / (2 * d12 * d01));
 
-     //  fit 2 with 1
-     if (d12 > d01 + d02 || count['12'] === 0) {
-       x2 = x0 - d02;
-       y2 = y0;
-     } else  if (d12 < d01 - d02 || d12 < d02 - d01) {
-       x2 = x0 + d02;
-       y2 = y0;
-     } else  {
-       alpha = Math.acos((d02*d02 + d01*d01 - d12*d12) / (2 * d02 * d01));
-       beta = Math.acos((d12*d12 + d01*d01 - d02*d02) / (2 * d12 * d01));
+         x2 = x0 + d02 * Math.cos(alpha);
+         y2 = y0 + d02 * Math.sin(alpha);
+       }
 
-       x2 = x0 + d02 * Math.cos(alpha);
-       y2 = y0 + d02 * Math.sin(alpha);
+       // find center
+       var cx = (r0 * x0 + r1 * x1 + r2 * x2 ) / (r0 + r1 + r2) ;
+       var cy = (r0 * y0 + r1 * y1 + r2 * y2 ) / (r0 + r1 + r2) ;
+
+       x0 = x0 - cx;
+       x1 = x1 - cx;
+       x2 = x2 - cx;
+
+       y0 = y0 - cx;
+       y1 = y1 - cx;
+       y2 = y2 - cx;
+     } else {
+       r0 = MAX_R * 0.7;
+       r1 = MAX_R * 0.7;
+       r2 = MAX_R * 0.7;
+
+       x0 = -MAX_R * 0.47;
+       x1 = MAX_R * 0.47;
+       x2 = 0;
+
+       y0 = -MAX_R * 0.4;
+       y1 = -MAX_R * 0.4;
+       y2 = MAX_R * 0.4;
      }
-
-     // find center
-     var cx = (r0 * x0 + r1 * x1 + r2 * x2 ) / (r0 + r1 + r2) ;
-     var cy = (r0 * y0 + r1 * y1 + r2 * y2 ) / (r0 + r1 + r2) ;
 
      // move and resize circles
-     moveCircle($criteria[0], r0, x0 - cx, y0 - cy);
-     moveCircle($criteria[1], r1, x1 - cx, y1 - cy);
-     moveCircle($criteria[2], r2, x2 - cx, y2 - cy);
+     moveCircle($criteria[0], r0, x0, y0);
+     moveCircle($criteria[1], r1, x1, y1);
+     moveCircle($criteria[2], r2, x2, y2);
 
-   } else if ($criteria.length === 3) {
-     // 3 circles
-     moveCircle($criteria[0], MAX_R * 0.7, -MAX_R * 0.47, -MAX_R * 0.4);
-     moveCircle($criteria[1], MAX_R * 0.7, +MAX_R * 0.47, -MAX_R * 0.4);
-     moveCircle($criteria[2], MAX_R * 0.7, 0, MAX_R * 0.4);
+     if (!count.total || show) {
+       drawText(count['0'], -MAX_R * 0.7, -MAX_R * 0.5);
+       drawText(count['1'], +MAX_R * 0.7, -MAX_R * 0.5);
+       drawText(count['2'], 0, MAX_R * 0.6);
 
-     drawText(count['0'], -MAX_R * 0.7, -MAX_R * 0.5);
-     drawText(count['1'], +MAX_R * 0.7, -MAX_R * 0.5);
-     drawText(count['2'], 0, MAX_R * 0.6);
+       drawText(count['01'], 0, -MAX_R * 0.5);
+       drawText(count['02'], -MAX_R * 0.35, MAX_R * 0.1);
+       drawText(count['12'], +MAX_R * 0.35, MAX_R * 0.1);
 
-     drawText(count['01'], 0, -MAX_R * 0.5);
-     drawText(count['02'], -MAX_R * 0.35, MAX_R * 0.1);
-     drawText(count['12'], +MAX_R * 0.35, MAX_R * 0.1);
+       drawText(count['012'], 0,  -MAX_R * 0.1);
+     }
 
-     drawText(count['012'], 0,  -MAX_R * 0.1);
+     drawSetOuter(r0, x0, y0, r1, x1, y1, r2, x2, y2);
+     drawSetMain('0', r0, x0, y0, r1, x1, y1, r2, x2, y2);
+     drawSetMain('1', r1, x1, y1, r0, x0, y0, r2, x2, y2);
+     drawSetMain('2', r2, x2, y2, r1, x1, y1, r0, x0, y0);
+     drawSetIntersect('01', r0, x0, y0, r1, x1, y1, r2, x2, y2);
+     drawSetIntersect('02', r0, x0, y0, r2, x2, y2, r1, x1, y1);
+     drawSetIntersect('12', r2, x2, y2, r1, x1, y1, r0, x0, y0);
 
-
+     //TODO cru: one element can not be clicked
+     //drawSetTriple('012', r2, x2, y2, r1, x1, y1, r0, x0, y0);
    }
-
    function calcR (size, limit) {
      return Math.max(limit * Math.sqrt(size / count.total), MIN_R);
    }
@@ -286,6 +332,158 @@ scout.AnalysisTableControl.prototype._render = function($parent) {
        .animateSVG('opacity', 1);
      }
    }
+
+   function drawSetOuter (r0, dx0, dy0, r1, dx1, dy1, r2, dx2, dy2) {
+     // TODO cru: clone existing object instead of creating new ones
+     // TODO cru: collect all functions
+
+     // create outer set
+     var $mask = $vennDefs.empty().appendSVG('mask', 'maskAll', '');
+
+     $mask.appendSVG('rect', '', '')
+       .attr('x', 5).attr('y', 15)
+       .attr('width', 490).attr('height', 300)
+       .attr('rx', 10).attr('ry', 10)
+       .attr('fill', 'white');
+
+     if (r0) {
+       $mask.appendSVG('circle', '', '')
+         .attr('cx', MID_X + dx0)
+         .attr('cy', MID_Y + dy0)
+         .attr('r', r0);
+     }
+
+     if (r1) {
+       $mask.appendSVG('circle', '', '')
+         .attr('cx', MID_X + dx1)
+         .attr('cy', MID_Y + dy1)
+         .attr('r', r1);
+     }
+
+     if (r2) {
+       $mask.appendSVG('circle', '', '')
+         .attr('cx', MID_X + dx2)
+         .attr('cy', MID_Y + dy2)
+         .attr('r', r2);
+     }
+
+     $vennContainer.appendSVG('rect', '', 'venn-set')
+       .attr('x', 5).attr('y', 15)
+       .attr('width', 490).attr('height', 300)
+       .attr('mask', 'url(#maskAll)')
+       .on('contextmenu', setOne);
+   }
+
+   function drawSetMain (id, ra, dxa, dya, rb, dxb, dyb, rc, dxc, dyc) {
+     var $mask = $vennDefs.appendSVG('mask', 'set' + id, '');
+
+     $mask.appendSVG('circle', '', '')
+       .attr('cx', MID_X + dxa)
+       .attr('cy', MID_Y + dya)
+       .attr('r', ra)
+       .attr('fill', 'white');
+
+     if (rb) {
+       $mask.appendSVG('circle', '', '')
+       .attr('cx', MID_X + dxb)
+       .attr('cy', MID_Y + dyb)
+       .attr('r', rb);
+     }
+
+     if (rc) {
+       $mask.appendSVG('circle', '', '')
+       .attr('cx', MID_X + dxc)
+       .attr('cy', MID_Y + dyc)
+       .attr('r', rc);
+     }
+
+     $vennContainer.appendSVG('circle', '', 'venn-set')
+       .attr('cx', MID_X + dxa)
+       .attr('cy', MID_Y + dya)
+       .attr('r', ra)
+       .attr('mask', "url(#set" + id + ")")
+       .on('contextmenu', setOne);
+   }
+
+   function drawSetIntersect (id, ra, dxa, dya, rb, dxb, dyb, rc, dxc, dyc) {
+     var $clip = $vennDefs.appendSVG('clipPath', 'set-clip' + id, '');
+
+     $clip.appendSVG('circle', '', '')
+         .attr('cx', MID_X + dxb)
+         .attr('cy', MID_Y + dyb)
+         .attr('r', rb);
+
+
+     if (rc) {
+       var $mask = $vennDefs.appendSVG('mask', 'set-mask' + id, '');
+
+       $mask.appendSVG('circle', '', '')
+         .attr('cx', MID_X + dxa)
+         .attr('cy', MID_Y + dya)
+         .attr('r', ra)
+         .attr('fill', 'white');
+
+       $mask.appendSVG('circle', '', '')
+           .attr('cx', MID_X + dxc)
+           .attr('cy', MID_Y + dyc)
+           .attr('r', rc);
+     }
+
+     $vennContainer.appendSVG('circle', '', 'venn-set')
+       .attr('cx', MID_X + dxa)
+       .attr('cy', MID_Y + dya)
+       .attr('r', ra)
+       .attr('clip-path', "url(#set-clip" + id + ")")
+       .attr('mask', "url(#set-mask" + id + ")")
+       .on('contextmenu', setOne);
+   }
+
+ function drawSetTriple (id, ra, dxa, dya, rb, dxb, dyb, rc, dxc, dyc) {
+   var $clip = $vennDefs.appendSVG('clipPath', 'set-clip' + id, '');
+
+   $clip.appendSVG('circle', '', '')
+       .attr('cx', MID_X + dxb)
+       .attr('cy', MID_Y + dyb)
+       .attr('r', rb);
+
+   var $mask = $vennDefs.appendSVG('mask', 'set-mask' + id, '');
+
+   $mask.appendSVG('circle', '', '')
+     .attr('cx', MID_X + dxa)
+     .attr('cy', MID_Y + dya)
+     .attr('r', ra)
+     .attr('fill', 'black');
+
+   $mask.appendSVG('circle', '', '')
+     .attr('cx', MID_X + dxc)
+     .attr('cy', MID_Y + dyc)
+     .attr('r', rc)
+     .attr('fill', 'white');
+
+   $vennContainer.appendSVG('circle', '', 'venn-set')
+     .attr('cx', MID_X + dxa)
+     .attr('cy', MID_Y + dya)
+     .attr('r', ra)
+     .attr('clip-path', "url(#set-clip" + id + ")")
+     .attr('mask', "url(#set-mask" + id + ")")
+     .on('contextmenu', setOne);
+ }
+}
+
+
+ function setUnion () {
+ }
+
+ function setIntersect () {
+ }
+
+ function setOne (e) {
+   if ($(this).hasClassSVG('selected')) {
+     $(this).removeClassSVG('selected');
+   } else {
+     $(this).addClassSVG('selected');
+   }
+   e.preventDefault();
  }
 
  function simulateServer () {
