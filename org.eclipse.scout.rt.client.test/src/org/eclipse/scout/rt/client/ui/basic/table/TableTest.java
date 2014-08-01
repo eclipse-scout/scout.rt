d241140232a5a02d435677902984f2bc71d61015
Jeremie Bresson <jbr@bsiag.com>
2014-08-01 17:38:57 +0200

    Bug 402298: Unit Tests for AbstractTable#sort()
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=402298
    
    Related Forum discussion:
    https://www.eclipse.org/forums/index.php/t/235749/
    
    Change-Id: If2d180b6d354f44e4803324db4c8aac652b1d1dc
    Reviewed-on: https://git.eclipse.org/r/30896
    Tested-by: Hudson CI
    Reviewed-by: Jeremie Bresson <jbr@bsiag.com>

--- org.eclipse.scout.rt.client.test/src/org/eclipse/scout/rt/client/ui/basic/table/TableTest.java
+++ org.eclipse.scout.rt.client.test/src/org/eclipse/scout/rt/client/ui/basic/table/TableTest.java
@@ -157,8 +157,160 @@ public class TableTest {
     assertRowCount(0, 0, table2);
   }
 
+  /**
+   * Test of {@link AbstractTable#sort()}. If {@link AbstractTable#isSortEnabled()} is false (e.g. sort not enabled),
+   * rows will stay in the order they were added to the table.
+   */
+  @Test
+  public void testSortNotEnabled() throws Exception {
+    P_Table table = new P_Table();
+    table.initTable();
+
+    table.setSortEnabled(false);
+
+    //ensure table state:
+    assertEquals("SortEnabled", false, table.isSortEnabled());
+    assertEquals("FirstColumn - sort index", -1, table.getFirstColumn().getSortIndex());
+    assertEquals("SecondColumn - sort index", -1, table.getSecondColumn().getSortIndex());
+    assertEquals("ThirdColumn - sort index", 0, table.getThridColumn().getSortIndex());
+    assertEquals("ThirdColumn - initial sort index", 20, table.getThridColumn().getInitialSortIndex());
+    assertEquals("ThirdColumn - initial alwaysIncludeSortAtBegin", true, table.getThridColumn().isInitialAlwaysIncludeSortAtBegin());
+
+    //fill the table and sort:
+    fillTable(table);
+    table.sort();
+
+    //check the sort order of the rows:
+    List<ITableRow> rows = table.getRows();
+    assertEquals("rows size", 5, rows.size());
+    int i = 0;
+    assertRowEquals(10, "Lorem", 1, table, rows, i++);
+    assertRowEquals(1, "A Total", 2, table, rows, i++);
+    assertRowEquals(30, "Ipsum", 1, table, rows, i++);
+    assertRowEquals(25, "Lorem", 1, table, rows, i++);
+    assertRowEquals(20, "Ipsum", 1, table, rows, i++);
+  }
+
+  /**
+   * Test of {@link AbstractTable#sort()}. Sorted by:
+   * - 1. ThridColumn (defined with AlwaysIncludeSortAtBegin in the column)
+   * - 2. FirstColumn descending.
+   */
+  @Test
+  public void testSortFirstColumn() throws Exception {
+    P_Table table = new P_Table();
+    table.initTable();
+
+    table.setSortEnabled(true);
+    table.getColumnSet().setSortColumn(table.getFirstColumn(), false, 5);
+
+    //ensure table state:
+    assertEquals("SortEnabled", true, table.isSortEnabled());
+    assertEquals("FirstColumn - sort index", 1, table.getFirstColumn().getSortIndex());
+    assertEquals("FirstColumn - sort ascending", false, table.getFirstColumn().isSortAscending());
+    assertEquals("SecondColumn - sort index", -1, table.getSecondColumn().getSortIndex());
+    assertEquals("ThirdColumn - sort index", 0, table.getThridColumn().getSortIndex());
+    assertEquals("ThirdColumn - initial sort index", 20, table.getThridColumn().getInitialSortIndex());
+    assertEquals("ThirdColumn - initial alwaysIncludeSortAtBegin", true, table.getThridColumn().isInitialAlwaysIncludeSortAtBegin());
+    assertEquals("ColumnSet PermanentHeadSortColumns size", 1, table.getColumnSet().getPermanentHeadSortColumns().size());
+    assertEquals("ColumnSet SortColumns size", 2, table.getColumnSet().getSortColumns().size());
+
+    //fill the table and sort:
+    fillTable(table);
+    table.sort();
+
+    //check the sort order of the rows:
+    List<ITableRow> rows = table.getRows();
+    assertEquals("rows size", 5, rows.size());
+    int i = 0;
+    assertRowEquals(30, "Ipsum", 1, table, rows, i++);
+    assertRowEquals(25, "Lorem", 1, table, rows, i++);
+    assertRowEquals(20, "Ipsum", 1, table, rows, i++);
+    assertRowEquals(10, "Lorem", 1, table, rows, i++);
+    assertRowEquals(1, "A Total", 2, table, rows, i++);
+  }
+
+  /**
+   * Test of {@link AbstractTable#sort()}. Sorted by:
+   * - 1. ThridColumn (defined with AlwaysIncludeSortAtBegin in the column)
+   * - 2. SecondColumn descending
+   * - 3. FirstColumn ascending.
+   */
+  @Test
+  public void testSortTwoColumns() throws Exception {
+    P_Table table = new P_Table();
+    table.initTable();
+
+    table.setSortEnabled(true);
+    table.getColumnSet().setSortColumn(table.getFirstColumn(), true, 5);
+    table.getColumnSet().setSortColumn(table.getSecondColumn(), false, 5);
+
+    //ensure table state:
+    assertEquals("SortEnabled", true, table.isSortEnabled());
+    assertEquals("FirstColumn - sort index", 2, table.getFirstColumn().getSortIndex());
+    assertEquals("FirstColumn - sort ascending", true, table.getFirstColumn().isSortAscending());
+    assertEquals("SecondColumn - sort index", 1, table.getSecondColumn().getSortIndex());
+    assertEquals("SecondColumn - sort ascending", false, table.getSecondColumn().isSortAscending());
+    assertEquals("ThirdColumn - sort index", 0, table.getThridColumn().getSortIndex());
+    assertEquals("ThirdColumn - initial sort index", 20, table.getThridColumn().getInitialSortIndex());
+    assertEquals("ThirdColumn - initial alwaysIncludeSortAtBegin", true, table.getThridColumn().isInitialAlwaysIncludeSortAtBegin());
+    assertEquals("ColumnSet PermanentHeadSortColumns size", 1, table.getColumnSet().getPermanentHeadSortColumns().size());
+    assertEquals("ColumnSet SortColumns size", 3, table.getColumnSet().getSortColumns().size());
+
+    //fill the table and sort:
+    fillTable(table);
+    table.sort();
+
+    //check the sort order of the rows:
+    List<ITableRow> rows = table.getRows();
+    assertEquals("rows size", 5, rows.size());
+    int i = 0;
+    assertRowEquals(10, "Lorem", 1, table, rows, i++);
+    assertRowEquals(25, "Lorem", 1, table, rows, i++);
+    assertRowEquals(20, "Ipsum", 1, table, rows, i++);
+    assertRowEquals(30, "Ipsum", 1, table, rows, i++);
+    assertRowEquals(1, "A Total", 2, table, rows, i++);
+  }
+
+  /**
+   * Test of {@link AbstractTable#sort()}. Only sorted by ThridColumn (defined with AlwaysIncludeSortAtBegin in the
+   * column).
+   */
+  @Test
+  public void testSortDefault() throws Exception {
+    P_Table table = new P_Table();
+    table.initTable();
+
+    //ensure table state:
+    assertEquals("SortEnabled", true, table.isSortEnabled());
+    assertEquals("FirstColumn - sort index", -1, table.getFirstColumn().getSortIndex());
+    assertEquals("SecondColumn - sort index", -1, table.getSecondColumn().getSortIndex());
+    assertEquals("ThirdColumn - sort index", 0, table.getThridColumn().getSortIndex());
+    assertEquals("ThirdColumn - initial sort index", 20, table.getThridColumn().getInitialSortIndex());
+    assertEquals("ThirdColumn - initial alwaysIncludeSortAtBegin", true, table.getThridColumn().isInitialAlwaysIncludeSortAtBegin());
+    assertEquals("ColumnSet PermanentHeadSortColumns size", 1, table.getColumnSet().getPermanentHeadSortColumns().size());
+    assertEquals("ColumnSet SortColumns size", 1, table.getColumnSet().getSortColumns().size());
+
+    //fill the table and sort:
+    table.setSortEnabled(false); //deactivate now, reactivate before the sort() method we want to test.
+    fillTable(table);
+    table.setSortEnabled(true); //reactivate
+    table.sort();
+
+    //check the sort order of the rows:
+    List<ITableRow> rows = table.getRows();
+    assertEquals("rows size", 5, rows.size());
+    int i = 0;
+    assertRowEquals(10, "Lorem", 1, table, rows, i++);
+    assertRowEquals(30, "Ipsum", 1, table, rows, i++);
+    assertRowEquals(25, "Lorem", 1, table, rows, i++);
+    assertRowEquals(20, "Ipsum", 1, table, rows, i++);
+    assertRowEquals(1, "A Total", 2, table, rows, i++);
+  }
+
   private P_Table createTable(int status) throws ProcessingException {
     P_Table table = new P_Table();
+    table.initTable();
 
     table.addRowsByMatrix(new Object[][]{new Object[]{10, "Lorem"}, new Object[]{11, "Ipsum"}}, status);
     assertRowCount(2, 0, table);
@@ -168,18 +320,61 @@ public class TableTest {
     return table;
   }
 
+  private void fillTable(P_Table table) throws ProcessingException {
+    addTableRow(table, 10, "Lorem", 1);
+    addTableRow(table, 1, "A Total", 2);
+    addTableRow(table, 30, "Ipsum", 1);
+    addTableRow(table, 25, "Lorem", 1);
+    addTableRow(table, 20, "Ipsum", 1);
+
+    assertRowCount(5, 0, table);
+  }
+
+  private void addTableRow(P_Table table, Integer first, String second, Integer third) throws ProcessingException {
+    ITableRow r = table.addRow(table.createRow());
+    table.getFirstColumn().setValue(r, first);
+    table.getSecondColumn().setValue(r, second);
+    table.getThridColumn().setValue(r, third);
+  }
+
   private static void assertRowCount(int expectedRowCount, int expectedDeletedRowCount, P_Table table) {
     assertEquals(expectedRowCount, table.getRowCount());
     assertEquals(expectedDeletedRowCount, table.getDeletedRowCount());
   }
 
   private static void asssertStatusAndTable(P_Table expectedTable, int expectedStatus, ITableRow row) {
-    assertEquals(expectedStatus, row.getStatus());
-    assertEquals(expectedTable, row.getTable());
+    assertEquals("status", decodeStatus(expectedStatus), decodeStatus(row.getStatus()));
+    assertEquals("table", expectedTable, row.getTable());
+  }
+
+  /**
+   * @param expectedStatus
+   * @return
+   */
+  private static String decodeStatus(int status) {
+    switch (status) {
+      case ITableRow.STATUS_DELETED:
+        return "ITableRow.STATUS_DELETED";
+      case ITableRow.STATUS_INSERTED:
+        return "ITableRow.STATUS_INSERTED";
+      case ITableRow.STATUS_NON_CHANGED:
+        return "ITableRow.STATUS_NON_CHANGED";
+      case ITableRow.STATUS_UPDATED:
+        return "ITableRow.STATUS_UPDATED";
+      default:
+        throw new IllegalArgumentException("Unexpected status parameter");
+    }
   }
 
   private static void asssertNoTable(ITableRow row) {
     assertNull(row.getTable());
+  }
+
+  private static void assertRowEquals(Integer expectedFirst, String expectedSecond, Integer expectedThird, P_Table table, List<ITableRow> rows, int i) {
+    ITableRow row = rows.get(i);
+    assertEquals("Row [" + i + "] First Value", expectedFirst, table.getFirstColumn().getValue(row));
+    assertEquals("Row [" + i + "] Second Value", expectedSecond, table.getSecondColumn().getValue(row));
+    assertEquals("Row [" + i + "] Third Value", expectedThird, table.getThridColumn().getValue(row));
   }
 
   public static class P_Table extends AbstractTable {
@@ -197,6 +392,10 @@ public class TableTest {
       return getColumnSet().getColumnByClass(SecondColumn.class);
     }
 
+    public ThirdColumn getThridColumn() {
+      return getColumnSet().getColumnByClass(ThirdColumn.class);
+    }
+
     @Order(10)
     public class FirstColumn extends AbstractIntegerColumn {
       @Override
@@ -207,6 +406,20 @@ public class TableTest {
 
     @Order(20)
     public class SecondColumn extends AbstractStringColumn {
+    }
+
+    @Order(30)
+    public class ThirdColumn extends AbstractIntegerColumn {
+
+      @Override
+      protected int getConfiguredSortIndex() {
+        return 20;
+      }
+
+      @Override
+      protected boolean getConfiguredAlwaysIncludeSortAtBegin() {
+        return true;
+      }
     }
   }
 }
