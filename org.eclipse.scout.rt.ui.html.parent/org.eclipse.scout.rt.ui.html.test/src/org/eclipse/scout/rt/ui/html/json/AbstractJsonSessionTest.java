9c4945b1386f409984e3fa441c632dc5cfe87b5f
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2014-08-13 19:25:38 +0200

    HtmlUi: Fixed test

--- org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html.test/src/org/eclipse/scout/rt/ui/html/json/AbstractJsonSessionTest.java
+++ org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html.test/src/org/eclipse/scout/rt/ui/html/json/AbstractJsonSessionTest.java
@@ -9,6 +9,7 @@ import java.lang.ref.WeakReference;
 
 import javax.servlet.http.HttpSessionBindingEvent;
 
+import org.eclipse.scout.rt.client.AbstractClientSession;
 import org.eclipse.scout.rt.client.IClientSession;
 import org.eclipse.scout.rt.ui.html.json.AbstractJsonSession.P_ClientSessionCleanupHandler;
 import org.eclipse.scout.rt.ui.html.json.testing.JsonTestUtility;
@@ -31,9 +32,13 @@ public class AbstractJsonSessionTest {
   }
 
   @Test
-  public void testSessionInvalidation() {
+  public void testSessionInvalidation() throws InterruptedException {
     AbstractJsonSession jsonSession = (AbstractJsonSession) JsonTestUtility.createAndInitializeJsonSession();
     IClientSession clientSession = jsonSession.getClientSession();
+
+    //Don't waste time waiting for client jobs to finish. Test job itself runs inside a client job so we always have to wait until max time
+    ((AbstractClientSession) clientSession).setMaxShutdownWaitTime(0);
+
     WeakReference<IJsonSession> ref = new WeakReference<IJsonSession>(jsonSession);
 
     HttpSessionBindingEvent mockEvent = Mockito.mock(HttpSessionBindingEvent.class);
@@ -41,6 +46,11 @@ public class AbstractJsonSessionTest {
 
     jsonSession.valueUnbound(mockEvent);
     dummyCleanupHandler.valueUnbound(mockEvent);
+    synchronized (jsonSession.getClientSession().getStateLock()) {
+      if (clientSession.isActive()) {
+        jsonSession.getClientSession().getStateLock().wait(5000);
+      }
+    }
     assertFalse(clientSession.isActive());
 
     jsonSession = null;
