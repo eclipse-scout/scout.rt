7dd9c9f7e4e01fd1a91268c4829c99a28918f654
Andre Wegmueller <awe@bsiag.com>
2014-08-14 14:07:41 +0200

    HtmlUi: - improv. sequence box

--- org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/form/fields/sequencebox/SequenceBox.js
+++ org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/form/fields/sequencebox/SequenceBox.js
@@ -1,22 +1,30 @@
 scout.SequenceBox = function() {
   scout.SequenceBox.parent.call(this);
   this._addAdapterProperties('fields');
+  this.$sequenceBox;
 };
 
 scout.inherits(scout.SequenceBox, scout.FormField);
 
 scout.SequenceBox.prototype._render = function($parent) {
-  // TODO AWE: check if we should use AbstractSequenceBox#SequenceBoxGrid
-  this.$container = $('<ul>').
-  addClass('sequence-box').
-  addClass('cols-' + this.fields.length).
-  attr('id', 'SequenceBox-' + this.id);
-  $parent.append(this.$container);
+  // TODO AWE: check if we can/should use AbstractSequenceBox#SequenceBoxGrid
+  this.$container = $parent;
+  this.$container.attr('id', 'SequenceBox-' + this.id);
+
+  this.$label = $('<label>').
+    appendTo(this.$container);
+
+  this.$sequenceBox = $('<ul>').
+    addClass('field').
+    addClass('sequence-box').
+    addClass('cols-' + this.fields.length).
+    appendTo(this.$container);
 
   var i, $li;
   for (i = 0; i < this.fields.length; i++) {
-    $li = $('<li>').addClass('form-field');
-    this.$container.append($li);
+    $li = $('<li>').
+      addClass('form-field').
+      appendTo(this.$sequenceBox);
     this.fields[i].render($li);
   }
 };
