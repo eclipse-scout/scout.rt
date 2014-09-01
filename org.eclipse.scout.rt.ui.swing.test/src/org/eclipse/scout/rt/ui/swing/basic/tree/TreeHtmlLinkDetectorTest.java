dc847baf7855e1fc335920c02d6a939ebbbb02bf
Matthias Nick <Matthias.Nick@bsiag.com>
2014-09-01 15:27:22 +0200

    381662: [SWING] Cursor appearance does not change to hand on links in HTML formatted tablecells
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=381662
    
    Add NPE guard. (cherry picked from commit c9fa7662b2f493703db4e45b486d5f23141053d1)
    
    Change-Id: I2548e41b62e27b4d747b1bfc43e09e60bad88319
    Signed-off-by: Matthias Nick <Matthias.Nick@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/32680
    Tested-by: Hudson CI

---
+++ org.eclipse.scout.rt.ui.swing.test/src/org/eclipse/scout/rt/ui/swing/basic/tree/TreeHtmlLinkDetectorTest.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2014 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.ui.swing.basic.tree;
+
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.awt.Point;
+import java.util.logging.Handler;
+import java.util.logging.LogManager;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+import javax.swing.JTree;
+import javax.swing.tree.TreeCellRenderer;
+
+import org.eclipse.scout.rt.ui.swing.basic.AbstractHtmlLinkDetector;
+import org.junit.Test;
+
+/**
+ * Test for {@link TreeHtmlLinkDetector}
+ * 
+ * @since 4.1.0
+ */
+public class TreeHtmlLinkDetectorTest {
+
+  @Test
+  public void testNoNPEinDetect() {
+    TreeHtmlLinkDetector detector = new TreeHtmlLinkDetector();
+    installLogInterceptor();
+    JTree container = mock(JTree.class);
+    when(container.getCellRenderer()).thenReturn(mock(TreeCellRenderer.class));
+    when(container.getPathForLocation(0, 0)).thenReturn(null);
+
+    detector.detect(container, new Point(0, 0)); //this should not lead to a NPE
+  }
+
+  private void installLogInterceptor() {
+    Logger logger = LogManager.getLogManager().getLogger(AbstractHtmlLinkDetector.class.getName());
+    logger.addHandler(new P_LogHandler());
+  }
+
+  private class P_LogHandler extends Handler {
+
+    @Override
+    public void publish(LogRecord record) {
+      if (record.getThrown() != null) {
+        fail("An exception ocurred! " + record.getThrown());
+      }
+    }
+
+    @Override
+    public void flush() {
+    }
+
+    @Override
+    public void close() throws SecurityException {
+    }
+  }
+}
