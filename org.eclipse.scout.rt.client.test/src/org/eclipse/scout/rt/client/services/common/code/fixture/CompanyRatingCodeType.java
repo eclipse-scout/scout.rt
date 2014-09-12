fac2c102e19b01acff758ea3d7c88b6c2dbbebcc
Jeremie Bresson <jbr@bsiag.com>
2014-09-12 13:15:54 +0200

    Bug 443634: Fix CodeServiceClientProxy.getCodeTypes(List)
    CODES.getAllCodeTypes no longer loads all available code types at client
    startup
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=443634
    
    (cherry picked from develop commit
    74e67faa37dfe5503ec065039504dcf9e2d027e2)
    
    Change-Id: I312e19df5ec6ea75391eb6c1dcd4b26f65e9e021
    Reviewed-on: https://git.eclipse.org/r/33310
    Tested-by: Hudson CI
    Reviewed-by: Jeremie Bresson <jbr@bsiag.com>

---
+++ org.eclipse.scout.rt.client.test/src/org/eclipse/scout/rt/client/services/common/code/fixture/CompanyRatingCodeType.java
@@ -0,0 +1,111 @@
+/*******************************************************************************
+ * Copyright (c) 2014 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.client.services.common.code.fixture;
+
+import org.eclipse.scout.commons.annotations.Order;
+import org.eclipse.scout.commons.exception.ProcessingException;
+import org.eclipse.scout.rt.shared.TEXTS;
+import org.eclipse.scout.rt.shared.services.common.code.AbstractCode;
+import org.eclipse.scout.rt.shared.services.common.code.AbstractCodeTypeWithGeneric;
+import org.eclipse.scout.rt.shared.services.common.code.ICode;
+
+/**
+ * Example code type for the CodeServiceClientProxyTest
+ * 
+ * @since Scout 4.1-M2
+ */
+public class CompanyRatingCodeType extends AbstractCodeTypeWithGeneric<Long, Long, ICode<Long>> {
+
+  private static final long serialVersionUID = 1L;
+  public static final Long ID = 10100L;
+
+  public CompanyRatingCodeType() throws ProcessingException {
+    super();
+  }
+
+  @Override
+  protected String getConfiguredText() {
+    return TEXTS.get("CompanyRating");
+  }
+
+  @Override
+  public Long getId() {
+    return ID;
+  }
+
+  @Order(10.0)
+  public static class ACode extends AbstractCode<Long> {
+
+    private static final long serialVersionUID = 1L;
+    public static final Long ID = 10101L;
+
+    @Override
+    protected String getConfiguredText() {
+      return TEXTS.get("A");
+    }
+
+    @Override
+    public Long getId() {
+      return ID;
+    }
+  }
+
+  @Order(20.0)
+  public static class BCode extends AbstractCode<Long> {
+
+    private static final long serialVersionUID = 1L;
+    public static final Long ID = 10102L;
+
+    @Override
+    protected String getConfiguredText() {
+      return TEXTS.get("B");
+    }
+
+    @Override
+    public Long getId() {
+      return ID;
+    }
+  }
+
+  @Order(30.0)
+  public static class CCode extends AbstractCode<Long> {
+
+    private static final long serialVersionUID = 1L;
+    public static final Long ID = 10103L;
+
+    @Override
+    protected String getConfiguredText() {
+      return TEXTS.get("C");
+    }
+
+    @Override
+    public Long getId() {
+      return ID;
+    }
+  }
+
+  @Order(40.0)
+  public static class DCode extends AbstractCode<Long> {
+
+    private static final long serialVersionUID = 1L;
+    public static final Long ID = 10104L;
+
+    @Override
+    protected String getConfiguredText() {
+      return TEXTS.get("D");
+    }
+
+    @Override
+    public Long getId() {
+      return ID;
+    }
+  }
+}
