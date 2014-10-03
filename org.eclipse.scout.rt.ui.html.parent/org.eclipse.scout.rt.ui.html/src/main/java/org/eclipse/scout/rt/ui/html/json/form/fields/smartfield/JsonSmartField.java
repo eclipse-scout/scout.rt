503470571532d5de750cc1a6ecbf4ce0ba26702d
Andre Wegmueller <awe@bsiag.com>
2014-10-03 14:50:07 +0200

    HtmlUi: - (simple) SmartField works... somehow

--- org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/form/fields/smartfield/JsonSmartField.java
+++ org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/form/fields/smartfield/JsonSmartField.java
@@ -10,22 +10,18 @@
  ******************************************************************************/
 package org.eclipse.scout.rt.ui.html.json.form.fields.smartfield;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.util.List;
 
-import org.eclipse.scout.commons.exception.ProcessingException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.scout.commons.holders.Holder;
 import org.eclipse.scout.commons.logger.IScoutLogger;
 import org.eclipse.scout.commons.logger.ScoutLogManager;
+import org.eclipse.scout.rt.client.ClientSyncJob;
 import org.eclipse.scout.rt.client.ui.form.fields.smartfield.IContentAssistField;
-import org.eclipse.scout.rt.client.ui.form.fields.smartfield.IContentAssistFieldProposalForm;
-import org.eclipse.scout.rt.client.ui.form.fields.smartfield.IContentAssistFieldUIFacade;
 import org.eclipse.scout.rt.client.ui.form.fields.smartfield.ISmartField;
 import org.eclipse.scout.rt.shared.services.lookup.ILookupRow;
 import org.eclipse.scout.rt.ui.html.json.IJsonSession;
-import org.eclipse.scout.rt.ui.html.json.JsonEvent;
-import org.eclipse.scout.rt.ui.html.json.JsonException;
-import org.eclipse.scout.rt.ui.html.json.JsonResponse;
 import org.eclipse.scout.rt.ui.html.json.form.fields.JsonValueField;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -34,18 +30,10 @@ public class JsonSmartField extends JsonValueField<ISmartField> {
 
   private static final IScoutLogger LOG = ScoutLogManager.getLogger(JsonSmartField.class);
 
-  private PropertyChangeListener m_pcl = new PropertyChangeListener() {
-    @Override
-    public void propertyChange(PropertyChangeEvent evt) {
-      if (IContentAssistField.PROP_PROPOSAL_FORM.equals(evt.getPropertyName())) {
-        LOG.debug("form opened " + evt.getNewValue());
-      }
-    }
-  };
+  private static final int MAX_OPTIONS = 100;
 
   public JsonSmartField(ISmartField model, IJsonSession session, String id) {
     super(model, session, id);
-    getModel().addPropertyChangeListener(m_pcl);
   }
 
   @Override
@@ -54,43 +42,26 @@ public class JsonSmartField extends JsonValueField<ISmartField> {
   }
 
   @Override
-  public void handleUiEvent(JsonEvent event, JsonResponse res) {
-    if ("proposal".equals(event.getType())) {
-      // TODO AWE: (smartfield) wir müssen unterscheiden zwischen:
-      // 1. proposal form ist noch nicht offen
-      // 2. proposal form ist offen und der text ändert sich
-      String text = event.getData().optString("text");
-      ISmartField model = getModel();
-      IContentAssistFieldUIFacade uiFacade = model.getUIFacade();
-      IContentAssistFieldProposalForm<?> proposalForm = model.getProposalForm();
-      if (proposalForm == null) {
-        uiFacade.openProposalFromUI(text, false);
-        proposalForm = model.getProposalForm();
-      }
-      JSONArray searchResults = new JSONArray();
-      if (proposalForm.isFormOpen()) {
-        List<? extends ILookupRow<?>> searchResultsLr = proposalForm.getSearchResult().getLookupRows();
-        for (ILookupRow<?> lr : searchResultsLr) {
-          searchResults.put(lr.getText());
-        }
-        System.out.println("text=" + text + " searchResults=" + searchResults);
-      }
-      try {
-        proposalForm.doClose();
-//      uiFacade.acceptProposalFromUI();
-      }
-      catch (ProcessingException e) {
-        throw new JsonException(e);
-      }
-      // TODO AWE: (json) wir sollten die methode so erweitern, dass man auch ein JSONArray übergeben kann
-      // ohne in ein JSONObject zu verpacken. Blöd ist, dass es keine gemeinsame Basis-Klasse dafür gibt.
-      JSONObject json = new JSONObject();
-      putProperty(json, "searchResults", searchResults);
-      res.addActionEvent(getId(), "searchResultsUpdated", json);
-    }
-    else {
-      super.handleUiEvent(event, res);
-    }
+  public JSONObject toJson() {
+    return addOptions(super.toJson());
   }
 
+  private JSONObject addOptions(JSONObject json) {
+    // TODO AWE: (smartfield) überlegen ob wir die optionen wirklich hier laden wollen oder besser erst,
+    // wenn das popup im UI zum ersten mal geöffnet wird.
+    final Holder<List<? extends ILookupRow<?>>> holder = new Holder<>();
+    new ClientSyncJob("loadOptions", getJsonSession().getClientSession()) {
+      @Override
+      protected void runVoid(IProgressMonitor monitor) throws Throwable {
+        @SuppressWarnings("unchecked")
+        List<? extends ILookupRow<?>> list = getModel().callBrowseLookup(IContentAssistField.BROWSE_ALL_TEXT, MAX_OPTIONS);
+        holder.setValue(list);
+      }
+    }.runNow(new NullProgressMonitor());
+    JSONArray optionsArray = new JSONArray();
+    for (ILookupRow<?> lr : holder.getValue()) {
+      optionsArray.put(lr.getText());
+    }
+    return putProperty(json, "options", optionsArray);
+  }
 }
--- org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/form/fields/smartfield/SmartField.js
+++ org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/form/fields/smartfield/SmartField.js
@@ -1,6 +1,8 @@
 scout.SmartField = function() {
   scout.SmartField.parent.call(this);
   this._$popup;
+  this.options;
+  this._selectedOption = -1;
 };
 scout.inherits(scout.SmartField, scout.ValueField);
 
@@ -18,56 +20,128 @@ scout.SmartField.prototype._render = function($parent) {
     // TODO AWE: (smartfield) event-handling mit C.GU besprechen, siehe auch ValueField.js
     // --> 1. auf ValueField eine attachXyzEvent() Methode machen
     //     2. _onXyzEvent überschreiben
-    on('keyup', this._onKeyup.bind(this)).
-    on('blur', this._onBlur.bind(this));
+    focus(this._onFocus.bind(this)).
+    blur(this._onBlur.bind(this)).
+    keyup(this._onKeyup.bind(this));
 
   this.addIcon();
   this.addStatus();
 };
 
-scout.SmartField.prototype._onKeyup = function() {
-  $.log.debug("_onKeyup");
+scout.SmartField.prototype._onKeyup = function(e) {
+
+  // escape
+  if (e.which == 27) { // TODO AWE (smartfield) key-constanten von C.GU verwenden
+    this.$field.blur();
+    return;
+  }
+
+  // enter
+  if (e.which == 13) {
+    if (this._selectedOption > -1) {
+      var value = $(this._$popup.children(':visible').get(this._selectedOption)).html();
+      this.$field.val(value);
+      this._closePopup();
+      this.$field.select();
+    }
+    return;
+  }
+
+  // ensure popup is opened for following operations
   if (!this._$popup) {
-    var fieldBounds = scout.HtmlComponent.getBounds(this.$field);
-    var popupBounds = new scout.Rectangle(fieldBounds.x, fieldBounds.y + fieldBounds.height, fieldBounds.width, 100);
-    this._$popup = $('<div>').
-      addClass('smart-field-popup').
-      appendTo(this.$container);
-    scout.HtmlComponent.setBounds(this._$popup, popupBounds);
-    this.session.send('proposal', this.id, {'text':this.$field.val()});
+    setTimeout(function() {
+      this._openPopup();
+    }.bind(this));
+    return;
+  }
+
+  // navigate in options
+  // TODO AWE: (smartfield) support page-up/down 33/34
+  if (e.which == 38 || e.which == 40) {
+    var $options = this._$popup.children(':visible');
+    var pos = this._selectedOption;
+    if (e.which == 38) { pos--; }
+    if (e.which == 40) { pos++; }
+    if (pos < 0) {
+      pos = 0;
+    } else {
+      if (pos >= $options.length) {
+        pos = $options.length - 1;
+      }
+    }
+    if (pos != this.selectedOption) {
+      if (this._selectedOption >= 0 && this._selectedOption < $options.length) {
+        $($options[this._selectedOption]).removeClass('selected');
+      }
+      $($options[pos]).addClass('selected');
+      $.log.info('_selectedOption=' + this._selectedOption + ' pos='+pos);
+      this._selectedOption = pos;
+    }
+    return;
+  }
+
+  // filter options
+  this._filterOptions(this.$field.val());
+};
+
+scout.SmartField.prototype._openPopup = function() {
+  var fieldBounds = scout.HtmlComponent.getBounds(this.$field);
+  var popupBounds = new scout.Rectangle(fieldBounds.x, fieldBounds.y + fieldBounds.height, fieldBounds.width, 100);
+  this._$popup = $('<div>').
+    addClass('smart-field-popup').
+    appendTo(this.$container);
+  scout.HtmlComponent.setBounds(this._$popup, popupBounds);
+
+  // add options
+  var i, option;
+  for (i=0; i<this.options.length; i++) {
+    option = this.options[i];
+    $('<div>').
+      on('mousedown', this._onOptionMousedown.bind(this)).
+      appendTo(this._$popup).
+      html(option);
+  }
+};
+
+scout.SmartField.prototype._onOptionMousedown = function(e) {
+  var selectedText = $(e.target).html();
+  $.log.info('option selected ' + selectedText);
+  this.$field.val(selectedText);
+  this._closePopup();
+};
+
+
+scout.SmartField.prototype._onFocus = function() {
+  $.log.debug("_onFocus");
+  if (!this._$popup) {
+    this._openPopup();
   }
 };
 
 scout.SmartField.prototype._onBlur = function() {
   $.log.debug("_onBlur");
+  this._closePopup();
+};
+
+scout.SmartField.prototype._closePopup = function() {
   if (this._$popup) {
+    this._selectedOption = -1;
     this._$popup.remove();
     this._$popup = null;
   }
 };
 
-scout.SmartField.prototype.onModelAction = function(event) {
-  if (event.type == 'searchResultsUpdated') {
-    this._onSearchResultsUpdated(event.searchResults);
-  } else {
-    scout.SmartField.parent.prototype.onModelAction(event).bind(this);
-  }
+scout.SmartField.prototype._filterOptions = function(text) {
+  this._selectedOption = -1;
+  var regexp = new RegExp(text, 'i');
+  this._$popup.children().each(function() {
+    if (!text || '*' === text) {
+      $(this).setVisible(true);
+    } else {
+      // TODO AWE: (smartfield) implement clever client-side match algorithm
+      $.log.debug('regexp='+regexp + ' html='+$(this).html());
+      $(this).setVisible($(this).html().match(regexp));
+    }
+  });
 };
 
-scout.SmartField.prototype._onSearchResultsUpdated = function(searchResults) {
-  $.log.info('on searchResultsUpdated: ' + searchResults);
-  this._$popup.children().remove();
-  var i, sr;
-  for (i=0; i<searchResults.length; i++) {
-    sr = searchResults[i];
-    $('<div>').
-      on('click', this._proposalSelected.bind(this)).
-      appendTo(this._$popup).
-      html(sr);
-  }
-};
-
-scout.SmartField.prototype._proposalSelected = function(event) {
-  var source = event.target;
-  $.log.info('proposal selected ' + $(source).html());
-};
