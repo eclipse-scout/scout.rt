78b5cdb5870037da51454d85041ea9cb237be809
Nicolas Luenzer <nlu@bsi-software.com>
2014-10-09 17:00:52 +0200

    446478: Scout JAX-WS 2.1.6 bundle does not work with Java 1.8
    Change-Id: I057bea06b76605a70e9c09a4947b7852de6dc9c5
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=446478
    Signed-off-by: Nicolas Luenzer <nlu@bsi-software.com>
    Reviewed-on: https://git.eclipse.org/r/54580
    Tested-by: Hudson CI
    Reviewed-by: Stephan Leicht Vogt <stephan.leichtvogt@bsi-software.com>

---
+++ org.eclipse.scout.jaxws216.jre16.fragment/src/org/eclipse/scout/jaxws/internal/servlet/ServletConnection.java
@@ -0,0 +1,212 @@
+/*******************************************************************************
+ * Copyright (c) 2011 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Daniel Wiehl (BSI Business Systems Integration AG) - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.jaxws.internal.servlet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.handler.MessageContext;
+
+import com.sun.xml.internal.ws.api.message.Packet;
+import com.sun.xml.internal.ws.api.server.WebServiceContextDelegate;
+import com.sun.xml.internal.ws.developer.JAXWSProperties;
+import com.sun.xml.internal.ws.transport.http.HttpAdapter;
+import com.sun.xml.internal.ws.transport.http.WSHTTPConnection;
+
+@SuppressWarnings("restriction")
+public class ServletConnection extends WSHTTPConnection {
+
+  private static final PropertyMap PROP_MODEL;
+
+  private final HttpServletRequest m_request;
+  private final HttpServletResponse m_response;
+  private final ServletContext m_context;
+  private final WebServiceContextDelegate m_contextDelegate;
+  private int m_status;
+
+  private final Map<String, List<String>> m_responseHeaderMap;
+
+  static {
+    PROP_MODEL = parse(ServletConnection.class);
+  }
+
+  public ServletConnection(HttpAdapter adapter, ServletContext context, HttpServletRequest request, HttpServletResponse response) {
+    m_context = context;
+    m_request = request;
+    m_response = response;
+    m_contextDelegate = createContextDelegate(adapter, request);
+    m_responseHeaderMap = new HashMap<String, List<String>>();
+  }
+
+  @Property(MessageContext.SERVLET_RESPONSE)
+  public HttpServletResponse getResponse() {
+    return m_response;
+  }
+
+  @Property(MessageContext.SERVLET_REQUEST)
+  public HttpServletRequest getRequest() {
+    return m_request;
+  }
+
+  @Property(JAXWSProperties.HTTP_REQUEST_URL)
+  public String getRequestURL() {
+    return m_request.getRequestURL().toString();
+  }
+
+  @Override
+  public void setStatus(int status) {
+    m_status = status;
+  }
+
+  @Override
+  @Property(MessageContext.HTTP_RESPONSE_CODE)
+  public int getStatus() {
+    return m_status;
+  }
+
+  @Override
+  public boolean isSecure() {
+    return m_request.getScheme().equals("https");
+  }
+
+  @Override
+  public InputStream getInput() throws IOException {
+    return m_request.getInputStream();
+  }
+
+  @Override
+  public String getProtocol() {
+    return m_request.getProtocol();
+  }
+
+  @Override
+  public OutputStream getOutput() throws IOException {
+    m_response.setStatus(m_status);
+    return m_response.getOutputStream();
+  }
+
+  @Override
+  @Property(MessageContext.PATH_INFO)
+  public String getPathInfo() {
+    return m_request.getPathInfo();
+  }
+
+  @Override
+  public String getBaseAddress() {
+    return JaxWsHelper.getBaseAddress(m_request, true);
+  }
+
+  @Override
+  @Property(MessageContext.QUERY_STRING)
+  public String getQueryString() {
+    return m_request.getQueryString();
+  }
+
+  @Override
+  @Property(MessageContext.HTTP_REQUEST_METHOD)
+  public String getRequestMethod() {
+    return m_request.getMethod();
+  }
+
+  @Override
+  public String getRequestHeader(String headerName) {
+    return m_request.getHeader(headerName);
+  }
+
+  @Override
+  @SuppressWarnings("deprecation")
+  @Property({MessageContext.HTTP_REQUEST_HEADERS, Packet.INBOUND_TRANSPORT_HEADERS})
+  public Map<String, List<String>> getRequestHeaders() {
+    Map<String, List<String>> headerMap = new HashMap<String, List<String>>();
+
+    Enumeration headerNames = m_request.getHeaderNames();
+    while (headerNames.hasMoreElements()) {
+      String name = (String) headerNames.nextElement();
+      String value = m_request.getHeader(name);
+
+      if (headerMap.get(name) == null) {
+        headerMap.put(name, new ArrayList<String>());
+      }
+      headerMap.get(name).add(value);
+    }
+
+    return headerMap;
+  }
+
+  @Override
+  public void setResponseHeaders(Map<String, List<String>> responseHeaders) {
+    m_responseHeaderMap.clear();
+    m_response.reset();
+    m_response.setStatus(m_status);
+
+    // update headers of @{link HttpServletResponse}
+    if (responseHeaders == null || responseHeaders.size() == 0) {
+      return;
+    }
+    for (Entry<String, List<String>> entry : responseHeaders.entrySet()) {
+      String name = entry.getKey();
+      List<String> values = entry.getValue();
+
+      // According to JavaDoc, ignore 'Content-Type' and 'Content-Length'
+      if (name.equalsIgnoreCase("Content-Type") || name.equalsIgnoreCase("Content-Length")) {
+        continue;
+      }
+      for (String value : values) {
+        m_response.addHeader(name, value);
+      }
+    }
+  }
+
+  @Override
+  @Property({MessageContext.HTTP_RESPONSE_HEADERS, Packet.OUTBOUND_TRANSPORT_HEADERS})
+  public Map<String, List<String>> getResponseHeaders() {
+    return JaxWsHelper.cloneHeaderMap(m_responseHeaderMap); // clone according do JavaDoc
+  }
+
+  @Override
+  public void setContentTypeResponseHeader(String value) {
+    m_response.setContentType(value);
+  }
+
+  @Override
+  public void setContentLengthResponseHeader(int value) {
+    m_response.setContentLength(value);
+  }
+
+  @Property(MessageContext.SERVLET_CONTEXT)
+  public ServletContext getContext() {
+    return m_context;
+  }
+
+  @Override
+  public WebServiceContextDelegate getWebServiceContextDelegate() {
+    return m_contextDelegate;
+  }
+
+  @Override
+  protected PropertyMap getPropertyMap() {
+    return PROP_MODEL;
+  }
+
+  protected WebServiceContextDelegate createContextDelegate(HttpAdapter adapter, HttpServletRequest request) {
+    return new ServletContextDelegate(adapter, request);
+  }
+}
