9f01b5fd5137fd3f5bd4e307421311fc159306d6
Matthias Fischer <mfi@bsiag.com>
2014-10-21 15:23:36 +0200

    350389: improve Scout RT testing support
    Change-Id: Ic7fa80cd2444aadc5460c1d7c07f71d104288a82
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=350389
    Signed-off-by: Matthias Fischer <mfi@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/35218
    Tested-by: Hudson CI
    Reviewed-by: Judith Gull <jgu@bsiag.com>

--- org.eclipse.scout.commons.test/src/org/eclipse/scout/commons/CollectionUtilityTest.java
+++ org.eclipse.scout.commons.test/src/org/eclipse/scout/commons/CollectionUtilityTest.java
@@ -18,8 +18,10 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.Queue;
 import java.util.SortedMap;
 import java.util.TreeMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.junit.Test;
 
@@ -27,6 +29,46 @@ import org.junit.Test;
  * JUnit tests for {@link CollectionUtility}
  */
 public class CollectionUtilityTest {
+
+  /**
+   * Test for {@link CollectionUtility#equalsCollection(Collection<? extends T> c1, Collection<? extends T> c2)} and
+   * {@link CollectionUtility#equalsCollection(Collection<? extends T> c1, Collection<? extends T> c2, boolean
+   * considerElementPosition)}
+   */
+  @Test
+  public void testEqualsCollection() {
+    Queue<Object> s = createQueue("a", "b");
+    assertEquals(true, CollectionUtility.equalsCollection(null, null));
+    assertEquals(true, CollectionUtility.equalsCollection(s, s));
+    assertEquals(false, CollectionUtility.equalsCollection(s, null));
+    assertEquals(false, CollectionUtility.equalsCollection(null, s));
+
+    assertEquals(true, CollectionUtility.equalsCollection(createQueue("a", "b"), createQueue("a", "b")));
+    assertEquals(true, CollectionUtility.equalsCollection(createQueue("a", "b"), createQueue("b", "a")));
+    assertEquals(false, CollectionUtility.equalsCollection(createQueue("a", "b"), createQueue("a")));
+    assertEquals(false, CollectionUtility.equalsCollection(createQueue("a", "b"), createQueue("x", "y")));
+
+    assertEquals(true, CollectionUtility.equalsCollection(createQueue("a", "b"), createQueue("a", "b"), true));
+    assertEquals(false, CollectionUtility.equalsCollection(createQueue("a", "b"), createQueue("b", "a"), true));
+    assertEquals(false, CollectionUtility.equalsCollection(createQueue("a", "b"), createQueue("a"), true));
+    assertEquals(false, CollectionUtility.equalsCollection(createQueue("a", "b"), createQueue("x", "y"), true));
+  }
+
+  /**
+   * Test for {@link CollectionUtility#equalsCollection(List<? extends T> c1, List<? extends T> c2)}
+   */
+  @Test
+  public void testEqualsCollectionList() {
+    List<Object> l = createList("a", "b");
+    assertEquals(true, CollectionUtility.equalsCollection(null, null));
+    assertEquals(true, CollectionUtility.equalsCollection(l, l));
+    assertEquals(false, CollectionUtility.equalsCollection(l, null));
+    assertEquals(false, CollectionUtility.equalsCollection(null, l));
+
+    assertEquals(true, CollectionUtility.equalsCollection(createList("a", "b"), createList("a", "b")));
+    assertEquals(false, CollectionUtility.equalsCollection(createList("a", "b"), createList("a")));
+    assertEquals(false, CollectionUtility.equalsCollection(createList("a", "b"), createList("b", "a")));
+  }
 
   /**
    * Test for {@link CollectionUtility#firstElement(Object)}
@@ -122,4 +164,30 @@ public class CollectionUtilityTest {
     }));
   }
 
+  /**
+   * Test for {@link CollectionUtility#size(Collection<T> list)}
+   */
+  @Test
+  public void testSize() {
+    assertEquals(0, CollectionUtility.size(null));
+    assertEquals(0, CollectionUtility.size(new ArrayList<Object>()));
+    assertEquals(1, CollectionUtility.size(createList("1")));
+    assertEquals(2, CollectionUtility.size(createList("1", "2")));
+  }
+
+  private List<Object> createList(Object... elements) {
+    List<Object> list = new ArrayList<Object>();
+    for (Object s : elements) {
+      list.add(s);
+    }
+    return list;
+  }
+
+  private Queue<Object> createQueue(Object... elements) {
+    Queue<Object> list = new ConcurrentLinkedQueue<Object>();
+    for (Object s : elements) {
+      list.add(s);
+    }
+    return list;
+  }
 }
