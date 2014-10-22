b6d46b4e305f614a15a6dca91a1d1abb3c7a3d97
Samuel Renold <sre@bsiag.com>
2014-10-22 10:50:53 +0200

    448063: DataModelAttributeOp$Between displays wrong texts
    Change-Id: I9ee125b259c2abf7e37f607a153a7f8e8dacf0ba
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=448063
    Signed-off-by: Samuel Renold <sre@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/35308
    Tested-by: Hudson CI
    Reviewed-by: Judith Gull <jgu@bsiag.com>

--- org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/data/model/DataModelAttributeOp.java
+++ org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/data/model/DataModelAttributeOp.java
@@ -1432,10 +1432,10 @@ public final class DataModelAttributeOp implements DataModelConstants {
     @Override
     public String createVerboseText(Integer aggregationType, String attributeText, List<String> valueTexts) {
       if (StringUtility.isNullOrEmpty(valueTexts.get(0))) {
-        return buildText(aggregationType, attributeText, ScoutTexts.get("LogicLE"), valueTexts.get(0));
+        return buildText(aggregationType, attributeText, ScoutTexts.get("LogicLE"), valueTexts.get(1));
       }
       else if (StringUtility.isNullOrEmpty(valueTexts.get(1))) {
-        return buildText(aggregationType, attributeText, ScoutTexts.get("LogicGE"), valueTexts.get(1));
+        return buildText(aggregationType, attributeText, ScoutTexts.get("LogicGE"), valueTexts.get(0));
       }
       else {
         return buildText(aggregationType, attributeText, getText(), valueTexts);
