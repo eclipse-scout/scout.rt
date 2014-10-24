e7e7c89fabd954194b299b44c2651ed803ad323d
Stephan Merkli <sme@bsiag.com>
2014-10-24 17:50:41 +0200

    448648: MailUtility.collectMailParts: inline attachments are not collected
    Change-Id: I4f0c47496de869b944846b0fd08b720b8fe5f944
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=448648
    Reviewed-on: https://git.eclipse.org/r/35482
    Tested-by: Hudson CI
    Reviewed-by: Bruno  Koeferli <bruno.koeferli@bsiag.com>

--- org.eclipse.scout.commons.test/src/org/eclipse/scout/commons/MailUtilityTest.java
+++ org.eclipse.scout.commons.test/src/org/eclipse/scout/commons/MailUtilityTest.java
@@ -18,14 +18,22 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.activation.DataSource;
+import javax.mail.BodyPart;
 import javax.mail.MessagingException;
+import javax.mail.Part;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.mail.util.ByteArrayDataSource;
 
 import org.eclipse.scout.commons.exception.ProcessingException;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -36,7 +44,7 @@ public class MailUtilityTest {
 
   /**
    * Message without sender can be created
-   * 
+   *
    * @throws ProcessingException
    *           ,MessagingException
    */
@@ -88,5 +96,89 @@ public class MailUtilityTest {
 
     MimeMessage message = MailUtility.createMimeMessage("test", null, new DataSource[]{ds});
     message.writeTo(new ByteArrayOutputStream());
+  }
+
+  @Test
+  public void testGetParts() throws ProcessingException, IOException, MessagingException {
+    final byte[] sampleData = new byte[]{0x0, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF};
+    DataSource[] attachments = new DataSource[]{
+        MailUtility.createDataSource(new ByteArrayInputStream(sampleData), "sample1.dat", null),
+        MailUtility.createDataSource(new ByteArrayInputStream(sampleData), "sample2.dat", null),
+        MailUtility.createDataSource(new ByteArrayInputStream(sampleData), "sample3.dat", null)
+    };
+
+    MimeMessage message = MailUtility.createMimeMessage("plain text", "<html><body><p>plain text</p></html>", attachments);
+
+    Assert.assertEquals("wrong plain text", "plain text", MailUtility.getPlainText(message));
+
+    Part[] bodyParts = MailUtility.getBodyParts(message);
+    Assert.assertEquals("body parts size is wrong", 2, bodyParts.length);
+
+    Part[] attachmentParts = MailUtility.getAttachmentParts(message);
+    Assert.assertEquals("attachments parts size is wrong", 3, attachmentParts.length);
+
+    Part plainTextPart = MailUtility.getPlainTextPart(bodyParts);
+    Assert.assertNotNull("no plain text part found", plainTextPart);
+    Assert.assertTrue("plain text part content is not string", plainTextPart.getContent() instanceof String);
+    Assert.assertEquals("wrong plain text", "plain text", (String) plainTextPart.getContent());
+
+    Part htmlPart = MailUtility.getHtmlPart(bodyParts);
+    Assert.assertNotNull("no html part found", htmlPart);
+    Assert.assertTrue("html part content is not string", htmlPart.getContent() instanceof String);
+    Assert.assertEquals("wrong html text", "<html><body><p>plain text</p></html>", (String) htmlPart.getContent());
+
+    // validate that the method call does not throw an exception
+    MailUtility.collectMailParts(null, null, null, null);
+    MailUtility.collectMailParts(message, null, null, null);
+
+    List<Part> bodyCollector = new ArrayList<Part>();
+    List<Part> attachmentCollector = new ArrayList<Part>();
+    List<Part> inlineAttachmentCollector = new ArrayList<Part>();
+    MailUtility.collectMailParts(message, bodyCollector, attachmentCollector, inlineAttachmentCollector);
+    Assert.assertEquals("body parts size is wrong", 2, bodyCollector.size());
+    Assert.assertEquals("attachments parts size is wrong", 3, attachmentCollector.size());
+    Assert.assertEquals("inline attachments parts size is wrong", 0, inlineAttachmentCollector.size());
+  }
+
+  @Test
+  public void testInlineAttachmentCollector() throws MessagingException, ProcessingException {
+    CharsetSafeMimeMessage message = new CharsetSafeMimeMessage();
+    MimeMultipart multiPart = new MimeMultipart();
+    message.setContent(multiPart);
+
+    MimeBodyPart bodyPart = new MimeBodyPart();
+    bodyPart.setText("plain text", "UTF-8");
+    bodyPart.addHeader(MailUtility.CONTENT_TYPE_ID, MailUtility.CONTENT_TYPE_TEXT_PLAIN);
+    multiPart.addBodyPart(bodyPart);
+
+    BodyPart inlineAttachmentPart = new MimeBodyPart();
+    inlineAttachmentPart.setContent("base-64-encoded-image-content", "image/gif");
+    inlineAttachmentPart.addHeader(MailUtility.CONTENT_TYPE_ID, "image/gif; name=\"graycol.gif\"");
+    inlineAttachmentPart.addHeader("Content-ID", "<5__=4EBBF65EEFABF58A8f9e8a9@example.org>");
+    inlineAttachmentPart.addHeader("Content-Disposition", Part.INLINE);
+    multiPart.addBodyPart(inlineAttachmentPart);
+
+    List<Part> bodyCollector = new ArrayList<Part>();
+    List<Part> attachmentCollector = new ArrayList<Part>();
+    List<Part> inlineAttachmentCollector = new ArrayList<Part>();
+    MailUtility.collectMailParts(message, bodyCollector, attachmentCollector, inlineAttachmentCollector);
+    Assert.assertEquals("body parts size is wrong", 1, bodyCollector.size());
+    Assert.assertEquals("attachments parts size is wrong", 0, attachmentCollector.size());
+    Assert.assertEquals("inline attachments parts size is wrong", 1, inlineAttachmentCollector.size());
+  }
+
+  @Test
+  public void testAddAttachmentsToMimeMessage() throws ProcessingException, IOException, MessagingException {
+    MimeMessage message = MailUtility.createMimeMessage("plain text", "<html><body><p>plain text</p></html>", null);
+    Assert.assertEquals("attachments parts size is wrong", 0, MailUtility.getAttachmentParts(message).length);
+
+    final byte[] sampleData = new byte[]{0x0, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF};
+    List<File> attachments = new ArrayList<File>();
+
+    attachments.add(IOUtility.createTempFile("sample1.dat", sampleData));
+    attachments.add(IOUtility.createTempFile("sample2.dat", sampleData));
+    attachments.add(IOUtility.createTempFile("sample3.dat", sampleData));
+    MailUtility.addAttachmentsToMimeMessage(message, attachments);
+    Assert.assertEquals("attachments parts size is wrong", 3, MailUtility.getAttachmentParts(message).length);
   }
 }
