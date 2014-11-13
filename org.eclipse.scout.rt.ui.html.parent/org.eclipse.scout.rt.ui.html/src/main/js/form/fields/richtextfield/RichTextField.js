67bb495078eb6c62f9d0d5a3cc5c92322fba315b
Christian Rusche <christian.rusche@bsi-software.com>
2014-11-13 12:32:49 +0100

    html ui: rich text

--- org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/form/fields/richtextfield/RichTextField.js
+++ org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/form/fields/richtextfield/RichTextField.js
@@ -9,16 +9,43 @@ scout.RichTextField.prototype._renderProperties = function() {
 
 scout.RichTextField.prototype._render = function($parent) {
   this.addContainer($parent, 'rich-text-field');
-  this.addLabel();
-  this.addMandatoryIndicator();
 
-  this.$field = $('<div>').text('rich text div');
+  // create editabel div
+  this.$field = $.makeDIV('field', 'Beispieltext');
+  this.$field
+    .blur(this._onFieldBlur.bind(this))
+    .attr('contentEditable', 'true')
+    .appendTo(this.$container);
 
-  this.$field.
-    addClass('field').
-    blur(this._onFieldBlur.bind(this)).
-    appendTo(this.$container);
+  // return should not create div
+  this.$field.keydown(function(e) {
+    if (e.keyCode === 13) {
+      document.execCommand('insertHTML', false, '<br><br>');
+      return false;
+    }
+  });
 
-  this.addStatus();
+  // command bar
+  this.$commandBar = this.$container.appendDIV('rich-text-bar');
+
+  this.$commandFormat = this.$commandBar.appendDIV('rich-text-bar-group');
+  this.$commandFormat.appendDIV('rich-text-command rich-text-bar-bold', 'a').data('command', 'bold');
+  this.$commandFormat.appendDIV('rich-text-command rich-text-bar-underline', 'a').data('command', 'underline');
+  this.$commandFormat.appendDIV('rich-text-command rich-text-bar-strike', 'a').data('command', 'strikeThrough');
+
+  this.$commandMark = this.$commandBar.appendDIV('rich-text-bar-group');
+  this.$commandMark.appendDIV('rich-text-command rich-text-bar-white', '&nbsp');
+  this.$commandMark.appendDIV('rich-text-command rich-text-bar-yellow', '&nbsp');
+  this.$commandMark.appendDIV('rich-text-command rich-text-bar-green', '&nbsp');
+
+  this.$commandList = this.$commandBar.appendDIV('rich-text-bar-group');
+  this.$commandList.appendDIV('rich-text-command rich-text-bar-plain', 'ohne');
+  this.$commandList.appendDIV('rich-text-command rich-text-bar-bullet', 'bullet');
+  this.$commandList.appendDIV('rich-text-command rich-text-bar-number', 'num');
+
+  // anders lösen, denn: feld soll über die ganze breite gehen
+  //this.addLabel();
+  //this.addMandatoryIndicator();
+  //this.addStatus();
 };
 
