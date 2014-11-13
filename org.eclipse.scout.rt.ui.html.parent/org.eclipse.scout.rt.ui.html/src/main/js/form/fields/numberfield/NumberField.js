b41e5f149246353fe29810978d5c9d7804fb975e
Christian Rusche <christian.rusche@bsi-software.com>
2014-11-13 12:32:02 +0100

    html ui: number field calculator

--- org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/form/fields/numberfield/NumberField.js
+++ org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/form/fields/numberfield/NumberField.js
@@ -12,8 +12,30 @@ scout.NumberField.prototype._render = function($parent) {
   this.$field = $('<input>').
     attr('type', 'text').
     addClass('field').
+    blur(this._parse.bind(this)).
     blur(this._onFieldBlur.bind(this)).
     appendTo(this.$container);
 
   this.addStatus();
+};
+
+scout.NumberField.prototype._parse = function () {
+  var value = this.$field.val();
+
+  // find simple format for value function
+  value = value.split(this.parent.session.locale.decimalFormat.groupChar).join('');
+  value = value.split(this.parent.session.locale.decimalFormat.pointChar).join('.');
+
+  // in case of non math symbols return false
+  if (value.match(/[^\s\d\(\)\+\-\*\/\.]/g, '')) {
+    return false;
+  }
+
+  // evaluate, in case of math errors return false
+  try {
+    value = String(eval(value));
+    this.$field.val(value);
+  } catch (err) {
+    return false;
+  }
 };
