f926d08560da27f4875952bc1948b9aa237fe8b2
Beat Schwarzentrub <bsh@bsiag.com>
2014-11-27 10:52:29 +0100

    HtmlUi: JsonKeyStroke extends JsonAction

--- org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/action/keystroke/JsonKeyStroke.java
+++ org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/action/keystroke/JsonKeyStroke.java
@@ -11,52 +11,17 @@
 package org.eclipse.scout.rt.ui.html.json.action.keystroke;
 
 import org.eclipse.scout.rt.client.ui.action.keystroke.IKeyStroke;
-import org.eclipse.scout.rt.ui.html.json.AbstractJsonPropertyObserver;
 import org.eclipse.scout.rt.ui.html.json.IJsonSession;
-import org.eclipse.scout.rt.ui.html.json.JsonEvent;
-import org.eclipse.scout.rt.ui.html.json.JsonProperty;
-import org.eclipse.scout.rt.ui.html.json.JsonResponse;
+import org.eclipse.scout.rt.ui.html.json.action.JsonAction;
 
-// TODO BSH Combine with JsonMenu --> JsonAction?
-public class JsonKeyStroke<T extends IKeyStroke> extends AbstractJsonPropertyObserver<T> {
-
-  public static final String EVENT_KEYSTROKE_ACTION = "keystrokeAction";
+public class JsonKeyStroke<T extends IKeyStroke> extends JsonAction<T> {
 
   public JsonKeyStroke(T model, IJsonSession jsonSession, String id) {
     super(model, jsonSession, id);
   }
 
   @Override
-  protected void initJsonProperties(T model) {
-    super.initJsonProperties(model);
-
-    putJsonProperty(new JsonProperty<IKeyStroke>(IKeyStroke.PROP_ENABLED, model) {
-      @Override
-      protected Boolean modelValue() {
-        return getModel().isEnabled();
-      }
-    });
-    putJsonProperty(new JsonProperty<IKeyStroke>(IKeyStroke.PROP_KEYSTROKE, model) {
-      @Override
-      protected String modelValue() {
-        return getModel().getKeyStroke();
-      }
-    });
-  }
-
-  @Override
   public String getObjectType() {
     return "KeyStroke";
-  }
-
-  @Override
-  public void handleUiEvent(JsonEvent event, JsonResponse res) {
-    if (EVENT_KEYSTROKE_ACTION.equals(event.getType())) {
-      handleUiKeyStrokeAction(event, res);
-    }
-  }
-
-  public void handleUiKeyStrokeAction(JsonEvent event, JsonResponse res) {
-    getModel().getUIFacade().fireActionFromUI();
   }
 }
