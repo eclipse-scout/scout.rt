9d9505b6d3d74ac3663a7e92e2fd6df40af0df21
Judith Gull <jgu@bsiag.com>
2014-11-28 10:11:54 +0100

    bug 453425: Annotation @DevTestMarker should be Inherited https://bugs.eclipse.org/bugs/show_bug.cgi?id=453425 Change-Id: I696f7b38ab4564282f97c4ea586f081b6f73f894 Signed-off-by: Judith Gull <jgu@bsiag.com> Reviewed-on: https://git.eclipse.org/r/37228 Tested-by: Hudson CI

--- org.eclipse.scout.rt.testing.shared/src/org/eclipse/scout/rt/testing/shared/DevTestMarker.java
+++ org.eclipse.scout.rt.testing.shared/src/org/eclipse/scout/rt/testing/shared/DevTestMarker.java
@@ -11,6 +11,7 @@
 package org.eclipse.scout.rt.testing.shared;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -24,5 +25,6 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Inherited
 public @interface DevTestMarker {
 }
