e2d8437150fe9303592f50b39b03acb1f63424b7
Silvio Kohler <silvio.kohler@bsiag.com>
2014-12-04 16:27:01 +0100

    Bug 402298
    Change-Id: Ia910833f3e87a170de76f90d551c153e4044837c
    Signed-off-by: Silvio Kohler <silvio.kohler@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/37586
    Tested-by: Hudson CI
    Reviewed-by: Judith Gull <jgu@bsiag.com>

--- org.eclipse.scout.rt.client.test/src/org/eclipse/scout/rt/client/services/common/bookmark/BookmarkServiceTest.java
+++ org.eclipse.scout.rt.client.test/src/org/eclipse/scout/rt/client/services/common/bookmark/BookmarkServiceTest.java
@@ -10,15 +10,21 @@
  ******************************************************************************/
 package org.eclipse.scout.rt.client.services.common.bookmark;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import org.eclipse.scout.rt.client.testenvironment.TestEnvironmentClientSession;
 import org.eclipse.scout.rt.client.ui.desktop.IDesktop;
+import org.eclipse.scout.rt.shared.services.common.bookmark.Bookmark;
 import org.eclipse.scout.service.SERVICES;
 import org.eclipse.scout.testing.client.runner.ScoutClientTestRunner;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 
 /**
  * Test for {@link IBookmarkService}
@@ -26,13 +32,53 @@ import org.junit.runner.RunWith;
 @RunWith(ScoutClientTestRunner.class)
 public class BookmarkServiceTest {
 
+  private IDesktop m_desktop;
+  private IDesktop m_mockDesktop;
+
+  @Before
+  public void setUp() {
+    m_desktop = TestEnvironmentClientSession.get().getDesktop();
+    m_mockDesktop = Mockito.mock(IDesktop.class);
+    TestEnvironmentClientSession.get().replaceDesktop(m_mockDesktop);
+  }
+
+  @After
+  public void tearDown() {
+    TestEnvironmentClientSession.get().replaceDesktop(m_desktop);
+  }
+
   @Test
   public void testSetStartBookmark() throws Exception {
-    IDesktop desktop = TestEnvironmentClientSession.get().getDesktop();
-    assertNotNull(desktop);
+    Bookmark bookmark = new Bookmark();
+    bookmark.setText("My Bookmark Text");
+    Mockito.when(m_mockDesktop.createBookmark()).thenReturn(bookmark);
     IBookmarkService s = SERVICES.getService(IBookmarkService.class);
     Assert.assertNotNull(s);
     s.setStartBookmark();
+
+    // Get the Bookmark
+    Bookmark startBookmark = s.getStartBookmark();
+    assertEquals("Kind", Bookmark.USER_BOOKMARK, startBookmark.getKind());
+    assertEquals("Text", "My Bookmark Text", startBookmark.getText());
   }
 
+  @Test
+  public void testDeleteBookmark() throws Exception {
+    Bookmark bookmark = new Bookmark();
+    bookmark.setText("My Bookmark Text");
+    Mockito.when(m_mockDesktop.createBookmark()).thenReturn(bookmark);
+    IBookmarkService s = SERVICES.getService(IBookmarkService.class);
+    Assert.assertNotNull(s);
+    s.setStartBookmark();
+
+    // Get the Bookmark
+    Bookmark startBookmark = s.getStartBookmark();
+    assertNotNull(startBookmark);
+
+    // Delete the bookmark
+    s.deleteStartBookmark();
+
+    startBookmark = s.getStartBookmark();
+    assertNull(startBookmark);
+  }
 }
