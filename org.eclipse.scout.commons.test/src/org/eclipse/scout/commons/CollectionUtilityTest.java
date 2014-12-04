591fdaf4e9d15b88576d7f9ee162065448f1db7f
Otmar Caduff <otmar.caduff@bsiag.com>
2014-12-04 15:04:17 +0100

    402298 added JUnit Test for CollectionUtility
    Change-Id: I6a0f20de90115cdfdd8d3c63e2a88c8ed7ca144d
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=381318
    Signed-off-by: Otmar Caduff <otmar.caduff@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/37582
    Tested-by: Hudson CI
    Reviewed-by: Judith Gull <jgu@bsiag.com>

--- org.eclipse.scout.commons.test/src/org/eclipse/scout/commons/CollectionUtilityTest.java
+++ org.eclipse.scout.commons.test/src/org/eclipse/scout/commons/CollectionUtilityTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumSet;
@@ -164,6 +165,17 @@ public class CollectionUtilityTest {
         put(-1, "-1");
       }
     }));
+  }
+
+  /**
+   * Test for {@link CollectionUtility#containsAny(Collection, Collection)
+
+   */
+  @Test
+  public void testContainsAny() {
+    assertEquals(false, CollectionUtility.containsAny(Collections.emptyList(), Collections.emptyList()));
+    assertEquals(false, CollectionUtility.containsAny(null, Collections.emptyList()));
+    assertEquals(false, CollectionUtility.containsAny(Arrays.asList("a"), Arrays.asList("b")));
   }
 
   /**
