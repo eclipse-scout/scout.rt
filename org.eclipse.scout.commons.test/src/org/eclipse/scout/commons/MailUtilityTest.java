515b810a83b82c70043f963ee0c711602a0bc03a
Judith Gull <jgu@bsiag.com>
2014-12-05 09:53:12 +0100

    Bug 454033: MailUtility: use MimeUtility.encodeText when setting
    FileName for FileDataSource
    
    Change-Id: I67f49bf8076a3f071cd0c7b651b1c535e7e83ad0
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=454033
    Reviewed-on: https://git.eclipse.org/r/37571
    Tested-by: Hudson CI
    Reviewed-by: Judith Gull <jgu@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/37633

--- org.eclipse.scout.commons.test/src/org/eclipse/scout/commons/MailUtilityTest.java
+++ org.eclipse.scout.commons.test/src/org/eclipse/scout/commons/MailUtilityTest.java
@@ -19,9 +19,12 @@ import static org.junit.Assert.assertTrue;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.activation.DataSource;
 import javax.mail.BodyPart;
@@ -30,6 +33,7 @@ import javax.mail.Part;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimeUtility;
 import javax.mail.util.ByteArrayDataSource;
 
 import org.eclipse.scout.commons.exception.ProcessingException;
@@ -107,25 +111,11 @@ public class MailUtilityTest {
         MailUtility.createDataSource(new ByteArrayInputStream(sampleData), "sample3.dat", null)
     };
 
-    MimeMessage message = MailUtility.createMimeMessage("plain text", "<html><body><p>plain text</p></html>", attachments);
+    final String plainText = "plain text";
+    final String htmlText = "<html><body><p>plain text</p></body></html>";
+    MimeMessage message = MailUtility.createMimeMessage(plainText, htmlText, attachments);
 
-    Assert.assertEquals("wrong plain text", "plain text", MailUtility.getPlainText(message));
-
-    Part[] bodyParts = MailUtility.getBodyParts(message);
-    Assert.assertEquals("body parts size is wrong", 2, bodyParts.length);
-
-    Part[] attachmentParts = MailUtility.getAttachmentParts(message);
-    Assert.assertEquals("attachments parts size is wrong", 3, attachmentParts.length);
-
-    Part plainTextPart = MailUtility.getPlainTextPart(bodyParts);
-    Assert.assertNotNull("no plain text part found", plainTextPart);
-    Assert.assertTrue("plain text part content is not string", plainTextPart.getContent() instanceof String);
-    Assert.assertEquals("wrong plain text", "plain text", (String) plainTextPart.getContent());
-
-    Part htmlPart = MailUtility.getHtmlPart(bodyParts);
-    Assert.assertNotNull("no html part found", htmlPart);
-    Assert.assertTrue("html part content is not string", htmlPart.getContent() instanceof String);
-    Assert.assertEquals("wrong html text", "<html><body><p>plain text</p></html>", (String) htmlPart.getContent());
+    verifyPlainTextAndHtml(message, plainText, htmlText);
 
     // validate that the method call does not throw an exception
     MailUtility.collectMailParts(null, null, null, null);
@@ -138,6 +128,26 @@ public class MailUtilityTest {
     Assert.assertEquals("body parts size is wrong", 2, bodyCollector.size());
     Assert.assertEquals("attachments parts size is wrong", 3, attachmentCollector.size());
     Assert.assertEquals("inline attachments parts size is wrong", 0, inlineAttachmentCollector.size());
+  }
+
+  private void verifyPlainTextAndHtml(MimeMessage message, String plainText, String htmlText) throws ProcessingException, IOException, MessagingException {
+    Assert.assertEquals("wrong plain text", plainText, MailUtility.getPlainText(message));
+
+    Part[] bodyParts = MailUtility.getBodyParts(message);
+    Assert.assertEquals("body parts size is wrong", 2, bodyParts.length);
+
+    Part[] attachmentParts = MailUtility.getAttachmentParts(message);
+    Assert.assertEquals("attachments parts size is wrong", 3, attachmentParts.length);
+
+    Part plainTextPart = MailUtility.getPlainTextPart(bodyParts);
+    Assert.assertNotNull("no plain text part found", plainTextPart);
+    Assert.assertTrue("plain text part content is not string", plainTextPart.getContent() instanceof String);
+    Assert.assertEquals("wrong plain text", plainText, (String) plainTextPart.getContent());
+
+    Part htmlPart = MailUtility.getHtmlPart(bodyParts);
+    Assert.assertNotNull("no html part found", htmlPart);
+    Assert.assertTrue("html part content is not string", htmlPart.getContent() instanceof String);
+    Assert.assertEquals("wrong html text", htmlText, (String) htmlPart.getContent());
   }
 
   @Test
@@ -169,16 +179,85 @@ public class MailUtilityTest {
 
   @Test
   public void testAddAttachmentsToMimeMessage() throws ProcessingException, IOException, MessagingException {
+    // create html mime message without attachments
     MimeMessage message = MailUtility.createMimeMessage("plain text", "<html><body><p>plain text</p></html>", null);
-    Assert.assertEquals("attachments parts size is wrong", 0, MailUtility.getAttachmentParts(message).length);
+    verifyAttachments(message /* no attachments*/);
 
+    // add 3 attachments to mime message
     final byte[] sampleData = new byte[]{0x0, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF};
     List<File> attachments = new ArrayList<File>();
-
     attachments.add(IOUtility.createTempFile("sample1.dat", sampleData));
     attachments.add(IOUtility.createTempFile("sample2.dat", sampleData));
-    attachments.add(IOUtility.createTempFile("sample3.dat", sampleData));
+    attachments.add(IOUtility.createTempFile("sample3_öüä.dat", sampleData));
     MailUtility.addAttachmentsToMimeMessage(message, attachments);
-    Assert.assertEquals("attachments parts size is wrong", 3, MailUtility.getAttachmentParts(message).length);
+
+    // verify added attachments in java instance
+    verifyAttachments(message, "sample1.dat", "sample2.dat", MimeUtility.encodeText("sample3_öüä.dat"));
+
+    // store and recreate mime message (byte[])
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    message.writeTo(bos);
+    message = new MimeMessage(null, new ByteArrayInputStream(bos.toByteArray()));
+
+    // verify new instance
+    verifyAttachments(message, "sample1.dat", "sample2.dat", MimeUtility.encodeText("sample3_öüä.dat"));
+  }
+
+  @Test
+  public void testCreateMimeMessageFromWordArchiveDirectory() throws ProcessingException, IOException, MessagingException {
+    // create temp files for 3 attachments
+    final byte[] sampleData = new byte[]{0x0, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF};
+    List<File> attachments = new ArrayList<File>();
+    attachments.add(IOUtility.createTempFile("sample1.dat", sampleData));
+    attachments.add(IOUtility.createTempFile("sample2.dat", sampleData));
+    attachments.add(IOUtility.createTempFile("sample3_öüä.dat", sampleData));
+
+    // create files for word archive with plain text and html files (no additional attachments in archive)
+    final String plainText = "plain text";
+    final String htmlText = "<html><body><p>plain text</p></body></html>";
+    final String archiveSimpleName = "myarchive";
+    File archiveDir = IOUtility.createTempDirectory("word_archive");
+    File txtFile = new File(archiveDir, archiveSimpleName + ".txt");
+    File htmlFile = new File(archiveDir, archiveSimpleName + ".html");
+    IOUtility.writeContent(new FileOutputStream(txtFile), plainText.getBytes());
+    IOUtility.writeContent(new FileOutputStream(htmlFile), htmlText.getBytes());
+
+    // create word archive file (zip)
+    File archiveFile = IOUtility.createTempFile(archiveSimpleName + ".zip", null);
+    FileUtility.compressArchive(archiveDir, archiveFile);
+
+    // remove temp files
+    txtFile.delete();
+    htmlFile.delete();
+    archiveDir.delete();
+
+    // create mime message from word archive and verify message
+    MimeMessage message = MailUtility.createMimeMessageFromWordArchive(archiveFile, attachments.toArray(new File[attachments.size()]));
+    verifyPlainTextAndHtml(message, plainText, htmlText);
+    verifyAttachments(message, "sample1.dat", "sample2.dat", MimeUtility.encodeText("sample3_öüä.dat"));
+  }
+
+  /**
+   * Verifies that the mime message has the correct number of attachments with the correct filenames.
+   *
+   * @param message
+   *          Message to check attachments
+   * @param attachmentFilenames
+   *          Filenames of attachments (use {@link MimeUtility#encodeText(String)} in case the filename as non-supported
+   *          chars)
+   * @throws ProcessingException
+   * @throws MessagingException
+   */
+  protected void verifyAttachments(MimeMessage message, String... attachmentFilenames) throws ProcessingException, MessagingException {
+    Part[] attachmentParts = MailUtility.getAttachmentParts(message);
+    Assert.assertEquals("attachments parts size is wrong", attachmentFilenames.length, attachmentParts.length);
+    Set<String> attachmentFilenamesSet = new HashSet<String>();
+    for (Part part : attachmentParts) {
+      attachmentFilenamesSet.add(part.getFileName());
+    }
+    Assert.assertEquals("attachments filenames size is wrong", attachmentFilenames.length, attachmentFilenamesSet.size());
+    for (String attachmentFilename : attachmentFilenames) {
+      Assert.assertTrue("attachment filename " + attachmentFilename + " is missing", attachmentFilenamesSet.contains(attachmentFilename));
+    }
   }
 }
