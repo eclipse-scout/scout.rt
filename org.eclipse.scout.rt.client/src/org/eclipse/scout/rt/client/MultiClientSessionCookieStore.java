e816e7a8cc6ad45b42ff3f476f6ca52f71283750
Patrick Bänziger <patrick.baenziger@bsiag.com>
2014-12-15 10:53:24 +0100

    455184: Memory Leak: MultiClientSessionCookieStore hangs on to ClientSessions Convert to WeakHashMap. Add ReadWriteLock for better performance.
    Change-Id: I8ad441c8ca117142105e121c08dc052091292e42
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=455184
    Signed-off-by: Patrick Bänziger <patrick.baenziger@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/38341
    Tested-by: Hudson CI
    Reviewed-by: Judith Gull <jgu@bsiag.com>

--- org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/MultiClientSessionCookieStore.java
+++ org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/MultiClientSessionCookieStore.java
@@ -4,9 +4,11 @@ import java.net.CookieManager;
 import java.net.CookieStore;
 import java.net.HttpCookie;
 import java.net.URI;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.WeakHashMap;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * HTTP cookie store implementation that distinguishes between different {@link IClientSession} connecting concurrently
@@ -14,12 +16,16 @@ import java.util.Map;
  */
 public class MultiClientSessionCookieStore implements CookieStore {
 
-  private final Object m_cookieStoreLock = new Object();
+  private final ReadWriteLock m_cookieStoresLock = new ReentrantReadWriteLock();
+  /**
+   * Access to this map synchronized with the ReadWriteLock {@link #m_cookieStoresLock}.
+   */
   private final Map<IClientSession, CookieStore> m_cookieStores;
   private final CookieStore m_defaultCookieStore;
 
   public MultiClientSessionCookieStore() {
-    m_cookieStores = new HashMap<IClientSession, CookieStore>();
+    // Use a WeakHashMap to ensure that client sessions are not retained when no longer needed.
+    m_cookieStores = new WeakHashMap<IClientSession, CookieStore>();
     m_defaultCookieStore = new CookieManager().getCookieStore();
   }
 
@@ -54,23 +60,52 @@ public class MultiClientSessionCookieStore implements CookieStore {
   }
 
   private CookieStore getDelegate() {
-    IClientSession currentSession = ClientJob.getCurrentSession();
+    IClientSession currentSession = getClientSession();
     if (currentSession == null) {
       return m_defaultCookieStore;
     }
-    synchronized (m_cookieStoreLock) {
+
+    // Check cache with read lock first.
+    m_cookieStoresLock.readLock().lock();
+    try {
       CookieStore cookieStore = m_cookieStores.get(currentSession);
-      if (cookieStore == null) {
+      if (cookieStore != null) {
+        return cookieStore;
+      }
+    }
+    finally {
+      m_cookieStoresLock.readLock().unlock();
+    }
+    // No entry found - get write lock to create it
+    m_cookieStoresLock.writeLock().lock();
+    try {
+      // In the meantime, the cookie store might have been created already by another thread - check again.
+      CookieStore cookieStore = m_cookieStores.get(currentSession);
+      if (cookieStore != null) {
+        return cookieStore;
+      }
+      else {
         cookieStore = new CookieManager().getCookieStore();
         m_cookieStores.put(currentSession, cookieStore);
+        return cookieStore;
       }
-      return cookieStore;
+    }
+    finally {
+      m_cookieStoresLock.writeLock().unlock();
     }
   }
 
+  protected IClientSession getClientSession() {
+    return ClientJob.getCurrentSession();
+  }
+
   public void sessionStopped(IClientSession clientSession) {
-    synchronized (m_cookieStoreLock) {
+    m_cookieStoresLock.writeLock().lock();
+    try {
       m_cookieStores.remove(clientSession);
+    }
+    finally {
+      m_cookieStoresLock.writeLock().unlock();
     }
   }
 }
