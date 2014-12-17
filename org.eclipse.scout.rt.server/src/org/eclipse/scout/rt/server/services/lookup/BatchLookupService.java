3c8b25fe19a6c916114e72b1dabcc543958a47e3
Jeremie Bresson <jbr@bsiag.com>
2014-12-17 09:22:00 +0100

    Bug 447592: move BatchLookupService to the shared plugin
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=447592
    
    Change-Id: I2e1c8f0de66ab90d024ff475d5bc5f5cdb03d473
    Reviewed-on: https://git.eclipse.org/r/35108
    Tested-by: Hudson CI
    Reviewed-by: Patrick Bänziger <patrick.baenziger@bsiag.com>
    Reviewed-by: Jeremie Bresson <jbr@bsiag.com>

--- org.eclipse.scout.rt.server/src/org/eclipse/scout/rt/server/services/lookup/BatchLookupService.java
+++ org.eclipse.scout.rt.server/src/org/eclipse/scout/rt/server/services/lookup/BatchLookupService.java
@@ -10,66 +10,16 @@
  ******************************************************************************/
 package org.eclipse.scout.rt.server.services.lookup;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.scout.commons.annotations.Priority;
-import org.eclipse.scout.commons.exception.ProcessingException;
-import org.eclipse.scout.rt.shared.services.lookup.BatchLookupCall;
-import org.eclipse.scout.rt.shared.services.lookup.BatchLookupResultCache;
-import org.eclipse.scout.rt.shared.services.lookup.IBatchLookupService;
-import org.eclipse.scout.rt.shared.services.lookup.ILookupCall;
-import org.eclipse.scout.rt.shared.services.lookup.ILookupRow;
-import org.eclipse.scout.service.AbstractService;
 
+/**
+ * @deprecated Use {@link org.eclipse.scout.rt.shared.services.lookup.BatchLookupService} instead. Will be removed with
+ *             the N release.
+ */
+@Deprecated
 @Priority(-1)
-public class BatchLookupService extends AbstractService implements IBatchLookupService {
+public class BatchLookupService extends org.eclipse.scout.rt.shared.services.lookup.BatchLookupService {
 
   public BatchLookupService() {
   }
-
-  @Override
-  public List<List<ILookupRow<?>>> getBatchDataByKey(BatchLookupCall batch) throws ProcessingException {
-    List<ILookupCall<?>> calls = batch.getCallBatch();
-    List<List<ILookupRow<?>>> result = new ArrayList<List<ILookupRow<?>>>();
-    BatchLookupResultCache cache = new BatchLookupResultCache();
-    for (ILookupCall<?> call : calls) {
-      result.add(new ArrayList<ILookupRow<?>>(cache.getDataByKey(call)));
-    }
-    return result;
-  }
-
-  @Override
-  public List<List<ILookupRow<?>>> getBatchDataByText(BatchLookupCall batch) throws ProcessingException {
-    List<ILookupCall<?>> calls = batch.getCallBatch();
-    List<List<ILookupRow<?>>> result = new ArrayList<List<ILookupRow<?>>>();
-    BatchLookupResultCache cache = new BatchLookupResultCache();
-    for (ILookupCall<?> call : calls) {
-      result.add(new ArrayList<ILookupRow<?>>(cache.getDataByText(call)));
-    }
-    return result;
-  }
-
-  @Override
-  public List<List<ILookupRow<?>>> getBatchDataByAll(BatchLookupCall batch) throws ProcessingException {
-    List<ILookupCall<?>> calls = batch.getCallBatch();
-    List<List<ILookupRow<?>>> result = new ArrayList<List<ILookupRow<?>>>();
-    BatchLookupResultCache cache = new BatchLookupResultCache();
-    for (ILookupCall<?> call : calls) {
-      result.add(new ArrayList<ILookupRow<?>>(cache.getDataByAll(call)));
-    }
-    return result;
-  }
-
-  @Override
-  public List<List<ILookupRow<?>>> getBatchDataByRec(BatchLookupCall batch) throws ProcessingException {
-    List<ILookupCall<?>> calls = batch.getCallBatch();
-    List<List<ILookupRow<?>>> result = new ArrayList<List<ILookupRow<?>>>();
-    BatchLookupResultCache cache = new BatchLookupResultCache();
-    for (ILookupCall<?> call : calls) {
-      result.add(new ArrayList<ILookupRow<?>>(cache.getDataByRec(call)));
-    }
-    return result;
-  }
-
 }
