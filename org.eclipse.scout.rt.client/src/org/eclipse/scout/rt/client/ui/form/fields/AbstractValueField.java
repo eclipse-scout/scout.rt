09cb6358aeeaa67c5cd99e2ef9e783d03df40266
Andreas Hoegger <Andreas.Hoegger@bsiag.com>
2014-12-17 10:45:12 +0100

    443421: MessageBox shows with big delay
    Change-Id: Ie6dc9f2467a60221539a116ac9f137ca3fc8c552
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=443421

--- org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/form/fields/AbstractValueField.java
+++ org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/form/fields/AbstractValueField.java
@@ -38,7 +38,6 @@ import org.eclipse.scout.commons.holders.IHolder;
 import org.eclipse.scout.commons.logger.IScoutLogger;
 import org.eclipse.scout.commons.logger.ScoutLogManager;
 import org.eclipse.scout.commons.xmlparser.SimpleXmlElement;
-import org.eclipse.scout.rt.client.ClientSyncJob;
 import org.eclipse.scout.rt.client.extension.ui.action.tree.MoveActionNodesHandler;
 import org.eclipse.scout.rt.client.extension.ui.form.fields.AbstractValueFieldExtension;
 import org.eclipse.scout.rt.client.extension.ui.form.fields.IValueFieldExtension;
@@ -330,23 +329,6 @@ public abstract class AbstractValueField<VALUE> extends AbstractFormField implem
   @Override
   @SuppressWarnings("unchecked")
   public VALUE getValue() {
-    if (isValueValidating() && ClientSyncJob.isSyncClientJob()) {
-      throw new IllegalStateException("The value of " + getClass().getSimpleName() + " can not be accessed while the value is beeing validated");
-    }
-    else {
-      //caller from outside thread (ui)
-      //wait at most 10 seconds
-      int i = 0;
-      while (isValueValidating() && i < 100) {
-        try {
-          Thread.sleep(100);
-        }
-        catch (InterruptedException e) {
-          //nop
-        }
-        i++;
-      }
-    }
     return (VALUE) propertySupport.getProperty(PROP_VALUE);
   }
 
