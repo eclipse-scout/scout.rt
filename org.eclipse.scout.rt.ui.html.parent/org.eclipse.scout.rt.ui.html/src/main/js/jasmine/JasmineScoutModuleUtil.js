396b9d0fa86bd1dd6f716ad3982921f52611145c
Beat Schwarzentrub <bsh@bsiag.com>
2015-01-09 12:54:36 +0100

    HtmlUi: Add 'sourceURL' to make dynamic code visible in JS debuggers

--- org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/jasmine/JasmineScoutModuleUtil.js
+++ org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/jasmine/JasmineScoutModuleUtil.js
@@ -1,16 +1,21 @@
-// Load a module.js, replace all __include tags, and add it as a dynamic script tag to the head section
-function createDynamicScript(modulePath) {
-  var moduleText=loadAndResolveScriptContent(modulePath);
+/**
+ * Loads a module.js, replace all __include tags, and eval() the dynamic code
+ */
+function loadDynamicScript(modulePath) {
+  var moduleText = _loadAndResolveScriptContent(modulePath);
 
-  var script=document.createElement('script');
-  script.async=false;
-  script.setAttribute('type', 'text/javascript');
-  script.innerHTML = moduleText;
-  document.head.appendChild(script);
+  // Add sourceURL marker to make the the dynamic code show up in the browser's dev tools debugger
+  var matches = /^(.*)\/(.*?)$/.exec(window.location.href);
+  if (matches !== null) {
+    var sourceUrl = matches[1] + '/(' + (modulePath.substring(modulePath.lastIndexOf('/') + 1) + ')');
+    moduleText += '\n//# sourceURL=' + sourceUrl;
+  }
+
+  eval(moduleText);
 }
 
-function loadAndResolveScriptContent(path) {
-  var content='';
+function _loadAndResolveScriptContent(path) {
+  var content = '';
   $.ajax({
     async: false,
     type: 'GET',
@@ -25,20 +30,74 @@ function loadAndResolveScriptContent(path) {
   });
   //replace includes
   content = content.replace(
-      /(?:\/\/\s*@|__)include\s*\(\s*(?:\"([^\"]+)\"|'([^']+)')\s*\)(?:;)?/g,
-      function(match, p1, p2, offset, string) {
-        var includePrefix=path.replace(/^(.*\/)[^\/]*$/,'$1');
-        var includePath=includePrefix+(p1 ? p1 : p2);
-        var stars=includePath.replace(/./g,'*');
-        return ''+
-        '/***********'+stars+'***********\n'+
-        ' ********** '+includePath+' **********\n'+
-        ' ***********'+stars+'***********/\n'+
-        loadAndResolveScriptContent(includePath);
-      }
+    /(?:\/\/\s*@|__)include\s*\(\s*(?:\"([^\"]+)\"|'([^']+)')\s*\)(?:;)?/g,
+    function(match, p1, p2, offset, string) {
+      var includePrefix = path.replace(/^(.*\/)[^\/]*$/, '$1');
+      var includePath = includePrefix + (p1 ? p1 : p2);
+      return _insertLineNumbers(includePath, _loadAndResolveScriptContent(includePath));
+    }
   );
+
   return content;
 }
 
+// Copied from ScriptFileBuilder.java
+function _insertLineNumbers(filename, text) {
+  if (typeof text === 'undefined' || text === null) {
+    return text;
+  }
+  var index = filename.lastIndexOf('/');
+  if (index >= 0) {
+    filename = filename.substring(index + 1);
+  }
+  index = filename.lastIndexOf('.');
+  if (index >= 0) {
+    filename = filename.substring(0, index);
+  }
+  var lineNo = 1;
+  var insideBlockComment = false;
+  var buf = '';
+  var lines = text.split('\n');
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i];
+    buf += (insideBlockComment ? '//' : '/*');
+    buf += filename + ':';
+    buf += lineNo;
+    for (var j = (lineNo + '').length + 1; j < (lines.length + '').length; j++) {
+      buf += ' ';
+    }
+    buf += (insideBlockComment ? '//' : '*/') + ' ';
+    buf += line + '\n';
+    if (_lineIsBeginOfMultilineBlockComment(line, insideBlockComment)) {
+      // also if line is endMLBC AND beginMLBC
+      insideBlockComment = true;
+    }
+    else if (_lineIsEndOfMultilineBlockComment(line, insideBlockComment)) {
+      insideBlockComment = false;
+    }
+    lineNo++;
+  }
+  return buf;
+}
 
+// Copied from ScriptFileBuilder.java
+function _lineIsBeginOfMultilineBlockComment(line, insideBlockComment) {
+  if (insideBlockComment) {
+    return false;
+  }
+  var a = line.lastIndexOf("/*");
+  var b = line.lastIndexOf("*/");
+  var c = line.lastIndexOf("/*/");
+  return (a >= 0 && (b < 0 || b < a || (c == a)));
+}
 
+// Copied from ScriptFileBuilder.java
+function _lineIsEndOfMultilineBlockComment(line, insideBlockComment) {
+  if (!insideBlockComment) {
+    return false;
+  }
+  var a = line.indexOf("/*");
+  var b = line.indexOf("*/");
+  var c = line.lastIndexOf("/*/");
+  return (b >= 0 && (a < 0 || a < b || (c == a)));
+}
