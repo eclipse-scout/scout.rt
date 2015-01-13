e5f68d23e908258a7e09a660fc8402e9d6b9d7a7
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2015-01-13 11:17:53 +0100

    Added ISearchOutline
    The intention of the search outline is to provide a search over several
    table pages.
    The AbstractSearchOutline provides a frame, the search itself has to be
    implemented by the project.
    In order to use it add the SearchOutline to the desktop using
    getConfiguredOutlines (don't create an outlineViewButton).

---
+++ org.eclipse.scout.rt.client/src/org/eclipse/scout/rt/client/ui/desktop/outline/AbstractSearchOutline.java
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * Copyright (c) 2015 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.client.ui.desktop.outline;
+
+import org.eclipse.scout.commons.exception.ProcessingException;
+import org.eclipse.scout.rt.client.ui.basic.tree.AbstractTree;
+import org.eclipse.scout.rt.client.ui.basic.tree.ITreeUIFacade;
+import org.eclipse.scout.rt.shared.TEXTS;
+import org.eclipse.scout.rt.shared.services.common.exceptionhandler.IExceptionHandlerService;
+import org.eclipse.scout.service.SERVICES;
+
+public class AbstractSearchOutline extends AbstractOutline implements ISearchOutline {
+
+  @Override
+  protected String getConfiguredTitle() {
+    return TEXTS.get("Search");
+  }
+
+  @Override
+  protected String getConfiguredIconId() {
+    return "\\f002"; //Icons.Search
+  }
+
+  @Override
+  public void search() throws ProcessingException {
+    execSearch(getSearchQuery());
+  }
+
+  protected void execSearch(String query) throws ProcessingException {
+
+  }
+
+  @Override
+  public void setSearchQuery(String searchQuery) {
+    propertySupport.setPropertyString(PROP_SEARCH_QUERY, searchQuery);
+  }
+
+  @Override
+  public String getSearchQuery() {
+    return propertySupport.getPropertyString(PROP_SEARCH_QUERY);
+  }
+
+  @Override
+  public void setSearchStatus(String searchStatus) {
+    propertySupport.setPropertyString(PROP_SEARCH_STATUS, searchStatus);
+  }
+
+  @Override
+  public String getSearchStatus() {
+    return propertySupport.getPropertyString(PROP_SEARCH_STATUS);
+  }
+
+  @Override
+  protected ITreeUIFacade createUIFacade() {
+    return new P_UIFacade();
+  }
+
+  @Override
+  public ISearchOutlineUiFacade getUIFacade() {
+    return (ISearchOutlineUiFacade) super.getUIFacade();
+  }
+
+  protected class P_UIFacade extends AbstractTree.P_UIFacade implements ISearchOutlineUiFacade {
+
+    @Override
+    public void search(String query) {
+      try {
+        pushUIProcessor();
+        setProperty(PROP_SEARCH_QUERY, query);
+        AbstractSearchOutline.this.search();
+      }
+      catch (ProcessingException se) {
+        SERVICES.getService(IExceptionHandlerService.class).handleException(se);
+      }
+      finally {
+        popUIProcessor();
+      }
+    }
+
+  }
+}
