e126dc4a19bf516b3f828efc7b2b8df376848b73
Beat Schwarzentrub <bsh@bsiag.com>
2015-02-09 19:21:18 +0100

    HtmlUi: Fixed menu bar 'parent invalidation bug'
    And some code cleanup...

--- org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/menu/MenuBar.js
+++ org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/menu/MenuBar.js
@@ -3,12 +3,15 @@ scout.MenuBar = function($parent, position, orderFunc) {
   this.orderFunc = orderFunc;
   this.menuItems = [];
   this.$parent = $parent;
-  this.$container = $.makeDiv('menubar');
+
+  // Create a menubar container and add it to the parent, but don't show it yet. It will
+  // be shown automatically when items are added to the menubar, see _updateVisibility().
+  this.$container = $.makeDiv('menubar').hide();
   if (this.position === 'top') {
-    $parent.prepend(this.$container);
+    this.$parent.prepend(this.$container);
   } else {
     this.$container.addClass('bottom');
-    $parent.append(this.$container);
+    this.$parent.append(this.$container);
   }
 };
 
@@ -16,44 +19,37 @@ scout.MenuBar.prototype.remove = function() {
   this.menuItems.forEach(function(item) {
     item.remove();
   });
+  if (this.$container) {
+    this.$container.remove();
+  }
 };
 
 scout.MenuBar.prototype.updateItems = function(menuItems) {
-  var i, orderedMenuItems;
-
   // stop if menus are the same as before
   if (scout.arrays.equals(this.menuItems, menuItems)) {
-    this._updateVisibility();
     return;
   }
 
   // remove existing menu items
-  if (this.menuItems) {
-    for (i = 0; i < this.menuItems.length; i++) {
-      this.menuItems[i].remove();
-    }
-  }
+  this.menuItems.forEach(function(item) {
+    item.remove();
+  });
 
-  /* The orderFunc may add separators to the list of items, that's why we
-   * store the return value of orderFunc in this.menuItems and not the
-   * menuItems passed to the updateItems method. We must do this because
-   * otherwise we could not remove the added separator later.
-   */
-  orderedMenuItems = this.orderFunc(menuItems);
+  // The orderFunc may add separators to the list of items, that's why we
+  // store the return value of orderFunc in this.menuItems and not the
+  // menuItems passed to the updateItems method. We must do this because
+  // otherwise we could not remove the added separator later.
+  var orderedMenuItems = this.orderFunc(menuItems);
   this.menuItems = orderedMenuItems.left.concat(orderedMenuItems.right);
   this._renderMenuItems(orderedMenuItems.left, false);
   this._renderMenuItems(orderedMenuItems.right, true);
 
   // Fix for Firefox issue with float:right. In Firefox elements with float:right must
   // come first in the HTML order of elements. Otherwise a strange layout bug occurs.
-  this.$container.children('.menu-right').
-  detach().
-  prependTo(this.$container);
+  this.$container.children('.menu-right').detach().prependTo(this.$container);
 
   // The _first_ menu-right must have the 'last' class (reverse order because of float:right)
-  this.$container.children('.menu-right').
-  first().
-  addClass('last');
+  this.$container.children('.menu-right').first().addClass('last');
 
   // FIXME AWE: (menu) check if this code is still needed
   // if (this.lastItem && !this.lastItem.$container.hasClass('menu-right')) {
@@ -76,9 +72,12 @@ scout.MenuBar.prototype._updateVisibility = function() {
 };
 
 scout.MenuBar.prototype._renderMenuItems = function(menuItems, right) {
+  var tooltipPosition = (this.position === 'top' ? 'bottom' : 'top');
+
   menuItems.forEach(function(item) {
-    item.tooltipPosition = this.position === 'top' ? 'bottom' : 'top';
+    item.tooltipPosition = tooltipPosition;
     item.render(this.$container);
+    // TODO BSH Menu | Ask AW.E why this is needed:
     item.$container.removeClass('form-field');
     if (right) {
       item.$container.addClass('menu-right');
