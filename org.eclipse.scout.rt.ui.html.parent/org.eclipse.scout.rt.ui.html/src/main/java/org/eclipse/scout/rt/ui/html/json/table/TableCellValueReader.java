597dccb7d379dd5ea1cdc23d1fdb0e3383d71292
Beat Schwarzentrub <bsh@bsiag.com>
2015-02-24 10:04:12 +0100

    HtmlUi: Check TableCellValueReader arguments

--- org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/table/TableCellValueReader.java
+++ org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/table/TableCellValueReader.java
@@ -4,10 +4,17 @@ import org.eclipse.scout.rt.client.ui.basic.cell.ICell;
 import org.eclipse.scout.rt.ui.html.json.basic.cell.ICellValueReader;
 
 public class TableCellValueReader implements ICellValueReader {
-  private JsonColumn<?> m_jsonColumn;
-  private ICell m_cell;
+
+  private final JsonColumn<?> m_jsonColumn;
+  private final ICell m_cell;
 
   public TableCellValueReader(JsonColumn<?> jsonColumn, ICell cell) {
+    if (jsonColumn == null) {
+      throw new IllegalArgumentException("Argument 'jsonColumn' must not be null");
+    }
+    if (cell == null) {
+      throw new IllegalArgumentException("Argument 'cell' must not be null");
+    }
     m_jsonColumn = jsonColumn;
     m_cell = cell;
   }
@@ -15,12 +22,10 @@ public class TableCellValueReader implements ICellValueReader {
   @Override
   public Object read() {
     Object cellValue = m_jsonColumn.cellValueToJson(m_cell.getValue());
-
     // only return value if it is different than the text to prevent sending duplicate values
     if (cellValue != null && !String.valueOf(cellValue).equals(m_cell.getText())) {
       return cellValue;
     }
     return null;
-
   }
 }
