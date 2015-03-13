9198756812de04820d6749f0587fc4356c83e310
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2015-03-13 15:27:02 +0100

    HtmlUi: Fixed exception
    Keystrokes and menus must not use same reference

--- org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/table/TableKeyStrokeAdapter.js
+++ org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/table/TableKeyStrokeAdapter.js
@@ -8,14 +8,6 @@ scout.inherits(scout.TableKeyStrokeAdapter, scout.AbstractKeyStrokeAdapter);
 
 scout.TableKeyStrokeAdapter.prototype.installModelKeystrokes = function() {
   scout.TableKeyStrokeAdapter.parent.prototype.installModelKeystrokes.call(this);
-  if (this.keyStrokes.length > 0) {
-    this.keyStrokes = this.keyStrokes.concat(this._field.tableControls);
-  } else if (this._field.tableControls) {
-    this.keyStrokes = this._field.tableControls;
-  }
-  if (this.keyStrokes.length > 0) {
-    this.keyStrokes = this.keyStrokes.concat(this._field.menus);
-  } else if (this._field.menus) {
-    this.keyStrokes = this._field.menus;
-  }
+  this.keyStrokes = this.keyStrokes.concat(this._field.tableControls);
+  this.keyStrokes = this.keyStrokes.concat(this._field.menus);
 };
