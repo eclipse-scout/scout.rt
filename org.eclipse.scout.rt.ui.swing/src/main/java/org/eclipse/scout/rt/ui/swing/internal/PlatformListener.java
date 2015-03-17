b6270c9f9a7a3ee31b1e5ca4d6e43da441065830
Andi Bur <andi.bur@gmail.com>
2015-03-17 08:57:52 +0100

    use OBJ.one

--- org.eclipse.scout.rt.ui.swing/src/main/java/org/eclipse/scout/rt/ui/swing/internal/PlatformListener.java
+++ org.eclipse.scout.rt.ui.swing/src/main/java/org/eclipse/scout/rt/ui/swing/internal/PlatformListener.java
@@ -14,8 +14,8 @@ import org.eclipse.scout.rt.platform.IPlatform;
 import org.eclipse.scout.rt.platform.IPlatformListener;
 import org.eclipse.scout.rt.platform.PlatformEvent;
 import org.eclipse.scout.rt.platform.PlatformException;
-import org.eclipse.scout.rt.platform.cdi.BeanImplementor;
 import org.eclipse.scout.rt.platform.cdi.IBeanContext;
+import org.eclipse.scout.rt.platform.cdi.OBJ;
 import org.eclipse.scout.rt.platform.pluginxml.internal.PluginXmlParser;
 import org.eclipse.scout.rt.ui.swing.extension.FormFieldExtensions;
 import org.eclipse.scout.rt.ui.swing.extension.FormFieldsPluginXmlVisitor;
@@ -26,9 +26,7 @@ public class PlatformListener implements IPlatformListener {
   public void stateChanged(PlatformEvent event) throws PlatformException {
     if (event.getState() == IPlatform.State.BeanContextPrepared) {
       IBeanContext context = event.getSource().getBeanContext();
-      FormFieldExtensions formFieldExtensions = new FormFieldExtensions();
-      BeanImplementor<FormFieldExtensions> bean = new BeanImplementor<FormFieldExtensions>(FormFieldExtensions.class);
-      context.registerBean(bean, formFieldExtensions);
+      FormFieldExtensions formFieldExtensions = OBJ.one(FormFieldExtensions.class);
 
       // register form fields from plugin.xml
       PluginXmlParser.get().visit(new FormFieldsPluginXmlVisitor(context, formFieldExtensions));
