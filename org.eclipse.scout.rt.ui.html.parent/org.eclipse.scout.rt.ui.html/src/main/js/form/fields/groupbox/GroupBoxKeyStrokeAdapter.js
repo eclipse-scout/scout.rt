05ccdcce251e32556f839f4c627738514c3e6860
Nathan Burgherr <nathan.burgherr@bsiag.com>
2015-03-23 13:08:02 +0100

    HtmlUi: register key strokes on rootgroupbox. ->first registered wins
    Change-Id: If5580319e77c7c7d356b5dfafcc61ae95c95f470

--- org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/form/fields/groupbox/GroupBoxKeyStrokeAdapter.js
+++ org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/form/fields/groupbox/GroupBoxKeyStrokeAdapter.js
@@ -3,3 +3,19 @@ scout.GroupBoxKeyStrokeAdapter = function(field) {
 };
 
 scout.inherits(scout.GroupBoxKeyStrokeAdapter, scout.AbstractKeyStrokeAdapter);
+
+scout.GroupBoxKeyStrokeAdapter.prototype.registerKeyStroke = function(keyStroke) {
+  if (this._field.mainBox) {
+    scout.GroupBoxKeyStrokeAdapter.parent.prototype.registerKeyStroke.call(this, keyStroke);
+  } else {
+    this._fieldgetForm().rootGroupBox().keyStrokeAdapter.registerKeyStroke(keyStroke);
+  }
+};
+
+scout.GroupBoxKeyStrokeAdapter.prototype.unregisterKeyStroke = function(keyStroke) {
+  if (this._field.mainBox) {
+    scout.GroupBoxKeyStrokeAdapter.parent.prototype.unregisterKeyStroke.call(this, keyStroke);
+  } else {
+    this._field.getForm().rootGroupBox().keyStrokeAdapter.unregisterKeyStroke(keyStroke);
+  }
+};
