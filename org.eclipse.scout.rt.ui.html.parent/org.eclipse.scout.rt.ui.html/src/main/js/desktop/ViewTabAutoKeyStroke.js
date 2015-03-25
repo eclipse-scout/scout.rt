434386bb56ff8974d912f5ea9b59baffad10775d
Nathan Burgherr <nathan.burgherr@bsiag.com>
2015-03-25 14:52:10 +0100

    HtmlUi: changed ViewTabKeystrokes from 1 to 9
    Change-Id: Ic725e8090aa30294d04946d54ba9e3c0e4f3afbc

--- org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/desktop/ViewTabAutoKeyStroke.js
+++ org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/desktop/ViewTabAutoKeyStroke.js
@@ -13,18 +13,10 @@ scout.inherits(scout.ViewTabAutoKeyStroke, scout.KeyStroke);
  * @Override scout.KeyStroke
  */
 scout.ViewTabAutoKeyStroke.prototype.handle = function(event) {
-  if (this._tabs.length === 0 || (event.which !== 57 && event.which !== 48 && event.which - 48 > this._tabs.length)) {
+  if (this._tabs.length === 0 || (event.which !== 57 && event.which !== 49 && event.which - 49 > this._tabs.length)) {
     return;
   }
-  if (event.which === 57) {
-    //if 9 is hit go to last view tab
-    this._tabs[this._tabs.length - 1].$container.trigger('click');
-  } else if (event.which === 48) {
-    //if 0 is hit go to first view tab
-    this._tabs[0].$container.trigger('click');
-  } else {
-    this._tabs[event.which - 49].$container.trigger('click');
-  }
+  this._tabs[event.which - 49].$container.trigger('click');
   event.preventDefault();
 };
 
@@ -32,7 +24,7 @@ scout.ViewTabAutoKeyStroke.prototype.handle = function(event) {
  * @Override scout.KeyStroke
  */
 scout.ViewTabAutoKeyStroke.prototype.accept = function(event) {
-  if (this._enabled && event && event.which >= 48 && event.which <= 57 && // 0-9
+  if (this._enabled && event && event.which >= 49 && event.which <= 57 && // 1-9
     event.ctrlKey === this.ctrl && event.altKey === this.alt && event.shiftKey === this.shift) {
     return true;
   }
@@ -59,20 +51,10 @@ scout.ViewTabAutoKeyStroke.prototype._drawKeyBox = function($container) {
     return;
   }
   if (this._enabled && this._tabs) {
-    for (var i = 0; i < this._tabs.length; i++) {
-      var offsetLeft = 4,
-        firstField = true;
-      if (i === 0) {
-        scout.keyStrokeBox.drawSingleKeyBoxItem(offsetLeft, '0', this._tabs[i].$container, this.ctrl, this.alt, this.shift);
-        firstField = false;
-      }
-      if (i < 8) {
-        scout.keyStrokeBox.drawSingleKeyBoxItem(offsetLeft, i + 1, this._tabs[i].$container, this.ctrl, this.alt, this.shift);
-        firstField = false;
-      }
-      if (i + 1 === this._tabs.length) {
-        scout.keyStrokeBox.drawSingleKeyBoxItem(offsetLeft, 9, this._tabs[i].$container, this.ctrl, this.alt, this.shift);
-        firstField = false;
+    for (var i = 1; i < this._tabs.length+1; i++) {
+      var offsetLeft = 4;
+      if (i <= 9) {
+        scout.keyStrokeBox.drawSingleKeyBoxItem(offsetLeft, i, this._tabs[i - 1].$container, this.ctrl, this.alt, this.shift);
       }
     }
     this.keyBoxDrawed = true;
