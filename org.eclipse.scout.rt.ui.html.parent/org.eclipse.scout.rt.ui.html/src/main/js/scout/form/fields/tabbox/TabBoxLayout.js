e2d397f530c1de0be959f887c0221f87df258a97
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2015-04-07 18:06:44 +0200

    HtmlUi: Fixed tabbox layout
    - getVisibleSize now works well because the dialog isn't invisible
    anymore when getting layouted

--- org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/scout/form/fields/tabbox/TabBoxLayout.js
+++ org.eclipse.scout.rt.ui.html.parent/org.eclipse.scout.rt.ui.html/src/main/js/scout/form/fields/tabbox/TabBoxLayout.js
@@ -7,9 +7,9 @@ scout.inherits(scout.TabBoxLayout, scout.AbstractLayout);
 scout.TabBoxLayout.prototype.layout = function($container) {
   var containerSize, tabContentSize, htmlTabArea, tabAreaSize,
     htmlContainer = scout.HtmlComponent.get($container),
-    $tabContent = $container.children('.tab-content'),
+    $tabArea = this._tabBox._$tabArea,
+    $tabContent = this._tabBox._$tabContent,
     htmlTabContent = scout.HtmlComponent.get($tabContent),
-    $tabArea = $container.children('.tab-area'),
     tabAreaHeight = 0;
 
   containerSize = htmlContainer.getAvailableSize()
@@ -31,35 +31,22 @@ scout.TabBoxLayout.prototype.layout = function($container) {
  * Preferred size of the tab-box aligns every tab-item in a single line, so that each item is visible.
  */
 scout.TabBoxLayout.prototype.preferredLayoutSize = function($container) {
-  var htmlContainer = scout.HtmlComponent.get($container),
-    htmlTabContent = scout.HtmlComponent.get($container.children('.tab-content')),
-    tabButtonInsets = new scout.Insets(1, 20, 3, 20),
-    // FIXME AWE: (layout) cannot read insets dynamically because items are not rendered yet
-    // tabButtonInsets = scout.graphics.getInsets($container.children('.tab-area > button'), {includeMargin: true}),
+  var prefSize,
+    htmlContainer = scout.HtmlComponent.get($container),
+    $tabArea = this._tabBox._$tabArea,
+    $tabContent = this._tabBox._$tabContent,
+    htmlTabContent = scout.HtmlComponent.get($tabContent),
     tabAreaSize = new scout.Dimension(),
-    tabContentSize = new scout.Dimension(),
-    prefSize, textSize;
+    tabContentSize = new scout.Dimension();
 
-  // tab-area
-  this._tabBox.tabItems.forEach(function(tabItem) {
-    textSize = scout.graphics.measureString(tabItem.label, ['font-text-normal', 'make-bold']);
-    tabAreaSize.width += textSize.width + tabButtonInsets.left + tabButtonInsets.right;
-    tabAreaSize.height = 32;
-    // FIXME AWE: (layout) cannot read height dynamically because items are not rendered yet
-    // tabAreaSize.height = Math.max(tabAreaSize.height, textSize.height + tabButtonInsets.top + tabButtonInsets.bottom);
-  });
-  $.log.info('tabAreaSize='+tabAreaSize);
-
-  // tab-content
+  tabAreaSize = scout.graphics.getVisibleSize($tabArea, true);
   tabContentSize = htmlTabContent.getPreferredSize()
     .add(htmlContainer.getInsets())
     .add(htmlTabContent.getMargins());
 
   prefSize = new scout.Dimension(
-      Math.max(tabAreaSize.width, tabContentSize.width),
-      Math.max(tabAreaSize.height, tabContentSize.height));
-
-  $.log.trace('(TabBoxLayout#preferredLayoutSize)='+prefSize);
+    Math.max(tabAreaSize.width, tabContentSize.width),
+    tabContentSize.height + tabAreaSize.height);
 
   return prefSize;
 };
