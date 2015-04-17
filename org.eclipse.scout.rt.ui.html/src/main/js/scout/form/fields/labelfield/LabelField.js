9afb2f21dc7dd67f1ed8bb37c704a68d5ea3bc4f
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2015-04-17 18:18:10 +0200

    HtmlUi: Removed white-space: nowrap from form-field
    -> Text wrapping behavior differs from field to field
    - Label field now considers wrapText property

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/form/fields/labelfield/LabelField.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/form/fields/labelfield/LabelField.js
@@ -10,9 +10,19 @@ scout.LabelField.prototype._render = function($parent) {
   this.addStatus();
 };
 
+scout.LabelField.prototype._renderProperties = function() {
+  scout.LabelField.parent.prototype._renderProperties.call(this);
+  this._renderWrapText(this.wrapText);
+  //FIXME render selectable
+};
+
 /**
  * @override
  */
 scout.LabelField.prototype._renderDisplayText = function(displayText) {
   this.$field.html(displayText);
+};
+
+scout.LabelField.prototype._renderWrapText = function() {
+  this.$field.toggleClass('white-space-nowrap', !this.wrapText);
 };
