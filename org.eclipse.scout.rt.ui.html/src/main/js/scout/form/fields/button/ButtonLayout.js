c4426681825655fcb4a32bbc2e7909be55dd861a
Beat Schwarzentrub <bsh@bsiag.com>
2015-05-04 18:16:54 +0200

    HtmlUi: Fix ButtonLayout for non-process-buttons
    8px margin from Button.css has to be considered in calculation.

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/form/fields/button/ButtonLayout.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/form/fields/button/ButtonLayout.js
@@ -8,41 +8,32 @@ scout.ButtonLayout = function(button) {
 scout.inherits(scout.ButtonLayout, scout.AbstractLayout);
 
 scout.ButtonLayout.prototype.layout = function($container) {
-  // button has no children - nothing to do here
-  var $button = $container.children('button');
-  $button.css('width', '100%');
+  var htmlContainer = scout.HtmlComponent.get($container),
+    containerSize = htmlContainer.getSize(),
+    containerInsets = htmlContainer.getInsets({includeMargin: true}); // includeMargin=true because of 'margin-left' in Button.css
+
+  var buttonSize = containerSize.subtract(containerInsets);
+  scout.graphics.setSize(this.button.$field, buttonSize);
 };
 
 scout.ButtonLayout.prototype.preferredLayoutSize = function($container) {
-  var $button = $container.children('button'),
-    insets = scout.graphics.getInsets($button),
-    label,
-    labelSize = new scout.Dimension(0, 0),
-    iconSize = new scout.Dimension(0, 0),
-    contentSize,
-    gap = 0,
-    hasLabel = false,
-    hasIcon = false;
+  var htmlContainer = scout.HtmlComponent.get($container),
+    fieldSize = scout.graphics.getSize(this.button.$field),
+    containerInsets = htmlContainer.getInsets({includeMargin: true}), // includeMargin=true because of 'margin-left' in Button.css
+    iconSize = new scout.Dimension(0, 0);
 
-  if (this.button.label) {
-    label = scout.strings.removeAmpersand(this.button.label);
-    labelSize = scout.graphics.measureString(label);
-    hasLabel = true;
-  }
   if (this.button.iconId) {
     // FIXME AWE: (button) read size from image dynamically
     // maybe add event-handler when IMG is loaded and invalidate layout
     iconSize = new scout.Dimension(16, 16);
-    hasIcon = true;
+    if (this.button.label) {
+      iconSize.width += 8; // add gap between text and icon
+    }
   }
-  if (hasLabel && hasIcon) {
-    gap = 8;
-  }
-  contentSize = new scout.Dimension(
-      labelSize.width + gap + iconSize.width,
-      Math.max(labelSize.height, iconSize.height)
-  );
-  return new scout.Dimension(
-      contentSize.width + insets.left + insets.right,
-      contentSize.height + insets.top + insets.bottom);
+
+  var prefSize = fieldSize.add(containerInsets);
+  prefSize.width += iconSize.width;
+  prefSize.height = Math.max(prefSize.height, iconSize.height);
+
+  return prefSize;
 };
