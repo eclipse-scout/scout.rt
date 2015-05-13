d212d6efff8b0b9b91f8c8832821af341f8a8dc9
Ivan Motsch <ivan.motsch@bsiag.com>
2015-05-13 17:23:41 +0200

    try to fix download of files

--- org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/desktop/JsonDesktop.java
+++ org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/desktop/JsonDesktop.java
@@ -231,7 +231,7 @@ public class JsonDesktop<T extends IDesktop> extends AbstractJsonPropertyObserve
     String fileName = handler.getResource().getFilename();
     m_downloads.put(fileName, handler);
     String downloadUrl = BinaryResourceUrlUtility.createDynamicAdapterResourceUrl(this, fileName);
-    handleModelOpenUri(downloadUrl, TargetWindow.AUTO);
+    handleModelOpenUri(downloadUrl, TargetWindow.BLANK);//FIXME awe (from imo) AUTO causes in
   }
 
   @Override
