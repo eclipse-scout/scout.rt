72d22940196a4aa54a83febab25bb5027cda3a18
Nathan Burgherr <nathan.burgherr@bsiag.com>
2015-05-18 14:39:36 +0200

    HtmlUi: prevent scrolling and focusvalidation when app-link is clicked.
    Change-Id: I9343fecf87d375cd30700f5fb584765d313c765e

--- org.eclipse.scout.rt.svg.ui.html/src/main/js/scout/svg/SvgField.js
+++ org.eclipse.scout.rt.svg.ui.html/src/main/js/scout/svg/SvgField.js
@@ -22,13 +22,16 @@ scout.SvgField.prototype._renderSvgDocument = function() {
     return;
   }
   this.$field.html(this.svgDocument);
-  this.$field.find('.app-link').on('click', this._onAppLinkAction.bind(this));
+  this.$field.find('.app-link').on('click', this._onAppLinkAction.bind(this))
+  //prevent focusvalidation on other field.
+  .on('mousedown', function(event){event.preventDefault(); event.stopPropagation();};
 };
 
 scout.SvgField.prototype._onAppLinkAction = function(event) {
   var $target = $(event.target);
   var ref = $target.data('ref');
   this._sendAppLinkAction(ref);
+  event.preventDefault();
 };
 
 scout.SvgField.prototype._sendAppLinkAction = function(ref) {
--- org.eclipse.scout.rt.ui.html/src/main/js/scout/form/fields/htmlfield/HtmlField.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/form/fields/htmlfield/HtmlField.js
@@ -26,7 +26,9 @@ scout.HtmlField.prototype._renderDisplayText = function() {
     return;
   }
   this.$field.html(this.displayText);
-  this.$field.find('.app-link').on('click', this._onAppLinkAction.bind(this));
+  this.$field.find('.app-link').on('click', this._onAppLinkAction.bind(this))
+  //prevent focus validation on other field
+  .on('mousedown', function(event){event.preventDefault(); event.stopPropagation();});
 };
 
 scout.HtmlField.prototype._renderScrollBarsEnabled = function(scrollBarsEnabled) {
