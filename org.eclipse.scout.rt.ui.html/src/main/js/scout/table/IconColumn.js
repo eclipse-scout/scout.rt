f70de286427a1b5525919fb713753c82e8e013cd
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2015-05-22 12:33:22 +0200

    HtmlUi: Refactored iconColumn

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/table/IconColumn.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/table/IconColumn.js
@@ -4,23 +4,7 @@ scout.IconColumn = function() {
 scout.inherits(scout.IconColumn, scout.Column);
 
 scout.IconColumn.prototype.buildCell = function(row) {
-  var $cell = $(scout.IconColumn.parent.prototype.buildCell.call(this, row));
-  var value = this.table.cellValue(this, row);
-  this._renderValue($cell, value);
-  return $cell[0].outerHTML;
-};
-
-
-scout.IconColumn.prototype._renderValue = function($cell, value) {
-  var iconChar, $icon;
-  if(value){
-    if(scout.strings.startsWith(value, "font:")){
-      iconChar = value.substr(5);
-      $cell.appendSpan('font-icon', iconChar);
-    } else {
-      $icon = $.make('<img>')
-        .attr('src', scout.helpers.dynamicRsourceUrl(this, value)
-        .appendTo($cell));
-    }
-  }
+  var cell = this.table.cell(this, row);
+  cell.iconId = cell.value;
+  return scout.IconColumn.parent.prototype.buildCell.call(this, row);
 };
