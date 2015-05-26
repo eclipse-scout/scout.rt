e68939463fe3a3a9874825a03be514c00a8b2bb4
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2015-05-26 20:22:48 +0200

    HtmlUi: Removed unused / duplicate function

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/scrollbar/scrollbars.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/scrollbar/scrollbars.js
@@ -141,13 +141,5 @@ scout.scrollbars = {
       var $elem = $scrollParents[i];
       $elem.off('scroll', handler);
     }
-  },
-
-  nextScrollableUpwards: function($startElement, $context) {
-   return $startElement.parentsUntil($context).filter( function( index, element ) {
-     return $(element).data('scrollable')===true;
-   });
-
   }
-
 };
