c7148277da06b223bc0ca064bc41478f3ff7d99e
Daniel Wiehl <daniel.wiehl@bsiag.com>
2015-05-26 18:50:40 +0200

    Add JavaDoc to 'CreateImmediately' annotation

--- org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/CreateImmediately.java
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/CreateImmediately.java
@@ -6,10 +6,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Bean
+/**
+ * Indicates that an 'application-scoped' bean should be instantiated immediately upon platform startup. However, the
+ * bean must be declared as 'application-scoped', otherwise platform startup fails.
+ */
+@Bean // do not remove to enforce validation on platform startup.
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface CreateImmediately {
-
 }
