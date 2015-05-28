eb293de699f8ea785affbefdcd2f3ec44919b627
Andi Bur <andi.bur@gmail.com>
2015-05-28 16:18:58 +0200

    fixed race condition

--- org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/internal/BeanHierarchy.java
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/internal/BeanHierarchy.java
@@ -20,6 +20,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.eclipse.scout.commons.annotations.Order;
 import org.eclipse.scout.commons.annotations.Replace;
@@ -34,12 +36,14 @@ public class BeanHierarchy<T> {
   private final Set<IBean<T>> m_beans;
   private final Map<Object, List<IBean<T>>> m_singleQueryByScope;
   private final Map<Object, List<IBean<T>>> m_allQueryByScope;
+  private final ReadWriteLock m_queryCacheLock;
 
   public BeanHierarchy(Class<T> clazz) {
     m_clazz = clazz;
     m_beans = new HashSet<>();
     m_singleQueryByScope = new HashMap<>();
     m_allQueryByScope = new HashMap<>();
+    m_queryCacheLock = new ReentrantReadWriteLock();
   }
 
   public Class<T> getClazz() {
@@ -70,9 +74,7 @@ public class BeanHierarchy<T> {
    *         without {@link Replace} then C is not part of the result.
    */
   public List<IBean<T>> querySingle(IBeanScopeEvaluator evaluator) {
-    Object currentScope = getCurrentScope(evaluator);
-    validate(currentScope, evaluator);
-    return m_singleQueryByScope.get(currentScope);
+    return query(evaluator, true);
   }
 
   /**
@@ -83,9 +85,7 @@ public class BeanHierarchy<T> {
    *         without {@link Replace} then C is part of the result.
    */
   public List<IBean<T>> queryAll(IBeanScopeEvaluator evaluator) {
-    Object currentScope = getCurrentScope(evaluator);
-    validate(currentScope, evaluator);
-    return m_allQueryByScope.get(currentScope);
+    return query(evaluator, false);
   }
 
   protected Object getCurrentScope(IBeanScopeEvaluator evaluator) {
@@ -96,92 +96,122 @@ public class BeanHierarchy<T> {
   }
 
   protected void invalidate() {
-    m_singleQueryByScope.clear();
-    m_allQueryByScope.clear();
+    m_queryCacheLock.writeLock().lock();
+    try {
+      m_singleQueryByScope.clear();
+      m_allQueryByScope.clear();
+    }
+    finally {
+      m_queryCacheLock.writeLock().unlock();
+    }
   }
 
   @SuppressWarnings("unchecked")
-  protected void validate(Object scope, IBeanScopeEvaluator evaluator) {
-    List<IBean<T>> singleList = m_singleQueryByScope.get(scope);
-    List<IBean<T>> allList = m_allQueryByScope.get(scope);
+  protected List<IBean<T>> query(IBeanScopeEvaluator evaluator, boolean querySingle) {
+    Object scope = getCurrentScope(evaluator);
+
+    List<IBean<T>> singleList;
+    List<IBean<T>> allList;
+
+    m_queryCacheLock.readLock().lock();
+    try {
+      singleList = m_singleQueryByScope.get(scope);
+      allList = m_allQueryByScope.get(scope);
+    }
+    finally {
+      m_queryCacheLock.readLock().unlock();
+    }
 
     if (singleList == null || allList == null) {
-      List<IBean<T>> list = new ArrayList<>(m_beans);
-      //sort by Order ascending
-      Collections.sort(list, ORDER_COMPARATOR);
-      //remove duplicate classes
-      Class<?> lastSeen = null;
-      for (Iterator<IBean<T>> it = list.iterator(); it.hasNext();) {
-        IBean<T> bean = it.next();
-        if (bean.getBeanClazz() == lastSeen) {
-          it.remove();
+      m_queryCacheLock.writeLock().lock();
+      try {
+        List<IBean<T>> list = new ArrayList<>(m_beans);
+        //sort by Order ascending
+        Collections.sort(list, ORDER_COMPARATOR);
+        //remove duplicate classes
+        Class<?> lastSeen = null;
+        for (Iterator<IBean<T>> it = list.iterator(); it.hasNext();) {
+          IBean<T> bean = it.next();
+          if (bean.getBeanClazz() == lastSeen) {
+            it.remove();
+          }
+          lastSeen = bean.getBeanClazz();
         }
-        lastSeen = bean.getBeanClazz();
-      }
-      //filter beans not matching scope
-      if (evaluator != null) {
-        list = evaluator.filter(list, scope);
-      }
+        //filter beans not matching scope
+        if (evaluator != null) {
+          list = evaluator.filter(list, scope);
+        }
 
-      //manage replaced beans
-      HashMap<Class<?>, Class<?>> extendsMap = new HashMap<>();//key is replaced by value
-      for (IBean<T> bean : list) {
-        if (bean.getBeanAnnotation(Replace.class) != null) {
-          Class<?> superClazz = bean.getBeanClazz().getSuperclass();
-          if (superClazz != null && !superClazz.isInterface() && !Modifier.isAbstract(superClazz.getModifiers())) {
-            //only add if first to override, respects @Order annotation
-            if (!extendsMap.containsKey(superClazz)) {
-              extendsMap.put(superClazz, bean.getBeanClazz());
+        //manage replaced beans
+        HashMap<Class<?>, Class<?>> extendsMap = new HashMap<>();//key is replaced by value
+        for (IBean<T> bean : list) {
+          if (bean.getBeanAnnotation(Replace.class) != null) {
+            Class<?> superClazz = bean.getBeanClazz().getSuperclass();
+            if (superClazz != null && !superClazz.isInterface() && !Modifier.isAbstract(superClazz.getModifiers())) {
+              //only add if first to override, respects @Order annotation
+              if (!extendsMap.containsKey(superClazz)) {
+                extendsMap.put(superClazz, bean.getBeanClazz());
+              }
             }
           }
         }
-      }
-      //find most specific version of @Replaced class if this hierarchy is not based on an interface
-      Class<T> refClazz = m_clazz;
-      if (!refClazz.isInterface()) {
-        while (extendsMap.containsKey(refClazz)) {
-          refClazz = (Class<T>) extendsMap.get(refClazz);
-        }
-      }
-      //remove replaced beans
-      for (Iterator<IBean<T>> it = list.iterator(); it.hasNext();) {
-        if (extendsMap.containsKey(it.next().getBeanClazz())) {
-          it.remove();
-        }
-      }
-
-      m_allQueryByScope.put(scope, Collections.unmodifiableList(new ArrayList<IBean<T>>(list)));
-
-      //now retain only beans that are exactly of type refClazz, but only if refClass is not an interface
-      if (!refClazz.isInterface()) {
-        for (Iterator<IBean<T>> it = list.iterator(); it.hasNext();) {
-          IBean<T> bean = it.next();
-          if (bean.getBeanClazz().isInterface()) {
-            continue;
+        //find most specific version of @Replaced class if this hierarchy is not based on an interface
+        Class<T> refClazz = m_clazz;
+        if (!refClazz.isInterface()) {
+          while (extendsMap.containsKey(refClazz)) {
+            refClazz = (Class<T>) extendsMap.get(refClazz);
           }
-          if (bean.getBeanClazz() == refClazz) {
-            continue;
-          }
-          it.remove();
         }
-      }
-      //only retain lowest order and if lowest order is same for multiple beans, keep them all, provocating a mutliple instance exception on getBean()
-      if (list.size() > 1) {
-        Double lowestOrder = null;
+        //remove replaced beans
         for (Iterator<IBean<T>> it = list.iterator(); it.hasNext();) {
-          IBean<T> bean = it.next();
-          if (lowestOrder == null || orderOf(bean) == lowestOrder.doubleValue()) {
-            //keep it
-            if (lowestOrder == null) {
-              lowestOrder = orderOf(bean);
+          if (extendsMap.containsKey(it.next().getBeanClazz())) {
+            it.remove();
+          }
+        }
+
+        allList = Collections.unmodifiableList(new ArrayList<IBean<T>>(list));
+        m_allQueryByScope.put(scope, allList);
+
+        //now retain only beans that are exactly of type refClazz, but only if refClass is not an interface
+        if (!refClazz.isInterface()) {
+          for (Iterator<IBean<T>> it = list.iterator(); it.hasNext();) {
+            IBean<T> bean = it.next();
+            if (bean.getBeanClazz().isInterface()) {
+              continue;
             }
-            continue;
+            if (bean.getBeanClazz() == refClazz) {
+              continue;
+            }
+            it.remove();
           }
-          it.remove();
         }
+        //only retain lowest order and if lowest order is same for multiple beans, keep them all, provocating a mutliple instance exception on getBean()
+        if (list.size() > 1) {
+          Double lowestOrder = null;
+          for (Iterator<IBean<T>> it = list.iterator(); it.hasNext();) {
+            IBean<T> bean = it.next();
+            if (lowestOrder == null || orderOf(bean) == lowestOrder.doubleValue()) {
+              //keep it
+              if (lowestOrder == null) {
+                lowestOrder = orderOf(bean);
+              }
+              continue;
+            }
+            it.remove();
+          }
+        }
+        singleList = Collections.unmodifiableList(new ArrayList<IBean<T>>(list));
+        m_singleQueryByScope.put(scope, singleList);
       }
-      m_singleQueryByScope.put(scope, Collections.unmodifiableList(new ArrayList<IBean<T>>(list)));
+      finally {
+        m_queryCacheLock.writeLock().unlock();
+      }
     }
+
+    if (querySingle) {
+      return singleList;
+    }
+    return allList;
   }
 
   private static final Comparator<IBean<?>> ORDER_COMPARATOR = new Comparator<IBean<?>>() {
