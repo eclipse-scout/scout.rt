dbbd7eb550fedbf22d6ab013ca33d61c9d83cdb9
Andi Bur <andi.bur@gmail.com>
2015-06-10 10:33:16 +0200

    470202: Performance issues in CloneUtility
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=470202
    Change-Id: I286888d9e591c7c9a9e8cfab5928580cd831c00d
    (cherry picked from commit 7c68e9101a8c39dea184b056186d8b81065752a9)
    Reviewed-on: https://git.eclipse.org/r/57972
    Reviewed-by: Andi Bur <andi.bur@gmail.com>
    Tested-by: Andi Bur <andi.bur@gmail.com>

--- org.eclipse.scout.commons/src/main/java/org/eclipse/scout/commons/CloneUtility.java
+++ org.eclipse.scout.commons/src/main/java/org/eclipse/scout/commons/CloneUtility.java
@@ -18,10 +18,17 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.ObjectStreamClass;
 import java.io.OutputStream;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.scout.commons.logger.IScoutLogger;
+import org.eclipse.scout.commons.logger.ScoutLogManager;
+import org.eclipse.scout.commons.serialization.IObjectSerializer;
+import org.eclipse.scout.commons.serialization.SerializationUtility;
 
 public final class CloneUtility {
+
+  private static final IScoutLogger LOG = ScoutLogManager.getLogger(CloneUtility.class);
 
   private CloneUtility() {
   }
@@ -31,11 +38,42 @@ public final class CloneUtility {
    * copy of the object and all its references.
    */
   public static <T> T createDeepCopyBySerializing(T obj) throws Exception {
+    if (obj == null) {
+      return null;
+    }
+    try {
+      // 1. use serialization utility
+      IObjectSerializer serializer = SerializationUtility.createObjectSerializer();
+      byte[] serialData = serializer.serialize(obj);
+      @SuppressWarnings("unchecked")
+      Class<T> expectedType = (Class<T>) obj.getClass();
+      T copy = serializer.deserialize(serialData, expectedType);
+      return copy;
+    }
+    catch (ClassNotFoundException e) {
+      // 2. use fall back implementation if the object to be copied references classes that are not visible to the optimized class loader
+      if (LOG.isDebugEnabled()) {
+        LOG.debug(ClassNotFoundException.class.getSimpleName() + " occurred while creating a deep copy using " + SerializationUtility.class.getSimpleName() + ". Using fallback strategy.", e);
+      }
+      return fallBackCreateDeepCopyBySerializing(obj);
+    }
+  }
+
+  /**
+   * Fall back implementation used in cases the object to be deep copied references classes that are not visible to any
+   * class loader used by the {@link SerializationUtility} (typically OSGi bundle and framework class loaders).
+   * <p/>
+   * This implementation creates a mapping of any class name (except classes in any package staring with <em>java.</em>)
+   * to its class object during the serialization process. A class lookup during the deserialization process is trivial.
+   *
+   * @since 5.2
+   */
+  private static <T> T fallBackCreateDeepCopyBySerializing(T obj) throws Exception {
     ByteArrayOutputStream o = new ByteArrayOutputStream();
     DeepCopyObjectWriter oo = new DeepCopyObjectWriter(o);
     oo.writeObject(obj);
     oo.close();
-    DeepCopyObjectReader oi = new DeepCopyObjectReader(new ByteArrayInputStream(o.toByteArray()), oo.getUsedClassLoaders());
+    DeepCopyObjectReader oi = new DeepCopyObjectReader(new ByteArrayInputStream(o.toByteArray()), oo.getClassesByName());
     Object copy = oi.readObject();
     oi.close();
     @SuppressWarnings("unchecked")
@@ -44,7 +82,7 @@ public final class CloneUtility {
   }
 
   private static class DeepCopyObjectWriter extends ObjectOutputStream {
-    private Set<ClassLoader> m_usedClassLoaders = new HashSet<ClassLoader>();
+    private final Map<String, Class<?>> m_classesByName = new HashMap<>();
 
     public DeepCopyObjectWriter(OutputStream out) throws IOException {
       super(out);
@@ -53,42 +91,29 @@ public final class CloneUtility {
     @Override
     protected void annotateClass(Class<?> c) throws IOException {
       if (c != null) {
-        ClassLoader cl = c.getClassLoader();
-        if (cl != null) {
-          m_usedClassLoaders.add(cl);
-        }
+        m_classesByName.put(c.getName(), c);
       }
     }
 
-    public Set<ClassLoader> getUsedClassLoaders() {
-      return m_usedClassLoaders;
+    public Map<String, Class<?>> getClassesByName() {
+      return m_classesByName;
     }
   }
 
   private static class DeepCopyObjectReader extends ObjectInputStream {
-    private Set<ClassLoader> m_classLoaders;
+    private final Map<String, Class<?>> m_classesByName;
 
-    public DeepCopyObjectReader(InputStream in, Set<ClassLoader> classLoaders) throws IOException {
+    public DeepCopyObjectReader(InputStream in, Map<String, Class<?>> classesByName) throws IOException {
       super(in);
-      m_classLoaders = classLoaders;
+      m_classesByName = classesByName;
     }
 
     @Override
     protected Class<?> resolveClass(ObjectStreamClass desc) throws IOException, ClassNotFoundException {
-      try {
-        return super.resolveClass(desc);
+      Class<?> c = m_classesByName.get(desc.getName());
+      if (c != null) {
+        return c;
       }
-      catch (Exception e1) {
-        for (ClassLoader cl : m_classLoaders) {
-          try {
-            return Class.forName(desc.getName(), true, cl);
-          }
-          catch (Exception e2) {
-            //nop
-          }
-        }
-      }
-      //repeat original path
       return super.resolveClass(desc);
     }
   }
