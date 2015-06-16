c2ed4f30d998e5a99ddea786b30cfcbfe205759f
Beat Schwarzentrub <bsh@bsiag.com>
2015-06-16 13:19:03 +0200

    HtmlUi: Cleanup function order in Device.js
    - Re-order functions
    - Use supports* function to determine unselectableAttribute
    - Add supportsIframeSecurityAttribute()

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/util/Device.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/util/Device.js
@@ -8,7 +8,7 @@ scout.Device = function(userAgent) {
   this.features = {};
   this.device;
   this.browser = scout.Device.SupportedBrowsers.UNKNWON;
-  this.unselectableAttribute = ''; // see initUnselectableAttribute()
+
   this.parseUserAgent(this.userAgent);
 };
 
@@ -23,75 +23,20 @@ scout.Device.SupportedBrowsers = {
   INTERNET_EXPLORER: 'InternetExplorer'
 };
 
-scout.Device.prototype.isIos = function() {
-  return this.system === scout.Device.SYSTEM_IOS;
-};
-
-scout.Device.prototype.supportsTouch = function() {
-  // Implement when needed, see https://hacks.mozilla.org/2013/04/detecting-touch-its-the-why-not-the-how/
-};
-
-scout.Device.prototype.supportsCssAnimation = function() {
-  return this.supportsCssProperty('animation');
-};
-
-scout.Device.prototype.supportsCssUserSelect = function() {
-  return this.supportsCssProperty('userSelect');
-};
-
-scout.Device.prototype.supportsInternationalization = function() {
-  return window.Intl && typeof window.Intl === 'object';
-};
+scout.Device.SYSTEM_IOS = 'IOS';
 
 /**
- * Returns true if the device supports the download of resources in the same window as the single page app is running.
- * With "download" we mean: change <code>window.location.href</code> to the URL of the resource to download. Some browsers don't
- * support this behavior and require the resource to be opened in a new window with <code>window.open</code>.
+ * Called by index.html. Precalculates the value of "getUnselectableAttribute()" and
+ * stores it as a static property (to prevent many function calls inside loops, e.g.
+ * when generating table rows).
  */
-scout.Device.prototype.supportsDownloadInSameWindow = function() {
-  return scout.Device.SupportedBrowsers.FIREFOX !== this.browser;
-};
-
-scout.Device.prototype.hasPrettyScrollbars = function() {
-  return this.supportsFeature('prettyScrollbars', check.bind(this));
-
-  function check(property) {
-    // FIXME CGU check for android, osx, or just exclude windows?
-    return scout.Device.SYSTEM_IOS === this.system;
-  }
-};
-
-scout.Device.prototype.supportsCssProperty = function(property) {
-  return this.supportsFeature(property, check);
-
-  function check(property) {
-    var i;
-    if (document.body.style[property] !== undefined) {
-      return true;
-    }
-
-    property = property.charAt(0).toUpperCase() + property.slice(1);
-    for (i = 0; i < scout.Device.vendorPrefixes.length; i++) {
-      if (document.body.style[scout.Device.vendorPrefixes[i] + property] !== undefined) {
-        return true;
-      }
-    }
-
-    return false;
-  }
-};
-
-scout.Device.prototype.supportsFeature = function(property, checkFunc) {
-  if (this.features[property] === undefined) {
-    this.features[property] = checkFunc(property);
-  }
-  return this.features[property];
-};
-
 scout.Device.prototype.initUnselectableAttribute = function() {
-  if (!this.supportsCssUserSelect()) {
-    this.unselectableAttribute = ' unselectable="on"'; // workaround for IE 9
-  }
+  // Precalculate value and store in a simple property, to prevent many function calls inside loops (e.g. when generating table rows)
+  this.unselectableAttribute = this.getUnselectableAttribute();
+};
+
+scout.Device.prototype.isIos = function() {
+  return this.system === scout.Device.SYSTEM_IOS;
 };
 
 scout.Device.prototype.parseUserAgent = function(userAgent) {
@@ -123,7 +68,89 @@ scout.Device.prototype.parseUserAgent = function(userAgent) {
   }
 };
 
-scout.Device.SYSTEM_IOS = 'IOS';
+scout.Device.prototype.supportsFeature = function(property, checkFunc) {
+  if (this.features[property] === undefined) {
+    this.features[property] = checkFunc(property);
+  }
+  return this.features[property];
+};
 
-// singleton
+scout.Device.prototype.supportsTouch = function() {
+  // Implement when needed, see https://hacks.mozilla.org/2013/04/detecting-touch-its-the-why-not-the-how/
+};
+
+scout.Device.prototype.supportsCssAnimation = function() {
+  return this.supportsCssProperty('animation');
+};
+
+scout.Device.prototype.supportsCssUserSelect = function() {
+  return this.supportsCssProperty('userSelect');
+};
+
+scout.Device.prototype.supportsInternationalization = function() {
+  return window.Intl && typeof window.Intl === 'object';
+};
+
+/**
+ * Returns true if the device supports the download of resources in the same window as the single page app is running.
+ * With "download" we mean: change <code>window.location.href</code> to the URL of the resource to download. Some browsers don't
+ * support this behavior and require the resource to be opened in a new window with <code>window.open</code>.
+ */
+scout.Device.prototype.supportsDownloadInSameWindow = function() {
+  return scout.Device.SupportedBrowsers.FIREFOX !== this.browser;
+};
+
+scout.Device.prototype.hasPrettyScrollbars = function() {
+  return this.supportsFeature('_prettyScrollbars', check.bind(this));
+
+  function check(property) {
+    // FIXME CGU check for android, osx, or just exclude windows?
+    return scout.Device.SYSTEM_IOS === this.system;
+  }
+};
+
+scout.Device.prototype.supportsCssProperty = function(property) {
+  return this.supportsFeature(property, check);
+
+  function check(property) {
+    var i;
+    if (document.body.style[property] !== undefined) {
+      return true;
+    }
+
+    property = property.charAt(0).toUpperCase() + property.slice(1);
+    for (i = 0; i < scout.Device.vendorPrefixes.length; i++) {
+      if (document.body.style[scout.Device.vendorPrefixes[i] + property] !== undefined) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+};
+
+/**
+ * Returns '' for modern browsers, that support the 'user-select' CSS property.
+ * Returns ' unselectable="on"' for IE9.
+ * This string can be used to add to any HTML element as attribute.
+ */
+scout.Device.prototype.getUnselectableAttribute = function() {
+  return this.supportsFeature('_unselectableAttribute', function(property) {
+    if (this.supportsCssUserSelect()) {
+      return '';
+    }
+    // workaround for IE 9
+    return ' unselectable="on"';
+  }.bind(this));
+};
+
+scout.Device.prototype.supportsIframeSecurityAttribute = function() {
+  return this.supportsFeature('_iframeSecurityAttribute', function(property) {
+    var test = document.createElement('iframe');
+    return ('security' in test);
+  }.bind(this));
+};
+
+// ------------ Singleton ----------------
+
 scout.device = new scout.Device(navigator.userAgent);
