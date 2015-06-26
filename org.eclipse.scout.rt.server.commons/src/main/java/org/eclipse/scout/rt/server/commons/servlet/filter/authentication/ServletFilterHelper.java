060f52cf743ceb242a4318af246ae796186257aa
Ivan Motsch <ivan.motsch@bsiag.com>
2015-06-26 15:37:58 +0200

    simplify, verify and fix security filters

--- org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/servlet/filter/authentication/ServletFilterHelper.java
+++ org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/servlet/filter/authentication/ServletFilterHelper.java
@@ -5,6 +5,7 @@
 package org.eclipse.scout.rt.server.commons.servlet.filter.authentication;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.security.AccessController;
 import java.security.Principal;
 import java.security.PrivilegedActionException;
@@ -16,7 +17,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.scout.commons.Base64Utility;
 import org.eclipse.scout.rt.platform.Bean;
+import org.eclipse.scout.rt.platform.exception.PlatformException;
 
 /**
  * @since 5.0
@@ -25,6 +28,11 @@ import org.eclipse.scout.rt.platform.Bean;
 public class ServletFilterHelper {
   public static final String SESSION_ATTRIBUTE_FOR_PRINCIPAL = Principal.class.getName();
   public static final String SESSION_ATTRIBUTE_FOR_PRINCIPAL_TIMESTAMP = SESSION_ATTRIBUTE_FOR_PRINCIPAL + ".timestamp";
+
+  public static final String HTTP_HEADER_AUTHORIZATION = "Authorization";
+  public static final String HTTP_HEADER_AUTHORIZED = "Authorized";
+  public static final String HTTP_BASIC_AUTH_NAME = "Basic";
+  public static final String HTTP_BASIC_AUTH_CHARSET = "ISO-8859-1";
 
   public Subject createSubject(Principal principal) {
     // create subject if necessary
@@ -62,6 +70,31 @@ public class ServletFilterHelper {
       else {
         throw new ServletException(t);
       }
+    }
+  }
+
+  public String createBasicAuthRequest(String username, char[] password) {
+    try {
+      StringBuffer cred = new StringBuffer(username).append(":").append(password);
+      String encodedCred;
+      encodedCred = Base64Utility.encode(cred.toString().getBytes(HTTP_BASIC_AUTH_CHARSET));
+      return new StringBuffer(HTTP_BASIC_AUTH_NAME).append(" ").append(encodedCred).toString();
+    }
+    catch (UnsupportedEncodingException e) {
+      throw new PlatformException("charset " + HTTP_BASIC_AUTH_CHARSET, e);
+    }
+  }
+
+  public String[] parseBasicAuthRequest(HttpServletRequest req) {
+    try {
+      String h = req.getHeader(HTTP_HEADER_AUTHORIZATION);
+      if (h == null || !h.matches(HTTP_BASIC_AUTH_NAME + " .*")) {
+        return null;
+      }
+      return new String(Base64Utility.decode(h.substring(6)), HTTP_BASIC_AUTH_CHARSET).split(":", 2);
+    }
+    catch (UnsupportedEncodingException e) {
+      throw new PlatformException("charset " + HTTP_BASIC_AUTH_CHARSET, e);
     }
   }
 
