2042fc1df5a808df652300e6c4dd90947d2fa53b
Matthias Otterbach <Matthias.Otterbach@bsi-software.com>
2015-07-01 15:46:01 +0200

    HtmlUi: Testing pattern replacements within html fields.
    Change-Id: I8d6e0c5b03e39923560de079b434548acb8482a8

---
+++ org.eclipse.scout.rt.ui.html.test/src/test/java/org/eclipse/scout/rt/ui/html/json/form/fields/htmlfield/JsonHtmlFieldTest.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2010 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.ui.html.json.form.fields.htmlfield;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.scout.commons.html.HTML;
+import org.eclipse.scout.rt.client.ui.form.fields.htmlfield.AbstractHtmlField;
+import org.eclipse.scout.rt.client.ui.form.fields.htmlfield.IHtmlField;
+import org.eclipse.scout.rt.ui.html.json.form.fields.BaseFormFieldTest;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Testing: {@link AbstractHtmlField}
+ */
+public class JsonHtmlFieldTest extends BaseFormFieldTest {
+
+  private AbstractHtmlField m_model = new AbstractHtmlField() {
+  };
+
+  private JsonHtmlField m_htmlField = new JsonHtmlField<IHtmlField>(m_model, m_session, m_session.createUniqueIdFor(null), null);
+
+  @Before
+  public void setUp() {
+    m_htmlField.init();
+  }
+
+  @Test
+  public void testToImageByIconIdReplacement() throws JSONException {
+    m_model.setDisplayText(HTML.imgByIconId("test_icon").toEncodedHtml());
+    JSONObject json = m_htmlField.toJson();
+    // currently no icon provider services are available during html ui test, therefore any icon string should be replaced by null
+    assertEquals("<img src=\"null\">", json.get(IHtmlField.PROP_DISPLAY_TEXT));
+  }
+
+  @Test
+  public void testToImageByBinaryResourceReplacement() throws JSONException {
+    m_model.setDisplayText(HTML.imgByBinaryResource("test_resource").toEncodedHtml());
+    JSONObject json = m_htmlField.toJson();
+    assertEquals("<img src=\"dynamic/null/" + m_htmlField.getId() + "/test_resource\">", json.get(IHtmlField.PROP_DISPLAY_TEXT));
+  }
+
+}
