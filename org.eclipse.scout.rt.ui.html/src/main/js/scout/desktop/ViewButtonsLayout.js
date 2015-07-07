d4f9cbb09694a45277c125c2dddfa27ab47327e4
Andre Wegmueller <awe@bsiag.com>
2015-07-07 11:09:43 +0200

    HtmlUi: ViewButtonsLayout clean-up

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/desktop/ViewButtonsLayout.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/desktop/ViewButtonsLayout.js
@@ -5,6 +5,12 @@ scout.ViewButtonsLayout = function(htmlComp) {
 };
 scout.inherits(scout.ViewButtonsLayout, scout.AbstractLayout);
 
+/**
+ * Should be the same as in DesktopNavigation.css .view-button-tab > min-width.
+ */
+scout.ViewButtonsLayout.MIN_TAB_WIDTH = '50px';
+scout.ViewButtonsLayout.ANIMATION_DURATION = 300; // ms
+
 scout.ViewButtonsLayout.prototype.layout = function($container) {
   var $selectedTab, selectedTabWidth,
     fixedWidth = 0,
@@ -12,33 +18,24 @@ scout.ViewButtonsLayout.prototype.layout = function($container) {
   $container.children().each(function() {
     var $tab = $(this);
     var oldStyle = $tab.attr('style');
-    $tab
-//      .removeClass('view-button-fadeout view-button-fadein');
-      .removeAttr('style');
+    $tab.removeAttr('style');
     if ($tab.isSelected()) {
       $selectedTab = $tab;
     } else {
       fixedWidth += scout.graphics.getBounds($tab).width;
     }
-    if (oldStyle) { // restore of style used for animation
+    if (oldStyle) { // restore style  (required for animation)
       $tab.attr('style', oldStyle);
     }
   });
   if ($selectedTab) { // when no view-buttons exist
     selectedTabWidth = containerBounds.width - fixedWidth;
     if (this._$oldSelectedTab && !this._$oldSelectedTab.is($selectedTab)) {
-      this._$oldSelectedTab
-      //  FIXME AWE: remove color animation after review with C.RU (see comment out code in this class, also .css)
-      //        .addClass('view-button-fadeout')
-        .animate({width: '50px'}, 300);
-
-      $selectedTab
-//        .addClass('view-button-fadein')
-        .animate({width: selectedTabWidth}, 300);
+      this._$oldSelectedTab.animate({width: scout.ViewButtonsLayout.MIN_TAB_WIDTH}, scout.ViewButtonsLayout.ANIMATION_DURATION);
+      $selectedTab.animate({width: selectedTabWidth}, scout.ViewButtonsLayout.ANIMATION_DURATION);
     } else {
       scout.graphics.setSize($selectedTab, new scout.Dimension(selectedTabWidth, containerBounds.height));
     }
   }
-
   this._$oldSelectedTab = $selectedTab;
 };
