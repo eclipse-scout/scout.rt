30957c752450d6017cea52b07527835d54501aa3
Matthias Otterbach <Matthias.Otterbach@bsi-software.com>
2015-07-14 11:32:14 +0200

    HtmlUi: Disable browser autocompletion for text fields.
    Change-Id: I27651af8771d9062bcfd236e6d058b0a50dd63ed

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/form/fields/fields.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/form/fields/fields.js
@@ -3,6 +3,7 @@ scout.fields = {
   new$TextField: function() {
     return $('<input>')
       .attr('type', 'text')
+      .attr('autocomplete', 'false') /* use false instead of off, off is currently ignored in chrome, false should work with all major browsers*/
       .disableSpellcheck();
   },
 
