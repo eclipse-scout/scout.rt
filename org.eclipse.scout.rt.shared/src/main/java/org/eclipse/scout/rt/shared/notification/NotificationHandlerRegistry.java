0bf51efea760b53a6362112814027db9a5aaad2a
Judith Gull <jgu@bsiag.com>
2015-08-05 18:01:15 +0200

    generalize NotificationHandlerRegistry for reuse in coalescer
    Change-Id: I22260b0171c9afb5fac74311214271ea4a5318e9

--- org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/notification/NotificationHandlerRegistry.java
+++ org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/notification/NotificationHandlerRegistry.java
@@ -1,18 +1,10 @@
 package org.eclipse.scout.rt.shared.notification;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import javax.annotation.PostConstruct;
 
-import org.eclipse.scout.commons.TypeCastUtility;
 import org.eclipse.scout.commons.logger.IScoutLogger;
 import org.eclipse.scout.commons.logger.ScoutLogManager;
 import org.eclipse.scout.rt.platform.ApplicationScoped;
@@ -24,37 +16,11 @@ import org.eclipse.scout.rt.platform.BEANS;
 @ApplicationScoped
 public class NotificationHandlerRegistry {
   private static final IScoutLogger LOG = ScoutLogManager.getLogger(NotificationHandlerRegistry.class);
+  private final TypeParameterBeanRegistry<INotificationHandler> m_registry = new TypeParameterBeanRegistry<>();
 
-  private final Map<Class<? extends Serializable> /*notification class*/, List<INotificationHandler<? extends Serializable>>> m_notificationClassToHandler = new HashMap<>();
-  protected final ReadWriteLock m_classToHandlerLock = new ReentrantReadWriteLock();
-
-  private final Map<Class<? extends Serializable> /*notification class*/, List<INotificationHandler<? extends Serializable>>> m_cachedHandlers = new HashMap<>();
-  protected final ReadWriteLock m_cachedHandlerLock = new ReentrantReadWriteLock();
-
-  /**
-   * builds a linking of all handlers generic types to handlers. This is used to find the corresponding handler of a
-   * notification.
-   */
-  @SuppressWarnings("unchecked")
   @PostConstruct
   protected void buildHandlerLinking() {
-    List<INotificationHandler> notificationHandlers = BEANS.all(INotificationHandler.class);
-    m_classToHandlerLock.writeLock().lock();
-    try {
-
-      for (INotificationHandler<?> notificationHandler : notificationHandlers) {
-        Class notificationClass = TypeCastUtility.getGenericsParameterClass(notificationHandler.getClass(), INotificationHandler.class);
-        List<INotificationHandler<?>> handlerList = m_notificationClassToHandler.get(notificationClass);
-        if (handlerList == null) {
-          handlerList = new LinkedList<>();
-          m_notificationClassToHandler.put(notificationClass, handlerList);
-        }
-        handlerList.add(notificationHandler);
-      }
-    }
-    finally {
-      m_classToHandlerLock.writeLock().unlock();
-    }
+    m_registry.registerBeans(INotificationHandler.class, BEANS.all(INotificationHandler.class));
   }
 
   /**
@@ -65,7 +31,7 @@ public class NotificationHandlerRegistry {
    */
   @SuppressWarnings("unchecked")
   public void notifyHandlers(Serializable notification) {
-    List<INotificationHandler<? extends Serializable>> handlers = getHandlers(notification.getClass());
+    final List<INotificationHandler> handlers = m_registry.getBeans(notification.getClass());
     for (INotificationHandler handler : handlers) {
       try {
         handler.handleNotification(notification);
@@ -74,54 +40,6 @@ public class NotificationHandlerRegistry {
         LOG.error(String.format("Handler '%s' notification with notification '%s' failed.", handler, notification), e);
       }
     }
-  }
-
-  protected List<INotificationHandler<? extends Serializable>> getHandlers(Class<? extends Serializable> notificationClass) {
-    List<INotificationHandler<? extends Serializable>> handlers = getCachedHandlers(notificationClass);
-    if (handlers != null) {
-      return new ArrayList<INotificationHandler<? extends Serializable>>(handlers);
-    }
-    else {
-      handlers = findHandlers(notificationClass);
-      cacheHandlers(notificationClass, handlers);
-      return new ArrayList<INotificationHandler<? extends Serializable>>(handlers);
-    }
-  }
-
-  private List<INotificationHandler<? extends Serializable>> getCachedHandlers(Class<? extends Serializable> notificationClass) {
-    m_cachedHandlerLock.readLock().lock();
-    try {
-      return m_cachedHandlers.get(notificationClass);
-    }
-    finally {
-      m_cachedHandlerLock.readLock().unlock();
-    }
-  }
-
-  private void cacheHandlers(Class<? extends Serializable> notificationClass, List<INotificationHandler<? extends Serializable>> notificationHandlers) {
-    m_cachedHandlerLock.writeLock().lock();
-    try {
-      m_cachedHandlers.put(notificationClass, notificationHandlers);
-    }
-    finally {
-      m_cachedHandlerLock.writeLock().unlock();
-    }
-  }
-
-  protected List<INotificationHandler<? extends Serializable>> findHandlers(Class<? extends Serializable> notificationClass) {
-    List<INotificationHandler<? extends Serializable>> handlers = new LinkedList<>();
-    m_classToHandlerLock.readLock().lock();
-    try {
-      for (Entry<Class<? extends Serializable> /*notification class*/, List<INotificationHandler<? extends Serializable>>> e : m_notificationClassToHandler.entrySet()) {
-        if (e.getKey().isAssignableFrom(notificationClass)) {
-          handlers.addAll(e.getValue());
-        }
-      }
-    }
-    finally {
-      m_classToHandlerLock.readLock().unlock();
-    }
-    return handlers;
   }
 
 }
---
+++ org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/notification/TypeParameterBeanRegistry.java
@@ -0,0 +1,121 @@
+/*******************************************************************************
+ * Copyright (c) 2015 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.shared.notification;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import org.eclipse.scout.commons.TypeCastUtility;
+
+/**
+ * A registry for beans implementing an interface with one generic parameter type B&ltT&gt that allows querying beans
+ * with a given parameter type.
+ *
+ * @param B
+ *          bean type
+ */
+public class TypeParameterBeanRegistry<B> {
+
+  private final Map<Class<?>, List<B>> m_typeParamToBeans = new HashMap<>();
+  protected final ReadWriteLock m_typeParamToBeansLock = new ReentrantReadWriteLock();
+
+  private final Map<Class<?>, List<B>> m_typeParamSubclassToBeans = new HashMap<>();
+  protected final ReadWriteLock m_typeParamSubclassToBeansLock = new ReentrantReadWriteLock();
+
+  /**
+   * Register beans
+   *
+   * @param definingInterface
+   *          interface B&ltT&gt with a type parameter used in this registry
+   * @param beans
+   *          beans to register
+   */
+  public void registerBeans(Class<B> definingInterface, List<B> beans) {
+    m_typeParamToBeansLock.writeLock().lock();
+    try {
+
+      for (B b : beans) {
+        Class typeParam = TypeCastUtility.getGenericsParameterClass(b.getClass(), definingInterface);
+        List<B> handlerList = m_typeParamToBeans.get(typeParam);
+        if (handlerList == null) {
+          handlerList = new LinkedList<>();
+          m_typeParamToBeans.put(typeParam, handlerList);
+        }
+        handlerList.add(b);
+      }
+    }
+    finally {
+      m_typeParamToBeansLock.writeLock().unlock();
+    }
+  }
+
+  /**
+   * Returns all beans B&ltT&gt in this registry with the given generic parameter type &ltT&gt or a supertype of the
+   * given generic parameter type.
+   *
+   * @param typeParamClass
+   *          generic parameter type
+   */
+  public List<B> getBeans(Class<?> typeParamClass) {
+    List<B> beans = getCached(typeParamClass);
+    if (beans != null) {
+      return new ArrayList<>(beans);
+    }
+    else {
+      beans = findBeansForTypeParam(typeParamClass);
+      cacheTypeParamSubclass(typeParamClass, beans);
+      return new ArrayList<B>(beans);
+    }
+  }
+
+  private List<B> getCached(Class<?> clazz) {
+    m_typeParamSubclassToBeansLock.readLock().lock();
+    try {
+      return m_typeParamSubclassToBeans.get(clazz);
+    }
+    finally {
+      m_typeParamSubclassToBeansLock.readLock().unlock();
+    }
+  }
+
+  protected List<B> findBeansForTypeParam(Class<?> typeParam) {
+    List<B> handlers = new LinkedList<>();
+    m_typeParamToBeansLock.readLock().lock();
+    try {
+      for (Entry<Class<?>, List<B>> e : m_typeParamToBeans.entrySet()) {
+        if (e.getKey().isAssignableFrom(typeParam)) {
+          handlers.addAll(e.getValue());
+        }
+      }
+    }
+    finally {
+      m_typeParamToBeansLock.readLock().unlock();
+    }
+    return handlers;
+  }
+
+  private void cacheTypeParamSubclass(Class<?> clazz, List<B> beans) {
+    m_typeParamSubclassToBeansLock.writeLock().lock();
+    try {
+      m_typeParamSubclassToBeans.put(clazz, beans);
+    }
+    finally {
+      m_typeParamSubclassToBeansLock.writeLock().unlock();
+    }
+  }
+
+}
