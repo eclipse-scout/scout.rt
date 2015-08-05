9efebaf60999721371a4a38f13a0784549020048
Oli Schmid <oliver.schmid@bsiag.com>
2015-08-05 16:27:17 +0200

    Fixes issues with wrong cache values in swing table rendering.
    Change-Id: Ib1fee02d9f5daf54231cd28a2cbbc092706feb75
    Reviewed-on: https://git.eclipse.org/r/53237
    Tested-by: Hudson CI
    Reviewed-by: Oli Schmid <oliver.schmid@bsiag.com>
    (cherry picked from commit 0e48eae5b1f5eea853b4889e69ad2612c962795f)
    Reviewed-on: https://git.eclipse.org/r/53239

--- org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/ext/HtmlViewCache.java
+++ org.eclipse.scout.rt.ui.swing/src/org/eclipse/scout/rt/ui/swing/ext/HtmlViewCache.java
@@ -12,53 +12,61 @@ package org.eclipse.scout.rt.ui.swing.ext;
 
 import java.awt.Color;
 import java.util.HashMap;
-import java.util.zip.Adler32;
 
 import javax.swing.JLabel;
 import javax.swing.UIManager;
 import javax.swing.plaf.basic.BasicHTML;
 import javax.swing.text.View;
 
+import org.eclipse.scout.commons.Base64Utility;
+import org.eclipse.scout.commons.StringUtility;
+
 /**
- * Since html rendering is very expensive in swing and tables render views every time they are repainted, this cache
- * supports for re-using html views based on the crc of a string.
+ * Since html rendering is very expensive in swing and tables render views every
+ * time they are repainted, this cache supports for re-using html views based on
+ * the crc of a string.
  * <p>
  * To use on JLabels you must perform the following actions:
  * <ul>
- * <li>Disable automatic html on the field: label.putClientProperty("html.disable",true);</li>
- * <li>Whenever {@link JLabel#setText(String)} is called, call {@link #updateHtmlView(JLabel)} just after it.</li>
+ * <li>Disable automatic html on the field:
+ * label.putClientProperty("html.disable",true);</li>
+ * <li>Whenever {@link JLabel#setText(String)} is called, call
+ * {@link #updateHtmlView(JLabel)} just after it.</li>
  * </ul>
  */
 public class HtmlViewCache {
-  private HashMap<Object, View> m_viewMap;
+	private HashMap<Object, View> m_viewMap;
 
-  public HtmlViewCache() {
-    m_viewMap = new HashMap<Object, View>();
-  }
+	public HtmlViewCache() {
+		m_viewMap = new HashMap<Object, View>();
+	}
 
-  /**
-   * update html view using the cache
-   */
-  public void updateHtmlView(JLabel label, boolean customForeground) {
-    String text = label.getText();
-    View value = null;
-    if (BasicHTML.isHTMLString(text)) {
-      if (!customForeground) {
-        label.setForeground(label.isEnabled() ? UIManager.getDefaults().getColor("TextField.foreground") : UIManager.getDefaults().getColor("TextField.inactiveForeground"));
-      }
+	/**
+	 * update html view using the cache
+	 */
+	public void updateHtmlView(JLabel label, boolean customForeground) {
+		String text = label.getText();
+		View value = null;
+		if (BasicHTML.isHTMLString(text)) {
+			if (!customForeground) {
+				label.setForeground(label.isEnabled() ? UIManager.getDefaults()
+						.getColor("TextField.foreground") : UIManager
+						.getDefaults().getColor("TextField.inactiveForeground"));
+			}
 
-      Adler32 crc = new Adler32();
-      crc.update(text.getBytes());
-      Color fg = label.getForeground();
-      Object key = "" + crc.getValue() + "." + label.getFont() + "." + (fg != null ? fg.getRGB() : 0);
-      value = m_viewMap.get(key);
-      if (value == null) {
-        value = BasicHTML.createHTMLView(label, text);
-        if (value != null) {
-          m_viewMap.put(key, value);
-        }
-      }
-    }
-    label.putClientProperty(BasicHTML.propertyKey, value);
-  }
+			Color fg = label.getForeground();
+			// Key needs to be unique. To reduce memory usage the string is been
+			// compressed.
+			Object key = Base64Utility.encode(StringUtility.compress(text + "."
+					+ label.getFont() + "." + (fg != null ? fg.getRGB() : 0)));
+			value = m_viewMap.get(key);
+			if (value == null) {
+				value = BasicHTML.createHTMLView(label, text);
+				if (value != null) {
+					m_viewMap.put(key, value);
+				}
+			}
+		}
+		label.putClientProperty(BasicHTML.propertyKey, value);
+	}
 }
