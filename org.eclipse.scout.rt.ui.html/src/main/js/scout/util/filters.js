3b39f0ec945a20b82d5a859737b94c18107e7909
Daniel Wiehl <daniel.wiehl@bsi-software.com>
2015-08-13 14:49:01 +0200

    Follow HTML UI convention for static classes

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/util/filters.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/util/filters.js
@@ -1,43 +1,53 @@
-/**
- * Filters to be used in streaming API (Array.prototype.filter()).
- */
-scout.filters = function() {
-};
+scout.filters = {
 
-/**
- * Returns a function that always evaluates to 'true'.
- */
-scout.filters.returnTrue = function() {
-  return true;
-};
+  /**
+   * Ensures the given parameter is an array
+   */
+  ensure: function(array) {
+    if (!array) {
+      return [];
+    }
+    if (!Array.isArray(array)) {
+      return [array];
+    }
+    return array;
+  },
 
-/**
- * Returns a function that always evaluates to 'false'.
- */
-scout.filters.returnFalse = function() {
-  return false;
-};
+  /**
+   * Returns a function that always evaluates to 'true'.
+   */
+  returnTrue: function() {
+    return true;
+  },
 
-/**
- * Returns a filter to accept only elements which are located outside the given container, meaning not the container itself nor one of its children.
- *
- * @param DOM or jQuery container.
- */
-scout.filters.outsideFilter = function (container) {
-  container = container instanceof jQuery ? container[0] : container;
-  return function() {
-    return this !== container && !$.contains(container, this);
-  };
-};
+  /**
+   * Returns a function that always evaluates to 'false'.
+   */
+  returnFalse : function() {
+    return false;
+  },
 
-/**
- * Returns a filter to accept only elements which are not the given element.
- *
- * @param DOM or jQuery element.
- */
-scout.filters.notSameFilter = function(element) {
-  element = element instanceof jQuery ? element[0] : element;
-  return function() {
-    return this !== element;
-  };
+  /**
+   * Returns a filter to accept only elements which are located outside the given container, meaning not the container itself nor one of its children.
+   *
+   * @param DOM or jQuery container.
+   */
+  outsideFilter : function (container) {
+    container = container instanceof jQuery ? container[0] : container;
+    return function() {
+      return this !== container && !$.contains(container, this);
+    };
+  },
+
+  /**
+   * Returns a filter to accept only elements which are not the given element.
+   *
+   * @param DOM or jQuery element.
+   */
+  notSameFilter : function(element) {
+    element = element instanceof jQuery ? element[0] : element;
+    return function() {
+      return this !== element;
+    };
+  }
 };
