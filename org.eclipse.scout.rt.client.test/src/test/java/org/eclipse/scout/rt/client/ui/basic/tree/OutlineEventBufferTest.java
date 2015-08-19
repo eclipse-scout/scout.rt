8c97f08810528f76a0854814fc1b5d5ee809ddf2
Beat Schwarzentrub <bsh@bsiag.com>
2015-08-19 22:17:08 +0200

    Fix OutlineEventBufferTest missed in previous commit

--- org.eclipse.scout.rt.client.test/src/test/java/org/eclipse/scout/rt/client/ui/basic/tree/OutlineEventBufferTest.java
+++ org.eclipse.scout.rt.client.test/src/test/java/org/eclipse/scout/rt/client/ui/basic/tree/OutlineEventBufferTest.java
@@ -13,8 +13,11 @@ package org.eclipse.scout.rt.client.ui.basic.tree;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.scout.rt.client.ui.desktop.outline.IOutline;
 import org.eclipse.scout.rt.client.ui.desktop.outline.OutlineEvent;
@@ -28,10 +31,12 @@ import org.junit.Test;
 public class OutlineEventBufferTest {
 
   private OutlineEventBuffer m_testBuffer;
+  private Map<String, ITreeNode> m_mockNodes;
 
   @Before
   public void setup() {
     m_testBuffer = new OutlineEventBuffer();
+    m_mockNodes = new HashMap<>();
   }
 
   /**
@@ -39,12 +44,24 @@ public class OutlineEventBufferTest {
    */
   @Test
   public void testSelections() {
-    final OutlineEvent e1 = new OutlineEvent(mock(IOutline.class), OutlineEvent.TYPE_PAGE_CHANGED);
-    final OutlineEvent e2 = new OutlineEvent(mock(IOutline.class), OutlineEvent.TYPE_PAGE_CHANGED);
+    final OutlineEvent e1 = new OutlineEvent(mock(IOutline.class), OutlineEvent.TYPE_PAGE_CHANGED, mockNode("A"));
+    final OutlineEvent e2 = new OutlineEvent(mock(IOutline.class), OutlineEvent.TYPE_PAGE_CHANGED, mockNode("A"));
     m_testBuffer.add(e1);
     m_testBuffer.add(e2);
     final List<TreeEvent> coalesced = m_testBuffer.consumeAndCoalesceEvents();
     assertEquals(1, coalesced.size());
     assertSame(e1, coalesced.get(0));
+  }
+
+  private ITreeNode mockNode(String nodeId) {
+    ITreeNode node = m_mockNodes.get(nodeId);
+    if (node != null) {
+      return node;
+    }
+    // Create a new
+    node = mock(ITreeNode.class, "MockNode[" + nodeId + "]");
+    when(node.getNodeId()).thenReturn(nodeId);
+    m_mockNodes.put(nodeId, node);
+    return node;
   }
 }
