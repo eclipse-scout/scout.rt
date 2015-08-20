dcbbc3798d7ba73c06c7de98db4d5b21bb8c4552
André Wegmüller <awe@bsiag.com>
2015-08-20 09:44:07 +0200

    Deleted projects: - org.eclipse.scout.rt.ui.swing - org.eclipse.scout.rt.ui.swing.browser.swt.fragment - org.eclipse.scout.rt.ui.swing.test - org.eclipse.scout.rt.svg.ui.swing

--- org.eclipse.scout.rt.ui.swing/src/main/java/org/eclipse/scout/rt/ui/swing/form/fields/button/ISwingScoutRadioButton.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.scout.rt.ui.swing.form.fields.button;
-
-import org.eclipse.scout.rt.client.ui.form.fields.button.IRadioButton;
-
-public interface ISwingScoutRadioButton extends ISwingScoutButton<IRadioButton> {
-
-}
