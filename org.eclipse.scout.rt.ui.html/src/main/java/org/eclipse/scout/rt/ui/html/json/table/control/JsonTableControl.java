a1ac006fdacb0135139befce101520f1462c99d8
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2015-09-08 09:20:11 +0200

    Revert "HtmlUI: Property Form should be transmitted every time because otherwise there are errors when change form fields from faviorits and adapter is already registered in model but not on ui. Ticket->#163180"
    This reverts commit a6560123b726313100db69b8f2033d5f9aae2f28.

--- org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/table/control/JsonTableControl.java
+++ org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/table/control/JsonTableControl.java
@@ -42,6 +42,10 @@ public class JsonTableControl<TABLE_CONTROL extends ITableControl> extends JsonA
         return getModel().getForm();
       }
 
+      @Override
+      public boolean accept() {
+        return getModel().isSelected();
+      }
     });
     getJsonProperty(IAction.PROP_SELECTED).addSlaveProperty(getJsonProperty(ITableControl.PROP_FORM));
   }
