d833aace74a4e01cf9b56c66bdd1ab807f212033
Judith Gull <jgu@bsiag.com>
2015-09-09 11:09:58 +0200

    ignore CollationRulesPatchTest (to be replaced)
    Change-Id: I6abffe7bf4b32cc7536ce321046caed65cf97b2b

--- org.eclipse.scout.commons.test/src/test/java/org/eclipse/scout/commons/CollationRulesPatchTest.java
+++ org.eclipse.scout.commons.test/src/test/java/org/eclipse/scout/commons/CollationRulesPatchTest.java
@@ -20,13 +20,16 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Locale;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * JUnit tests for {@link CollationRulesPatch}
- * 
- * @since 3.8.2
+ *
+ * @since 3.8.2 <br>
+ *        TODO asa: Remove {@link CollationRulesPatch} and replace bean.
  */
+@Ignore
 public class CollationRulesPatchTest {
 
   @Test
