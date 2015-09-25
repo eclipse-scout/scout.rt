32fe234202c38efb951d59886a4d524b946cf40f
Beat Schwarzentrub <bsh@bsiag.com>
2015-09-25 17:49:23 +0200

    Add placeholder tile classes for all types, add demo for chart tile

--- com.bsiag.scout.rt.client/src/main/java/com/bsiag/scout/rt/client/ui/form/fields/tile/AbstractChartTile.java
+++ com.bsiag.scout.rt.client/src/main/java/com/bsiag/scout/rt/client/ui/form/fields/tile/AbstractChartTile.java
@@ -10,14 +10,18 @@
  ******************************************************************************/
 package com.bsiag.scout.rt.client.ui.form.fields.tile;
 
+import org.eclipse.scout.commons.annotations.ConfigProperty;
+import org.eclipse.scout.commons.annotations.Order;
+
 import com.bsiag.scout.rt.client.ui.form.fields.chartfield.AbstractChart;
 import com.bsiag.scout.rt.client.ui.form.fields.chartfield.AbstractChartField;
-import com.bsiag.scout.rt.client.ui.form.fields.chartfield.IChartField;
+import com.bsiag.scout.rt.client.ui.form.fields.tile.AbstractChartTile.ChartField;
+import com.bsiag.scout.rt.client.ui.form.fields.tile.AbstractChartTile.ChartField.Chart;
 
 /**
  * @since 5.2
  */
-public abstract class AbstractChartTile extends AbstractTile<IChartField<AbstractChartTile.ChartField.Chart>> {
+public abstract class AbstractChartTile extends AbstractTile<ChartField> {
 
   public AbstractChartTile() {
     this(true);
@@ -27,10 +31,43 @@ public abstract class AbstractChartTile extends AbstractTile<IChartField<Abstrac
     super(callInitializer);
   }
 
+  /**
+   * If set, this value is applied to the tile field's "autoColor" property.
+   */
+  @ConfigProperty(ConfigProperty.STRING)
+  @Order(70)
+  protected Boolean getConfiguredAutoColor() {
+    return null;
+  }
+
+  /**
+   * If set, this value is applied to the tile field's "chartType" property.
+   */
+  @ConfigProperty(ConfigProperty.INTEGER)
+  @Order(70)
+  protected Integer getConfiguredChartType() {
+    return null;
+  }
+
+  @Override
+  protected void initFileFieldInternal() {
+    super.initFileFieldInternal();
+
+    if (getConfiguredAutoColor() != null) {
+      getTileField().getChart().setAutoColor(getConfiguredAutoColor());
+    }
+    if (getConfiguredChartType() != null) {
+      getTileField().getChart().setChartType(getConfiguredChartType());
+    }
+  }
+
+  public Chart getChart() {
+    return getTileField().getChart();
+  }
+
   public class ChartField extends AbstractChartField<ChartField.Chart> {
 
     public class Chart extends AbstractChart {
-
     }
   }
 }
