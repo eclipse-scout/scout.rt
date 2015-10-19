1dda7b4b16388595c492cc683703af58ea4eb3f8
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2015-10-19 18:31:24 +0200

    Started with aggregate table control

---
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/basic/table/control/AggregateTableControl.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2010 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.client.ui.basic.table.control;
+
+import org.eclipse.scout.rt.shared.AbstractIcons;
+import org.eclipse.scout.rt.shared.TEXTS;
+
+public class AggregateTableControl extends AbstractTableControl implements IAggregateTableControl {
+
+  public AggregateTableControl() {
+    this(true);
+  }
+
+  public AggregateTableControl(boolean callInitializer) {
+    super(callInitializer);
+  }
+
+  @Override
+  protected void initConfig() {
+    super.initConfig();
+    setTooltipText(TEXTS.get("ui.Sum"));
+    setIconId(AbstractIcons.Sum);
+  }
+
+}
---
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/basic/table/control/IAggregateTableControl.java
@@ -0,0 +1,4 @@
+package org.eclipse.scout.rt.client.ui.basic.table.control;
+
+public interface IAggregateTableControl extends ITableControl {
+}
