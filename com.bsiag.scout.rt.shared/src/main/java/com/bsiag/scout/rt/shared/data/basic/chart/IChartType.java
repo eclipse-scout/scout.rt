14a4df9781bd8ec57eacc4893f214855e3a14aa2
Nathan Burgherr <nathan.burgherr@bsiag.com>
2015-10-27 14:45:56 +0100

    HtmlUI: Donut chart

--- com.bsiag.scout.rt.shared/src/main/java/com/bsiag/scout/rt/shared/data/basic/chart/IChartType.java
+++ com.bsiag.scout.rt.shared/src/main/java/com/bsiag/scout/rt/shared/data/basic/chart/IChartType.java
@@ -15,24 +15,25 @@ package com.bsiag.scout.rt.shared.data.basic.chart;
  */
 public interface IChartType {
 
-  final int PIE = 1;
-  final int LINE = 2;
-  final int BAR = 3;
-  final int BAR_HORIZONTAL = 4;
-  final int SCATTER = 5;
-  final int FULFILLMENT = 6;
-  final int SPEEDO = 7;
-  final int SALESFUNNEL = 8;
-  final int VENN = 9;
+	final int PIE = 1;
+	final int LINE = 2;
+	final int BAR = 3;
+	final int BAR_HORIZONTAL = 4;
+	final int SCATTER = 5;
+	final int FULFILLMENT = 6;
+	final int SPEEDO = 7;
+	final int SALESFUNNEL = 8;
+	final int VENN = 9;
+	final int DONUT = 10;
 
-  /**
-   * PossibleValues see SpeedoChartRenderer.js:<br/>
-   * scout.SpeedoChartRenderer.GREEN_AREA_POSITION_LEFT = 1;<br/>
-   * scout.SpeedoChartRenderer.GREEN_AREA_POSITION_CENTER = 2;<br/>
-   * scout.SpeedoChartRenderer.GREEN_AREA_POSITION_RIGHT = 3;<br/>
-   */
-  final String PROP_SPEEDO_CHART_GREEN_AREA_POSITION = "greenAreaPosition";
+	/**
+	 * PossibleValues see SpeedoChartRenderer.js:<br/>
+	 * scout.SpeedoChartRenderer.GREEN_AREA_POSITION_LEFT = 1;<br/>
+	 * scout.SpeedoChartRenderer.GREEN_AREA_POSITION_CENTER = 2;<br/>
+	 * scout.SpeedoChartRenderer.GREEN_AREA_POSITION_RIGHT = 3;<br/>
+	 */
+	final String PROP_SPEEDO_CHART_GREEN_AREA_POSITION = "greenAreaPosition";
 
-  final String PROP_SALESFUNNEL_PROPORTIONAL = "proportional";
-  final String PROP_SALESFUNNEL_CALC_CONVERSION_RATE = "calcConversionRate";
+	final String PROP_SALESFUNNEL_PROPORTIONAL = "proportional";
+	final String PROP_SALESFUNNEL_CALC_CONVERSION_RATE = "calcConversionRate";
 }
