9d39dfbd51336d49427b1749a1d5912380e23626
Daniel Wiehl <daniel.wiehl@bsi-software.com>
2015-10-31 12:20:54 +0100

    Minor modifications to JavaDoc of job manager

--- org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/job/IJobManager.java
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/job/IJobManager.java
@@ -26,61 +26,59 @@ import org.eclipse.scout.rt.platform.job.listener.IJobListener;
 import org.eclipse.scout.rt.platform.job.listener.JobEvent;
 
 /**
- * Job manager to execute jobs in parallel.
- * <p/>
- * This job manager supports sequential execution of jobs that belong to the same mutex object (mutual exclusion),
- * meaning that no more than one job will be active at any given time for that mutex object. Also, jobs can be put into
- * waiting state to wait for a {@link IBlockingCondition} to fall. Thereby, the condition can be used across multiple
- * jobs or any thread. See {@link IBlockingCondition} for more information.
+ * Job manager to run tasks in parallel.
+ * <p>
+ * This job manager supports sequential execution of tasks that belong to the same mutex object (mutual exclusion),
+ * meaning that no more than one task will be active at any given time for that mutex object.
  *
  * @since 5.1
  */
 public interface IJobManager {
 
   /**
-   * Runs the given job asynchronously on behalf of a worker thread at the next reasonable opportunity. The caller of
-   * this method continues to run in parallel. If the job is subject for mutual exclusion, the job only commence
+   * Runs the given task asynchronously on behalf of a worker thread at the next reasonable opportunity. The caller of
+   * this method continues to run in parallel. If the task is subject for mutual exclusion, the task only commence
    * execution once acquired the mutex.
-   * <p/>
-   * The {@link IFuture} returned allows to wait for the job to complete or to cancel the execution of the job. To
-   * immediately block waiting for the job to complete, you can use constructions of the form
+   * <p>
+   * The {@link IFuture} returned allows to wait for the task to complete or to cancel the execution of the task. To
+   * immediately block waiting for the task to complete, you can use constructions of the form
    * <code>result = Jobs.schedule(...).awaitDone();</code>.
    *
    * @param callable
    *          the callable to be executed
    * @param input
-   *          describes the job to be executed; must not be <code>null</code>.
-   * @return Future to wait for the job's completion or to cancel the job's execution.
+   *          describes the task to be executed; must not be <code>null</code>.
+   * @return Future to wait for the task's completion or to cancel the task's execution.
    */
   <RESULT> IFuture<RESULT> schedule(Callable<RESULT> callable, JobInput input);
 
   /**
-   * Runs the given job asynchronously on behalf of a worker thread after the specified delay has elapsed. The caller of
-   * this method continues to run in parallel. If the job is subject for mutual exclusion, the job only commence
+   * Runs the given task asynchronously on behalf of a worker thread after the specified delay has elapsed. The caller
+   * of this method continues to run in parallel. If the task is subject for mutual exclusion, the task only commence
    * execution once acquired the mutex.
-   * <p/>
-   * The {@link IFuture} returned allows to wait for the job to complete or to cancel the execution of the job. To
-   * immediately block waiting for the job to complete, you can use constructions of the form
+   * <p>
+   * The {@link IFuture} returned allows to wait for the task to complete or to cancel the execution of the task. To
+   * immediately block waiting for the task to complete, you can use constructions of the form
    * <code>result = Jobs.schedule(...).awaitDone();</code>.
    *
    * @param callable
    *          the runnable to be executed
    * @param delay
-   *          the delay after which the job should commence execution.
+   *          the delay after which the task should commence execution.
    * @param delayUnit
    *          the time unit of the <code>delay</code> argument.
    * @param input
-   *          describes the job to be executed; must not be <code>null</code>.
-   * @return Future to wait for the job's completion or to cancel the job's execution.
+   *          describes the task to be executed; must not be <code>null</code>.
+   * @return Future to wait for the task's completion or to cancel the task's execution.
    */
   <RESULT> IFuture<RESULT> schedule(Callable<RESULT> callable, long delay, TimeUnit delayUnit, JobInput input);
 
   /**
-   * Periodically runs the given job on behalf of a worker thread.<br/>
+   * Periodically runs the given task on behalf of a worker thread.<br/>
    * The first execution is after the given <code>initialDelay</code>, the second after <code>initialDelay+period</code>
    * , the third after <code>initialDelay+period+period</code> and so on. If an execution takes longer than the
    * <code>period</code>, the subsequent execution is delayed and starts only once the current execution completed. The
-   * job only terminates via cancellation or termination of the job manager.
+   * task only terminates via cancellation or termination of the job manager.
    *
    * @param runnable
    *          the runnable to be executed periodically.
@@ -91,15 +89,15 @@ public interface IJobManager {
    * @param unit
    *          the time unit of the <code>initialDelay</code> and <code>period</code> arguments.
    * @param input
-   *          describes the job to be executed; must not be <code>null</code>.
+   *          describes the task to be executed; must not be <code>null</code>.
    * @return Future to cancel the periodic action.
    */
   IFuture<Void> scheduleAtFixedRate(IRunnable runnable, long initialDelay, long period, TimeUnit unit, JobInput input);
 
   /**
-   * Periodically runs the given job on behalf of a worker thread.<br/>
+   * Periodically runs the given task on behalf of a worker thread.<br/>
    * The first execution is after the given <code>initialDelay</code>, and subsequently with the given
-   * <code>delay</code> between the termination of one execution and the commencement of the next. The job only
+   * <code>delay</code> between the termination of one execution and the commencement of the next. The task only
    * terminates via cancellation or termination of the job manager.
    *
    * @param runnable
@@ -111,17 +109,17 @@ public interface IJobManager {
    * @param unit
    *          the time unit of the <code>initialDelay</code> and <code>period</code> arguments.
    * @param input
-   *          describes the job to be executed; must not be <code>null</code>.
+   *          describes the task to be executed; must not be <code>null</code>.
    * @return Future to cancel the periodic action.
    */
   IFuture<Void> scheduleWithFixedDelay(IRunnable runnable, long initialDelay, long delay, TimeUnit unit, JobInput input);
 
   /**
    * Checks whether all Futures accepted by the given Filter are in 'done-state' (completed or cancelled).
-   * <p/>
+   * <p>
    * Filters can be plugged by using logical filters like {@link AndFilter} or {@link OrFilter}, or negated by enclosing
    * a filter in {@link NotFilter}. Also see {@link JobFutureFilters} for simplified usage:<br/>
-   * <code>Jobs.newFutureFilter().notCurrentFuture().session(...);</code>
+   * <code>Jobs.newFutureFilter().andMatchAnyFuture(...);</code>
    *
    * @param filter
    *          filter to limit the Futures to be checked for their 'done-state'. If <code>null</code>, all Futures are
@@ -133,10 +131,10 @@ public interface IJobManager {
   /**
    * Blocks the calling thread until all Futures accepted by the given Filter are in 'done-state' (completed or
    * cancelled), or the given timeout elapses.
-   * <p/>
+   * <p>
    * Filters can be plugged by using logical filters like {@link AndFilter} or {@link OrFilter}, or negated by enclosing
    * a filter in {@link NotFilter}. Also see {@link JobFutureFilters} for simplified usage:<br/>
-   * <code>Jobs.newFutureFilter().notCurrentFuture().session(...);</code>
+   * <code>Jobs.newFutureFilter().andMatchAnyFuture(...);</code>
    *
    * @param filter
    *          filter to limit the Futures to await to become 'done'. If <code>null</code>, all Futures are awaited,
@@ -147,14 +145,14 @@ public interface IJobManager {
    *          unit of the given timeout.
    * @return <code>false</code> if the deadline has elapsed upon return, else <code>true</code>.
    * @throws ProcessingException
-   *           if this thread was interrupted while waiting for the job to complete; see
+   *           if this thread was interrupted while waiting for the task to complete; see
    *           {@link ProcessingException#isInterruption()}
    */
   boolean awaitDone(IFilter<IFuture<?>> filter, long timeout, TimeUnit unit);
 
   /**
    * Visits all Futures that are accepted by the given Filter and are not in 'done-state'.
-   * <p/>
+   * <p>
    * Filters can be plugged by using logical filters like {@link AndFilter} or {@link OrFilter}, or negated by enclosing
    * a filter in {@link NotFilter}. Use a {@link CollectorVisitor} to collect all visited Futures. Also see
    * {@link JobFutureFilters} for simplified usage: <code>Jobs.newFutureFilter().notCurrentFuture().notBlocked();</code>
@@ -170,23 +168,23 @@ public interface IJobManager {
 
   /**
    * Cancels all Futures which are accepted by the given Filter.
-   * <p/>
+   * <p>
    * Filters can be plugged by using logical filters like {@link AndFilter} or {@link OrFilter}, or negated by enclosing
    * a filter in {@link NotFilter}. Also see {@link JobFutureFilters} for simplified usage:<br/>
-   * <code>Jobs.newFutureFilter().notCurrentFuture().session(...);</code>
+   * <code>Jobs.newFutureFilter().andMatchAnyFuture(...);</code>
    *
    * @param filter
    *          to limit the Futures to be cancelled. If <code>null</code>, all Futures are cancelled, which is the same
    *          as using {@link AlwaysFilter}.
    * @param interruptIfRunning
-   *          <code>true</code> to interrupt in-progress jobs.
+   *          <code>true</code> to interrupt in-progress tasks.
    * @return <code>true</code> if all Futures matching the Filter are cancelled successfully, or <code>false</code>, if
    *         a Future could not be cancelled, typically because already completed normally.
    */
   boolean cancel(IFilter<IFuture<?>> filter, boolean interruptIfRunning);
 
   /**
-   * Creates a blocking condition to put a job into waiting mode until the condition falls. See
+   * Creates a blocking condition to put a task into waiting mode until the condition falls. See
    * {@link IBlockingCondition} for more information.
    *
    * @param name
@@ -198,7 +196,7 @@ public interface IJobManager {
   IBlockingCondition createBlockingCondition(String name, boolean blocking);
 
   /**
-   * Interrupts all running jobs and prevents scheduled jobs from running. After having shutdown, this job manager
+   * Interrupts all running tasks and prevents scheduled tasks from running. After having shutdown, this job manager
    * cannot be used anymore.
    */
   void shutdown();
@@ -222,7 +220,7 @@ public interface IJobManager {
    * a filter in {@link NotFilter}. Also see {@link JobEventFilters} for simplified usage:<br/>
    * <code>Jobs.newEventFilter().andMatchAnyFuture(..);</code>
    * </p>
-   * 
+   *
    * @param filter
    *          filter to only get notified about events of interest - that is for events accepted by the filter.
    * @param listener
