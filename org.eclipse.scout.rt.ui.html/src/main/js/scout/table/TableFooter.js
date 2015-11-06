02408ea93bec2c09815b17fede960bc0db73dc78
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2015-11-06 17:21:00 +0100

    HtmlUi: Fixed table footer animation

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/table/TableFooter.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/table/TableFooter.js
@@ -398,8 +398,6 @@ scout.TableFooter.prototype.openControlContainer = function(control) {
 
   // adjust content
   this.$controlContent.outerHeight(contentHeight);
-  //FIXME CGU ask bsh what is this for? this prevents shrinking
-  this.$controlContainer.outerHeight(0); // start animation at zero
 
   // open container, stop existing (close) animations before
   // use delay to make sure form is rendered and layouted with new size
