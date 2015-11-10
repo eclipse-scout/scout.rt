7bee425db7e55279c1c9cacc194721d811800c9d
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2015-11-10 14:53:35 +0100

    HtmlUi: Fixed form layout
    Title height was not calculated properly -> dialog got 3px too big ->
    bottom dialog border was not visible

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/form/FormLayout.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/form/FormLayout.js
@@ -16,7 +16,7 @@ scout.inherits(scout.FormLayout, scout.AbstractLayout);
 
 scout.FormLayout.prototype.layout = function($container) {
   var htmlContainer = scout.HtmlComponent.get($container),
-    htmlRootGb = this._htmlRootGroupBox($container),
+    htmlRootGb = this._htmlRootGroupBox(),
     rootGbSize;
 
   rootGbSize = htmlContainer.getAvailableSize()
@@ -24,7 +24,7 @@ scout.FormLayout.prototype.layout = function($container) {
     .subtract(htmlRootGb.getMargins());
 
   if (this._form.isDialog()) {
-    rootGbSize.height -= this._titleHeight($container);
+    rootGbSize.height -= this._titleHeight();
   }
 
   $.log.trace('(FormLayout#layout) rootGbSize=' + rootGbSize);
@@ -33,35 +33,23 @@ scout.FormLayout.prototype.layout = function($container) {
 
 scout.FormLayout.prototype.preferredLayoutSize = function($container) {
   var htmlContainer = scout.HtmlComponent.get($container),
-    htmlRootGb = this._htmlRootGroupBox($container),
+    htmlRootGb = this._htmlRootGroupBox(),
     prefSize;
 
   prefSize = htmlRootGb.getPreferredSize()
     .add(htmlContainer.getInsets())
     .add(htmlRootGb.getMargins());
-  prefSize.height += this._titleHeight($container);
+  prefSize.height += this._titleHeight();
 
   return prefSize;
 };
 
-scout.FormLayout.prototype._htmlRootGroupBox = function($container) {
-  return scout.HtmlComponent.get($container.children('.root-group-box'));
+scout.FormLayout.prototype._htmlRootGroupBox = function() {
+  var $rootGroupBox = this._form.$container.children('.root-group-box');
+  return scout.HtmlComponent.get($rootGroupBox);
 };
 
-scout.FormLayout.prototype._titleHeight = function($container) {
-  // getVisibleSize doesn't work for dialogs, because the are still invisible when
-  // the render() method is called (they'll become visible later, when the fade-in
-  // animation is completed). That's why we must calculate the title size here:
-  var height = 0,
-    insets = scout.graphics.getInsets($container.children('.title-box'), {
-      includeMargin: true
-    });
-  if (this._form.title) {
-    height += scout.graphics.measureString(this._form.title, 'font-text-large').height;
-  }
-  if (this._form.subTitle) {
-    height += scout.graphics.measureString(this._form.subTitle).height;
-  }
-  return height + insets.top + insets.bottom;
-
+scout.FormLayout.prototype._titleHeight = function() {
+  var $titleBox = this._form.$container.children('.title-box');
+  return scout.graphics.prefSize($titleBox, true).height;
 };
