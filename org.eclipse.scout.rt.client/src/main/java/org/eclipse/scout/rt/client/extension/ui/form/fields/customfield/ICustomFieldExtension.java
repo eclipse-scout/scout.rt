0fd50f3e9a2256a7b0428030a4021e18223d6f06
André Wegmüller <awe@bsiag.com>
2015-11-17 09:26:05 +0100

    Deleted ICustomField/AbstractCustomField and all associated classes and files.

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/extension/ui/form/fields/customfield/ICustomFieldExtension.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.scout.rt.client.extension.ui.form.fields.customfield;
-
-import org.eclipse.scout.rt.client.extension.ui.form.fields.IFormFieldExtension;
-import org.eclipse.scout.rt.client.ui.form.fields.customfield.AbstractCustomField;
-
-public interface ICustomFieldExtension<OWNER extends AbstractCustomField> extends IFormFieldExtension<OWNER> {
-}
