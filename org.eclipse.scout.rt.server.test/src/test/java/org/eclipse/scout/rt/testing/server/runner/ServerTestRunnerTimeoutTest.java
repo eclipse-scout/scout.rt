1c172389e43c12a7ed0eef0265edba0f6b78f921
Judith Gull <jgu@bsiag.com>
2015-11-23 13:23:05 +0100

    add stacktrace to assertion error
    Change-Id: Ic75de5518f50b20a1e440fc92ebfcd15f58b4faa

--- org.eclipse.scout.rt.server.test/src/test/java/org/eclipse/scout/rt/testing/server/runner/ServerTestRunnerTimeoutTest.java
+++ org.eclipse.scout.rt.server.test/src/test/java/org/eclipse/scout/rt/testing/server/runner/ServerTestRunnerTimeoutTest.java
@@ -75,7 +75,7 @@ public class ServerTestRunnerTimeoutTest {
         if (f.getException() != null && f.getException().getCause() != null) {
           sb.append("Cause:");
           sb.append(f.getException().getCause());
-          sb.append(f.getException().getCause().getStackTrace());
+          sb.append(Arrays.asList(f.getException().getCause().getStackTrace()));
         }
       }
       Assert.fail(sb.toString());
