ab315bb08597c052f00d8f4d8b9cbaf5b8dca484
Judith Gull <jgu@bsiag.com>
2015-12-02 12:50:37 +0100

    remove unnecessary functionality in SerializationUtility
    Change-Id: Ic2562657220dff4198b4755a30600074bdbfd837

--- org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/serialization/SerializationUtility.java
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/serialization/SerializationUtility.java
@@ -10,70 +10,22 @@
  ******************************************************************************/
 package org.eclipse.scout.rt.platform.serialization;
 
-import java.lang.reflect.Modifier;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
- * Utility for serializing and deserializing java objects. The utility works in a standard java environment as well as
- * in an OSGi environment. All classes that are available on the class path or within an OSGi bundle can be
- * deserialized.
+ * Utility for serializing and deserializing java objects. The utility works in a standard java environment.
  * <p/>
  * The utility uses an environment-dependent {@link IObjectSerializerFactory} for creating {@link IObjectSerializer}
  * instances. An optional {@link IObjectReplacer} can be used for replacing and resolving objects during the
  * serialization and deserialization process, respectively.
  * <p/>
- * <h3>Custom Strategy</h3> A custom {@link IObjectSerializerFactory} can be provided by a fragment bundle using
- * <em>org.eclipse.scout.commons</em> as host bundle. The custom {@link IObjectSerializerFactory} class must use the
- * following fully qualified class name:
- * <p/>
- * <code>org.eclipse.scout.platform.serialization.CustomObjectSerializerFactory</code>
  *
  * @since 3.8.2
  */
 public final class SerializationUtility {
-  private static final Logger LOG;
 
-  private static final IObjectSerializerFactory FACTORY;
-
-  static {
-    LOG = LoggerFactory.getLogger(SerializationUtility.class);
-    FACTORY = createObjectSerializerFactory();
-  }
+  private static final IObjectSerializerFactory FACTORY = new BasicObjectSerializerFactory();
 
   private SerializationUtility() {
     // nop
-  }
-
-  private static IObjectSerializerFactory createObjectSerializerFactory() {
-    IObjectSerializerFactory factory = null;
-    // check whether there is a custom object serializer factory available
-    try {
-      Class<?> customSerializerFactory = Class.forName("org.eclipse.scout.platform.serialization.CustomObjectSerializerFactory");
-      LOG.info("loaded custom object serializer factory: [" + customSerializerFactory + "]");
-      if (!IObjectSerializerFactory.class.isAssignableFrom(customSerializerFactory)) {
-        LOG.warn("custom object serializer factory is not implementing [" + IObjectSerializerFactory.class + "]");
-      }
-      else if (Modifier.isAbstract(customSerializerFactory.getModifiers())) {
-        LOG.warn("custom object serializer factory is an abstract class [" + customSerializerFactory + "]");
-      }
-      else {
-        factory = (IObjectSerializerFactory) customSerializerFactory.newInstance();
-      }
-    }
-    catch (ClassNotFoundException e) {
-      // no custom object serializer factory installed
-    }
-    catch (Exception e) {
-      LOG.warn("Unexpected problem while creating a new instance of custom object serializer factory", e);
-    }
-
-    if (factory == null) {
-      factory = new BasicObjectSerializerFactory();
-    }
-
-    return factory;
   }
 
   /**
