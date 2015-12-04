c752c02eecf46bf7db593d8c05dd3e41e301461d
Fritz Schinkel <Fritz.Schinkel@bsi-software.com>
2015-12-04 09:36:07 +0100

    update license headers
    Change-Id: Id864f3e43a7f4c993ee8626b04207d4ce46cb2f7
    
    Original Author: Stephan Leicht Vogt <stephan.leichtvogt@bsi-software.com>

--- com.bsiag.scout.rt.client/src/main/java/com/bsiag/scout/rt/client/ui/basic/table/controls/IChartTableControl.java
+++ com.bsiag.scout.rt.client/src/main/java/com/bsiag/scout/rt/client/ui/basic/table/controls/IChartTableControl.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2015 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the BSI CRM Software License v1.0
+ * which accompanies this distribution as bsi-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
 package com.bsiag.scout.rt.client.ui.basic.table.controls;
 
 import org.eclipse.scout.rt.client.ui.basic.table.controls.ITableControl;
