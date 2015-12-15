8710b163ee50c75c9704163ec792b69842bbf301
Daniel Wiehl <daniel.wiehl@bsi-software.com>
2015-12-15 18:42:15 +0100

    Unblock condition instead of closing forms
    This is more general, especially because a blocking condition is not limited to forms.

--- org.eclipse.scout.rt.client.test/src/main/java/org/eclipse/scout/testing/client/BlockingTestUtility.java
+++ org.eclipse.scout.rt.client.test/src/main/java/org/eclipse/scout/testing/client/BlockingTestUtility.java
@@ -7,7 +7,6 @@ package org.eclipse.scout.testing.client;
 import org.eclipse.scout.rt.client.context.ClientRunContext;
 import org.eclipse.scout.rt.client.context.ClientRunContexts;
 import org.eclipse.scout.rt.client.job.ModelJobs;
-import org.eclipse.scout.rt.client.ui.form.IForm;
 import org.eclipse.scout.rt.platform.BEANS;
 import org.eclipse.scout.rt.platform.exception.DefaultRuntimeExceptionTranslator;
 import org.eclipse.scout.rt.platform.job.IBlockingCondition;
@@ -20,37 +19,30 @@ import org.eclipse.scout.rt.platform.job.listener.JobEvent;
 import org.eclipse.scout.rt.platform.job.listener.JobEventType;
 import org.eclipse.scout.rt.platform.util.concurrent.IRunnable;
 import org.eclipse.scout.rt.testing.platform.runner.JUnitExceptionHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Utility class to test code that enters a blocking condition.
- * <p>
- * TOOD [jgu] please move into scout testing module
  *
- * @see IBlockingCondition#waitFor()
+ * @see IBlockingCondition#waitFor(String...)
+ * @see IBlockingCondition#waitFor(long, java.util.concurrent.TimeUnit, String...)
  */
 public final class BlockingTestUtility {
-
-  private static final Logger LOG = LoggerFactory.getLogger(BlockingTestUtility.class);
 
   private BlockingTestUtility() {
   }
 
   /**
    * Helper method to test code which will enter a blocking condition.
+   * <p>
+   * If <code>runnableOnceBlocked</code> throws an exception, it is given to {@link JUnitExceptionHandler} to make the
+   * JUnit test fail.
    *
    * @param runnableGettingBlocked
    *          {@code IRunnable} that will enter a blocking condition.
    * @param runnableOnceBlocked
    *          {@code IRunnable} to be executed once the 'runnableGettingBlocked' enters a blocking condition.
-   * @param closeBlockingFormsOnDone
-   *          <code>true</code> to close all blocking Forms once 'runnableOnceBlocked' ends execution.
-   * @throws RuntimeException
-   *           thrown if the execution of 'runnableGettingBlocked' or 'runnableOnceBlocked' throws an exception.
    */
-  @SafeVarargs
-  public static void runBlockingAction(final IRunnable runnableGettingBlocked, final IRunnable runnableOnceBlocked, final Class<? extends IForm>... formsToClose) {
+  public static void runBlockingAction(final IRunnable runnableGettingBlocked, final IRunnable runnableOnceBlocked) {
     final ClientRunContext runContext = ClientRunContexts.copyCurrent();
 
     final IJobListenerRegistration listenerRegistration = IFuture.CURRENT.get().addListener(Jobs.newEventFilterBuilder()
@@ -61,24 +53,19 @@ public final class BlockingTestUtility {
           @Override
           public void changed(final JobEvent event) {
             ModelJobs.schedule(new IRunnable() {
+
               @Override
               public void run() throws Exception {
                 try {
                   runnableOnceBlocked.run();
                 }
                 finally {
-                  for (final Class<? extends IForm> formToClose : formsToClose) {
-                    final IForm form = runContext.getDesktop().findForm(formToClose);
-                    if (form != null) {
-                      LOG.info("closing form '{}' ({}).", form.getTitle(), form.getClass().getSimpleName());
-                      form.doClose();
-                    }
-                  }
+                  event.getData().getBlockingCondition().setBlocking(false);
                 }
               }
             }, ModelJobs.newInput(runContext)
                 .withExceptionHandling(JUnitExceptionHandler.class, true)
-                .withName("runnable once blocked"));
+                .withName("JUnit: Handling blocked thread because waiting for a blocking condition"));
           }
         });
     try {
