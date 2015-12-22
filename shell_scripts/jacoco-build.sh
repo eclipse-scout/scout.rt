78e185b98110f01293b8e409241fa8c60604ca0e
Stephan Leicht Vogt <stephan.leichtvogt@bsi-software.com>
2015-12-22 17:11:39 +0100

    remove jacoco plugin from this build as its already in MM 2.0.2..
    Change-Id: I9bca2ee26d84edaca387a1d48e48164eeb14b0c5
    Reviewed-on: https://git.eclipse.org/r/63163
    Tested-by: Hudson CI
    Reviewed-by: Stephan Leicht Vogt <stephan.leichtvogt@bsi-software.com>

--- shell_scripts/jacoco-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Parallel executions of maven modules and tests.
-# Half of CPU core are used in to keep other half for OS and other programs.
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -B -e -V -Pcoverage-per-test -Dmaven.test.failure.ignore=true -T0.5C -DforkCount=0.5C $*
