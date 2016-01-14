faa29dcbcb13cb90975c8188f9d53a28e3d064f5
Daniel Wiehl <daniel.wiehl@bsi-software.com>
2016-01-14 15:07:30 +0100

    Make JsonAdapterRegistry thread safe
    Review: Beat.Schwarzentrub@bsi-software.com

--- org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/JsonAdapterRegistry.java
+++ org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/JsonAdapterRegistry.java
@@ -12,6 +12,9 @@ package org.eclipse.scout.rt.ui.html.json;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
 
 import org.eclipse.scout.rt.platform.index.AbstractMultiValueIndex;
 import org.eclipse.scout.rt.platform.index.AbstractSingleValueIndex;
@@ -21,7 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This class represents a per session registry for {@link IJsonAdapter} instances.
+ * This class represents a per session registry for {@link IJsonAdapter} instances. This class is thread safe.
  */
 public class JsonAdapterRegistry {
 
@@ -34,6 +37,9 @@ public class JsonAdapterRegistry {
   private final P_IndexByParent m_idxByParent;
   private final P_IndexByModel m_idxByModel;
 
+  private final ReadLock m_readLock;
+  private final WriteLock m_writeLock;
+
   public JsonAdapterRegistry() {
     m_store = new IndexedStore<>();
 
@@ -41,22 +47,38 @@ public class JsonAdapterRegistry {
     m_idxByModelAndParent = m_store.registerIndex(new P_IndexByModelAndParent());
     m_idxByParent = m_store.registerIndex(new P_IndexByParent());
     m_idxByModel = m_store.registerIndex(new P_IndexByModel());
+
+    final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    m_readLock = lock.readLock();
+    m_writeLock = lock.writeLock();
   }
 
   /**
    * Adds the given adapter to this registry.
    */
   public void add(final IJsonAdapter<?> adapter) {
-    m_store.add(adapter);
+    m_writeLock.lock();
+    try {
+      m_store.add(adapter);
+    }
+    finally {
+      m_writeLock.unlock();
+    }
   }
 
   /**
    * Removes the adapter with the given <code>adapterId</code> from this registry.
    */
   public void remove(final String adapterId) {
-    final IJsonAdapter<?> adapter = getById(adapterId);
-    if (adapter != null) {
-      m_store.remove(adapter);
+    m_writeLock.lock();
+    try {
+      final IJsonAdapter<?> adapter = getById(adapterId);
+      if (adapter != null) {
+        m_store.remove(adapter);
+      }
+    }
+    finally {
+      m_writeLock.unlock();
     }
   }
 
@@ -64,14 +86,26 @@ public class JsonAdapterRegistry {
    * Returns the number of adapters contained in this registry.
    */
   public int size() {
-    return m_store.size();
+    m_readLock.lock();
+    try {
+      return m_store.size();
+    }
+    finally {
+      m_readLock.unlock();
+    }
   }
 
   /**
    * Returns the adapter with the given <code>adapterId</code>.
    */
   public IJsonAdapter<?> getById(final String adapterId) {
-    return m_idxById.get(adapterId);
+    m_readLock.lock();
+    try {
+      return m_idxById.get(adapterId);
+    }
+    finally {
+      m_readLock.unlock();
+    }
   }
 
   /**
@@ -79,14 +113,26 @@ public class JsonAdapterRegistry {
    */
   @SuppressWarnings("unchecked")
   public <MODEL, ADAPTER extends IJsonAdapter<? super MODEL>> ADAPTER getByModelAndParentAdapter(final MODEL model, final IJsonAdapter<?> parent) {
-    return (ADAPTER) m_idxByModelAndParent.get(createModelAndParentAdapterPair(model, parent));
+    m_readLock.lock();
+    try {
+      return (ADAPTER) m_idxByModelAndParent.get(createModelAndParentAdapterPair(model, parent));
+    }
+    finally {
+      m_readLock.unlock();
+    }
   }
 
   /**
    * Returns all adapters that have the given adapter as their parent adapter.
    */
   public List<IJsonAdapter<?>> getByParentAdapter(final IJsonAdapter<?> parentAdapter) {
-    return m_idxByParent.get(parentAdapter);
+    m_readLock.lock();
+    try {
+      return m_idxByParent.get(parentAdapter);
+    }
+    finally {
+      m_readLock.unlock();
+    }
   }
 
   /**
@@ -94,28 +140,40 @@ public class JsonAdapterRegistry {
    */
   @SuppressWarnings("unchecked")
   public <MODEL> List<IJsonAdapter<MODEL>> getByModel(final MODEL model) {
-    final List<IJsonAdapter<?>> adaptersRaw = m_idxByModel.get(model);
+    m_readLock.lock();
+    try {
+      final List<IJsonAdapter<?>> adaptersRaw = m_idxByModel.get(model);
 
-    final List<IJsonAdapter<MODEL>> adapters = new ArrayList<>(adaptersRaw.size());
-    for (final IJsonAdapter<?> adapterRaw : adaptersRaw) {
-      adapters.add((IJsonAdapter<MODEL>) adapterRaw);
+      final List<IJsonAdapter<MODEL>> adapters = new ArrayList<>(adaptersRaw.size());
+      for (final IJsonAdapter<?> adapterRaw : adaptersRaw) {
+        adapters.add((IJsonAdapter<MODEL>) adapterRaw);
+      }
+      return adapters;
     }
-    return adapters;
+    finally {
+      m_readLock.unlock();
+    }
   }
 
   /**
    * Clears and disposes all contained adapters.
    */
   public void disposeAdapters() {
-    for (final IJsonAdapter<?> adapter : m_store.values()) {
-      if (!adapter.isDisposed()) {
-        adapter.dispose();
+    m_writeLock.lock();
+    try {
+      for (final IJsonAdapter<?> adapter : m_store.values()) {
+        if (!adapter.isDisposed()) {
+          adapter.dispose();
+        }
+      }
+
+      // "Memory leak detection". After disposing all adapters and flushing the session, no adapters should be remaining.
+      if (!m_store.isEmpty()) {
+        LOG.error("Memory leak detected: JsonAdapterRegistry expected to be empty after disposing all adapters [adapterCount={}]", m_store.size());
       }
     }
-
-    // "Memory leak detection". After disposing all adapters and flushing the session, no adapters should be remaining.
-    if (!m_store.isEmpty()) {
-      LOG.error("Memory leak detected: JsonAdapterRegistry expected to be empty after disposing all adapters [adapterCount={}]", m_store.size());
+    finally {
+      m_writeLock.unlock();
     }
   }
 
