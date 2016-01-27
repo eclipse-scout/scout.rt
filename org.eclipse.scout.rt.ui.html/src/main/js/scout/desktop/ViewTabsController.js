bd507a009ef80def3f7e8ced9089fbd3d02c92ea
Nathan Burgherr <nathan.burgherr@bsiag.com>
2016-01-27 09:18:43 +0100

    HtmlUI: removed fixme after test
    Change-Id: I0a21514acfef8dc22268a0cecca8bd3d0054903d

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/desktop/ViewTabsController.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/desktop/ViewTabsController.js
@@ -71,12 +71,7 @@ scout.ViewTabsController.prototype.createAndRenderViewTab = function(view, posit
 
 scout.ViewTabsController.prototype._calculateExactPosition = function(parent, position) {
   if (position === 0) {
-    return 0; // return 1; --> BenchModeTest = grün
-    // FIXME nbu: (von A.WE) das hier macht den BENCH modus vom Desktop kaputt (siehe BenchModeTest)
-    // dort gibt es keine outline, die app wird mit einem einzigen form (view) gestartet
-    // von diesem form aus öffnet man ein weiteres form (view), das modal zum aktuellen form sein soll
-    // der aktuelle code fügt dieses neue form an position 0 ein. Das ist nicht richtig, das neue form
-    // muss rechts vom aktuellen tab
+    return 0;
   } else {
     var tabs = position || parent.views.length;
     var searchUntil = position || parent.views.length;
