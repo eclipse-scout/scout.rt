e94eaec1effc13c9cee701866d4d0c3f2b086319
Nathan Burgherr <nathan.burgherr@bsiag.com>
2016-02-08 09:04:30 +0100

    HtmlUI: fixed double execution of upload.
    Change-Id: I4b1e5915f18820b25840d40e053204ee791a5e1c

--- org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/UploadRequestHandler.java
+++ org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/UploadRequestHandler.java
@@ -110,6 +110,10 @@ public class UploadRequestHandler extends AbstractUiServletRequestHandler {
 
     IBinaryResourceConsumer binaryResourceConsumer = resolveJsonAdapter(uiSession, targetAdapterId);
 
+    if (binaryResourceConsumer == null) {
+      //Request was already processed and adapter does not exist anymore;
+      return;
+    }
     // Read uploaded data
     Map<String, String> uploadProperties = new HashMap<String, String>();
     List<BinaryResource> uploadResources = new ArrayList<>();
@@ -195,7 +199,7 @@ public class UploadRequestHandler extends AbstractUiServletRequestHandler {
       throw new IllegalArgumentException("Missing target adapter ID");
     }
     IJsonAdapter<?> jsonAdapter = uiSession.getJsonAdapter(targetAdapterId);
-    if (!(jsonAdapter instanceof IBinaryResourceConsumer)) {
+    if (jsonAdapter != null && !(jsonAdapter instanceof IBinaryResourceConsumer)) {
       throw new IllegalStateException("Invalid adapter for ID " + targetAdapterId + (jsonAdapter == null ? "" : " (unexpected type)"));
     }
     return (IBinaryResourceConsumer) jsonAdapter;
