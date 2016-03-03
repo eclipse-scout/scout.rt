9eac9e46a23ddf28404693ccbc4e73f1c46ee5d7
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2016-03-03 15:37:52 +0100

    Add license header to modules

--- org.eclipse.scout.rt.ui.html/src/main/js/scout-login-module.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout-login-module.js
@@ -1,13 +1,11 @@
-/*******************************************************************************
- * Copyright (c) 2014-2015 BSI Business Systems Integration AG.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     BSI Business Systems Integration AG - initial API and implementation
- ******************************************************************************/
+/*!
+* Eclipse Scout Login
+* https://eclipse.org/scout/
+*
+* Copyright (c) BSI Business Systems Integration AG. All rights reserved.
+* Released under the Eclipse Public License v1.0
+* http://www.eclipse.org/legal/epl-v10.html
+*/
 // protects $ and undefined from being redefined by another library
 (function(scout, $, undefined) {
   __include("jquery/jquery-scout.js");
