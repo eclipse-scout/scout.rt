2f22828ec34cd4bc05bc7411ed251cde5e726099
Matthias Villiger <mvi@bsi-software.com>
2016-03-18 14:18:25 +0100

    Create TestCases for Scout elements

--- scout-helloworld-app/src/main/resources/archetype-resources/__rootArtifactId__.server/src/main/java/server/helloworld/HelloWorldService.java
+++ scout-helloworld-app/src/main/resources/archetype-resources/__rootArtifactId__.server/src/main/java/server/helloworld/HelloWorldService.java
@@ -17,7 +17,7 @@ public class HelloWorldService implements IHelloWorldService {
   @Override
   public HelloWorldFormData load(HelloWorldFormData input) {
     StringBuilder msg = new StringBuilder();
-    msg.append("Hello ").append(ServerSession.get().getUserId()).append("!");
+    msg.append("Hello ").append(ServerSession.get().getUserId()).append('!');
     input.getMessage().setValue(msg.toString());
     return input;
   }
