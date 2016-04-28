41246c3fa4dcbeb9f066af18709ffb702514e81f
Nathan Burgherr <nathan.burgherr@bsiag.com>
2016-04-28 07:36:40 +0200

    fixed inserting when nodes in subtree already exists
    Change-Id: Ib7c0f4977c76791f4a3e7d329282b16ccc407712

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/tree/Tree.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/tree/Tree.js
@@ -1519,7 +1519,7 @@ scout.Tree.prototype._addChildsToFlatList = function(parentNode, parentIndex, an
     } else if (node.initialized && node.isFilterAccepted(forceFilter) && isAlreadyAdded) {
       this.insertBatchInVisibleNodes(insertBatch, this.viewRangeRendered.from + this.viewRangeSize >= insertBatch.lastBatchInsertIndex() && this.viewRangeRendered.from <= insertBatch.lastBatchInsertIndex(), animatedRendering);
       this.checkAndHandleBatchAnimationWrapper(parentNode, animatedRendering, insertBatch);
-      insertBatch = this.setUpInsertBatch(insertBatch.lastBatchInsertIndex());
+      insertBatch = this.setUpInsertBatch(insertBatch.lastBatchInsertIndex()+1);
       if (node.expanded) {
         insertBatch = this._addChildsToFlatList(node, insertBatch.lastBatchInsertIndex(), animatedRendering, insertBatch, forceFilter);
       }
@@ -1543,6 +1543,9 @@ scout.Tree.prototype.setUpInsertBatch = function(insertIndex) {
     insertNodes: [insertIndex, 0],
     $animationWrapper: null,
     lastBatchInsertIndex: function() {
+      if(this.insertNodes.length === 2){
+        return this.insertNodes[0];
+      }
       return this.insertNodes[0] + this.insertNodes.length - 3;
     }
   };
@@ -1593,7 +1596,7 @@ scout.Tree.prototype.checkAndHandleBatch = function(insertBatch, parentNode, ani
 };
 
 scout.Tree.prototype.insertBatchInVisibleNodes = function(insertBatch, showNodes, animate) {
-  if (insertBatch.insertNodes < 2) {
+  if (insertBatch.insertNodes < 3) {
     //nothing to add
     return;
   }
