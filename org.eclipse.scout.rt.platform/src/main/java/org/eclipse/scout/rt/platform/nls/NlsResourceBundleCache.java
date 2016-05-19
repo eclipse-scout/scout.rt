60bc94844da6bab0b3b66f1f6ee0b986c140ebd4
Stephan Merkli <stephan.merkli@bsi-software.com>
2016-05-19 13:52:39 +0200

    NlsResourceBundleCache: replace synchronize block by concurrent map
    Change-Id: I9fe9aeae951d7e832594b215b61be1db4ac952c1
    Reviewed-on: https://git.eclipse.org/r/73155
    Tested-by: Hudson CI
    Reviewed-by: Stephan Merkli <stephan.merkli@bsi-software.com>

--- org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/nls/NlsResourceBundleCache.java
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/nls/NlsResourceBundleCache.java
@@ -10,10 +10,11 @@
  ******************************************************************************/
 package org.eclipse.scout.rt.platform.nls;
 
-import java.util.HashMap;
+import java.util.Enumeration;
 import java.util.Locale;
-import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * <h4>ResourceBundleCache</h4> Is used to cache resource bundle instances per {@link java.util.Locale}
@@ -23,29 +24,59 @@ import java.util.ResourceBundle;
 public class NlsResourceBundleCache {
   private final String m_resourceBundleName;
   private final Class<?> m_wrapperClass;
-  private final Map<Locale, ResourceBundle> m_resourceBundles;
+  private final ConcurrentMap<Locale, ResourceBundle> m_resourceBundles;
+
+  /** constant indicating that no resource bundle exists */
+  private static final ResourceBundle NONEXISTENT_BUNDLE = new ResourceBundle() {
+    @Override
+    public Enumeration<String> getKeys() {
+      return null;
+    }
+
+    @Override
+    protected Object handleGetObject(String key) {
+      return null;
+    }
+
+    @Override
+    public String toString() {
+      return "NONEXISTENT_BUNDLE";
+    }
+  };
 
   public NlsResourceBundleCache(String resourceBundleName, Class wrapperClass) {
     m_resourceBundleName = resourceBundleName;
     m_wrapperClass = wrapperClass;
-    m_resourceBundles = new HashMap<Locale, ResourceBundle>();
+    m_resourceBundles = new ConcurrentHashMap<Locale, ResourceBundle>();
   }
 
   public ResourceBundle getResourceBundle(Locale locale) {
     if (locale == null) {
       throw new IllegalArgumentException("locale must not be null");
     }
-    synchronized (this) {
-      // double check with lock
-      ResourceBundle r = m_resourceBundles.get(locale);
-      if (r == null) {
-        r = NlsResourceBundle.getBundle(m_resourceBundleName, locale, m_wrapperClass.getClassLoader());
-        if (r != null) {
-          m_resourceBundles.put(locale, r);
-        }
-      }
-      return r;
+
+    ResourceBundle resourceBundle = m_resourceBundles.get(locale);
+    if (resourceBundle == NONEXISTENT_BUNDLE) {
+      return null;
     }
+
+    if (resourceBundle != null) {
+      return resourceBundle;
+    }
+
+    resourceBundle = NlsResourceBundle.getBundle(m_resourceBundleName, locale, m_wrapperClass.getClassLoader());
+    if (resourceBundle == null) {
+      // each execution will return the same result (thus it's okay to directly put this result, it will always be null once null)
+      // null is not supported, thus using a singleton instead to mark non existent bundle
+      m_resourceBundles.put(locale, NONEXISTENT_BUNDLE);
+      return null;
+    }
+
+    ResourceBundle r = m_resourceBundles.putIfAbsent(locale, resourceBundle);
+    // check for non existent bundle not necessary because already verified
+    // by explicitly calling NlsResourceBundle.getBundle above
+    // always returning same instance (thus using r if set in which case locale resource bundle is obsolete)
+    return r != null ? r : resourceBundle;
   }
 
   public Class<?> getWrapperClass() {
