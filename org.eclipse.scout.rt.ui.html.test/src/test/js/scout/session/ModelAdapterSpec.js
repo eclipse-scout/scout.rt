eclipse-scout/scout.rt

commit 0d68bafdbe1ea677e3a4e41ae7c219da08348871

Mobile: Optimize Smart and DateField popup
The field on the touch popup didn't always show the same value as the
original field. The sync worked sometimes because the server sent the
display text back. This didn't happen in every case (e.g. if display
text was empty) -> make sure sync happens on gui correctly. Because the
popup was kept as variable, it would have been necessary to sync in both
directions. To avoid this the popup variable will only be set if the
popup is open -> makes it a lot easier to handle and is more consistent
with other widgets (e.g. menu)

The fix in the model adapter was necessary because not all properties
were in the list of modelProperties. The concrete problem was: If the
displayText was initially set it was correctly copied to the clone. If
not, the property was not in the list of modelProperties and therefore
not copied.

org.eclipse.scout.rt.ui.html/src/main/js/scout/session/ModelAdapter.js
@@ -66,6 +66,9 @@ scout.ModelAdapter.prototype._init = function(model) {
     this.remoteHandler = this.session.sendEvent.bind(this.session);
   }
 
+  // Fill in the missing default values (has to before copying the properties, so that modelProperties considers default values as well)
+  scout.defaultValues.applyTo(model);
+
   // copy all properties from model to this adapter
   this._eachProperty(model, function(propertyName, value, isAdapterProp) {
     // if property is not yet in the array of property names -> add property
@@ -81,9 +84,6 @@ scout.ModelAdapter.prototype._init = function(model) {
     }
     this[propertyName] = value;
   }.bind(this));
-
-  // Fill in the missing default values
-  scout.defaultValues.applyTo(this);
 };
 
 /**
@@ -151,7 +151,7 @@ scout.ModelAdapter.prototype._addAdapterProperties = function(properties) {
 /**
  * Adds property name(s) of model properties. They're used when a model adpater is cloned (see #cloneAdapter()).
  * You only need to call this method for UI-only properties. Properties from the server-side model are automatically
- * added in the _init method of the model adpater.
+ * added in the _init method of the model adapter.
  *
  * @param properties String or String-array with property names.
  */

org.eclipse.scout.rt.ui.html.test/src/test/js/scout/form/fields/datefield/DateFieldSpec.js
@@ -71,6 +71,13 @@ describe("DateField", function() {
     expect(date.getDate()).toBe(day);
   }
 
+  function selectFirstDayInPicker($picker) {
+    var $day = $picker.find('.date-picker-day').first();
+    var date = $day.data('date');
+    $day.triggerClick();
+    return date;
+  }
+
   describe("Clicking the field", function() {
 
     it("opens the datepicker", function() {
@@ -80,7 +87,6 @@ describe("DateField", function() {
       expect(findPicker().length).toBe(0);
 
       dateField.$dateField.triggerMouseDown();
-
       expect(findPicker().length).toBe(1);
     });
 
@@ -247,7 +253,10 @@ describe("DateField", function() {
       var model = createModel();
       var dateField = createFieldAndFocusAndOpenPicker(model);
 
-      dateField._onDatePickerDateSelect({type: 'dateSelect', date: new Date(2016,1,1)});
+      dateField._onDatePickerDateSelect({
+        type: 'dateSelect',
+        date: new Date(2016, 1, 1)
+      });
       expect(dateField.$dateField.val()).toBe('01.02.2016');
       sendQueuedAjaxCalls();
       expect(jasmine.Ajax.requests.count()).toBe(1);
@@ -275,7 +284,6 @@ describe("DateField", function() {
         expect(findPicker().length).toBe(1);
 
         dateField.$dateField.triggerKeyDown(scout.keys.ESC);
-
         expect(findPicker().length).toBe(0);
       });
 
@@ -485,6 +493,168 @@ describe("DateField", function() {
       expectDate(dateField.allowedDates[0], 2016, 2, 14);
     });
 
+  });
+
+  describe('Touch = true', function() {
+
+    describe('touch popup', function() {
+
+      it('is opened if datefield is touched', function() {
+        var model = createModel();
+        model.touch = true;
+        var dateField = createField(model);
+        dateField.render(session.$entryPoint);
+
+        dateField.$dateField.triggerClick();
+        expect(dateField.popup.rendered).toBe(true);
+        expect($('.touch-popup').length).toBe(1);
+        expect($('.date-picker-popup').length).toBe(0);
+        dateField.popup.close();
+      });
+
+      it('is closed when date in picker is selected', function() {
+        var model = createModel();
+        model.touch = true;
+        var dateField = createField(model);
+        dateField.render(session.$entryPoint);
+
+        dateField.$dateField.triggerClick();
+        expect(dateField.popup.rendered).toBe(true);
+
+        var selectedDate = selectFirstDayInPicker(dateField.popup.$container.find('.date-picker'));
+        expect(dateField.popup).toBe(null);
+      });
+
+      it('updates displayText and timestamp of datefield if date in picker is selected', function() {
+        var model = createModel();
+        model.touch = true;
+        var dateField = createField(model);
+        dateField.render(session.$entryPoint);
+
+        dateField.$dateField.triggerClick();
+        expect(dateField.popup.rendered).toBe(true);
+
+        var selectedDate = selectFirstDayInPicker(dateField.popup.$container.find('.date-picker'));
+        expect(dateField.popup).toBe(null);
+        expect(dateField.timestampAsDate).toEqual(selectedDate);
+        expect(dateField.displayText).toBe(dateField.isolatedDateFormat.format(selectedDate));
+        expect(dateField.$dateField.text()).toBe(dateField.displayText);
+      });
+
+      it('updates displayText and timestamp of datefield if date in picker is entered', function() {
+        var model = createModel();
+        model.touch = true;
+        var dateField = createField(model);
+        dateField.render(session.$entryPoint);
+
+        dateField.$dateField.triggerClick();
+        expect(dateField.popup.rendered).toBe(true);
+
+        dateField.popup._field.$dateField.val('11.02.2015');
+        dateField.popup._field.$dateField.triggerKeyDown(scout.keys.ENTER);
+        expect(dateField.popup).toBe(null);
+        expectDate(dateField.timestampAsDate, 2015, 02, 11);
+        expect(dateField.displayText).toBe('11.02.2015');
+        expect(dateField.$dateField.text()).toBe(dateField.displayText);
+      });
+
+      it('shows datefield with same date as clicked datefield', function() {
+        var model = createModel();
+        model.touch = true;
+        model.timestamp = '2012-07-01';
+        model.displayText = '01.07.2012';
+        var dateField = createField(model);
+        dateField.render(session.$entryPoint);
+
+        expect(dateField.$dateField.text()).toBe(dateField.displayText);
+        dateField.$dateField.triggerClick();
+        expect(dateField.popup.rendered).toBe(true);
+        expect(dateField.popup._field.timestampAsDate).toEqual(dateField.timestampAsDate);
+        expect(dateField.popup._field.displayText).toBe(dateField.displayText);
+        expect(dateField.popup._field.$dateField.val()).toBe(dateField.displayText);
+      });
+
+      it('shows datefield with same date as clicked datefield, if field empty initially', function() {
+        var model = createModel();
+        model.touch = true;
+        var dateField = createField(model);
+        dateField.render(session.$entryPoint);
+
+        // Open
+        dateField.$dateField.triggerClick();
+        expect(dateField.popup.rendered).toBe(true);
+
+        // Enter date and close
+        dateField.popup._field.$dateField.val('11.02.2015');
+        dateField.popup._field.$dateField.triggerKeyDown(scout.keys.ENTER);
+        expect(dateField.popup).toBe(null);
+        expect(dateField.displayText).toBe('11.02.2015');
+
+        // Reopen and verify
+        dateField.$dateField.triggerClick();
+        expect(dateField.popup.rendered).toBe(true);
+        expect(dateField.popup._field.timestampAsDate).toEqual(dateField.timestampAsDate);
+        expect(dateField.popup._field.displayText).toBe(dateField.displayText);
+        expect(dateField.popup._field.$dateField.val()).toBe(dateField.displayText);
+      });
+
+      it('clears displayText and timestamp of datefield if date in picker was removed', function() {
+        var model = createModel();
+        model.touch = true;
+        model.timestamp = '2012-07-01';
+        model.displayText = '01.07.2012';
+        var dateField = createField(model);
+        dateField.render(session.$entryPoint);
+
+        dateField.$dateField.triggerClick();
+        expect(dateField.popup.rendered).toBe(true);
+        expect(dateField.popup._field.$dateField.val()).toBe(dateField.displayText);
+
+        dateField.popup._field.$dateField.val('');
+        dateField.popup._field.$dateField.triggerKeyDown(scout.keys.ENTER);
+        expect(dateField.popup).toBe(null);
+        expect(dateField.timestampAsDate).toBe(null);
+        expect(dateField.displayText).toBe('');
+        expect(dateField.$dateField.text()).toBe('');
+      });
+
+      it('shows datefield with same date as clicked datefield, even if value was deleted before', function() {
+        var model = createModel();
+        model.touch = true;
+        model.timestamp = '2012-07-01';
+        model.displayText = '01.07.2012';
+        var dateField = createField(model);
+        dateField.render(session.$entryPoint);
+
+        // Open and check display text
+        dateField.$dateField.triggerClick();
+        expect(dateField.popup.rendered).toBe(true);
+        expect(dateField.popup._field.$dateField.val()).toBe(dateField.displayText);
+
+        // Clear text
+        dateField.popup._field.$dateField.val('');
+        dateField.popup._field.$dateField.triggerKeyDown(scout.keys.ENTER);
+        expect(dateField.popup).toBe(null);
+        expect(dateField.$dateField.text()).toBe('');
+
+        // Open again
+        dateField.$dateField.triggerClick();
+        expect(dateField.popup.rendered).toBe(true);
+        expect(dateField.popup._field.$dateField.val()).toBe(dateField.displayText);
+
+        // Select a date
+        var selectedDate = selectFirstDayInPicker(dateField.popup.$container.find('.date-picker'));
+        expect(dateField.popup).toBe(null);
+        expect(dateField.timestampAsDate).toEqual(selectedDate);
+        expect(dateField.displayText).toBe(dateField.isolatedDateFormat.format(selectedDate));
+        expect(dateField.$dateField.text()).toBe(dateField.displayText);
+
+        // Open again and verify
+        dateField.$dateField.triggerClick();
+        expect(dateField.popup.rendered).toBe(true);
+        expect(dateField.popup._field.$dateField.val()).toBe(dateField.displayText);
+      });
+    });
   });
 
 });

org.eclipse.scout.rt.ui.html.test/src/test/js/scout/form/fields/smartfield/SmartFieldSpec.js
@@ -51,7 +51,6 @@ describe('SmartField', function() {
     it('calls _openProposal() when a character key has been pressed', function() {
       smartField.render(session.$entryPoint);
       smartField._browseOnce = true;
-      smartField._popup = {};
       smartField._openProposal = function(displayText, selectCurrentValue) {};
       var event = {
         which: scout.keys.A
@@ -171,16 +170,16 @@ describe('SmartField', function() {
       smartField.onModelPropertyChange(createPropertyChangeEvent(smartField, {
         proposalChooser: proposalChooser.id
       }));
-      expect(smartField._popup.rendered).toBe(true);
-      expect(smartField._popup._$widgetContainer.has(smartField.proposalChooser.$container));
+      expect(smartField.popup.rendered).toBe(true);
+      expect(smartField.popup._$widgetContainer.has(smartField.proposalChooser.$container));
       expect($('.touch-popup').length).toBe(1);
       expect($('.smart-field-popup').length).toBe(0);
 
-      smartField._popup.close();
+      smartField.popup.close();
       smartField.onModelPropertyChange(createPropertyChangeEvent(smartField, {
         proposalChooser: null
       }));
-      expect(smartField._popup.rendered).toBe(false);
+      expect(smartField.popup).toBe(null);
       expect($('.touch-popup').length).toBe(0);
       expect($('.smart-field-popup').length).toBe(0);
 
@@ -189,23 +188,36 @@ describe('SmartField', function() {
       smartField.onModelPropertyChange(createPropertyChangeEvent(smartField, {
         proposalChooser: proposalChooser.id
       }));
-      expect(smartField._popup.rendered).toBe(true);
-      expect(smartField._popup._$widgetContainer.has(smartField.proposalChooser.$container));
+      expect(smartField.popup.rendered).toBe(true);
+      expect(smartField.popup._$widgetContainer.has(smartField.proposalChooser.$container));
       expect($('.touch-popup').length).toBe(1);
       expect($('.smart-field-popup').length).toBe(0);
-      smartField._popup.close();
+      smartField.popup.close();
     });
 
     it('opens a touch popup if there already is a proposal chooser while rendering', function() {
       smartField.proposalChooser = scout.create('Table', {parent: new scout.NullWidget(), session: session, _register: true});
       smartField.touch = true;
       smartField.render(session.$entryPoint);
-      expect(smartField._popup.rendered).toBe(true);
-      expect(smartField._popup._$widgetContainer.has(smartField.proposalChooser.$container));
+      expect(smartField.popup.rendered).toBe(true);
+      expect(smartField.popup._$widgetContainer.has(smartField.proposalChooser.$container));
       expect($('.touch-popup').length).toBe(1);
       expect($('.smart-field-popup').length).toBe(0);
       expect($('.smart-field-popup').length).toBe(0);
-      smartField._popup.close();
+      smartField.popup.close();
+    });
+
+    it('shows smartfield with same text as clicked smartfield', function() {
+      smartField.proposalChooser = scout.create('Table', {parent: new scout.NullWidget(), session: session, _register: true});
+      smartField.touch = true;
+      smartField.displayText = 'row 1';
+      smartField.render(session.$entryPoint);
+
+      smartField.$field.triggerClick();
+      expect(smartField.popup.rendered).toBe(true);
+      expect(smartField.popup._field.displayText).toBe(smartField.displayText);
+      expect(smartField.popup._field.$field.val()).toBe(smartField.displayText);
+      smartField.popup.close();
     });
 
   });

org.eclipse.scout.rt.ui.html/src/main/js/scout/action/Action.js
@@ -10,7 +10,7 @@
  ******************************************************************************/
 scout.Action = function() {
   scout.Action.parent.call(this);
-  this._addModelProperties(['text', 'iconId', 'tooltipText', 'keyStroke', 'enabled', 'selected', 'visible', 'tabbable', 'cssClass']);
+  this._addModelProperties(['keyStroke', 'tabbable', 'cssClass']);
 
   /**
    * This property decides whether or not the tabindex attribute is set in the DOM.

org.eclipse.scout.rt.ui.html/src/main/js/scout/datepicker/DatePickerPopup.js
@@ -28,8 +28,7 @@ scout.DatePickerPopup.prototype._init = function(options) {
 scout.DatePickerPopup.prototype._render = function($parent) {
   this.picker.render($parent);
   this.$container = this.picker.$container;
-  this.$container
-    .addClass('date-picker-popup');
+  this.$container.addClass('date-picker-popup');
   this.htmlComp = this.picker.htmlComp;
   this.htmlComp.validateRoot = true;
 };

org.eclipse.scout.rt.ui.html/src/main/js/scout/datepicker/DatePickerTouchPopup.js
@@ -16,6 +16,7 @@ scout.inherits(scout.DatePickerTouchPopup, scout.TouchPopup);
 scout.DatePickerTouchPopup.prototype._init = function(options) {
   scout.DatePickerTouchPopup.parent.prototype._init.call(this, options);
   this._field.on('displayTextChanged', this._onFieldDisplayTextChanged.bind(this));
+  this._field.on('timestampChanged', this._onFieldTimestampChanged.bind(this));
 };
 
 /**
@@ -40,4 +41,10 @@ scout.DatePickerTouchPopup.prototype._onFieldDisplayTextChanged = function(event
   this._touchField.dateDisplayText = this._field.dateDisplayText;
   this._touchField.timeDisplayText = this._field.timeDisplayText;
   this._touchField.setDisplayText(event.displayText);
+};
+
+scout.DatePickerTouchPopup.prototype._onFieldTimestampChanged = function(event) {
+  // Delegate to original field
+  this._touchField.timestamp = this._field.timestamp;
+  this._touchField.timestampAsDate = this._field.timestampAsDate;
 };

commit 4ee056d2e4c7bc261ddb6e08ef635ddbb7a20ea9

ModelAdapter: Fix clone
Adapter properties were not copied anymore because cloneModel was
modified by scout.create. Concrete problem: Sub menus were not visible
anymore because childActionsw were not copied.

Bug was introduced with commit 0d68bafdbe1ea677e3a4e41ae7c219da08348871.

org.eclipse.scout.rt.ui.html/src/main/js/scout/session/ModelAdapter.js
@@ -66,6 +66,8 @@ scout.ModelAdapter.prototype._init = function(model) {
     this.remoteHandler = this.session.sendEvent.bind(this.session);
   }
 
+  // Make a copy to prevent a modification of the given object
+  model = $.extend({}, model);
   // Fill in the missing default values (has to before copying the properties, so that modelProperties considers default values as well)
   scout.defaultValues.applyTo(model);
 

org.eclipse.scout.rt.ui.html/src/main/js/scout/session/Session.js
@@ -327,8 +327,8 @@ scout.Session.prototype._processStartupResponse = function(data) {
     this.ready = true;
     $.log.info('Session initialized. Detected ' + scout.device);
     if ($.log.isDebugEnabled()) {
-      $.log.debug('size of _adapterDataCache after session has been initialized: ' + scout.objects.countProperties(this._adapterDataCache));
-      $.log.debug('size of modelAdapterRegistry after session has been initialized: ' + scout.objects.countProperties(this.modelAdapterRegistry));
+      $.log.debug('size of _adapterDataCache after session has been initialized: ' + scout.objects.countOwnProperties(this._adapterDataCache));
+      $.log.debug('size of modelAdapterRegistry after session has been initialized: ' + scout.objects.countOwnProperties(this.modelAdapterRegistry));
     }
   }.bind(this);
 
@@ -693,9 +693,9 @@ scout.Session.prototype._processSuccessResponse = function(message) {
   }
 
   if ($.log.isDebugEnabled()) {
-    var cacheSize = scout.objects.countProperties(this._adapterDataCache);
+    var cacheSize = scout.objects.countOwnProperties(this._adapterDataCache);
     $.log.debug('size of _adapterDataCache after response has been processed: ' + cacheSize);
-    cacheSize = scout.objects.countProperties(this.modelAdapterRegistry);
+    cacheSize = scout.objects.countOwnProperties(this.modelAdapterRegistry);
     $.log.debug('size of modelAdapterRegistry after response has been processed: ' + cacheSize);
   }
 };

org.eclipse.scout.rt.ui.html/src/main/js/scout/util/objects.js
@@ -11,6 +11,7 @@
 scout.objects = {
 
   /**
+   * Copies all the properties (including the ones from the prototype.) from dest to source
    * @memberOf scout.objects
    */
   copyProperties: function(source, dest) {
@@ -21,9 +22,22 @@ scout.objects = {
   },
 
   /**
+   * Copies the own properties (excluding the ones from the prototype) from dest to source
+   * @memberOf scout.objects
+   */
+  copyOwnProperties: function(source, dest) {
+    var propertyName;
+    for (propertyName in source) {
+      if (source.hasOwnProperty(propertyName)) {
+        dest[propertyName] = source[propertyName];
+      }
+    }
+  },
+
+  /**
    * Counts and returns the properties of a given object.
    */
-  countProperties: function(obj) {
+  countOwnProperties: function(obj) {
     var count = 0;
     for (var prop in obj) {
       if (obj.hasOwnProperty(prop)) {

org.eclipse.scout.rt.ui.html.test/src/test/js/scout/desktop/outline/OutlineSpec.js
@@ -306,7 +306,7 @@ describe("Outline", function() {
         session._processSuccessResponse(message);
 
         expect(tree._onNodeDeleted.calls.count()).toBe(39);
-        expect(scout.objects.countProperties(tree.nodesMap)).toBe(0);
+        expect(scout.objects.countOwnProperties(tree.nodesMap)).toBe(0);
       });
 
     });

org.eclipse.scout.rt.ui.html.test/src/test/js/scout/mainSpec.js
@@ -118,14 +118,14 @@ describe('main', function() {
       });
 
       it('session must be set, but adapter should not be registered', function() {
-        var oldNumProperties = scout.objects.countProperties(session.modelAdapterRegistry),
+        var oldNumProperties = scout.objects.countOwnProperties(session.modelAdapterRegistry),
           menu = scout.create('Menu', {
             parent: new scout.NullWidget(),
             session: session
           });
         expect(menu.session === session).toBe(true);
         expect(menu._register).toBe(false);
-        expect(scout.objects.countProperties(session.modelAdapterRegistry)).toBe(oldNumProperties);
+        expect(scout.objects.countOwnProperties(session.modelAdapterRegistry)).toBe(oldNumProperties);
       });
 
     });

org.eclipse.scout.rt.ui.html.test/src/test/js/scout/session/ModelAdapterSpec.js
@@ -168,7 +168,7 @@ describe('ModelAdapter', function() {
 
   describe("init", function() {
 
-    it("check if model properties are copied to adapter", function() {
+    it("copies properties to adapter", function() {
       var model = {
           foo: 6
         },
@@ -178,6 +178,19 @@ describe('ModelAdapter', function() {
       model.session = session;
       adapter.init(model);
       expect(adapter.foo).toBe(6);
+    });
+
+    it("sets default values", function() {
+      // model does not contain a property visible
+      var model = createSimpleModel('Button', session);
+      expect(model.visible).toBe(undefined);
+
+      // because visible is a default property, the property is set on the adapter
+      var adapter = createModelAdapter(model);
+      expect(adapter.visible).toBe(true);
+
+      // verify that the original model is not modified
+      expect(model.visible).toBe(undefined);
     });
 
   });
@@ -250,8 +263,8 @@ describe('ModelAdapter', function() {
     it('fires the expected event object', function() {
       firePropertyChange(false, true);
 
-      expect(scout.objects.countProperties(propertyChangeEvent.oldProperties)).toBe(1);
-      expect(scout.objects.countProperties(propertyChangeEvent.newProperties)).toBe(1);
+      expect(scout.objects.countOwnProperties(propertyChangeEvent.oldProperties)).toBe(1);
+      expect(scout.objects.countOwnProperties(propertyChangeEvent.newProperties)).toBe(1);
       expect(propertyChangeEvent.changedProperties.length).toBe(1);
 
       expect(propertyChangeEvent.oldProperties.selected).toBe(false);
@@ -264,8 +277,8 @@ describe('ModelAdapter', function() {
     // event be fired anyway?
     it('changedProperties is only set when new and old value are not equals', function() {
       firePropertyChange(true, true);
-      expect(scout.objects.countProperties(propertyChangeEvent.oldProperties)).toBe(1);
-      expect(scout.objects.countProperties(propertyChangeEvent.newProperties)).toBe(1);
+      expect(scout.objects.countOwnProperties(propertyChangeEvent.oldProperties)).toBe(1);
+      expect(scout.objects.countOwnProperties(propertyChangeEvent.newProperties)).toBe(1);
       expect(propertyChangeEvent.changedProperties.length).toBe(0);
     });
 
@@ -290,6 +303,15 @@ describe('ModelAdapter', function() {
       });
       // but not the $container property (which has been added later)
       expect(adapterClone.$container).toBe(undefined);
+    });
+
+    it('also considers default properties', function() {
+      // change a default property
+      adapter._setProperty('visible', false);
+
+      // expect that this property is correctly copied to the clone
+      var adapterClone = adapter.cloneAdapter();
+      expect(adapterClone.visible).toBe(false);
     });
 
     it('\'label\' must be recognized as model property, but not \'$container\'', function() {

org.eclipse.scout.rt.ui.html.test/src/test/js/scout/util/objectsSpec.js
@@ -12,7 +12,7 @@ describe("scout.objects", function() {
 
   describe("copyProperties", function() {
 
-    it("check if all properties are copied", function() {
+    it("copies all properties", function() {
       var dest = {}, source = {
           foo: 6,
           bar: 7
@@ -22,7 +22,7 @@ describe("scout.objects", function() {
       expect(dest.bar).toBe(7);
     });
 
-    it("check if properties from prototype are copied", function() {
+    it("copies the properties from prototype as well", function() {
       var dest = {};
       var TestConstructor = function() {
         this.foo = 6;
@@ -39,9 +39,9 @@ describe("scout.objects", function() {
 
   });
 
-  describe("countProperties", function() {
+  describe("countOwnProperties", function() {
 
-    it("check if all properties are counted", function() {
+    it("counts all own properties", function() {
       var o = {
           first: 1,
           second: 2
@@ -57,10 +57,10 @@ describe("scout.objects", function() {
       scout.objects.copyProperties(x, y);
       y.qux = 9999;
 
-      expect(scout.objects.countProperties(o)).toBe(2); // first, second
-      expect(scout.objects.countProperties(F)).toBe(1); // myProp
-      expect(scout.objects.countProperties(x)).toBe(2); // foo, bar (but not myProp or anotherProp)
-      expect(scout.objects.countProperties(y)).toBe(4); // foo, bar, anotherProp, qux (because copyProperties also copies properties from prototype)
+      expect(scout.objects.countOwnProperties(o)).toBe(2); // first, second
+      expect(scout.objects.countOwnProperties(F)).toBe(1); // myProp
+      expect(scout.objects.countOwnProperties(x)).toBe(2); // foo, bar (but not myProp or anotherProp)
+      expect(scout.objects.countOwnProperties(y)).toBe(4); // foo, bar, anotherProp, qux (because copyProperties also copies properties from prototype)
     });
 
   });