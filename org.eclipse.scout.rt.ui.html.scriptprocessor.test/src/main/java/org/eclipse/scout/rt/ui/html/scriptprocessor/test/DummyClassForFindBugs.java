d27e95d1340e76adcb2dde4d903b066cff769366
Arthur van Dorp <Arthur.vanDorp@bsi-software.com>
2016-07-12 17:07:44 +0200

    Remove intermediate fix for findbugs/sonar issue.
    See
    https://github.com/SonarQubeCommunity/sonar-findbugs/issues/33
    
    Change-Id: Icfa58e6b927f5f570fff51689fdbc6602fc25910
    Signed-off-by: Arthur van Dorp <Arthur.vanDorp@bsi-software.com>
    Reviewed-on: https://git.eclipse.org/r/77144
    Tested-by: Hudson CI

--- org.eclipse.scout.rt.ui.html.scriptprocessor.test/src/main/java/org/eclipse/scout/rt/ui/html/scriptprocessor/test/DummyClassForFindBugs.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.scout.rt.ui.html.scriptprocessor.test;
-
-import javax.annotation.Generated;
-
-@Generated(value = {"manuallyGenerated"}, comments = "Dummy class for issue https://github.com/SonarQubeCommunity/sonar-findbugs/issues/33 ")
-public class DummyClassForFindBugs {
-
-}
