eclipse-scout/scout.rt

commit 94302fe2568625341dd70304a4dfed25ba44e3ea

Table: Improve table control performance
Open and close animations of the control container are very slow on
mobile devices -> Do not validate the layout on each animation step.

Also added an active state to visualize when the user touches a table
control.

org.eclipse.scout.rt.ui.html/src/main/js/scout/table/TableFooter.js
@@ -402,6 +402,11 @@ scout.TableFooter.prototype._revalidateTableLayout = function() {
 };
 
 scout.TableFooter.prototype.openControlContainer = function(control) {
+  if (this.open) {
+    // Calling open again may resize the container -> don't return
+  }
+  this.open = true;
+
   var insets = scout.graphics.getInsets(this.$controlContainer),
     contentHeight = control.height - insets.top - insets.bottom;
 
@@ -412,34 +417,40 @@ scout.TableFooter.prototype.openControlContainer = function(control) {
     this.$controlContainer.outerHeight(0);
   }
 
+  if (this.$controlContainer.outerHeight() > control.height) {
+    // Container gets smaller -> layout first to prevent having a white area
+    this.table.invalidateLayoutTree();
+  }
+
   // open container, stop existing (close) animations before
   // use delay to make sure form is rendered and layouted with new size
   this.$controlContainer.stop(true).show().delay(1).animate({
     height: control.height
   }, {
     duration: this.rendered ? control.animateDuration : 0,
-    progress: this._revalidateTableLayout.bind(this),
     complete: function() {
       control.onControlContainerOpened();
-    }
+      this.table.invalidateLayoutTree();
+    }.bind(this)
   });
-  this.open = true;
 };
 
 scout.TableFooter.prototype.closeControlContainer = function(control) {
+  if (!this.open) {
+    return;
+  }
+  this.open = false;
+  this.table.invalidateLayoutTree();
+
   this.$controlContainer.stop(true).show().animate({
     height: 0
   }, {
     duration: control.animateDuration,
-    progress: this._revalidateTableLayout.bind(this)
+    done: function() {
+      this.$controlContainer.hide();
+      control.onControlContainerClosed();
+    }.bind(this)
   });
-
-  this.$controlContainer.promise().done(function() {
-    this.$controlContainer.hide();
-    control.onControlContainerClosed();
-  }.bind(this));
-
-  this.open = false;
 };
 
 scout.TableFooter.prototype._hideTableStatusTooltip = function() {

org.eclipse.scout.rt.ui.html/src/main/js/scout/table/TableLayout.js
@@ -41,7 +41,11 @@ scout.TableLayout.prototype.layout = function($container) {
     // Layout table footer and add size of footer (including the control content) to 'height'
     footer.revalidateLayout();
     height += scout.graphics.getSize(footer.$container).height;
-    height += scout.graphics.getSize(footer.$controlContainer).height;
+    if (footer.open) {
+      // Layout may be called when container stays open but changes its size using an animation.
+      // At that time the controlContainer has not yet the final size, therefore measuring is not possible, but not necessary anyway.
+      height += footer.selectedControl.height;
+    }
   }
   if (header) {
     height += scout.graphics.getSize(header.$container).height;

org.eclipse.scout.rt.ui.html/src/main/js/scout/table/controls/TableControl.css
@@ -31,6 +31,10 @@
     color: @table-control-hover-color;
   }
 
+  &:active > .font-icon {
+    color: @text-color;
+  }
+
   &.disabled:hover,
   &.disabled > .font-icon,
   &.disabled.selected > .font-icon {

org.eclipse.scout.rt.ui.html/src/main/js/scout/table/controls/TableControl.js
@@ -163,7 +163,7 @@ scout.TableControl.prototype.setSelected = function(selected, notifyServer, clos
   if (selected === this.selected) {
     return;
   }
-//TODO cgu: this.tableFooter can be null
+
   if (this.tableFooter && this.tableFooter.selectedControl && this.tableFooter.selectedControl !== this) {
     this.tableFooter.selectedControl.setSelected(false, true, false);
   }

commit 6628eadfe6b96102e4ca60e6d56387157d56f49d

Table: Fix footer resizing
Commit 94302fe2568625341dd70304a4dfed25ba44e3ea broke the ability to
resize the table control container using the splitter. The height of the
table data was wrong after resizing.

org.eclipse.scout.rt.ui.html/src/main/js/scout/table/TableFooter.js
@@ -409,6 +409,7 @@ scout.TableFooter.prototype.openControlContainer = function(control) {
   if (this.open) {
     // Calling open again may resize the container -> don't return
   }
+  this.opening = true;
   this.open = true;
 
   var insets = scout.graphics.getInsets(this.$controlContainer),
@@ -433,6 +434,7 @@ scout.TableFooter.prototype.openControlContainer = function(control) {
   }, {
     duration: this.rendered ? control.animateDuration : 0,
     complete: function() {
+      this.opening = false;
       control.onControlContainerOpened();
       this.table.invalidateLayoutTree();
     }.bind(this)

org.eclipse.scout.rt.ui.html/src/main/js/scout/table/TableLayout.js
@@ -41,10 +41,13 @@ scout.TableLayout.prototype.layout = function($container) {
     // Layout table footer and add size of footer (including the control content) to 'height'
     footer.revalidateLayout();
     height += scout.graphics.getSize(footer.$container).height;
-    if (footer.open) {
+    if (footer.opening) {
       // Layout may be called when container stays open but changes its size using an animation.
       // At that time the controlContainer has not yet the final size, therefore measuring is not possible, but not necessary anyway.
       height += footer.selectedControl.height;
+    } else if (footer.open) {
+      // Measure the real height
+      height += scout.graphics.getSize(footer.$controlContainer).height;
     }
   }
   if (header) {
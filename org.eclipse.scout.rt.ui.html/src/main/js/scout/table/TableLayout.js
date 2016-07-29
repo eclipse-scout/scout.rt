6628eadfe6b96102e4ca60e6d56387157d56f49d
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2016-07-29 17:21:43 +0200

    Table: Fix footer resizing
    Commit 94302fe2568625341dd70304a4dfed25ba44e3ea broke the ability to
    resize the table control container using the splitter. The height of the
    table data was wrong after resizing.

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/table/TableLayout.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/table/TableLayout.js
@@ -41,10 +41,13 @@ scout.TableLayout.prototype.layout = function($container) {
     // Layout table footer and add size of footer (including the control content) to 'height'
     footer.revalidateLayout();
     height += scout.graphics.getSize(footer.$container).height;
-    if (footer.open) {
+    if (footer.opening) {
       // Layout may be called when container stays open but changes its size using an animation.
       // At that time the controlContainer has not yet the final size, therefore measuring is not possible, but not necessary anyway.
       height += footer.selectedControl.height;
+    } else if (footer.open) {
+      // Measure the real height
+      height += scout.graphics.getSize(footer.$controlContainer).height;
     }
   }
   if (header) {
