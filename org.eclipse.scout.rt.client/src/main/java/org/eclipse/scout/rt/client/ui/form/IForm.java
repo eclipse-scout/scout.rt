ce0a8cc006630af74ecc65c82ed60d86ce91d28b
Beat Schwarzentrub <bsh@bsiag.com>
2016-08-31 09:02:23 +0200

    Fix JavaDoc (duplicate line)
    Change-Id: Ie5d3500dc23a063784dda20dc4dbc7f2d7d01fd6
    Reviewed-on: https://git.eclipse.org/r/80097
    Tested-by: Hudson CI
    Reviewed-by: Beat Schwarzentrub <bsh@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/80099

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/form/IForm.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/form/IForm.java
@@ -496,7 +496,6 @@ public interface IForm extends IPropertyObserver, ITypeWithSettableClassId, ISty
    * @return the display hint to control visualization of this {@link IForm}.
    * @see #DISPLAY_HINT_VIEW
    * @see #DISPLAY_HINT_POPUP_DIALOG
-   * @see #DISPLAY_HINT_POPUP_DIALOG
    * @see #DISPLAY_HINT_POPUP_WINDOW
    */
   int getDisplayHint();
@@ -505,7 +504,6 @@ public interface IForm extends IPropertyObserver, ITypeWithSettableClassId, ISty
    * Set the given hint to control visualization of this {@link IForm}.
    * <ul>
    * <li>{@link #DISPLAY_HINT_VIEW}</li>
-   * <li>{@link #DISPLAY_HINT_POPUP_DIALOG}</li>
    * <li>{@link #DISPLAY_HINT_POPUP_DIALOG}</li>
    * <li>{@link #DISPLAY_HINT_POPUP_WINDOW}</li>
    * </ul>
