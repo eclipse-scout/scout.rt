b3d61b45dd9a4c4047693e05bfe0986cb8a9a7b5
Oli Schmid <oliver.schmid@bsi-software.com>
2016-09-02 09:05:34 +0200

    Use filenames with fingerprint to make binary resource more unique.
    Change-Id: Ic6789ae5cd86a2b1af81a25126fcab96754b247f

--- org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/res/BinaryResourceMediator.java
+++ org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/res/BinaryResourceMediator.java
@@ -5,26 +5,36 @@ import java.util.Map;
 
 import org.eclipse.scout.rt.platform.resource.BinaryResource;
 import org.eclipse.scout.rt.ui.html.json.IJsonAdapter;
+import org.eclipse.scout.rt.ui.html.json.JsonBean;
 
+/**
+ * {@link BinaryResourceMediator} can be used by any {@link IBinaryResourceProvider} in order to provide binary
+ * resources defined in any child (such as {@link JsonBean} of the {@link IJsonAdapter}.
+ * <p>
+ * The child must add the binary resource by calling {@link BinaryResourceMediator#addBinaryResource(BinaryResource)}.
+ * <p>
+ * The implementation of the {@link IBinaryResourceProvider} can call
+ * {@link BinaryResourceMediator#getBinaryResourceHolder(String)} to retrieve the BinearyResourceHolder.
+ */
 public class BinaryResourceMediator {
 
   private final IJsonAdapter<?> m_jsonAdapter;
-  private Map<String, BinaryResource> m_binaryResources = new HashMap<>(0);
+  private final Map<String, BinaryResource> m_binaryResources = new HashMap<>(0);
 
   public BinaryResourceMediator(IJsonAdapter<?> jsonAdapter) {
     m_jsonAdapter = jsonAdapter;
   }
 
   public void addBinaryResource(BinaryResource binaryResource) {
-    m_binaryResources.put(binaryResource.getFilename(), binaryResource);
+    m_binaryResources.put(BinaryResourceUrlUtility.getFilenameWithFingerprint(binaryResource), binaryResource);
   }
 
   public String createUrl(BinaryResource binaryResource) {
-    return BinaryResourceUrlUtility.createDynamicAdapterResourceUrl(m_jsonAdapter, binaryResource.getFilename());
+    return BinaryResourceUrlUtility.createDynamicAdapterResourceUrl(m_jsonAdapter, binaryResource);
   }
 
-  public BinaryResourceHolder getBinaryResourceHolder(String filename) {
-    BinaryResource binaryResource = m_binaryResources.get(filename);
+  public BinaryResourceHolder getBinaryResourceHolder(String filenameWithFingerprint) {
+    BinaryResource binaryResource = m_binaryResources.get(filenameWithFingerprint);
     if (binaryResource != null) {
       return new BinaryResourceHolder(binaryResource);
     }
