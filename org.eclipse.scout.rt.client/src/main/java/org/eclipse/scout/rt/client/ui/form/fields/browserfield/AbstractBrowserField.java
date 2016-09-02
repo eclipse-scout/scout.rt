d4337a252d0e7dd286fa8a96032de63c7bf85b38
André Wegmüller <awe@bsiag.com>
2016-09-02 08:22:07 +0200

    Reverted accidentally changed filename-comparison

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/form/fields/browserfield/AbstractBrowserField.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/form/fields/browserfield/AbstractBrowserField.java
@@ -352,7 +352,7 @@ public abstract class AbstractBrowserField extends AbstractFormField implements 
       return null;
     }
     BinaryResource binaryResource = getBinaryResource();
-    if (binaryResource != null && binaryResource.getFilename().equals(filename)) {
+    if (binaryResource != null && CompareUtility.equals(binaryResource.getFilename(), filename)) {
       return binaryResource;
     }
     Set<BinaryResource> attachments = getAttachments();
