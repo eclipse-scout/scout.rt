8a1dd1fb17cbcf5b71c6e69c7d45ca2c07c38e39
André Wegmüller <awe@bsiag.com>
2016-09-02 09:07:03 +0200

    Committed change from TPU again

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/desktop/outline/pages/PageWithNodes.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/desktop/outline/pages/PageWithNodes.js
@@ -64,17 +64,19 @@ scout.PageWithNodes.prototype._loadTableData = function() {
  * @override TreeNode.js
  */
 scout.PageWithNodes.prototype.loadChildren = function() {
-  var childPages = this._createChildPages();
-  if (childPages.length > 0) {
-    this.getOutline().insertNodes(childPages, this);
-  }
-  return this.loadTableData();
+  return this._createChildPages().done(function(childPages) {
+    if (childPages.length > 0) {
+      this.getOutline().insertNodes(childPages, this);
+    }
+    this.loadTableData();
+  }.bind(this));
 };
 
 /**
  * Override this method to create child pages for this page. The default impl. returns an empty array.
- * @returns Array of child pages
+ * @return jQuery.Deferred
  */
 scout.PageWithNodes.prototype._createChildPages = function() {
-  return [];
+  return $.resolvedDeferred();
 };
+
