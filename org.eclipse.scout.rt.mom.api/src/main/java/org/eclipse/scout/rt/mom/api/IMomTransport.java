92d373f43a332f22897a322d54ea3001a7827e47
Daniel Wiehl <daniel.wiehl@bsi-software.com>
2016-09-16 18:42:13 +0200

    Create MOM transport immediately upon platform startup
    That way, a misconfiguration in the network/broker settings is detected
    early during platform startup.
    
    Change-Id: I92631d4f3cbf0bda9a4a4475c0bf3a4b498cfc14
    Reviewed-on: https://git.eclipse.org/r/81263
    Tested-by: Hudson CI
    Reviewed-by: Judith Gull <jgu@bsiag.com>

--- org.eclipse.scout.rt.mom.api/src/main/java/org/eclipse/scout/rt/mom/api/IMomTransport.java
+++ org.eclipse.scout.rt.mom.api/src/main/java/org/eclipse/scout/rt/mom/api/IMomTransport.java
@@ -1,14 +1,19 @@
 package org.eclipse.scout.rt.mom.api;
 
 import org.eclipse.scout.rt.platform.ApplicationScoped;
+import org.eclipse.scout.rt.platform.CreateImmediately;
 
 /**
  * Marker interface for a {@link IMom} initialized with a transport to connect to a messaging system like a network or
  * broker, and which is used by the application to send and receive messages.
+ * <p>
+ * The transport is a singleton and connects to the messaging system immediately upon platform startup. That way, a
+ * misconfiguration in the network/broker settings is detected early during platform startup.
  *
  * @see IMom
  * @since 6.1
  */
 @ApplicationScoped
+@CreateImmediately
 public interface IMomTransport {
 }
