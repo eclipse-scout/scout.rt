0a909128c837b0b12c983c1bddf189466945fec4
Beat Schwarzentrub <bsh@bsiag.com>
2016-09-23 15:24:20 +0200

    Fix Jasmine tests (dialog layout)
    Change-Id: I6313a92d2f97c6c61ac6e8f1348b579696016b41

--- org.eclipse.scout.rt.ui.html.test/src/test/js/scout/form/fields/groupbox/GroupBoxSpec.js
+++ org.eclipse.scout.rt.ui.html.test/src/test/js/scout/form/fields/groupbox/GroupBoxSpec.js
@@ -72,9 +72,12 @@ describe("GroupBox", function() {
     });
 
     it('adds group-box div when label is set', function() {
+      var $tmpStyle = $('<style type="text/css">.dialog { position: absolute; }</style>')
+        .appendTo($('head'));
       session.desktop.formController._renderDialog(form);
       expect(form.rootGroupBox.$container.cssHeight()).toBe(123);
       expect(form.rootGroupBox.$container.cssWidth()).toBe(97);
+      $tmpStyle.remove();
     });
 
   });
