2631f0a3c6193f69d75f3966d646598722375fd0
Beat Schwarzentrub <bsh@bsiag.com>
2016-11-28 19:28:40 +0100

    AbstractMomDelegate: ensure m_delegate is initialized when used
    188851
    
    Change-Id: I2a1be732236bde0ef0a5157919b4d8f09d54569b
    Reviewed-on: https://git.eclipse.org/r/85938
    Tested-by: Hudson CI
    Reviewed-by: Beat Schwarzentrub <bsh@bsiag.com>

--- org.eclipse.scout.rt.mom.api/src/main/java/org/eclipse/scout/rt/mom/api/AbstractMomDelegate.java
+++ org.eclipse.scout.rt.mom.api/src/main/java/org/eclipse/scout/rt/mom/api/AbstractMomDelegate.java
@@ -1,10 +1,11 @@
 package org.eclipse.scout.rt.mom.api;
 
-import javax.annotation.PostConstruct;
+import java.util.concurrent.Callable;
 
 import org.eclipse.scout.rt.mom.api.encrypter.IEncrypter;
 import org.eclipse.scout.rt.mom.api.marshaller.IMarshaller;
 import org.eclipse.scout.rt.platform.context.RunContext;
+import org.eclipse.scout.rt.platform.util.FinalValue;
 import org.eclipse.scout.rt.platform.util.IRegistrationHandle;
 
 /**
@@ -15,50 +16,69 @@ import org.eclipse.scout.rt.platform.util.IRegistrationHandle;
  */
 public abstract class AbstractMomDelegate implements IMom {
 
-  protected IMom m_delegate;
+  private final FinalValue<IMom> m_delegate = new FinalValue<>();
 
-  @PostConstruct
-  public final void init() throws Exception {
-    m_delegate = initDelegate();
-  }
-
-  @Override
-  public <DTO> void publish(final IDestination<DTO> destination, final DTO transferObject, final PublishInput input) {
-    m_delegate.publish(destination, transferObject, input);
-  }
-
-  @Override
-  public <DTO> ISubscription subscribe(final IDestination<DTO> destination, final IMessageListener<DTO> listener, final RunContext runContext, final int acknowledgementMode) {
-    return m_delegate.subscribe(destination, listener, runContext, acknowledgementMode);
-  }
-
-  @Override
-  public <REQUEST, REPLY> REPLY request(final IBiDestination<REQUEST, REPLY> destination, final REQUEST requestObject, final PublishInput input) {
-    return m_delegate.request(destination, requestObject, input);
-  }
-
-  @Override
-  public <REQUEST, REPLY> ISubscription reply(final IBiDestination<REQUEST, REPLY> destination, final IRequestListener<REQUEST, REPLY> listener, final RunContext runContext) {
-    return m_delegate.reply(destination, listener, runContext);
-  }
-
-  @Override
-  public IRegistrationHandle registerMarshaller(final IDestination<?> destination, final IMarshaller marshaller) {
-    return m_delegate.registerMarshaller(destination, marshaller);
-  }
-
-  @Override
-  public IRegistrationHandle registerEncrypter(final IDestination<?> destination, final IEncrypter encrypter) {
-    return m_delegate.registerEncrypter(destination, encrypter);
-  }
-
-  @Override
-  public void destroy() {
-    m_delegate.destroy();
+  /**
+   * @return the MOM delegate. The delegate is initialized <i>synchronously</i> when this method is first called (i.e.
+   *         this call may block until the delegate is initialized).
+   */
+  protected IMom getDelegate() {
+    if (!m_delegate.isSet()) {
+      synchronized (m_delegate) {
+        m_delegate.setIfAbsent(new Callable<IMom>() {
+          @Override
+          public IMom call() throws Exception {
+            return initDelegate();
+          }
+        });
+      }
+    }
+    return m_delegate.get();
   }
 
   /**
    * Initializes this {@link IMom} delegate.
+   * <p>
+   * <h3>Warning: risk of deadlocks!</h3>
+   * <p>
+   * This method is called from within a <i>synchronized</i> block inside {@link #getDelegate()}. The implementation
+   * must <b>not</b> call (directly or indirectly) any other methods on {@link AbstractMomDelegate}. Be very cautious
+   * when calling other code.
    */
   protected abstract IMom initDelegate() throws Exception; // NOSONAR
+
+  @Override
+  public <DTO> void publish(final IDestination<DTO> destination, final DTO transferObject, final PublishInput input) {
+    getDelegate().publish(destination, transferObject, input);
+  }
+
+  @Override
+  public <DTO> ISubscription subscribe(final IDestination<DTO> destination, final IMessageListener<DTO> listener, final RunContext runContext, final int acknowledgementMode) {
+    return getDelegate().subscribe(destination, listener, runContext, acknowledgementMode);
+  }
+
+  @Override
+  public <REQUEST, REPLY> REPLY request(final IBiDestination<REQUEST, REPLY> destination, final REQUEST requestObject, final PublishInput input) {
+    return getDelegate().request(destination, requestObject, input);
+  }
+
+  @Override
+  public <REQUEST, REPLY> ISubscription reply(final IBiDestination<REQUEST, REPLY> destination, final IRequestListener<REQUEST, REPLY> listener, final RunContext runContext) {
+    return getDelegate().reply(destination, listener, runContext);
+  }
+
+  @Override
+  public IRegistrationHandle registerMarshaller(final IDestination<?> destination, final IMarshaller marshaller) {
+    return getDelegate().registerMarshaller(destination, marshaller);
+  }
+
+  @Override
+  public IRegistrationHandle registerEncrypter(final IDestination<?> destination, final IEncrypter encrypter) {
+    return getDelegate().registerEncrypter(destination, encrypter);
+  }
+
+  @Override
+  public void destroy() {
+    getDelegate().destroy();
+  }
 }
