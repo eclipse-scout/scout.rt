eclipse-scout/scout.rt

commit ced9d48c8acbf09d41c32b90cf734ae3c1ca83f8

ValidateLayout checks for invisible components (recursively)
see also https://git.eclipse.org/r/#/c/84911/ and
http://git.eclipse.org/c/scout/org.eclipse.scout.rt.git/commit/?h=releases/6.0.x&id=0ee0d8e7de2d4c47b510ea5ba8d57f0f4dbdb826

Tests:
- Local Jasmine and Selenium Run
- Set GridData programmatically
- Scroll with open DatePicker in editable table

Change-Id: I65c2c9d81d3b845ea9da04fb48b6c65b99d7562f
Signed-off-by: Michael Schmuki <michael.schmuki@bsiag.com>
Reviewed-on: https://git.eclipse.org/r/86367
Reviewed-by: Claudio Guglielmo <claudio.guglielmo@bsiag.com>
Tested-by: Claudio Guglielmo <claudio.guglielmo@bsiag.com>

org.eclipse.scout.rt.ui.html/src/main/js/scout/layout/LayoutValidator.js
@@ -70,7 +70,8 @@ scout.LayoutValidator.prototype.validate = function() {
   clearTimeout(this._validateTimeoutId);
   this._validateTimeoutId = null;
   this._invalidComponents.slice().forEach(function(comp) {
-    if (comp.isAttached()) { // don't layout components which don't exist anymore or are detached from the DOM
+    // don't layout components which don't exist anymore or are detached from the DOM
+    if (comp.isAttachedAndVisible() && comp.$comp.isEveryParentVisible()) { 
       comp.validateLayout();
       scout.arrays.remove(this._invalidComponents, comp);
     }

org.eclipse.scout.rt.ui.html/src/main/js/scout/form/fields/FormField.js
@@ -385,7 +385,12 @@ scout.FormField.prototype._renderLabelBackgroundColor = function() {
 };
 
 scout.FormField.prototype._renderGridData = function() {
-  this.invalidateLayoutTree();
+  if (this.rendered) {
+    var htmlCompParent = this.htmlComp.getParent();
+    if (htmlCompParent) { // may be null if $container is detached
+      this.htmlComp.getParent().invalidateLayoutTree();
+    }
+  }
 };
 
 scout.FormField.prototype._renderMenus = function() {

org.eclipse.scout.rt.ui.html/src/main/js/scout/table/TableLayout.js
@@ -27,11 +27,6 @@ scout.TableLayout.prototype.layout = function($container) {
     containerSize = htmlContainer.getAvailableSize()
     .subtract(htmlContainer.getInsets());
 
-  //FIXME CGU/AWE remove this check as soon as HtmlComp.validateLayout checks for invisible components
-  if (!htmlContainer.isAttachedAndVisible() || !htmlContainer.$comp.isEveryParentVisible()) {
-    return;
-  }
-
   if (menuBar.visible) {
     menuBarSize = scout.MenuBarLayout.size(htmlMenuBar, containerSize);
     htmlMenuBar.setSize(menuBarSize);

org.eclipse.scout.rt.ui.html/src/main/js/scout/tree/TreeLayout.js
@@ -17,11 +17,6 @@ scout.inherits(scout.TreeLayout, scout.AbstractLayout);
 scout.TreeLayout.prototype.layout = function($container) {
   var htmlContainer = this.tree.htmlComp;
 
-  //FIXME CGU/AWE remove this check as soon as HtmlComp.validateLayout checks for invisible components
-  if (!htmlContainer.isAttachedAndVisible() || !htmlContainer.$comp.isEveryParentVisible()) {
-    return;
-  }
-
   this._layout($container);
   scout.scrollbars.update(this.tree.$data);
 };

commit 416e93479fe586a0cc4ce06cedc912e091825c5d

Layout: Fix exception introduced with ced9d48
Exception occurred because validateLayout was called for proposalChooser
inside an invisible smart field popup. This happened because
layoutValidator was not used due to the call of validateLayout instead
of validateLayoutTree (smartfield popup is invisible in editable table
if not displayed in viewport)

Fixed by moving the invisible check to the HtmlComponent -> Makes the
layout validator optional again.

See ced9d48c8acbf09d41c32b90cf734ae3c1ca83f8

org.eclipse.scout.rt.ui.html/src/main/js/scout/layout/HtmlComponent.js
@@ -26,6 +26,11 @@ scout.HtmlComponent = function($comp, session) {
   this.layouted = false;
 
   /**
+   * Flag to indicate that the component is being layouted.
+   */
+  this.layouting = false;
+
+  /**
    * May be set to temporarily disable invalidation (e.g. if the component gets modified during the layouting process)
    */
   this.suppressInvalidate = false;
@@ -97,10 +102,22 @@ scout.HtmlComponent.prototype.invalidateLayout = function() {
 /**
  * Calls the layout of the component to layout its children but only if the component is not valid.
  * @exception when component has no layout
+ * @return true if validation was successful, false if it could not be executed (e.g. because the element is invisible or detached)
  */
 scout.HtmlComponent.prototype.validateLayout = function() {
   if (!this.layout) {
     throw new Error('Called layout() but component has no layout');
+  }
+  // Don't layout components which don't exist anymore, are invisible or are detached from the DOM
+  if (!this.isAttachedAndVisible()) {
+    return false;
+  }
+  var parent = this.getParent();
+  // Check the visibility of the parents as well.
+  // This check loops to the top of the DOM tree.
+  // To improve performance, the check (isEveryParentVisible) is not executed if the parent already did it, which is the case if parent is being layouted.
+  if ((!parent || !parent.layouting) && !this.$comp.isEveryParentVisible()) {
+    return false;
   }
   if (!this.valid) {
     this.layouting = true;
@@ -111,6 +128,7 @@ scout.HtmlComponent.prototype.validateLayout = function() {
     this.size = this.getSize();
     this.valid = true;
   }
+  return true;
 };
 
 /**

org.eclipse.scout.rt.ui.html/src/main/js/scout/layout/LayoutValidator.js
@@ -70,9 +70,7 @@ scout.LayoutValidator.prototype.validate = function() {
   clearTimeout(this._validateTimeoutId);
   this._validateTimeoutId = null;
   this._invalidComponents.slice().forEach(function(comp) {
-    // don't layout components which don't exist anymore or are detached from the DOM
-    if (comp.isAttachedAndVisible() && comp.$comp.isEveryParentVisible()) { 
-      comp.validateLayout();
+    if (comp.validateLayout()) {
       scout.arrays.remove(this._invalidComponents, comp);
     }
   }, this);

org.eclipse.scout.rt.ui.html/src/main/js/scout/menu/menubar/MenuBar.js
@@ -178,6 +178,7 @@ scout.MenuBar.prototype.setMenuItems = function(menuItems) {
       return !elem.rendered;
     });
     if (!sameMenuItems || hasUnrenderedMenuItems) {
+      this.updateVisibility();
       this.rebuildItems(); // Re-layout menubar
     } else {
       // Don't rebuild menubar, but update "markers"

org.eclipse.scout.rt.ui.html.test/src/test/js/scout/layout/HtmlComponentSpec.js
@@ -171,4 +171,53 @@ describe("HtmlComponent", function() {
 
   });
 
+  describe("validateLayout", function() {
+    var $comp;
+    var $child;
+    var htmlComp;
+    var htmlChild;
+
+    beforeEach(function() {
+      $comp = $('<div>').appendTo(session.$entryPoint);
+      $child = $comp.appendDiv();
+      htmlComp = scout.HtmlComponent.install($comp, session);
+      htmlChild = scout.HtmlComponent.install($child, session);
+    });
+
+    it("calls htmlComp.layout", function() {
+      spyOn(htmlComp.layout, 'layout').and.callThrough();
+      htmlComp.validateLayout();
+      expect(htmlComp.layout.layout).toHaveBeenCalled();
+    });
+
+    it("calls layout of the child component", function() {
+      spyOn(htmlChild.layout, 'layout').and.callThrough();
+      htmlComp.validateLayout();
+      expect(htmlChild.layout.layout).toHaveBeenCalled();
+    });
+
+    it("does not layout invisible components", function() {
+      $comp.setVisible(false);
+      spyOn(htmlComp.layout, 'layout').and.callThrough();
+      htmlComp.validateLayout();
+      expect(htmlComp.layout.layout).not.toHaveBeenCalled();
+    });
+
+    it("does not layout components with an invisible parent", function() {
+      $comp.setVisible(false);
+      spyOn(htmlChild.layout, 'layout').and.callThrough();
+      htmlComp.validateLayout();
+      expect(htmlChild.layout.layout).not.toHaveBeenCalled();
+    });
+
+    it("does not call isParentVisible too many times", function() {
+      spyOn(htmlComp.$comp, 'isEveryParentVisible').and.callThrough();
+      spyOn(htmlChild.$comp, 'isEveryParentVisible').and.callThrough();
+      htmlComp.validateLayout();
+      expect(htmlComp.$comp.isEveryParentVisible).toHaveBeenCalled();
+      expect(htmlChild.$comp.isEveryParentVisible).not.toHaveBeenCalled();
+    });
+
+  });
+
 });

org.eclipse.scout.rt.ui.html.test/src/test/js/scout/layout/LayoutValidatorSpec.js
@@ -74,32 +74,32 @@ describe("LayoutValidator", function() {
 
   describe("validate", function() {
 
-    it("calls validateLayout for each invalid html component", function() {
+    it("calls layout for each invalid html component", function() {
       var $comp = $('<div>').appendTo(session.$entryPoint);
       var htmlComp = scout.HtmlComponent.install($comp, session);
-      spyOn(htmlComp, 'validateLayout');
+      spyOn(htmlComp.layout, 'layout');
 
       htmlComp.invalidateLayoutTree();
       session.layoutValidator.validate();
-      expect(htmlComp.validateLayout).toHaveBeenCalled();
-      expect(htmlComp.validateLayout.calls.count()).toEqual(1);
+      expect(htmlComp.layout.layout).toHaveBeenCalled();
+      expect(htmlComp.layout.layout.calls.count()).toEqual(1);
     });
 
-    it("does not call validateLayout if component has been removed", function() {
+    it("does not call layout if component has been removed", function() {
       var $comp = $('<div>').appendTo(session.$entryPoint);
       var htmlComp = scout.HtmlComponent.install($comp, session);
-      spyOn(htmlComp, 'validateLayout');
+      spyOn(htmlComp.layout, 'layout');
 
       htmlComp.invalidateLayoutTree();
       $comp.remove();
       session.layoutValidator.validate();
-      expect(htmlComp.validateLayout).not.toHaveBeenCalled();
+      expect(htmlComp.layout.layout).not.toHaveBeenCalled();
     });
 
-    it("does not call validateLayout if component has been detached, but does not remove from invalid components either", function() {
+    it("does not call layout if component has been detached, but does not remove from invalid components either", function() {
       var $comp = $('<div>').appendTo(session.$entryPoint);
       var htmlComp = scout.HtmlComponent.install($comp, session);
-      spyOn(htmlComp, 'validateLayout');
+      spyOn(htmlComp.layout, 'layout');
 
       htmlComp.invalidateLayoutTree();
       expect(session.layoutValidator._invalidComponents.length).toBe(1);
@@ -107,7 +107,7 @@ describe("LayoutValidator", function() {
 
       $comp.detach();
       session.layoutValidator.validate();
-      expect(htmlComp.validateLayout).not.toHaveBeenCalled();
+      expect(htmlComp.layout.layout).not.toHaveBeenCalled();
       expect(session.layoutValidator._invalidComponents.length).toBe(1);
       expect(session.layoutValidator._invalidComponents[0]).toBe(htmlComp);
     });
@@ -115,15 +115,15 @@ describe("LayoutValidator", function() {
     it("removes the component from the list of invalidate components after validation", function() {
       var $comp = $('<div>').appendTo(session.$entryPoint);
       var htmlComp = scout.HtmlComponent.install($comp, session);
-      spyOn(htmlComp, 'validateLayout');
+      spyOn(htmlComp.layout, 'layout');
 
       htmlComp.invalidateLayoutTree();
       expect(session.layoutValidator._invalidComponents.length).toBe(1);
       expect(session.layoutValidator._invalidComponents[0]).toBe(htmlComp);
 
       session.layoutValidator.validate();
-      expect(htmlComp.validateLayout).toHaveBeenCalled();
-      expect(htmlComp.validateLayout.calls.count()).toEqual(1);
+      expect(htmlComp.layout.layout).toHaveBeenCalled();
+      expect(htmlComp.layout.layout.calls.count()).toEqual(1);
       expect(session.layoutValidator._invalidComponents.length).toBe(0);
     });
 