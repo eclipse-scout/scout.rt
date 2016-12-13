d0e14074a8c0a46bf149585f6408fdbea8bce36e
Beat Schwarzentrub <bsh@bsiag.com>
2016-12-13 14:40:57 +0100

    Add marker interface IMomDestinations, add default for BiDestinations
    Change-Id: Ia4c7faf2557dc855d670c82658be0d5e21d5d710

---
+++ org.eclipse.scout.rt.mom.api/src/main/java/org/eclipse/scout/rt/mom/api/IMomDestinations.java
@@ -0,0 +1,7 @@
+package org.eclipse.scout.rt.mom.api;
+
+/**
+ * Marker interface to correlate all interfaces that define destinations for {@link MOM}.
+ */
+public interface IMomDestinations {
+}
--- org.eclipse.scout.rt.server/src/main/java/org/eclipse/scout/rt/server/mom/IClusterMomDestinations.java
+++ org.eclipse.scout.rt.server/src/main/java/org/eclipse/scout/rt/server/mom/IClusterMomDestinations.java
@@ -6,10 +6,12 @@ package org.eclipse.scout.rt.server.mom;
 
 import org.eclipse.scout.rt.mom.api.AbstractDestinationConfigProperty;
 import org.eclipse.scout.rt.mom.api.ClusterMom;
+import org.eclipse.scout.rt.mom.api.IBiDestination;
 import org.eclipse.scout.rt.mom.api.IDestination;
 import org.eclipse.scout.rt.mom.api.IDestination.DestinationType;
 import org.eclipse.scout.rt.mom.api.IDestination.IDestinationType;
 import org.eclipse.scout.rt.mom.api.IDestination.ResolveMethod;
+import org.eclipse.scout.rt.mom.api.IMomDestinations;
 import org.eclipse.scout.rt.mom.api.MOM;
 import org.eclipse.scout.rt.platform.ApplicationScoped;
 import org.eclipse.scout.rt.platform.config.CONFIG;
@@ -20,7 +22,7 @@ import org.eclipse.scout.rt.server.services.common.clustersync.IClusterNotificat
  *
  * @since 6.1
  */
-public interface IClusterMomDestinations {
+public interface IClusterMomDestinations extends IMomDestinations {
 
   /**
    * Topic to transfer cluster notifications, or <code>null</code> if not configured in <i>'config.properties'</i>.
@@ -36,6 +38,10 @@ public interface IClusterMomDestinations {
 
     public <DTO> IDestination<DTO> newDestination(String name, IDestinationType type) {
       return MOM.newDestination(name, type, ResolveMethod.DEFINE, null);
+    }
+
+    public <REQUEST, REPLY> IBiDestination<REQUEST, REPLY> newBiDestination(String name, IDestinationType type) {
+      return MOM.newBiDestination(name, type, ResolveMethod.DEFINE, null);
     }
   }
 
