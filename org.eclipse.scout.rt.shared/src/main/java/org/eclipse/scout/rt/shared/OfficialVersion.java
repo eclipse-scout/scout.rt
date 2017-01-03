d235cbc64972bc76d8810c9a884274f6c125525f
Patrick Bänziger <patrick.baenziger@bsi-software.com>
2017-01-03 11:28:28 +0100

    Update version to 6.0.300 for future SR3
    Change-Id: Ie3dca41e0b40edac52011c5f1e3d0366f7d084c4
    Signed-off-by: Patrick Bänziger <patrick.baenziger@bsi-software.com>
    Reviewed-on: https://git.eclipse.org/r/87922
    Tested-by: Hudson CI

--- org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/OfficialVersion.java
+++ org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/OfficialVersion.java
@@ -17,7 +17,7 @@ public final class OfficialVersion {
   private OfficialVersion() {
   }
 
-  public static final String VERSION = "6.0.200";
+  public static final String VERSION = "6.0.300";
 
   public static final String COPYRIGHT = "Scout " + VERSION + ", &copy; BSI Business Systems Integration AG " + 2001 + "," + Calendar.getInstance().get(Calendar.YEAR) + " EPL";
 
