b2149b06fc2daae72a32da7d472a085576a63772
Matthias Villiger <mvi@bsi-software.com>
2017-04-10 11:55:19 +0200

    Upgrade HelloWorldFormTest to new Mockito

--- scout-helloworld-app/src/main/resources/archetype-resources/__rootArtifactId__.client/src/test/java/client/helloworld/HelloWorldFormTest.java
+++ scout-helloworld-app/src/main/resources/archetype-resources/__rootArtifactId__.client/src/test/java/client/helloworld/HelloWorldFormTest.java
@@ -3,25 +3,26 @@
 #set( $symbol_escape = '\' )
 package ${package}.client.helloworld;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import ${package}.client.helloworld.HelloWorldForm.MainBox.TopBox.MessageField;
+import ${package}.shared.helloworld.HelloWorldFormData;
+import ${package}.shared.helloworld.IHelloWorldService;
 import org.eclipse.scout.rt.client.testenvironment.TestEnvironmentClientSession;
 import org.eclipse.scout.rt.testing.client.runner.ClientTestRunner;
 import org.eclipse.scout.rt.testing.client.runner.RunWithClientSession;
 import org.eclipse.scout.rt.testing.platform.mock.BeanMock;
 import org.eclipse.scout.rt.testing.platform.runner.RunWithSubject;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Matchers;
-import org.mockito.Mockito;
-
-import ${package}.client.helloworld.HelloWorldForm.MainBox.TopBox.MessageField;
-import ${package}.shared.helloworld.HelloWorldFormData;
-import ${package}.shared.helloworld.IHelloWorldService;
 
 /**
- * <h3>{@link HelloWorldFormTest}</h3>
- * Contains Tests for the {@link HelloWorldForm}.
+ * <h3>{@link HelloWorldFormTest}</h3> Contains Tests for the
+ * {@link HelloWorldForm}.
  *
  * @author ${userName}
  */
@@ -33,17 +34,19 @@ public class HelloWorldFormTest {
   private static final String MESSAGE_VALUE = "testData";
 
   // Register a mock service for {@link IHelloWorldService}
-  @BeanMock private IHelloWorldService m_mockSvc;
+  @BeanMock
+  private IHelloWorldService m_mockSvc;
 
   /**
-   * Return a reference {@link HelloWorldFormData} on method {@link IHelloWorldService${symbol_pound}load(HelloWorldFormData)}.
+   * Return a reference {@link HelloWorldFormData} on method
+   * {@link IHelloWorldService${symbol_pound}load(HelloWorldFormData)}.
    */
   @Before
   public void setup() {
     HelloWorldFormData result = new HelloWorldFormData();
     result.getMessage().setValue(MESSAGE_VALUE);
 
-    Mockito.when(m_mockSvc.load(Matchers.any(HelloWorldFormData.class))).thenReturn(result);
+    when(m_mockSvc.load(any())).thenReturn(result);
   }
 
   /**
@@ -52,7 +55,7 @@ public class HelloWorldFormTest {
   @Test
   public void testMessageFieldDisabled() {
     HelloWorldForm frm = new HelloWorldForm();
-    Assert.assertFalse(frm.getMessageField().isEnabled());
+    assertFalse(frm.getMessageField().isEnabled());
   }
 
   /**
@@ -63,6 +66,6 @@ public class HelloWorldFormTest {
     HelloWorldForm frm = new HelloWorldForm();
     frm.start();
 
-    Assert.assertEquals(MESSAGE_VALUE, frm.getMessageField().getValue());
+    assertEquals(MESSAGE_VALUE, frm.getMessageField().getValue());
   }
 }
