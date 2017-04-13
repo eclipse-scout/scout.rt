b05b63cdf48b43200c89d7428ba83ea113aebf3e
Beat Schwarzentrub <bsh@bsiag.com>
2017-04-13 11:15:02 +0200

    Fix menubar layout when menubar is getting visible initially
    When menubar is invisible because all menu items are invisible, and then
    one of the menus gets visible=true, the inner menu boxes have to be
    rendered before invalidating the layout tree. Otherwise, the outer
    layouts would use the wrong preferred size of the menubar.
    
    207489
    
    Change-Id: I51d91360591c02e9f2a0e3144a32298f22dbb117
    Reviewed-on: https://git.eclipse.org/r/94986
    Tested-by: Hudson CI
    Reviewed-by: Claudio Guglielmo <claudio.guglielmo@bsiag.com>

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/menu/menubar/MenuBar.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/menu/menubar/MenuBar.js
@@ -42,10 +42,18 @@ scout.MenuBar = function() {
       if (event.changedProperties.length === 1 && event.changedProperties[0] === 'enabled') {
         this.updateDefaultMenu();
       } else if (event.changedProperties.indexOf('visible') > -1) {
+        var oldVisible = this.visible;
         this.updateVisibility();
         // Mainly necessary for menus currently not rendered (e.g. in ellipsis menu).
         // If the menu is rendered, the menu itself triggers invalidateLayoutTree (see Menu.js#_renderVisible)
         this.invalidateLayoutTree();
+        if (!oldVisible && this.visible) {
+          // If the menubar was previously invisible (because all menus were invisible) but
+          // is now visible, the menuboxes and the menus have to be rendered now. Otherwise,
+          // calculating the preferred size of the menubar, e.g. in the TableLayout, would
+          // return the wrong value (even if the menubar itself is visible).
+          this.revalidateLayout();
+        }
       }
     }
   }.bind(this);
