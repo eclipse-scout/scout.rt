bc0dc59ffc65dabcbaa904d63b565fc69cd87e98
Beat Schwarzentrub <bsh@bsiag.com>
2017-05-17 17:37:21 +0200

    Selenium: Add BrowserLogRule to capture browser logs
    Change-Id: If27427b1f334f4383d2b630d9552994c35119bb1

---
+++ org.eclipse.scout.rt.ui.html.selenium/src/main/java/org/eclipse/scout/rt/ui/html/selenium/junit/BrowserLogRule.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2014-2015 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.ui.html.selenium.junit;
+
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.eclipse.scout.rt.ui.html.selenium.util.SeleniumDriver;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+
+/**
+ * Catches browser log and prints it to STDOUT. Requires setup of selenium logging preferences, see
+ * {@link SeleniumDriver}.
+ */
+public class BrowserLogRule extends TestWatcher {
+
+  private final WebDriver m_driver;
+  private Date m_start;
+
+  public BrowserLogRule(AbstractSeleniumTest test) {
+    m_driver = test.getDriver();
+  }
+
+  @Override
+  protected void starting(Description description) {
+    m_start = new Date();
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+    System.out.println(MessageFormat.format("v | {1}.{2} [{0}]",
+        sdf.format(m_start), description.getClassName(), description.getMethodName()));
+  }
+
+  @Override
+  protected void finished(Description description) {
+    Date end = new Date();
+    long duration = end.getTime() - m_start.getTime();
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+    LogEntries logEntries = m_driver.manage().logs().get(LogType.BROWSER);
+    for (LogEntry logEntry : logEntries) {
+      System.out.println(sdf.format(new Date(logEntry.getTimestamp())) + " " + logEntry.getLevel() + " " + logEntry.getMessage());
+    }
+    System.out.println(MessageFormat.format("^ | {1}.{2} [{0}] (took {3} ms)",
+        sdf.format(end), description.getClassName(), description.getMethodName(), duration));
+  }
+}
