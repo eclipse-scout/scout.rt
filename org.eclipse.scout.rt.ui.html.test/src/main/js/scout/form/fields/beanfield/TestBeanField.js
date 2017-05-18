9ea330a91d36277883dd805ee3a91c5fffb977b1
Beat Schwarzentrub <bsh@bsiag.com>
2017-05-18 08:56:02 +0200

    BeanField: Prevent unwanted change of value via 'format -> parse'
    Under some conditions, it was possible to clear the contents of a bean
    field by the user, even though the bean field is not actually editable.
    
    The problem was caused because the value (a bean) was also used as a
    displayText, but _readDisplayText() returned the empty string. Because
    the displayText seems to have changed, acceptInput() would call
    parseAndSetValue() and therefore overwrite the existing value.
    
    Added Jasmine test for BeanField.
    
    Change-Id: I5b3c84779859309a1fb9ce507735bc8eac0fce12

---
+++ org.eclipse.scout.rt.ui.html.test/src/main/js/scout/form/fields/beanfield/TestBeanField.js
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2015 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the BSI CRM Software License v1.0
+ * which accompanies this distribution as bsi-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+scout.TestBeanField = function() {
+  scout.TestBeanField.parent.call(this);
+};
+scout.inherits(scout.TestBeanField, scout.BeanField);
+
+scout.TestBeanField.prototype._render = function() {
+  scout.TestBeanField.parent.prototype._render.call(this);
+  this.$container.addClass('test-bean-field');
+};
+
+/**
+ * @override
+ */
+scout.TestBeanField.prototype._renderValue = function() {
+  this.$field.empty();
+  if (!this.value) {
+    return;
+  }
+
+  this.$field.appendDiv('msg-from')
+    .text('Message from ' + this.value.sender);
+
+  this.$field.appendDiv('msg-text')
+    .textOrNbsp(this.value.message);
+};
