2ca3ed543de2861f6144fe0d6c539c7cff7640a7
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2017-05-19 13:58:36 +0200

    KeyStrokes: desktop keystrokes don't work anymore sometimes
    Desktop keystrokes stop working if they were pressed while a modal
    dialog was open. After that, even if the modal dialog had been closed,
    the keystrokes did not work anymore.
    
    Reason: KeyStrokeManager created a new keyStrokeContext object with
    keystrokes which are available while a modal dialog is open. This new
    keyStrokeContext replaced the closure variable and from now on the
    context was broken.
    
    Also made sure the new keyStrokeContext is of type KeyStrokeContext and
    not just object (even though this had no effect on the bug).
    
    209160

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/keystroke/KeyStrokeContext.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/keystroke/KeyStrokeContext.js
@@ -8,7 +8,7 @@
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
-scout.KeyStrokeContext = function() {
+scout.KeyStrokeContext = function(options) {
   /*
    * Holds the target where to bind this context as keydown listener.
    * This can either be a static value or a function to resolve the target.
@@ -37,6 +37,9 @@ scout.KeyStrokeContext = function() {
    * Indicates whether to invoke 'acceptInput' on a currently focused value field prior handling the keystroke.
    */
   this.invokeAcceptInputOnActiveValueField = false;
+
+  options = options || {};
+  $.extend(this, options);
 };
 
 /**
