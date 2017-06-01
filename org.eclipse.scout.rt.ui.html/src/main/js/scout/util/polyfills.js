663e0878839d795c1f0c6cf6afcaf3648a585a11
Beat Schwarzentrub <bsh@bsiag.com>
2017-06-01 10:49:17 +0200

    Make polyfills.js more readable
    Change-Id: Icca8d46b31f1c998b64920c6d9ef1926d6bb1135

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/util/polyfills.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/util/polyfills.js
@@ -23,85 +23,88 @@
 scout.polyfills = {
 
   install: function(window) {
+    this._installPolyfillFormData(window);
+    this._installPolyfillMath(window);
+  },
 
-    polyfillFormData(window);
-    polyfillMath();
-
-    function polyfillMath() {
-      if (Math.sign) {
-        return;
-      }
-
-      Math.sign = function(x) {
-        x = +x; // convert to a number
-        if (x === 0 || isNaN(x)) {
-          return x;
-        }
-        return x > 0 ? 1 : -1;
-      };
+  /**
+   * Polyfills for "Math"
+   */
+  _installPolyfillMath: function(window) {
+    if (Math.sign) {
+      return; // Nothing to do
     }
 
-    function polyfillFormData(window) {
-      if (window.FormData) {
-        // Nothing to do
-        return;
+    Math.sign = function(x) {
+      x = +x; // convert to a number
+      if (x === 0 || isNaN(x)) {
+        return x;
       }
+      return x > 0 ? 1 : -1;
+    };
+  },
 
-      window.FormData = function() {
-        this.polyfill = true; // Marker
-        this._boundary = 'ScoutFormData-' + scout.numbers.randomId(50);
-        this._data = [];
-        this._crlf = '\r\n';
-      };
-
-      window.FormData.prototype.append = function(key, value, filename) {
-        var element = [key, value, filename];
-        this._data.push(element);
-      };
-
-      window.FormData.prototype.getContent = function() {
-        var result = '';
-        this._data.forEach(function(element) {
-          var key = element[0];
-          var value = element[1];
-          var filename = element[2];
-
-          result += '--' + this._boundary + this._crlf;
-          if (scout.device.supportsFile() && value instanceof window.File) {
-            // File
-            var file = value;
-            filename = filename || file.name || key;
-            result += 'Content-Disposition: form-data; name="' + key + '"; filename="' + filename + '"' + this._crlf;
-            result += 'Content-Type: ' + file.type + this._crlf;
-            result += this._crlf;
-            result += (file.getAsBinary ? file.getAsBinary() : 'UNKNOWN CONTENT') + this._crlf;
-          } else {
-            // Everything else
-            result += 'Content-Disposition: form-data; name="' + key + '";' + this._crlf;
-            result += this._crlf;
-            result += value + this._crlf;
-          }
-        }.bind(this));
-        result += '--' + this._boundary + '--';
-        return result;
-      };
-
-      window.FormData.prototype.applyToAjaxOptions = function(ajaxOpts) {
-        if (!ajaxOpts) {
-          return;
-        }
-        // Make sure no text encoding stuff is done by xhr
-        ajaxOpts.xhr = function() {
-          var xhr = $.ajaxSettings.xhr();
-          xhr.send = xhr.sendAsBinary || xhr.send; // sendAsBinary only exists in older browsers
-          return xhr;
-        };
-        // Manually set content type (with boundary)
-        ajaxOpts.contentType = 'multipart/form-data; boundary=' + this._boundary;
-        // Generate content
-        ajaxOpts.data = this.getContent();
-      };
+  /**
+   * Polyfills for "FormData"
+   */
+  _installPolyfillFormData: function(window) {
+    if (window.FormData) {
+      return; // Nothing to do
     }
 
+    window.FormData = function() {
+      this.polyfill = true; // Marker
+      this._boundary = 'ScoutFormData-' + scout.numbers.randomId(50);
+      this._data = [];
+      this._crlf = '\r\n';
+    };
+
+    window.FormData.prototype.append = function(key, value, filename) {
+      var element = [key, value, filename];
+      this._data.push(element);
+    };
+
+    window.FormData.prototype.getContent = function() {
+      var result = '';
+      this._data.forEach(function(element) {
+        var key = element[0];
+        var value = element[1];
+        var filename = element[2];
+
+        result += '--' + this._boundary + this._crlf;
+        if (scout.device.supportsFile() && value instanceof window.File) {
+          // File
+          var file = value;
+          filename = filename || file.name || key;
+          result += 'Content-Disposition: form-data; name="' + key + '"; filename="' + filename + '"' + this._crlf;
+          result += 'Content-Type: ' + file.type + this._crlf;
+          result += this._crlf;
+          result += (file.getAsBinary ? file.getAsBinary() : 'UNKNOWN CONTENT') + this._crlf;
+        } else {
+          // Everything else
+          result += 'Content-Disposition: form-data; name="' + key + '";' + this._crlf;
+          result += this._crlf;
+          result += value + this._crlf;
+        }
+      }.bind(this));
+      result += '--' + this._boundary + '--';
+      return result;
+    };
+
+    window.FormData.prototype.applyToAjaxOptions = function(ajaxOpts) {
+      if (!ajaxOpts) {
+        return;
+      }
+      // Make sure no text encoding stuff is done by xhr
+      ajaxOpts.xhr = function() {
+        var xhr = $.ajaxSettings.xhr();
+        xhr.send = xhr.sendAsBinary || xhr.send; // sendAsBinary only exists in older browsers
+        return xhr;
+      };
+      // Manually set content type (with boundary)
+      ajaxOpts.contentType = 'multipart/form-data; boundary=' + this._boundary;
+      // Generate content
+      ajaxOpts.data = this.getContent();
+    };
   }
 };
