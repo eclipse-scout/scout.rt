2ad0dff52ae3ab42b146abc445c5df9862afb676
Beat Schwarzentrub <bsh@bsiag.com>
2017-06-01 11:25:40 +0200

    graphics: Fix Rectangle.subtract(), add floor/ceil functions
    - When subtracting insets from a rectangle, the width and height gets
      smaller on both sides.
    - Added floor() and ceil() functions for all helper classes (Point,
      Dimension, Rectangle, Insets). Ordered methods consistently.
    - Added some jasmine tests.
    - Moved scout.TreeSet to own *.js file in "util" package.
    
    Change-Id: I44606cb34dac634635ff851d9230650df20613f9

--- org.eclipse.scout.rt.ui.html.test/src/test/js/scout/layout/graphicsSpec.js
+++ org.eclipse.scout.rt.ui.html.test/src/test/js/scout/layout/graphicsSpec.js
@@ -8,28 +8,10 @@
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
-describe("scout.Dimension", function() {
+describe("graphics", function() {
 
   beforeEach(function() {
     setFixtures(sandbox());
-  });
-
-  describe("Ctor", function() {
-
-    it("accepts two numbers as width and height arguments", function() {
-      var dim = new scout.Dimension(6, 7);
-      expect(dim.width).toBe(6);
-      expect(dim.height).toBe(7);
-    });
-
-    it("accepts a single scout.Dimension argument", function() {
-      var dim1 = new scout.Dimension(6, 7);
-      var dim2 = new scout.Dimension(dim1);
-      expect(dim2.width).toBe(6);
-      expect(dim2.height).toBe(7);
-      expect(dim1).toEqual(dim2);
-    });
-
   });
 
   describe("getBounds", function() {
@@ -97,6 +79,98 @@ describe("scout.Dimension", function() {
       var rect = scout.graphics.bounds($div, true);
       expect(rect.width).toBe(8 + 2 * 10);
       expect(rect.height).toBe(9 + 2 * 10);
+    });
+
+  });
+
+  describe("Point", function() {
+
+    it("equals", function() {
+      var p1 = new scout.Point(10, 5);
+      var p2 = new scout.Point(20, -1);
+      var p3 = new scout.Point(20, -1);
+      var p4 = new scout.Point(10.2, 5.9);
+
+      expect(p1.equals(p2)).toBe(false);
+      expect(p2.equals(p3)).toBe(true);
+      expect(p1.equals(p4)).toBe(false);
+      expect(p1.equals(p4.floor())).toBe(true);
+      expect(p1.equals(p4.ceil())).toBe(false);
+    });
+
+  });
+
+  describe("Dimension", function() {
+
+    it("accepts two numbers as width and height arguments", function() {
+      var dim = new scout.Dimension(6, 7);
+      expect(dim.width).toBe(6);
+      expect(dim.height).toBe(7);
+    });
+
+    it("accepts a single scout.Dimension argument", function() {
+      var dim1 = new scout.Dimension(6, 7);
+      var dim2 = new scout.Dimension(dim1);
+      expect(dim2.width).toBe(6);
+      expect(dim2.height).toBe(7);
+      expect(dim1).toEqual(dim2);
+    });
+
+    it("equals", function() {
+      var d1 = new scout.Dimension(10, 5);
+      var d2 = new scout.Dimension(20, 20);
+      var d3 = new scout.Dimension(d2);
+      var d4 = new scout.Dimension(10.2, 5.9);
+
+      expect(d1.equals(d2)).toBe(false);
+      expect(d2.equals(d3)).toBe(true);
+      expect(d1.equals(d4)).toBe(false);
+      expect(d1.equals(d4.floor())).toBe(true);
+      expect(d1.equals(d4.ceil())).toBe(false);
+    });
+
+  });
+
+  describe("Rectangle", function() {
+
+    it("equals", function() {
+      var r1 = new scout.Rectangle(0, 0, 10, 5);
+      var r2 = new scout.Rectangle(0, 0, 20, -1);
+      var r3 = new scout.Rectangle(0, 0, 20, -1);
+      var r4 = new scout.Rectangle(0.5, 0.1, 10.2, 5.9);
+      var r5 = new scout.Rectangle(14, 15, 10, 5);
+      var r6 = new scout.Rectangle(14, 15, 20, -1);
+
+      expect(r1.equals(r2)).toBe(false);
+      expect(r2.equals(r3)).toBe(true);
+      expect(r1.equals(r4)).toBe(false);
+      expect(r1.equals(r4.floor())).toBe(true);
+      expect(r1.equals(r4.ceil())).toBe(false);
+      expect(r1.equals(r5)).toBe(false);
+      expect(r2.equals(r6)).toBe(false);
+      expect(r5.equals(r6)).toBe(false);
+    });
+
+  });
+
+  describe("Insets", function() {
+
+    it("equals", function() {
+      var i1 = new scout.Insets(0, 0, 10, 5);
+      var i2 = new scout.Insets(0, 0, 20, -1);
+      var i3 = new scout.Insets(0, 0, 20, -1);
+      var i4 = new scout.Insets(0.5, 0.1, 10.2, 5.9);
+      var i5 = new scout.Insets(14, 15, 10, 5);
+      var i6 = new scout.Insets(14, 15, 20, -1);
+
+      expect(i1.equals(i2)).toBe(false);
+      expect(i2.equals(i3)).toBe(true);
+      expect(i1.equals(i4)).toBe(false);
+      expect(i1.equals(i4.floor())).toBe(true);
+      expect(i1.equals(i4.ceil())).toBe(false);
+      expect(i1.equals(i5)).toBe(false);
+      expect(i2.equals(i6)).toBe(false);
+      expect(i5.equals(i6)).toBe(false);
     });
 
   });
