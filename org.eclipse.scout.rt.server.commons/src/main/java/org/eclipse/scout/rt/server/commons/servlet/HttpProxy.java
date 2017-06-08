11fde8bbac5f702cfc4aafd60ebbe53c110d3a41
Matthias Otterbach <matthias.otterbach@bsi-software.com>
2017-06-08 16:47:58 +0200

    Use Apache HTTP client for servlet based HttpProxy.
    Change-Id: I62bdd02feeeea13d4ebda573be6b3b73f79c2b71
    Reviewed-on: https://git.eclipse.org/r/98919
    Tested-by: Hudson CI
    Reviewed-by: Matthias Otterbach <matthias.otterbach@bsi-software.com>

--- org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/servlet/HttpProxy.java
+++ org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/servlet/HttpProxy.java
@@ -4,16 +4,15 @@
  */
 package org.eclipse.scout.rt.server.commons.servlet;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
@@ -24,12 +23,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.scout.rt.platform.BEANS;
 import org.eclipse.scout.rt.platform.Bean;
 import org.eclipse.scout.rt.platform.util.CollectionUtility;
 import org.eclipse.scout.rt.platform.util.IOUtility;
 import org.eclipse.scout.rt.platform.util.StringUtility;
+import org.eclipse.scout.rt.shared.http.DefaultHttpTransportManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpHeaders;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.InputStreamContent;
 
 /**
  * Forwards get and post requests to the given remote URL.
@@ -49,6 +56,9 @@ public class HttpProxy {
 
   @PostConstruct
   protected void initialize() {
+    // field is set by http client itself
+    m_requestHeaderFilters.add(new HttpHeaderNameFilter("Content-Length"));
+
     // remove null header from response headers
     m_responseHeaderFilters.add(new HttpHeaderNameFilter(null));
 
@@ -68,14 +78,16 @@ public class HttpProxy {
    */
   public void proxyGet(HttpServletRequest req, HttpServletResponse resp, HttpProxyOptions options) throws ServletException, IOException {
     String url = StringUtility.join("", getRemoteUrl(), req.getPathInfo(), StringUtility.box("?", req.getQueryString(), ""));
-    URLConnection connection = openConnection(url);
+    HttpRequest httpReq = BEANS.get(DefaultHttpTransportManager.class).getHttpRequestFactory().buildGetRequest(new GenericUrl(url));
+    httpReq = prepareRequest(httpReq);
 
-    writeRequestHeaders(req, connection);
-    writeCustomRequestHeaders(connection, options.getCustomRequestHeaders());
+    writeRequestHeaders(req, httpReq);
+    writeCustomRequestHeaders(httpReq, options.getCustomRequestHeaders());
+    HttpResponse httpResp = httpReq.execute();
 
-    writeResponseHeaders(resp, connection);
-    writeResponseStatus(resp, connection);
-    writeResponsePayload(resp, connection);
+    writeResponseHeaders(resp, httpResp);
+    writeResponseStatus(resp, httpResp);
+    writeResponsePayload(resp, httpResp);
     LOG.debug("Forwarded get request to " + url);
   }
 
@@ -90,33 +102,34 @@ public class HttpProxy {
    */
   public void proxyPost(HttpServletRequest req, HttpServletResponse resp, HttpProxyOptions options) throws ServletException, IOException {
     String url = StringUtility.join("", getRemoteUrl(), req.getPathInfo());
-    URLConnection connection = openConnection(url);
-    connection.setDoOutput(true);
-    connection.setDefaultUseCaches(false);
-    connection.setUseCaches(false);
+    HttpRequest httpReq = BEANS.get(DefaultHttpTransportManager.class).getHttpRequestFactory().buildPostRequest(new GenericUrl(url), null);
+    httpReq.getHeaders().setCacheControl("no-cache");
+    httpReq = prepareRequest(httpReq);
 
-    writeRequestHeaders(req, connection);
-    writeCustomRequestHeaders(connection, options.getCustomRequestHeaders());
+    writeRequestHeaders(req, httpReq);
+    writeCustomRequestHeaders(httpReq, options.getCustomRequestHeaders());
 
     // Payload is empty if parameters are used (usually with content type = application/x-www-form-urlencoded)
     // -> write parameters if there are any, otherwise write the raw payload
     if (req.getParameterMap().size() > 0) {
-      writeRequestParameters(req, connection);
+      writeRequestParameters(req, httpReq);
     }
     else {
-      writeRequestPayload(req, connection);
+      writeRequestPayload(req, httpReq);
     }
-    writeResponseHeaders(resp, connection);
-    writeResponseStatus(resp, connection);
-    writeResponsePayload(resp, connection);
+    HttpResponse httpResp = httpReq.execute();
+
+    writeResponseHeaders(resp, httpResp);
+    writeResponseStatus(resp, httpResp);
+    writeResponsePayload(resp, httpResp);
     LOG.debug("Forwarded post request to " + url);
   }
 
-  protected URLConnection openConnection(String url) throws IOException {
-    return new URL(url).openConnection();
+  protected HttpRequest prepareRequest(HttpRequest httpReq) {
+    return httpReq;
   }
 
-  protected void writeRequestHeaders(HttpServletRequest req, URLConnection connection) {
+  protected void writeRequestHeaders(HttpServletRequest req, HttpRequest httpReq) {
     Enumeration<String> headerNames = req.getHeaderNames();
     while (headerNames.hasMoreElements()) {
       String name = headerNames.nextElement();
@@ -125,7 +138,8 @@ public class HttpProxy {
         value = filter.filter(name, value);
       }
       if (value != null) {
-        connection.setRequestProperty(name, value);
+        HttpHeaders headers = httpReq.getHeaders();
+        headers.set(name, Collections.singletonList(value));
         LOG.trace("Wrote request header: {}: {}", name, value);
       }
       else {
@@ -134,28 +148,23 @@ public class HttpProxy {
     }
   }
 
-  protected void writeCustomRequestHeaders(URLConnection connection, Map<String, String> customHeaders) {
+  protected void writeCustomRequestHeaders(HttpRequest httpReq, Map<String, String> customHeaders) {
     if (customHeaders == null) {
       return;
     }
     for (Entry<String, String> header : customHeaders.entrySet()) {
-      connection.setRequestProperty(header.getKey(), header.getValue());
+      httpReq.getHeaders().set(header.getKey(), header.getValue());
       LOG.trace("Wrote custom request header: {}: {}", header.getValue(), header.getValue());
     }
   }
 
-  protected void writeRequestPayload(HttpServletRequest req, URLConnection connection) throws IOException {
-    try (OutputStream out = connection.getOutputStream()) {
-      IOUtility.writeFromToStream(out, req.getInputStream());
-    }
+  protected void writeRequestPayload(HttpServletRequest req, HttpRequest httpReq) throws IOException {
+    httpReq.setContent(new InputStreamContent(null, req.getInputStream()));
   }
 
-  protected void writeRequestParameters(HttpServletRequest req, URLConnection connection) throws IOException {
+  protected void writeRequestParameters(HttpServletRequest req, HttpRequest httpReq) throws IOException {
     String parameters = formatFormParameters(req.getParameterMap());
-
-    try (OutputStream out = connection.getOutputStream()) {
-      out.write(parameters.getBytes(StandardCharsets.UTF_8));
-    }
+    httpReq.setContent(new InputStreamContent(null, new ByteArrayInputStream(parameters.getBytes(StandardCharsets.UTF_8))));
   }
 
   protected String formatFormParameters(Map<String, String[]> parameterMap) throws UnsupportedEncodingException {
@@ -177,24 +186,14 @@ public class HttpProxy {
   /**
    * Writes the response payload of forwarded request to the servlet response.
    */
-  protected void writeResponsePayload(HttpServletResponse resp, URLConnection connection) throws IOException {
-    HttpURLConnection httpConnection = (HttpURLConnection) connection;
-    int responseCode = httpConnection.getResponseCode();
-    if (responseCode == 200) {
-      try (InputStream in = httpConnection.getInputStream()) {
-        writeResponsePayload(resp, in);
-      }
-    }
-    else {
-      try (InputStream in = httpConnection.getErrorStream()) {
-        writeResponsePayload(resp, in);
-      }
+  protected void writeResponsePayload(HttpServletResponse resp, HttpResponse httpResp) throws IOException {
+    try (InputStream in = httpResp.getContent()) {
+      writeResponsePayload(resp, in);
     }
   }
 
-  protected void writeResponseStatus(HttpServletResponse resp, URLConnection connection) throws IOException {
-    HttpURLConnection httpConnection = (HttpURLConnection) connection;
-    int responseCode = httpConnection.getResponseCode();
+  protected void writeResponseStatus(HttpServletResponse resp, HttpResponse httpResp) throws IOException {
+    int responseCode = httpResp.getStatusCode();
     resp.setStatus(responseCode);
   }
 
@@ -205,10 +204,10 @@ public class HttpProxy {
     IOUtility.writeFromToStream(resp.getOutputStream(), inputStream);
   }
 
-  protected void writeResponseHeaders(HttpServletResponse resp, URLConnection connection) {
-    for (Entry<String, List<String>> entry : connection.getHeaderFields().entrySet()) {
+  protected void writeResponseHeaders(HttpServletResponse resp, HttpResponse httpResp) {
+    for (Entry<String, Object> entry : httpResp.getHeaders().entrySet()) {
       String name = entry.getKey();
-      String value = CollectionUtility.format(entry.getValue(), ",");
+      String value = entry.getValue() instanceof Collection<?> ? CollectionUtility.firstElement(entry.getValue()).toString() : entry.getValue().toString();
       String originalValue = value;
       for (IHttpHeaderFilter filter : getResponseHeaderFilters()) {
         value = filter.filter(name, value);
