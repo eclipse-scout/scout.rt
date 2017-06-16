ecae27d9371e2590b4a7e48f89759cdb5680dd68
Beat Schwarzentrub <bsh@bsiag.com>
2017-06-16 13:03:39 +0200

    events: Add touch-related methods
    fixTouchEvent():
    Writes touch coordinates to event.pageX and event.pageY, so event
    listeners don't have to differentiate between mouse and touch events.
    This method additionally converts fractional touch coordinates to
    integer coordinates.
    
    isTouchEvent():
    Returns true if the given event is a touch event.
    
    Change-Id: Ic02b9dd8d24cc34ae56ea54c48b10a3a6be8e05c

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/util/events.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/util/events.js
@@ -51,5 +51,23 @@ scout.events = {
       return touchevent + suffix;
     }
     return mouseevent + suffix;
+  },
+
+  isTouchEvent: function(event) {
+    return event && scout.strings.startsWith(event.type, 'touch');
+  },
+
+  fixTouchEvent: function(event) {
+    if (this.isTouchEvent(event)) {
+      var touches = event.touches || (event.originalEvent ? event.originalEvent.touches : null);
+      var touch = touches ? touches[0] : null;
+      if (touch) {
+        // Touch events may contain fractional values, while mouse events should not
+        // - https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/pageX
+        // - https://www.chromestatus.com/features/6169687914184704
+        event.pageX = Math.round(touch.pageX);
+        event.pageY = Math.round(touch.pageY);
+      }
+    }
   }
 };
