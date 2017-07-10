8ea9349ad835e1315c138fcebec52ef5719a3152
André Wegmüller <awe@bsiag.com>
2017-07-10 14:08:36 +0200

    Removed unreferenced .js file from module

--- org.eclipse.scout.rt.ui.html/src/main/js/scout-module.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout-module.js
@@ -459,7 +459,6 @@
   __include("scout/form/fields/smartfield2/SmartField2Adapter.js");
   __include("scout/form/fields/smartfield2/SmartField2Layout.js");
   __include("scout/form/fields/smartfield2/SmartField2CancelKeyStroke.js");
-  __include("scout/form/fields/smartfield2/SmartField2ToggleKeyStroke.js");
   __include("scout/form/fields/smartfield2/SmartField2Popup.js");
   __include("scout/form/fields/smartfield2/SmartField2TouchPopup.js");
   __include("scout/form/fields/smartfield2/SmartField2PopupLayout.js");
