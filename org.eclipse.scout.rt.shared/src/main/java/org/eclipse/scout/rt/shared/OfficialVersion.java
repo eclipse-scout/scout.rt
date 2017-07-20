581a436b03c5d7b13eba29869bc41bbbad96ee1f
Patrick Bänziger <patrick.baenziger@bsi-software.com>
2017-07-20 12:23:51 +0200

    Initial commit for 7.1 Branch: Update all version numbers

--- org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/OfficialVersion.java
+++ org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/OfficialVersion.java
@@ -17,7 +17,7 @@ public final class OfficialVersion {
   private OfficialVersion() {
   }
 
-  public static final String VERSION = "7.0.100";
+  public static final String VERSION = "7.1.0";
 
   public static final String COPYRIGHT = "Scout " + VERSION + ", &copy; BSI Business Systems Integration AG " + 2001 + "," + Calendar.getInstance().get(Calendar.YEAR) + " EPL";
 
