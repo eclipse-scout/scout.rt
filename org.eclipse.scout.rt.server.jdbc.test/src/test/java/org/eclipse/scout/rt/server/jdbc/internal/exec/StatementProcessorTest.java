d4c195ac8cde2bf6fd2f110491a78f0f2f58340c
Matthias Villiger <mvi@bsi-software.com>
2017-07-25 10:59:56 +0200

    Add duplicate bind name check
    Log duplicate bind names. Only enabled in development mode by default.
    
    #209703
    
    Conflicts:
    	org.eclipse.scout.rt.server.jdbc/src/main/java/org/eclipse/scout/rt/server/jdbc/internal/exec/StatementProcessor.java
    
    Change-Id: I667b729c27b34dd7369e84b5ef70b8853e872229
    Reviewed-on: https://git.eclipse.org/r/101885
    Tested-by: Hudson CI
    Reviewed-by: Andi Bur <andi.bur@gmail.com>

--- org.eclipse.scout.rt.server.jdbc.test/src/test/java/org/eclipse/scout/rt/server/jdbc/internal/exec/StatementProcessorTest.java
+++ org.eclipse.scout.rt.server.jdbc.test/src/test/java/org/eclipse/scout/rt/server/jdbc/internal/exec/StatementProcessorTest.java
@@ -10,16 +10,25 @@
  ******************************************************************************/
 package org.eclipse.scout.rt.server.jdbc.internal.exec;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 
+import org.eclipse.scout.rt.platform.exception.ProcessingException;
 import org.eclipse.scout.rt.platform.holders.IntegerHolder;
 import org.eclipse.scout.rt.platform.holders.LongHolder;
 import org.eclipse.scout.rt.platform.holders.NVPair;
 import org.eclipse.scout.rt.platform.internal.BeanInstanceUtil;
 import org.eclipse.scout.rt.server.TestJdbcServerSession;
 import org.eclipse.scout.rt.server.jdbc.AbstractSqlService;
+import org.eclipse.scout.rt.server.jdbc.parsers.token.IToken;
+import org.eclipse.scout.rt.server.jdbc.parsers.token.ValueInputToken;
+import org.eclipse.scout.rt.server.jdbc.parsers.token.ValueOutputToken;
 import org.eclipse.scout.rt.shared.data.basic.table.AbstractTableRowData;
 import org.eclipse.scout.rt.shared.data.form.AbstractFormData;
 import org.eclipse.scout.rt.shared.data.form.fields.AbstractValueFieldData;
@@ -60,6 +69,93 @@ public class StatementProcessorTest {
 
     String sqlPlainTextDump = sp.createSqlDump(false, true);
     assertFalse(sqlPlainTextDump.contains("UNPARSED"));
+  }
+
+  @Test
+  public void testDuplicateBindNames() {
+    final String bind1Name = "bind1";
+    final String bind2Name = "bind2";
+    final String bindSessionName = "userId";
+    final String stmtWithInBinds = "select :" + bind1Name + ", :" + bind2Name + " from dual";
+    final String stmtWithOutBinds = "select 1, 2 from dual into :" + bind1Name + ", :" + bind2Name;
+    final String stmtWithSessionProperty = "select :" + bindSessionName + ", :" + bind1Name + " from dual";
+
+    // test in binds
+    assertArrayEquals(new String[]{}, exec(true, stmtWithInBinds, new NVPair(bind1Name, null), new NVPair(bind2Name, null)));
+    assertArrayEquals(new String[]{bind1Name}, exec(true, stmtWithInBinds, new NVPair(bind1Name, null), new NVPair(bind2Name, null), new NVPair(bind1Name, null)));
+
+    // test out binds
+    assertArrayEquals(new String[]{}, exec(true, stmtWithOutBinds, new NVPair(bind1Name, new IntegerHolder()), new NVPair(bind2Name, new IntegerHolder())));
+    assertArrayEquals(new String[]{bind1Name}, exec(true, stmtWithOutBinds, new NVPair(bind1Name, new IntegerHolder()), new NVPair(bind2Name, new IntegerHolder()), new NVPair(bind1Name, new IntegerHolder())));
+
+    // test duplicate with property from session
+    assertArrayEquals(new String[]{bindSessionName}, exec(true, stmtWithSessionProperty, new NVPair(bindSessionName, null), new NVPair(bind1Name, null)));
+
+    // test with duplicate check disabled
+    assertArrayEquals(new String[]{}, exec(false, stmtWithInBinds, new NVPair(bind1Name, null), new NVPair(bind2Name, null), new NVPair(bind1Name, null)));
+    assertArrayEquals(new String[]{}, exec(false, stmtWithOutBinds, new NVPair(bind1Name, new IntegerHolder()), new NVPair(bind2Name, new IntegerHolder()), new NVPair(bind1Name, new IntegerHolder())));
+
+    // missing input with duplicate checking
+    try {
+      exec(true, stmtWithInBinds, new NVPair(bind1Name, null));
+      fail();
+    }
+    catch (ProcessingException e) {
+      assertNotNull(e);
+    }
+
+    // missing output with duplicate checking
+    try {
+      exec(true, stmtWithOutBinds, new NVPair(bind1Name, new IntegerHolder()));
+      fail();
+    }
+    catch (ProcessingException e) {
+      assertNotNull(e);
+    }
+
+    // missing input without duplicate checking
+    try {
+      exec(false, stmtWithInBinds, new NVPair(bind1Name, null));
+      fail();
+    }
+    catch (ProcessingException e) {
+      assertNotNull(e);
+    }
+
+    // missing output without duplicate checking
+    try {
+      exec(false, stmtWithOutBinds, new NVPair(bind1Name, new IntegerHolder()));
+      fail();
+    }
+    catch (ProcessingException e) {
+      assertNotNull(e);
+    }
+  }
+
+  private String[] exec(final boolean checkForDuplicateBinds, final String statement, final Object... binds) {
+    final Collection<String> duplicates = new ArrayList<>();
+    final AbstractSqlService sqlService = new AbstractSqlService() {
+    };
+    new StatementProcessor(sqlService, statement, binds) {
+      @Override
+      protected boolean isBindDuplicateCheckEnabled() {
+        return checkForDuplicateBinds;
+      }
+
+      @Override
+      protected void onDuplicateBind(IToken token) {
+        if (token instanceof ValueInputToken) {
+          duplicates.add(((ValueInputToken) token).getName());
+        }
+        else if (token instanceof ValueOutputToken) {
+          duplicates.add(((ValueOutputToken) token).getName());
+        }
+        else {
+          throw new ProcessingException("Unexpected token: {}", token);
+        }
+      }
+    }.simulate();
+    return duplicates.toArray(new String[duplicates.size()]);
   }
 
   @Test
