ce9fccf9632ef7c23c6345ea9ab96c794459f515
Matthias Otterbach <matthias.otterbach@bsi-software.com>
2017-07-27 11:36:39 +0200

    promises.js, correct result argument handling.
    - Fail handling: If multiple arguments are supplied, all arguments must
    be provided for fail method.
    
    - Success handling: _addResults should use the same behavior for results
    as if $.when or $.promiseAll would have been used.
    
    Change-Id: I80fd8c8eac3465fac25b658a27ca7cc28c484dd1
    Reviewed-on: https://git.eclipse.org/r/102058
    Tested-by: Hudson CI
    Reviewed-by: Matthias Otterbach <matthias.otterbach@bsi-software.com>
    (cherry picked from commit 775b0147a543dadde4deebadbc02b2eac520ea01)
    Reviewed-on: https://git.eclipse.org/r/102140

---
+++ org.eclipse.scout.rt.ui.html.test/src/test/js/scout/util/promisesSpec.js
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * Copyright (c) 2014-2015 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+describe("scout.promises", function() {
+
+  function createDeferredArray(len) {
+    return scout.arrays.init(len, null);
+  }
+
+  function createPromiseCreatorForDeferredArray(deferredArray) {
+    return new scout.PromiseCreator(deferredArray.map(function(v, i) { return function() { deferredArray[i] = new $.Deferred(); return deferredArray[i]; }; }));
+  }
+
+  it("oneByOne stops executing after failure", function(done) {
+    var deferredArray = createDeferredArray(3);
+    var promiseCreator = createPromiseCreatorForDeferredArray(deferredArray);
+    scout.promises.oneByOne(promiseCreator).then(function() {
+      fail('Unexpected code branch');
+    }, function(msg) {
+      expect(msg).toBe('Foo');
+      expect(deferredArray[1]).toBeNull();
+      expect(deferredArray[2]).toBeNull();
+      done();
+    });
+    setTimeout(deferredArray[0].reject.bind(deferredArray[0], 'Foo'));
+  });
+
+  it("groupwise stops executing after failed group", function(done) {
+    var deferredArray = createDeferredArray(3);
+    var promiseCreator = createPromiseCreatorForDeferredArray(deferredArray);
+    scout.promises.groupwise(2, promiseCreator).then(function() {
+      fail('Unexpected code branch');
+    }, function(msg) {
+      expect(msg).toBe('Bar');
+      expect(deferredArray[1]).not.toBeNull();
+      expect(deferredArray[2]).toBeNull();
+      done();
+    });
+    setTimeout(deferredArray[0].reject.bind(deferredArray[0], 'Bar'));
+  });
+
+  it("does not cut off error arguments", function(done) {
+    var deferredArray = createDeferredArray(1);
+    var promiseCreator = createPromiseCreatorForDeferredArray(deferredArray);
+    scout.promises.oneByOne(promiseCreator).then(function() {
+      fail('Unexpected code branch');
+    }, function() {
+      expect(arguments).toBeTruthy();
+      expect(arguments.length).toBe(2);
+      expect(arguments[0]).toBe('Foo');
+      expect(arguments[1]).toBe('Bar');
+      done();
+    });
+    setTimeout(deferredArray[0].reject.bind(deferredArray[0], 'Foo', 'Bar'));
+  });
+
+  it("adds all result arguments, one for each deferred", function(done) {
+    var deferredArray = scout.arrays.init(3, null).map(function() { return new $.Deferred(); });
+    var promiseCreator = new scout.PromiseCreator(deferredArray.map(function(v, i) { return function() { return deferredArray[i].promise(); }; }));
+    scout.promises.oneByOne(promiseCreator).then(function() {
+      expect(arguments).toBeTruthy();
+      expect(arguments.length).toBe(3);
+      // same behavior as if multiple Deferred or Promise or Thenable objects have been used with $.when or $.promiseAll method
+      // empty argument resolve call adds an undefined to result
+      expect(arguments[0]).toBeUndefined();
+      // one argument resolve call just adds the argument to result
+      expect(arguments[1]).toBe('Foo');
+      // multiple argument resolve call adds all arguments as an array to result
+      expect(arguments[2]).toEqual(['Bar', true]);
+      done();
+    }, function(msg) {
+      fail('Unexpected code branch');
+      done();
+    });
+    deferredArray[0].resolve();
+    deferredArray[1].resolve('Foo');
+    deferredArray[2].resolve('Bar', true);
+  });
+
+});
