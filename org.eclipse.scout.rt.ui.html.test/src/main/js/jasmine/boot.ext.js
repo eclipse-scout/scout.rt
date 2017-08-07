555272d0bb0a7568b8be5d328580cb53eba8ca3a
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2017-08-07 16:16:13 +0200

    Jasmine: remove obsolete reporter
    The custom reporter is not necessary anymore since the maven plugin is
    now compatible with Jasmine 2.0

--- org.eclipse.scout.rt.ui.html.test/src/main/js/jasmine/boot.ext.js
+++ org.eclipse.scout.rt.ui.html.test/src/main/js/jasmine/boot.ext.js
@@ -9,9 +9,8 @@
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
 /*jshint -W079 */
-/*global JasmineMavenReporter*/
 
-// Since we don't use a IIF to wrap the scout code when running tests, we need to define the scout namespace here
+// Since we don't use an IIFE to wrap the scout code when running tests, we need to define the scout namespace here
 var scout = {};
 
 // Patches necessary for phantomJs
@@ -26,15 +25,5 @@ if (!Function.prototype.bind) {
     };
   };
 }
-
-// Add reporter for jasmine-maven-plugin
-// The plugin actually uses jsApiReporter but requires jasmine 1.x.
-// To make it work with jasmine 2.0 we use a custom reporter which fakes jasmine 1.x api
-var jasmineMavenReporter = new JasmineMavenReporter({
-  timer: new jasmine.Timer()
-});
-jasmine.getEnv().addReporter(jasmineMavenReporter);
-window.reporter = jasmineMavenReporter;
-
 
 $.fx.off = true;
