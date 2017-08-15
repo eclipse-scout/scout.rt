b198319cc43665f49ec5cc56757662d3f5794ee3
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2017-08-15 10:57:17 +0200

    RadioButton: remove before pseudo element
    IE breaks the layout if a radio button is toggled and a tree box layout
    invalidated. Popular example is the active/inactive radio button group
    of a tree box (see widgets examples).
    
    This is a similar issue to 71799a956fe2e0342f0aaee05ce3c94faf6935fa. To
    avoid such problems in the future and to clean up some other workarounds
    around that before element, the radio button has been refactored so that
    the circle is not drawn by a before pseudo element anymore. It is now
    implemented the same way as a checkbox.
    
    Also removed ButtonLayout because $fieldContainer and $field is the
    same. Added RadioButtonLayout instead because it is not possible to set
    the max-width using CSS due to dynamic icon width.

---
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/form/fields/radiobutton/RadioButtonLayout.js
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2014-2015 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+scout.RadioButtonLayout = function(radioButton) {
+  scout.RadioButtonLayout.parent.call(this, radioButton);
+  this.radioButton = radioButton;
+};
+scout.inherits(scout.RadioButtonLayout, scout.FormFieldLayout);
+
+scout.RadioButtonLayout.prototype.layout = function($container) {
+  scout.RadioButtonLayout.parent.prototype.layout.call(this, $container);
+
+  var $icon = this.radioButton.get$Icon(),
+    $circle = this.radioButton.$radioButton,
+    $label = this.radioButton.$buttonLabel,
+    $fieldContainer = this.radioButton.$fieldContainer;
+
+  var labelMaxWidth = $fieldContainer.width() - ($circle.outerWidth(true) + ($icon.length ? $icon.outerWidth(true) : 0));
+  $label.css('max-width', labelMaxWidth);
+};
