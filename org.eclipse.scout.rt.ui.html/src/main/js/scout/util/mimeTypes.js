9794e0bf6ce0f6d57d854d0b94bc7e9457c75e47
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2017-08-29 17:39:09 +0200

    MimeTypes: add correct mime type for java script
    Deprecated text/javascript is not deleted because it may be still used.

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/util/mimeTypes.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/util/mimeTypes.js
@@ -16,23 +16,35 @@ scout.mimeTypes = {
   TEXT_PLAIN: 'text/plain',
   TEXT_HTML: 'text/html',
   TEXT_CSS: 'text/css',
+  TEXT_JAVASCRIPT: 'text/javascript',
 
   IMAGE_PNG: 'image/png',
   IMAGE_JPG: 'image/jpg',
   IMAGE_JPEG: 'image/jpeg',
   IMAGE_GIF: 'image/gif',
 
-  JSON: 'application/json',
-  JAVASCRIPT: 'text/javascript',
-  ZIP: 'application/zip',
+  APPLICATION_JSON: 'application/json',
+  APPLICATION_JAVASCRIPT: 'application/javascript',
+  APPLICATION_ZIP: 'application/zip',
+
+  JSON: this.APPLICATION_JSON,
+  JAVASCRIPT: this.APPLICATION_JAVASCRIPT,
+  ZIP: this.APPLICATION_ZIP,
 
   isTextMimeType: function(mimeType) {
     return scout.isOneOf(mimeType,
       this.TEXT_PLAIN,
       this.TEXT_HTML,
       this.TEXT_CSS,
-      this.JAVASCRIPT,
-      this.JSON);
+      this.TEXT_JAVASCRIPT,
+      this.APPLICATION_JAVASCRIPT,
+      this.APPLICATION_JSON);
+  },
+
+  isJavaScript: function(mimeType) {
+    return scout.isOneOf(mimeType,
+      this.TEXT_JAVASCRIPT,
+      this.APPLICATION_JAVASCRIPT);
   }
 
 };
