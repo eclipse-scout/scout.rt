eclipse-scout/scout.rt

commit 4b6694638c31c80b286c8724c7f4c952183ed5ed

DesktopToolBox: invalidate layout when menu images load
The layout of the menu box in the desktop header needs to be invalidated
after an image loads, otherwise it is likely that the menu won't have
the correct size.

Introduced scout.Icon which may handle font and image icons and makes
use of scout.Image for the loading behavior (renamed existing scout.Icon
to scout.IconDesc).

The same is not possible for the menubar because layouting rebuilds
every item which would lead to a loop

212286

org.eclipse.scout.rt.ui.html/src/main/js/scout/action/Action.js
@@ -14,6 +14,7 @@ scout.Action = function() {
   this.selected = false;
   this.horizontalAlignment = -1;
   this.iconId = null;
+  this.imageLoadingInvalidatesLayout = true;
   this.tooltipText = null;
   this.text = null;
   this.cssClass = null;
@@ -77,6 +78,7 @@ scout.Action.prototype._renderProperties = function() {
 
 scout.Action.prototype._remove = function() {
   this._removeText();
+  this._removeIconId();
   scout.Action.parent.prototype._remove.call(this);
 };
 
@@ -100,9 +102,41 @@ scout.Action.prototype._removeText = function() {
   }
 };
 
+scout.Action.prototype.setIconId = function(iconId) {
+  this.setProperty('iconId', iconId);
+};
+
 scout.Action.prototype._renderIconId = function() {
   var iconId = this.iconId || '';
-  this.$container.icon(iconId);
+  if (this.imageLoadingInvalidatesLayout) {
+    // If the icon is an image (and not a font icon), the scout.Icon class will invalidate the layout when the image has loaded
+    // This may not work for every container using the action (.e.g. MenuBar), so it may be disabled
+    if (!iconId) {
+      this._removeIconId();
+      return;
+    }
+    if (this.icon) {
+      this.icon.setIconDesc(iconId);
+      return;
+    }
+    this.icon = scout.create('Icon', {
+      parent: this,
+      iconDesc: iconId,
+      prepend: true
+    });
+    this.icon.one('destroy', function() {
+      this.icon = null;
+    }.bind(this));
+    this.icon.render();
+  } else {
+    this.$container.icon(iconId);
+  }
+};
+
+scout.Action.prototype._removeIconId = function() {
+  if (this.icon) {
+    this.icon.destroy();
+  }
 };
 
 /**
@@ -127,6 +161,10 @@ scout.Action.prototype._renderKeyStroke = function() {
   }
 };
 
+scout.Action.prototype.setTooltipText = function(tooltipText) {
+  this.setProperty('tooltipText', tooltipText);
+};
+
 scout.Action.prototype._renderTooltipText = function() {
   this._updateTooltip();
 };
@@ -148,10 +186,6 @@ scout.Action.prototype._shouldInstallTooltip = function() {
 
 scout.Action.prototype._renderTabbable = function() {
   this.$container.setTabbable(this.tabbable && !scout.device.supportsTouch());
-};
-
-scout.Action.prototype._renderHorizontalAlignment = function() {
-  // nothing to render, property is only considered by the menubar
 };
 
 scout.Action.prototype._renderCompact = function() {

org.eclipse.scout.rt.ui.html.test/src/test/js/scout/image/IconSpec.js
@@ -1,0 +1,68 @@
+/*******************************************************************************
+ * Copyright (c) 2014-2015 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+describe('Icon', function() {
+
+  var session;
+
+  beforeEach(function() {
+    setFixtures(sandbox());
+    session = sandboxSession();
+  });
+
+  describe('render', function() {
+
+    it('creates a span if it is a font icon', function() {
+      var icon = scout.create('Icon', {
+        parent: session.desktop,
+        iconDesc: scout.icons.INFO
+      });
+      icon.render();
+      expect(icon.$container[0].tagName).toBe('SPAN');
+    });
+
+    it('creates an img if it is an image icon', function() {
+      var icon = scout.create('Icon', {
+        parent: session.desktop,
+        iconDesc: 'icon/image.png'
+      });
+      icon.render();
+      expect(icon.$container[0].tagName).toBe('IMG');
+    });
+
+  });
+
+  describe('setIconDesc', function() {
+
+    it('accepts a string representing the iconId', function() {
+      var icon = scout.create('Icon', {
+        parent: session.desktop,
+        iconDesc: 'icon/image.png'
+      });
+      expect(icon.iconDesc.iconUrl).toBe('icon/image.png');
+
+      icon.setIconDesc('icon/image2.png');
+      expect(icon.iconDesc.iconUrl).toBe('icon/image2.png');
+    });
+
+    it('accepts a scout.IconDesc', function() {
+      var icon = scout.create('Icon', {
+        parent: session.desktop,
+        iconDesc: scout.icons.parseIconId('icon/image.png')
+      });
+      expect(icon.iconDesc.iconUrl).toBe('icon/image.png');
+
+      icon.setIconDesc(scout.icons.parseIconId('icon/image2.png'));
+      expect(icon.iconDesc.iconUrl).toBe('icon/image2.png');
+    });
+
+  });
+
+});

org.eclipse.scout.rt.ui.html.test/src/test/js/scout/util/iconsSpec.js
@@ -37,15 +37,15 @@ describe('scout.icons', function() {
   });
 
   it('parses returns a CSS class for custom fonts', function() {
-    icon = new scout.Icon();
-    icon.iconType = scout.Icon.IconType.FONT_ICON;
+    icon = new scout.IconDesc();
+    icon.iconType = scout.IconDesc.IconType.FONT_ICON;
     icon.font = 'widgetIcons';
     expect(icon.cssClass()).toBe('font-widgetIcons');
   });
 
   it('appends CSS class string with custom fonts', function() {
-    icon = new scout.Icon();
-    icon.iconType = scout.Icon.IconType.FONT_ICON;
+    icon = new scout.IconDesc();
+    icon.iconType = scout.IconDesc.IconType.FONT_ICON;
     icon.font = 'widgetIcons';
     expect(icon.appendCssClass('font-icon')).toBe('font-icon font-widgetIcons');
   });

org.eclipse.scout.rt.ui.html/src/main/js/jquery/jquery-scout.js
@@ -644,11 +644,19 @@ $.fn.appendTextNode = function(text) {
   return $(this.document(true).createTextNode(text)).appendTo(this);
 };
 
+/**
+ * @param {scout.IconDesc|string} iconId
+ */
 $.fn.appendIcon = function(iconId, cssClass) {
   if (!iconId) {
     return this.appendImg(null, cssClass);
   }
-  var icon = scout.icons.parseIconId(iconId);
+  var icon;
+  if (iconId instanceof scout.IconDesc) {
+    icon = iconId;
+  } else {
+    icon = scout.icons.parseIconId(iconId);
+  }
   if (icon.isFontIcon()) {
     return this.makeSpan(cssClass, icon.iconCharacter)
       .addClass('icon')
@@ -760,6 +768,7 @@ $.fn.setTabbable = function(tabbable) {
  * @param {function} [addToDomFunc] optional function which is used to add the new icon element to the DOM
  *     When not set, this.prepend($icon) is called.
  * @returns {$}
+ * @see scout.Icon as an alternative
  */
 $.fn.icon = function(iconId, addToDomFunc) {
   var icon, $icon = this.data('$icon');

org.eclipse.scout.rt.ui.html/src/main/js/scout-module.js
@@ -157,6 +157,8 @@
   __include("scout/datepicker/DatePickerPopupLayout.js");
   __include("scout/datepicker/DatePickerTouchPopup.js");
   __include("scout/datepicker/DatePickerTouchPopupLayout.js");
+  __include("scout/image/Icon.js");
+  __include("scout/image/IconDesc.js");
   __include("scout/image/Image.js");
   __include("scout/image/ImageLayout.js");
   __include("scout/notification/Notification.js");

org.eclipse.scout.rt.ui.html/src/main/js/scout/desktop/toolbox/DesktopToolBox.less
@@ -78,6 +78,11 @@
     font-size: 17px;
   }
 
+  & > .icon.image-icon {
+    /* Increase max image height, no need to limit it to 16px. 33px is the desktop logo height */
+    max-height: 33px;
+  }
+
   &.menu-textandicon > .icon {
     padding-right: @header-menu-textandicon-padding-right;
   }

org.eclipse.scout.rt.ui.html/src/main/js/scout/image/Icon.js
@@ -1,0 +1,112 @@
+/*******************************************************************************
+ * Copyright (c) 2014-2015 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+/**
+ * Widget representing an icon. It may be a font icon or an image icon. Depending on the type, either a span or an img tag will be rendered.
+ * <p>
+ * See also jquery-scout.icon/appendIcon. Main difference to these implementations is that the image loading will invalidate the layout by using {@link scout.Image}.
+ */
+scout.Icon = function() {
+  scout.Icon.parent.call(this);
+
+  /** @type {scout.IconDesc} */
+  this.iconDesc = null;
+
+  /**
+   * Is set if the icon is rendered and an image, it is not set if it is a font icon
+   * @type {scout.Inage}
+   */
+  this.image = null;
+  this.prepend = false;
+};
+scout.inherits(scout.Icon, scout.Widget);
+
+scout.Icon.prototype._init = function(model) {
+  scout.Icon.parent.prototype._init.call(this, model);
+  this._setIconDesc(this.iconDesc);
+};
+
+scout.Icon.prototype._renderProperties = function() {
+  scout.Icon.parent.prototype._renderProperties.call(this);
+  this._renderIconDesc();
+};
+
+/**
+ * Accepts either an iconId as string or an {@link scout.IconDesc}.
+ * @param {(string|scout.IconDesc)} icon
+ */
+scout.Icon.prototype.setIconDesc = function(iconDesc) {
+  this.setProperty('iconDesc', iconDesc);
+};
+
+scout.Icon.prototype._setIconDesc = function(iconDesc) {
+  iconDesc = scout.IconDesc.ensure(iconDesc);
+  this._setProperty('iconDesc', iconDesc);
+};
+
+scout.Icon.prototype._renderIconDesc = function() {
+  this._removeFontIcon();
+  this._removeImageIcon();
+
+  if (!this.iconDesc) {
+    this._renderFontIcon(null);
+    return;
+  }
+
+  if (this.iconDesc.isFontIcon()) {
+    this._renderFontIcon();
+  } else {
+    this._renderImageIcon();
+  }
+};
+
+/**
+ * @param {scout.IconDesc} icon
+ */
+scout.Icon.prototype._renderFontIcon = function() {
+  this.$container = this.$parent.appendIcon(this.iconDesc);
+  if (this.prepend) {
+    this.$container.prependTo(this.$parent);
+  }
+};
+
+scout.Icon.prototype._removeFontIcon = function() {
+  if (this.$container) {
+    this.$container.remove();
+    this.$container = null;
+  }
+};
+
+scout.Icon.prototype._renderImageIcon = function() {
+  if (this.image) {
+    return;
+  }
+  this.image = scout.create('Image', {
+    parent: this,
+    imageUrl: this.iconDesc.iconUrl,
+    cssClass: 'icon image-icon',
+    prepend: this.prepend
+  });
+  this.image.render(this.$parent);
+  this.image.one('destroy', function() {
+    this.image = null;
+  }.bind(this));
+  this.image.on('load error', function(event) {
+    // propagate event
+    this.trigger(event.type, event);
+  }.bind(this));
+  this.$container = this.image.$container;
+};
+
+scout.Icon.prototype._removeImageIcon = function() {
+  if (this.image) {
+    this.image.destroy();
+  }
+};

org.eclipse.scout.rt.ui.html/src/main/js/scout/image/IconDesc.js
@@ -1,0 +1,68 @@
+/*******************************************************************************
+ * Copyright (c) 2014-2015 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+scout.IconDesc = function() {
+  this.iconType;
+  this.font;
+  this.iconCharacter;
+  this.iconUrl;
+};
+
+scout.IconDesc.IconType = {
+  FONT_ICON: 0,
+  BITMAP: 1
+};
+
+scout.IconDesc.DEFAULT_FONT = 'scoutIcons';
+
+/**
+ * Returns a CSS class based on the used font-name.
+ */
+scout.IconDesc.prototype.cssClass = function() {
+  if (this.isFontIcon() && this.font !== scout.IconDesc.DEFAULT_FONT) {
+    return 'font-' + this.font;
+  } else {
+    return '';
+  }
+};
+
+/**
+ * Returns a CSS class string to be used with JQuery.add/removeClass().
+ */
+scout.IconDesc.prototype.appendCssClass = function(cssClass) {
+  var additionalCssClass = this.cssClass();
+  if (additionalCssClass.length > 0) {
+    return cssClass + ' ' + additionalCssClass;
+  } else {
+    return cssClass;
+  }
+};
+
+scout.IconDesc.prototype.isFontIcon = function() {
+  return this.iconType === scout.IconDesc.IconType.FONT_ICON;
+};
+
+scout.IconDesc.prototype.isBitmap = function() {
+  return this.iconType === scout.IconDesc.IconType.BITMAP;
+};
+
+/**
+ * Ensures that the given icon is of type {@link scout.iconDesc}. It a string is provided it is assumed that the string is the iconId which may be parsed to create the {@link scout.IconDesc}.
+ * @param {(string|scout.IconDesc)} icon
+ */
+scout.IconDesc.ensure = function(icon) {
+  if (!icon) {
+    return icon;
+  }
+  if (icon instanceof scout.IconDesc) {
+    return icon;
+  }
+  return scout.icons.parseIconId(icon);
+};

commit cda08acea67ac39b9b47dd6649c76762784ccbbe

Fix exception when tooltip is shown which contains a menu with icon
Bug introduced by commit 4b6694638c31c80b286c8724c7f4c952183ed5ed.

213545

org.eclipse.scout.rt.ui.html/src/main/js/scout/action/Action.js
@@ -133,6 +133,13 @@ scout.Action.prototype._renderIconId = function() {
   }
 };
 
+scout.Action.prototype.get$Icon = function() {
+  if (this.icon) {
+    return this.icon.$container;
+  }
+  return this.$container.data('$icon');
+};
+
 scout.Action.prototype._removeIconId = function() {
   if (this.icon) {
     this.icon.destroy();

org.eclipse.scout.rt.ui.html/src/main/js/scout/menu/ContextMenuPopupLayout.js
@@ -88,7 +88,7 @@ scout.ContextMenuPopupLayout.prototype._setMaxWidthFor = function($menuItems) {
 
 scout.ContextMenuPopupLayout.prototype._calcTextMaxWidth = function(menu) {
   var containerWidth = menu.$container.width(),
-    $icon = menu.$container.data('$icon'),
+    $icon = menu.get$Icon(),
     $text = menu.$text,
     $submenuIcon = menu.$submenuIcon,
     textWidth = containerWidth + 1; // add 1px to make it work even if containerWidth is a float

org.eclipse.scout.rt.ui.html/src/main/js/scout/tooltip/Tooltip.js
@@ -205,7 +205,7 @@ scout.Tooltip.prototype._renderMenus = function() {
     var iconWidth = 0;
     menu.render(this.$menus);
     if (menu.iconId) {
-      iconWidth = menu.$container.data('$icon').outerWidth(true);
+      iconWidth = menu.get$Icon().outerWidth(true);
       maxIconWidth = Math.max(iconWidth, maxIconWidth);
     }
   }, this);
@@ -216,7 +216,7 @@ scout.Tooltip.prototype._renderMenus = function() {
       if (!menu.iconId) {
         menu.$text.cssPaddingLeft(maxIconWidth);
       } else {
-        menu.$text.cssPaddingLeft(maxIconWidth - menu.$container.data('$icon').outerWidth(true));
+        menu.$text.cssPaddingLeft(maxIconWidth - menu.get$Icon().outerWidth(true));
       }
     }, this);
   }