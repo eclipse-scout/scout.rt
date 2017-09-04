054bed17fff5bfd34c31a3fff4ab169626a93432
André Wegmüller <awe@bsiag.com>
2017-09-04 09:46:55 +0200

    Added needsclick class on glasspane to avoid FastClick problems
    FastClick interfered with the touch popup functionality, where the
    embedded field must execute acceptInput before the touch popup is
    closed. Because of FastClick the close event of the popup was fired
    before the blur event of the embedded field.

--- org.eclipse.scout.rt.ui.html.selenium/src/main/resources/selenium.js
+++ org.eclipse.scout.rt.ui.html.selenium/src/main/resources/selenium.js
@@ -32,7 +32,13 @@ scout.selenium = {
   },
 
   setSupportsTouch: function(touch) {
-    scout.device.features['_touch'] = true;
+    scout.device.features['_touch'] = touch;
+    /* Also load FastClick because without it, we would not test the real thing */
+    /* However: once FastClick is loaded, we cannot unload it. This means you   */
+    /* should not switch touch mode in the middle of a selenium test.           */
+    if (touch) {
+      scout.device._loadFastClickDeferred();
+    }
   },
 
   scrollToBottom: function($element) {
