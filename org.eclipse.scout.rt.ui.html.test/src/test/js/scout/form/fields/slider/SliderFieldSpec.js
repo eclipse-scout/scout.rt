d2e24bb73d452fbdde00eae55d56626f9abf3c68
Beat Schwarzentrub <bsh@bsiag.com>
2017-10-16 09:47:27 +0200

    SliderField: fix parsing error when accepting input
    Because the SliderField is a ValueField but does not hold the value
    itself (it delegates to the inner Slider widget), it must correctly
    override methods related to reading and parsing the display text.
    
    215438
    
    Change-Id: If7b6896ef5df997ac1e3e81907eb161cf3717925
    Reviewed-on: https://git.eclipse.org/r/110111
    Tested-by: Hudson CI
    Reviewed-by: Beat Schwarzentrub <bsh@bsiag.com>

---
+++ org.eclipse.scout.rt.ui.html.test/src/test/js/scout/form/fields/slider/SliderFieldSpec.js
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2014-2015 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+describe("SliderField", function() {
+  var session, helper, field;
+
+  beforeEach(function() {
+    setFixtures(sandbox());
+    session = sandboxSession();
+    helper = new scout.FormSpecHelper(session);
+    field = createField(createModel());
+    jasmine.Ajax.install();
+    jasmine.clock().install();
+  });
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
+    jasmine.Ajax.uninstall();
+  });
+
+  function createField(model) {
+    var field = new scout.SliderField();
+    field.init(model);
+    return field;
+  }
+
+  function createModel() {
+    return helper.createFieldModel();
+  }
+
+  describe("slider", function() {
+
+    it("accepts the value", function() {
+      field.render();
+      field.setValue(25);
+
+      expect(field.value).toBe(null);
+      expect(field.slider.value).toBe(25);
+      expect(field.displayText).toBe('');
+      field.acceptInput();
+      expect(field.displayText).toBe('25');
+
+      field.slider.setValue(30);
+      field.acceptInput();
+      expect(field.value).toBe(null);
+      expect(field.slider.value).toBe(30);
+      expect(field.displayText).toBe('30');
+    });
+
+  });
+
+});
--- org.eclipse.scout.rt.ui.html/src/main/js/scout/slider/Slider.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/slider/Slider.js
@@ -11,10 +11,13 @@
 scout.Slider = function() {
   scout.Slider.parent.call(this);
 
-  this.value;
-  this.minValue;
-  this.maxValue;
-  this.step;
+  this.value = null;
+  this.minValue = null;
+  this.maxValue = null;
+  this.step = null;
+
+  this.$sliderInput = null;
+  this.$sliderValue = null;
 };
 scout.inherits(scout.Slider, scout.Widget);
 
@@ -44,6 +47,12 @@ scout.Slider.prototype._renderProperties = function() {
   this._renderMinValue();
   this._renderMaxValue();
   this._renderStep();
+};
+
+scout.Slider.prototype._remove = function() {
+  scout.Slider.parent.prototype._remove.call(this);
+  this.$sliderInput = null;
+  this.$sliderValue = null;
 };
 
 scout.Slider.prototype._renderValue = function() {
