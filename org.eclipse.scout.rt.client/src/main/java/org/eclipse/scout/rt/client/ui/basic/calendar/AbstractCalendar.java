a6f61380933cc829a5ac7235b6133779b139263f
Beat Schwarzentrub <bsh@bsiag.com>
2017-10-24 08:54:56 +0200

    Calendar: ensure component to resolve is contained in PROP_COMPONENTS
    Selected component must be part of the list of all components. This
    check is necessary e.g. when deleting a component. The components are
    reloaded, but the selected component is not updated automatically
    (because it is a separate property). The resolveComponent() will now
    return null when the given component is not contained in the list of all
    components, thus clearing the selection.
    
    215698
    
    Change-Id: I691396013fc22b2892e532134502e360ebe3968c
    Reviewed-on: https://git.eclipse.org/r/110540
    Tested-by: Hudson CI
    Reviewed-by: Hannes Mueller <hannes.mueller@bsi-software.com>

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/basic/calendar/AbstractCalendar.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/basic/calendar/AbstractCalendar.java
@@ -626,7 +626,10 @@ public abstract class AbstractCalendar extends AbstractPropertyObserver implemen
     }
   }
 
-  private CalendarComponent resolveComponent(CalendarComponent comp) {
+  protected CalendarComponent resolveComponent(CalendarComponent comp) {
+    if (comp == null || !getComponents().contains(comp)) {
+      return null;
+    }
     return comp;
   }
 
