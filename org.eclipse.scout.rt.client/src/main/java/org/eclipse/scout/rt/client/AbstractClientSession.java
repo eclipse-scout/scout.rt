105951c83ed11d5fa3545fd884e3138a0a78de8e
Andi Bur <andi.bur@gmail.com>
2017-11-01 14:04:30 +0100

    Fix logging of cancelled client jobs due to session shutdown
    All client jobs assigned to a particular session are canceled when the
    session is shutting down. The affected jobs query did not restrict the
    job state, so that completed jobs might be returned as well (the
    FutureSet that manages all running jobs references DONE jobs as well, at
    least for a _small_ amount of time). Excluding DONE and REJECTED jobs
    prevents logging of false-positives.
    
    216696
    
    Change-Id: I514f8c68e5a2015fa986a802601e290e6f6a3311
    (cherry picked from commit 64e543e9465e54c8b8d0f69a468d84f468b03bc2)
    Reviewed-on: https://git.eclipse.org/r/110844
    Tested-by: Hudson CI
    Reviewed-by: Andi Bur <andi.bur@gmail.com>

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/AbstractClientSession.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/AbstractClientSession.java
@@ -47,6 +47,7 @@ import org.eclipse.scout.rt.platform.exception.ProcessingException;
 import org.eclipse.scout.rt.platform.filter.IFilter;
 import org.eclipse.scout.rt.platform.job.IExecutionSemaphore;
 import org.eclipse.scout.rt.platform.job.IFuture;
+import org.eclipse.scout.rt.platform.job.JobState;
 import org.eclipse.scout.rt.platform.job.Jobs;
 import org.eclipse.scout.rt.platform.nls.NlsLocale;
 import org.eclipse.scout.rt.platform.reflect.AbstractPropertyObserver;
@@ -476,6 +477,7 @@ public abstract class AbstractClientSession extends AbstractPropertyObserver imp
         .andMatch(new SessionFutureFilter(ISession.CURRENT.get()))
         .andMatchNotFuture(IFuture.CURRENT.get())
         .andMatchNot(ModelJobFutureFilter.INSTANCE)
+        .andMatchNotState(JobState.DONE, JobState.REJECTED)
         .toFilter();
 
     // Wait for running jobs to complete before we cancel them
@@ -506,6 +508,7 @@ public abstract class AbstractClientSession extends AbstractPropertyObserver imp
         .andMatch(new SessionFutureFilter(ISession.CURRENT.get()))
         .andMatchNotFuture(IFuture.CURRENT.get())
         .andMatch(ModelJobFutureFilter.INSTANCE)
+        .andMatchNotState(JobState.DONE, JobState.REJECTED)
         .toFilter());
     if (!runningModelJobs.isEmpty()) {
       LOG.info("Cancel running model jobs because the client session was shut down. [session={}, user={}, jobs={}]", AbstractClientSession.this, getUserId(), runningModelJobs);
