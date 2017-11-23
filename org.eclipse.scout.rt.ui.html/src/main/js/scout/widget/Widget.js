542bfa74fe8945d74bf9216ab7f35c1008664248
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2017-11-23 16:23:06 +0100

    Widget: don't accept layout operations while removing
    Use case: Removing the tree while the collapse animation is still
    running. 
    This will fail because it tries to revalidate the layout on each step.
    Because _remove of the tree does not just stop the animation but let it
    jump to the end (to make the complete function execute), a revalidation
    will be executed while removing the tree. And in that case an exception
    occured because the the menu bar had already been removed.

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/widget/Widget.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/widget/Widget.js
@@ -620,7 +620,7 @@ scout.Widget.prototype._renderLoading = function() {
 //--- Layouting / HtmlComponent methods ---
 
 scout.Widget.prototype.pack = function() {
-  if (!this.rendered) {
+  if (!this.rendered || this.removing) {
     return;
   }
   if (!this.htmlComp) {
@@ -630,7 +630,7 @@ scout.Widget.prototype.pack = function() {
 };
 
 scout.Widget.prototype.invalidateLayout = function() {
-  if (!this.rendered) {
+  if (!this.rendered || this.removing) {
     return;
   }
   if (!this.htmlComp) {
@@ -640,7 +640,7 @@ scout.Widget.prototype.invalidateLayout = function() {
 };
 
 scout.Widget.prototype.validateLayout = function() {
-  if (!this.rendered) {
+  if (!this.rendered || this.removing) {
     return;
   }
   if (!this.htmlComp) {
@@ -650,7 +650,7 @@ scout.Widget.prototype.validateLayout = function() {
 };
 
 scout.Widget.prototype.revalidateLayout = function() {
-  if (!this.rendered) {
+  if (!this.rendered || this.removing) {
     return;
   }
   if (!this.htmlComp) {
@@ -660,7 +660,7 @@ scout.Widget.prototype.revalidateLayout = function() {
 };
 
 scout.Widget.prototype.invalidateLayoutTree = function(invalidateParents) {
-  if (!this.rendered) {
+  if (!this.rendered || this.removing) {
     return;
   }
   if (!this.htmlComp) {
@@ -670,7 +670,7 @@ scout.Widget.prototype.invalidateLayoutTree = function(invalidateParents) {
 };
 
 scout.Widget.prototype.validateLayoutTree = function() {
-  if (!this.rendered) {
+  if (!this.rendered || this.removing) {
     return;
   }
   if (!this.htmlComp) {
@@ -680,7 +680,7 @@ scout.Widget.prototype.validateLayoutTree = function() {
 };
 
 scout.Widget.prototype.revalidateLayoutTree = function() {
-  if (!this.rendered) {
+  if (!this.rendered || this.removing) {
     return;
   }
   if (!this.htmlComp) {
