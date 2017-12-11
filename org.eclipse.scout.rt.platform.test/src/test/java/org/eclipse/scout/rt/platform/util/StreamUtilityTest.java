37ccfc8f9fa8905a76ad9c293db595352792c6de
Andi Bur <andi.bur@gmail.com>
2017-12-11 14:29:06 +0100

    Add collection of additional Stream support that is missing in Java 8
    Change-Id: I9d2f5f01624a55ebad24ccfa9ad0831c793d088e
    Reviewed-on: https://git.eclipse.org/r/113158
    Tested-by: Hudson CI
    Reviewed-by: Andi Bur <andi.bur@gmail.com>

---
+++ org.eclipse.scout.rt.platform.test/src/test/java/org/eclipse/scout/rt/platform/util/StreamUtilityTest.java
@@ -0,0 +1,215 @@
+/*******************************************************************************
+ * Copyright (c) 2017 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.platform.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.eclipse.scout.rt.platform.util.Assertions.AssertionException;
+import org.junit.Test;
+
+public class StreamUtilityTest {
+
+  @Test
+  public void testIterateNullParameters() {
+    // null initialElement is ok
+    assertEquals(
+        Arrays.asList(null, 0, 1, 2, 3, 4),
+        StreamUtility.<Integer> iterate(null, i -> i == null || i < 5, i -> i == null ? 0 : i + 1).collect(Collectors.toList()));
+
+    try {
+      StreamUtility.<Integer> iterate(null, null, i -> i + 1);
+      fail("null hasNext predicate is not supported");
+    }
+    catch (AssertionException expected) {
+      // expected
+    }
+
+    try {
+      StreamUtility.<Integer> iterate(null, i -> i < 10, null);
+      fail("null next function is not supported");
+    }
+    catch (AssertionException expected) {
+      // expected
+    }
+  }
+
+  @Test
+  public void testIterate() {
+    assertEquals(0, StreamUtility.iterate(0, i -> false, i -> failUnaryOperator(i)).count());
+    assertEquals(Arrays.asList(0), StreamUtility.iterate(0, i -> i <= 0, i -> i + 1).collect(Collectors.toList()));
+    assertEquals(Arrays.asList(0, 1), StreamUtility.iterate(0, i -> i <= 1, i -> i + 1).collect(Collectors.toList()));
+
+    CircuitBreaker<Integer> cb = new CircuitBreaker<Integer>();
+    try {
+      StreamUtility.iterate(0, i -> i <= 0, cb::next).count();
+      fail("expecting " + IllegalStateException.class.getName());
+    }
+    catch (IllegalStateException expected) {
+      // expected
+    }
+  }
+
+  @Test
+  public void testIterateSequential() {
+    doTestIterate(false);
+  }
+
+  @Test
+  public void testIterateParallel() {
+    doTestIterate(true);
+  }
+
+  protected void doTestIterate(boolean parallel) {
+    final int upperLimit = 10_000;
+    try (Stream<Integer> stream = StreamUtility.iterate(0, i -> i < upperLimit, i -> i + 1)) {
+      if (parallel) {
+        stream.parallel();
+      }
+      int expected = 0;
+      for (Integer actual : stream.collect(Collectors.toList())) {
+        assertEquals(expected, actual.intValue());
+        expected++;
+      }
+      assertEquals(upperLimit, expected);
+    }
+  }
+
+  protected static class CircuitBreaker<T> {
+    private int m_counter = 0;
+
+    public T next(T current) {
+      if (m_counter > 5_000) {
+        throw new IllegalStateException("endless loop (" + m_counter + " invocations)");
+      }
+      m_counter++;
+      return current;
+    }
+  }
+
+  @Test
+  public void testTakeWhileEmptyStream() {
+    assertEquals(0, StreamUtility.takeWhile(Stream.empty(), x -> failPredicate()).count()); // predicate is not expected to be tested
+  }
+
+  @Test
+  public void testTakeWhileSingleElementStream() {
+    assertEquals(1, StreamUtility.takeWhile(Stream.of("a"), x -> true).count());
+    assertEquals(0, StreamUtility.takeWhile(Stream.of("a"), x -> false).count());
+  }
+
+  @Test
+  public void testTakeWhileMultipleElementsStream() {
+    assertEquals(2, StreamUtility.takeWhile(Stream.of("a", "b"), x -> true).count());
+    assertEquals(1, StreamUtility.takeWhile(Stream.of("a", "b"), x -> "a".equals(x)).count());
+
+    // takeWhile is not a filter, i.e. if the takeWhile predicate states false, no more elemnts are taken
+    assertEquals(1, StreamUtility.takeWhile(Stream.of("a", "b", "a"), x -> "a".equals(x)).count());
+  }
+
+  @Test
+  public void testTakeWhileSequentialStream() {
+    doTestTakeWhileIntStream(false, false);
+    doTestTakeWhileIntStream(false, true);
+  }
+
+  @Test
+  public void testTakeWhileParallelStream() {
+    doTestTakeWhileIntStream(true, false);
+    doTestTakeWhileIntStream(true, true);
+  }
+
+  protected void doTestTakeWhileIntStream(boolean parallelSourceStream, boolean parallelTakeWhileStream) {
+    final int upperLimit = 10_000;
+    final int takeWhileLimit = 5_000;
+    try (IntStream intStream = IntStream.range(0, upperLimit)) {
+      if (parallelSourceStream) {
+        intStream.parallel();
+      }
+      Stream<Integer> takeWhileStream = StreamUtility.takeWhile(intStream.mapToObj(Integer::valueOf), i -> i < takeWhileLimit);
+      if (parallelTakeWhileStream) {
+        takeWhileStream.parallel();
+      }
+      int expected = 0;
+      for (Integer actual : takeWhileStream.collect(Collectors.toList())) {
+        assertEquals(expected, actual.intValue());
+        expected++;
+      }
+      assertEquals(takeWhileLimit, expected);
+    }
+  }
+
+  @Test
+  public void testToReverseListEmptyStream() {
+    // empty
+    LinkedList<Object> list = Stream.empty().collect(StreamUtility.toReverseList());
+    assertNotNull(list);
+    assertEquals(LinkedList.class, list.getClass());
+    assertEquals(Collections.emptyList(), list);
+  }
+
+  @Test
+  public void testToReverseListSingleElementStream() {
+    LinkedList<String> list = Stream.of("a").collect(StreamUtility.toReverseList());
+    assertNotNull(list);
+    assertEquals(Collections.singletonList("a"), list);
+  }
+
+  @Test
+  public void testToReverseListMultipleElementsStream() {
+    LinkedList<String> list = Stream.of("a", "b", "c").collect(StreamUtility.toReverseList());
+    assertNotNull(list);
+    assertEquals(Arrays.asList("c", "b", "a"), list);
+  }
+
+  @Test
+  public void testToReverseListSequentialStream() {
+    doTestToReverseListIntStream(false);
+  }
+
+  @Test
+  public void testToReverseListParallelStream() {
+    doTestToReverseListIntStream(true);
+  }
+
+  protected void doTestToReverseListIntStream(boolean parallel) {
+    final int upperLimit = 10_000;
+    try (IntStream stream = IntStream.range(0, upperLimit)) {
+      if (parallel) {
+        stream.parallel();
+      }
+      int expected = upperLimit - 1;
+      for (Integer actual : stream.mapToObj(Integer::valueOf).collect(StreamUtility.toReverseList())) {
+        assertEquals(expected, actual.intValue());
+        expected--;
+      }
+      assertEquals(-1, expected);
+    }
+  }
+
+  protected static boolean failPredicate() {
+    fail("unexpected check of predicate");
+    return false; // actually value is never returned
+  }
+
+  protected static <T> T failUnaryOperator(T t) {
+    fail("unexpected invocation of unaryOperator");
+    return t; // actually value is never returned
+  }
+}
---
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/util/StreamUtility.java
@@ -0,0 +1,160 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2017 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.platform.util;
+
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/**
+ * Collection of additional {@link Stream} support that is missing in Java 8.
+ */
+public final class StreamUtility {
+
+  private StreamUtility() {
+  }
+
+  /**
+   * Returns a sequential ordered <code>Stream</code> produced by consecutive invocation of the <code>next</code>
+   * function to an initial element, as long as the given <code>terminate</code> predicate returns <code>false</code>.
+   * <p>
+   * <b>Note:</b> This method is motivated by Java 9's <code>Stream#iterate(Object, Predicate, UnaryOperator)</code> and
+   * can be removed when upgrading.
+   */
+  public static <T> Stream<T> iterate(T initialElement, Predicate<? super T> hasNext, UnaryOperator<T> next) {
+    Assertions.assertNotNull(next);
+    Assertions.assertNotNull(hasNext);
+
+    Spliterator<T> spliterator = new Spliterators.AbstractSpliterator<T>(Long.MAX_VALUE, Spliterator.ORDERED | Spliterator.IMMUTABLE) {
+      private T m_prev;
+      private boolean m_started;
+      private boolean m_exhausted;
+
+      @Override
+      public boolean tryAdvance(Consumer<? super T> action) {
+        Objects.requireNonNull(action);
+        if (m_exhausted) {
+          return false;
+        }
+        T currentElement;
+        if (m_started) {
+          currentElement = next.apply(m_prev);
+        }
+        else {
+          currentElement = initialElement;
+          m_started = true;
+        }
+        if (!hasNext.test(currentElement)) {
+          m_prev = null;
+          m_exhausted = true;
+          return false;
+        }
+        m_prev = currentElement;
+        action.accept(currentElement);
+        return true;
+      }
+
+      @Override
+      public void forEachRemaining(Consumer<? super T> action) {
+        Assertions.assertNotNull(action);
+        if (m_exhausted) {
+          return;
+        }
+        m_exhausted = true;
+        T currentElement = m_started ? next.apply(m_prev) : initialElement;
+        m_prev = null;
+        while (hasNext.test(currentElement)) {
+          action.accept(currentElement);
+          currentElement = next.apply(currentElement);
+        }
+      }
+    };
+
+    return StreamSupport.stream(spliterator, false);
+  }
+
+  /**
+   * Wraps the given stream into one that streams elements as long as the given predicate evaluates to
+   * <code>true</code>. The stream returned does not support parallel execution and invoking {@link Stream#parallel()}
+   * does not have any effects.
+   * <p>
+   * <b>Note:</b> This method is motivated by Java 9's <code>Stream#takeWhile(Predicate)</code> and can be removed when
+   * upgrading.
+   */
+  public static <T> Stream<T> takeWhile(Stream<T> stream, Predicate<? super T> predicate) {
+    Assertions.assertNotNull(predicate);
+    Spliterator<T> streamSpliterator = Assertions.assertNotNull(stream).spliterator();
+
+    Spliterator<T> takeWhileSpleiterator = new Spliterators.AbstractSpliterator<T>(Long.MAX_VALUE, Spliterator.ORDERED | Spliterator.IMMUTABLE) {
+      private boolean m_exhausted;
+
+      @Override
+      public Spliterator<T> trySplit() {
+        // this implementation does not support parallel execution
+        return null;
+      }
+
+      @Override
+      public boolean tryAdvance(Consumer<? super T> action) {
+        Assertions.assertNotNull(action);
+        if (m_exhausted) {
+          return false;
+        }
+
+        return streamSpliterator.tryAdvance(t -> {
+          if (predicate.test(t)) {
+            action.accept(t);
+          }
+          else {
+            m_exhausted = true;
+          }
+        });
+      }
+    };
+
+    return StreamSupport.stream(takeWhileSpleiterator, false);
+  }
+
+  /**
+   * Returns a {@link Collector} that accumulates input elements into a new {@link LinkedList}, by adding each of them
+   * at the beginning.
+   * <p>
+   * <b>Note:</b> Other than {@link Collectors#toList()}, this collector adds stream elements at the beginning of the
+   * resulting list, which effectively results in a reverse ordered list. Hence this collector is a replacement for the
+   * following sequence:
+   *
+   * <pre>
+   * Stream&lt;?&gt; s = ...;
+   * List&lt;?&gt; list = s.collect(Collectors.toList());
+   * Collections.reverse(list);
+   * ...
+   * </pre>
+   */
+  public static <T> Collector<T, ?, LinkedList<T>> toReverseList() {
+    // Use LinkedList because it allows O(1) insertion at the beginning.
+    // ArrayList would require O(n) for every insertion at the beginning, resulting in total costs of O(n^2)!
+    return Collector.of(
+        LinkedList::new,
+        (l, b) -> l.addFirst(b),
+        (l1, l2) -> {
+          l2.addAll(l1);
+          return l2;
+        });
+  }
+}
