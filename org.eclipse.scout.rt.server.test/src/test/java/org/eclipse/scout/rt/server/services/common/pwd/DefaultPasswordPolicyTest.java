6ebd2ad575234db43e5d83dd51f38a86c0e7da42
Stephan Merkli <stephan.merkli@bsi-software.com>
2017-12-13 12:42:43 +0100

    Password policy: enforce lowercase and uppercase character
    Before either lowercase or uppercase character was enforced, now at
    least one of each is required.
    
    Change-Id: Icb729d45c83eeb5fe0eef2e9f4d433fb6b743363
    Reviewed-on: https://git.eclipse.org/r/113315
    Tested-by: Hudson CI
    Reviewed-by: Ivan Motsch <ivan.motsch@bsiag.com>

--- org.eclipse.scout.rt.server.test/src/test/java/org/eclipse/scout/rt/server/services/common/pwd/DefaultPasswordPolicyTest.java
+++ org.eclipse.scout.rt.server.test/src/test/java/org/eclipse/scout/rt/server/services/common/pwd/DefaultPasswordPolicyTest.java
@@ -12,18 +12,19 @@ public class DefaultPasswordPolicyTest {
   @Test
   public void testPolicy() {
     IPasswordPolicy policy = new DefaultPasswordPolicy();
-    assertVetoException(policy, "11", "whatever".toCharArray(), "uname", 1);
-    assertVetoException(policy, "11", "whatever".toCharArray(), "uname", 0);
-    assertVetoException(policy, "11", "11uname22_".toCharArray(), "uname", -1);
-    assertVetoException(policy, "11", "11uname22_".toCharArray(), "UNAME", -1);
+    assertVetoException(policy, "11", "whatEver".toCharArray(), "uname", 1);
+    assertVetoException(policy, "11", "whatEver".toCharArray(), "uname", 0);
+    assertVetoException(policy, "11", "11unameA22_".toCharArray(), "uname", -1);
+    assertVetoException(policy, "11", "11unameA22_".toCharArray(), "UNAME", -1);
     assertVetoException(policy, "11", null, "uname", -1);
     assertVetoException(policy, "11", "".toCharArray(), "uname", -1);
-    assertVetoException(policy, "11", "whatever".toCharArray(), "uname", -1);
+    assertVetoException(policy, "11", "whatEver".toCharArray(), "uname", -1);
     assertVetoException(policy, "11", "123456789".toCharArray(), "uname", -1);
-    assertVetoException(policy, "11", "1234567ABC".toCharArray(), "uname", -1);
+    assertVetoException(policy, "11", "1234567ABCabc".toCharArray(), "uname", -1);
+    assertVetoException(policy, "11", "WHATEVER1_".toCharArray(), "uname", -1);
 
-    policy.check("11", "whatever1_".toCharArray(), "uid", -1);
-    policy.check("11", "whatever1_".toCharArray(), null, -1);
+    policy.check("11", "whatEver1_".toCharArray(), "uid", -1);
+    policy.check("11", "whatEver1_".toCharArray(), null, -1);
   }
 
   private void assertVetoException(IPasswordPolicy policy, String userId, char[] newPassword, String userName, int historyIndex) {
