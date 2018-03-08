fe82c951b7aebff25a6d03d7f08c972a2b892f2f
Beat Schwarzentrub <bsh@bsiag.com>
2018-03-08 10:16:22 +0100

    SimpleTabArea: prevent destruction of tab order
    Bug: when rendering/removing the same tab are multiple times, the order
    of the tabs got reversed each time. This is because the Array.reverse()
    method is destructive and permanently changed the member "tabs"!
    Fix: slice the array before reversing it (creates a copy).
    
    Added a jasmine test.
    
    Change-Id: I29bb1b50439fe105206cd8b386abb927d20de877
    Reviewed-on: https://git.eclipse.org/r/118962
    Tested-by: Hudson CI
    Reviewed-by: Beat Schwarzentrub <bsh@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/118970

---
+++ org.eclipse.scout.rt.ui.html.test/src/test/js/scout/tabbox/SimpleTabAreaSpec.js
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright (c) 2014-2017 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+describe('SimpleTabArea', function() {
+  var session;
+
+  beforeEach(function() {
+    setFixtures(sandbox());
+    session = sandboxSession();
+  });
+
+  it('renders the tabs in the correct order', function() {
+    var tabBox = scout.create('SimpleTabBox', {
+      parent: session.desktop
+    });
+    var view1 = scout.create('GroupBox', {
+      parent: tabBox,
+      title: 'One'
+    });
+    var view2 = scout.create('GroupBox', {
+      parent: tabBox,
+      title: 'Two'
+    });
+    var view3 = scout.create('GroupBox', {
+      parent: tabBox,
+      title: 'Three'
+    });
+    tabBox.addView(view1);
+    tabBox.addView(view3, false);
+    tabBox.addView(view2, false); // <-- IMPORTANT: this has to be added last! (Reason: in 6.0.x, a view is always added next to the current view...)
+
+    expect(tabBox.tabArea.tabs.length).toBe(3);
+    expect(tabBox.tabArea.tabs[0].view).toBe(view1);
+    expect(tabBox.tabArea.tabs[1].view).toBe(view2);
+    expect(tabBox.tabArea.tabs[2].view).toBe(view3);
+    expect(tabBox.tabArea.tabs[0].title).toBe('One');
+    expect(tabBox.tabArea.tabs[1].title).toBe('Two');
+    expect(tabBox.tabArea.tabs[2].title).toBe('Three');
+    expect(tabBox.tabArea.tabs[0].selected).toBe(true);
+    expect(tabBox.tabArea.tabs[1].selected).toBe(false);
+    expect(tabBox.tabArea.tabs[2].selected).toBe(false);
+
+    // -----
+
+    tabBox.render(session.$entryPoint);
+    expect(tabBox.$tabArea).toBeTruthy();
+    expect(tabBox.$tabArea.children().length).toBe(3);
+    expect(tabBox.$tabArea.children().eq(0)).toHaveClass('selected');
+    expect(tabBox.$tabArea.children().eq(0).text().trim()).toBe('One');
+    expect(tabBox.$tabArea.children().eq(1)).not.toHaveClass('selected');
+    expect(tabBox.$tabArea.children().eq(1).text().trim()).toBe('Two');
+    expect(tabBox.$tabArea.children().eq(2)).not.toHaveClass('selected');
+    expect(tabBox.$tabArea.children().eq(2).text().trim()).toBe('Three');
+
+    tabBox.remove();
+    expect(tabBox.$tabArea).not.toBeTruthy;
+
+    tabBox.render(session.$entryPoint);
+    expect(tabBox.$tabArea).toBeTruthy();
+    expect(tabBox.$tabArea.children().length).toBe(3);
+    expect(tabBox.$tabArea.children().eq(0)).toHaveClass('selected');
+    expect(tabBox.$tabArea.children().eq(0).text().trim()).toBe('One');
+    expect(tabBox.$tabArea.children().eq(1)).not.toHaveClass('selected');
+    expect(tabBox.$tabArea.children().eq(1).text().trim()).toBe('Two');
+    expect(tabBox.$tabArea.children().eq(2)).not.toHaveClass('selected');
+    expect(tabBox.$tabArea.children().eq(2).text().trim()).toBe('Three');
+  });
+
+});
