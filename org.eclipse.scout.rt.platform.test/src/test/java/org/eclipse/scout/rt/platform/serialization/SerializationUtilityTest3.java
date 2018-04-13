8ff8f15944305ed26895effafecae1f2f8d55c0c
Ivan Motsch <ivan.motsch@bsiag.com>
2018-04-13 17:19:27 +0200

    Fix serialVersionUID for interfaces with default methods
    Change-Id: I5d02e2b89c99a98f837823c035ae206ecfbb9b34
    Signed-off-by: Ivan Motsch <ivan.motsch@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/121188
    Tested-by: CI Bot
    Reviewed-by: Andi Bur <andi.bur@gmail.com>

--- org.eclipse.scout.rt.platform.test/src/test/java/org/eclipse/scout/rt/platform/serialization/SerializationUtilityTest3.java
+++ org.eclipse.scout.rt.platform.test/src/test/java/org/eclipse/scout/rt/platform/serialization/SerializationUtilityTest3.java
@@ -17,75 +17,50 @@ import org.eclipse.scout.rt.platform.util.HexUtility;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class SerializationUtilityTest2 {
+public class SerializationUtilityTest3 {
 
-  private interface IAnimal0 {
-  }
-
-  private interface IAnimal1 extends Serializable, Cloneable {
-    Object clone();
+  private interface IAnimal0 extends Serializable {
   }
 
   @SuppressWarnings("unused")
-  private interface IAnimal2 extends Serializable, Cloneable {
-    //long serialVersionUID = 0x0123456789012345L;
+  private interface IAnimal1 extends Serializable, Cloneable {
 
     String __fixtureField = "F";
 
     default void __fixtureMethod() {
     }
-
-    Object clone();
   }
 
-  private static final class Cat0 implements IAnimal0, Serializable {
-    private static final long serialVersionUID = 0x0123456789012345L;
-  }
+  @SuppressWarnings("unused")
+  private interface IAnimal2 extends Serializable, Cloneable {
+    long serialVersionUID = 0x0123456789012345L;
 
-  private static final class Cat1 implements IAnimal1 {
-    private static final long serialVersionUID = 0x0123456789012345L;
+    String __fixtureField = "F";
 
-    @Override
-    public Object clone() {
-      return null;
-    }
-  }
-
-  private static final class Cat2 implements IAnimal2 {
-    private static final long serialVersionUID = 0x0123456789012345L;
-
-    @Override
-    public Object clone() {
-      return null;
+    default void __fixtureMethod() {
     }
   }
 
   @Test
-  public void testInterfaceImplementationsWithClone() throws Exception {
+  public void testInterfaceLiteralsWithClone() throws Exception {
     IObjectSerializer ser = new BasicObjectSerializer(null);
-    byte[] a0 = ser.serialize(new Cat0());
-    byte[] a1 = ser.serialize(new Cat1());
-    byte[] a2 = ser.serialize(new Cat2());
+    byte[] a0 = ser.serialize(IAnimal0.class);
+    byte[] a1 = ser.serialize(IAnimal1.class);
+    byte[] a2 = ser.serialize(IAnimal2.class);
     dump("0", a0);
     dump("1", a1);
     dump("2", a2);
-    Assert.assertArrayEquals(mockSerialData(Cat0.class, 0x0123456789012345L), a0);
-    Assert.assertArrayEquals(mockSerialData(Cat1.class, 0x0123456789012345L), a1);
-    Assert.assertArrayEquals(mockSerialData(Cat2.class, 0x0123456789012345L), a2);
+    Assert.assertArrayEquals(mockSerialData(IAnimal0.class, 0x61df14aa141f7c9eL), a0);
+    Assert.assertArrayEquals(mockSerialData(IAnimal1.class, 0x8d207f4e15d110a8L), a1);
+    Assert.assertArrayEquals(mockSerialData(IAnimal2.class, 0x0123456789012345L), a2);
 
-    //serialize Cat0, deserialize Cat2
-    byte[] a = ser.serialize(new Cat0());
+    //serialize IAnimal0, deserialize IAnimal2
+    byte[] a = ser.serialize(IAnimal0.class);
     Assert.assertEquals('0', a[a.length - 14]);
     a[a.length - 14] = '2';
-    Cat2 cat2 = ser.deserialize(a, Cat2.class);
-    Assert.assertNotNull(cat2);
 
-    //serialize Cat2, deserialize Cat0
-    a = ser.serialize(new Cat2());
-    Assert.assertEquals('2', a[a.length - 14]);
-    a[a.length - 14] = '0';
-    Cat0 cat0 = ser.deserialize(a, Cat0.class);
-    Assert.assertNotNull(cat0);
+    Class<?> c2 = ser.deserialize(a, Class.class);
+    Assert.assertSame(IAnimal2.class, c2);
   }
 
   private static void dump(String name, byte[] a) {
@@ -109,7 +84,7 @@ public class SerializationUtilityTest2 {
     ByteBuffer buf = ByteBuffer.allocate(21 + className.length());
     buf.putShort((short) 0xaced);//STREAM_MAGIC
     buf.putShort((short) 0x0005);//STREAM_VERSION
-    buf.put((byte) 0x73);//TC_OBJECT
+    buf.put((byte) 0x76);//TC_CLASS
     buf.put((byte) 0x72);//TC_CLASSDESC
     buf.putShort((short) className.length());//length of class name
     buf.put(className.getBytes());//class name
