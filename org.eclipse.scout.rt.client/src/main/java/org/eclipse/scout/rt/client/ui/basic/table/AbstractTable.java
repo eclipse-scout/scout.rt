20021cc114d232ccad586f88115d1e2dfb51aa1f
Jeremie Miserez <jeremie.miserez@bsi-software.com>
2018-04-20 15:05:15 +0200

    Fix or suppress Sonar issues
    - squid:S1066: Fix issue in ConcurrentExpiringMap
    - squid:S1171: Catch PlatformError instead of Error
    - squid:S1143: Suppress warnings about throwing inside finally-blocks,
    as the method explicitly handles this correctly by using addSuppressed.
    
    Change-Id: Idbdb7e0ffe2dc318c440c1460372bc191128d2ec
    Signed-off-by: Jeremie Miserez <jeremie.miserez@bsi-software.com>
    Reviewed-on: https://git.eclipse.org/r/121478
    Tested-by: CI Bot
    Reviewed-by: Beat Schwarzentrub <bsh@bsiag.com>

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/basic/table/AbstractTable.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/basic/table/AbstractTable.java
@@ -95,6 +95,7 @@ import org.eclipse.scout.rt.platform.annotations.ConfigProperty;
 import org.eclipse.scout.rt.platform.classid.ClassId;
 import org.eclipse.scout.rt.platform.classid.ITypeWithClassId;
 import org.eclipse.scout.rt.platform.exception.ExceptionHandler;
+import org.eclipse.scout.rt.platform.exception.PlatformError;
 import org.eclipse.scout.rt.platform.html.HTML;
 import org.eclipse.scout.rt.platform.reflect.AbstractPropertyObserver;
 import org.eclipse.scout.rt.platform.reflect.ConfigurationUtility;
@@ -1618,6 +1619,7 @@ public abstract class AbstractTable extends AbstractPropertyObserver implements 
   }
 
   @Override
+  @SuppressWarnings("squid:S1143")
   public void setTableChanging(boolean b) {
     // use a stack counter because setTableChanging might be called in nested
     // loops
@@ -1641,7 +1643,7 @@ public abstract class AbstractTable extends AbstractPropertyObserver implements 
             }
           }
         }
-        catch (RuntimeException | Error t) {
+        catch (RuntimeException | PlatformError t) {
           // covers all unchecked exceptions
           saveEx = t;
           throw t;
@@ -1653,7 +1655,7 @@ public abstract class AbstractTable extends AbstractPropertyObserver implements 
             try {
               processEventBuffer();
             }
-            catch (RuntimeException | Error t) {
+            catch (RuntimeException | PlatformError t) {
               if (saveEx != null) {
                 saveEx.addSuppressed(t);
               }
@@ -1666,7 +1668,7 @@ public abstract class AbstractTable extends AbstractPropertyObserver implements 
               try {
                 propertySupport.setPropertiesChanging(false);
               }
-              catch (RuntimeException | Error t) {
+              catch (RuntimeException | PlatformError t) {
                 if (saveEx != null) {
                   saveEx.addSuppressed(t);
                 }
