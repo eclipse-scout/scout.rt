a8ea940bf819d7cd3257232126cc1d4ba6084fa0
Beat Schwarzentrub <bsh@bsiag.com>
2018-05-10 15:53:47 +0200

    Prevent potential errors in $.fn.selectAllText()
    - Gracefully ignore missing DOM elements
    - Not all execution paths returned "this"
    
    Change-Id: I1c0d244e55e4e1e7d4165b4d9d128a19134d020f

--- org.eclipse.scout.rt.ui.html/src/main/js/jquery/jquery-scout.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/jquery/jquery-scout.js
@@ -1515,11 +1515,15 @@ $.fn.selectAllText = function() {
     myWindow = this.window(true),
     element = this[0];
 
+  if (!myDocument || !myDocument.body || !myWindow || !element) {
+    return this;
+  }
+
   if (myDocument.body.createTextRange) {
     range = myDocument.body.createTextRange();
     range.moveToElementText(element);
     range.select();
-    return;
+    return this;
   }
 
   if (myWindow.getSelection) {
@@ -1528,6 +1532,7 @@ $.fn.selectAllText = function() {
     myWindow.getSelection().removeAllRanges();
     myWindow.getSelection().addRange(range);
   }
+
   return this;
 };
 
