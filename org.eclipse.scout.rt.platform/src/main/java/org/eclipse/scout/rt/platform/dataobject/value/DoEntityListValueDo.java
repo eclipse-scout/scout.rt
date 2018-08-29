b3d98caf49b23049e6679ccc693c1e93693e17d8
Paolo Bazzi <paolo.bazzi@bsi-software.com>
2018-08-29 17:30:57 +0200

    Value Data Objects: Added byte[] and List<IDoEntity> value DOs
    Change-Id: I99cae2e79abbf083217357880ef75e7d6f025dc0

---
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/dataobject/value/DoEntityListValueDo.java
@@ -0,0 +1,47 @@
+package org.eclipse.scout.rt.platform.dataobject.value;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import javax.annotation.Generated;
+
+import org.eclipse.scout.rt.platform.BEANS;
+import org.eclipse.scout.rt.platform.dataobject.DoEntity;
+import org.eclipse.scout.rt.platform.dataobject.DoList;
+import org.eclipse.scout.rt.platform.dataobject.IDoEntity;
+import org.eclipse.scout.rt.platform.dataobject.TypeName;
+
+@TypeName("DoEntityListValue")
+public class DoEntityListValueDo extends DoEntity implements IValueDo<List<IDoEntity>> {
+
+  public static DoEntityListValueDo of(IDoEntity value) {
+    return BEANS.get(DoEntityListValueDo.class).withValue(value);
+  }
+
+  @Override
+  public DoList<IDoEntity> value() {
+    return doList(VALUE_ATTRIBUTE);
+  }
+
+  /* **************************************************************************
+   * GENERATED CONVENIENCE METHODS
+   * *************************************************************************/
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public DoEntityListValueDo withValue(Collection<IDoEntity> value) {
+    value().clear();
+    value().get().addAll(value);
+    return this;
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public DoEntityListValueDo withValue(IDoEntity... value) {
+    return withValue(Arrays.asList(value));
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public List<IDoEntity> getValue() {
+    return value().get();
+  }
+}
