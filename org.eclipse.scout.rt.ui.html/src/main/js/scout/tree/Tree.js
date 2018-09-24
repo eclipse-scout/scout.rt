9142070a94a2c692690cef7367e89c247bb46294
André Wegmüller <awe@bsiag.com>
2018-09-24 09:20:21 +0200

    Fixed bug in TreeSpec (and also in Tree#_destroy)
    Because of a wrong expectation the TreeSpec failed on the CI-build. The
    code tried to count properties on a scalar (.length) which makes no
    sense. However, with that line of code fixed it showed that the
    tree.nodes array was not cleaned-up properly when _destory has been
    called.

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/tree/Tree.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/tree/Tree.js
@@ -280,12 +280,13 @@ scout.Tree.prototype._initTreeNodeInternal = function(node, parentNode) {
 scout.Tree.prototype._destroy = function() {
   scout.Tree.parent.prototype._destroy.call(this);
   this.visitNodes(this._destroyTreeNode.bind(this));
+  this.nodes = []; // finally, clear array with root tree-nodes
 };
 
 scout.Tree.prototype._destroyTreeNode = function(node, parentNode) {
   delete this.nodesMap[node.id];
   scout.arrays.remove(this.selectedNodes, node); // ensure deleted node is not in selection list anymore (in case the model does not update the selection)
-  this._removeFromFlatList(node, false); //ensure node is not longer in visible nodes list.
+  this._removeFromFlatList(node, false); // ensure node is not longer in visible nodes list.
   node.destroy();
 
   if (this._onNodeDeleted) { // Necessary for subclasses
