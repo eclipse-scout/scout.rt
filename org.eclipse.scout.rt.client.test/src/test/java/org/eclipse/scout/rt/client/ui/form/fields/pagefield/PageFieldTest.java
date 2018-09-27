8917d193241055460f6c0c1ce7bdd0ea9718d792
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2018-09-27 14:05:34 +0200

    PageField: fix memory leak
    The page of the page field is never disposed. This is mostly no problem,
    because disposing the page field will dispose the search form, detail
    form and table of the page. But: If page.searchActive is set to false,
    the search form will never set on the outline and therefore never set on
    the page field. This means, the search form is started but not closed.
    
    232371

---
+++ org.eclipse.scout.rt.client.test/src/test/java/org/eclipse/scout/rt/client/ui/form/fields/pagefield/PageFieldTest.java
@@ -0,0 +1,65 @@
+package org.eclipse.scout.rt.client.ui.form.fields.pagefield;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.scout.rt.client.testenvironment.TestEnvironmentClientSession;
+import org.eclipse.scout.rt.client.ui.basic.table.AbstractTable;
+import org.eclipse.scout.rt.client.ui.basic.table.ITable;
+import org.eclipse.scout.rt.client.ui.desktop.outline.pages.AbstractPageWithTable;
+import org.eclipse.scout.rt.client.ui.desktop.outline.pages.AbstractPageWithTableTest.TestSearchForm;
+import org.eclipse.scout.rt.client.ui.desktop.outline.pages.IPage;
+import org.eclipse.scout.rt.client.ui.desktop.outline.pages.ISearchForm;
+import org.eclipse.scout.rt.platform.holders.IntegerHolder;
+import org.eclipse.scout.rt.testing.client.runner.ClientTestRunner;
+import org.eclipse.scout.rt.testing.client.runner.RunWithClientSession;
+import org.eclipse.scout.rt.testing.platform.runner.RunWithSubject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+@RunWith(ClientTestRunner.class)
+@RunWithSubject("default")
+@RunWithClientSession(TestEnvironmentClientSession.class)
+public class PageFieldTest {
+
+  @Test
+  public void testDispose() {
+    final IntegerHolder pageDisposedCount = new IntegerHolder(0);
+    final TestSearchForm searchForm = new TestSearchForm();
+    final TestSearchForm searchFormMock = Mockito.spy(searchForm);
+    final ITable table = new AbstractTable() {
+    };
+    AbstractPageWithTable<ITable> page = new AbstractPageWithTable<ITable>() {
+
+      @Override
+      protected ITable createTable() {
+        return table;
+      }
+
+      @Override
+      protected ISearchForm createSearchForm() {
+        return searchFormMock;
+      }
+
+      @Override
+      public void disposeInternal() {
+        super.disposeInternal();
+        pageDisposedCount.setValue(pageDisposedCount.getValue() + 1);
+      }
+    };
+
+    PageField pageField = new PageField();
+    pageField.init();
+    pageField.setPage(page);
+    Mockito.verify(searchFormMock, Mockito.times(0)).disposeFormInternal();
+    assertEquals(0, pageDisposedCount.getValue().intValue());
+
+    pageField.disposeField();
+    Mockito.verify(searchFormMock, Mockito.times(1)).disposeFormInternal();
+    assertEquals(1, pageDisposedCount.getValue().intValue());
+  }
+
+  public static class PageField extends AbstractPageField<IPage> {
+  }
+
+}
