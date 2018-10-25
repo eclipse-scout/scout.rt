0e86019a26388eb7b02472f4b40e8c761672b879
Ivan Motsch <ivan.motsch@bsiag.com>
2018-10-25 14:52:37 +0200

    add missing since tag and copyright header

--- org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/transaction/TransactionCancelledError.java
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/transaction/TransactionCancelledError.java
@@ -1,9 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2018 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.scout.rt.platform.transaction;
 
 import org.eclipse.scout.rt.platform.util.concurrent.AbstractInterruptionError;
 
 /**
- * @since 16.0
+ * @since 16.1
  */
 public class TransactionCancelledError extends AbstractInterruptionError {
   private static final long serialVersionUID = 1L;
