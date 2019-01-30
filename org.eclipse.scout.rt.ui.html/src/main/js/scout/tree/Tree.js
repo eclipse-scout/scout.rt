00a42f6e95cddae871e29fbef2cad4d1dbb01755
Roger Stebler <Roger.Stebler@bsi-software.com>
2019-01-30 14:53:23 +0100

    If tree was manually expanded it should not be collapsed by lazy-expand
    When a tree is lazy-expanded only the relevant nodes are displayed. When
    a node becomes irrelevant it is automatically collapsed. Currently this
    can also collapse nodes that are manually expanded by the user.
    
    This commit changes the tree behavior so that nodes that are manually
    expanded by the user are not affected anymore by the
    lazy-expand feature until it is manually collapsed again by the user.
    
    239002
    
    Change-Id: Ibf8661c224e8a1224762bdb23d0a329176e0d6fd
    Signed-off-by: Roger Stebler <Roger.Stebler@bsi-software.com>
    Tested-by: CI Bot
    Reviewed-by: Beat Schwarzentrub <bsh@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/136014

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/tree/Tree.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/tree/Tree.js
@@ -1265,6 +1265,11 @@ scout.Tree.prototype.setNodeExpanded = function(node, expanded, opts) {
     }
   }
 
+  // Do not allow a lazy expansion/collapse if the node was manually expanded
+  if (lazy && node.expanded && !node.expandedLazy) {
+    return;
+  }
+
   // Optionally collapse all children (recursively)
   if (opts.collapseChildNodes) {
     // Suppress render expansion
