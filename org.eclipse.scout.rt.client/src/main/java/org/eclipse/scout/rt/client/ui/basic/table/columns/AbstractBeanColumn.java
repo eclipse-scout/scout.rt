9e64a06e93ae9f4df8e9e051768945b216a6dc88
Daniel Schmid <daniel.schmid@bsiag.com>
2019-02-10 16:47:42 +0100

    fix sorting of BeanColumn
    the server has to sort the rows of a BeanColumn the same way as the UI
    (see BeanColumn.js)
    
    223907
    
    Change-Id: If494e009b34b24ffa6ff67e3a3e91f6573040ff9
    Signed-off-by: Daniel Schmid <daniel.schmid@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/136470
    Tested-by: CI Bot
    Reviewed-by: Beat Schwarzentrub <bsh@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/136651

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/basic/table/columns/AbstractBeanColumn.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/basic/table/columns/AbstractBeanColumn.java
@@ -13,6 +13,7 @@ package org.eclipse.scout.rt.client.ui.basic.table.columns;
 import org.eclipse.scout.rt.client.ui.basic.cell.Cell;
 import org.eclipse.scout.rt.client.ui.basic.table.ITableRow;
 import org.eclipse.scout.rt.platform.classid.ClassId;
+import org.eclipse.scout.rt.platform.util.StringUtility;
 
 /**
  * This column may be used if the value is relevant for the gui, and not just the display text.
@@ -32,5 +33,37 @@ public abstract class AbstractBeanColumn<VALUE> extends AbstractColumn<VALUE> im
   @Override
   protected void execDecorateCell(Cell cell, ITableRow row) {
     cell.setText(getPlainText(row)); // used for excel export
+  }
+
+  /**
+   * sorting of rows based on this column<br>
+   * in order to get a reasonable sorting the bean needs to implement {@link Comparable} or the subclass of
+   * {@link #AbstractBeanColumn} needs to implement {@link #getPlainText(ITableRow)}
+   */
+  @Override
+  @SuppressWarnings("unchecked")
+  public int compareTableRows(ITableRow r1, ITableRow r2) {
+    VALUE o1 = getValue(r1);
+    VALUE o2 = getValue(r2);
+    if (o1 == null && o2 == null) {
+      return 0;
+    }
+    if (o1 == null) {
+      return -1;
+    }
+    if (o2 == null) {
+      return 1;
+    }
+    if ((o1 instanceof Comparable) && (o2 instanceof Comparable)) {
+      return ((Comparable) o1).compareTo(o2);
+    }
+
+    String plainText1 = getPlainText(r1);
+    String plainText2 = getPlainText(r2);
+    int c = StringUtility.compareIgnoreCase(plainText1, plainText2);
+    if (c != 0) {
+      return c;
+    }
+    return super.compareTableRows(r1, r2);
   }
 }
