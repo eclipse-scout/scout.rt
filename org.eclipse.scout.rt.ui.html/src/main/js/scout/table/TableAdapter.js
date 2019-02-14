c9909360eca077ea9b5b631f8749a16888813bef
Beat Schwarzentrub <bsh@bsiag.com>
2019-02-14 15:18:13 +0100

    Fix typos
    Change-Id: I9bed80940caffae023808c31208642ca41bd2cc3

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/table/TableAdapter.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/table/TableAdapter.js
@@ -297,7 +297,7 @@ scout.TableAdapter.prototype._onWidgetReload = function(event) {
   this._send('reload');
 };
 
-scout.TableAdapter.prototype._onWidgetExportToClipbaord = function(event) {
+scout.TableAdapter.prototype._onWidgetExportToClipboard = function(event) {
   this._send('clipboardExport');
 };
 
@@ -327,7 +327,7 @@ scout.TableAdapter.prototype._onWidgetEvent = function(event) {
   } else if (event.type === 'appLinkAction') {
     this._onWidgetAppLinkAction(event);
   } else if (event.type === 'clipboardExport') {
-    this._onWidgetExportToClipbaord(event);
+    this._onWidgetExportToClipboard(event);
   } else if (event.type === 'reload') {
     this._onWidgetReload(event);
   } else if (event.type === 'filterAdded') {
