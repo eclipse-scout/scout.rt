556fc02a7f3cca3eba095776221a65eaf37fbd0f
Roger Stebler <Roger.Stebler@bsi-software.com>
2019-02-21 14:24:27 +0100

    Unselect submenus when menu is closed
    In a table when a menu is opened and then closed again by clicking
    somewhere else on the screen the menu button also gets unselected again.
    In case the menu is being closed while submenus are expanded these
    submenus are not unselected. This results in opening the submenu again
    when reloading the page or opening an element in the table that has the
    same menu.
    
    Solution is to unselect the expanded submenus when the menu is closed.
    
    241718
    
    Change-Id: I211816e2974eabae2516e2df429d6222f2a97591
    Signed-off-by: Roger Stebler <Roger.Stebler@bsi-software.com>
    Reviewed-on: https://git.eclipse.org/r/136896
    Tested-by: CI Bot
    Reviewed-by: Claudio Guglielmo <claudio.guglielmo@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/137372
    Tested-by: Claudio Guglielmo <claudio.guglielmo@bsiag.com>

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/menu/Menu.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/menu/Menu.js
@@ -154,6 +154,17 @@ scout.Menu.prototype._removeSubMenuItems = function(parentMenu) {
 scout.Menu.prototype._renderSubMenuItems = function(parentMenu, menus) {
   if (this.parent instanceof scout.ContextMenuPopup) {
     this.parent.renderSubMenuItems(parentMenu, menus, true);
+    var closeHandler = function(event) {
+      parentMenu.setSelected(false);
+    }.bind(this);
+    var propertyChangeHandler = function(event) {
+      if (event.propertyName === 'selected' && event.newValue === false) {
+        this.parent.off('close', closeHandler);
+        parentMenu.off('propertyChange', propertyChangeHandler);
+      }
+    }.bind(this);
+    this.parent.on('close', closeHandler);
+    parentMenu.on('propertyChange', propertyChangeHandler);
   } else if (this.parent instanceof scout.Menu) {
     this.parent._renderSubMenuItems(parentMenu, menus);
   }
