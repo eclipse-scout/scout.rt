e1a14c97927e4bc71c249467b778bb130e7ef0d7
Beat Schwarzentrub <bsh@bsiag.com>
2019-03-07 16:57:01 +0100

    TableRow: updates should not override row status INSERTED
    When a row has status INSERTED, it should keep this status when values
    are changed afterwards. Only when a row is NON_CHANGED, updates should
    update the status to UPDATED.
    
    243107
    
    Change-Id: I3b0dda5f70d65ed24df64f3b332b7dff39c029bc

--- org.eclipse.scout.rt.ui.html.test/src/test/js/scout/table/TableSpec.js
+++ org.eclipse.scout.rt.ui.html.test/src/test/js/scout/table/TableSpec.js
@@ -2973,7 +2973,7 @@ describe("Table", function() {
       });
       expect(table.rows[4].status).toBe(scout.TableRow.Status.INSERTED);
       column0.setCellValue(table.rows[4], 99);
-      expect(table.rows[4].status).toBe(scout.TableRow.Status.UPDATED);
+      expect(table.rows[4].status).toBe(scout.TableRow.Status.INSERTED); // Still inserted
     });
   });
 
