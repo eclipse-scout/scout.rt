17e47565ef83a0a23f215e2d51925877d7235efa
André Wegmüller <awe@bsiag.com>
2019-03-11 08:18:01 +0100

    Make sure LabelField doesn't do anything in acceptInput
    Since a LabelField cannot be changed by a user, acceptInput does
    nothing. Otherwise LabelFields could become 'touched', because value and
    displayText of the LabelField don't match.

--- org.eclipse.scout.rt.ui.html.test/src/test/js/scout/form/fields/labelfield/LabelFieldSpec.js
+++ org.eclipse.scout.rt.ui.html.test/src/test/js/scout/form/fields/labelfield/LabelFieldSpec.js
@@ -11,19 +11,16 @@
 describe("LabelField", function() {
   var session;
   var helper;
+  var field;
 
   beforeEach(function() {
     setFixtures(sandbox());
     session = sandboxSession();
     helper = new scout.FormSpecHelper(session);
+    field = helper.createField('LabelField');
   });
 
   describe("HtmlEnabled", function() {
-    var field;
-
-    beforeEach(function() {
-      field = helper.createField('LabelField');
-    });
 
     it("if false, encodes html in display text", function() {
       field.htmlEnabled = false;
@@ -52,6 +49,21 @@ describe("LabelField", function() {
       field.render();
       expect(field.$field.html()).toBe('<b>Hello</b>\nGoodbye');
     });
+  });
+
+  describe("acceptInput", function() {
+
+    /**
+     * If acceptInput wasn't overridden this test would call parseValue and set the touched property.
+     */
+    it("must be a NOP operation", function() {
+      field.setValue("foo");
+      field.markAsSaved();
+      expect(field.touched).toBe(false);
+      field.acceptInput();
+      expect(field.touched).toBe(false);
+    });
+
   });
 
 });
