7efdb96f2e888c7d0a1e2991656fefcdffd91d6c
Matthias Villiger <mvi@bsi-software.com>
2019-03-18 18:02:27 +0100

    Form is marked saved even if the save operation returns an error status
    239731
    
    Change-Id: I36d2696dc65f89c7d3c2d44187f48c165585d1fd

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/form/Form.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/form/Form.js
@@ -358,9 +358,10 @@ scout.Form.prototype.whenSave = function() {
 
 scout.Form.prototype._onLifecycleSave = function() {
   var data = this.exportData();
-  return this._save(data).then(function() {
+  return this._save(data).then(function(status) {
     this.setData(data);
     this.trigger('save');
+    return status;
   }.bind(this));
 };
 
@@ -379,6 +380,12 @@ scout.Form.prototype._validate = function() {
   return scout.Status.ok();
 };
 
+/**
+ * This function is called by the lifecycle, when the 'save' function is called.<p>
+ * The data given to this function is the result of 'exportData' which was called in advance.
+ *
+ * @returns {Promise} promise which may contain a scout.Status specifying if the save operation was successful. The promise may be empty which means the save operation was successful.
+ */
 scout.Form.prototype._save = function(data) {
   return $.resolvedPromise();
 };
