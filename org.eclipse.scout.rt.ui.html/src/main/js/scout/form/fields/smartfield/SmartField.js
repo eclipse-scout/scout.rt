d7b859c083a38647dfb1fe27c214ccec7185e576
André Wegmüller <awe@bsiag.com>
2019-03-27 14:43:46 +0100

    Don't perform lookup when Ctrl+[Key] has been pressed
    This issue occurred when a user clicked into the SmartField and quickly
    after that pressed Ctrl+A. In that case the KeyboardEvent for the A-Key
    does not have the ctrlKey flag set, so we have to treat it as a regular
    key-press. This also applies to copy/paste shortcuts.
    
    The behavior of the event is the same in Firefox and Chrome. It can be
    reproduced with plain-HTML/jQuery. See:
    https://jsfiddle.net/andre_wegmueller/35espnvj/7/
    
    Since we cannot easily change the keyboard-handling of the SmartField we
    double-check if the search-text has changed before we execute the
    lookup-call.
    
    226643

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/form/fields/smartfield/SmartField.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/form/fields/smartfield/SmartField.js
@@ -237,9 +237,13 @@ scout.SmartField.prototype._checkSearchTextChanged = function(searchText) {
   // check if search text has changed since the last search, when it has changed
   // we cannot use the currently selected lookup row, because these proposals are
   // out-dated.
+  return !this._searchTextEquals(searchText, this._lastSearchText);
+};
+
+scout.SmartField.prototype._searchTextEquals = function(searchText, lastSearchText) {
   var a = scout.strings.nullIfEmpty(this._firstTextLine(searchText));
-  var b = scout.strings.nullIfEmpty(this._lastSearchText);
-  return !scout.strings.equalsIgnoreCase(a, b);
+  var b = scout.strings.nullIfEmpty(lastSearchText);
+  return scout.strings.equalsIgnoreCase(a, b);
 };
 
 scout.SmartField.prototype._clearPendingLookup = function() {
@@ -663,6 +667,20 @@ scout.SmartField.prototype._lookupByTextOrAll = function(browse, searchText) {
   // never do a text-lookup if field has dropdown style
   if (this.isDropdown()) {
     browse = true;
+  }
+
+  // this avoids unnecessary lookup-calls when a keyboard event has triggered
+  // the lookupByTextOrAll function but the search-text has not changed #226643.
+  if (!browse) {
+    var lastSearchText = null;
+    if (this._lastSearchText) {
+      lastSearchText = this._lastSearchText;
+    } else if (this.lookupRow) {
+      lastSearchText = this.lookupRow.text;
+    }
+    if (this._searchTextEquals(searchText, lastSearchText)) {
+      return;
+    }
   }
 
   this._clearPendingLookup();
