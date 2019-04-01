228d48a9fa98c9d6984ad922a6b34fb66286ef67
Beat Schwarzentrub <bsh@bsiag.com>
2019-04-01 10:29:57 +0200

    Un-nest accidentally nested classes
    Change-Id: I22ffaf2c2f4c1a97e868da8809182ded78fa02d8

--- org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/ServerCommonsConfigProperties.java
+++ org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/ServerCommonsConfigProperties.java
@@ -96,23 +96,23 @@ public final class ServerCommonsConfigProperties {
     public String getKey() {
       return "scout.cspEnabled";
     }
+  }
 
-    public static class CspDirectiveProperty extends AbstractMapConfigProperty {
+  public static class CspDirectiveProperty extends AbstractMapConfigProperty {
 
-      @Override
-      public String getKey() {
-        return "scout.cspDirective";
-      }
+    @Override
+    public String getKey() {
+      return "scout.cspDirective";
+    }
 
-      @Override
-      @SuppressWarnings("findbugs:VA_FORMAT_STRING_USES_NEWLINE")
-      public String description() {
-        return String.format("Configures individual Content Security Policy (CSP) directives.\n"
-            + "See https://www.w3.org/TR/CSP2/ and the Bean '%s' for more details.\n"
-            + "The value must be provided as a Map.\n"
-            + "Example: scout.cspDirective[img-src]='self' data: https: http://localhost:8086",
-            ContentSecurityPolicy.class.getName());
-      }
+    @Override
+    @SuppressWarnings("findbugs:VA_FORMAT_STRING_USES_NEWLINE")
+    public String description() {
+      return String.format("Configures individual Content Security Policy (CSP) directives.\n"
+          + "See https://www.w3.org/TR/CSP2/ and the Bean '%s' for more details.\n"
+          + "The value must be provided as a Map.\n"
+          + "Example: scout.cspDirective[img-src]='self' data: https: http://localhost:8086",
+          ContentSecurityPolicy.class.getName());
     }
   }
 }
