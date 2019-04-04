4596ecbfcc1d48ecbc69853db6d35ff62a5c6719
Ralf Muri <ralf.muri@bsi-software.com>
2019-04-04 08:09:31 +0200

    Add execution hint for model jobs that cannot be cancelled by the user
    Sometimes it is necessary to either perform all operations of a block or
    none of them. Typically, such a block would be executed within its own
    transaction. However, Scout client itself does not support transactions. 
    
    With a new execution hint, a model job can at least be protected against
    termination by the user (other termination sources are not suppressed).
    
    Change-Id: I3cc603ea08cffe160ee9c7cab0f0dbe9e8354c7b
    Signed-off-by: Ralf Muri <ralf.muri@bsi-software.com>
    Reviewed-on: https://git.eclipse.org/r/140000
    Tested-by: CI Bot
    Reviewed-by: Andi Bur <andi.bur@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/140182
    Reviewed-by: Patrick B??nziger <patrick.baenziger@bsi-software.com>

---
+++ org.eclipse.scout.rt.ui.html.test/src/test/java/org/eclipse/scout/rt/ui/html/UiJobsTest.java
@@ -0,0 +1,106 @@
+package org.eclipse.scout.rt.ui.html;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.scout.rt.client.IClientSession;
+import org.eclipse.scout.rt.client.context.ClientRunContexts;
+import org.eclipse.scout.rt.client.job.ModelJobs;
+import org.eclipse.scout.rt.client.testenvironment.TestEnvironmentClientSession;
+import org.eclipse.scout.rt.platform.BEANS;
+import org.eclipse.scout.rt.platform.BeanMetaData;
+import org.eclipse.scout.rt.platform.IBean;
+import org.eclipse.scout.rt.platform.job.IFuture;
+import org.eclipse.scout.rt.platform.util.concurrent.FutureCancelledError;
+import org.eclipse.scout.rt.testing.platform.runner.PlatformTestRunner;
+import org.eclipse.scout.rt.testing.shared.TestingUtility;
+import org.eclipse.scout.rt.ui.html.json.testing.JsonTestUtility;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(PlatformTestRunner.class)
+public class UiJobsTest {
+
+  private List<IBean<?>> m_beans;
+  private UiSession m_session;
+
+  @Before
+  public void before() {
+    m_beans = TestingUtility.registerBeans(new BeanMetaData(TestEnvironmentClientSession.class));
+    m_session = (UiSession) JsonTestUtility.createAndInitializeUiSession();
+  }
+
+  @After
+  public void after() throws Exception {
+    try {
+      JsonTestUtility.endRequest(m_session);
+    }
+    finally {
+      m_session = null;
+      TestingUtility.unregisterBeans(m_beans);
+    }
+  }
+
+  protected IClientSession clientSession() {
+    return m_session.getClientSession();
+  }
+
+  @Test
+  public void testCancelOrdinaryModelJob() {
+    final CountDownLatch jobStarted = new CountDownLatch(1);
+    final CountDownLatch jobsCancelled = new CountDownLatch(1);
+    IFuture<String> future = ModelJobs.schedule(new Callable<String>() {
+      @Override
+      public String call() throws Exception {
+        jobStarted.countDown();
+        try {
+          jobsCancelled.await();
+        }
+        catch (InterruptedException expected) {
+          // expected
+        }
+        return "completed";
+      }
+    }, ModelJobs.newInput(ClientRunContexts.empty().withSession(clientSession(), true)));
+
+    BEANS.get(UiJobs.class).cancelModelJobs(clientSession());
+    assertTrue(future.isCancelled());
+    jobsCancelled.countDown();
+    try {
+      future.awaitDoneAndGet(5, TimeUnit.MILLISECONDS);
+      fail("Expecting a " + FutureCancelledError.class);
+    }
+    catch (FutureCancelledError expected) {
+      // expected
+    }
+  }
+
+  @Test
+  public void testCancelNotCancellableByUserModelJob() {
+    final CountDownLatch jobStarted = new CountDownLatch(1);
+    final CountDownLatch jobsCancelled = new CountDownLatch(1);
+    IFuture<String> future = ModelJobs.schedule(new Callable<String>() {
+      @Override
+      public String call() throws Exception {
+        jobStarted.countDown();
+        jobsCancelled.await();
+        return "completed";
+      }
+    }, ModelJobs.newInput(ClientRunContexts.empty().withSession(clientSession(), true))
+        .withExecutionHint(ModelJobs.EXECUTION_HINT_NOT_CANCELLABLE_BY_USER));
+
+    BEANS.get(UiJobs.class).cancelModelJobs(clientSession());
+    assertFalse(future.isCancelled());
+    jobsCancelled.countDown();
+    assertEquals("completed", future.awaitDoneAndGet(5, TimeUnit.MILLISECONDS));
+  }
+}
