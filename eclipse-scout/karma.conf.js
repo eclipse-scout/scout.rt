924534f1fa53863f7cca0ae46f3b4ef7f7e3403c
André Wegmüller <awe@bsiag.com>
2019-04-12 17:32:46 +0200

    Karma: added Jasmine Test results to browser output

--- eclipse-scout/karma.conf.js
+++ eclipse-scout/karma.conf.js
@@ -6,13 +6,23 @@ preprocessorObj[specIndex] = ['webpack'];
 
 module.exports = function(config) {
   config.set({
-    // browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
     files: [{
       pattern: specIndex,
       watched: false
     }],
     frameworks: ['jasmine'],
+    // Reporter for "Jasmine Spec Runner" results in browser
+    // https://www.npmjs.com/package/karma-jasmine-html-reporter
+    reporters: ['kjhtml'],
+    client: {
+      // Leave "Jasmine Spec Runner" output visible in browser
+      clearContext: false
+    },
     preprocessors: preprocessorObj,
+    // If true, Karma will start and capture all configured browsers, run tests and then exit with an exit code of 0 or 1
+    // depending on whether all tests passed or any tests failed.
+    singleRun: false,
     /*
     webpack: {
         module: {
