2276f49106f0008dd05e3f40a9dccf32a3dd9a06
André Wegmüller <awe@bsiag.com>
2019-04-18 09:08:05 +0200

    Updated index.js, improved comments

--- eclipse-scout/index.js
+++ eclipse-scout/index.js
@@ -10,17 +10,20 @@
  ******************************************************************************/
 let scout = require('./src/scout/index');
 
-// This is used for our ES5 eclipse-scout.js export, which is used to create
-// simply Scout apps that depend only on the built JS file and don't require
-// the whole build stack with modules, Webpack, etc.
-if (window && !window.scout) {
-  window.scout = Object.assign({}, scout.default, scout);
-}
-
+// This is used for the ES6 build stack, where this module is exported and
+// consumed by other modules. When this piece of code is executed in a browser
+// it will fail, because 'exports' is a read-only property.
 try {
   module.exports = scout;
 } catch (e) {
-  // NOP - silently fail in browser because .exports is read-only
+  // NOP - silently fail
+}
+
+// This is used for our ES5 eclipse-scout.js export, which is used to create
+// simple Scout apps that depend only on the built JS file and don't require
+// the whole build stack with modules, Webpack, etc.
+if (window) {
+  window.scout = Object.assign({}, scout.default, scout);
 }
 
 // FIXME [awe] ES6: review, improve this index.js. Maybe we should have two separate files?
