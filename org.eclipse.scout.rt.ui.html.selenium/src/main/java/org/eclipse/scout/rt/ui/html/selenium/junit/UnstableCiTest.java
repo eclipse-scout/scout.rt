f10db0b1434a9ba6bafa8ce0353f183397cc5501
André Wegmüller <awe@bsiag.com>
2019-05-16 09:38:12 +0200

    Added Javadoc to UnstableCiTest.java

--- org.eclipse.scout.rt.ui.html.selenium/src/main/java/org/eclipse/scout/rt/ui/html/selenium/junit/UnstableCiTest.java
+++ org.eclipse.scout.rt.ui.html.selenium/src/main/java/org/eclipse/scout/rt/ui/html/selenium/junit/UnstableCiTest.java
@@ -14,6 +14,9 @@ package org.eclipse.scout.rt.ui.html.selenium.junit;
  * A category used to mark tests that are unstable on the CI build system but are successful when run locally. When the
  * CI build executes tests, this category is excluded. So you should only apply this category when there's no way to fix
  * the test for CI build - usually there's some sort of timing problem, when a test fails on CI build.
+ * <p>
+ * <strong>IMPORTANT</strong>: when you must use this category, always document <i>why</i> you had to use it. This will
+ * help to analyze or solve the issue in a later release.
  */
 public class UnstableCiTest implements UiTestCategory {
 
