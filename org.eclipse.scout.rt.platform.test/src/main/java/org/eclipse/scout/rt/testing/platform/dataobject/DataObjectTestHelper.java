4a4bc763ad7a42db846844fe9110fda4a602e4d8
Paolo Bazzi <paolo.bazzi@bsi-software.com>
2019-05-22 17:44:38 +0200

    Added ScoutAssert.assertEqualsWithComparisonFailure() method
    Added assertEqualsWithComparisonFailure method comparing two objects for
    equality and throwing a ComparisonFailure exception providing a textual
    comparable representation using the toString() method of the expected
    and actual objects.
    
    Conflicts:
    	org.eclipse.scout.rt.platform.test/src/test/java/org/eclipse/scout/rt/testing/platform/dataobject/DataObjectTestHelperTest.java
    
    Change-Id: I2329a272b537a73c0e9760f3dd1f0b80dd3d4b84
    Reviewed-on: https://git.eclipse.org/r/142606
    Tested-by: CI Bot
    Reviewed-by: Ralph Steiner <rst@bsiag.com>
    Reviewed-on: https://git.eclipse.org/r/142711
    Reviewed-by: Paolo Bazzi <paolo.bazzi@bsi-software.com>

--- org.eclipse.scout.rt.platform.test/src/main/java/org/eclipse/scout/rt/testing/platform/dataobject/DataObjectTestHelper.java
+++ org.eclipse.scout.rt.platform.test/src/main/java/org/eclipse/scout/rt/testing/platform/dataobject/DataObjectTestHelper.java
@@ -10,26 +10,20 @@
  ******************************************************************************/
 package org.eclipse.scout.rt.testing.platform.dataobject;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Objects;
-
 import org.eclipse.scout.rt.platform.ApplicationScoped;
-import org.eclipse.scout.rt.platform.dataobject.DoEntity;
-import org.eclipse.scout.rt.platform.dataobject.DoList;
-import org.eclipse.scout.rt.platform.dataobject.DoNode;
 import org.eclipse.scout.rt.platform.dataobject.IDataObject;
-import org.eclipse.scout.rt.platform.dataobject.IDoEntity;
-import org.eclipse.scout.rt.platform.util.Assertions;
-import org.eclipse.scout.rt.platform.util.ObjectUtility;
+import org.eclipse.scout.rt.testing.platform.util.ScoutAssert;
 import org.junit.ComparisonFailure;
 
 /**
  * Helper for unit tests dealing with {@link IDataObject}.
+ *
+ * @deprecated use {@link ScoutAssert#assertEqualsWithComparisonFailure(Object, Object)} or
+ *             {@link ScoutAssert#assertEqualsWithComparisonFailure(String, Object, Object)} instead
+ *             <p>
+ *             FIXME [10.0] pbz: Remove after up-merge
  */
+@Deprecated
 @ApplicationScoped
 public class DataObjectTestHelper {
 
@@ -44,7 +38,7 @@ public class DataObjectTestHelper {
    *          actual value
    */
   public void assertEquals(Object expected, Object actual) {
-    assertEquals(null, expected, actual);
+    ScoutAssert.assertEqualsWithComparisonFailure(null, expected, actual);
   }
 
   /**
@@ -60,155 +54,6 @@ public class DataObjectTestHelper {
    *          actual value
    */
   public void assertEquals(String message, Object expected, Object actual) {
-    if (!ObjectUtility.equals(expected, actual)) {
-      String cleanMessage = message == null ? "objects not equals" : message;
-      throw new ComparisonFailure(cleanMessage, Objects.toString(expected), Objects.toString(actual));
-    }
-  }
-
-  /**
-   * Asserts (deep) equality for specified {@link DoEntity} objects and additionally asserts, that concrete
-   * {@link DoEntity} class of expected entity and class of actual {@link DoEntity} is identical.
-   *
-   * @deprecated use {@link #assertEquals(Object, Object)} instead
-   */
-  @Deprecated
-  public void assertDoEntityEquals(IDoEntity expected, IDoEntity actual) {
-    Assertions.assertEquals(expected, actual);
-
-    // assert all attribute names are set correctly to be equals to the corresponding map key
-    assertMapKeyEqualsAttributeName(actual);
-    assertMapKeyEqualsAttributeName(expected);
-  }
-
-  /**
-   * Asserts (deep) equality for specified {@link DoList} objects and additionally asserts, that all nested concrete
-   * {@link DoEntity} classes within the specified lists are identical.
-   *
-   * @deprecated use {@link #assertEquals(Object, Object)} instead
-   */
-  @Deprecated
-  public void assertDoListEquals(DoList<?> expected, DoList<?> actual) {
-    assertDoListEquals(expected, actual, true);
-  }
-
-  /**
-   * Asserts (deep) equality for specified {@link DoList} objects.
-   *
-   * @param assertClassEquals
-   *          if {@code true} concrete class of all nested {@link DoEntity}'s must be the identical
-   * @deprecated use {@link #assertEquals(Object, Object)} instead
-   */
-  @Deprecated
-  public void assertDoListEquals(DoList<?> expected, DoList<?> actual, boolean assertClassEquals) {
-    if (!equalsObject(expected, actual, assertClassEquals)) {
-      assertFail(expected, actual);
-    }
-  }
-
-  /**
-   * @deprecated Method will be removed in a later release
-   */
-  @Deprecated
-  protected void assertMapKeyEqualsAttributeName(IDoEntity actual) {
-    for (String key : actual.allNodes().keySet()) {
-      assertEquals("key of attribute map is not equals to node attribute name", key, actual.getNode(key).getAttributeName());
-    }
-  }
-
-  /**
-   * Asserts (deep) equality of two {@link Object}, taking into account nested {@link DoNode} elements which requires
-   * custom equality check.
-   *
-   * @deprecated use {@link #assertEquals(Object, Object)} instead
-   */
-  @Deprecated
-  public void assertObjectEquals(Object expected, Object actual, boolean assertClassEquals) {
-    if (!equalsObject(expected, actual, assertClassEquals)) {
-      assertFail(expected, actual);
-    }
-  }
-
-  /**
-   * @deprecated Method will be removed in a later release
-   */
-  @Deprecated
-  protected void assertFail(Object expected, Object actual) {
-    throw new ComparisonFailure("Objects not equal", Objects.toString(expected), Objects.toString(actual));
-  }
-
-  /**
-   * Check equality of two {@link Object}, taking into account nested {@link DoNode} elements which requires custom
-   * equality check.
-   * <p>
-   *
-   * @deprecated use {@link #assertEquals(Object, Object)} instead
-   * @return {@code true} if equal
-   */
-  @Deprecated
-  public boolean equalsObject(Object expected, Object actual, boolean assertClassEquals) {
-    if (expected == null || actual == null) {
-      return expected == actual;
-    }
-
-    if (expected instanceof IDoEntity) {
-      if (assertClassEquals && expected.getClass() != actual.getClass()) {
-        return false;
-      }
-      return equalsObject(((IDoEntity) expected).allNodes(), ((IDoEntity) actual).allNodes(), assertClassEquals);
-    }
-    else if (expected instanceof DoNode) {
-      return expected.getClass() == actual.getClass() && equalsObject(((DoNode<?>) expected).get(), ((DoNode<?>) actual).get(), assertClassEquals);
-    }
-    else if (expected instanceof Collection) {
-      Collection<?> expectedCollection = (Collection<?>) expected;
-      Collection<?> actualCollection = (Collection<?>) actual;
-      if (expectedCollection.size() != actualCollection.size()) {
-        return false;
-      }
-      if (expected instanceof List) {
-        Iterator<?> expectedIter = expectedCollection.iterator();
-        Iterator<?> actualIter = actualCollection.iterator();
-        while (expectedIter.hasNext()) {
-          if (!equalsObject(expectedIter.next(), actualIter.next(), assertClassEquals)) {
-            return false;
-          }
-        }
-        return true;
-      }
-      else {
-        for (Object expectedElement : expectedCollection) {
-          boolean found = false;
-          Iterator<?> actualIter = actualCollection.iterator();
-          while (actualIter.hasNext() && !found) {
-            found = equalsObject(expectedElement, actualIter.next(), assertClassEquals);
-          }
-          if (!found) {
-            return false;
-          }
-        }
-        return true;
-      }
-    }
-    else if (expected instanceof Map) {
-      Map<?, ?> expectedMap = (Map<?, ?>) expected;
-      Map<?, ?> actualMap = (Map<?, ?>) actual;
-      if (expectedMap.size() != actualMap.size()) {
-        return false;
-      }
-
-      for (Entry<?, ?> expectedEntry : expectedMap.entrySet()) {
-        if (!equalsObject(expectedEntry.getValue(), actualMap.get(expectedEntry.getKey()), assertClassEquals)) {
-          return false;
-        }
-      }
-      return true;
-    }
-    else if (expected.getClass().isArray()) {
-      return Objects.deepEquals(expected, actual);// delegates to Arrays.deepEquals0()
-    }
-    else {
-      return expected.equals(actual);
-    }
+    ScoutAssert.assertEqualsWithComparisonFailure(message, expected, actual);
   }
 }
