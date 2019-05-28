cab38144bb80dc0705295bb95421e26c6926386a
André Wegmüller <awe@bsiag.com>
2019-05-28 10:17:41 +0200

    Write warn, error and fatal to console
    Even when no logger is active. This helps to make errors visible, that
    have been catched. This is especially important for errors that occur
    within a Promise. Without logging this type of error is hard to track.
    
    Pass option 'resourceUrl' to logging.js - so we can provide an URL,
    which is important when resources are not on the same host as the
    index.html. Which is the case in BSI Briefcase.

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/logging/NullLogger.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/logging/NullLogger.js
@@ -1,39 +1,70 @@
-scout.NullLogger = function() {
+/*******************************************************************************
+ * Copyright (c) 2014-2017 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
 
+/**
+ * A NullLogger instance is installed when Log4Javascript is not active (the popup
+ * is not opened). In that case we write WARN, ERROR and FATAL to the console output
+ * because otherwise some errors are hard to track. This is true especially for errors
+ * that occur in a Promise. A developer should at least log these errors.
+ */
+scout.NullLogger = function() {
 };
 
 scout.NullLogger.prototype = {
-  trace: function () {
+  trace: function() {},
+  debug: function() {},
+  info: function() {},
+  warn: function() {
+    this._log('WARN', scout.objects.argumentsToArray(arguments));
   },
-  debug: function () {
+  error: function(logArgs) {
+    this._log('ERROR', scout.objects.argumentsToArray(arguments));
   },
-  info: function () {
+  fatal: function(logArgs) {
+    this._log('FATAL', scout.objects.argumentsToArray(arguments));
   },
-  warn: function () {
-  },
-  error: function () {
-  },
-  fatal: function () {
-  },
-  isEnabledFor: function () {
+  isEnabledFor: function() {
     return false;
   },
-  isTraceEnabled: function () {
+  isTraceEnabled: function() {
     return false;
   },
-  isDebugEnabled: function () {
+  isDebugEnabled: function() {
     return false;
   },
-  isInfoEnabled: function () {
+  isInfoEnabled: function() {
     return false;
   },
-  isWarnEnabled: function () {
+  isWarnEnabled: function() {
     return false;
   },
-  isErrorEnabled: function () {
+  isErrorEnabled: function() {
     return false;
   },
-  isFatalEnabled: function () {
+  isFatalEnabled: function() {
     return false;
+  },
+  _log: function(level, logArgs) {
+    if (logArgs.length > 0) {
+      logArgs[0] = this._formatTime() + ' [' + level + '] ' + logArgs[0];
+    }
+    if (window.console) {
+      window.console.error.apply(window.console, logArgs);
+    }
+  },
+  _formatTime: function() {
+    var date = new Date();
+    return scout.strings.padZeroLeft(date.getHours(), 2) + ':' +
+      scout.strings.padZeroLeft(date.getMinutes(), 2) + ':' +
+      scout.strings.padZeroLeft(date.getSeconds(), 2) + '.' +
+      scout.strings.padZeroLeft(date.getMilliseconds(), 3);
   }
 };
