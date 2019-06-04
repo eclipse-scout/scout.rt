6ef1374b554eb0ef264367088448a60df0b88e6b
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2019-06-04 17:31:49 +0200

    Add scout.ajax utility to make rest calls easier

--- org.eclipse.scout.rt.ui.html/src/main/js/scout-module.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout-module.js
@@ -62,6 +62,9 @@
   __include("scout/util/aggregation.js");
   __include("scout/util/webstorage.js");
   __include("scout/util/cookies.js");
+  __include("scout/ajax/ajax.js");
+  __include("scout/ajax/AjaxCall.js");
+  __include("scout/ajax/AjaxError.js");
   // Lookup calls
   __include("scout/lookup/LookupCall.js");
   __include("scout/lookup/LookupRow.js");
@@ -75,7 +78,6 @@
   __include("scout/code/codes.js");
   __include("scout/code/CodeLookupCall.js"); // requires LookupCall.js
   // Session related
-  __include("scout/session/AjaxCall.js");
   __include("scout/session/BackgroundJobPollingSupport.js");
   __include("scout/session/BusyIndicator.js");
   __include("scout/session/RemoteEvent.js");
