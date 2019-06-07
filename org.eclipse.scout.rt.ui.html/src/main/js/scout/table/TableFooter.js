0bf2bbc363a2c82fd88c173279ff8a8b2f699ea1
Beat Schwarzentrub <bsh@bsiag.com>
2019-06-07 10:54:22 +0200

    TableFooter: fix info animation
    When an info element is made visible while the 'hiding' animation is
    still running, the resulting size is wrong. This is because the
    animation is stopped but the 'hiding' data attribute is still present.
    
    This can happen when the column structure changes while a text filter
    is present. Because the filter is first removed, the filter $info
    element will shortly be made invisible. After the filter is re-applied,
    it is made visible again.
    
    Fix: after the 'hiding' animation is stopped, the 'hiding' data
    attribute must be removed as well. This must be done manually, because
    calling $el.stop() does _not_ run the "complete" callback function.
    (See http://api.jquery.com/stop/: "Callback functions are not called")
    
    248980
    
    Change-Id: Ie9528f85d1a9445c43d14a89d579f40d7bc6bfd2
    Reviewed-on: https://git.eclipse.org/r/143518
    Tested-by: CI Bot
    Reviewed-by: Claudio Guglielmo <claudio.guglielmo@bsiag.com>

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/table/TableFooter.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/table/TableFooter.js
@@ -396,7 +396,7 @@ scout.TableFooter.prototype._setInfoVisible = function($info, visible, complete)
     if ($info[0].style.width === '') {
       $info.cssWidth(0);
     }
-    $info.show().stop().widthToContent(animationOpts);
+    $info.stop().removeData('hiding').show().widthToContent(animationOpts);
   } else {
     // Mark element as hiding so that the layout does not try to resize it
     $info.data('hiding', true);
