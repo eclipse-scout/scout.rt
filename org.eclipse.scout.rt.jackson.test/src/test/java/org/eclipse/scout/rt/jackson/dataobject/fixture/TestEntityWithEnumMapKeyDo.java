2bf7a82d8266096d11d633503d50999b787470bf
Nino Weingart <nino.weingart@bsi-software.com>
2019-06-27 12:51:13 +0200

    Fixes for enum as map keys for jackson (de-) serialization
    Change-Id: I37c9c4248f43a78c7b598faf60ef76318d18cf54
    Signed-off-by: Nino Weingart <nino.weingart@bsi-software.com>
    Reviewed-on: https://git.eclipse.org/r/144997
    Tested-by: CI Bot
    Reviewed-by: Paolo Bazzi <paolo.bazzi@bsi-software.com>

---
+++ org.eclipse.scout.rt.jackson.test/src/test/java/org/eclipse/scout/rt/jackson/dataobject/fixture/TestEntityWithEnumMapKeyDo.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2019 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.jackson.dataobject.fixture;
+
+import java.util.Map;
+
+import javax.annotation.Generated;
+
+import org.eclipse.scout.rt.dataobject.DoEntity;
+import org.eclipse.scout.rt.dataobject.DoValue;
+import org.eclipse.scout.rt.dataobject.TypeName;
+import org.eclipse.scout.rt.dataobject.fixture.FixtureEnum;
+
+@TypeName("scout.TestEntityWithEnumMapKey")
+public class TestEntityWithEnumMapKeyDo extends DoEntity {
+
+  public DoValue<Map<FixtureEnum, String>> map() {
+    return doValue("map");
+  }
+
+  /* **************************************************************************
+   * GENERATED CONVENIENCE METHODS
+   * *************************************************************************/
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public TestEntityWithEnumMapKeyDo withMap(Map<FixtureEnum, String> map) {
+    map().set(map);
+    return this;
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public Map<FixtureEnum, String> getMap() {
+    return map().get();
+  }
+}
