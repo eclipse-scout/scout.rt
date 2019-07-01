eclipse-scout/scout.rt

commit 6af13392e32d3b426411f2c4838abd7aff7e092b

Fixed JS error in editable table which occurred 'on detach'
This bugfix solves the issue where a user has chosen a lookup row in an
editable table (smart column) and then clicked on an outline-tab,
without pressing ENTER or TAB first, this caused a JS error, because the
popup has been destroyed too early.

Added additional ticks in Jasmine specs, required because of the new
promise used in _destroyCellEditorPopup.

249385

org.eclipse.scout.rt.ui.html/src/main/js/scout/table/Table.js
@@ -4651,9 +4651,14 @@ scout.Table.prototype._destroyCellEditorPopup = function() {
   // When a cell editor popup is open and table is detached, we close the popup immediately
   // and don't wait for the model event 'endCellEdit'. By doing this we can avoid problems
   // with invalid focus contexts.
+  // However: when 'completeCellEdit' is already scheduled, we must wait because Scout classic
+  // must send a request to the server first #249385.
   if (this.cellEditorPopup) {
-    this.cellEditorPopup.destroy();
-    this.cellEditorPopup = null;
+    this.cellEditorPopup.waitForCompleteCellEdit()
+      .then(function() {
+        this.cellEditorPopup.destroy();
+        this.cellEditorPopup = null;
+      }.bind(this));
   }
 };
 

org.eclipse.scout.rt.ui.html.test/src/test/js/scout/table/editor/CellEditorSpec.js
@@ -264,6 +264,7 @@ describe("CellEditor", function() {
 
       table.completeCellEdit();
       expect(table.endCellEdit).toHaveBeenCalledWith(field, true);
+      jasmine.clock().tick();
       expect($findPopup().length).toBe(0);
     });
 
@@ -312,6 +313,7 @@ describe("CellEditor", function() {
 
       table.cancelCellEdit();
       expect(table.endCellEdit).toHaveBeenCalledWith(field);
+      jasmine.clock().tick();
       expect($findPopup().length).toBe(0);
     });
 
@@ -356,6 +358,7 @@ describe("CellEditor", function() {
       expect(field.destroyed).toBe(false);
 
       table.endCellEdit(field);
+      jasmine.clock().tick();
       expect($findPopup().length).toBe(0);
       expect($findPopup().find('.form-field').length).toBe(0);
       expect(popup.rendered).toBe(false);
@@ -466,6 +469,7 @@ describe("CellEditor", function() {
       jasmine.clock().tick();
       expect(table.cellEditorPopup).toBeTruthy();
       table._beforeDetach(); // called by parent.detach();
+      jasmine.clock().tick();
       expect(table.cellEditorPopup).toBe(null);
     });
 
@@ -476,6 +480,7 @@ describe("CellEditor", function() {
       jasmine.clock().tick();
       expect(table.cellEditorPopup).toBeTruthy();
       table.remove();
+      jasmine.clock().tick();
       expect(table.cellEditorPopup).toBe(null);
     });
 

org.eclipse.scout.rt.ui.html.test/src/test/js/scout/table/editor/CellEditorAdapterSpec.js
@@ -91,6 +91,7 @@ describe("CellEditorAdapter", function() {
       };
       session._processSuccessResponse(message);
 
+      jasmine.clock().tick();
       expect($findPopup().length).toBe(0);
       expect($findPopup().find('.form-field').length).toBe(0);
       expect(popup.rendered).toBe(false);

commit 61da85917c8b7473c9f71bb7c8ef0556bd908230

Table: fix exception when closing cell editor
1. Open cell editor
2. Type some text and close the editor -> Exception

Uncaught Error: Widget is destroyed: Widget[id=305
objectType=StringField rendered=false]
    at g.StringField.g.Widget.render
    at g.CellEditorPopup._render

Reason: update event comes after endCellEdit event. InstallRow tries to
start a new cell editor because cellEditorPopup is not null yet. This
bug was introduced with commit 6af13392e32d3b426411f2c4838abd7aff7e092b.

Solution: make sure cell editor is destroyed as soon as possible. The
previous commit (1fc11b7a6a886fbe3cfcee5025bd9bb9cb32527a) actually
already fixes the bug by making sure, end cell
edit always comes after update row event. But this fix is still
necessary to prevent a flickering when closing the editor (flickering
happens because the cell editor is opened and closed again after it was
closed the first time).

250293

org.eclipse.scout.rt.ui.html/src/main/js/scout/table/Table.js
@@ -4654,11 +4654,20 @@ scout.Table.prototype._destroyCellEditorPopup = function() {
   // However: when 'completeCellEdit' is already scheduled, we must wait because Scout classic
   // must send a request to the server first #249385.
   if (this.cellEditorPopup) {
-    this.cellEditorPopup.waitForCompleteCellEdit()
-      .then(function() {
-        this.cellEditorPopup.destroy();
-        this.cellEditorPopup = null;
-      }.bind(this));
+    var destroyEditor = function() {
+      this.cellEditorPopup.destroy();
+      this.cellEditorPopup = null;
+    }.bind(this);
+
+    var promise = this.cellEditorPopup.waitForCompleteCellEdit();
+    if (promise.state() === 'resolved') {
+      // Do it immediately if promise has already been resolved. 
+      // This makes sure updateRow does not immediately reopen the editor after closing.
+      // At least for Scout JS, for Scout Classic it prevents flickering (endCellEdit comes after updateRows, but updateRows does not know whether the editor is closing so it will reopen it)
+      destroyEditor();
+    } else {
+      promise.then(destroyEditor);
+    }
   }
 };
 

org.eclipse.scout.rt.ui.html.test/src/test/js/scout/table/editor/CellEditorSpec.js
@@ -278,6 +278,20 @@ describe("CellEditor", function() {
       expect(table.rows[0].cells[0].value).toBe('my new value');
     });
 
+    it("does not reopen the editor again", function() {
+      table.columns[0].setEditable(true);
+      table.prepareCellEdit(table.columns[0], table.rows[0]);
+      jasmine.clock().tick();
+      table.cellEditorPopup.cell.field.setValue('my new value');
+
+      var triggeredStartCellEditEvent = null;
+      table.on('startCellEdit', function(event) {
+        triggeredStartCellEditEvent = event;
+      });
+      table.completeCellEdit();
+      // CompleteCellEdit triggers updateRows which would reopen the editor -> this must not happen if the editor was closed
+      expect(triggeredStartCellEditEvent).toBe(null);
+    });
   });
 
   describe("cancelCellEdit", function() {