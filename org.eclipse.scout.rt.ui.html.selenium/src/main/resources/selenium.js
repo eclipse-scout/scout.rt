ecd276e7ac07682b2089ac9fe5460d69ae1acc5b
Beat Schwarzentrub <bsh@bsiag.com>
2019-07-03 18:47:05 +0200

    selenium.js: fix potential error in delayCancelRequest()
    The method delayCancelRequest() replaces the default implementation of
    _sendCancelRequest. In some rare test cases, the object found in
    scout.sessions[0] was apparently not the expected session object,
    causing a JS error ("_sendCancelRequest is not a function").
    
    To prevent this problem, the binding was changed to "this". This works
    because for unbound function (such as the replaced function on the
    prototype), this is always the object where the method was called on.
    
    Additionally, SeleniumJavaScript.java was improved. When injecting
    selenium.js in the DOM, line breaks are now preserved. This allows to
    use // comments without breaking the code.
    
    Change-Id: I67fc1dcb073270f7e143b846171f572a53db8f06
    Reviewed-on: https://git.eclipse.org/r/145417
    Tested-by: CI Bot
    Reviewed-by: Beat Schwarzentrub <bsh@bsiag.com>

--- org.eclipse.scout.rt.ui.html.selenium/src/main/resources/selenium.js
+++ org.eclipse.scout.rt.ui.html.selenium/src/main/resources/selenium.js
@@ -9,23 +9,16 @@
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
 /**
- * This file adds some JavaScript patches which are required in our Selenium test suite.
+ * Utility functions that are used in the Selenium test suite (see SeleniumJavaScript.java).
  */
 scout.selenium = {
 
-  /* Properties */
   origSendCancelRequest: scout.Session.prototype._sendCancelRequest,
 
-  /* Functions */
   delayCancelRequest: function(delayMs) {
-    var scoutSession = scout.sessions[0];
-    if (!scoutSession) {
-      throw new Error('Scout session not available');
-    }
-    var func = this.origSendCancelRequest.bind(scoutSession);
-
+    var origFunc = this.origSendCancelRequest;
     scout.Session.prototype._sendCancelRequest = function() {
-      setTimeout(func, delayMs);
+      setTimeout(origFunc.bind(this), delayMs);
     };
   },
 
@@ -35,9 +28,9 @@ scout.selenium = {
 
   setSupportsTouch: function(touch) {
     scout.device.features['_touch'] = touch;
-    /* Also load FastClick because without it, we would not test the real thing */
-    /* However: once FastClick is loaded, we cannot unload it. This means you   */
-    /* should not switch touch mode in the middle of a selenium test.           */
+    // Also load FastClick because without it, we would not test the real thing
+    // However: once FastClick is loaded, we cannot unload it. This means you
+    // should not switch touch mode in the middle of a selenium test.
     if (touch) {
       scout.device._loadFastClickDeferred();
     }
