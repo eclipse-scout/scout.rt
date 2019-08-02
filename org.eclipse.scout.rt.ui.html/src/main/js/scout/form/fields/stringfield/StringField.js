4aa65a8e19facdca39706d819b83abfa1f5b8e73
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2019-08-02 16:29:45 +0200

    StringField: allow container to scroll when end of scrollarea is reached
    Currently, when scrolling in a multiline string field, the container will never scroll (at least with Firefox, with Chrome and Edge they will because of the hybrid scrolling, see scrollbars.js). This is consistent to how other fields behave. But the string field is the only scrollable field which is used in an editable table. In that case it can be quite confusing that the table itself will never scroll when using the mousewheel over the edited (multiline) cell.
    
    The browser itself handles it in a different way: if the end of the scroll area is reached (top or bottom), the outer scrollables will scroll. In order to solve the issue with the table the same behavior has now been implemented for StringFields.
    
    Other fields still behave as before, but it is to discuss if all scrollables (=Scrollbar.js) should be changed as well.
    
    241943
    
    Change-Id: If075cef16ce1d47d7400052611f950aee7378bf7
    Reviewed-on: https://git.eclipse.org/r/146974
    Tested-by: CI Bot
    Reviewed-by: Claudio Guglielmo <claudio.guglielmo@bsiag.com>

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/form/fields/stringfield/StringField.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/form/fields/stringfield/StringField.js
@@ -87,10 +87,7 @@ scout.StringField.prototype._makeMultilineField = function() {
   }.bind(this);
 
   return this.$parent.makeElement('<textarea>')
-    .on('DOMMouseScroll mousewheel', function(event) {
-      // otherwise scout.Scrollbar.prototype would handle this event for scrollable group boxes and prevent scrolling on textarea
-      event.stopPropagation();
-    })
+    .on('DOMMouseScroll mousewheel', this._onMouseWheel.bind(this))
     .on('mousedown', mouseDownHandler)
     .on('focus', function(event) {
       this.$field.off('mousedown', mouseDownHandler);
@@ -120,6 +117,24 @@ scout.StringField.prototype._onFieldBlur = function() {
     // Restore obfuscated display text.
     this.$field.val(this.displayText);
   }
+};
+
+scout.StringField.prototype._onMouseWheel = function(event) {
+  event = event.originalEvent || this.$container.window(true).event.originalEvent;
+  var delta = event.wheelDelta ? -event.wheelDelta : event.detail;
+  var scrollTop = this.$field[0].scrollTop;
+  if (delta < 0 && scrollTop === 0) {
+    // StringField is scrolled to the very top -> parent may scroll
+    return;
+  }
+  var maxScrollTop = this.$field[0].scrollHeight - this.$field[0].clientHeight;
+  if (delta > 0 && scrollTop >= maxScrollTop - 1) { // -1 because it can sometimes happen that scrollTop is maxScrollTop -1 or +1, just because clientHeight and scrollHeight are rounded values
+    // StringField is scrolled to the very bottom -> parent may scroll
+    this.$field[0].scrollTop = maxScrollTop; // Ensure it is really at the bottom (not -1px above)
+    return;
+  }
+  // Don't allow others to scroll (e.g. scout.Scrollbar) while scrolling in the text area
+  event.stopPropagation();
 };
 
 scout.StringField.prototype._renderProperties = function() {
