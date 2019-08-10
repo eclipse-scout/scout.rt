9066e319fa6b60a7e0a2d83129f14333ae3d649c
Matthias Villiger <mvi@bsi-software.com>
2019-08-10 15:55:35 +0200

    Update copyright file header to block comment

--- com.bsiag.scout.rt.ui.html/src/main/java/com/bsiag/scout/rt/ui/html/json/form/fields/chartfield/JsonChartField.java
+++ com.bsiag.scout.rt.ui.html/src/main/java/com/bsiag/scout/rt/ui/html/json/form/fields/chartfield/JsonChartField.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * Copyright (c) 2014-2015 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the BSI CRM Software License v1.0
@@ -6,7 +6,7 @@
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
- ******************************************************************************/
+ */
 package com.bsiag.scout.rt.ui.html.json.form.fields.chartfield;
 
 import org.eclipse.scout.rt.ui.html.IUiSession;
