fb9a9f03ac7f7860f8541ae6bcec2f727d77cef5
Arthur van Dorp <Arthur.vanDorp@bsi-software.com>
2019-08-27 14:34:57 +0200

    No parallel builds
    Parallel builds affect tests.
    
    Change-Id: I2c98a5d6db1cfcd8cb5ff34061ba133cb2029021
    Signed-off-by: Arthur van Dorp <Arthur.vanDorp@bsi-software.com>
    Reviewed-on: https://git.eclipse.org/r/148443

--- shell_scripts/build.sh
+++ shell_scripts/build.sh
@@ -6,32 +6,31 @@ BASEDIR=$(dirname $0)
 function usage {
   cat << EOF
 
-	${PRG} [-h] -r <RELEASE>
+  ${PRG} [-h] -r <RELEASE>
 
-	-h				- Usage info
+  -h        - Usage info
 
-	Example: ${PRG} -f <module>/pom.xml
+  Example: ${PRG} -f <module>/pom.xml
 
 EOF
 }
 
 function get_options {
-	# Loop until all parameters are used up
-	while [ "$1" != "" ]; do
-		case $1 in
-			-h | --help )				usage
-										exit 7
-										;;
-			* )							break;;
-		esac
-		shift
-	done
-	_MAVEN_OPTS="$_MAVEN_OPTS $@"
+  # Loop until all parameters are used up
+  while [ "$1" != "" ]; do
+    case $1 in
+      -h | --help )        usage
+                    exit 7
+                    ;;
+      * )              break;;
+    esac
+    shift
+  done
+  _MAVEN_OPTS="$_MAVEN_OPTS $@"
 }
 get_options $*
 
 _MAVEN_OPTS="$_MAVEN_OPTS -e -B"
 
 # Parallel executions of maven modules and tests.
-# Half of CPU core are used in to keep other half for OS and other programs.
-mvn clean install -T0.5C -Dmaster_test_forkCount=0.5C -f org.eclipse.scout.rt $_MAVEN_OPTS
+mvn clean install -T1 -Dmaster_test_forkCount=1 -f org.eclipse.scout.rt $_MAVEN_OPTS
