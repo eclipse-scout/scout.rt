621eb18ac437e067e9d37819f4d55b358202c2ec
Matthias Villiger <mvi@bsi-software.com>
2019-09-03 17:03:44 +0200

    Remove unnecessary import

--- org.eclipse.scout.migration.ecma6/src/main/java/org/eclipse/scout/migration/ecma6/pathfilter/ExcludeMigignoreFilter.java
+++ org.eclipse.scout.migration.ecma6/src/main/java/org/eclipse/scout/migration/ecma6/pathfilter/ExcludeMigignoreFilter.java
@@ -11,7 +11,6 @@
 package org.eclipse.scout.migration.ecma6.pathfilter;
 
 import java.io.IOException;
-import java.io.ObjectInputFilter.Config;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -23,38 +22,37 @@ import javax.annotation.PostConstruct;
 
 import org.eclipse.scout.migration.ecma6.Configuration;
 import org.eclipse.scout.migration.ecma6.PathInfo;
-import org.eclipse.scout.migration.ecma6.task.T5000_ReplaceLocalReferences;
 import org.eclipse.scout.rt.platform.exception.ProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-public class ExcludeMigignoreFilter implements  IMigrationExcludePathFilter {
+public class ExcludeMigignoreFilter implements IMigrationExcludePathFilter {
   private static final Logger LOG = LoggerFactory.getLogger(ExcludeMigignoreFilter.class);
 
   private Set<Path> m_ignoredFiles = new HashSet<>();
+
   @PostConstruct
-  public void setup(){
+  public void setup() {
     Path migignoreFile = Configuration.get().getSourceModuleDirectory().resolve(".migignore");
-    if(Files.exists(migignoreFile)){
+    if (Files.exists(migignoreFile)) {
       try {
         m_ignoredFiles = readMigignore(migignoreFile);
       }
       catch (IOException e) {
-        throw new ProcessingException("Could not parse migignore file: "+migignoreFile);
+        throw new ProcessingException("Could not parse migignore file: " + migignoreFile);
       }
     }
   }
 
   private Set<Path> readMigignore(Path migignoreFile) throws IOException {
-
+    //noinspection resource
     return Files.lines(migignoreFile).map(line -> Paths.get(line)).collect(Collectors.toSet());
   }
 
   @Override
   public boolean test(PathInfo pathInfo) {
-    if(m_ignoredFiles.contains(pathInfo.getModuleRelativePath())){
-      LOG.debug("Migignore excluded file: "+pathInfo);
+    if (m_ignoredFiles.contains(pathInfo.getModuleRelativePath())) {
+      LOG.debug("Migignore excluded file: " + pathInfo);
       return true;
     }
     return false;
