efebfd6b73cc0e6f2bb544a733a99b71b8f091a0
Matthias Villiger <mvi@bsi-software.com>
2019-09-07 17:05:53 +0200

    Remove unused function

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/util/arrays.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/util/arrays.js
@@ -487,14 +487,5 @@ scout.arrays = {
     if (index >= 0) {
       arr.splice(index, 1);
     }
-  },
-
-  /**
-   * Converts a JS array of jQuery elements to a jQuery collection.
-   */
-  $: function(arrayOfJQueryElements) {
-    return $(arrayOfJQueryElements).map(function() {
-      return this[0];
-    });
   }
 };
