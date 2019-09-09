89f9cef561079146e3612491bbd11fa19587d4af
Daniel Schmid <Daniel.Schmid@bsiag.com>
2019-09-09 11:07:52 +0200

    move index.js into src folder
    Signed-off-by: Daniel Schmid <Daniel.Schmid@bsiag.com>
    Change-Id: I77d78811437a40d1001187af84166202a2fb543d
    Reviewed-on: https://git.eclipse.org/r/149152
    Reviewed-by: Matthias Villiger <mvi@bsi-software.com>
    Tested-by: Matthias Villiger <mvi@bsi-software.com>

--- eclipse-scout/index.js
+++ eclipse-scout/index.js
@@ -8,23 +8,44 @@
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  */
-let scout = require('./src/scout/index');
+
+import * as self from './src/index.js';
+
+import * as scout from 'src/scout/scout';
+import * as models from 'src/scout/util/models';
+import * as strings from 'src/scout/util/strings';
+
+export default scout;
+export { models };
+export { strings };
+
+export { default as App } from 'src/scout/App';
+export { default as Session } from 'src/scout/session/Session';
+export { default as Desktop } from 'src/scout/desktop/Desktop';
+export { default as NullWidget } from 'src/scout/widget/NullWidget';
+export { default as ViewButton } from 'src/scout/desktop/ViewButton';
+export { default as OutlineViewButton } from 'src/scout/outline/OutlineViewButton';
+export { default as Device } from 'src/scout/util/Device';
+export { default as Menu } from 'src/scout/menu/Menu';
+
+window.scout = Object.assign({}, self);
 
 // This is used for the ES6 build stack, where this module is exported and
 // consumed by other modules. When this piece of code is executed in a browser
 // it will fail, because 'exports' is a read-only property.
-try {
-  module.exports = scout;
-} catch (e) {
-  // NOP - silently fail
-}
+// try {
+//   // module.exports = scout;
+// } catch (e) {
+//   // NOP - silently fail
+// }
 
 // This is used for our ES5 eclipse-scout.js export, which is used to create
 // simple Scout apps that depend only on the built JS file and don't require
 // the whole build stack with modules, Webpack, etc.
-if (window) {
-  window.scout = Object.assign({}, scout.default, scout);
-}
+// if (window) {
+//
+// }
+// window.scout = Object.assign({}, self);
 
 // FIXME [awe] ES6: review, improve this index.js. Maybe we should have two separate files?
 // one is the main, as referenced by package.json and the other is the build-entry as used
