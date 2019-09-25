8abfe7f7dbc6f5ca3c8a41f7c88490dc5d1c42ef
Matthias Villiger <mvi@bsi-software.com>
2019-09-25 10:05:43 +0200

    Add manual fix for alphanumeric column comparator

--- org.eclipse.scout.migration.ecma6/src/main/java/org/eclipse/scout/migration/ecma6/task/T70010_ManualFixes.java
+++ org.eclipse.scout.migration.ecma6/src/main/java/org/eclipse/scout/migration/ecma6/task/T70010_ManualFixes.java
@@ -12,25 +12,17 @@ package org.eclipse.scout.migration.ecma6.task;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.function.Predicate;
 
 import org.eclipse.scout.migration.ecma6.Configuration;
-import org.eclipse.scout.migration.ecma6.PathFilters;
 import org.eclipse.scout.migration.ecma6.PathInfo;
 import org.eclipse.scout.migration.ecma6.WorkingCopy;
 import org.eclipse.scout.migration.ecma6.context.Context;
 import org.eclipse.scout.migration.ecma6.model.old.Exemption;
 import org.eclipse.scout.rt.platform.Order;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Order(70010)
 public class T70010_ManualFixes extends AbstractTask {
-  private static final Logger LOG = LoggerFactory.getLogger(T70010_ManualFixes.class);
 
-  private static final Path SRC_MAIN_JS = Paths.get("src", "main", "js");
-
-  private Predicate<PathInfo> m_filter = PathFilters.and(PathFilters.inSrcMainJs(), PathFilters.withExtension("js"));
   private Path m_relativeNamespaceDirectory;
 
   @Override
@@ -100,6 +92,11 @@ public class T70010_ManualFixes extends AbstractTask {
         // there is no longer a "res" folder for resources. therefore the log4javascript lib folder can be found on top level now.
         WorkingCopy wc = context.ensureWorkingCopy(pathInfo.getPath());
         wc.setSource(wc.getSource().replace("'res/logo.png'", "'img/logo.png'"));
+      }
+
+      if (pathEndsWith(pathInfo, "/table/columns/comparators.js")) {
+        WorkingCopy wc = context.ensureWorkingCopy(pathInfo.getPath());
+        wc.setSource(wc.getSource().replace("return _compare(valueA, ", "return this._compare(valueA, "));
       }
     }
 
