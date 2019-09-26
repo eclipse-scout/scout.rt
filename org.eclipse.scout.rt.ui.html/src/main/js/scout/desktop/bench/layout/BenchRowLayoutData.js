eclipse-scout/scout.rt

commit 82967544e058ec9239f16ca78de2638ade3b5dd5

Execute constructor super call first
Change-Id: I333f55ec0053a0a2e632029caf3537340af94bf2
Reviewed-on: https://git.eclipse.org/r/149532
Tested-by: CI Bot
Reviewed-by: Matthias Villiger <mvi@bsi-software.com>

org.eclipse.scout.rt.ui.html/src/main/js/scout/desktop/bench/layout/BenchRowLayoutData.js
@@ -9,9 +9,8 @@
  *     BSI Business Systems Integration AG - initial API and implementation
  */
 scout.BenchRowLayoutData = function(model) {
-  this.rows = [null, null, null];
   scout.BenchRowLayoutData.parent.call(this, model);
-
+  this.rows = [null, null, null];
   this._ensureRows();
 };
 

commit 4d3b30f6e3abfe3573cea1da0f7657486dc47c67

Fix BenchRowLayoutData
The rows array must be initialized before the super constructor is
executed. The bug was introduced in commit
82967544e058ec9239f16ca78de2638ade3b5dd5 (Execute constructor super call
first). To solve the problem the parent class defines a prepare method
which is executed as the first instruction in the constructor. This
method may be overridden by child classes to execute code before the
parent constructors instructions are running.
At the same time the constructor super call of the child class is still
the first expression as required by ES6.


Change-Id: I9fa53cb4ab65b2bac8566ba02a20a8c1b96f800d
Reviewed-on: https://git.eclipse.org/r/150202
Tested-by: CI Bot
Reviewed-by: Matthias Villiger <mvi@bsi-software.com>

org.eclipse.scout.rt.ui.html/src/main/js/scout/desktop/bench/layout/BenchRowLayoutData.js
@@ -10,11 +10,14 @@
  */
 scout.BenchRowLayoutData = function(model) {
   scout.BenchRowLayoutData.parent.call(this, model);
-  this.rows = [null, null, null];
   this._ensureRows();
 };
-
 scout.inherits(scout.BenchRowLayoutData, scout.FlexboxLayoutData);
+
+scout.BenchRowLayoutData.prototype._prepare = function() {
+  scout.BenchRowLayoutData.parent.prototype._prepare.call(this);
+  this.rows = [null, null, null];
+};
 
 scout.BenchRowLayoutData.prototype.getRows = function() {
   return this.rows;

org.eclipse.scout.rt.ui.html/src/main/js/scout/layout/flexbox/FlexboxLayoutData.js
@@ -9,6 +9,7 @@
  *     BSI Business Systems Integration AG - initial API and implementation
  */
 scout.FlexboxLayoutData = function(model) {
+  this._prepare();
   // inital
   this.relative = true;
   this.grow = 1;
@@ -20,6 +21,10 @@ scout.FlexboxLayoutData = function(model) {
   this.sizePx; /*current display size in pixel*/
   this.initialPx; /*initial in pixel */
   this.delta;
+};
+
+scout.FlexboxLayoutData.prototype._prepare = function() {
+  // nop. may be overridden by sub classes
 };
 
 scout.FlexboxLayoutData.prototype.withOrder = function(order) {