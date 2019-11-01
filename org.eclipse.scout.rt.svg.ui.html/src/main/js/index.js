8e55df0dbada086e0f346b0c094374e093af62f2
Andreas Hoegger <Andreas.Hoegger@bsiag.com>
2019-11-01 11:07:07 +0100

    ES6 Mig: Move svg files to new structure

--- org.eclipse.scout.rt.svg.ui.html/src/main/js/index.js
+++ org.eclipse.scout.rt.svg.ui.html/src/main/js/index.js
@@ -1,18 +1,6 @@
-/*
- * Copyright (c) 2010-2019 BSI Business Systems Integration AG.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     BSI Business Systems Integration AG - initial API and implementation
- */
+export { default as SvgField } from './svg/SvgField';
+export { default as SvgFieldAdapter } from './svg/SvgFieldAdapter';
 
-import * as self from './index.js';
-
-export let scout =Object.assign(scout || {}, self);
-
-export default {
-  scout
-};
+import * as self from './index.js';
+export default self;
+window.scout = Object.assign(window.scout || {}, self);
--- org.eclipse.scout.rt.svg.ui.html/src/main/js/svg-module.js
+++
@@ -1,6 +0,0 @@
-export { default as SvgField } from './svg/SvgField';
-export { default as SvgFieldAdapter } from './svg/SvgFieldAdapter';
-
-import * as self from './index.js';
-export default self;
-window.scout = Object.assign(window.scout || {}, self);
