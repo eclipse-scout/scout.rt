c226151c702e9791dcbd4158305121e2b6c9035c
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2019-12-10 10:13:14 +0100

    Tree is empty sometimes
    Problem:
    Filter nodes and remove filter again while the animation is still
    running -> runningAnimations counter is greater than 0 which blocks
    all upcoming renderViewport calls -> Tree can never be rendered
    correctly again.
    
    Reason:
    Show and hideNode increment the runningAnimations counter. This counter
    is decremented when the animation completes. Unfortunately the complete
    callback is not called when the animation is stopped using stop() -> use
    stop(false, true) instead.
    
    Also, the close animation was not visible because of the setVisible
    call.
    
    And, the nodes had the wrong height when the filter was added again.
    This happened because of the style reset code.
    
    256733

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/tree/Tree.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/tree/Tree.js
@@ -2687,7 +2687,13 @@ scout.Tree.prototype.filter = function(notAnimated) {
       // with the previous filter. That's why we must make sure, the node is attached to
       // the DOM, even though the filter state hasn't changed. Otherwise we'd have a
       // problem when we insert nodes in this._insertNodeInDOMAtPlace.
-      this.showNode(node, useAnimation);
+      if (!node.attached) {
+        this.showNode(node, useAnimation);
+        if (node.attached) {
+          // If sibling nodes are hiding at the same time, the nodes to be shown should be added after these nodes to make the animation look correctly -> move them
+          node.$node.insertAfter(node.$node.nextAll('.hiding:last'));
+        }
+      }
     }
     if ((node.expanded || node.expandedLazy) && node.isFilterAccepted()) {
       return false;
@@ -2853,11 +2859,13 @@ scout.Tree.prototype._insertNodeInDOMAtPlace = function(node, index) {
 };
 
 scout.Tree.prototype.showNode = function(node, useAnimation, indexHint) {
-  if (node.attached || !this.rendered) {
+  if (!this.rendered || (node.attached && !node.$node.hasClass('hiding'))) {
     return;
   }
-  this._ensureNodeInDOM(node.parentNode, useAnimation, indexHint - 1);
-  this._insertNodeInDOM(node, indexHint);
+  if (!node.attached) {
+    this._ensureNodeInDOM(node.parentNode, useAnimation, indexHint - 1);
+    this._insertNodeInDOM(node, indexHint);
+  }
   if (!node.rendered) {
     return;
   }
@@ -2869,21 +2877,17 @@ scout.Tree.prototype.showNode = function(node, useAnimation, indexHint) {
   $node.removeClass('hiding');
   var that = this;
   if (useAnimation) {
-    var oldStyle = $node.attr('style');
-    var oldVisible = $node.isVisible();
-    $node.setVisible(false);
-    $node.stop().slideDown({
+    // hide node first and then make it appear using slideDown (setVisible(false) won't work because it would stay invisible during the animation)
+    $node.hide();
+    $node.stop(false, true).slideDown({
       duration: 250,
       start: that.startAnimationFunc,
       complete: function() {
         that.runningAnimationsFinishFunc();
         $node.removeClass('showing');
-        $node.setVisible(oldVisible);
-        $node.attrOrRemove('style', oldStyle);
       }
     });
   }
-
 };
 
 scout.Tree.prototype.hideNode = function(node, useAnimation, suppressDetachHandling) {
@@ -2904,18 +2908,18 @@ scout.Tree.prototype.hideNode = function(node, useAnimation, suppressDetachHandl
 
   $node.addClass('hiding');
   $node.removeClass('showing');
-
   if (useAnimation) {
     this._renderViewportBlocked = true;
-    var oldStyle = $node.attr('style');
-    $node.stop().slideUp({
+    $node.stop(false, true).slideUp({
       duration: 250,
       start: that.startAnimationFunc,
       complete: function() {
         that.runningAnimationsFinishFunc();
-        $node.detach();
-        node.attached = false;
-        $node.attrOrRemove('style', oldStyle);
+        $node.removeClass('hiding');
+        if (!$node.hasClass('showing')) {
+          $node.detach();
+          node.attached = false;
+        }
       }
     });
   } else if (!suppressDetachHandling) {
