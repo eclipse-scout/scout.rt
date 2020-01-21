b172e24f2f9dd2015b227c334311209ef5136c09
Michael Rudolf <mru@bsi-software.com>
2020-01-21 10:08:52 +0100

    Improve MalwareScanner API, throw more specific exception
    Use a specific exception for unsafe resources found by malware scanner.
    This way we can handle those exceptions separately and don't hide other
    possible exceptions.
    
    Change-Id: I55351edf1440478950f9a1374cb39db487ad2b2e

---
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/security/UnsafeResourceException.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2020 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.platform.security;
+
+import org.eclipse.scout.rt.platform.exception.PlatformException;
+
+public class UnsafeResourceException extends PlatformException {
+
+  private static final long serialVersionUID = 1L;
+
+  public UnsafeResourceException(String message, Object... args) {
+    super(message, args);
+  }
+}
