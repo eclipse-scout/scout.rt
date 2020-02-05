32dea4340745bc5ea7d404baa285e5a99355ccdd
Fritz Schinkel <Fritz.Schinkel@bsi-software.com>
2020-02-05 13:29:11 +0100

    Fix UI Error for empty tab boxes
    Set the selected tab to null if the tab box has no visible fields.
    Otherwise, a previously visible tab that is now invisible could still be
    the selected tab.
    
    233071
    
    Change-Id: I95366ed7295b99ef465a1994e9da5f1d7285f0d2
    Signed-off-by: Fritz Schinkel <Fritz.Schinkel@bsi-software.com>
    Reviewed-on: https://git.eclipse.org/r/157205
    Tested-by: CI Bot
    Reviewed-by: Beat Schwarzentrub <bsh@bsiag.com>

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/form/fields/tabbox/AbstractTabBox.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/form/fields/tabbox/AbstractTabBox.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010-2017 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2020 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -167,6 +167,9 @@ public abstract class AbstractTabBox extends AbstractCompositeField implements I
 
     if (hasVisibleFieldsInternal()) {
       setSelectedTab(findNewSelectedTab(getSelectedTab()));
+    }
+    else {
+      setSelectedTab(null);
     }
   }
 
