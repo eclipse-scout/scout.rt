b820ee2c1805fc4b95da764fab4f8293b1fc0ef1
Michael Rudolf <mru@bsi-software.com>
2020-02-12 19:03:06 +0100

    Add annotation to allow rest resources to be marked as external

---
+++ org.eclipse.scout.rt.rest/src/main/java/org/eclipse/scout/rt/rest/ExternalRestResource.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2010-2020 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
+package org.eclipse.scout.rt.rest;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RUNTIME)
+public @interface ExternalRestResource {
+}
