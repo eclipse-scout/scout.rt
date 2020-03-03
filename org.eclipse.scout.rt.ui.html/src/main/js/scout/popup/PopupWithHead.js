f29b9e1de6d0e910b5162afbcfbb8b4788065208
Beat Schwarzentrub <bsh@bsiag.com>
2020-03-03 17:37:39 +0100

    PopupWithHead: copy 'line-height' property from blueprint element
    Prevents "jumping" of label if original menu item uses a special
    line-height value (e.g. overflow-tab-item).
    
    230392
    
    Change-Id: Ia06c884e6f661a004dfae4deb6cd7ebd02388f49
    Reviewed-on: https://git.eclipse.org/r/138383
    Tested-by: CI Bot
    Reviewed-by: Beat Schwarzentrub <bsh@bsiag.com>

--- org.eclipse.scout.rt.ui.html/src/main/js/scout/popup/PopupWithHead.js
+++ org.eclipse.scout.rt.ui.html/src/main/js/scout/popup/PopupWithHead.js
@@ -210,6 +210,7 @@ scout.PopupWithHead.prototype._positionImpl = function(horizontalAlignment, vert
   // Make sure the elements inside the header have the same style as to blueprint (menu)
   // This makes it possible to position the content in the header (icon, text) exactly on top of the content of the blueprint
   this.$head.copyCssIfGreater(this.$headBlueprint, 'padding');
+  this.$head.copyCss(this.$headBlueprint, 'line-height');
   this.$head.height(this.$headBlueprint.height());
   this.$head.width(this.$headBlueprint.width());
 
