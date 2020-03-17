31f4fc90515391caa4b28eb5e7ad5d87f362910b
Fritz Schinkel <Fritz.Schinkel@bsi-software.com>
2020-03-17 13:36:38 +0100

    Open BinaryResource in a consistent way
    Add a new method to the Desktop that handles BinaryResources in a
    consistent way depending on the type.
    
    250225
    
    Change-Id: I545ddcad2c1a88a61d5f4b9d8ba5f521c9830ede
    Signed-off-by: Fritz Schinkel <Fritz.Schinkel@bsi-software.com>
    Reviewed-on: https://git.eclipse.org/r/159522
    Tested-by: CI Bot
    Reviewed-by: Beat Schwarzentrub <bsh@bsiag.com>

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/desktop/AbstractDesktop.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/desktop/AbstractDesktop.java
@@ -1650,6 +1650,12 @@ public abstract class AbstractDesktop extends AbstractWidget implements IDesktop
   }
 
   @Override
+  public void openUri(BinaryResource res) {
+    Assertions.assertNotNull(res);
+    openUri(res, BEANS.get(OpenUriHelper.class).getOpenUriActionForResource(res));
+  }
+
+  @Override
   public void openUri(BinaryResource res, IOpenUriAction openUriAction) {
     Assertions.assertNotNull(res);
     Assertions.assertNotNull(openUriAction);
