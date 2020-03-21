eclipse-scout/scout.rt

commit d68421fe879932bedaecdecf2261d52d3d8fd9e7

ajax: move error handling to AjaxCall
scout.ajax.call() returns a promise that in case of an HTTP error is
rejected with a single AjaxError argument. This conversion was moved
to AjaxCall.js, so all usages of AjaxCall can profit from this
conversion, even if they don't use the scout.ajax utility.

For required migration steps, see migration guide.

eclipse-scout-core/src/ajax/AjaxCall.js
@@ -8,7 +8,7 @@
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  */
-import {Call, URL} from '../index';
+import {AjaxError, Call, URL} from '../index';
 import * as $ from 'jquery';
 
 export default class AjaxCall extends Call {
@@ -16,6 +16,7 @@ export default class AjaxCall extends Call {
   constructor() {
     super();
     this.type = 'ajax';
+    this.ajaxOptions = null;
   }
 
   init(model) {
@@ -32,6 +33,19 @@ export default class AjaxCall extends Call {
   }
 
   // ==================================================================================
+
+  _promise() {
+    return super._promise()
+      .catch(function(jqXHR, textStatus, errorThrown) {
+        // Wrap arguments in an object to make rethrowing the error easier.
+        return $.rejectedPromise(new AjaxError({
+          jqXHR: jqXHR,
+          textStatus: textStatus,
+          errorThrown: errorThrown,
+          requestOptions: this.ajaxOptions
+        }));
+      }.bind(this));
+  }
 
   _callImpl() {
     // Mark retries by adding an URL parameter
@@ -67,6 +81,7 @@ export default class AjaxCall extends Call {
   /* --- STATIC HELPERS ------------------------------------------------------------- */
 
   static isOfflineError(jqXHR, textStatus, errorThrown, request) {
+    // noinspection UnnecessaryLocalVariableJS
     var offline = (
       // Status code = 0 -> no connection
       !jqXHR.status ||

eclipse-scout-core/src/util/Call.js
@@ -79,6 +79,10 @@ export default class Call {
     this.deferred.reject.apply(this.deferred, this.result);
   }
 
+  _promise() {
+    return this.deferred.promise();
+  }
+
   // ==================================================================================
 
   /**
@@ -114,7 +118,7 @@ export default class Call {
   call() {
     this._checkInitialized();
     this._call();
-    return this.deferred.promise();
+    return this._promise();
   }
 
   /**
@@ -158,7 +162,7 @@ export default class Call {
    * >>> This method MUST be implemented by a subclass. <<<
    */
   _callImpl() {
-    throw new Error('Missing implemention: _callImpl()');
+    throw new Error('Missing implementation: _callImpl()');
   }
 
   _onCallDone() {

eclipse-scout-core/src/ajax/ajax.js
@@ -167,21 +167,11 @@ export function call(options) {
     cache: false
   }, options);
 
-  var ajaxCall = scout.create('AjaxCall', {
+  return scout.create('AjaxCall', {
     ajaxOptions: opts
   }, {
     ensureUniqueId: false
   });
-  return ajaxCall.call()
-    .catch(function(jqXHR, textStatus, errorThrown) {
-      // Wrap arguments in an object to make rethrowing the error easier.
-      return $.rejectedPromise(new AjaxError({
-        jqXHR: jqXHR,
-        textStatus: textStatus,
-        errorThrown: errorThrown,
-        requestOptions: opts
-      }));
-    });
 }
 
 export default {

eclipse-scout-core/src/session/Session.js
@@ -729,12 +729,12 @@ export default class Session {
       }
     }
 
-    function onAjaxFail(jqXHR, textStatus, errorThrown) {
+    function onAjaxFail(ajaxError) {
       try {
         if (busyHandling) {
           this.setBusy(false);
         }
-        this._processErrorResponse(jqXHR, textStatus, errorThrown, request);
+        this._processErrorResponse(ajaxError.jqXHR, ajaxError.textStatus, ajaxError.errorThrown, request);
       } catch (err) {
         jsError = jsError || err;
       }
@@ -884,9 +884,9 @@ export default class Session {
       }
     }
 
-    function onAjaxFail(jqXHR, textStatus, errorThrown) {
+    function onAjaxFail(ajaxError) {
       this.backgroundJobPollingSupport.setFailed();
-      this._processErrorResponse(jqXHR, textStatus, errorThrown, request);
+      this._processErrorResponse(ajaxError.jqXHR, ajaxError.textStatus, ajaxError.errorThrown, request);
     }
   }
 

commit 2fd157283337f3acacc29a8149cb138b2868bf71

AjaxCall: fix rejection reason
$.ajax() does not return a normal promise, therefore it cannot be
changed by simply adding a catch() clause to the promise chain.
To correctly convert the result of AjaxCall in the "reject" case, the
"this.result" value must be changed accordingly. Internally, AjaxCall.js
still needs to adhere to the jQuery API.

This is a fix for an error that was introduced with commit
d68421fe879932bedaecdecf2261d52d3d8fd9e7.

eclipse-scout-core/src/ajax/AjaxCall.js
@@ -34,19 +34,6 @@ export default class AjaxCall extends Call {
 
   // ==================================================================================
 
-  _promise() {
-    return super._promise()
-      .catch(function(jqXHR, textStatus, errorThrown) {
-        // Wrap arguments in an object to make rethrowing the error easier.
-        return $.rejectedPromise(new AjaxError({
-          jqXHR: jqXHR,
-          textStatus: textStatus,
-          errorThrown: errorThrown,
-          requestOptions: this.ajaxOptions
-        }));
-      }.bind(this));
-  }
-
   _callImpl() {
     // Mark retries by adding an URL parameter
     if (this.callCounter !== 1) {
@@ -57,6 +44,16 @@ export default class AjaxCall extends Call {
     $.log.isTraceEnabled() && $.log.trace(this.logPrefix + (this.callCounter === 1 ? '--- ' : '') + this.ajaxOptions.type + ' "' + this.ajaxOptions.url + '"' + (this.callCounter === 1 ? ' ---' : ''));
 
     return $.ajax(this.ajaxOptions);
+  }
+
+  _setResultFail(jqXHR, textStatus, errorThrown) {
+    // Store result as single object to make rethrowing the error easier for callers of AjaxCall
+    this._setResult(new AjaxError({
+      jqXHR: jqXHR,
+      textStatus: textStatus,
+      errorThrown: errorThrown,
+      requestOptions: this.ajaxOptions
+    }));
   }
 
   _onCallDone(data, textStatus, jqXHR) {

eclipse-scout-core/src/util/Call.js
@@ -36,6 +36,12 @@ export default class Call {
     this.name = null; // Identifier for the call, used to build the uniqueName
     this.uniqueName = null; // Unique identifier of this call instance for logging and debugging purposes
     this.logPrefix = ''; // All log messages are prefixed with this string. It contains the uniqueName and the current state (e.g. callCounter)
+
+    // Temporary store for the arguments to the done/fail callbacks of the _callImpl() function.
+    // They will be used be _resolve() and _reject(). We could pass them through the _onCallX functions,
+    // but when a call is aborted while it is only scheduled (setTimeout), we would not have any values
+    // to pass to _reject().
+    this.result = null;
   }
 
   static GLOBAL_SEQ = 0;
@@ -71,16 +77,16 @@ export default class Call {
 
   _resolve() {
     $.log.isTraceEnabled() && $.log.trace(this.logPrefix + '[RESOLVE]');
-    this.deferred.resolve.apply(this.deferred, this.result);
+    this.deferred.resolve.apply(this.deferred, arrays.ensure(this.result));
   }
 
   _reject() {
     $.log.isTraceEnabled() && $.log.trace(this.logPrefix + '[REJECT]');
-    this.deferred.reject.apply(this.deferred, this.result);
+    this.deferred.reject.apply(this.deferred, arrays.ensure(this.result));
   }
 
-  _promise() {
-    return this.deferred.promise();
+  _setResult() {
+    this.result = objects.argumentsToArray(arguments);
   }
 
   // ==================================================================================
@@ -118,7 +124,7 @@ export default class Call {
   call() {
     this._checkInitialized();
     this._call();
-    return this._promise();
+    return this.deferred.promise();
   }
 
   /**
@@ -147,12 +153,10 @@ export default class Call {
     this.pendingCall = this._callImpl()
       .always(function() {
         this.pendingCall = null;
-        // Store the last callback's arguments. They will be used be _resolve() and _reject().
-        // We could pass them through the _onCallX functions, but when a call is aborted while
-        // it is only scheduled (setTimeout), we would not have any values to pass to _reject().
-        this.result = objects.argumentsToArray(arguments);
       }.bind(this))
+      .done(this._setResultDone.bind(this))
       .done(this._onCallDone.bind(this))
+      .fail(this._setResultFail.bind(this))
       .fail(this._onCallFail.bind(this));
   }
 
@@ -163,6 +167,14 @@ export default class Call {
    */
   _callImpl() {
     throw new Error('Missing implementation: _callImpl()');
+  }
+
+  _setResultDone() {
+    this._setResult.apply(this, arguments);
+  }
+
+  _setResultFail() {
+    this._setResult.apply(this, arguments);
   }
 
   _onCallDone() {