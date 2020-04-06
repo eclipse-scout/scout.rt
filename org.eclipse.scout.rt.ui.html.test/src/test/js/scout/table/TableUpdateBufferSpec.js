e8df0f8a97b5ec07db58375df71845dc39d6c826
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2020-04-06 09:23:39 +0200

    Fix specs

--- org.eclipse.scout.rt.ui.html.test/src/test/js/scout/table/TableUpdateBufferSpec.js
+++ org.eclipse.scout.rt.ui.html.test/src/test/js/scout/table/TableUpdateBufferSpec.js
@@ -9,22 +9,16 @@
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
 /* global removePopups */
-describe("TableUpdateBuffer", function() {
+describe('TableUpdateBuffer', function() {
   var session, helper;
 
   beforeEach(function() {
     setFixtures(sandbox());
     session = sandboxSession();
     helper = new scout.TableSpecHelper(session);
-    jasmine.clock().install();
   });
 
-  afterEach(function() {
-    jasmine.clock().uninstall();
-  });
-
-
-  it("buffers updateRow calls and processes them when all promises resolve", function(done) {
+  it('buffers updateRow calls and processes them when all promises resolve', function(done) {
     var table = helper.createTable(helper.createModelFixture(2, 2));
     table.render();
 
@@ -41,7 +35,7 @@ describe("TableUpdateBuffer", function() {
     expect(table.rows[0].cells[0].text).toBe('0_0');
     expect(table.loading).toBe(true);
 
-    promise.done(function() {
+    promise.then(function() {
       expect(table.updateBuffer.isBuffering()).toBe(false);
       expect(table.rows[0].cells[0].text).toBe('newCellText0');
       expect(table.loading).toBe(false);
@@ -50,7 +44,7 @@ describe("TableUpdateBuffer", function() {
     deferred.resolve();
   });
 
-  it("prevents rendering viewport while buffering", function(done) {
+  it('prevents rendering viewport while buffering', function(done) {
     var table = helper.createTable(helper.createModelFixture(2, 0));
     table.render();
 
@@ -71,7 +65,7 @@ describe("TableUpdateBuffer", function() {
     table.updateRow(row);
     expect(table.$rows().length).toBe(0);
 
-    promise.done(function() {
+    promise.then(function() {
       expect(table.$rows().length).toBe(1);
       var $cells0 = table.$cellsForRow(table.$rows().eq(0));
       expect($cells0.eq(0).text()).toBe('newCellText0');
@@ -79,25 +73,4 @@ describe("TableUpdateBuffer", function() {
     });
     deferred.resolve();
   });
-
-  it("processes immediately when a resolved promise is added", function() {
-    var table = helper.createTable(helper.createModelFixture(2, 2));
-    table.render();
-
-    var deferred = $.Deferred();
-    var promise = deferred.promise();
-    deferred.resolve();
-    table.updateBuffer.pushPromise(promise);
-
-    var row = {
-      id: table.rows[0].id,
-      cells: ['newCellText0', 'newCellText1']
-    };
-    table.updateRow(row);
-    expect(table.updateBuffer.isBuffering()).toBe(false);
-    expect(table.rows[0].cells[0].text).toBe('newCellText0');
-    expect(table.loading).toBe(false);
-    expect(table._renderViewportBlocked).toBe(false);
-  });
-
 });
