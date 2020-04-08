988188c3aca65f56f594d2f6195f15ace4ed2721
Adrian Egloff <adrian.egloff@bsi-software.com>
2020-04-08 21:47:08 +0200

    Remove deprecated scout version in OfficialVersion.java

--- org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/OfficialVersion.java
+++ org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/OfficialVersion.java
@@ -12,19 +12,10 @@ package org.eclipse.scout.rt.shared;
 
 import java.util.Calendar;
 
-import org.eclipse.scout.rt.platform.config.PlatformConfigProperties;
-
-// TODO [11.0] rst cleanup deprecations
 public final class OfficialVersion {
 
   private OfficialVersion() {
   }
-
-  /**
-   * @deprecated use instead {@link PlatformConfigProperties.PlatformVersionProperty}
-   */
-  @Deprecated
-  public static final String VERSION = "10.0.0";
 
   public static final String COPYRIGHT_VERSION = "10";
   public static final String COPYRIGHT = "Scout " + COPYRIGHT_VERSION + ", &copy; BSI Business Systems Integration AG " + 2001 + "," + Calendar.getInstance().get(Calendar.YEAR) + " EPL";
