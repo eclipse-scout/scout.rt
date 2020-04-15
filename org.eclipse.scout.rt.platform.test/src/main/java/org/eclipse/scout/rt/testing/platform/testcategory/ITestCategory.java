fb1a5a4724f86558fde99158a169d6e452c08a47
Ralph Steiner <rst@bsiag.com>
2020-04-15 17:11:41 +0200

    Mark slow and resource intensive tests with a JUnit test category
    Mark JobManagerLoadTest as a resource intensive test as it requires up
    to 25k native threads.
    Mark tests which require more than five seconds to be executed as slow
    tests.
    
    Change-Id: I45773bd89763b98d195753b915768d57a217533e
    Reviewed-on: https://git.eclipse.org/r/161036
    Tested-by: CI Bot
    Reviewed-by: Beat Schwarzentrub <bsh@bsiag.com>

---
+++ org.eclipse.scout.rt.platform.test/src/main/java/org/eclipse/scout/rt/testing/platform/testcategory/ITestCategory.java
@@ -0,0 +1,9 @@
+package org.eclipse.scout.rt.testing.platform.testcategory;
+
+import org.junit.experimental.categories.Category;
+
+/**
+ * Marker interface for test categories used with JUnit {@link Category}
+ */
+public interface ITestCategory {
+}
