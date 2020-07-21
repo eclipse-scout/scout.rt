27d3bf4a223885996f6a0688bc9a0f65bfb9f060
Ralph Steiner <rst@bsiag.com>
2020-07-21 17:56:36 +0200

    Introduce URI scheme 'binref' to allow referencing BinaryResource
    An URI with scheme 'binref' identifies a BinaryResource. The utility
    class BinaryRefs provides methods to get the BinaryResource reference by
    such a URI. An application must create classes implementing
    'IBinaryRefHandler' in order to load a BinaryResource for a URI. A
    handler registers itself for a path. If a (prefix-)path of a binref URI
    matches the registration path of a handler this handler is called to
    load BinaryResource for this URI.
    
    
    
    Change-Id: I16b333223fcf6ed0f1a23bf5ecca3e2362376684
    Reviewed-on: https://git.eclipse.org/r/c/scout/org.eclipse.scout.rt/+/166605
    Tested-by: Ralph Steiner <rst@bsiag.com>
    Reviewed-by: Ralph Steiner <rst@bsiag.com>
    (cherry picked from commit b9aa4537f30da9ab5fa8220850738a5bb7b4e45b)
    Reviewed-on: https://git.eclipse.org/r/c/scout/org.eclipse.scout.rt/+/165663
    Tested-by: Scout Bot <scout-bot@eclipse.org>

---
+++ org.eclipse.scout.rt.platform.test/src/test/java/org/eclipse/scout/rt/platform/resource/BinaryRefTest.java
@@ -0,0 +1,202 @@
+/*******************************************************************************
+ * Copyright (c) 2020 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.platform.resource;
+
+import static org.eclipse.scout.rt.testing.platform.util.ScoutAssert.assertThrows;
+import static org.junit.Assert.*;
+
+import java.net.URI;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.scout.rt.platform.BEANS;
+import org.eclipse.scout.rt.platform.BeanMetaData;
+import org.eclipse.scout.rt.platform.IBean;
+import org.eclipse.scout.rt.testing.platform.BeanTestingHelper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BinaryRefTest {
+
+  protected final List<IBean<?>> m_beans = new ArrayList<>();
+
+  private static final URI NO_BINREF_URI = URI.create("foobar:/test/existing");
+
+  private static final URI NO_HANDLER_BINREF_URI = URI.create("binref:/nohandler/some");
+
+  private static final URI UNKNOWN_BINREF_URI = URI.create("binref:/test/unknown");
+  private static final URI RELATIVE_BINREF_URI = URI.create("binref:test/existing1.txt");
+  private static final URI EXISTING_1_BINREF_URI = URI.create("binref:/test/existing1.txt");
+  private static final URI EXISTING_2_BINREF_URI = URI.create("binref:/test/existing2.png");
+
+  @Before
+  public void before() {
+    m_beans.add(BeanTestingHelper.get().registerBean(new BeanMetaData(TestingBinaryRefHandler.class, new TestingBinaryRefHandler(EXISTING_1_BINREF_URI, EXISTING_2_BINREF_URI))));
+    BEANS.get(BinaryRefSupport.class).reload();
+  }
+
+  @After
+  public void after() {
+    BeanTestingHelper.get().unregisterBeans(m_beans);
+    m_beans.clear();
+    BEANS.get(BinaryRefSupport.class).reload();
+  }
+
+  protected Set<URI> uris(URI... uris) {
+    return new HashSet<>(Arrays.asList(uris));
+  }
+
+  @Test
+  public void testGetDisplayTexts() {
+    assertTrue(BinaryRefs.getDisplayTexts(null).isEmpty());
+
+    Map<URI, String> displayTexts = BinaryRefs.getDisplayTexts(uris(null, NO_BINREF_URI, NO_HANDLER_BINREF_URI, UNKNOWN_BINREF_URI, RELATIVE_BINREF_URI, EXISTING_1_BINREF_URI, EXISTING_2_BINREF_URI));
+    assertEquals(uris(NO_HANDLER_BINREF_URI, UNKNOWN_BINREF_URI, RELATIVE_BINREF_URI, EXISTING_1_BINREF_URI, EXISTING_2_BINREF_URI), displayTexts.keySet());
+    assertEquals("Unknown URI [" + NO_HANDLER_BINREF_URI + "]", displayTexts.get(NO_HANDLER_BINREF_URI));
+    assertEquals("Unknown URI [" + UNKNOWN_BINREF_URI + "]", displayTexts.get(UNKNOWN_BINREF_URI));
+    assertEquals("Unknown URI [" + RELATIVE_BINREF_URI + "]", displayTexts.get(RELATIVE_BINREF_URI));
+    assertEquals("File: existing1.txt", displayTexts.get(EXISTING_1_BINREF_URI));
+    assertEquals("File: existing2.png", displayTexts.get(EXISTING_2_BINREF_URI));
+  }
+
+  @Test
+  public void testGetDisplayTextsFallback() {
+    assertTrue(BinaryRefs.getDisplayTexts(null, URI::toString).isEmpty());
+
+    Map<URI, String> displayTexts = BinaryRefs.getDisplayTexts(uris(null, NO_BINREF_URI, NO_HANDLER_BINREF_URI, UNKNOWN_BINREF_URI, RELATIVE_BINREF_URI, EXISTING_1_BINREF_URI, EXISTING_2_BINREF_URI), URI::toString);
+    assertEquals(uris(NO_HANDLER_BINREF_URI, UNKNOWN_BINREF_URI, RELATIVE_BINREF_URI, EXISTING_1_BINREF_URI, EXISTING_2_BINREF_URI), displayTexts.keySet());
+    assertEquals(NO_HANDLER_BINREF_URI.toString(), displayTexts.get(NO_HANDLER_BINREF_URI));
+    assertEquals(UNKNOWN_BINREF_URI.toString(), displayTexts.get(UNKNOWN_BINREF_URI));
+    assertEquals(RELATIVE_BINREF_URI.toString(), displayTexts.get(RELATIVE_BINREF_URI));
+    assertEquals("File: existing1.txt", displayTexts.get(EXISTING_1_BINREF_URI));
+    assertEquals("File: existing2.png", displayTexts.get(EXISTING_2_BINREF_URI));
+  }
+
+  @Test
+  public void testGetDisplayText() {
+    assertNull(BinaryRefs.getDisplayText(null));
+    assertNull(BinaryRefs.getDisplayText(NO_BINREF_URI));
+
+    assertEquals("Unknown URI [" + NO_HANDLER_BINREF_URI + "]", BinaryRefs.getDisplayText(NO_HANDLER_BINREF_URI));
+    assertEquals("Unknown URI [" + UNKNOWN_BINREF_URI + "]", BinaryRefs.getDisplayText(UNKNOWN_BINREF_URI));
+    assertEquals("Unknown URI [" + RELATIVE_BINREF_URI + "]", BinaryRefs.getDisplayText(RELATIVE_BINREF_URI));
+    assertEquals("File: existing1.txt", BinaryRefs.getDisplayText(EXISTING_1_BINREF_URI));
+    assertEquals("File: existing2.png", BinaryRefs.getDisplayText(EXISTING_2_BINREF_URI));
+  }
+
+  @Test
+  public void testGetFilenames() {
+    assertTrue(BinaryRefs.getFilenames(null).isEmpty());
+
+    Map<URI, String> filenames = BinaryRefs.getFilenames(uris(null, NO_BINREF_URI, NO_HANDLER_BINREF_URI, UNKNOWN_BINREF_URI, RELATIVE_BINREF_URI, EXISTING_1_BINREF_URI, EXISTING_2_BINREF_URI));
+    assertEquals(uris(EXISTING_1_BINREF_URI, EXISTING_2_BINREF_URI), filenames.keySet());
+    assertEquals("existing1.txt", filenames.get(EXISTING_1_BINREF_URI));
+    assertEquals("existing2.png", filenames.get(EXISTING_2_BINREF_URI));
+  }
+
+  @Test
+  public void testGetFilename() {
+    assertNull(BinaryRefs.getFilename(null));
+    assertNull(BinaryRefs.getFilename(NO_BINREF_URI));
+    assertNull(BinaryRefs.getFilename(NO_HANDLER_BINREF_URI));
+    assertNull(BinaryRefs.getFilename(UNKNOWN_BINREF_URI));
+    assertNull(BinaryRefs.getFilename(RELATIVE_BINREF_URI));
+    assertEquals("existing1.txt", BinaryRefs.getFilename(EXISTING_1_BINREF_URI));
+    assertEquals("existing2.png", BinaryRefs.getFilename(EXISTING_2_BINREF_URI));
+  }
+
+  @Test
+  public void testGetContentTypes() {
+    assertTrue(BinaryRefs.getContentTypes(null).isEmpty());
+
+    Map<URI, String> contentTypes = BinaryRefs.getContentTypes(uris(null, NO_BINREF_URI, NO_HANDLER_BINREF_URI, UNKNOWN_BINREF_URI, RELATIVE_BINREF_URI, EXISTING_1_BINREF_URI, EXISTING_2_BINREF_URI));
+    assertEquals(uris(EXISTING_1_BINREF_URI, EXISTING_2_BINREF_URI), contentTypes.keySet());
+    assertEquals("text/plain", contentTypes.get(EXISTING_1_BINREF_URI));
+    assertEquals("image/png", contentTypes.get(EXISTING_2_BINREF_URI));
+  }
+
+  @Test
+  public void testGetContentType() {
+    assertNull(BinaryRefs.getContentType(null));
+    assertNull(BinaryRefs.getContentType(NO_BINREF_URI));
+    assertNull(BinaryRefs.getContentType(NO_HANDLER_BINREF_URI));
+    assertNull(BinaryRefs.getContentType(UNKNOWN_BINREF_URI));
+    assertNull(BinaryRefs.getContentType(RELATIVE_BINREF_URI));
+    assertEquals("text/plain", BinaryRefs.getContentType(EXISTING_1_BINREF_URI));
+    assertEquals("image/png", BinaryRefs.getContentType(EXISTING_2_BINREF_URI));
+  }
+
+  @Test
+  public void testLoadBinaryResource() {
+    assertNull(BinaryRefs.loadBinaryResource(null));
+    assertNull(BinaryRefs.loadBinaryResource(NO_BINREF_URI));
+    assertThrows(IllegalStateException.class, () -> BinaryRefs.loadBinaryResource(NO_HANDLER_BINREF_URI));
+    assertThrows(IllegalStateException.class, () -> BinaryRefs.loadBinaryResource(UNKNOWN_BINREF_URI));
+    assertThrows(IllegalStateException.class, () -> BinaryRefs.loadBinaryResource(RELATIVE_BINREF_URI));
+    assertEquals(new BinaryResource("existing1.txt", new byte[0]), BinaryRefs.loadBinaryResource(EXISTING_1_BINREF_URI));
+    assertEquals(new BinaryResource("existing2.png", new byte[0]), BinaryRefs.loadBinaryResource(EXISTING_2_BINREF_URI));
+  }
+
+  @Test
+  public void testLoadBinaryResourceOrNull() {
+    assertNull(BinaryRefs.loadBinaryResourceOrNull(null));
+    assertNull(BinaryRefs.loadBinaryResourceOrNull(NO_BINREF_URI));
+    assertNull(BinaryRefs.loadBinaryResourceOrNull(NO_HANDLER_BINREF_URI));
+    assertNull(BinaryRefs.loadBinaryResourceOrNull(UNKNOWN_BINREF_URI));
+    assertNull(BinaryRefs.loadBinaryResourceOrNull(RELATIVE_BINREF_URI));
+    assertEquals(new BinaryResource("existing1.txt", new byte[0]), BinaryRefs.loadBinaryResourceOrNull(EXISTING_1_BINREF_URI));
+    assertEquals(new BinaryResource("existing2.png", new byte[0]), BinaryRefs.loadBinaryResourceOrNull(EXISTING_2_BINREF_URI));
+  }
+
+  protected static class TestingBinaryRefHandler implements IBinaryRefHandler {
+
+    private final Collection<URI> m_supportedUris;
+
+    public TestingBinaryRefHandler(URI... supportedUris) {
+      m_supportedUris = new ArrayList<>(Arrays.asList(supportedUris));
+    }
+
+    @Override
+    public String getRegistrationPath() {
+      return "/test";
+    }
+
+    @Override
+    public BinaryResource loadBinaryResource(URI uri) {
+      if (m_supportedUris.contains(uri)) {
+        return new BinaryResource(getFilename(uri), new byte[0]);
+      }
+      return null;
+    }
+
+    @Override
+    public void getDisplayTexts(Map<URI, String> resultCollector, Collection<URI> uris) {
+      uris.stream().filter(m_supportedUris::contains).forEach(uri -> resultCollector.put(uri, "File: " + getFilename(uri)));
+    }
+
+    @Override
+    public void getFilenames(Map<URI, String> resultCollector, Collection<URI> uris) {
+      uris.stream().filter(m_supportedUris::contains).forEach(uri -> resultCollector.put(uri, getFilename(uri)));
+    }
+
+    protected String getFilename(URI uri) {
+      return Paths.get(uri.getPath()).getFileName().toString();
+    }
+  }
+
+}
---
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/resource/IBinaryRefHandler.java
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ * Copyright (c) 2020 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.platform.resource;
+
+import java.net.URI;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.eclipse.scout.rt.platform.ApplicationScoped;
+import org.eclipse.scout.rt.platform.util.FileUtility;
+
+/**
+ * Implementations of this interface can handle binref {@link URI} for a specific path.
+ * <p>
+ * No method should not be called directly. Only {@link BinaryRefSupport} should call methods of this interface.
+ */
+@ApplicationScoped
+public interface IBinaryRefHandler {
+
+  /**
+   * URI [base-] path under which this handler is registered. {@link URI} starting with this path will be handled by
+   * this handler except there is a handler registered with a more specific path.<br/>
+   * Note that this path should be an absolute path and therefore should begin with a slash character ('/')
+   *
+   * @return non null registration path
+   */
+  String getRegistrationPath();
+
+  /**
+   * Gets resource to given URI as {@link BinaryResource}. Returns null if resource not found.
+   *
+   * @param uri
+   *          non null URI with a path matching {@link #getRegistrationPath()}
+   * @return BinaryResource of URI
+   */
+  BinaryResource loadBinaryResource(URI uri);
+
+  /**
+   * Lookups display texts for all provided URIs. Display texts are only returned for resources which were found. Input
+   * URI collection must contain only URIs for which this handler is registered.
+   *
+   * @param resultCollector
+   *          collector map in which implementor puts resolved display texts
+   * @param uris
+   *          non null collection of {@link URI} for this handler to resolve display texts
+   */
+  default void getDisplayTexts(Map<URI, String> resultCollector, Collection<URI> uris) {
+    // use as display texts the filenames
+    getFilenames(resultCollector, uris);
+  }
+
+  /**
+   * Looks up filenames for provided URIs. Filenames are only returned for resources which were found. Input URI
+   * collection must contain only URIs for which this handler is registered.
+   *
+   * @param resultCollector
+   *          collector map in which implementor puts resolved filenames
+   * @param uris
+   *          non null collection of {@link URI} for this handler to resolve filenames
+   */
+  void getFilenames(Map<URI, String> resultCollector, Collection<URI> uris);
+
+  /**
+   * Looks up content types for provided URIs. Content types are only returned for resources which were found. Input URI
+   * collection must contain only URIs for which this handler is registered.
+   *
+   * @param resultCollector
+   *          collector map in which implementor puts resolved filenames
+   * @param uris
+   *          non null collection of {@link URI} for this handler to resolve content types
+   */
+  default void getContentTypes(Map<URI, String> resultCollector, Collection<URI> uris) {
+    // get content types from filenames
+    Map<URI, String> filenames = new HashMap<URI, String>();
+    getFilenames(filenames, uris);
+    for (Entry<URI, String> entry : filenames.entrySet()) {
+      resultCollector.put(entry.getKey(), FileUtility.getMimeType(entry.getValue()));
+    }
+  }
+}
