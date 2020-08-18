c9c2dde6c7ed5f45084248873ebfcdf2018faa80
Beat Schwarzentrub <bsh@bsiag.com>
2020-08-18 11:01:09 +0200

    RestApplication: reduce default log output
    Change-Id: I3063e221635528421aa65c4f414e74c295a66b0f
    Reviewed-on:
    https://git.eclipse.org/r/c/scout/org.eclipse.scout.rt/+/167886
    Tested-by: Paolo Bazzi <paolo.bazzi@bsi-software.com>
    Tested-by: Scout Bot <scout-bot@eclipse.org>
    Reviewed-by: Paolo Bazzi <paolo.bazzi@bsi-software.com>
    (cherry picked from commit c039ff98a929a34869464c660073bd290c04409e)

--- org.eclipse.scout.rt.rest/src/main/java/org/eclipse/scout/rt/rest/RestApplication.java
+++ org.eclipse.scout.rt.rest/src/main/java/org/eclipse/scout/rt/rest/RestApplication.java
@@ -20,6 +20,7 @@ import javax.ws.rs.core.Application;
 import org.eclipse.scout.rt.platform.ApplicationScoped;
 import org.eclipse.scout.rt.platform.BEANS;
 import org.eclipse.scout.rt.platform.util.StreamUtility;
+import org.eclipse.scout.rt.platform.util.StringUtility;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,9 +40,22 @@ public class RestApplication extends Application {
   private final Map<String, Object> m_properties;
 
   public RestApplication() {
+    long t0 = System.nanoTime();
     m_classes = initClasses();
     m_singletons = initSingletons();
     m_properties = initProperties();
+    if (LOG.isDebugEnabled()) {
+      LOG.info("{} initialized in {} ms\nClasses ({}):\n{}\nSingletons ({}):\n{}\nProperties ({}):\n{}",
+          getClass().getName(), StringUtility.formatNanos(System.nanoTime() - t0),
+          m_classes.size(), (m_classes.isEmpty() ? "  (none)" : m_classes.stream().map(c -> "- " + c.getName()).sorted().collect(Collectors.joining("\n"))),
+          m_singletons.size(), (m_singletons.isEmpty() ? "  (none)" : m_singletons.stream().map(o -> "- " + o).sorted().collect(Collectors.joining("\n"))),
+          m_properties.size(), (m_properties.isEmpty() ? "  (none)" : m_properties.keySet().stream().map(k -> "- " + k).sorted().collect(Collectors.joining("\n"))));
+    }
+    else {
+      LOG.info("{} initialized in {} ms [{} classes, {} singletons, {} properties]",
+          getClass().getName(), StringUtility.formatNanos(System.nanoTime() - t0),
+          m_classes.size(), m_singletons.size(), m_properties.size());
+    }
   }
 
   protected boolean filterClass(Class<?> clazz) {
@@ -52,7 +66,7 @@ public class RestApplication extends Application {
     return BEANS.all(IRestApplicationClassesContributor.class).stream()
         .flatMap(contributor -> {
           Set<Class<?>> classes = contributor.contribute();
-          LOG.info("Contributed {} classes by {}", classes.size(), contributor.getClass());
+          LOG.debug("Contributed {} classes by {}", classes.size(), contributor.getClass());
           return classes.stream();
         })
         .filter(this::filterClass)
@@ -62,7 +76,7 @@ public class RestApplication extends Application {
   protected Set<Object> initSingletons() {
     return BEANS.all(IRestApplicationSingletonsContributor.class).stream().flatMap(contributor -> {
       Set<Object> singletons = contributor.contribute();
-      LOG.info("Contributed {} singletons by {}", singletons.size(), contributor.getClass());
+      LOG.debug("Contributed {} singletons by {}", singletons.size(), contributor.getClass());
       return singletons.stream();
     }).collect(Collectors.toSet());
   }
@@ -70,7 +84,7 @@ public class RestApplication extends Application {
   protected Map<String, Object> initProperties() {
     return BEANS.all(IRestApplicationPropertiesContributor.class).stream().flatMap(contributor -> {
       Map<String, Object> properties = contributor.contribute();
-      LOG.info("Contributed {} properties by {}", properties.size(), contributor.getClass());
+      LOG.debug("Contributed {} properties by {}", properties.size(), contributor.getClass());
       return properties.entrySet().stream();
     }).collect(StreamUtility.toMap(Entry::getKey, Entry::getValue, StreamUtility.ignoringMerger())); // use first provided property value for multiple properties with the same key
   }
