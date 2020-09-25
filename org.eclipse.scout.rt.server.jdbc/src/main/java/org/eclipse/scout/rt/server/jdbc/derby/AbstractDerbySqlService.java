cdec9f2c682b6c7dcb8dd0391bb02bbced086e14
Matthias Villiger <mvi@bsi-software.com>
2020-09-25 11:13:19 +0200

    Correctly shutdown Derby database
    On platform shutdown the Derby database should be stopped as well.

--- org.eclipse.scout.rt.server.jdbc/src/main/java/org/eclipse/scout/rt/server/jdbc/derby/AbstractDerbySqlService.java
+++ org.eclipse.scout.rt.server.jdbc/src/main/java/org/eclipse/scout/rt/server/jdbc/derby/AbstractDerbySqlService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2017 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2020 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,8 +10,18 @@
  */
 package org.eclipse.scout.rt.server.jdbc.derby;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.eclipse.scout.rt.platform.BEANS;
+import org.eclipse.scout.rt.platform.exception.PlatformExceptionTranslator;
+import org.eclipse.scout.rt.platform.util.Assertions;
+import org.eclipse.scout.rt.platform.util.StringUtility;
 import org.eclipse.scout.rt.server.jdbc.AbstractSqlService;
 import org.eclipse.scout.rt.server.jdbc.style.ISqlStyle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Cause Derby supports no sequences, we model this behavior with a table with only one column AND entry of type BIGINT
@@ -20,6 +30,8 @@ import org.eclipse.scout.rt.server.jdbc.style.ISqlStyle;
  * name of this column. NOTE: With "CREATE SYNONYM DUAL FOR SYSIBM.SYSDUMMY1" one can better reuse Oracle styled SQL.
  */
 public abstract class AbstractDerbySqlService extends AbstractSqlService {
+
+  private static final Logger LOG = LoggerFactory.getLogger(AbstractDerbySqlService.class);
 
   protected String getConfiguredSequenceColumnName() {
     return "LAST_VAL";
@@ -56,4 +68,49 @@ public abstract class AbstractDerbySqlService extends AbstractSqlService {
     return DerbySqlStyle.class;
   }
 
+  protected void runDerbyCommand(String commands) throws SQLException {
+    Assertions.assertTrue(StringUtility.hasText(commands), "Commands must be provided.");
+    try (Connection connection = DriverManager.getConnection(getJdbcMappingName() + commands)) {
+      LOG.debug("Executed derby command '{}' on connection '{}'.", commands, connection);
+    }
+  }
+
+  public void dropDB() {
+    try {
+      runDerbyCommand(";drop=true");
+    }
+    catch (SQLException e) {
+      throw BEANS.get(PlatformExceptionTranslator.class).translate(e);
+    }
+  }
+
+  @Override
+  public void destroy() {
+    try {
+      // Destroy connection pool first
+      super.destroy(); // Expects the property scout.sql.jdbc.driverUnload to be set to false, otherwise super.destroy() will throw an exception
+    }
+    finally {
+      shutdownDB();
+    }
+  }
+
+  protected void shutdownDB() {
+    // How to embed derby: https://db.apache.org/derby/papers/DerbyTut/embedded_intro.html
+    // Shutdown: https://docs.oracle.com/javadb/10.8.3.0/ref/rrefattrib16471.html
+    try {
+      runDerbyCommand(";shutdown=true"); // Shutdown derby
+    }
+    catch (SQLException e) {
+      boolean shutDownSuccessful = (e.getErrorCode() == 50000 && "XJ015".equals(e.getSQLState())) // error codes for successful full Derby shutdown
+          || (e.getErrorCode() == 45000 && "08006".equals(e.getSQLState())); // error code for successful single Derby database shutdown
+      if (shutDownSuccessful) {
+        // According to the embedding HowTo above: A clean shutdown throws SQL exceptions, which can be ignored
+        LOG.debug("Derby database shutdown completed successfully.", e);
+      }
+      else {
+        LOG.warn("Exception while shutting down derby", e);
+      }
+    }
+  }
 }
