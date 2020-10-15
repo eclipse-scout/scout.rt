610e6bd66d6724ef59f272a2c97696a5bd5ee59b
Beat Schwarzentrub <bsh@bsiag.com>
2020-10-15 16:18:17 +0200

    LocaleUtility: add methods to get display texts from locale

--- org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/nls/LocaleUtility.java
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/nls/LocaleUtility.java
@@ -12,7 +12,10 @@ package org.eclipse.scout.rt.platform.nls;
 
 import java.util.Locale;
 
+import org.eclipse.scout.rt.platform.util.StringUtility;
+
 public final class LocaleUtility {
+
   private LocaleUtility() {
   }
 
@@ -36,5 +39,50 @@ public final class LocaleUtility {
       return new Locale(s.substring(0, a), s.substring(a + 1));
     }
     return new Locale(s);
+  }
+
+  /**
+   * Returns the given locale as a string in the form <i>Language, Country</i>. The name of the language is returned in
+   * the language of the {@linkplain NlsLocale#get() current locale}. The country is returned in the short form
+   * (region/country code). It is omitted if the locale contains no country information.
+   * <p>
+   * Examples:
+   * <ul>
+   * <li>"English, US"
+   * <li>"German, CH"
+   * <li>"Deutsch, CH"
+   * </ul>
+   *
+   * @return {@code null} if the given locale is {@code null}
+   */
+  public static String getLocaleText(Locale locale) {
+    if (locale == null) {
+      return null;
+    }
+    StringBuilder sb = new StringBuilder();
+
+    // Language
+    sb.append(getLanguageText(locale));
+
+    // Country
+    String countryShort = locale.getCountry();
+    if (StringUtility.hasText(countryShort)) {
+      sb.append(", ").append(countryShort.toUpperCase());
+    }
+
+    return sb.toString();
+  }
+
+  /**
+   * Returns the name of the language represented by the given locale. The name is returned in the language of the
+   * {@linkplain NlsLocale#get() current locale}.
+   *
+   * @return {@code null} if the given locale is {@code null}
+   */
+  public static String getLanguageText(Locale locale) {
+    if (locale == null) {
+      return null;
+    }
+    return locale.getDisplayLanguage(NlsLocale.get());
   }
 }
