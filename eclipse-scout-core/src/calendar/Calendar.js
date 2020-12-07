240c1b3fdef8e6834bebd256fef38852cccc30d5
André Wegmüller <awe@bsiag.com>
2020-12-07 18:20:33 +0100

    Fixed bug for full-day item spanning more than one day
    When calendar was in week-mode and user switched to the next week
    _updateTopGrid tried to remove a component which has already been
    removed. This bug is now fixed.
    
    270250

--- eclipse-scout-core/src/calendar/Calendar.js
+++ eclipse-scout-core/src/calendar/Calendar.js
@@ -818,8 +818,11 @@ export default class Calendar extends Widget {
   }
 
   _updateTopGrid() {
-    $('.calendar-component', this.$topGrid).each(function($index, part) {
-      $(part).data('component').remove();
+    $('.calendar-component', this.$topGrid).each((index, part) => {
+      let component = $(part).data('component');
+      if (component) {
+        component.remove();
+      }
     });
     var allDayComponents = this.components.filter(component => component.fullDay);
     // first remove all components and add them from scratch
