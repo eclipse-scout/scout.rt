4efe93e4dc89dc0d593366f2683371b835c0f2a6
Matthias Villiger <mvi@bsi-software.com>
2020-12-17 11:42:13 +0100

    Update file headers

--- scout-hellojs-app/src/main/resources/archetype-resources/__rootArtifactId__.ui/src/test/js/test-index.js
+++ scout-hellojs-app/src/main/resources/archetype-resources/__rootArtifactId__.ui/src/test/js/test-index.js
@@ -1,13 +1,3 @@
-/*
- * Copyright (c) 2010-2020 BSI Business Systems Integration AG.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     BSI Business Systems Integration AG - initial API and implementation
- */
 import {JasmineScout} from '@eclipse-scout/core/src/testing';
 
 import * as ref1 from '../../main/js/index';
