8d344d9f3e703ae9322211ec0f57917dc315ae98
Matthias Villiger <mvi@bsi-software.com>
2020-12-17 11:47:12 +0100

    Update dependencies to Eclipse 2020-12
    # Conflicts:
    #	scout-hellojs-app/src/main/resources/archetype-resources/__rootArtifactId__/pom.xml

--- scout-hellojs-app/src/main/resources/archetype-resources/__rootArtifactId__.db/src/main/java/db/JooqEnvironmentService.java
+++ scout-hellojs-app/src/main/resources/archetype-resources/__rootArtifactId__.db/src/main/java/db/JooqEnvironmentService.java
@@ -29,29 +29,19 @@ public class JooqEnvironmentService implements IService {
   private static final Logger LOG = LoggerFactory.getLogger(JooqEnvironmentService.class);
 
   public void runInJooq(Consumer<DSLContext> task, String jdbcMappingName, SQLDialect dialect, String username, String password) {
-	    try (Connection connection = DriverManager.getConnection(jdbcMappingName, username, password)) {
-	      Configuration configuration = new DefaultConfiguration()
-	          .set(connection)
-	          .set(dialect);
-
-	      Settings s = configuration.settings();
-	      s.withRenderMapping(new RenderMapping()
-	          .withSchemata(
-	              new MappedSchema()
-	          .withInputExpression(Pattern.compile("Schema"))
-	                  .withOutput(CONFIG.getPropertyValue(SchemaProperty.class))));
-
-	      configuration.set(s);
-	      runWithConfiguration(task, configuration);
-	    }
-	    catch (SQLException e) {
-	      LOG.error("Failed to create connection.", e);
-	    }
-	  }
-
-  protected void runWithConfiguration(Consumer<DSLContext> task, Configuration configuration) {
-    try (DSLContext ctx = DSL.using(configuration)) {
-      task.accept(ctx);
+    try (Connection connection = DriverManager.getConnection(jdbcMappingName, username, password)) {
+      Configuration configuration = new DefaultConfiguration()
+          .set(connection)
+          .set(dialect);
+      Settings s = configuration.settings();
+      s.withRenderMapping(new RenderMapping()
+          .withSchemata(new MappedSchema()
+              .withInputExpression(Pattern.compile("Schema"))
+              .withOutput(CONFIG.getPropertyValue(SchemaProperty.class))));
+      configuration.set(s);
+      task.accept(DSL.using(configuration));
+    } catch (SQLException e) {
+      LOG.error("Failed to create connection.", e);
     }
   }
 }
