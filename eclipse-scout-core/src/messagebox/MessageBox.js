5f56fb586922450fd8fc4e030652a552e54d3aed
Beat Schwarzentrub <bsh@bsiag.com>
2021-01-15 15:02:16 +0100

    MessageBox: try to preserve line breaks when copying HTML
    Use strings.plainText() to convert HTML to plain text, instead of
    jQuery's text() function. This way <br>, <p> and the like are converted
    to line breaks. For header and body the property values can be added
    directly to the result, without converting it back from the HTML.
    
    285828

--- eclipse-scout-core/src/messagebox/MessageBox.js
+++ eclipse-scout-core/src/messagebox/MessageBox.js
@@ -264,9 +264,9 @@ export default class MessageBox extends Widget {
       }
       let dataType = ie ? 'Text' : 'text/plain';
       let plainText = strings.join('\n\n',
-        this.$header.text(),
-        this.$body.text(),
-        this.$html.text(),
+        this.header,
+        this.body,
+        strings.plainText(this.$html[0].outerHTML, {compact: true, trim: true}),
         this.hiddenText);
       clipboardData.setData(dataType, plainText);
       this.$container.window(true).getSelection().removeAllRanges();
