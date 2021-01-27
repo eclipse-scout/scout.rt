36c4c8ac5f8624e9afcfd3f09000b3a1d63754fc
Beat Schwarzentrub <bsh@bsiag.com>
2021-01-27 18:41:25 +0100

    Outline: add inspector hints to pages again
    Some Scout versions ago, the inspector hints were added to the page
    so that inspecting the page using browsers dev tool revealed the
    model class and class id. This feature broke as the tree node logic
    was separated from tree into the class TreeNode without moving
    the function responsible for the inspector hints.
    
    [Backport of cfbd0a6cb52fc11e756e958e015162a4c6634db4 to 10.0]
    
    287063

--- eclipse-scout-core/src/desktop/outline/Outline.js
+++ eclipse-scout-core/src/desktop/outline/Outline.js
@@ -17,7 +17,6 @@ import {
   Form,
   GroupBoxMenuItemsOrder,
   HtmlComponent,
-  inspector,
   keyStrokeModifier,
   MenuBar,
   menus as menus_1,
@@ -343,16 +342,6 @@ export default class Outline extends Tree {
       // Also make sure other objects hold no reference to a destroyed form (e.g. bench)
       this._triggerPageChanged(node);
     }.bind(this));
-  }
-
-  /**
-   * @override
-   */
-  _decorateNode(node) {
-    super._decorateNode(node);
-    if (this.session.inspector) {
-      inspector.applyInfo(node, node.$node);
-    }
   }
 
   // Info: we pass the keyStrokeContext of the parent (table or form) to
