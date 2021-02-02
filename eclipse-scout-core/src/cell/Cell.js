eclipse-scout/scout.rt

commit dbdecf03ca68a36719a271ed793960e346c9e50a

Table.js: fix flickering when editing a cell of a smart column
The display text of a smart column will be resolved later.
To prevent flickering the text should always be set.
If the column formats the text in another way than the field
(which is very unlikely) the text will change again after
 setValue is called.

Change-Id: I66fd869b55707344266b83ab3ffda5aec07ba10d
Reviewed-on: https://git.eclipse.org/r/c/scout/org.eclipse.scout.rt/+/175429
Tested-by: Scout Bot <scout-bot@eclipse.org>
Reviewed-by: Beat Schwarzentrub <bsh@bsiag.com>

eclipse-scout-core/src/table/Table.js
@@ -3108,11 +3108,13 @@ export default class Table extends Widget {
     if (saveEditorValue) {
       let column = cellEditorPopup.column;
       let row = cellEditorPopup.row;
-      this.setCellErrorStatus(column, row, field.errorStatus); // always get the errorStatus from the editor
-      if (field.errorStatus) {
-        // if there is an error from the editor, the displayText of the cell has to be updated
-        this.setCellText(column, row, field.displayText);
-      } else {
+      // Always update text even if value will be set to prevent flickering when display text is updated async.
+      // In most of the cases the text computed by the column will be the same as the one from the field.
+      this.setCellText(column, row, field.displayText);
+      // Always get the errorStatus from the editor
+      this.setCellErrorStatus(column, row, field.errorStatus);
+      if (!field.errorStatus) {
+        // Only update the cell value if there is no error
         this.setCellValue(column, row, field.value);
       }
     }

commit 1936518a61cbe4b80a0ea0ecc95f8dfd1f25e5da

Table.js: always setting text after edit can break custom columns
The commit dbdecf03 (fix flickering when editing a cell of a smart
column) introduced a non intended breaking change.
Column.buildCell is called when a row is updated
which happens when the cell text changes.
If a custom column overrides that method
and uses the value to build the cell it could fail
because the value is not updated yet.

To fix this, the flickering prevention is now moved
to the smart column because it is the only (built in) column that
resolves the text async.

Also add a check in setCellText to prevent an unnecessary double
execution of updateRow. It should actually belong to Column.js
but unfortunately it breaks the boolean column (mouse click on the
checkbox would not toggle it anymore).

287569

eclipse-scout-core/src/cell/Cell.js
@@ -70,7 +70,7 @@ export default class Cell {
     this.text = text;
 
     // reset cached encodedText, so when encodedText() is called the next time
-    // will be set to the a new value
+    // it will be set to the a new value
     if (oldText !== this.text) {
       this._cachedEncodedText = null;
     }

eclipse-scout-core/src/table/Table.js
@@ -3107,16 +3107,7 @@ export default class Table extends Widget {
     saveEditorValue = scout.nvl(saveEditorValue, false);
     if (saveEditorValue) {
       let column = cellEditorPopup.column;
-      let row = cellEditorPopup.row;
-      // Always update text even if value will be set to prevent flickering when display text is updated async.
-      // In most of the cases the text computed by the column will be the same as the one from the field.
-      this.setCellText(column, row, field.displayText);
-      // Always get the errorStatus from the editor
-      this.setCellErrorStatus(column, row, field.errorStatus);
-      if (!field.errorStatus) {
-        // Only update the cell value if there is no error
-        this.setCellValue(column, row, field.value);
-      }
+      column.updateCellFromEditor(cellEditorPopup.row, field);
     }
     field.destroy();
   }

eclipse-scout-core/src/table/columns/Column.js
@@ -730,6 +730,15 @@ export default class Column {
     });
   }
 
+  updateCellFromEditor(row, field) {
+    this.setCellErrorStatus(row, field.errorStatus);
+    if (field.errorStatus) {
+      this.setCellText(row, field.displayText);
+    } else {
+      this.setCellValue(row, field.value);
+    }
+  }
+
   /**
    * Override this function to install a specific compare function on a column instance.
    * The default impl. installs a generic comparator working with less than and greater than.

eclipse-scout-core/src/table/columns/SmartColumn.js
@@ -216,6 +216,24 @@ export default class SmartColumn extends Column {
   }
 
   /**
+   * @override
+   */
+  updateCellFromEditor(row, field) {
+    // Always set the text even if there is no error and the value will be set (by the super call).
+    // This prevents flickering when display text is updated async.
+    // In most of the cases the text computed by the column will be the same as the one from the field.
+    if (!this.errorStatus) {
+      let cell = this.cell(row);
+      if (cell.value === field.value) {
+        // If value did not change do nothing (important if column formats the value in a different way than the field)
+        return;
+      }
+      this.setCellText(row, field.displayText);
+    }
+    super.updateCellFromEditor(row, field);
+  }
+
+  /**
    * Since we don't know the type of the key from the lookup-row we must deal with numeric and string types here.
    */
   _hasCellValue(cell) {
@@ -230,5 +248,17 @@ export default class SmartColumn extends Column {
     super.setCellValue(row, value);
     let cell = this.cell(row);
     cell.setSortCode(this._calculateCellSortCode(cell));
+  }
+
+  setCellText(row, text, cell) {
+    if (!cell) {
+      cell = this.cell(row);
+    }
+    if (cell.text === text) {
+      // Break if text did not change.
+      // This should actually be in Column.js but some columns never use a text but still need updateRows to be called (e.g. BooleanColumn)
+      return;
+    }
+    super.setCellText(row, text, cell);
   }
 }