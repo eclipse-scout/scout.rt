4f5bf18b1152a5237f8245f0622775f5cf4723d1
Torsten Trompler <Torsten.Trompler@bsi-software.com>
2021-03-31 10:30:39 +0200

    Add icon id and css class to AbstractLookupRowDo
    Signed-off-by: Torsten Trompler <Torsten.Trompler@bsi-software.com>

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/services/lookup/AbstractRestLookupCall.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/services/lookup/AbstractRestLookupCall.java
@@ -270,7 +270,9 @@ public abstract class AbstractRestLookupCall<RESTRICTION extends AbstractLookupR
     return new LookupRow<>(row.getId(), row.getText())
         .withActive(row.isActive())
         .withEnabled(row.isEnabled())
-        .withParentKey(row.getParentId());
+        .withParentKey(row.getParentId())
+        .withIconId(row.getIconId())
+        .withCssClass(row.getCssClass());
   }
 
   @Override
