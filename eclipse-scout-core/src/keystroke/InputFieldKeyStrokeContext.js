eclipse-scout/scout.rt

commit 9e44d2e8b82ff0288319a0b31099fdaa4441189f

Optimize key strokes
Table:
The removal of the visibleRows check by the commit 5a9ca395 led to an
exception if the user presses a navigation key in an empty table,
because the handle functions don't check for empty table again.
The NPE was (accidentally) fixed by the cherry pick commit 2a14b858.
Even though there is no exception anymore, it still behaves differently
than the tree: if there is an empty table in a search form, pressing
up moves the selection of the outer table. This does not happen when
doing the same in a tree, because the tree accepts the key stroke and
applies the propagation flags, the table does not accept it due to the
visibleRows check -> Since we do not want to refactor the navigation
keystrokes and check for visible rows in every sub class, we just
apply the propagation flags even if there are no rows.

To make sure the key strokes are shown correctly in the described case,
the fix in KeyStroke.js is necessary. When pressing F1, a virtual key
event is created. If this is done for a table navigation key stroke,
the property "shift" would be undefined and therefore never accepted.
This change should only affect F1, because a real key stroke event
never uses undefined.

StringField:
A similar issue can be found in a string field. If a multiline string
field is contained in a search form, up/down won't move the cursor
in the string field. Instead, the selection of the outer table is moved.

294248

eclipse-scout-core/src/keystroke/InputFieldKeyStrokeContext.js
@@ -16,45 +16,29 @@ import $ from 'jquery';
  */
 export default class InputFieldKeyStrokeContext extends KeyStrokeContext {
 
-  constructor() {
+  constructor(multiline) {
     super();
 
     this.invokeAcceptInputOnActiveValueField = true;
 
+    let navigationKeys = [
+      keys.RIGHT,
+      keys.LEFT,
+      keys.HOME,
+      keys.END,
+      keys.NUMPAD_4,
+      keys.NUMPAD_6
+    ];
     this.registerStopPropagationKeys(keyStrokeModifier.CTRL, [
       keys.A,
       keys.C,
       keys.Y,
       keys.V,
       keys.Z,
-      keys.RIGHT,
-      keys.BACKSPACE,
-      keys.LEFT,
-      keys.HOME,
-      keys.END,
-      keys.NUMPAD_4,
-      keys.NUMPAD_6
-    ]);
-
-    this.registerStopPropagationKeys(keyStrokeModifier.CTRL | keyStrokeModifier.SHIFT, [ // NOSONAR
-      keys.RIGHT,
-      keys.BACKSPACE,
-      keys.LEFT,
-      keys.HOME,
-      keys.END,
-      keys.NUMPAD_4,
-      keys.NUMPAD_6
-    ]);
-
-    this.registerStopPropagationKeys(keyStrokeModifier.SHIFT, [
-      keys.RIGHT,
-      keys.BACKSPACE,
-      keys.LEFT,
-      keys.HOME,
-      keys.END,
-      keys.NUMPAD_4,
-      keys.NUMPAD_6
-    ]);
+      keys.BACKSPACE
+    ].concat(navigationKeys));
+    this.registerStopPropagationKeys(keyStrokeModifier.CTRL | keyStrokeModifier.SHIFT, navigationKeys);
+    this.registerStopPropagationKeys(keyStrokeModifier.SHIFT, navigationKeys);
     this.registerStopPropagationKeys(keyStrokeModifier.NONE, [
       keys.SEMICOLON,
       keys.DASH,
@@ -81,14 +65,31 @@ export default class InputFieldKeyStrokeContext extends KeyStrokeContext {
       keys.NUMPAD_8,
       keys.NUMPAD_9,
       keys.MULTIPLY,
-      keys.END,
-      keys.HOME,
-      keys.RIGHT,
       keys.BACKSPACE,
-      keys.LEFT,
       keys.DELETE,
       keys.SPACE
-    ]);
+    ].concat(navigationKeys));
+    this.setMultiline(multiline);
+  }
+
+  setMultiline(multiline) {
+    let multilineNavigationKeys = [
+      keys.UP,
+      keys.DOWN,
+      keys.NUMPAD_2,
+      keys.NUMPAD_8
+    ];
+    if (multiline) {
+      this.registerStopPropagationKeys(keyStrokeModifier.CTRL, multilineNavigationKeys);
+      this.registerStopPropagationKeys(keyStrokeModifier.CTRL | keyStrokeModifier.SHIFT, multilineNavigationKeys);
+      this.registerStopPropagationKeys(keyStrokeModifier.SHIFT, multilineNavigationKeys);
+      this.registerStopPropagationKeys(keyStrokeModifier.NONE, multilineNavigationKeys);
+    } else {
+      this.unregisterStopPropagationKeys(keyStrokeModifier.CTRL, multilineNavigationKeys);
+      this.unregisterStopPropagationKeys(keyStrokeModifier.CTRL | keyStrokeModifier.SHIFT, multilineNavigationKeys);
+      this.unregisterStopPropagationKeys(keyStrokeModifier.SHIFT, multilineNavigationKeys);
+      this.unregisterStopPropagationKeys(keyStrokeModifier.NONE, multilineNavigationKeys);
+    }
   }
 
   _applyPropagationFlags(event) {

eclipse-scout-core/src/form/fields/stringfield/StringField.js
@@ -72,10 +72,15 @@ export default class StringField extends BasicField {
   }
 
   /**
-   * @override Widget.js
+   * @override
    */
   _createKeyStrokeContext() {
     return new InputFieldKeyStrokeContext();
+  }
+
+  _init(model) {
+    super._init(model);
+    this._setMultilineText(this.multilineText);
   }
 
   _render() {
@@ -416,6 +421,11 @@ export default class StringField extends BasicField {
 
   _renderTrimText() {
     // nop, property used in _validateDisplayText()
+  }
+
+  _setMultilineText(multilineText) {
+    this._setProperty('multilineText', multilineText);
+    this.keyStrokeContext.setMultiline(this.multilineText);
   }
 
   _renderGridData() {

eclipse-scout-core/src/keystroke/KeyStroke.js
@@ -313,6 +313,6 @@ export default class KeyStroke {
   }
 
   static _acceptModifier(modifier, eventModifier) {
-    return modifier === undefined || modifier === eventModifier;
+    return modifier === undefined || eventModifier === undefined || modifier === eventModifier;
   }
 }

eclipse-scout-core/src/keystroke/KeyStrokeContext.js
@@ -50,12 +50,25 @@ export default class KeyStrokeContext {
   /**
    * Registers the given keys as 'stopPropagation' keys, meaning that any keystroke event with that key and matching the modifier bit mask is prevented from bubbling the DOM tree up.
    *
-   * @param modifierBitMask bitwise OR'ing together modifier constants to match a keystroke event. (KeyStrokeModifier.js)
-   * @param keys the keys to match a keystroke event.
+   * @param {number} modifierBitMask bitwise OR'ing together modifier constants to match a keystroke event. (KeyStrokeModifier.js)
+   * @param {[number]} keys the keys to match a keystroke event.
    */
   registerStopPropagationKeys(modifierBitMask, keys) {
     this._stopPropagationKeys[modifierBitMask] = this._stopPropagationKeys[modifierBitMask] || [];
     arrays.pushAll(this._stopPropagationKeys[modifierBitMask], keys);
+  }
+
+  /**
+   * Unregisters the given keys as 'stopPropagation' keys.
+   *
+   * @param {number} modifierBitMask bitwise OR'ing together modifier constants to match a keystroke event. (KeyStrokeModifier.js)
+   * @param {[number]} keys the keys to match a keystroke event.
+   */
+  unregisterStopPropagationKeys(modifierBitMask, keys) {
+    if (!this._stopPropagationKeys[modifierBitMask]) {
+      return;
+    }
+    arrays.removeAll(this._stopPropagationKeys[modifierBitMask], keys);
   }
 
   /**

eclipse-scout-core/src/table/keystrokes/AbstractTableNavigationKeyStroke.js
@@ -28,6 +28,9 @@ export default class AbstractTableNavigationKeyStroke extends KeyStroke {
     }
 
     if (!this.field.visibleRows.length) {
+      // Key stroke is accepted but currently not executable because there are no rows.
+      // Apply propagation flags to prevent bubbling, a focused table should always swallow the navigation keys even if there are no rows.
+      this._applyPropagationFlags(event);
       return false;
     }
 

eclipse-scout-core/src/widget/Widget.js
@@ -264,12 +264,8 @@ export default class Widget {
     if (!this.keyStrokeContext) {
       return;
     }
-    this.keyStrokeContext.$scopeTarget = function() {
-      return this.$container;
-    }.bind(this);
-    this.keyStrokeContext.$bindTarget = function() {
-      return this.$container;
-    }.bind(this);
+    this.keyStrokeContext.$scopeTarget = () => this.$container;
+    this.keyStrokeContext.$bindTarget = () => this.$container;
   }
 
   destroy() {

commit c01a0e28ddbcd8f08ab73e3e5fec75a92672c749

InputField: don't stop propagation for numpad keys with modifiers
ctrl-1-9 activates the desktop tabs. Using ctrl-4 and ctrl-6 on the
numpad does not work while the focus is in an input field. But other
numbers work (e.g. ctrl-2) which is confusing.

This change also fixes a bug introduced by
9e44d2e8b82ff0288319a0b31099fdaa4441189f. Pressing a numpad
key without a modifier must always stop propagation, otherwise entering
number 2 or 8 in an input field of a search form won't work anymore,
since the table filter would grab the focus. This broke
because numpad_2 and 8 were unregistered in setMultiline.

256789

eclipse-scout-core/src/keystroke/InputFieldKeyStrokeContext.js
@@ -25,9 +25,7 @@ export default class InputFieldKeyStrokeContext extends KeyStrokeContext {
       keys.RIGHT,
       keys.LEFT,
       keys.HOME,
-      keys.END,
-      keys.NUMPAD_4,
-      keys.NUMPAD_6
+      keys.END
     ];
     this.registerStopPropagationKeys(keyStrokeModifier.CTRL, [
       keys.A,
@@ -75,9 +73,7 @@ export default class InputFieldKeyStrokeContext extends KeyStrokeContext {
   setMultiline(multiline) {
     let multilineNavigationKeys = [
       keys.UP,
-      keys.DOWN,
-      keys.NUMPAD_2,
-      keys.NUMPAD_8
+      keys.DOWN
     ];
     if (multiline) {
       this.registerStopPropagationKeys(keyStrokeModifier.CTRL, multilineNavigationKeys);