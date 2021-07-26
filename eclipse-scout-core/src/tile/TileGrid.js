5384c227b59a7e661d248d4d792fbdfae0739a1d
Fritz Schinkel <fritz.schinkel@bsi-software.com>
2021-07-26 15:37:42 +0200

    TileGrid: ensure layout is valid after inserting a hidden tile
    If a tile is inserted but not visible (e.g. due to an active filter),
    it gets the animate-insert class which prevents the layout validation
    -> don't animate invisible tiles
    
    Also, if all tiles are made visible/invisible by a filter, the scrollbar
    won't be updated correctly. It works well, if only some tiles are
    affected because in that case the TileGridLayout does the scrollbar
    update after the move animation => Update scrollbar after the
    filter-animation.
    Since this is a fast process it is not necessary to do it only once
    for all tiles, as it is done when tiles are deleted.
    
    274512
    
    Change-Id: I4ec40618eb366475af237b7fe21547c14d7d56ae
    Reviewed-on: https://git.eclipse.org/r/c/scout/org.eclipse.scout.rt/+/183381
    Tested-by: Scout Bot <scout-bot@eclipse.org>
    Reviewed-by: Fritz Schinkel <fritz.schinkel@bsi-software.com>

--- eclipse-scout-core/src/tile/TileGrid.js
+++ eclipse-scout-core/src/tile/TileGrid.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2018 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2021 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -406,11 +406,11 @@ export default class TileGrid extends Widget {
   }
 
   _animateTileRemoval(tile) {
-    return this.animateTileRemoval && !(tile instanceof PlaceholderTile);
+    return this.animateTileRemoval && tile && tile.isVisible() && !(tile instanceof PlaceholderTile);
   }
 
   _animateTileInsertion(tile) {
-    return this.animateTileInsertion && !(tile instanceof PlaceholderTile);
+    return this.animateTileInsertion && tile && tile.isVisible() && !(tile instanceof PlaceholderTile);
   }
 
   _onAnimatedTileRemove(tile) {
