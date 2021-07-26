dadd68ce5ab4211cb09c58fc83eb3b4475db9482
Fritz Schinkel <fritz.schinkel@bsi-software.com>
2021-07-26 10:37:05 +0200

    Fix HTMLTest
    Add font-icon here as it is always added by HTML.icon(CharSequence).
    
    Change-Id: I13bebab5f4ccb0e0f3b5ece7ee91f54aa403cefc
    Reviewed-on: https://git.eclipse.org/r/c/scout/org.eclipse.scout.rt/+/183360
    Tested-by: Scout Bot <scout-bot@eclipse.org>
    Reviewed-by: Fritz Schinkel <fritz.schinkel@bsi-software.com>

--- org.eclipse.scout.rt.platform.test/src/test/java/org/eclipse/scout/rt/platform/html/HTMLTest.java
+++ org.eclipse.scout.rt.platform.test/src/test/java/org/eclipse/scout/rt/platform/html/HTMLTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2018 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2021 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -319,7 +319,7 @@ public class HTMLTest {
 
   @Test
   public void testIconFromCustomFont() {
-    assertEquals("<span class=\"font-testIcons\">\uE100</span>", HTML.icon("font:testIcons \uE100").toHtml());
+    assertEquals("<span class=\"font-icon font-testIcons\">\uE100</span>", HTML.icon("font:testIcons \uE100").toHtml());
   }
 
   @Test
