e2900e3078c69928dfb8caab4cf6cbdfc1b20ab5
Ivan Motsch <ivan.motsch@bsiag.com>
2021-08-25 23:34:39 +0200

    Update password policy
    Signed-off-by: Ivan Motsch <ivan.motsch@bsiag.com>

--- org.eclipse.scout.rt.server/src/main/java/org/eclipse/scout/rt/server/services/common/pwd/DefaultPasswordPolicy.java
+++ org.eclipse.scout.rt.server/src/main/java/org/eclipse/scout/rt/server/services/common/pwd/DefaultPasswordPolicy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2018 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2021 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -17,7 +17,7 @@ import org.eclipse.scout.rt.platform.text.TEXTS;
 
 public class DefaultPasswordPolicy implements IPasswordPolicy {
 
-  private static final int MIN_PASSWORD_LENGTH = 8;
+  private static final int MIN_PASSWORD_LENGTH = 12;
 
   @Override
   public String getText() {
