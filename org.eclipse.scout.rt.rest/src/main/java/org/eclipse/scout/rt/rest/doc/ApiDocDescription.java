274c60d86d2d6ce80e8669b5a19ab7de103b1b53
Bruno Koeferli <bruno.koeferli@bsiag.com>
2021-10-27 14:06:05 +0200

    ApiDocGenerator: Support for inheritance for @ApiDocDescription

--- org.eclipse.scout.rt.rest/src/main/java/org/eclipse/scout/rt/rest/doc/ApiDocDescription.java
+++ org.eclipse.scout.rt.rest/src/main/java/org/eclipse/scout/rt/rest/doc/ApiDocDescription.java
@@ -11,6 +11,7 @@
 package org.eclipse.scout.rt.rest.doc;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -25,6 +26,7 @@ import org.eclipse.scout.rt.platform.text.TEXTS;
  */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 public @interface ApiDocDescription {
 
   /**
