ed7678a6ee6fa4b300fdc58004e62d9b96c2c72e
Fritz Schinkel <fritz.schinkel@bsi-software.com>
2021-11-16 11:30:05 +0100

    BrushUp: loading indicators
    Remove venn chart loading indicator.
    Fix loading indicator for DesktopBench, RadioButtonGroup and Table.

--- eclipse-scout-core/src/widget/LoadingSupport.js
+++ eclipse-scout-core/src/widget/LoadingSupport.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2018 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2021 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -83,8 +83,10 @@ export default class LoadingSupport {
       return;
     }
 
-    this._$loadingIndicator.fadeOutAndRemove(() => {
-      this._$loadingIndicator = null;
+    this._$loadingIndicator.css('opacity', this._$loadingIndicator.css('opacity'));
+    this._$loadingIndicator.addClass('animate-remove');
+    this._$loadingIndicator.oneAnimationEnd(() => {
+      this.remove();
       if (this.widget.rendered) {
         // Show widget's content (layout if necessary)
         this.$container.removeClass('loading');
