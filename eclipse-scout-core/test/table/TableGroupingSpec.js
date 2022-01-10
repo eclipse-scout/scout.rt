0f3c86786d52f6fb99aa202173568cc1becc9ff8
Matthias Villiger <mvi@bsi-software.com>
2022-01-10 16:38:15 +0100

    BrushUp: Table grouping style
    Fix test case because default grouping style is TOP now

--- eclipse-scout-core/test/table/TableGroupingSpec.js
+++ eclipse-scout-core/test/table/TableGroupingSpec.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2021 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -224,6 +224,7 @@ describe('Table Grouping', () => {
     prepareTable();
     prepareContent();
     table.viewRangeSize = 3;
+    table.groupingStyle = Table.GroupingStyle.BOTTOM;
     render(table);
 
     expect(find$aggregateRows(table).length).toBe(0);
