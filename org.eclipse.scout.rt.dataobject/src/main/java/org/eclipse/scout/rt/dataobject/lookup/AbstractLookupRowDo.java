90f0c42f30e2ba9784df30f6ef8269bcb8719e59
Fritz Schinkel <fritz.schinkel@bsi-software.com>
2022-02-18 23:26:26 +0100

    Add iconId constants to AbstractLookupRowDo

--- org.eclipse.scout.rt.dataobject/src/main/java/org/eclipse/scout/rt/dataobject/lookup/AbstractLookupRowDo.java
+++ org.eclipse.scout.rt.dataobject/src/main/java/org/eclipse/scout/rt/dataobject/lookup/AbstractLookupRowDo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2020 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -28,6 +28,7 @@ public abstract class AbstractLookupRowDo<ID> extends DoEntity {
   public static final String TEXT = "text";
   public static final String ENABLED = "enabled";
   public static final String ACTIVE = "active";
+  public static final String ICON_ID = "iconId";
 
   protected AbstractLookupRowDo() {
     withEnabled(true); // lookup rows are enabled by default
@@ -49,7 +50,7 @@ public abstract class AbstractLookupRowDo<ID> extends DoEntity {
   }
 
   public DoValue<String> iconId() {
-    return doValue("iconId");
+    return doValue(ICON_ID);
   }
 
   public DoValue<String> cssClass() {
