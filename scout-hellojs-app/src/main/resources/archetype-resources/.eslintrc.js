24214d019ff670b2f20f8716579260ca65a2208e
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2022-03-04 13:21:38 +0100

    Fix eslint
    eslint-plugin-babel is deprecated and not compatible with
     @babel/eslint-parser -> running eslint fails.
    
    Also adjust some rules and update to latest version.
    
    315412

--- scout-hellojs-app/src/main/resources/archetype-resources/.eslintrc.js
+++ scout-hellojs-app/src/main/resources/archetype-resources/.eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   extends: '@eclipse-scout',
-  plugins: ['babel'],
-  parser: 'babel-eslint',
+  plugins: ['@babel'],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false
+  },
   rules: {
     'linebreak-style': 'off'
   }
