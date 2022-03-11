d77d2bcd40ccd3b85544c81603e71241481d6502
Paolo Bazzi <paolo.bazzi@bsi-software.com>
2022-03-11 13:30:22 +0100

    Improve JarManifestHelper: allow an inner class as manifestResourceBase
    315436

--- org.eclipse.scout.rt.platform.test/src/test/java/org/eclipse/scout/rt/platform/util/JarManifestHelperTest.java
+++ org.eclipse.scout.rt.platform.test/src/test/java/org/eclipse/scout/rt/platform/util/JarManifestHelperTest.java
@@ -18,6 +18,10 @@ import java.util.jar.Attributes.Name;
 
 import org.eclipse.scout.rt.platform.BEANS;
 import org.eclipse.scout.rt.platform.Platform;
+import org.eclipse.scout.rt.platform.config.PlatformConfigProperties;
+import org.eclipse.scout.rt.platform.config.PlatformConfigProperties.PlatformVersionProperty;
+import org.eclipse.scout.rt.platform.util.JarManifestHelperTest.Fixture.FixtureEnclosing;
+import org.eclipse.scout.rt.platform.util.JarManifestHelperTest.Fixture.FixtureEnclosing.FixtureEnclosingEnclosing;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -39,6 +43,9 @@ public class JarManifestHelperTest {
 
     assertEquals("org.eclipse.scout.rt.platform", m_helper.getAttribute(Platform.class, Name.IMPLEMENTATION_TITLE.toString()));
     assertEquals("org.eclipse.scout.rt.platform", m_helper.getAttribute(Platform.class, Name.SPECIFICATION_TITLE.toString()));
+
+    assertEquals("org.eclipse.scout.rt.platform", m_helper.getAttribute(PlatformVersionProperty.class, Name.IMPLEMENTATION_TITLE.toString()));
+    assertEquals("org.eclipse.scout.rt.platform", m_helper.getAttribute(PlatformConfigProperties.PlatformVersionProperty.class, Name.SPECIFICATION_TITLE.toString()));
   }
 
   @Test
@@ -53,10 +60,31 @@ public class JarManifestHelperTest {
     assertNull(m_helper.getAttribute(Platform.class, "fooBarBaz"));
   }
 
+  @Test
+  public void testToClassName() {
+    assertEquals("PlatformConfigProperties$PlatformVersionProperty.class", m_helper.toClassName(PlatformVersionProperty.class));
+    assertEquals("Platform.class", m_helper.toClassName(Platform.class));
+    assertEquals("JarManifestHelperTest$Fixture.class", m_helper.toClassName(Fixture.class));
+    assertEquals("JarManifestHelperTest$Fixture$FixtureEnclosing.class", m_helper.toClassName(FixtureEnclosing.class));
+    assertEquals("JarManifestHelperTest$Fixture$FixtureEnclosing$FixtureEnclosingEnclosing.class", m_helper.toClassName(FixtureEnclosingEnclosing.class));
+  }
+
   /**
    * manifest file is not available if the module is not compiled to jar file, skip tests when executed locally
    */
   protected void assumeModuleCompiledToJar() {
     assumeTrue(m_helper.toJarClasspath(Platform.class) != null);
+  }
+
+  /**
+   * Fixture test classes
+   */
+  protected static class Fixture {
+    // nested static class
+    protected static class FixtureEnclosing {
+      // next nested static class
+      protected static class FixtureEnclosingEnclosing {
+      }
+    }
   }
 }
