ba4d5ef4418d481ac0f0d13d7ca76548565a1869
Hannes Mueller <hannes.mueller@bsi-software.com>
2022-03-22 16:41:55 +0100

    AbstractFormMenu: add missing constructors

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/form/AbstractFormMenu.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/form/AbstractFormMenu.java
@@ -35,6 +35,14 @@ import org.eclipse.scout.rt.platform.util.CollectionUtility;
 @ClassId("c40132fa-5aac-4d25-8330-76b9210c07ca")
 public abstract class AbstractFormMenu<FORM extends IForm> extends AbstractMenu implements IFormMenu<FORM> {
 
+  public AbstractFormMenu() {
+    this(true);
+  }
+
+  public AbstractFormMenu(boolean callInitializer) {
+    super(callInitializer);
+  }
+
   @Override
   protected boolean getConfiguredToggleAction() {
     return true;
@@ -58,7 +66,7 @@ public abstract class AbstractFormMenu<FORM extends IForm> extends AbstractMenu 
    * Subclasses can override this method. Default is {@code null}.
    *
    * @return a form type token
-   * @see {@link #startForm(IForm)} for details how the form gets started
+   * @see #startForm() for details how the form gets started see startForm()
    */
   @ConfigProperty(ConfigProperty.FORM)
   @Order(90)
