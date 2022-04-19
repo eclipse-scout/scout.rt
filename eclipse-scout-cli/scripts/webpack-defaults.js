a37b1936b342e84cccbfc25a6e3d17941e76fe57
Matthias Villiger <mvi@bsi-software.com>
2022-04-19 14:01:32 +0200

    Reduce parallelism for Terser
    The JavaScript minimizer (Terser) by default computes the parallelism
    for minimizing by the number of CPU cores in the system. This might be
    way too much as
    1. The code does not allow an unlimited number of parallel minimizing
    2. The CPUs in the system might be much more than available to the
       process (e.g. when running in a limited container)
    
    More parallel workers use more memory without giving a performance gain.
    Therefore, the parallelism is reduced to constant default value.

--- eclipse-scout-cli/scripts/webpack-defaults.js
+++ eclipse-scout-cli/scripts/webpack-defaults.js
@@ -3,7 +3,7 @@
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -216,7 +216,8 @@ module.exports = (env, args) => {
       }),
       // minify js
       new TerserPlugin({
-        test: /\.js(\?.*)?$/i
+        test: /\.js(\?.*)?$/i,
+        parallel: 4
       })
     ];
   }
