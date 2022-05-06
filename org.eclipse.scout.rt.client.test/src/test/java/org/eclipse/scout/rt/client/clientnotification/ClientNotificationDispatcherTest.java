f74ea1521fe3595fb4570790cbd217d61ad9ffc2
Matthias Villiger <mvi@bsi-software.com>
2022-05-06 09:21:52 +0200

    Skip ClientNotifications for sessions not starting or started
    If the client receives a notification for a session which is not yet
    starting or startet, the notification should not be dispatched.
    This ensures no UnsupportedOperationExceptions occur e.g. on not yet
    startet sessions when still a VirtualDesktop is active on the session.
    
    320004

---
+++ org.eclipse.scout.rt.client.test/src/test/java/org/eclipse/scout/rt/client/clientnotification/ClientNotificationDispatcherTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
+package org.eclipse.scout.rt.client.clientnotification;
+
+import static org.junit.Assert.*;
+
+import org.eclipse.scout.rt.client.AbstractClientSession;
+import org.eclipse.scout.rt.client.IClientSession;
+import org.eclipse.scout.rt.client.context.ClientRunContexts;
+import org.eclipse.scout.rt.client.job.ModelJobs;
+import org.eclipse.scout.rt.platform.IgnoreBean;
+import org.eclipse.scout.rt.shared.session.Sessions;
+import org.eclipse.scout.rt.testing.platform.runner.PlatformTestRunner;
+import org.eclipse.scout.rt.testing.platform.runner.RunWithSubject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWithSubject("default")
+@RunWith(PlatformTestRunner.class)
+public class ClientNotificationDispatcherTest {
+  @Test
+  public void testIsSessionValid() {
+    ClientNotificationDispatcher dispatcher = new ClientNotificationDispatcher();
+    String sid = Sessions.randomSessionId();
+
+    assertFalse(dispatcher.isSessionValid(null, sid)); // session not found
+
+    IClientSession session = new P_ClientNotificationDispatcherTestSession(dispatcher);
+    assertFalse(dispatcher.isSessionValid(session, sid)); // session not valid to receive notification if not started/starting
+
+    ModelJobs.schedule(() -> session.start(sid), ModelJobs
+        .newInput(ClientRunContexts.copyCurrent().withSession(session, true))
+        .withName("Starting ClientSession [sessionId={}]", sid)
+        .withExceptionHandling(null, false)).awaitDoneAndGet();
+    assertTrue(dispatcher.isSessionValid(session, sid)); // session valid to receive notification while started
+
+    ModelJobs.schedule(() -> session.stop(), ModelJobs
+        .newInput(ClientRunContexts.copyCurrent().withSession(session, true))
+        .withName("Stopping ClientSession [sessionId={}]", sid)
+        .withExceptionHandling(null, false)).awaitDoneAndGet();
+    assertTrue(dispatcher.isSessionValid(session, sid)); // session valid to receive notification while stopped
+  }
+
+  @IgnoreBean
+  public static class P_ClientNotificationDispatcherTestSession extends AbstractClientSession {
+
+    private final ClientNotificationDispatcher m_dispatcher;
+
+    public P_ClientNotificationDispatcherTestSession(ClientNotificationDispatcher dispatcher) {
+      super(true);
+      m_dispatcher = dispatcher;
+    }
+
+    @Override
+    protected void execLoadSession() {
+      assertTrue(m_dispatcher.isSessionValid(this, this.getId())); // session valid to receive notification while starting
+    }
+  }
+}
