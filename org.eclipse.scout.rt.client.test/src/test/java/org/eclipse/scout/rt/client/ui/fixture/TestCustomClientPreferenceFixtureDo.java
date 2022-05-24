99c869a3885535c24384e64debd97ceba3c8e6d9
Stephan Merkli <stephan.merkli@bsi-software.com>
2022-05-24 09:50:47 +0200

    Client preferences: allow typed custom client preferences
    Implemented as a preparation for a more type structure for client preferences.
    
    321546

---
+++ org.eclipse.scout.rt.client.test/src/test/java/org/eclipse/scout/rt/client/ui/fixture/TestCustomClientPreferenceFixtureDo.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2019 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
+package org.eclipse.scout.rt.client.ui.fixture;
+
+import javax.annotation.Generated;
+
+import org.eclipse.scout.rt.client.ui.fixture.ScoutTestFixtureTypeVersions.ScoutTestFixture_1_0_0;
+import org.eclipse.scout.rt.dataobject.DoEntity;
+import org.eclipse.scout.rt.dataobject.DoValue;
+import org.eclipse.scout.rt.dataobject.TypeName;
+import org.eclipse.scout.rt.dataobject.TypeVersion;
+import org.eclipse.scout.rt.shared.prefs.ICustomClientPreferenceDo;
+
+@TypeName("scoutTestFixture.TestCustomClientPreferenceFixture")
+@TypeVersion(ScoutTestFixture_1_0_0.class)
+public class TestCustomClientPreferenceFixtureDo extends DoEntity implements ICustomClientPreferenceDo {
+
+  public DoValue<String> name() {
+    return doValue("name");
+  }
+
+  /* **************************************************************************
+   * GENERATED CONVENIENCE METHODS
+   * *************************************************************************/
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public TestCustomClientPreferenceFixtureDo withName(String name) {
+    name().set(name);
+    return this;
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public String getName() {
+    return name().get();
+  }
+}
