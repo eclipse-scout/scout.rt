94564b9d424a6fd3aae04e9c0c3c44d6a6843f3d
Michael Rudolf <mru@bsi-software.com>
2022-06-30 15:19:43 +0200

    Added OrderAnnotationComparator
    A Comparator sorting Objects annotated with @Order

---
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/OrderAnnotationComparator.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
+package org.eclipse.scout.rt.platform;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+public final class OrderAnnotationComparator implements Comparator<Object>, Serializable {
+  private static final long serialVersionUID = 1L;
+
+  public static final OrderAnnotationComparator INSTANCE = new OrderAnnotationComparator();
+
+  private OrderAnnotationComparator() {
+  }
+
+  @Override
+  public int compare(Object a, Object b) {
+    if (a == b) {
+      return 0;
+    }
+    else if (a == null) {
+      return 1;
+    }
+    else if (b == null) {
+      return -1;
+    }
+    int cmp = Double.compare(getOrderOf(a.getClass()), getOrderOf(b.getClass()));
+    if (cmp != 0) {
+      return cmp;
+    }
+    return a.getClass().getName().compareTo(b.getClass().getName());
+  }
+
+  public static double getOrderOf(Class<?> c) {
+    double d = IBean.DEFAULT_BEAN_ORDER;
+    Order orderAnnotation;
+    while ((orderAnnotation = c.getAnnotation(Order.class)) == null && c.isAnnotationPresent(Replace.class)) {
+      c = c.getSuperclass();
+    }
+    if (orderAnnotation != null) {
+      d = orderAnnotation.value();
+    }
+    return d;
+  }
+}
