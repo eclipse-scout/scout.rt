b6c5aea1d39d32c0a3038246520257e836cb79f5
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2022-07-05 17:09:47 +0200

    Add default code format and linting files
    Add some typescript specific rules

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: '@eclipse-scout/eslint-config/src/typescript',
+  parserOptions: {
+    requireConfigFile: false
+  },
+  rules: {
+    'linebreak-style': 'off'
+  }
+};
