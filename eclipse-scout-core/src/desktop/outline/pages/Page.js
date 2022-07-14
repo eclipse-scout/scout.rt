fbf6ed2514e3912c5b6e29f16790285d06a81ffb
Fritz Schinkel <fritz.schinkel@bsi-software.com>
2022-07-14 16:41:41 +0200

    Add JsForm
    The JsForm is a Java-form that wraps a form implemented in JS. This
    allows a Scout Classic application to open a ScoutJS form. The whole
    lifecycle of this form will be handled by ScoutJS.
    Add possibility to not show a ScoutJS form on open().
    Improve detailForm support of ScoutJS page.
    
    325223

--- eclipse-scout-core/src/desktop/outline/pages/Page.js
+++ eclipse-scout-core/src/desktop/outline/pages/Page.js
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2010-2021 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -198,6 +198,11 @@ export default class Page extends TreeNode {
     if (form instanceof Form) {
       form.setModal(false);
       form.setClosable(false);
+
+      form.setDisplayHint(Form.DisplayHint.VIEW);
+      form.setDisplayViewId('C');
+
+      form.setShowOnOpen(false);
     }
     if (form instanceof TileOverviewForm) {
       form.setPage(this);
