6d25bec42230e04a3f821aa876a8b355cf6d9ea9
Matthias Villiger <mvi@bsi-software.com>
2022-08-02 15:04:05 +0200

    Add TypeScript support to webpack build

---
+++ eslint-config/src/common.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
+module.exports = {
+  rules: {
+    'prefer-const': 'off',
+    'prefer-rest-params': 'warn',
+    'prefer-spread': 'warn'
+  }
+};
