0ec35fd3301a9444b6ad836a4daf0efb770a897d
Matthias Villiger <mvi@bsi-software.com>
2022-08-02 15:04:05 +0200

    Add TypeScript support to webpack build

--- eclipse-scout-cli/bin/scout-scripts.js
+++ eclipse-scout-cli/bin/scout-scripts.js
@@ -1,10 +1,9 @@
-#!/usr/bin/env node
 /*
  * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -136,7 +135,7 @@ function runWebpack(args) {
 }
 
 function readWebpackConfig() {
-  let configFilePath = null;
+  let configFilePath;
   const devConfigFilePath = path.resolve(webpackCustomConfigFileName);
   if (fs.existsSync(devConfigFilePath)) {
     console.log(`Reading config from ${webpackCustomConfigFileName}`);
