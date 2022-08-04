adf30ed92c01bbbb8512dc29ec11ab1a07b3025c
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2022-08-04 15:59:20 +0200

    Add support for scout.create() with class reference
    This provides more type safety when writing Scout JS apps
    which helps the IDE for code completion and makes development and
    refactorings more robust.
    
    It also improves tree shaking because the required classes now need
    to be imported.
    
    325205

--- eclipse-scout-core/src/TypeDescriptor.js
+++ eclipse-scout-core/src/TypeDescriptor.js
@@ -25,48 +25,52 @@ export default class TypeDescriptor {
     this.typeDescriptor = typeDescriptor;
     this.objectType = objectType;
     this.modelVariant = modelVariant;
-  }
-
-  createInstance(options) {
-    let i, namespaces, className,
-      namespace = window.scout; // default namespace
 
     if (this.modelVariant) {
-      className = this.modelVariant.name + this.objectType.name;
-      namespaces = this.modelVariant.namespaces;
+      this.className = this.modelVariant.name + this.objectType.name;
+      this.namespaces = this.modelVariant.namespaces;
     } else {
-      className = this.objectType.name;
-      namespaces = this.objectType.namespaces;
+      this.className = this.objectType.name;
+      this.namespaces = this.objectType.namespaces;
     }
+  }
 
-    if (namespaces.length) {
+  resolve(options) {
+    let namespace = window.scout; // default namespace
+    options = options || {};
+
+    if (this.namespaces.length) {
       namespace = window;
-      for (i = 0; i < namespaces.length; i++) {
-        namespace = namespace[namespaces[i]];
+      for (let i = 0; i < this.namespaces.length; i++) {
+        namespace = namespace[this.namespaces[i]];
         if (!namespace) {
-          throw this.error('Could not resolve namespace "' + namespaces[i] + '"');
+          throw this.error('Could not resolve namespace "' + this.namespaces[i] + '"');
         }
       }
     }
 
-    if (!namespace[className]) { // Try without variant if variantLenient is true
+    if (!namespace[this.className]) { // Try without variant if variantLenient is true
       if (options.variantLenient && this.modelVariant) {
         let infoWithoutVariant = new TypeDescriptor(this.typeDescriptor, this.objectType, null);
-        return infoWithoutVariant.createInstance(options);
+        return infoWithoutVariant.resolve(options);
       }
-      throw this.error('Could not find "' + className + '" in namespace "' + namespaces.join('.') + '"');
+      return null;
     }
 
-    return new namespace[className](options.model);
+    return namespace[this.className];
   }
 
   error(details) {
     return new Error('Failed to create object for objectType "' + this.typeDescriptor + '": ' + details);
   }
 
-  static newInstance(typeDescriptor, options) {
+  notFoundError() {
+    return this.error('Could not find "' + this.className + '" in namespace "' + this.namespaces.join('.') + '"');
+  }
+
+  static resolveType(typeDescriptor, options) {
     let info = TypeDescriptor.parse(typeDescriptor);
-    return info.createInstance(options);
+    return info.resolve(options);
   }
 
   /**
