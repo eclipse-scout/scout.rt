2639ad49c7510c1510983d52b62f6b35cfa2a735
Fritz Schinkel <fritz.schinkel@bsi-software.com>
2022-08-04 17:11:40 +0200

    Form: move special cases for rootGroupBox to GroupBox/WrappedFormField

--- eclipse-scout-core/src/form/fields/groupbox/GroupBox.js
+++ eclipse-scout-core/src/form/fields/groupbox/GroupBox.js
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2010-2021 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -578,6 +578,12 @@ export default class GroupBox extends CompositeField {
     let position = this.menuBarPosition;
     if (position === GroupBox.MenuBarPosition.AUTO) {
       position = GroupBox.MenuBarPosition.TOP;
+      if (this.mainBox) {
+        let form = this.getForm();
+        if (form && form.isDialog()) {
+          position = GroupBox.MenuBarPosition.BOTTOM;
+        }
+      }
     }
 
     if (position === GroupBox.MenuBarPosition.BOTTOM) {
