4df36297bf7622a7b425d1f5dbe7eb3e7440e218
Matthias Villiger <mvi@bsi-software.com>
2022-08-31 11:42:57 +0200

    Utility migration to typescript (rename)

---
+++ eclipse-scout-core/src/text/DateFormatPatternDefinition.ts
@@ -0,0 +1,162 @@
+/*
+ * Copyright (c) 2010-2019 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
+
+/**
+ * Definition of a date format pattern.
+ *
+ * A definition provides the following properties (most of which can be set with the 'options' argument):
+ *
+ * type:
+ *   The "group" where the pattern definition belongs to. E.g. "dd" and "d" belong to the same group "day".
+ *   This is used during analysis to find other definitions for the same type. Use one of the constants
+ *   defined in DateFormatPatternType.
+ *
+ * terms:
+ *   An array consisting of all pattern terms that this particular definition can handle. Multiple
+ *   terms with the same meaning may be accepted (e.g. "yyy", "yy" and "y" can be all used for a
+ *   2-digit year formatting, but different parsing rules may apply).
+ *
+ * dateFormat:
+ *   Reference to the corresponding dateFormat object.
+ *
+ * formatFunction:
+ *   An optional function that is used to format this particular term.
+ *   @param formatContext
+ *            See documentation at _createFormatContext().
+ *   @param acceptedTerm
+ *            The term that was accepted for this definition. This argument is usually only relevant,
+ *            if a definition can accept more than one term.
+ *   @return
+ *            The function may return a string as result. If a string is returned, it is appended
+ *            to the formatContext.formattedString automatically. If the formatFunction already did
+ *            this, it should return "undefined".
+ *
+ * parseRegExp:
+ *   A optional JavaScript RegExp object that is applied to the input string to extract this
+ *   definition's term. The expression _must_ use exactly two capturing groups:
+ *   [1] = matched part of the input
+ *   [2] = remaining input (will be parsed later by other definitions)
+ *   Example: /^(\d{4})(.*)$/
+ *
+ * applyMatchFunction:
+ *   If 'parseRegExp' is set and found a match, and this function is defined, it is called
+ *   to apply the matched part to the parseContext.
+ *   @param parseContext
+ *            See documentation at _createParseContext().
+ *   @param match
+ *            The first match from the reg exp.
+ *   @param acceptedTerm
+ *            The term that was accepted for this definition. This argument is usually only relevant,
+ *            if a definition can accept more than one term.
+ *   @return
+ *            No return value.
+ *
+ * parseFunction:
+ *   If parsing is not possible with a regular expression, this function may be defined to execute
+ *   more complex parse logic.
+ *   @param parseContext
+ *            See documentation at _createParseContext().
+ *   @param acceptedTerm
+ *            The term that was accepted for this definition. This argument is usually only relevant,
+ *            if a definition can accept more than one term.
+ *   @return
+ *            A string with the matched part of the input, or null if it did not match.
+ */
+export default class DateFormatPatternDefinition {
+  type: any;
+  terms: any;
+  dateFormat: any;
+  formatFunction: any;
+  parseRegExp: any;
+  applyMatchFunction: any;
+  parseFunction: any;
+  constructor(options) { // NOSONAR
+    options = options || {};
+    this.type = options.type;
+    this.terms = options.terms;
+    this.dateFormat = options.dateFormat;
+    this.formatFunction = options.formatFunction && options.formatFunction.bind(this);
+    this.parseRegExp = options.parseRegExp;
+    this.applyMatchFunction = options.applyMatchFunction && options.applyMatchFunction.bind(this);
+    this.parseFunction = options.parseFunction && options.parseFunction.bind(this);
+  }
+
+  createFormatFunction(acceptedTerm) {
+    return function(formatContext) {
+      if (this.formatFunction) {
+        let result = this.formatFunction(formatContext, acceptedTerm);
+        if (result !== undefined) { // convenience
+          formatContext.formattedString += result;
+        }
+      }
+    }.bind(this);
+  }
+
+  createParseFunction(acceptedTerm) {
+    return function(parseContext) {
+      let m, parsedTerm, match;
+
+      let success = false;
+      if (this.parseRegExp) {
+        // RegEx handling (default)
+        m = this.parseRegExp.exec(parseContext.inputString);
+        if (m) { // match found
+          if (this.applyMatchFunction) {
+            this.applyMatchFunction(parseContext, m[1], acceptedTerm);
+          }
+          match = m[1];
+          // update remaining string
+          parseContext.inputString = m[2];
+          success = true;
+        }
+      }
+      if (!success && this.parseFunction) {
+        // Custom function
+        match = this.parseFunction(parseContext, acceptedTerm);
+        if (match !== null) {
+          success = true;
+        }
+      }
+
+      if (success) {
+        // If patternDefinition accepts more than one term, try to choose
+        // the form that matches the length of the match.
+        parsedTerm = this.terms[0];
+        if (this.terms.length > 1) {
+          this.terms.some(term => {
+            if (term.length === match.length) {
+              parsedTerm = term;
+              return true; // found
+            }
+            return false; // look further
+          });
+        }
+        parseContext.parsedPattern += parsedTerm;
+      }
+      return success;
+    }.bind(this);
+  }
+
+  /**
+   * @return {string|null} the accepted term (if is accepted) or null (if it is not accepted)
+   */
+  accept(term): string | null {
+    if (term) {
+      // Check if one of the terms matches
+      for (let i = 0; i < this.terms.length; i++) {
+        if (term === this.terms[i]) {
+          return this.terms[i];
+        }
+      }
+    }
+    return null;
+  }
+}
---
+++ eclipse-scout-core/src/util/Call.ts
@@ -0,0 +1,262 @@
+/*
+ * Copyright (c) 2014-2018 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
+import {arrays, scout, strings} from '../index';
+import $ from 'jquery';
+
+/**
+ * Represents a robust "call" that, when it fails, is retried automatically for a specific
+ * number of times, before failing ultimately. The call implementation must be provided
+ * by a subclass by overriding the _callImpl() method.
+ */
+export default class Call {
+  retryIntervals: any[];
+  minCallDuration: number;
+  callCounter: number;
+  deferred: any;
+  aborted: boolean;
+  pendingCall: any;
+  type: any;
+  name: any;
+  uniqueName: any;
+  logPrefix: string;
+  result: any;
+
+  constructor() {
+    // Delays in ms between retries (from left to right). The call eventually fails when this list gets empty.
+    // Example: [100, 500, 500, 500]
+    this.retryIntervals = [];
+
+    // Minimal assumed call duration (throttles consecutive calls)
+    this.minCallDuration = 500; // ms
+
+    // Counts how many times this call was actually performed (normally, only 1 try is expected)
+    this.callCounter = 0;
+
+    this.deferred = $.Deferred();
+    this.aborted = false;
+    this.pendingCall = null;
+    this.type = null; // Identifier for the type of call (default is 'call'), used to build the uniqueName
+    this.name = null; // Identifier for the call, used to build the uniqueName
+    this.uniqueName = null; // Unique identifier of this call instance for logging and debugging purposes
+    this.logPrefix = ''; // All log messages are prefixed with this string. It contains the uniqueName and the current state (e.g. callCounter)
+
+    // Temporary store for the arguments to the done/fail callbacks of the _callImpl() function.
+    // They will be used be _resolve() and _reject(). We could pass them through the _onCallX functions,
+    // but when a call is aborted while it is only scheduled (setTimeout), we would not have any values
+    // to pass to _reject().
+    this.result = null;
+  }
+
+  static GLOBAL_SEQ = 0;
+
+  init(model) {
+    $.extend(this, model);
+
+    // Ensure "retryIntervals" is a valid array
+    if (typeof this.maxRetries === 'number') {
+      this.retryIntervals = arrays.init(this.maxRetries, 0);
+    } else {
+      // Do not modify the passed value -> create a copy
+      this.retryIntervals = (this.retryIntervals ? this.retryIntervals.slice() : []);
+      // Remember initial number of retries (for logging)
+      this.maxRetries = this.retryIntervals.length;
+    }
+
+    // Assign a unique name to the call to help distinguish different calls in the log
+    this.uniqueName = scout.nvl(this.type, 'call') + '-' + (Call.GLOBAL_SEQ++) + strings.box(' ', this.name, '');
+
+    this.initialized = true;
+  }
+
+  protected _checkInitialized() {
+    if (!this.initialized) {
+      throw new Error('Not initialized');
+    }
+  }
+
+  protected _updateLogPrefix() {
+    this.logPrefix = this.callCounter + '/' + (this.maxRetries + 1) + ' [' + this.uniqueName + '] ';
+  }
+
+  protected _resolve() {
+    $.log.isTraceEnabled() && $.log.trace(this.logPrefix + '[RESOLVE]');
+    this.deferred.resolve(...arrays.ensure(this.result));
+  }
+
+  protected _reject() {
+    $.log.isTraceEnabled() && $.log.trace(this.logPrefix + '[REJECT]');
+    this.deferred.reject(...arrays.ensure(this.result));
+  }
+
+  protected _setResult(...args) {
+    this.result = args;
+  }
+
+  // ==================================================================================
+
+  /**
+   * Performs the call with retries.
+   *
+   * Returns a promise that is ...
+   * ... RESOLVED when the call was successful (possibly after some retries).
+   * ... REJECTED when the call failed and no more retries are possible.
+   *
+   *
+   *     | (promise)
+   *     |   ^
+   *     v   |
+   *   +--------+           +---------+           .---------------.  (yes)
+   *   | call() | . . . . . | _call() | ------>  <    success?     > ------> [RESOLVE]
+   *   +--------+           +---------+           '---------------'
+   *                             ^                       |(no)
+   *                             |                       |
+   *                             |                       v
+   *                             |                .---------------.  (yes)
+   *                             |               <    aborted?     > ------> [REJECT]
+   *                             |                '---------------'
+   *                             |                       |(no)
+   *                             |                       |
+   *                             |                       v
+   *                             |                .---------------.  (no)
+   *                             |               < retry possible? > ------> [REJECT]
+   *                             |                '---------------'
+   *                             |                       |(yes)
+   *                             |        sleep          |
+   *                             +-------- %%% ----------+
+   */
+  call() {
+    this._checkInitialized();
+    this._call();
+    return this.deferred.promise();
+  }
+
+  /**
+   * Aborts the call. If the request is currently running, it is aborted (interrupted).
+   * If a retry is scheduled, that retry is cancelled.
+   *
+   * The promise returned by call() is REJECTED.
+   */
+  abort() {
+    this._checkInitialized();
+    this._abort();
+  }
+
+  // ==================================================================================
+
+  protected _call() {
+    if (this.aborted) {
+      throw new Error('Call is aborted');
+    }
+
+    this.callTimeoutId = null;
+    this.callStartTimestamp = Date.now();
+    this.callCounter++;
+    this._updateLogPrefix();
+
+    this.pendingCall = this._callImpl()
+      .always(() => {
+        this.pendingCall = null;
+      })
+      .done(this._setResultDone.bind(this))
+      .done(this._onCallDone.bind(this))
+      .fail(this._setResultFail.bind(this))
+      .fail(this._onCallFail.bind(this));
+  }
+
+  /**
+   * Performs the actual request.
+   *
+   * >>> This method MUST be implemented by a subclass. <<<
+   */
+  protected _callImpl() {
+    throw new Error('Missing implementation: _callImpl()');
+  }
+
+  protected _setResultDone(...args) {
+    this._setResult(...args);
+  }
+
+  protected _setResultFail(...args) {
+    this._setResult(...args);
+  }
+
+  protected _onCallDone() {
+    // Call successful -> RESOLVE
+    this._resolve();
+  }
+
+  protected _onCallFail(...args) {
+    // Aborted? -> REJECT
+    if (this.aborted) {
+      $.log.isTraceEnabled() && $.log.trace(this.logPrefix + 'Call aborted');
+      this._reject();
+      return;
+    }
+
+    // Retry impossible? -> REJECT
+    let nextInterval = this._nextRetryImpl(...args);
+    if (typeof nextInterval !== 'number') {
+      $.log.isTraceEnabled() && $.log.trace(this.logPrefix + 'No retries remaining');
+      this._reject();
+      return;
+    }
+
+    // Retry
+    let callDuration = Date.now() - this.callStartTimestamp;
+    let additionalDelay = Math.max(this.minCallDuration - callDuration, 0);
+    let retryInterval = nextInterval + additionalDelay;
+    $.log.isTraceEnabled() && $.log.trace(this.logPrefix + 'Try again in ' + retryInterval + ' ms...');
+    this.callTimeoutId = setTimeout(this._call.bind(this), retryInterval);
+  }
+
+  /**
+   * Checks if the call can be retried. If a number is returned, a retry is performed
+   * with a delay of the corresponding amount of milliseconds.
+   *
+   * All other values indicate that no retry must be performed. (It is recommended
+   * to return 'false' or 'null' in this case.)
+   *
+   * >>> This method MAY be overridden by a subclass. <<<
+   */
+  protected _nextRetryImpl() {
+    if (this.retryIntervals.length) {
+      return this.retryIntervals.shift();
+    }
+    return false;
+  }
+
+  // ==================================================================================
+
+  protected _abort() {
+    this.aborted = true;
+
+    // Abort while waiting for the next retry (there is no running call)
+    if (this.callTimeoutId) {
+      $.log.isTraceEnabled() && $.log.trace(this.logPrefix + 'Cancelled scheduled retry');
+      clearTimeout(this.callTimeoutId);
+      this.callTimeoutId = null;
+      this._reject();
+      return;
+    }
+
+    // Abort a running call
+    this._abortImpl();
+  }
+
+  /**
+   * >>> This method MAY be overridden by a subclass. <<<
+   */
+  protected _abortImpl() {
+    if (this.pendingCall && typeof this.pendingCall.abort === 'function') {
+      this.pendingCall.abort();
+    }
+  }
+}
