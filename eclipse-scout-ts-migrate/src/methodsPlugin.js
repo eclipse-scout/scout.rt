cbf9026bb9a90e2d8a041ae1e11218f3a0362521
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2022-09-05 15:25:10 +0200

    ts-migrate: add task which removes types from js docs

--- eclipse-scout-ts-migrate/src/methodsPlugin.js
+++ eclipse-scout-ts-migrate/src/methodsPlugin.js
@@ -27,6 +27,7 @@ const methodsPlugin = {
           }
         }
         processReturnType(node, Object.values(returnTypeMap));
+        removeJsDocTypes(node);
       });
 
     insertMissingImportsForTypes(j, root, Array.from(referencedTypes), moduleMap);
@@ -60,6 +61,56 @@ function processParamType(param, typeMaps) {
       referencedTypes.add(typeDesc);
     }
   }
+}
+
+/**
+ * Removes all types form jsdoc if the types exist as TS types
+ */
+function removeJsDocTypes(func) {
+  let comments = func.leadingComments;
+  if (!comments) {
+    return;
+  }
+  let replaceReturnType = !!func.returnType;
+  let replaceParams = [];
+  if (func.params) {
+    replaceParams = func.params.filter(param => !!param.typeAnnotation);
+  }
+  if (!replaceReturnType && replaceParams.length === 0) {
+    return;
+  }
+  func.comments = comments.map(comment => {
+    if (comment.type !== 'CommentBlock') {
+      return comment;
+    }
+    let str = comment.value;
+    str = str.replaceAll(/ +\*/g, ''); // remove * at start of lines, makes the upcoming processing easier
+    let beforeReplace = str;
+    if (replaceReturnType) {
+      str = str.replace(/(@return[s]?) ({.*})/, '@returns'); // remove type from @return
+      str = str.replace(/@returns[\s]*$/, ''); // remove empty return at the end
+    }
+    for (let param of replaceParams) {
+      str = str.replace(new RegExp(`@param {.*} \\[?${param.name}\\]?`), `@param ${param.name}`); // remove type from @param
+      str = str.replace(new RegExp(`@param \\[?${param.name}\\]? {.*}`), `@param ${param.name}`); // consider reverse typing as well (@param [var] {type} instead of @param {type} [var]
+      str = str.replace(new RegExp(`@param \\[${param.name}\\]`), `@param ${param.name}`); // remove brackets even without type
+      str = str.replace(new RegExp(`@param ${param.name}[\\s]*$`), ''); // remove empty param at end
+      str = str.replace(new RegExp(`@param ${param.name}[\\s]*@param`), '@param'); // remove empty param before param
+      str = str.replace(new RegExp(`@param ${param.name}[\\s]*@return`), '@return'); // remove empty param before return
+    }
+    if (beforeReplace === str) {
+      // Do nothing if nothing was replaced
+      return comment;
+    }
+    str = str.replace(/\r\n +$/, '\r\n'); // remove whitespaces at the end (keep new line)
+    str = str.replaceAll(/\r\n/g, '\r\n *'); // add * again
+    str = str.replace(/\r\n \*$/, '\r\n'); // Remove last * that was added by the line before. It will be added automatically by the block comment.
+    if (str.trim() === '*') {
+      // Remove empty comments
+      return null;
+    }
+    return j.commentBlock(str);
+  }).filter(comment => comment !== null);
 }
 
 export default methodsPlugin;
