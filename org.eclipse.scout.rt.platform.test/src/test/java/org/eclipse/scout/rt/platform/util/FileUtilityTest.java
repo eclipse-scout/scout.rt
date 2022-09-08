b74a5baca9be3dedf57e3561185ef882fc5afe8d
Paolo Bazzi <paolo.bazzi@bsi-software.com>
2022-09-08 14:07:09 +0200

    FileUtility: Add ability to create zip archives

--- org.eclipse.scout.rt.platform.test/src/test/java/org/eclipse/scout/rt/platform/util/FileUtilityTest.java
+++ org.eclipse.scout.rt.platform.test/src/test/java/org/eclipse/scout/rt/platform/util/FileUtilityTest.java
@@ -10,10 +10,8 @@
  */
 package org.eclipse.scout.rt.platform.util;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,7 +19,13 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 
+import org.eclipse.scout.rt.platform.exception.ProcessingException;
+import org.eclipse.scout.rt.platform.resource.BinaryResource;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -101,7 +105,7 @@ public class FileUtilityTest {
   public void testGetFileExtension_File() {
     assertEquals(TEXT_EXT, FileUtility.getFileExtension(new File(FILE_NAME)));
     assertEquals(TEXT_EXT, FileUtility.getFileExtension(new File(FILE_NAME_MULTIPLE_DOTS)));
-    assertNull(FileUtility.getFileExtension((File) new File(FILE_NAME_NO_EXT)));
+    assertNull(FileUtility.getFileExtension(new File(FILE_NAME_NO_EXT)));
     assertNull(FileUtility.getFileExtension((File) null));
   }
 
@@ -179,4 +183,141 @@ public class FileUtilityTest {
                 "someReallyLongName01234567890123456789.01234567890123456789.01234567890123456789.01234567890123456789.01234567890123456789.01234567890123456789.01234567890123456789.01234567890123456789.01234567890123456789.01234567890123456789.01234567890123456789.01234567890123.doc"));
   }
 
+  @Test
+  public void testZip() {
+    BinaryResource expectedTextResource = new BinaryResource("text.txt", "some text".getBytes(UTF_8));
+    // create empty remote file manually (utility will return null)
+    BinaryResource expectedEmptyResource = new BinaryResource("empty_file.txt", new byte[0]);
+    BinaryResource expectedDataResource = new BinaryResource("data.dat", new byte[]{12, 4, 23, 41, 2, 5, 72, 12, 3});
+
+    BinaryResource zipResource = FileUtility.zip("archive.zip", CollectionUtility.arrayList(expectedTextResource, expectedEmptyResource, expectedDataResource));
+    Assert.assertNotNull(zipResource);
+    Assert.assertEquals("archive.zip", zipResource.getFilename());
+    Assert.assertTrue(zipResource.getContent().length > 0);
+
+    Collection<BinaryResource> resources = FileUtility.unzip(zipResource, null);
+    Assert.assertNotNull(resources);
+    Assert.assertEquals(3, resources.size());
+
+    // assuming same file order as when zipping
+    Iterator<BinaryResource> it = resources.iterator();
+
+    BinaryResource actualTextResource = it.next();
+    Assert.assertNotNull(actualTextResource);
+    Assert.assertEquals(expectedTextResource.getFilename(), actualTextResource.getFilename());
+    Assert.assertArrayEquals(expectedTextResource.getContent(), actualTextResource.getContent());
+
+    BinaryResource actualEmptyResource = it.next();
+    Assert.assertNotNull(actualEmptyResource);
+    Assert.assertEquals(expectedEmptyResource.getFilename(), actualEmptyResource.getFilename());
+    Assert.assertArrayEquals(expectedEmptyResource.getContent(), actualEmptyResource.getContent());
+
+    BinaryResource actualDataResource = it.next();
+    Assert.assertNotNull(actualDataResource);
+    Assert.assertEquals(expectedDataResource.getFilename(), actualDataResource.getFilename());
+    Assert.assertArrayEquals(expectedDataResource.getContent(), actualDataResource.getContent());
+  }
+
+  @Test
+  public void testZipAvoidConflicts() {
+    BinaryResource expectedEmptyResource1 = new BinaryResource("empty_file.txt", new byte[0]);
+    BinaryResource expectedEmptyResource2 = new BinaryResource("empty_file.txt", new byte[0]);
+    BinaryResource expectedEmptyResource3 = new BinaryResource("empty_file.txt", new byte[0]);
+
+    BinaryResource zipResource = FileUtility.zip("archive.zip", CollectionUtility.arrayList(expectedEmptyResource1, expectedEmptyResource2, expectedEmptyResource3), true);
+    Assert.assertNotNull(zipResource);
+    Assert.assertEquals("archive.zip", zipResource.getFilename());
+    Assert.assertTrue(zipResource.getContent().length > 0);
+
+    Collection<BinaryResource> resources = FileUtility.unzip(zipResource, null);
+    Assert.assertNotNull(resources);
+    Assert.assertEquals(3, resources.size());
+
+    Iterator<BinaryResource> it = resources.iterator();
+
+    BinaryResource actualEmptyResource1 = it.next();
+    Assert.assertNotNull(expectedEmptyResource1);
+    Assert.assertEquals(FileUtility.getFileName(expectedEmptyResource1.getFilename(), false) + "(1)." + FileUtility.getFileExtension(expectedEmptyResource1.getFilename()), actualEmptyResource1.getFilename());
+    Assert.assertArrayEquals(expectedEmptyResource1.getContent(), actualEmptyResource1.getContent());
+
+    BinaryResource actualEmptyResource2 = it.next();
+    Assert.assertNotNull(actualEmptyResource2);
+    Assert.assertEquals(FileUtility.getFileName(expectedEmptyResource2.getFilename(), false) + "(2)." + FileUtility.getFileExtension(expectedEmptyResource2.getFilename()), actualEmptyResource2.getFilename());
+    Assert.assertArrayEquals(expectedEmptyResource2.getContent(), actualEmptyResource2.getContent());
+
+    BinaryResource actualEmptyResource3 = it.next();
+    Assert.assertNotNull(actualEmptyResource3);
+    Assert.assertEquals(FileUtility.getFileName(expectedEmptyResource3.getFilename(), false) + "(3)." + FileUtility.getFileExtension(expectedEmptyResource3.getFilename()), actualEmptyResource3.getFilename());
+    Assert.assertArrayEquals(expectedEmptyResource3.getContent(), actualEmptyResource3.getContent());
+  }
+
+  @Test
+  public void testZipAvoidConflictsRepeatedly() {
+    BinaryResource expectedEmptyResource1 = new BinaryResource("empty_file.txt", new byte[0]);
+    BinaryResource expectedEmptyResource2 = new BinaryResource("empty_file.txt", new byte[0]);
+    BinaryResource expectedEmptyResource3 = new BinaryResource("empty_file(1).txt", new byte[0]);
+    BinaryResource expectedEmptyResource4 = new BinaryResource("empty_file(2).txt", new byte[0]);
+    BinaryResource expectedEmptyResource5 = new BinaryResource("empty_file(1)(3).txt", new byte[0]);
+    BinaryResource expectedEmptyResource6 = new BinaryResource("empty_file(2)(2).txt", new byte[0]);
+
+    BinaryResource zipResource = FileUtility.zip("archive.zip", CollectionUtility.arrayList(
+        expectedEmptyResource1,
+        expectedEmptyResource2,
+        expectedEmptyResource3,
+        expectedEmptyResource4,
+        expectedEmptyResource5,
+        expectedEmptyResource6), true);
+    Assert.assertNotNull(zipResource);
+    Assert.assertEquals("archive.zip", zipResource.getFilename());
+    Assert.assertTrue(zipResource.getContent().length > 0);
+
+    Collection<BinaryResource> resources = FileUtility.unzip(zipResource, null);
+    Assert.assertNotNull(resources);
+    Assert.assertEquals(6, resources.size());
+  }
+
+  @Test
+  public void testZipConflict() {
+    BinaryResource expectedEmptyResource1 = new BinaryResource("empty_file.txt", new byte[0]);
+    BinaryResource expectedEmptyResource2 = new BinaryResource("empty_file.txt", new byte[0]);
+    BinaryResource expectedEmptyResource3 = new BinaryResource("empty_file.txt", new byte[0]);
+    try {
+      FileUtility.zip("archive.zip", CollectionUtility.arrayList(expectedEmptyResource1, expectedEmptyResource2, expectedEmptyResource3));
+    }
+    catch (ProcessingException e) {
+      Assert.assertTrue(e.getCause() instanceof IOException);
+    }
+  }
+
+  @Test
+  public void testUnzipArchive() throws IOException {
+    File zipFile = null;
+    File[] files = null;
+    try {
+      zipFile = createTempFile("zip.zip");
+      files = FileUtility.unzipArchive(zipFile);
+      assertEquals(1, files.length);
+      assertEquals("zip", files[0].getName());
+    }
+    finally {
+      IOUtility.deleteFile(zipFile);
+      if (files != null) {
+        Arrays.stream(files).forEach(IOUtility::deleteFile);
+      }
+    }
+  }
+
+  @Test
+  public void testGetFilename() {
+    assertEquals("foo", FileUtility.getFileName("foo.bar", false));
+    assertEquals("foo.bar", FileUtility.getFileName("foo.bar", true));
+    assertEquals("foo.bar", FileUtility.getFileName("foo.bar.baz", false));
+    assertEquals("foo.bar.baz", FileUtility.getFileName("foo.bar.baz", true));
+
+    assertEquals("foo.bar", FileUtility.getFileName("/folder/foo.bar", true));
+    assertEquals("foo.bar", FileUtility.getFileName("/folder/subfolder/foo.bar", true));
+    assertEquals("foo.bar", FileUtility.getFileName("\\folder\\foo.bar", true));
+    assertEquals("foo.bar", FileUtility.getFileName("\\folder\\subfolder\\foo.bar", true));
+  }
 }
+
