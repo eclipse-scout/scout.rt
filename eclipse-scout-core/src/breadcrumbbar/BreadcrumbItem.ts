f83f2f61e3ca9e84c690337779454d541888ee9c
Matthias Villiger <mvi@bsi-software.com>
2022-09-20 10:27:19 +0200

    Breadcrumbbar migration to typescript (add types)

--- eclipse-scout-core/src/breadcrumbbar/BreadcrumbItem.ts
+++ eclipse-scout-core/src/breadcrumbbar/BreadcrumbItem.ts
@@ -1,29 +1,33 @@
 /*
- * Copyright (c) 2010-2020 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  */
 
-import {Action, HtmlComponent} from '../index';
+import {Action, BreadcrumbItemModel, HtmlComponent} from '../index';
 
-export default class BreadcrumbItem extends Action {
+export default class BreadcrumbItem extends Action implements BreadcrumbItemModel {
+  declare model: BreadcrumbItemModel;
+
+  /** Arbitrary reference value, can be used to find and select modes (see BreadcrumbBar.js) */
+  ref: any;
 
   constructor() {
     super();
-    this.ref = null; // Arbitrary reference value, can be used to find and select modes (see BreadcrumbBar.js)
+    this.ref = null;
   }
 
-  _render() {
+  protected override _render() {
     super._render();
     this.$container.addClass('breadcrumb-item');
   }
 
-  _renderText() {
+  protected override _renderText() {
     const text = this.text || '';
     if (text && this.textVisible) {
       if (!this.$text) {
