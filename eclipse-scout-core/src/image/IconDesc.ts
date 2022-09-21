73d8dcc70e0642cfcd3d0cc17fbeb8fa2719e2ad
Matthias Villiger <mvi@bsi-software.com>
2022-09-21 09:32:51 +0200

    Image migration to typescript (add types)

--- eclipse-scout-core/src/image/IconDesc.ts
+++ eclipse-scout-core/src/image/IconDesc.ts
@@ -1,16 +1,22 @@
 /*
- * Copyright (c) 2010-2021 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  */
-import {icons} from '../index';
+import {EnumObject, icons} from '../index';
+
+export type IconType = EnumObject<typeof IconDesc.IconType>;
 
 export default class IconDesc {
+  iconType: IconType;
+  font: string;
+  iconCharacter: string;
+  iconUrl: string;
 
   constructor() {
     this.iconType = null;
@@ -22,14 +28,14 @@ export default class IconDesc {
   static IconType = {
     FONT_ICON: 0,
     BITMAP: 1
-  };
+  } as const;
 
   static DEFAULT_FONT = 'scoutIcons';
 
   /**
    * Returns a CSS class based on the used font-name.
    */
-  cssClass() {
+  cssClass(): string {
     if (this.isFontIcon() && this.font !== IconDesc.DEFAULT_FONT) {
       return 'font-' + this.font;
     }
@@ -39,7 +45,7 @@ export default class IconDesc {
   /**
    * Returns a CSS class string to be used with JQuery.add/removeClass().
    */
-  appendCssClass(cssClass) {
+  appendCssClass(cssClass: string): string {
     let additionalCssClass = this.cssClass();
     if (additionalCssClass.length > 0) {
       return cssClass + ' ' + additionalCssClass;
@@ -47,21 +53,20 @@ export default class IconDesc {
     return cssClass;
   }
 
-  isFontIcon() {
+  isFontIcon(): boolean {
     return this.iconType === IconDesc.IconType.FONT_ICON;
   }
 
-  isBitmap() {
+  isBitmap(): boolean {
     return this.iconType === IconDesc.IconType.BITMAP;
   }
 
   /**
-   * Ensures that the given icon is of type {@link scout.iconDesc}. It a string is provided it is assumed that the string is the iconId which may be parsed to create the {@link IconDesc}.
-   * @param {(string|IconDesc)} icon
+   * Ensures that the given icon is of type {@link IconDesc}. If a string is provided, it is assumed that the string is the iconId which may be parsed to create the {@link IconDesc}.
    */
-  static ensure(icon) {
+  static ensure(icon: string | IconDesc): IconDesc {
     if (!icon) {
-      return icon;
+      return icon as IconDesc;
     }
     if (icon instanceof IconDesc) {
       return icon;
