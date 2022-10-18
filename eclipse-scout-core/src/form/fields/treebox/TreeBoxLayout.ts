06c64391bd9c92990ca760966ba5c1c4a013cda6
Fritz Schinkel <fritz.schinkel@bsi-software.com>
2022-10-18 12:54:32 +0200

    TreeBox migration to typescript (rename)

--- eclipse-scout-core/src/form/fields/treebox/TreeBoxLayout.ts
+++ eclipse-scout-core/src/form/fields/treebox/TreeBoxLayout.ts
@@ -1,19 +1,19 @@
 /*
- * Copyright (c) 2014-2017 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  */
-import {ValueFieldAdapter} from '../../../index';
+import {LookupBoxLayout} from '../../../index';
 
-export default class TreeBoxAdapter extends ValueFieldAdapter {
+export default class TreeBoxLayout extends LookupBoxLayout {
 
-  constructor() {
-    super();
+  constructor(treeBox, tree, filterBox) {
+    super(treeBox, tree, filterBox);
   }
 
 }
