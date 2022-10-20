c8ab4431ff601b43e380870cd95473cae835db88
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2022-10-20 10:08:25 +0200

    TS: migrate accordion field and bean field

---
+++ eclipse-scout-core/src/form/fields/beanfield/BeanFieldEventMap.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
+import {BeanField, Event, ValueFieldEventMap} from '../../../index';
+
+export interface AppLinkActionEvent<T = BeanField<object>> extends Event<T> {
+  ref: string;
+}
+
+export default interface BeanFieldEventMap<TValue extends object> extends ValueFieldEventMap<TValue> {
+  'appLinkAction': AppLinkActionEvent;
+}
