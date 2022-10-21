776e269acd5c5e7f64a8edf276cfab073d627cb4
Matthias Villiger <mvi@bsi-software.com>
2022-10-21 17:18:38 +0200

    FileChooserField: Consistently use long for property MaximumUploadSize
    331265

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/form/fields/filechooserfield/AbstractFileChooserField.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/form/fields/filechooserfield/AbstractFileChooserField.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2010-2018 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -102,7 +102,7 @@ public abstract class AbstractFileChooserField extends AbstractValueField<Binary
 
   @Override
   public long getMaximumUploadSize() {
-    return propertySupport.getPropertyInt(PROP_MAXIMUM_UPLOAD_SIZE);
+    return propertySupport.getPropertyLong(PROP_MAXIMUM_UPLOAD_SIZE);
   }
 
   // Convenience file getter
