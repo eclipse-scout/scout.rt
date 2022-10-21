041fdbe7b2a0a762b84868d88ee6f349e0ff37de
Matthias Villiger <mvi@bsi-software.com>
2022-10-21 11:17:37 +0200

    HtmlField migration to typescript (add types)

--- eclipse-scout-core/src/form/fields/htmlfield/HtmlFieldAdapter.ts
+++ eclipse-scout-core/src/form/fields/htmlfield/HtmlFieldAdapter.ts
@@ -1,44 +1,38 @@
 /*
- * Copyright (c) 2010-2020 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  */
-import {ValueFieldAdapter} from '../../../index';
+import {Event, HtmlField, HtmlFieldModel, ValueFieldAdapter} from '../../../index';
+import {HtmlFieldAppLinkActionEvent} from './HtmlFieldEventMap';
 
 export default class HtmlFieldAdapter extends ValueFieldAdapter {
 
-  constructor() {
-    super();
-  }
-
-  /**
-   * @param model.scrollToEnd
-   */
-  _initProperties(model) {
+  protected override _initProperties(model: HtmlFieldModel) {
     if (model.scrollToEnd !== undefined) {
       // ignore pseudo property initially (to prevent the function StringField#scrollToEnd() to be replaced)
       delete model.scrollToEnd;
     }
   }
 
-  _syncScrollToEnd() {
+  protected _syncScrollToEnd() {
     this.widget.scrollToBottom();
   }
 
-  _onWidgetAppLinkAction(event) {
+  protected _onWidgetAppLinkAction(event: HtmlFieldAppLinkActionEvent) {
     this._send('appLinkAction', {
       ref: event.ref
     });
   }
 
-  _onWidgetEvent(event) {
+  protected override _onWidgetEvent(event: Event<HtmlField>) {
     if (event.type === 'appLinkAction') {
-      this._onWidgetAppLinkAction(event);
+      this._onWidgetAppLinkAction(event as HtmlFieldAppLinkActionEvent);
     } else {
       super._onWidgetEvent(event);
     }
---
+++ eclipse-scout-core/src/form/fields/htmlfield/HtmlFieldEventMap.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
+import {Event, HtmlField, PropertyChangeEvent, ValueFieldEventMap} from '../../../index';
+
+export interface HtmlFieldAppLinkActionEvent<T = HtmlField> extends Event<T> {
+  ref: string;
+}
+
+export default interface HtmlFieldEventMap extends ValueFieldEventMap<string> {
+  'appLinkAction': HtmlFieldAppLinkActionEvent;
+  'propertyChange:scrollBarEnabled': PropertyChangeEvent<boolean>;
+  'propertyChange:selectable': PropertyChangeEvent<boolean>;
+}
