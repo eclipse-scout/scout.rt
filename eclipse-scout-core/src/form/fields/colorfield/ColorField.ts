29dec33df5d084fa331255cfa61f63926d94b570
Matthias Villiger <mvi@bsi-software.com>
2022-10-21 10:23:46 +0200

    ColorField migration to typescript (add types)

--- eclipse-scout-core/src/form/fields/colorfield/ColorField.ts
+++ eclipse-scout-core/src/form/fields/colorfield/ColorField.ts
@@ -1,22 +1,18 @@
 /*
- * Copyright (c) 2014-2017 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  */
 import {ValueField} from '../../../index';
 
-export default class ColorField extends ValueField {
+export default class ColorField extends ValueField<string> {
 
-  constructor() {
-    super();
-  }
-
-  _render() {
+  protected _render() {
     this.addContainer(this.$parent, 'color-field');
     this.addLabel();
     this.addField(this.$parent.makeDiv('not-implemented').text('not implemented yet'));
