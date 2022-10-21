5dc4a8b5acc981635c87d527c2bceff6c06dab88
Matthias Villiger <mvi@bsi-software.com>
2022-10-21 12:12:21 +0200

    Placeholder migration to typescript (add types)

--- eclipse-scout-core/src/form/fields/placeholder/PlaceholderField.ts
+++ eclipse-scout-core/src/form/fields/placeholder/PlaceholderField.ts
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2014-2017 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -12,19 +12,12 @@ import {FormField} from '../../../index';
 
 export default class PlaceholderField extends FormField {
 
-  constructor() {
-    super();
-  }
-
-  _render() {
+  protected _render() {
     this.addContainer(this.$parent, 'placeholder-field');
     this.addLabel();
   }
 
-  /**
-   * @override
-   */
-  _renderLabel() {
+  protected override _renderLabel() {
     // Field needs a label to ensure correct layout when labelPosition = TOP.
     // The label is always rendered empty, because place holder fields should not have any visible parts.
     this._renderEmptyLabel();
