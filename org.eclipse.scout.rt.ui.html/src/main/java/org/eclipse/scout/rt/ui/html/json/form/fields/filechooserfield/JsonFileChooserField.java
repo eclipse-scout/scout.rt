34d7cfce2ba2e7bba969b3e0ad42a62f4f61c1e2
Matthias Villiger <mvi@bsi-software.com>
2022-10-21 17:18:38 +0200

    FileChooserField: Consistently use long for property MaximumUploadSize
    331265

--- org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/form/fields/filechooserfield/JsonFileChooserField.java
+++ org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/form/fields/filechooserfield/JsonFileChooserField.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2010-2021 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -39,13 +39,13 @@ public class JsonFileChooserField<M extends IFileChooserField> extends JsonValue
   @Override
   protected void initJsonProperties(M model) {
     super.initJsonProperties(model);
-    putJsonProperty(new JsonProperty<M>(IFileChooserField.PROP_MAXIMUM_UPLOAD_SIZE, model) {
+    putJsonProperty(new JsonProperty<>(IFileChooserField.PROP_MAXIMUM_UPLOAD_SIZE, model) {
       @Override
       protected Long modelValue() {
         return getModel().getMaximumUploadSize();
       }
     });
-    putJsonProperty(new JsonProperty<M>(IFileChooserField.PROP_FILE_EXTENSIONS, model) {
+    putJsonProperty(new JsonProperty<>(IFileChooserField.PROP_FILE_EXTENSIONS, model) {
       @Override
       protected List<String> modelValue() {
         return getModel().getFileExtensions();
