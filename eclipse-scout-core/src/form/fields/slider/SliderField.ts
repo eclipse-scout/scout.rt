c3575ba768e047c42bceac69f4b717351087b89e
Matthias Villiger <mvi@bsi-software.com>
2022-10-21 12:57:05 +0200

    Slider migration to typescript (add types)

--- eclipse-scout-core/src/form/fields/slider/SliderField.ts
+++ eclipse-scout-core/src/form/fields/slider/SliderField.ts
@@ -1,60 +1,58 @@
 /*
- * Copyright (c) 2014-2017 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  */
-import {scout, Slider, ValueField} from '../../../index';
+import {scout, Slider, SliderFieldModel, SliderModel, ValueField} from '../../../index';
 import $ from 'jquery';
 
-export default class SliderField extends ValueField {
+export default class SliderField extends ValueField<number> {
+  declare model: SliderFieldModel;
 
-  constructor() {
-    super();
-    this.slider;
-  }
+  slider: Slider;
 
-  _init(model) {
+  protected override _init(model: SliderFieldModel) {
     super._init(model);
-    let sliderOptions = $.extend({
+    let sliderOptions: SliderModel = $.extend({
       parent: this
     }, model);
     this.slider = scout.create(Slider, sliderOptions);
   }
 
-  _render() {
+  protected _render() {
     this.addContainer(this.$parent, 'slider-field');
     this.addLabel();
     this.addMandatoryIndicator();
     this._renderSlider();
   }
 
-  _renderSlider() {
+  protected _renderSlider() {
     this.slider.render();
     this.addField(this.slider.$container);
   }
 
-  _readDisplayText() {
+  protected override _readDisplayText(): string {
     // Use the inner slider's value as display text, as the user cannot enter the value manually.
     // This value is already guaranteed to be a valid number (see Slider.js, _onValueChange). We
     // convert it to a string to match the expected data type for a display text.
     return String(this.slider.value);
   }
 
-  _parseValue(displayText) {
+  protected override _parseValue(displayText: string): number {
     // Convert display text back to number
     return Number(displayText);
   }
 
-  setValue(value) {
+  override setValue(value: number) {
     this.slider.setValue(value);
   }
 
-  getValue() {
+  getValue(): number {
     return this.slider.value;
   }
 }
