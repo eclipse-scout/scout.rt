cc2547a23252edcc7ab64597f5c2c4ae04fc5570
Paolo Bazzi <paolo.bazzi@bsi-software.com>
2022-10-27 15:04:31 +0200

    DO signature generator: Add support for abstract/interface IId types
    Added new type "IDI" representing abstract IId classes and custom
    IId interfaces.
    
    322156

---
+++ org.eclipse.scout.rt.dataobject.test/src/test/java/org/eclipse/scout/rt/dataobject/testing/signature/fixture/SignatureFixtureDo.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
+package org.eclipse.scout.rt.dataobject.testing.signature.fixture;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.annotation.Generated;
+
+import org.eclipse.scout.rt.dataobject.DoEntity;
+import org.eclipse.scout.rt.dataobject.DoList;
+import org.eclipse.scout.rt.dataobject.DoValue;
+import org.eclipse.scout.rt.dataobject.TypeName;
+import org.eclipse.scout.rt.dataobject.TypeVersion;
+import org.eclipse.scout.rt.dataobject.fixture.DataObjectFixtureTypeVersions.DataObjectFixture_1_0_0;
+import org.eclipse.scout.rt.dataobject.fixture.FixtureEnum;
+import org.eclipse.scout.rt.dataobject.fixture.FixtureStringId;
+import org.eclipse.scout.rt.dataobject.id.AbstractStringId;
+
+@TypeName("dataObjectFixture.SignatureFixture")
+@TypeVersion(DataObjectFixture_1_0_0.class)
+public class SignatureFixtureDo extends DoEntity {
+
+  public DoList<FixtureStringId> idAttribute() {
+    return doList("idAttribute");
+  }
+
+  public DoList<AbstractStringId> abstractIdAttribute() {
+    return doList("abstractIdAttribute");
+  }
+
+  public DoValue<FixtureEnum> enumAttribute() {
+    return doValue("enumAttribute");
+  }
+
+  public DoValue<SignatureSubFixtureDo> doAttribute() {
+    return doValue("doAttribute");
+  }
+
+  public DoValue<ISignatureFixtureAttributeDo> doInterfaceAttribute() {
+    return doValue("doInterfaceAttribute");
+  }
+
+  /* **************************************************************************
+   * GENERATED CONVENIENCE METHODS
+   * *************************************************************************/
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public SignatureFixtureDo withIdAttribute(Collection<? extends FixtureStringId> idAttribute) {
+    idAttribute().updateAll(idAttribute);
+    return this;
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public SignatureFixtureDo withIdAttribute(FixtureStringId... idAttribute) {
+    idAttribute().updateAll(idAttribute);
+    return this;
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public List<FixtureStringId> getIdAttribute() {
+    return idAttribute().get();
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public SignatureFixtureDo withAbstractIdAttribute(Collection<? extends AbstractStringId> abstractIdAttribute) {
+    abstractIdAttribute().updateAll(abstractIdAttribute);
+    return this;
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public SignatureFixtureDo withAbstractIdAttribute(AbstractStringId... abstractIdAttribute) {
+    abstractIdAttribute().updateAll(abstractIdAttribute);
+    return this;
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public List<AbstractStringId> getAbstractIdAttribute() {
+    return abstractIdAttribute().get();
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public SignatureFixtureDo withEnumAttribute(FixtureEnum enumAttribute) {
+    enumAttribute().set(enumAttribute);
+    return this;
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public FixtureEnum getEnumAttribute() {
+    return enumAttribute().get();
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public SignatureFixtureDo withDoAttribute(SignatureSubFixtureDo doAttribute) {
+    doAttribute().set(doAttribute);
+    return this;
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public SignatureSubFixtureDo getDoAttribute() {
+    return doAttribute().get();
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public SignatureFixtureDo withDoInterfaceAttribute(ISignatureFixtureAttributeDo doInterfaceAttribute) {
+    doInterfaceAttribute().set(doInterfaceAttribute);
+    return this;
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public ISignatureFixtureAttributeDo getDoInterfaceAttribute() {
+    return doInterfaceAttribute().get();
+  }
+}
