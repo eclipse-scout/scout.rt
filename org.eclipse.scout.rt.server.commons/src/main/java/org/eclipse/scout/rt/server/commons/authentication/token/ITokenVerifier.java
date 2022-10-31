cc0b51fed220531435e9a7f011bd3b0ad68150a9
Bruno Koeferli <Bruno.Koeferli@bsi-software.com>
2022-10-31 12:03:12 +0100

    Support multiple instances of BearerAuthAccessController
    Removing the ApplicationScoped annotation from ITokenVerifier and
    ITokenPrincipalProducer allows to have different instances of the same
    implementation. This is necessary in case of multiple
    BearerAuthAccessController instance with different configurations/states
    are required.
    
    333399

--- org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/authentication/token/ITokenVerifier.java
+++ org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/authentication/token/ITokenVerifier.java
@@ -3,7 +3,7 @@
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -13,13 +13,10 @@ package org.eclipse.scout.rt.server.commons.authentication.token;
 import java.io.IOException;
 import java.util.List;
 
-import org.eclipse.scout.rt.platform.ApplicationScoped;
-
 /**
  * Verifies a token against a data source like database, config.properties or others.
  */
 @FunctionalInterface
-@ApplicationScoped
 public interface ITokenVerifier {
 
   /**
