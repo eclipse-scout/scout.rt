c140e70391b014480c4fb70c395f03127f8ade1e
Bruno Koeferli <Bruno.Koeferli@bsi-software.com>
2022-10-31 12:03:12 +0100

    Support multiple instances of BearerAuthAccessController
    Removing the ApplicationScoped annotation from ITokenVerifier and
    ITokenPrincipalProducer allows to have different instances of the same
    implementation. This is necessary in case of multiple
    BearerAuthAccessController instance with different configurations/states
    are required.
    
    333399

--- org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/authentication/token/SingleStringTokenPrincipalProducer.java
+++ org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/authentication/token/SingleStringTokenPrincipalProducer.java
@@ -3,7 +3,7 @@
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -14,11 +14,13 @@ import java.security.Principal;
 import java.util.List;
 import java.util.function.Function;
 
+import org.eclipse.scout.rt.platform.Bean;
 import org.eclipse.scout.rt.platform.security.SimplePrincipal;
 import org.eclipse.scout.rt.platform.util.Assertions;
 import org.eclipse.scout.rt.platform.util.CollectionUtility;
 import org.eclipse.scout.rt.platform.util.TokenUtility;
 
+@Bean
 public class SingleStringTokenPrincipalProducer implements ITokenPrincipalProducer {
 
   protected Function<char[], String> m_tokenToPrincipalMapper;
