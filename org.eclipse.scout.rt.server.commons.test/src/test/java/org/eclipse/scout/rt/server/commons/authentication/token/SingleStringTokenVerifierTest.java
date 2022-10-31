86da375cfa4f1847de0f7650298082d67bcbb66e
Bruno Koeferli <Bruno.Koeferli@bsi-software.com>
2022-10-31 12:03:12 +0100

    Support multiple instances of BearerAuthAccessController
    Removing the ApplicationScoped annotation from ITokenVerifier and
    ITokenPrincipalProducer allows to have different instances of the same
    implementation. This is necessary in case of multiple
    BearerAuthAccessController instance with different configurations/states
    are required.
    
    333399

--- org.eclipse.scout.rt.server.commons.test/src/test/java/org/eclipse/scout/rt/server/commons/authentication/token/SingleStringTokenVerifierTest.java
+++ org.eclipse.scout.rt.server.commons.test/src/test/java/org/eclipse/scout/rt/server/commons/authentication/token/SingleStringTokenVerifierTest.java
@@ -3,7 +3,7 @@
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -31,5 +31,21 @@ public class SingleStringTokenVerifierTest {
     assertEquals(ITokenVerifier.AUTH_FORBIDDEN, verifier.verify(List.of("Wrong".getBytes())));
     assertEquals(ITokenVerifier.AUTH_FAILED, verifier.verify(null));
     assertEquals(ITokenVerifier.AUTH_FAILED, verifier.verify(List.of("Correct".getBytes(), "Wrong".getBytes())));
+  }
+
+  @Test
+  public void testMultipleVerifierInstances() {
+    SingleStringTokenVerifier verifier1 = BEANS.get(SingleStringTokenVerifier.class).init(token -> ObjectUtility.equals("Correct1".toCharArray(), token));
+    SingleStringTokenVerifier verifier2 = BEANS.get(SingleStringTokenVerifier.class).init(token -> ObjectUtility.equals("Correct2".toCharArray(), token));
+
+    assertEquals(ITokenVerifier.AUTH_OK, verifier1.verify(List.of("Correct1".getBytes())));
+    assertEquals(ITokenVerifier.AUTH_FORBIDDEN, verifier1.verify(List.of("Correct2".getBytes())));
+    assertEquals(ITokenVerifier.AUTH_FAILED, verifier1.verify(null));
+    assertEquals(ITokenVerifier.AUTH_FAILED, verifier1.verify(List.of("Correct1".getBytes(), "Wrong".getBytes())));
+
+    assertEquals(ITokenVerifier.AUTH_OK, verifier2.verify(List.of("Correct2".getBytes())));
+    assertEquals(ITokenVerifier.AUTH_FORBIDDEN, verifier2.verify(List.of("Correct1".getBytes())));
+    assertEquals(ITokenVerifier.AUTH_FAILED, verifier2.verify(null));
+    assertEquals(ITokenVerifier.AUTH_FAILED, verifier2.verify(List.of("Correct2".getBytes(), "Wrong".getBytes())));
   }
 }
