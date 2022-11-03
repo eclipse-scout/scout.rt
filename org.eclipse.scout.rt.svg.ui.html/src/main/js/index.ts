724c361444fd2d72d49778061c76679acde6c60c
Matthias Villiger <mvi@bsi-software.com>
2022-11-03 15:21:36 +0100

    TS migration: rename files (org.eclipse.scout.rt.svg.ui.html)

--- org.eclipse.scout.rt.svg.ui.html/src/main/js/index.ts
+++ org.eclipse.scout.rt.svg.ui.html/src/main/js/index.ts
@@ -9,7 +9,7 @@
  *     BSI Business Systems Integration AG - initial API and implementation
  */
 import {ObjectFactory} from '@eclipse-scout/core';
-import * as self from './index.js';
+import * as self from './index';
 
 export {default as SvgField} from './svg/SvgField';
 export {default as SvgFieldAdapter} from './svg/SvgFieldAdapter';
