7f50cc8f7bbf43d4feba4ea020d4a1c4b63100b2
Matthias Villiger <mvi@bsi-software.com>
2022-12-06 10:58:31 +0100

    IClientNotificationService proxy might not be available
    This happens e.g. when executing ResourceBuilder.main()

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/clientnotification/ClientNotificationPoller.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/clientnotification/ClientNotificationPoller.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2010-2018 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -142,7 +142,8 @@ public class ClientNotificationPoller {
       if (event.getState() == State.PlatformStopping) {
         ClientNotificationPoller poller = BEANS.get(ClientNotificationPoller.class);
         poller.stop();
-        poller.createRunContext().run(() -> BEANS.get(IClientNotificationService.class).unregisterNode(NodeId.current()));
+        poller.createRunContext().run(() -> BEANS.optional(IClientNotificationService.class)
+            .ifPresent(svc -> svc.unregisterNode(NodeId.current())));
       }
     }
   }
