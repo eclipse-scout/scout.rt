78e2987b13a5fac85b5ae4d6dab4ed251e76244f
Matthias Otterbach <matthias.otterbach@bsi-software.com>
2022-12-07 11:48:41 +0100

    Add TestingDefaultPlatform to avoid duplicate logger shutdown
    Some platform listeners must not be used within another platform (which
    can happen during tests)
    
    331773

--- org.eclipse.scout.rt.platform.test/src/main/java/org/eclipse/scout/rt/testing/platform/runner/RunWithNewPlatform.java
+++ org.eclipse.scout.rt.platform.test/src/main/java/org/eclipse/scout/rt/testing/platform/runner/RunWithNewPlatform.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2010-2017 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -16,12 +16,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.eclipse.scout.rt.platform.DefaultPlatform;
 import org.eclipse.scout.rt.platform.IPlatform;
+import org.eclipse.scout.rt.testing.platform.TestingDefaultPlatform;
 
 /**
  * Annotation to execute test-methods using a new {@link IPlatform}, not the globally shared Platform. This annotation
  * requires to be executed by the {@link PlatformTestRunner} ore one of its subclasses.
+ * <p>
+ * If no platform is specified, the {@link TestingDefaultPlatform} will be used. This platform may also be used as a
+ * template or super-class whenever using own platforms.
+ * </p>
  *
  * @since 5.1
  */
@@ -32,5 +36,5 @@ public @interface RunWithNewPlatform {
   /**
    * @return the platform class to be used
    */
-  Class<? extends IPlatform> platform() default DefaultPlatform.class;
+  Class<? extends IPlatform> platform() default TestingDefaultPlatform.class;
 }
