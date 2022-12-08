1d2f6c31060f13c6beba18c1dc36063f7cf7717d
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2022-12-08 18:05:49 +0100

    Use side effect import for entry point files
    Simplifies imports and makes workaround for unused variables obsolete.
    
    336712

--- scout-hellojs-app/src/main/resources/archetype-resources/__rootArtifactId__.ui/src/test/js/test-index.js
+++ scout-hellojs-app/src/main/resources/archetype-resources/__rootArtifactId__.ui/src/test/js/test-index.js
@@ -1,8 +1,6 @@
 import {JasmineScout} from '@eclipse-scout/core/testing';
 
-import * as ref1 from '../../main/js/index';
-
-Object.assign({}, ref1);
+import '../../main/js/index';
 
 let context = require.context('./', true, /[sS]pec\.js$/);
 JasmineScout.runTestSuite(context);
--- scout-helloworld-app/src/main/resources/archetype-resources/__rootArtifactId__.ui.html/src/main/js/__simpleArtifactName__.js
+++ scout-helloworld-app/src/main/resources/archetype-resources/__rootArtifactId__.ui.html/src/main/js/__simpleArtifactName__.js
@@ -1,6 +1,4 @@
 import {RemoteApp} from '@eclipse-scout/core';
-import * as ${simpleArtifactName} from './index';
-
-Object.assign({}, ${simpleArtifactName}); // Use import so that it is not marked as unused
+import './index';
 
 new RemoteApp().init();
