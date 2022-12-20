afa76be45909a98c371ba1ad520c54c58fb68880
Paolo Bazzi <paolo.bazzi@bsi-software.com>
2022-12-20 18:48:11 +0100

    DataObject: Suppress writing type information for raw DoEntity
    Skip writing type information for raw DoEntity if serialized as
    attribute of a typed entity indicating that type information should
    be available.
    
    321546

--- org.eclipse.scout.rt.jackson/src/main/java/org/eclipse/scout/rt/jackson/dataobject/DataObjectTypeResolverBuilder.java
+++ org.eclipse.scout.rt.jackson/src/main/java/org/eclipse/scout/rt/jackson/dataobject/DataObjectTypeResolverBuilder.java
@@ -10,18 +10,24 @@
  */
 package org.eclipse.scout.rt.jackson.dataobject;
 
+import java.io.IOException;
 import java.util.Collection;
 
 import org.eclipse.scout.rt.dataobject.DataObjectInventory;
+import org.eclipse.scout.rt.dataobject.TypeName;
 import org.eclipse.scout.rt.platform.Bean;
 import org.eclipse.scout.rt.platform.util.LazyValue;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.type.WritableTypeId;
 import com.fasterxml.jackson.databind.DeserializationConfig;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.SerializationConfig;
 import com.fasterxml.jackson.databind.jsontype.NamedType;
 import com.fasterxml.jackson.databind.jsontype.TypeDeserializer;
+import com.fasterxml.jackson.databind.jsontype.TypeIdResolver;
 import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
+import com.fasterxml.jackson.databind.jsontype.impl.AsPropertyTypeSerializer;
 import com.fasterxml.jackson.databind.jsontype.impl.StdTypeResolverBuilder;
 
 @Bean
@@ -36,7 +42,7 @@ public class DataObjectTypeResolverBuilder extends StdTypeResolverBuilder {
 
   @Override
   public TypeSerializer buildTypeSerializer(SerializationConfig config, JavaType baseType, Collection<NamedType> subtypes) {
-    return useForType(baseType) ? super.buildTypeSerializer(config, baseType, subtypes) : null;
+    return useForType(baseType) ? new DataObjectAsPropertyTypeSerializer(_customIdResolver, getTypeProperty()) : null;
   }
 
   protected boolean useForType(JavaType t) {
@@ -46,5 +52,32 @@ public class DataObjectTypeResolverBuilder extends StdTypeResolverBuilder {
     }
     // write type name if class is annotated with a type name
     return m_dataObjectInventory.get().toTypeName(t.getRawClass()) != null;
+  }
+
+  /**
+   * Custom {@link AsPropertyTypeSerializer} implementation used to add type information to serialized data object, but
+   * only if the class of the runtime value is annotated with type information (see {@link TypeName}).
+   */
+  protected static class DataObjectAsPropertyTypeSerializer extends AsPropertyTypeSerializer {
+
+    public DataObjectAsPropertyTypeSerializer(TypeIdResolver idRes, String propName) {
+      super(idRes, null, propName);
+    }
+
+    @Override
+    public WritableTypeId writeTypePrefix(JsonGenerator g, WritableTypeId idMetadata) throws IOException {
+      _generateTypeId(idMetadata);
+      if (idMetadata.id != null) {
+        return g.writeTypePrefix(idMetadata);
+      }
+      else {
+        // Skip writing type information if no type id could be generated for given value (e.g. class structure
+        // indicated a type name, but runtime class of value to be serialized is DoEntity which does not provide any
+        // type information by class). Note that the raw DoEntity which is serialized could contain a raw type
+        // information as ordinary attribute (which is preserved as is).
+        g.writeStartObject(idMetadata.forValue);
+        return idMetadata;
+      }
+    }
   }
 }
