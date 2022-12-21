e175825fdd59b41465cd98869db7407f854d29de
Matthias Villiger <mvi@bsi-software.com>
2022-12-21 13:58:36 +0100

    scout-migrate tool: Don't convert line separator if already crlf is used

--- eclipse-scout-migrate/src/convertToCRLFPlugin.js
+++ eclipse-scout-migrate/src/convertToCRLFPlugin.js
@@ -1,18 +1,35 @@
+/*
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
+
 /**
  * @type import('ts-migrate-server').Plugin<unknown>
  */
 import {lfToCrlf} from './common.js';
 
+let filesConvertedToCrLf = new Set();
 /**
  * @type import('ts-migrate-server').Plugin<{}>
  */
 const convertToCRLFPlugin = {
   name: 'convert-to-crlf',
 
-  run({text}) {
+  run({text, fileName}) {
     // declareMissingClassPropertiesPlugin needs \r\n to work -> ensure crlf line breaks. The next regex also depends on that.
+    if (text.indexOf('\r\n') >= 0) {
+      return text;
+    }
+    filesConvertedToCrLf.add(fileName); // remember files for which the newlines have been changed. Required to only revert the ones that have been modified.
     return lfToCrlf(text);
   }
 };
 
 export default convertToCRLFPlugin;
+export {filesConvertedToCrLf};
--- eclipse-scout-migrate/src/convertToLFPlugin.js
+++ eclipse-scout-migrate/src/convertToLFPlugin.js
@@ -1,7 +1,19 @@
+/*
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
+
 /**
  * @type import('ts-migrate-server').Plugin<unknown>
  */
 import {crlfToLf} from './common.js';
+import {filesConvertedToCrLf} from './convertToCRLFPlugin.js';
 
 /**
  * @type import('ts-migrate-server').Plugin<{}>
@@ -9,9 +21,11 @@ import {crlfToLf} from './common.js';
 const convertToLFPlugin = {
   name: 'convert-to-lf',
 
-  run({text}) {
-    // revert line ending style to lf
-    return crlfToLf(text);
+  run({text, fileName}) {
+    if (filesConvertedToCrLf.has(fileName)) {
+      // revert line ending style to lf
+      return crlfToLf(text);
+    }
   }
 };
 
