64db439f98ddaab0c036cf289c050c859f535690
Matthias Villiger <mvi@bsi-software.com>
2023-01-06 15:40:31 +0100

    UNSAFE_UPLOAD and REJECTED_UPLOAD error responses are not fatal
    JsonResponseError.UNSAFE_UPLOAD and JsonResponseError.REJECTED_UPLOAD
    may be set on the json response in addition to regular events. As these
    error types are not fatal (the application continues to work) the events
    must be processed as well. Otherwise, the UI might end up in an
    inconsistent state because some events have been skipped (e.g. about a
    destroyed widget).
    
    299642

--- eclipse-scout-core/src/session/Session.ts
+++ eclipse-scout-core/src/session/Session.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2023 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -391,8 +391,10 @@ export class Session extends EventEmitter implements SessionModel, ModelAdapterL
   protected _processStartupResponse(data: SessionStartupResponse) {
     // Handle errors from server
     if (data.error) {
-      this._processErrorJsonResponse(data.error);
-      return;
+      let isFatalError = this._processErrorJsonResponse(data.error);
+      if (isFatalError) {
+        return;
+      }
     }
 
     webstorage.removeItemFromSessionStorage('scout:versionMismatch');
@@ -919,12 +921,13 @@ export class Session extends EventEmitter implements SessionModel, ModelAdapterL
    * Otherwise, the response queue's expected sequence number will get out of sync.
    */
   processJsonResponseInternal(data: RemoteResponse): boolean {
-    let success = false;
+    let success = true;
     if (data.error) {
-      this._processErrorJsonResponse(data.error);
-    } else {
+      let isFatalError = this._processErrorJsonResponse(data.error);
+      success = !isFatalError;
+    }
+    if (success) {
       this._processSuccessResponse(data);
-      success = true;
     }
     return success;
   }
@@ -991,14 +994,15 @@ export class Session extends EventEmitter implements SessionModel, ModelAdapterL
     this.showFatalMessage(boxOptions, jqXHR.status + '.net');
   }
 
-  protected _processErrorJsonResponse(jsonError: JsonErrorResponse) {
+  protected _processErrorJsonResponse(jsonError: JsonErrorResponse): boolean {
+    let isFatalError = true;
     if (jsonError.code === Session.JsonResponseError.VERSION_MISMATCH) {
       let loopDetection = webstorage.getItemFromSessionStorage('scout:versionMismatch');
       if (!loopDetection) {
         webstorage.setItemToSessionStorage('scout:versionMismatch', 'yes');
         // Reload page -> everything should then be up-to-date
         scout.reloadPage();
-        return;
+        return isFatalError;
       }
       webstorage.removeItemFromSessionStorage('scout:versionMismatch');
     }
@@ -1006,7 +1010,7 @@ export class Session extends EventEmitter implements SessionModel, ModelAdapterL
     if (this.loggedOut) {
       // When the session is terminated via user request (logout button), the poller might return
       // with a code 20. If we are already logged out, there is no need to show a message box.
-      return;
+      return isFatalError;
     }
 
     // Default values for fatal message boxes
@@ -1043,12 +1047,15 @@ export class Session extends EventEmitter implements SessionModel, ModelAdapterL
       boxOptions.header = this.optText('ui.UnsafeUpload', boxOptions.header);
       boxOptions.body = this.optText('ui.UnsafeUploadMsg', boxOptions.body);
       boxOptions.yesButtonText = this.optText('ui.Ok', 'Ok');
+      isFatalError = false; // unsafe upload allows the application to continue
     } else if (jsonError.code === Session.JsonResponseError.REJECTED_UPLOAD) {
       boxOptions.header = this.optText('ui.RejectedUpload', boxOptions.header);
       boxOptions.body = this.optText('ui.RejectedUploadMsg', boxOptions.body);
       boxOptions.yesButtonText = this.optText('ui.Ok', 'Ok');
+      isFatalError = false; // rejected upload allows the application to continue
     }
     this.showFatalMessage(boxOptions, jsonError.code + '');
+    return isFatalError;
   }
 
   protected _fireRequestFinished(message: RemoteResponse) {
