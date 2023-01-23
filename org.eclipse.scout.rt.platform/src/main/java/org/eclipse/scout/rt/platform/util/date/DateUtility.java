8545c6c37f84f11a611c2d09ae88d95495446d07
Niklas Sidler <niklas.sidler@gmail.com>
2023-01-23 15:48:06 +0100

    DateUtility: Update date format to DateFormat.DEFAULT
    340083

--- org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/util/date/DateUtility.java
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/util/date/DateUtility.java
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010-2023 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v10.html
  *
- * This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License 2.0
- * which is available at https://www.eclipse.org/legal/epl-2.0/
- *
- * SPDX-License-Identifier: EPL-2.0
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
  */
 package org.eclipse.scout.rt.platform.util.date;
 
@@ -48,7 +49,7 @@ public final class DateUtility {
   private static final List<String> FRY_SAT_WEEKEND_DAYS_COUNTRIES = Arrays.asList("AE", "DZ", "BH", "BD", "EG", "IQ", "IL", "JO", "KW", "LY", "MV", "MR", "OM", "PS", "QA", "SA", "SD", "SY", "YE");
 
   /**
-   * format date with {@value DateFormat#DEFAULT} pattern
+   * Formats date with {@link DateFormat#DEFAULT} pattern.
    */
   public static String formatDate(Date d) {
     if (d == null) {
@@ -59,7 +60,7 @@ public final class DateUtility {
   }
 
   /**
-   * format time with {@value DateFormat#SHORT} pattern
+   * Formats time with {@link DateFormat#SHORT} pattern.
    */
   public static String formatTime(Date d) {
     if (d == null) {
@@ -70,18 +71,40 @@ public final class DateUtility {
   }
 
   /**
-   * format time with {@value DateFormat#SHORT}, {@value DateFormat#SHORT} patterns
+   * Formats date with the {@link DateFormat#DEFAULT} pattern and time with {@link DateFormat#SHORT}.
    */
   public static String formatDateTime(Date d) {
+    return formatDateTime(d, DateFormat.DEFAULT, DateFormat.SHORT);
+  }
+
+  /**
+   * Formats both date and time with the {@link DateFormat#SHORT} pattern.
+   */
+  public static String formatDateTimeShort(Date d) {
+    return formatDateTime(d, DateFormat.SHORT, DateFormat.SHORT);
+  }
+
+  /**
+   * Formats the specified date and time with the given patterns as defined in {@link DateFormat}.
+   *
+   * @param d
+   *          date to format
+   * @param dateFormat
+   *          date format
+   * @param timeFormat
+   *          time format
+   * @return formatted date as {@link String}
+   */
+  public static String formatDateTime(Date d, int dateFormat, int timeFormat) {
     if (d == null) {
       return "";
     }
     Locale loc = NlsLocale.get();
-    return BEANS.get(DateFormatProvider.class).getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, loc).format(d);
+    return BEANS.get(DateFormatProvider.class).getDateTimeInstance(dateFormat, timeFormat, loc).format(d);
   }
 
   /**
-   * format date with specific pattern as defined in {@link SimpleDateFormat}
+   * Formats date with specific pattern as defined in {@link SimpleDateFormat}.
    */
   public static String format(Date d, String pattern) {
     if (d == null || !StringUtility.hasText(pattern)) {
