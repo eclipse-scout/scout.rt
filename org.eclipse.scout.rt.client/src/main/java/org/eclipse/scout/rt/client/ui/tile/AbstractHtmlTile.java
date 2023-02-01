5d0f748f27cdd306bf228f5f9706eaca2d8b611f
Beat Schwarzentrub <bsh@bsiag.com>
2023-02-01 10:18:22 +0100

    AbstractHtmlTile: fix constructor
    m_attachmentSupport was only initialized in the default constructor, but
    not in the constructor with the "callInitializer" argument. Using that
    second constructor therefore caused NullPointerExceptions when adding
    attachments.
    
    341096

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/tile/AbstractHtmlTile.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/tile/AbstractHtmlTile.java
@@ -22,15 +22,15 @@ import org.eclipse.scout.rt.platform.resource.BinaryResource;
 @ClassId("22f26922-32e7-49f1-9180-b48fbb5e75cd")
 public abstract class AbstractHtmlTile extends AbstractTile implements IHtmlTile {
 
-  private AttachmentSupport m_attachmentSupport;
+  protected final AttachmentSupport m_attachmentSupport;
 
   public AbstractHtmlTile() {
     this(true);
-    m_attachmentSupport = BEANS.get(AttachmentSupport.class);
   }
 
   public AbstractHtmlTile(boolean callInitializer) {
     super(false);
+    m_attachmentSupport = BEANS.get(AttachmentSupport.class);
     if (callInitializer) {
       callInitializer();
     }
@@ -40,13 +40,19 @@ public abstract class AbstractHtmlTile extends AbstractTile implements IHtmlTile
   protected void initConfig() {
     super.initConfig();
     setContent(getConfiguredContent());
-    setHtmlEnabled(true);
+    setHtmlEnabled(getConfiguredHtmlEnabled());
   }
 
   @ConfigProperty(ConfigProperty.TEXT)
   @Order(10)
   protected String getConfiguredContent() {
     return null;
+  }
+
+  @ConfigProperty(ConfigProperty.BOOLEAN)
+  @Order(11)
+  protected boolean getConfiguredHtmlEnabled() {
+    return true;
   }
 
   @Override
@@ -93,5 +99,4 @@ public abstract class AbstractHtmlTile extends AbstractTile implements IHtmlTile
   public boolean isHtmlEnabled() {
     return propertySupport.getPropertyBool(PROP_HTML_ENABLED);
   }
-
 }
