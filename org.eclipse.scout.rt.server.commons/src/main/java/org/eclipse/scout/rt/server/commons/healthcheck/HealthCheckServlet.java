1286c3ed53fa78e2637a9a4af2e3f5b9e0d7cff2
Paolo Bazzi <paolo.bazzi@bsi-software.com>
2023-03-06 07:07:01 +0100

    Health Check: Extract to service & consolidate implementations
    - Moved execution of health check from servlet to service
    - Consolidate servlets into a generic Scout HealthCheckServlet
    
    247627

--- org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/healthcheck/HealthCheckServlet.java
+++ org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/healthcheck/HealthCheckServlet.java
@@ -10,9 +10,6 @@
 package org.eclipse.scout.rt.server.commons.healthcheck;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Objects;
 
 import javax.servlet.ServletException;
@@ -21,7 +18,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.scout.rt.platform.BEANS;
 import org.eclipse.scout.rt.platform.Platform;
-import org.eclipse.scout.rt.platform.context.RunContext;
 import org.eclipse.scout.rt.platform.util.StringUtility;
 import org.eclipse.scout.rt.server.commons.healthcheck.IHealthChecker.IHealthCheckCategory;
 import org.eclipse.scout.rt.server.commons.servlet.AbstractHttpServlet;
@@ -31,9 +27,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * The <code>AbstractHealthCheckServlet</code> uses all available active {@link IHealthChecker} classes to determine the
- * application status. If the application status is OK, the servlet returns HTTP 200. In case any
- * <code>IHealthChecker</code> fails, the servlet returns HTTP 503.
+ * The <code>HealthCheckServlet</code> uses {@link HealthCheckService} classes to determine the application status. If
+ * the application status is OK, the servlet returns HTTP 200. In case any <code>IHealthChecker</code> fails, the
+ * servlet returns HTTP 503.
  * <p>
  * This servlet can be used in combination with load balancers or reverse proxies that use an HTTP-GET or HTTP-HEAD
  * check method to determine the availability of the application.
@@ -43,36 +39,23 @@ import org.slf4j.LoggerFactory;
  * explanation of filtering.
  *
  * @since 6.1
+ * @see HealthCheckService
  * @see AbstractHealthChecker
  */
-public abstract class AbstractHealthCheckServlet extends AbstractHttpServlet {
+public class HealthCheckServlet extends AbstractHttpServlet {
   private static final long serialVersionUID = 1L;
 
-  private static final Logger LOG = LoggerFactory.getLogger(AbstractHealthCheckServlet.class);
+  private static final Logger LOG = LoggerFactory.getLogger(HealthCheckServlet.class);
 
   public static final String QUERY_PARAMETER_NAME_CATEGORY = "category";
-
-  protected abstract RunContext execCreateRunContext();
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     disableCaching(req, resp);
     BEANS.get(HttpServletControl.class).doDefaults(this, req, resp);
 
-    RunContext context;
     try {
-      context = execCreateRunContext();
-      if (context == null) {
-        throw new IllegalArgumentException("context must not be null");
-      }
-    }
-    catch (Throwable t) {
-      LOG.error("Creating RunContext failed", t);
-      throw BEANS.get(ServletExceptionTranslator.class).translate(t);
-    }
-
-    try {
-      doChecks(context, req, resp);
+      doChecks(req, resp);
     }
     catch (Throwable t) {
       LOG.error("HealthChecking crashed", t);
@@ -87,41 +70,18 @@ public abstract class AbstractHealthCheckServlet extends AbstractHttpServlet {
     resp.setDateHeader("Expires", 0); // prevents caching at the proxy server
   }
 
-  /**
-   * @param category
-   *          nullable category to be checked; if category is null this parameter is ignored
-   * @return <code>false</code> to ignore given <code>IHealthChecker</code>
-   */
-  protected boolean execAcceptCheck(IHealthChecker check, HealthCheckCategoryId category) {
-    return (category == null || check.acceptCategory(category)) && check.isActive();
-  }
-
-  protected void doChecks(RunContext context, HttpServletRequest req, HttpServletResponse resp) throws IOException {
+  protected void doChecks(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     HealthCheckCategoryId category = parseCategory(req);
+    HealthCheckResult result = BEANS.get(HealthCheckService.class).check(category);
 
-    List<IHealthChecker> checks = getActiveHealthCheckers(category);
-    List<IHealthChecker> failed = new ArrayList<>();
-
-    for (IHealthChecker check : checks) {
-      try {
-        if (!check.checkHealth(context, category)) {
-          failed.add(check);
-        }
-      }
-      catch (Throwable t) {
-        LOG.error("HealthChecker[{}] failed", check.getName(), t);
-        failed.add(check);
-      }
-    }
-
-    int statusCode = failed.isEmpty() ? HttpServletResponse.SC_OK : HttpServletResponse.SC_SERVICE_UNAVAILABLE;
-    String output = generateOutput(statusCode, checks, failed, false);
+    int statusCode = result.getFailedChecks().isEmpty() ? HttpServletResponse.SC_OK : HttpServletResponse.SC_SERVICE_UNAVAILABLE;
+    String output = generateOutput(statusCode, result, false);
 
     resp.setContentType("text/plain");
     resp.setStatus(statusCode);
 
     if (LOG.isDebugEnabled() || Platform.get().inDevelopmentMode()) {
-      String detailedOutput = generateOutput(statusCode, checks, failed, true);
+      String detailedOutput = generateOutput(statusCode, result, true);
       LOG.debug(detailedOutput);
       resp.getWriter().print(Platform.get().inDevelopmentMode() ? detailedOutput : output);
     }
@@ -152,39 +112,19 @@ public abstract class AbstractHealthCheckServlet extends AbstractHttpServlet {
     return category;
   }
 
-  protected List<IHealthChecker> getActiveHealthCheckers(HealthCheckCategoryId category) {
-    List<IHealthChecker> all = BEANS.all(IHealthChecker.class);
-    List<IHealthChecker> actives = new ArrayList<>(all.size());
-    for (IHealthChecker check : all) {
-      try {
-        if (execAcceptCheck(check, category)) {
-          actives.add(check);
-        }
-        else {
-          LOG.debug("HealthChecker[{}] was ignored (called with category {})", check.getName(), category);
-        }
-      }
-      catch (Throwable t) {
-        LOG.error("Active-check crashed with HealthChecker[{}]", check.getName(), t);
-      }
-    }
-    return Collections.unmodifiableList(actives);
-  }
-
-  protected String generateOutput(int statusCode, List<IHealthChecker> checks, List<IHealthChecker> failed, boolean includeDetails) {
+  protected String generateOutput(int statusCode, HealthCheckResult result, boolean includeDetails) {
     StringBuilder buf = new StringBuilder();
     buf.append(statusCode);
     buf.append(' ');
-    buf.append(failed.isEmpty() ? "OK" : "SERVICE_UNAVAILABLE");
+    buf.append(result.isSuccess() ? "OK" : "SERVICE_UNAVAILABLE");
     if (includeDetails) {
-      for (IHealthChecker check : checks) {
+      for (IHealthChecker check : result.getAllChecks()) {
         buf.append('\n');
         buf.append(check.getName());
         buf.append(':');
-        buf.append(failed.contains(check) ? "ERROR" : "OK");
+        buf.append(result.getFailedChecks().contains(check) ? "ERROR" : "OK");
       }
     }
     return buf.toString();
   }
-
 }
