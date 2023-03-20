5ab3bb9a93e7a49fdf652c1eb202c958f43a4890
Ivan Motsch <Ivan.Motsch@bsi-software.com>
2023-03-20 18:59:29 +0100

    OWASP Dependency Check
    handle new CVEs; review with PMO.
    CVE-2023-24998  commons-fileupload
    set maximum file count per upload, default 100 (ref#345271)
    new config property 'scout.ui.maxUploadFileCount' default 100
    
    Signed-off-by: Ivan Motsch <Ivan.Motsch@bsi-software.com>

--- org.eclipse.scout.rt.ui.html.test/src/test/java/org/eclipse/scout/rt/ui/html/json/UploadRequestHandlerTest.java
+++ org.eclipse.scout.rt.ui.html.test/src/test/java/org/eclipse/scout/rt/ui/html/json/UploadRequestHandlerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2021 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2023 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,17 +10,38 @@
  */
 package org.eclipse.scout.rt.ui.html.json;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMultipart;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.fileupload.FileUploadException;
+import org.eclipse.scout.rt.platform.BEANS;
+import org.eclipse.scout.rt.platform.BeanMetaData;
+import org.eclipse.scout.rt.platform.IBean;
+import org.eclipse.scout.rt.platform.resource.BinaryResource;
 import org.eclipse.scout.rt.platform.resource.BinaryResources;
+import org.eclipse.scout.rt.platform.security.MalwareScanner;
 import org.eclipse.scout.rt.platform.security.RejectedResourceException;
 import org.eclipse.scout.rt.platform.util.HexUtility;
+import org.eclipse.scout.rt.server.commons.BufferedServletInputStream;
+import org.eclipse.scout.rt.testing.platform.BeanTestingHelper;
+import org.eclipse.scout.rt.ui.html.UiHtmlConfigProperties;
 import org.eclipse.scout.rt.ui.html.res.IUploadable;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class UploadRequestHandlerTest {
 
@@ -88,5 +109,71 @@ public class UploadRequestHandlerTest {
   @Test(expected = RejectedResourceException.class)
   public void testVerifyFileIntegrityWithNonImageExtAsGif() {
     new UploadRequestHandler().verifyFileIntegrity(BinaryResources.create().withFilename("x.foobar.mov").withContent(HexUtility.decode("474946383761")).build());
+  }
+
+  private static HttpServletRequest createUploadRequest(int fileCount) throws MessagingException, IOException {
+    MimeMultipart multipart = new MimeMultipart("form-data");
+
+    MimeBodyPart magicPart = new MimeBodyPart();
+    magicPart.setDisposition("form-data; name=\"rowId\"");
+    magicPart.setText("");
+    multipart.addBodyPart(magicPart);
+
+    for (int i = 1; i <= fileCount; i++) {
+      MimeBodyPart filePart = new MimeBodyPart();
+      filePart.setDisposition("form-data; name=\"files\"");
+      filePart.setFileName("file" + i + ".txt");
+      filePart.setText("Text " + i);
+      multipart.addBodyPart(filePart);
+    }
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    multipart.writeTo(out);
+
+    HttpServletRequest req = Mockito.mock(HttpServletRequest.class);
+    Mockito.doReturn(multipart.getContentType()).when(req).getContentType();
+    Mockito.doReturn(new BufferedServletInputStream(out.toByteArray())).when(req).getInputStream();
+    return req;
+  }
+
+  @Test
+  public void testFileCount() throws IOException, FileUploadException, MessagingException {
+    BeanTestingHelper testingHelper = BEANS.get(BeanTestingHelper.class);
+    List<IBean<?>> mocked = new ArrayList<>();
+    //limit upload file count to 10
+    mocked.add(testingHelper.mockConfigProperty(UiHtmlConfigProperties.MaxUploadFileCountProperty.class, 10L));
+    //disable malware scanner
+    mocked.add(testingHelper.registerBean(new BeanMetaData(MalwareScanner.class, new MalwareScanner() {
+      @Override
+      public void scan(BinaryResource res) {
+        //nop
+      }
+    })));
+    IUploadable uploadable = () -> 1000000;
+    Map<String, String> props = new HashMap<>();
+    List<BinaryResource> result = new ArrayList<>();
+
+    try {
+      //0 files are ok
+      new UploadRequestHandler().readUploadData(createUploadRequest(0), uploadable, props, result);
+      //1 file is ok
+      result.clear();
+      new UploadRequestHandler().readUploadData(createUploadRequest(1), uploadable, props, result);
+      //10 files are ok
+      result.clear();
+      new UploadRequestHandler().readUploadData(createUploadRequest(10), uploadable, props, result);
+      //11 files fail
+      result.clear();
+      try {
+        new UploadRequestHandler().readUploadData(createUploadRequest(11), uploadable, props, result);
+        Assert.fail("must fail");
+      }
+      catch (RejectedResourceException e) {
+        //expected
+      }
+    }
+    finally {
+      testingHelper.unregisterBeans(mocked);
+    }
   }
 }
