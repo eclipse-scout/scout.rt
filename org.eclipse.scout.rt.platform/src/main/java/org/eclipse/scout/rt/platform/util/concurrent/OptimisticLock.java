7a979d2cc09e4380482aa8937fbca1849bafeeb8
Clemens Stukenbrock <clemens.stukenbrock@bsi-software.com>
2023-03-22 13:21:01 +0100

    Decrease memory footprint of OptimisticLock

--- org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/util/concurrent/OptimisticLock.java
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/util/concurrent/OptimisticLock.java
@@ -9,8 +9,6 @@
  */
 package org.eclipse.scout.rt.platform.util.concurrent;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,35 +20,34 @@ import org.slf4j.LoggerFactory;
  */
 public class OptimisticLock {
   private static final Logger LOG = LoggerFactory.getLogger(OptimisticLock.class);
-  private final AtomicInteger m_lockCount = new AtomicInteger(0);
+  private volatile int m_lockCount = 0;
 
   /**
    * @return true if lock was acquired as first monitor
    */
   public synchronized boolean acquire() {
-    int count = m_lockCount.incrementAndGet();
-    if (count == 1) {
+    m_lockCount++;
+    if (m_lockCount == 1) {
       // this is the first
       return true;
     }
     else {
-      if (count > 10) {
+      if (m_lockCount > 10) {
         LOG.warn("potential programming problem; lock was 10 times acquired and not released", new Exception("origin"));
       }
       return false;
     }
   }
 
-  public void release() {
-    m_lockCount.decrementAndGet();
+  public synchronized void release() {
+    m_lockCount--;
   }
 
   public boolean isAcquired() {
-    return m_lockCount.get() > 0;
+    return m_lockCount > 0;
   }
 
   public boolean isReleased() {
     return !isAcquired();
   }
-
 }
