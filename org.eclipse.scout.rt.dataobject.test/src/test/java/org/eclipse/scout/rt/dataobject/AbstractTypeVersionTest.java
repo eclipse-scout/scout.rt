4045a32eb8cd7b88b36ac42b82178c54e63a9f0b
Stephan Merkli <stephan.merkli@bsi-software.com>
2023-04-19 17:21:32 +0200

    Abstract Type version: add support for optional suffix
    Optional suffix starts with __ followed by any word characters.
    The suffix is only part of the class name, but not the resulting NamespaceVersion.
    
    Examples:
    - Scout_22_0_0__lorem
    - Scout_22_0_0__ipsum_dolor
    
    346252

--- org.eclipse.scout.rt.dataobject.test/src/test/java/org/eclipse/scout/rt/dataobject/AbstractTypeVersionTest.java
+++ org.eclipse.scout.rt.dataobject.test/src/test/java/org/eclipse/scout/rt/dataobject/AbstractTypeVersionTest.java
@@ -14,6 +14,11 @@ import static org.eclipse.scout.rt.platform.util.Assertions.assertEquals;
 import static org.junit.Assert.assertNull;
 
 import org.eclipse.scout.rt.dataobject.fixture.DataObjectFixtureTypeVersions.DataObjectFixture_1_0_0;
+import org.eclipse.scout.rt.dataobject.fixture.DataObjectFixtureTypeVersions.DataObjectFixture_1_3_002__suffix;
+import org.eclipse.scout.rt.dataobject.fixture.DataObjectFixtureTypeVersions.DataObjectFixture_1_3_0__123456;
+import org.eclipse.scout.rt.dataobject.fixture.DataObjectFixtureTypeVersions.DataObjectFixture_1_3_0__loremIpsumDolor_3;
+import org.eclipse.scout.rt.dataobject.fixture.DataObjectFixtureTypeVersions.DataObjectFixture_1_3_0__lorem_ipsum;
+import org.eclipse.scout.rt.dataobject.fixture.DataObjectFixtureTypeVersions.DataObjectFixture_1_3_0__suffix;
 import org.eclipse.scout.rt.dataobject.fixture.DataObjectProjectFixtureTypeVersions.DataObjectProjectFixture_1_2_3_004;
 import org.eclipse.scout.rt.platform.namespace.NamespaceVersion;
 import org.junit.Assert;
@@ -31,5 +36,12 @@ public class AbstractTypeVersionTest {
 
     assertEquals(NamespaceVersion.of("dataObjectFixture", "1.0.0"), fromClassName(DataObjectFixture_1_0_0.class));
     assertEquals(NamespaceVersion.of("dataObjectProjectFixture", "1.2.3.004"), fromClassName(DataObjectProjectFixture_1_2_3_004.class));
+
+    // with suffix
+    assertEquals(NamespaceVersion.of("dataObjectFixture", "1.3.0"), fromClassName(DataObjectFixture_1_3_0__suffix.class));
+    assertEquals(NamespaceVersion.of("dataObjectFixture", "1.3.0"), fromClassName(DataObjectFixture_1_3_0__123456.class));
+    assertEquals(NamespaceVersion.of("dataObjectFixture", "1.3.0"), fromClassName(DataObjectFixture_1_3_0__lorem_ipsum.class));
+    assertEquals(NamespaceVersion.of("dataObjectFixture", "1.3.0"), fromClassName(DataObjectFixture_1_3_0__loremIpsumDolor_3.class));
+    assertEquals(NamespaceVersion.of("dataObjectFixture", "1.3.002"), fromClassName(DataObjectFixture_1_3_002__suffix.class));
   }
 }
