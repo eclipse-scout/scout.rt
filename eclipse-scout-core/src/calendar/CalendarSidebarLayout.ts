fe41f132b84679952ed083355ca3d3c14945b689
Niklas Sidler <niklas.sidler@gmail.com>
2023-05-08 14:39:56 +0200

    Add min gap of splitter and refactor layout

--- eclipse-scout-core/src/calendar/CalendarSidebarLayout.ts
+++ eclipse-scout-core/src/calendar/CalendarSidebarLayout.ts
@@ -7,10 +7,15 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-import {AbstractLayout, CalendarSidebar, HtmlComponent, Rectangle} from '../index';
+import {AbstractLayout, CalendarSidebar, Dimension, HtmlComponent, Rectangle, Splitter} from '../index';
 
 export class CalendarSidebarLayout extends AbstractLayout {
   widget: CalendarSidebar;
+
+  static MIN_SPLITTER_POSITION = 210;
+
+  protected _relativeSplitterPosition: number;
+  protected _availableHeight: number;
   protected _splitterConstraints: string;
 
   constructor(widget: CalendarSidebar) {
@@ -29,42 +34,54 @@ export class CalendarSidebarLayout extends AbstractLayout {
     const splitter = this.widget.splitter;
     const resourcesPanel = this.widget.resourcesPanel;
 
-    let splitterConstraints = JSON.stringify({
-      availableSize: availableSize,
-      splitterVisible: splitter.isVisible()
-    });
-    if (splitterConstraints !== this._splitterConstraints) {
-      // If container size has changed, ensure the splitter position is valid (e.g. when restoring a maximized window)
-      let splitterPosition = splitter.position;
-      // Set initial splitter position (depending on the container width, therefore must be done during the layout)
-      if (splitterPosition === null) {
-        splitterPosition = availableSize.height / 2;
-      }
-      // Set positions without triggering this layout again
-      htmlComp.suppressInvalidate = true;
-      try {
-        splitter.setPosition(splitterPosition);
-      } finally {
-        htmlComp.suppressInvalidate = false;
-      }
-      this._splitterConstraints = splitterConstraints;
-    }
+    this._validateSplitterPosition(htmlComp, splitter, availableSize);
 
     // Set sizes
     splitter.setVisible(yearPanel.isVisible() && resourcesPanel.isVisible());
 
     let yearPanelHeight, resourcesPanelHeight;
+    let splitterHeight = splitter.htmlComp.bounds().height;
+
     yearPanelHeight = yearPanel.isVisible() ? availableSize.height : 0;
     resourcesPanelHeight = resourcesPanel.isVisible() ? availableSize.height : 0;
 
-    let splitterHeight = splitter.htmlComp.bounds().height;
     if (splitter.isVisible()) {
       yearPanelHeight = splitter.position - splitterHeight / 2;
       resourcesPanelHeight = availableSize.height - yearPanelHeight - splitterHeight;
     }
 
     yearPanel.htmlComp.setBounds(new Rectangle(insets.left, insets.top, availableSize.width, yearPanelHeight));
-
     resourcesPanel.htmlComp.setBounds(new Rectangle(insets.left, insets.top + splitterHeight, availableSize.width, resourcesPanelHeight));
+  }
+
+  protected _validateSplitterPosition(htmlComp: HtmlComponent, splitter: Splitter, availableSize: Dimension) {
+    if (splitter.position === null) {
+      // Set initial splitter position
+      this._silentUpdateSpliterPosition(htmlComp, splitter, availableSize.height / 2);
+    } else if (availableSize.height !== this._availableHeight) {
+      // Window has been resized -> preserve relative splitter position
+      this._silentUpdateSpliterPosition(htmlComp, splitter, availableSize.height * this._relativeSplitterPosition);
+    }
+
+    // Validate min and max splitter position
+    let maxSplitterPosition = availableSize.height - CalendarSidebarLayout.MIN_SPLITTER_POSITION;
+    if (splitter.position < CalendarSidebarLayout.MIN_SPLITTER_POSITION) {
+      this._silentUpdateSpliterPosition(htmlComp, splitter, CalendarSidebarLayout.MIN_SPLITTER_POSITION);
+    } else if (splitter.position > maxSplitterPosition) {
+      this._silentUpdateSpliterPosition(htmlComp, splitter, Math.max(maxSplitterPosition, CalendarSidebarLayout.MIN_SPLITTER_POSITION));
+    }
+
+    // Update cached values
+    this._availableHeight = availableSize.height;
+    this._relativeSplitterPosition = splitter.position / availableSize.height;
+  }
+
+  protected _silentUpdateSpliterPosition(htmlComp: HtmlComponent, splitter: Splitter, newPosition: number) {
+    htmlComp.suppressInvalidate = true;
+    try {
+      splitter.setPosition(newPosition);
+    } finally {
+      htmlComp.suppressInvalidate = false;
+    }
   }
 }
