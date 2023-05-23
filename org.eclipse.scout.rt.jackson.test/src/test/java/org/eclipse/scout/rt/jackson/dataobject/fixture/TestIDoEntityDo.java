a1ebd045d7489be3f347a0528888afc1cbbff75a
Stephan Merkli <stephan.merkli@bsi-software.com>
2023-05-23 18:00:19 +0200

    DoEntityDeserializer: use raw contributions list if surrounding DO entity has an unknown type
    351324

---
+++ org.eclipse.scout.rt.jackson.test/src/test/java/org/eclipse/scout/rt/jackson/dataobject/fixture/TestIDoEntityDo.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2010-2023 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
+package org.eclipse.scout.rt.jackson.dataobject.fixture;
+
+import javax.annotation.Generated;
+
+import org.eclipse.scout.rt.dataobject.DoEntity;
+import org.eclipse.scout.rt.dataobject.DoValue;
+import org.eclipse.scout.rt.dataobject.IDoEntity;
+import org.eclipse.scout.rt.dataobject.TypeName;
+
+@TypeName("scout.TestIDoEntity")
+public class TestIDoEntityDo extends DoEntity {
+
+  public DoValue<String> id() {
+    return doValue("id");
+  }
+
+  public DoValue<IDoEntity> value() {
+    return doValue("value");
+  }
+
+  /* **************************************************************************
+   * GENERATED CONVENIENCE METHODS
+   * *************************************************************************/
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public TestIDoEntityDo withId(String id) {
+    id().set(id);
+    return this;
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public String getId() {
+    return id().get();
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public TestIDoEntityDo withValue(IDoEntity value) {
+    value().set(value);
+    return this;
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public IDoEntity getValue() {
+    return value().get();
+  }
+}
