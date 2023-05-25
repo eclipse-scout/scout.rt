eclipse-scout/scout.rt

commit acf6c036293ff2ebb4a3807b9a72713f8bba0d85

SmartField: don't show invalid key warning on consecutive setValue calls
There are 3 cases:
- value is changed using setValue in the same task multiple times.
  because catch() is executed at the end of the promise chain, the field
  would display the invalid message even though the last lookup resulted
  in a valid text, see also b609789778ad2189b7b9264fc06c41e25fd83a1f.
  Fixed by checking _updateDisplayTextPending.
  Could also be solved by checking abort flag.
- the first setValue call would result in an invalid lookup, but a
  second setValue call is scheduled using setTimeout which will have
  a valid lookup -> invalid message must not be shown while second
  lookup is still running.
  Fixed by checking _updateDisplayTextPending
- the value is changed two times using setValue but the lookup is still
  pending (because it is slow) -> don't show the invalid value message
  while lookup is still running
  Fixed by checking abort flag.

329358

eclipse-scout-core/src/form/fields/smartfield/SmartField.ts
@@ -8,9 +8,9 @@
  * SPDX-License-Identifier: EPL-2.0
  */
 import {
-  arrays, CellEditorPopup, CellEditorRenderedOptions, CodeLookupCall, ColumnDescriptor, Device, EnumObject, fields, FormField, InitModelOf, InputFieldKeyStrokeContext, keys, KeyStrokeContext, LoadingSupport, LookupCall, LookupCallOrModel,
-  LookupResult, LookupRow, MaxLengthHandler, objects, ProposalChooserActiveFilterSelectedEvent, ProposalChooserLookupRowSelectedEvent, QueryBy, scout, SimpleLoadingSupport, SmartFieldCancelKeyStroke, SmartFieldEventMap, SmartFieldLayout,
-  SmartFieldModel, SmartFieldPopup, SmartFieldTouchPopup, Status, strings, TreeProposalChooser, ValidationFailedStatus, ValueField
+  AjaxError, arrays, CellEditorPopup, CellEditorRenderedOptions, CodeLookupCall, ColumnDescriptor, Device, EnumObject, fields, FormField, InitModelOf, InputFieldKeyStrokeContext, keys, KeyStrokeContext, LoadingSupport, LookupCall,
+  LookupCallOrModel, LookupResult, LookupRow, MaxLengthHandler, objects, ProposalChooserActiveFilterSelectedEvent, ProposalChooserLookupRowSelectedEvent, QueryBy, scout, SimpleLoadingSupport, SmartFieldCancelKeyStroke, SmartFieldEventMap,
+  SmartFieldLayout, SmartFieldModel, SmartFieldPopup, SmartFieldTouchPopup, Status, strings, TreeProposalChooser, ValidationFailedStatus, ValueField
 } from '../../../index';
 import $ from 'jquery';
 
@@ -693,7 +693,17 @@ export class SmartField<TValue> extends ValueField<TValue> implements SmartField
         this._triggerLookupCallDone(result);
         return formattedValue;
       })
-      .catch(() => {
+      .catch((error: any) => {
+        $.log.isDebugEnabled() && $.log.debug('FormatValue failed for SmartField with id ' + this.id, error);
+        if (!this._updateDisplayTextPending) {
+          // If a new value was set in the meantime, the previous lookup call was aborted -> don't mark the field as invalid because only the new lookup matters
+          return '';
+        }
+        if (error && error.abort || error instanceof AjaxError && error.textStatus === 'abort') {
+          // If a new value was set in the meantime but the display text of the new value not resolved yet, ignore the abort event of the first lookup to not show invalid key message while the second lookup is still in progress.
+          // Depending on the implementation of the lookup call, abort could either reject with an object containing abort, or the AjaxCall is rejected directly.
+          return '';
+        }
         return this._invalidKeyLookup(value);
       });
   }

eclipse-scout-core/test/form/fields/smartfield/SmartFieldSpec.ts
@@ -7,8 +7,10 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-import {fields, keys, LookupResult, LookupRow, ProposalChooser, QueryBy, scout, SmartField, SmartFieldModel, SmartFieldMultiline, SmartFieldPopup, SmartFieldTouchPopup, Status, strings, ValidationFailedStatus} from '../../../../src/index';
-import {AbortableMicrotaskStaticLookupCall, ColumnDescriptorDummyLookupCall, DummyLookupCall, FormSpecHelper, JQueryTesting} from '../../../../src/testing/index';
+import {
+  fields, keys, LookupResult, LookupRow, ProposalChooser, QueryBy, scout, SmartField, SmartFieldModel, SmartFieldMultiline, SmartFieldPopup, SmartFieldTouchPopup, StaticLookupCall, Status, strings, ValidationFailedStatus
+} from '../../../../src/index';
+import {ColumnDescriptorDummyLookupCall, DelayedStaticLookupCall, DummyLookupCall, FormSpecHelper, JQueryTesting, MicrotaskStaticLookupCall} from '../../../../src/testing/index';
 import {LookupCall} from '../../../../src/lookup/LookupCall';
 import {SmartFieldLookupResult} from '../../../../src/form/fields/smartfield/SmartField';
 import {FullModelOf, InitModelOf, ObjectOrModel} from '../../../../src/scout';
@@ -174,7 +176,7 @@ describe('SmartField', () => {
     it('updates display text correctly even after consecutive setValue calls', done => {
       jasmine.clock().uninstall();
       field = createFieldWithLookupCall({}, {
-        objectType: AbortableMicrotaskStaticLookupCall,
+        objectType: MicrotaskStaticLookupCall,
         data: [[1, 'Foo'], [2, 'Bar', 1]]
       });
       field.setValue(1);
@@ -619,6 +621,68 @@ describe('SmartField', () => {
       jasmine.clock().tick(500);
       expect(field.errorStatus).toBeInstanceOf(ValidationFailedStatus);
       expect(field.errorStatus.isWarning()).toBeTrue();
+    });
+
+    it('lookupByKey should not set a validation warning status if lookup was aborted', async () => {
+      jasmine.clock().uninstall();
+      let field = createFieldWithLookupCall({}, {
+        objectType: MicrotaskStaticLookupCall,
+        data: [[1, 'Foo'], [2, 'Bar', 1]]
+      });
+      field.setValue(1);
+      field.setValue(2);
+      await field.when('propertyChange:displayText');
+      expect(field.errorStatus).toBe(null);
+      expect(field.value).toBe(2);
+      expect(field.displayText).toBe('Bar');
+
+      // Check again after catch() in _formatValue has been executed
+      await sleep();
+      expect(field.errorStatus).toBe(null);
+      expect(field.value).toBe(2);
+      expect(field.displayText).toBe('Bar');
+    });
+
+    it('lookupByKey should not set a validation warning status if a second setValue call sets a valid value with setTimeout', async () => {
+      jasmine.clock().uninstall();
+      let field = createFieldWithLookupCall({}, {
+        objectType: MicrotaskStaticLookupCall
+      });
+      field.setValue(1);
+      await sleep();
+      (field.lookupCall as StaticLookupCall<any>).data = [[1, 'Foo'], [2, 'Bar', 1]];
+      field.setValue(2);
+
+      await field.when('propertyChange:displayText');
+      expect(field.errorStatus).toBe(null);
+      expect(field.value).toBe(2);
+      expect(field.displayText).toBe(''); // Set to empty string by catch in _formatValue first
+
+      await field.when('propertyChange:displayText');
+      expect(field.errorStatus).toBe(null);
+      expect(field.value).toBe(2);
+      expect(field.displayText).toBe('Bar');
+    });
+
+    it('lookupByKey should not set a validation warning status if first lookup was aborted and second still running', async () => {
+      jasmine.clock().uninstall();
+      let field = createFieldWithLookupCall({}, {
+        objectType: DelayedStaticLookupCall,
+        data: [[1, 'Foo'], [2, 'Bar', 1]]
+      });
+      field.setValue(1);
+      field.setValue(2); // aborts previous set value
+      let lookupCall = (field._currentLookupCall as DelayedStaticLookupCall<any>);
+      await sleep(); // triggers abort, catch in _formatValue may not be executed yet
+      expect(field.errorStatus).toBe(null);
+      expect(field.value).toBe(2);
+      expect(field.displayText).toBe('');
+
+      lookupCall.resolve(); // Executes lookup -> triggers catch in _formatValue, updateDisplayTextPending is not true anymore -> don't set invalid text because lookup call is aborted
+      await field.when('propertyChange:displayText');
+      expect(field.errorStatus).toBe(null);
+      expect(field.value).toBe(2);
+      expect(field.displayText).toBe('Bar');
     });
 
     function createFieldWithNoDataKeyLookupCall(rejectPromise: boolean): SmartField<any> {

eclipse-scout-core/src/form/fields/ValueField.ts
@@ -568,7 +568,7 @@ export class ValueField<TValue extends TModelValue, TModelValue = TValue> extend
       this._updateDisplayTextPending = true;
       // Promise is returned -> set display text later
       returned
-        .done(this.setDisplayText.bind(this))
+        .done(text => this.setDisplayText(text))
         .fail(() => {
           // If display text was updated in the meantime, don't override the text with an empty string
           if (this._updateDisplayTextPending) {

eclipse-scout-core/src/lookup/PrepopulatedLookupCall.ts
@@ -9,15 +9,22 @@
  */
 import {arrays, LookupCall, LookupResult, LookupRow, QueryBy, scout, strings} from '../index';
 import $ from 'jquery';
-import Deferred = JQuery.Deferred;
 
 export class PrepopulatedLookupCall<TKey> extends LookupCall<TKey> {
-
   lookupRows: LookupRow<TKey>[];
+  protected _deferred: JQuery.Deferred<LookupResult<TKey>>;
 
   constructor() {
     super();
     this.lookupRows = [];
+    this._deferred = null;
+  }
+
+  override abort() {
+    this._deferred?.reject({
+      abort: true
+    });
+    super.abort();
   }
 
   setLookupRows(lookupRows: LookupRow<TKey>[] | LookupRow<TKey>) {
@@ -31,13 +38,13 @@ export class PrepopulatedLookupCall<TKey> extends LookupCall<TKey> {
   // --- ALL ---
 
   protected override _getAll(): JQuery.Promise<LookupResult<TKey>> {
-    let deferred = $.Deferred();
-    setTimeout(this._queryByAll.bind(this, deferred));
-    return deferred.promise();
+    this._deferred = $.Deferred();
+    setTimeout(this._queryByAll.bind(this));
+    return this._deferred.promise();
   }
 
-  protected _queryByAll(deferred: Deferred<LookupResult<TKey>>) {
-    deferred.resolve({
+  protected _queryByAll() {
+    this._deferred.resolve({
       queryBy: QueryBy.ALL,
       lookupRows: this._lookupRowsByAll()
     });
@@ -52,13 +59,13 @@ export class PrepopulatedLookupCall<TKey> extends LookupCall<TKey> {
   // --- TEXT ---
 
   protected override _getByText(text: string): JQuery.Promise<LookupResult<TKey>> {
-    let deferred = $.Deferred();
-    setTimeout(this._queryByText.bind(this, deferred, text));
-    return deferred.promise();
+    this._deferred = $.Deferred();
+    setTimeout(this._queryByText.bind(this, text));
+    return this._deferred.promise();
   }
 
-  protected _queryByText(deferred: Deferred<LookupResult<TKey>>, text: string) {
-    deferred.resolve({
+  protected _queryByText(text: string) {
+    this._deferred.resolve({
       queryBy: QueryBy.TEXT,
       text: text,
       lookupRows: this._lookupRowsByText(text)
@@ -76,20 +83,20 @@ export class PrepopulatedLookupCall<TKey> extends LookupCall<TKey> {
   // --- KEY ---
 
   protected override _getByKey(key: TKey): JQuery.Promise<LookupResult<TKey>> {
-    let deferred = $.Deferred();
-    setTimeout(this._queryByKey.bind(this, deferred, key));
-    return deferred.promise();
+    this._deferred = $.Deferred();
+    setTimeout(this._queryByKey.bind(this, key));
+    return this._deferred.promise();
   }
 
-  protected _queryByKey(deferred: Deferred<LookupResult<TKey>>, key: TKey) {
+  protected _queryByKey(key: TKey) {
     let lookupRow = this._lookupRowByKey(key);
     if (lookupRow) {
-      deferred.resolve({
+      this._deferred.resolve({
         queryBy: QueryBy.KEY,
         lookupRows: [lookupRow]
       });
     } else {
-      deferred.reject();
+      this._deferred.reject();
     }
   }
 
@@ -100,13 +107,13 @@ export class PrepopulatedLookupCall<TKey> extends LookupCall<TKey> {
   // --- REC ---
 
   protected override _getByRec(rec: TKey): JQuery.Promise<LookupResult<TKey>> {
-    let deferred = $.Deferred();
-    setTimeout(this._queryByRec.bind(this, deferred, rec));
-    return deferred.promise();
+    this._deferred = $.Deferred();
+    setTimeout(this._queryByRec.bind(this, rec));
+    return this._deferred.promise();
   }
 
-  protected _queryByRec(deferred: Deferred<LookupResult<TKey>>, rec: TKey) {
-    deferred.resolve({
+  protected _queryByRec(rec: TKey) {
+    this._deferred.resolve({
       queryBy: QueryBy.REC,
       rec: rec,
       lookupRows: this._lookupRowsByRec(rec)

eclipse-scout-core/src/lookup/RemoteLookupCall.ts
@@ -13,7 +13,7 @@ import Deferred = JQuery.Deferred;
 
 export class RemoteLookupCall<TKey> extends LookupCall<TKey> {
   adapter: LookupFieldAdapter;
-  deferred: Deferred<LookupResult<TKey>, { canceled: boolean }> & { requestParameter?: RemoteLookupRequest<string | TKey | void> };
+  deferred: Deferred<LookupResult<TKey>, { abort: boolean }> & { requestParameter?: RemoteLookupRequest<string | TKey | void> };
 
   constructor(adapter: LookupFieldAdapter) {
     super();
@@ -78,7 +78,7 @@ export class RemoteLookupCall<TKey> extends LookupCall<TKey> {
   protected _newDeferred(requestParameter: RemoteLookupRequest<string | TKey | void>) {
     if (this.deferred) {
       this.deferred.reject({
-        canceled: true
+        abort: true
       });
     }
     this.deferred = $.Deferred();

eclipse-scout-core/src/lookup/RestLookupCall.ts
@@ -49,7 +49,7 @@ export class RestLookupCall<TKey> extends LookupCall<TKey> implements RestLookup
 
   protected _restriction: Record<string, any>;
   protected _ajaxCall: AjaxCall;
-  protected _deferred: Deferred<LookupResult<TKey>, { canceled: boolean }>;
+  protected _deferred: Deferred<LookupResult<TKey>, { abort: boolean }>;
 
   constructor() {
     super();
@@ -190,7 +190,7 @@ export class RestLookupCall<TKey> extends LookupCall<TKey> implements RestLookup
 
   override abort() {
     this._deferred.reject({
-      canceled: true
+      abort: true
     });
     this._ajaxCall.abort();
     super.abort();

eclipse-scout-core/src/lookup/StaticLookupCall.ts
@@ -9,7 +9,6 @@
  */
 import {arrays, HierarchicalLookupResultBuilder, InitModelOf, LookupCall, LookupResult, LookupRow, objects, QueryBy, scout, StaticLookupCallModel, strings} from '../index';
 import $ from 'jquery';
-import Deferred = JQuery.Deferred;
 
 /**
  * Base class for lookup calls with static or local data. Implement the _data() and _dataToLookupRow()
@@ -21,10 +20,11 @@ export class StaticLookupCall<TKey> extends LookupCall<TKey> implements StaticLo
 
   delay: number;
   data: any[];
+  protected _deferred: JQuery.Deferred<LookupResult<TKey>>;
 
   constructor() {
     super();
-
+    this._deferred = null;
     this.delay = 0;
     this.data = null;
     this.active = true;
@@ -46,14 +46,21 @@ export class StaticLookupCall<TKey> extends LookupCall<TKey> implements StaticLo
     }
   }
 
-  protected override _getAll(): JQuery.Promise<LookupResult<TKey>> {
-    let deferred = $.Deferred();
-    setTimeout(this._queryByAll.bind(this, deferred), this.delay);
-    return deferred.promise();
+  override abort() {
+    this._deferred?.reject({
+      abort: true
+    });
+    super.abort();
   }
 
-  protected _queryByAll(deferred: Deferred<LookupResult<TKey>>) {
-    deferred.resolve({
+  protected override _getAll(): JQuery.Promise<LookupResult<TKey>> {
+    this._deferred = $.Deferred();
+    setTimeout(this._queryByAll.bind(this), this.delay);
+    return this._deferred.promise();
+  }
+
+  protected _queryByAll() {
+    this._deferred.resolve({
       queryBy: QueryBy.ALL,
       lookupRows: this._lookupRowsByAll()
     });
@@ -74,17 +81,17 @@ export class StaticLookupCall<TKey> extends LookupCall<TKey> implements StaticLo
   }
 
   protected override _getByText(text: string): JQuery.Promise<LookupResult<TKey>> {
-    let deferred = $.Deferred();
-    setTimeout(this._queryByText.bind(this, deferred, text), this.delay);
-    return deferred.promise();
+    this._deferred = $.Deferred();
+    setTimeout(this._queryByText.bind(this, text), this.delay);
+    return this._deferred.promise();
   }
 
-  protected _queryByText(deferred: Deferred<LookupResult<TKey>>, text: string) {
+  protected _queryByText(text: string) {
     let lookupRows = this._lookupRowsByText(text);
 
     // resolve non-hierarchical results immediately
     if (!this.hierarchical) {
-      deferred.resolve({
+      this._deferred.resolve({
         queryBy: QueryBy.TEXT,
         text: text,
         lookupRows: lookupRows
@@ -104,7 +111,7 @@ export class StaticLookupCall<TKey> extends LookupCall<TKey> implements StaticLo
     promise
       .then(lookupRows => builder.addParentLookupRows(lookupRows))
       .done(lookupRows => {
-        deferred.resolve({
+        this._deferred.resolve({
           queryBy: QueryBy.TEXT,
           text: text,
           lookupRows: lookupRows
@@ -150,20 +157,20 @@ export class StaticLookupCall<TKey> extends LookupCall<TKey> implements StaticLo
   }
 
   protected override _getByKey(key: TKey): JQuery.Promise<LookupResult<TKey>> {
-    let deferred = $.Deferred();
-    setTimeout(this._queryByKey.bind(this, deferred, key), this.delay);
-    return deferred.promise();
+    this._deferred = $.Deferred();
+    setTimeout(this._queryByKey.bind(this, key), this.delay);
+    return this._deferred.promise();
   }
 
-  protected _queryByKey(deferred: Deferred<LookupResult<TKey>>, key: TKey) {
+  protected _queryByKey(key: TKey) {
     let lookupRow = this._lookupRowByKey(key);
     if (lookupRow) {
-      deferred.resolve({
+      this._deferred.resolve({
         queryBy: QueryBy.KEY,
         lookupRows: [lookupRow]
       });
     } else {
-      deferred.reject();
+      this._deferred.reject();
     }
   }
 
@@ -176,13 +183,13 @@ export class StaticLookupCall<TKey> extends LookupCall<TKey> implements StaticLo
   }
 
   protected override _getByRec(rec: TKey): JQuery.Promise<LookupResult<TKey>> {
-    let deferred = $.Deferred();
-    setTimeout(this._queryByRec.bind(this, deferred, rec), this.delay);
-    return deferred.promise();
+    this._deferred = $.Deferred();
+    setTimeout(this._queryByRec.bind(this, rec), this.delay);
+    return this._deferred.promise();
   }
 
-  protected _queryByRec(deferred: Deferred<LookupResult<TKey>>, rec: TKey) {
-    deferred.resolve({
+  protected _queryByRec(rec: TKey) {
+    this._deferred.resolve({
       queryBy: QueryBy.REC,
       rec: rec,
       lookupRows: this._lookupRowsByRec(rec)

eclipse-scout-core/src/testing/JasmineScout.ts
@@ -12,6 +12,7 @@ import {
 } from '../index';
 import {jasmineScoutMatchers, LocaleSpecHelper, TestingApp} from './index';
 import 'jasmine-jquery';
+import $ from 'jquery';
 
 declare global {
 
@@ -57,6 +58,8 @@ declare global {
   function uninstallUnloadHandlers(session: Session);
 
   function createPropertyChangeEvent(model: { id: string }, properties: object);
+
+  function sleep(duration?: number): JQuery.Promise<void>;
 }
 
 export interface SandboxSessionOptions {
@@ -222,6 +225,12 @@ window.createPropertyChangeEvent = (model, properties) => ({
   properties: properties,
   type: 'property'
 });
+
+window.sleep = duration => {
+  let deferred = $.Deferred();
+  setTimeout(() => deferred.resolve(), duration);
+  return deferred.promise();
+};
 
 export const JasmineScout = {
   runTestSuite(context) {

commit eebf79eccf5104b38c76dc6d9bc1c29f2db63475

SmartField: don't remove text on consecutive setValue calls
DisplayText is empty if lookup call of second setValue call is very fast
and returns before the catch handler is executed.
Bug was introduced with commit acf6c036293ff2ebb4a3807b9a72713f8bba0d85.

351825

eclipse-scout-core/src/form/fields/smartfield/SmartField.ts
@@ -697,7 +697,7 @@ export class SmartField<TValue> extends ValueField<TValue> implements SmartField
         $.log.isDebugEnabled() && $.log.debug('FormatValue failed for SmartField with id ' + this.id, error);
         if (!this._updateDisplayTextPending) {
           // If a new value was set in the meantime, the previous lookup call was aborted -> don't mark the field as invalid because only the new lookup matters
-          return '';
+          throw error;
         }
         if (error && error.abort || error instanceof AjaxError && error.textStatus === 'abort') {
           // If a new value was set in the meantime but the display text of the new value not resolved yet, ignore the abort event of the first lookup to not show invalid key message while the second lookup is still in progress.

eclipse-scout-core/test/form/fields/smartfield/SmartFieldSpec.ts
@@ -656,11 +656,6 @@ describe('SmartField', () => {
       await field.when('propertyChange:displayText');
       expect(field.errorStatus).toBe(null);
       expect(field.value).toBe(2);
-      expect(field.displayText).toBe(''); // Set to empty string by catch in _formatValue first
-
-      await field.when('propertyChange:displayText');
-      expect(field.errorStatus).toBe(null);
-      expect(field.value).toBe(2);
       expect(field.displayText).toBe('Bar');
     });
 