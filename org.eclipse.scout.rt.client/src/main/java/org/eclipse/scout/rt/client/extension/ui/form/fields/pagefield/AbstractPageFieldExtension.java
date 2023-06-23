7440803221817488e751e896e3033165a36fe244
Adrian Egloff <adrian.egloff@bsi-software.com>
2023-06-23 11:48:25 +0200

    Add default method execPageChanged for AbstractPageFieldExtension

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/extension/ui/form/fields/pagefield/AbstractPageFieldExtension.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/extension/ui/form/fields/pagefield/AbstractPageFieldExtension.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2010-2015 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2023 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -11,6 +11,7 @@
 package org.eclipse.scout.rt.client.extension.ui.form.fields.pagefield;
 
 import org.eclipse.scout.rt.client.extension.ui.form.fields.groupbox.AbstractGroupBoxExtension;
+import org.eclipse.scout.rt.client.extension.ui.form.fields.pagefield.PageFieldChains.PageFieldPageChangedChain;
 import org.eclipse.scout.rt.client.ui.desktop.outline.pages.IPage;
 import org.eclipse.scout.rt.client.ui.form.fields.pagefield.AbstractPageField;
 
@@ -18,5 +19,10 @@ public abstract class AbstractPageFieldExtension<T extends IPage, OWNER extends 
 
   public AbstractPageFieldExtension(OWNER owner) {
     super(owner);
+  }
+
+  @Override
+  public void execPageChanged(PageFieldPageChangedChain<T> chain, T oldPage, T newPage) {
+    chain.execPageChanged(oldPage, newPage);
   }
 }
