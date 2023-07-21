189ffd5dbf322d53b82d7672a66d6e139f8b192e
Bruno Koeferli <Bruno.Koeferli@bsi-software.com>
2023-07-21 13:37:30 +0200

    Add basic OpenTelemetry integration for signal metrics
    The Scout OpenTelemetry integration uses OpenTelemetry SDK Autoconfigure
    for initialization.
    
    The other signals traces and logs are currently not tested and therefore
    not activated/exported by default.
    
    353620

---
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/opentelemetry/IMetricProvider.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.scout.rt.platform.opentelemetry;
+
+import org.eclipse.scout.rt.platform.ApplicationScoped;
+
+import io.opentelemetry.api.OpenTelemetry;
+
+/**
+ * A provider of one or more application metrics based on OpenTelemetry.
+ * <p>
+ * A metric provider is usually used for more generally or feature-independent metrics such as JVM/cpu metrics. It can
+ * also be used for metrics whose source code is not under your control, e.g. external libraries.
+ * </p>
+ * <p>
+ * Such metric providers are responsible to set up the corresponding metric(s) and also to properly close/free used
+ * resources on Scout platform shutdown.
+ * <p>
+ * A metric provider usually interacts with a {@link OpenTelemetry} instance.
+ *
+ * @see OpenTelemetry
+ */
+@ApplicationScoped
+public interface IMetricProvider {
+
+  /**
+   * Register the metrics to the given {@link OpenTelemetry}.
+   * <p>
+   * This method is usually called once at Scout platform startup.
+   *
+   * @see OpenTelemetry#getMeter(String)
+   */
+  void register(OpenTelemetry openTelemetry);
+
+  /**
+   * Close/free used resources.
+   */
+  void close();
+}
