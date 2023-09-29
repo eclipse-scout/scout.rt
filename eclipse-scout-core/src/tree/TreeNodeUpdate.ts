dacb1bfcd5c026068f6c1d5f05c995aff4f0e9a7
Niklas Sidler <niklas.sidler@gmail.com>
2023-09-29 17:51:23 +0200

    Add event emmiting of tree

---
+++ eclipse-scout-core/src/tree/TreeNodeUpdate.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+import {TreeNode} from './TreeNode';
+
+/**
+ * This object is needed during an update in the tree. It
+ * holds the updated nodes to be processed at the end of the update.
+ *
+ * This class holds two lists of {@link TreeNode}:
+ * - TreeNodes which have to be rendered
+ * - TreeNodes which triggered an update event
+ */
+export class TreeNodeUpdate {
+  protected _requireRenderTreeNodes: TreeNode[];
+  protected _requireTriggerEventNodes: TreeNode[];
+
+  constructor() {
+    this._requireRenderTreeNodes = [];
+    this._requireTriggerEventNodes = [];
+  }
+
+  addNodeForRendering(...nodes: TreeNode[]) {
+    this._requireRenderTreeNodes.push(...nodes);
+  }
+
+  addNodeForEventTrigger(...nodes: TreeNode[]) {
+    this._requireTriggerEventNodes.push(...nodes);
+  }
+
+  addNodeForRenderingAndEventTrigger(...nodes: TreeNode[]) {
+    this.addNodeForRendering(...nodes);
+    this.addNodeForEventTrigger(...nodes);
+  }
+
+  getNodesForRendering(): TreeNode[] {
+    return this._requireRenderTreeNodes;
+  }
+
+  getNodesForEventTrigger(): TreeNode[] {
+    return this._requireTriggerEventNodes;
+  }
+
+  add(treeNodeUpdate: TreeNodeUpdate) {
+    this.addNodeForRendering(...treeNodeUpdate.getNodesForRendering());
+    this.addNodeForEventTrigger(...treeNodeUpdate.getNodesForEventTrigger());
+  }
+}
