a8943fbfd2f85521481db0bc50c4a82bdacc25aa
Beat Schwarzentrub <bsh@bsiag.com>
2024-01-09 14:22:49 +0100

    Table: discarded rows should not be added to the decoration buffer
    discardRows() uses deleteRows() internally, but additionally sets the
    table status to INSERTED, so the rows will be discarded instead of just
    marked as deleted. Calling setStatus() on an InternalTableRow causes
    the row to be updated. If the table is currently "changing" this update
    is added to the "decoration buffer". When this buffer is finally
    processed, the column structure might not match the row data anymore,
    which can cause errors in execDecorateCell(). To fix this, use the
    property "autoDiscardOnDelete" to discard rows. It achieves the same
    result but does not cause the row to be updated unnecessarily.
    
    360881

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/basic/table/AbstractTable.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/basic/table/AbstractTable.java
@@ -3539,15 +3539,15 @@ public abstract class AbstractTable extends AbstractWidget implements ITable, IC
    */
   @Override
   public void discardRows(Collection<? extends ITableRow> rows) {
+    boolean oldAutoDiscardOnDelete = isAutoDiscardOnDelete();
     try {
       setTableChanging(true);
+      setAutoDiscardOnDelete(true);
       //
-      for (ITableRow row : rows) {
-        row.setStatus(ITableRow.STATUS_INSERTED);
-      }
       deleteRows(rows);
     }
     finally {
+      setAutoDiscardOnDelete(oldAutoDiscardOnDelete);
       setTableChanging(false);
     }
   }
@@ -4052,6 +4052,10 @@ public abstract class AbstractTable extends AbstractWidget implements ITable, IC
   }
 
   private void applyRowValueChanges(Map<Integer, Set<ITableRow>> changes) {
+    // performance quick-check
+    if (changes.isEmpty()) {
+      return;
+    }
     try {
       for (ITableRow tableRow : getRows()) {
         tableRow.setRowChanging(true);
@@ -4074,6 +4078,10 @@ public abstract class AbstractTable extends AbstractWidget implements ITable, IC
 
   @SuppressWarnings("unchecked")
   private void applyRowDecorations(Set<ITableRow> rows) {
+    // performance quick-check
+    if (rows.isEmpty()) {
+      return;
+    }
     try {
       for (ITableRow tableRow : rows) {
         tableRow.setRowChanging(true);
