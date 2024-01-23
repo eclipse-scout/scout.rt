6dca1a59b0ac4b4d74aad5ee39ca4a8f4fce2500
Bruno Koeferli <Bruno.Koeferli@bsi-software.com>
2024-01-23 17:57:26 +0100

    Update OpenTelemetry library version to 1.34.1

--- org.eclipse.scout.rt.opentelemetry.sdk/src/main/java/org/eclipse/scout/rt/opentelemetry/sdk/metrics/JvmMetricProvider.java
+++ org.eclipse.scout.rt.opentelemetry.sdk/src/main/java/org/eclipse/scout/rt/opentelemetry/sdk/metrics/JvmMetricProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -17,7 +17,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.instrumentation.runtimemetrics.java8.BufferPools;
 import io.opentelemetry.instrumentation.runtimemetrics.java8.Classes;
 import io.opentelemetry.instrumentation.runtimemetrics.java8.Cpu;
 import io.opentelemetry.instrumentation.runtimemetrics.java8.GarbageCollector;
@@ -35,12 +34,11 @@ public class JvmMetricProvider implements IMetricProvider {
 
   private static final Logger LOG = LoggerFactory.getLogger(JvmMetricProvider.class);
 
-  private List<AutoCloseable> m_observables = new ArrayList<>();
+  private final List<AutoCloseable> m_observables = new ArrayList<>();
 
   @Override
   public void register(OpenTelemetry openTelemetry) {
     // see https://github.com/open-telemetry/opentelemetry-java-instrumentation/tree/main/instrumentation/runtime-telemetry/runtime-telemetry-java8/library#usage
-    m_observables.addAll(BufferPools.registerObservers(openTelemetry));
     m_observables.addAll(Classes.registerObservers(openTelemetry));
     m_observables.addAll(Cpu.registerObservers(openTelemetry));
     m_observables.addAll(MemoryPools.registerObservers(openTelemetry));
