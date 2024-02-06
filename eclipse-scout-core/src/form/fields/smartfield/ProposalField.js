e18e40bb27f085d64de887332e74d49c62e876c2
Fritz Schinkel <fritz.schinkel@bsi-software.com>
2024-02-06 17:41:09 +0100

    ProposalField: fix custom text in touch mode while error status present
    Problem: If there is an error status present on the proposal field it
    will be mirrored to the field in the touch popup. When the value of the
    field in the touch popup changes, its error status is cleared. If this
    value change happens because a custom text was entered, the error status
    of the main proposal field will not be cleared. The SmartFieldAdapter
    will not send the value to the server if there is an error status
    present on the field. Therefore, the value in the model on the server
    never changes.
    
    This problem only occurs in the touch mode when entering a custom text.
    When a lookup row is selected, the error status will always be cleared
    regardless of the touch mode. If a custom text is entered in the desktop
    mode, the error status will also be cleared.
    
    Therefore, add the error status to the properties that are copied from
    the touch popups field to the main proposal field. This leads to the
    error status being cleared for all different ways the proposal fields
    value can be changed and therefore, the new value will be sent to the
    server, where it is updated in the model.
    
    369873

--- eclipse-scout-core/src/form/fields/smartfield/ProposalField.js
+++ eclipse-scout-core/src/form/fields/smartfield/ProposalField.js
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2014-2018 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2024 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -163,6 +163,7 @@ export default class ProposalField extends SmartField {
     if (this.lookupRow !== otherField.lookupRow) {
       this.setLookupRow(otherField.lookupRow);
     }
+    this.setErrorStatus(otherField.errorStatus);
     if (this.value !== otherField.value) {
       this.setValue(otherField.value);
     }
