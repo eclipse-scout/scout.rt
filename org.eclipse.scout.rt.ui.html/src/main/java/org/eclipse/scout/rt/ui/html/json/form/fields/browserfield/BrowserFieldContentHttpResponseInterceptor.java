493f6cc831756c243caaeedd00433e5bc2b358d3
Beat Schwarzentrub <bsh@bsiag.com>
2024-02-08 16:08:14 +0100

    BrowserField: fix CSP report-uri
    If a browser field is used to display a document, the report-uri cannot
    be relative, because the dynamic URL has a different base than the
    application. Instead, use the browserUri from the client session to
    create an absolute URL.
    
    290376

--- org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/form/fields/browserfield/BrowserFieldContentHttpResponseInterceptor.java
+++ org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/form/fields/browserfield/BrowserFieldContentHttpResponseInterceptor.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.scout.rt.platform.BEANS;
+import org.eclipse.scout.rt.platform.util.UriUtility;
 import org.eclipse.scout.rt.server.commons.servlet.ContentSecurityPolicy;
 import org.eclipse.scout.rt.server.commons.servlet.HttpClientInfo;
 import org.eclipse.scout.rt.server.commons.servlet.HttpServletControl;
@@ -34,15 +35,22 @@ public class BrowserFieldContentHttpResponseInterceptor implements IHttpResponse
   public void intercept(HttpServletRequest req, HttpServletResponse resp) {
     ContentSecurityPolicy csp = BEANS.get(ContentSecurityPolicy.class).appendScriptSrc("'unsafe-inline'");
 
-    // Bug in Chrome: CSP 'self' is not interpreted correctly in sandboxed iframes, see https://bugs.chromium.org/p/chromium/issues/detail?id=443444
-    // Workaround: Add resolved URI to image and style CSP directive to allow loading of images and styles from same origin as nested iframe in browser field
     HttpClientInfo httpClientInfo = HttpClientInfo.get(req);
-    if (httpClientInfo.isWebkit()) {
-      String resolvedSelfUri = m_browserUri.toString();
-      csp
-          .appendImgSrc(resolvedSelfUri)
-          .appendStyleSrc(resolvedSelfUri);
+    String baseUri = UriUtility.toBaseUri(m_browserUri);
+
+    if (baseUri != null) {
+      // Normally, the csp report url is relative. Because documents inside the browser field are
+      // loaded from a "/dynamic/..." URL, the relative url has to be converted to an absolute url.
+      csp.withReportUri(baseUri + HttpServletControl.CSP_REPORT_URL);
+
+      // Bug in Chrome: CSP 'self' is not interpreted correctly in sandboxed iframes, see https://bugs.chromium.org/p/chromium/issues/detail?id=443444
+      // Workaround: Add resolved URI to image and style CSP directive to allow loading of images and styles from same origin as nested iframe in browser field
+      if (httpClientInfo.isWebkit()) {
+        csp.appendImgSrc(baseUri);
+        csp.appendStyleSrc(baseUri);
+      }
     }
+
     String cspToken = csp.toToken();
     if (httpClientInfo.isMshtml()) {
       resp.setHeader(HttpServletControl.HTTP_HEADER_CSP_LEGACY, cspToken);
