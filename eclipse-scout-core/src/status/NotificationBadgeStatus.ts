d49074d668e6c01489c0bd77c1ee67b0aa3e1e33
Matthias Villiger <mvi@bsi-software.com>
2024-03-01 10:09:44 +0100

    Consistently use @returns in jsdoc

--- eclipse-scout-core/src/status/NotificationBadgeStatus.ts
+++ eclipse-scout-core/src/status/NotificationBadgeStatus.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -21,7 +21,7 @@ export class NotificationBadgeStatus extends Status {
   }
 
   /**
-   * @return {NotificationBadgeStatus} a clone of this Status instance.
+   * @returns {NotificationBadgeStatus} a clone of this Status instance.
    */
   override clone(): Status {
     let modelClone = $.extend({}, this);
