c70bd6646baa5f35f0948aa421925a27e6bc60bf
Paolo Bazzi <paolo.bazzi@bsi-software.com>
2024-03-06 09:52:45 +0100

    JacksonDataObjectMapper: Allow read/write constraints config
    Jackson's object mapper enforces some read/write constraints since
    release 2.15. The maximum length for strings was limited to 20mb as
    default. Since Scout dataobject mapper may be used with base64-encoded
    binary data occasionally, 20mb may not be sufficient for each case.
    This fix increases the default limit to 100mb and adds support to
    modify all constraints using config properties.
    
    See
    https://github.com/eclipse-ee4j/jersey/issues/5283
    https://github.com/FasterXML/jackson-core/issues/962
    https://github.com/FasterXML/jackson-core/pull/964
    
    376418

--- org.eclipse.scout.rt.jackson/src/main/java/org/eclipse/scout/rt/jackson/dataobject/JacksonDataObjectMapper.java
+++ org.eclipse.scout.rt.jackson/src/main/java/org/eclipse/scout/rt/jackson/dataobject/JacksonDataObjectMapper.java
@@ -13,6 +13,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.scout.rt.dataobject.IDataObject;
 import org.eclipse.scout.rt.dataobject.IDataObjectMapper;
@@ -20,11 +25,20 @@ import org.eclipse.scout.rt.dataobject.IDoEntity;
 import org.eclipse.scout.rt.dataobject.IValueFormatConstants;
 import org.eclipse.scout.rt.platform.ApplicationScoped;
 import org.eclipse.scout.rt.platform.BEANS;
+import org.eclipse.scout.rt.platform.config.AbstractConfigProperty;
+import org.eclipse.scout.rt.platform.config.CONFIG;
+import org.eclipse.scout.rt.platform.config.ConfigUtility;
+import org.eclipse.scout.rt.platform.exception.PlatformException;
 import org.eclipse.scout.rt.platform.exception.PlatformExceptionTranslator;
 import org.eclipse.scout.rt.platform.util.Assertions;
 import org.eclipse.scout.rt.platform.util.LazyValue;
+import org.eclipse.scout.rt.platform.util.ObjectUtility;
+import org.eclipse.scout.rt.platform.util.TypeCastUtility;
 
+import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.StreamReadConstraints;
+import com.fasterxml.jackson.core.StreamWriteConstraints;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -120,6 +134,7 @@ public class JacksonDataObjectMapper implements IDataObjectMapper {
    * @deprecated Use Scout data object mapper instead, see BEANS.get(IDataObjectMapper.class)
    */
   @Deprecated
+  @SuppressWarnings("DeprecatedIsStillUsed")
   public ObjectMapper getObjectMapper() {
     return m_objectMapper.get();
   }
@@ -128,7 +143,12 @@ public class JacksonDataObjectMapper implements IDataObjectMapper {
    * Creates new {@link ObjectMapper} instance configured to be used with {@link IDoEntity}.
    */
   protected ObjectMapper createObjectMapperInstance(boolean ignoreTypeAttribute) {
-    ObjectMapper om = new ObjectMapper();
+    // setup custom-configured JsonFactory used for ObjectMapper
+    JsonFactory jsonFactory = JsonFactory.builder()
+        .streamReadConstraints(CONFIG.getPropertyValue(StreamReadConstraintsConfigProperty.class))
+        .streamWriteConstraints(CONFIG.getPropertyValue(StreamWriteConstraintsConfigProperty.class))
+        .build();
+    ObjectMapper om = new ObjectMapper(jsonFactory);
     ScoutDataObjectModule scoutDataObjectModule = BEANS.get(ScoutDataObjectModule.class).withIgnoreTypeAttribute(ignoreTypeAttribute);
     prepareScoutDataModuleContext(scoutDataObjectModule.getModuleContext());
     om.registerModule(scoutDataObjectModule);
@@ -144,5 +164,113 @@ public class JacksonDataObjectMapper implements IDataObjectMapper {
    */
   protected void prepareScoutDataModuleContext(ScoutDataObjectModuleContext moduleContext) {
     moduleContext.withDataObjectMapperClass(getClass());
+  }
+
+  /**
+   * {@link StreamReadConstraints} for {@link JsonFactory}.
+   */
+  public static class StreamReadConstraintsConfigProperty extends AbstractConfigProperty<StreamReadConstraints, Map<String, String>> {
+
+    static final String MAX_NESTING_DEPTH = "maxNestingDepth";
+    static final String MAX_DOCUMENT_LENGTH = "maxDocumentLength";
+    static final String MAX_NAME_LENGTH = "maxNameLength";
+    static final String MAX_NUMBER_LENGTH = "maxNumberLength";
+    static final String MAX_STRING_LENGTH = "maxStringLength";
+
+    /**
+     * Default setting for maximum string length is 100 MB.<br>
+     * Jackson default value is {@link StreamReadConstraints#DEFAULT_MAX_STRING_LEN}.
+     */
+    public static final int DEFAULT_MAX_STRING_LEN = 100_000_000;
+
+    @Override
+    public String getKey() {
+      return "scout.dataobject.jackson.streamReadConstraints";
+    }
+
+    @Override
+    public String description() {
+      return String.format("Jackson constraints to use for JSON reading.\n"
+              + "Map property with the keys as follows:\n"
+              + "- %s: Sets the maximum nesting depth. The depth is a count of objects and arrays that have not been closed, `{` and `[` respectively. (default: %d)\n"
+              + "- %s: Sets the maximum allowed document length (for positive values over 0) or indicate that any length is acceptable (0 or negative number). The length is in input units of the input source, that is, in bytes or chars. (default: %d)\n"
+              + "- %s: Sets the maximum name length (in chars or bytes, depending on input context). (default: %d)\n"
+              + "- %s: Sets the maximum number length (in chars or bytes, depending on input context). (default: %d)\n"
+              + "- %s: Sets the maximum string length for a single attribute value of type text (in chars or bytes, depending on input context). (default: %d)\n",
+          MAX_NESTING_DEPTH, StreamReadConstraints.DEFAULT_MAX_DEPTH,
+          MAX_DOCUMENT_LENGTH, StreamReadConstraints.DEFAULT_MAX_DOC_LEN,
+          MAX_NAME_LENGTH, StreamReadConstraints.DEFAULT_MAX_NAME_LEN,
+          MAX_NUMBER_LENGTH, StreamReadConstraints.DEFAULT_MAX_NUM_LEN,
+          MAX_STRING_LENGTH, DEFAULT_MAX_STRING_LEN);
+    }
+
+    @Override
+    public Map<String, String> readFromSource(String namespace) {
+      return ConfigUtility.getPropertyMap(getKey(), null, namespace);
+    }
+
+    @Override
+    public StreamReadConstraints getDefaultValue() {
+      return parse(Collections.emptyMap()); // defaults are on a per key base
+    }
+
+    @Override
+    protected StreamReadConstraints parse(Map<String, String> value) {
+      Set<String> invalidMapKeys = new HashSet<>(value.keySet());
+      Arrays.asList(MAX_NESTING_DEPTH, MAX_DOCUMENT_LENGTH, MAX_NAME_LENGTH, MAX_NUMBER_LENGTH, MAX_STRING_LENGTH).forEach(invalidMapKeys::remove);
+      if (!invalidMapKeys.isEmpty()) {
+        throw new PlatformException("Invalid values for map property {}: {}", getKey(), invalidMapKeys);
+      }
+      return StreamReadConstraints.builder()
+          .maxNestingDepth(ObjectUtility.nvl(TypeCastUtility.castValue(value.get(MAX_NESTING_DEPTH), Integer.class), StreamReadConstraints.DEFAULT_MAX_DEPTH))
+          .maxDocumentLength(ObjectUtility.nvl(TypeCastUtility.castValue(value.get(MAX_DOCUMENT_LENGTH), Long.class), StreamReadConstraints.DEFAULT_MAX_DOC_LEN))
+          .maxNameLength(ObjectUtility.nvl(TypeCastUtility.castValue(value.get(MAX_NAME_LENGTH), Integer.class), StreamReadConstraints.DEFAULT_MAX_NAME_LEN))
+          .maxNumberLength(ObjectUtility.nvl(TypeCastUtility.castValue(value.get(MAX_NUMBER_LENGTH), Integer.class), StreamReadConstraints.DEFAULT_MAX_NUM_LEN))
+          .maxStringLength(ObjectUtility.nvl(TypeCastUtility.castValue(value.get(MAX_STRING_LENGTH), Integer.class), DEFAULT_MAX_STRING_LEN))
+          .build();
+    }
+  }
+
+  /**
+   * {@link StreamReadConstraints} for {@link JsonFactory}.
+   */
+  public static class StreamWriteConstraintsConfigProperty extends AbstractConfigProperty<StreamWriteConstraints, Map<String, String>> {
+
+    private static final String MAX_NESTING_DEPTH = "maxNestingDepth";
+
+    @Override
+    public String getKey() {
+      return "scout.dataobject.jackson.streamWriteConstraints";
+    }
+
+    @Override
+    public String description() {
+      return String.format("Jackson constraints to use for JSON writing.\n"
+              + "Map property with the keys as follows:\n"
+              + "- %s: Sets the maximum nesting depth. The depth is a count of objects and arrays that have not been closed, `{` and `[` respectively. (default: %d)\n",
+          MAX_NESTING_DEPTH, StreamWriteConstraints.DEFAULT_MAX_DEPTH);
+    }
+
+    @Override
+    public Map<String, String> readFromSource(String namespace) {
+      return ConfigUtility.getPropertyMap(getKey(), null, namespace);
+    }
+
+    @Override
+    public StreamWriteConstraints getDefaultValue() {
+      return parse(Collections.emptyMap()); // defaults are on a per key base
+    }
+
+    @Override
+    protected StreamWriteConstraints parse(Map<String, String> value) {
+      Set<String> invalidMapKeys = new HashSet<>(value.keySet());
+      Arrays.asList(MAX_NESTING_DEPTH).forEach(invalidMapKeys::remove);
+      if (!invalidMapKeys.isEmpty()) {
+        throw new PlatformException("Invalid values for map property {}: {}", getKey(), invalidMapKeys);
+      }
+      return StreamWriteConstraints.builder()
+          .maxNestingDepth(ObjectUtility.nvl(TypeCastUtility.castValue(value.get(MAX_NESTING_DEPTH), Integer.class), StreamWriteConstraints.DEFAULT_MAX_DEPTH))
+          .build();
+    }
   }
 }
