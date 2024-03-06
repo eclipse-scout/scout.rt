158dadaad91c49134ee98d9c462579e8d99ece06
Paolo Bazzi <paolo.bazzi@bsi-software.com>
2024-03-06 09:52:45 +0100

    JacksonDataObjectMapper: Allow read/write constraints config
    Jackson's object mapper enforces some read/write constraints since
    release 2.15. The maximum length for strings was limited to 20mb as
    default. Since Scout dataobject mapper may be used with base64-encoded
    binary data occasionally, 20mb may not be sufficient for each case.
    This fix increases the default limit to 100mb and adds support to
    modify all constraints using config properties.
    
    See
    https://github.com/eclipse-ee4j/jersey/issues/5283
    https://github.com/FasterXML/jackson-core/issues/962
    https://github.com/FasterXML/jackson-core/pull/964
    
    376418

--- org.eclipse.scout.rt.jackson.test/src/test/java/org/eclipse/scout/rt/jackson/dataobject/JacksonDataObjectMapperTest.java
+++ org.eclipse.scout.rt.jackson.test/src/test/java/org/eclipse/scout/rt/jackson/dataobject/JacksonDataObjectMapperTest.java
@@ -17,17 +17,26 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.UUID;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.eclipse.scout.rt.dataobject.DataObjectHelper;
 import org.eclipse.scout.rt.dataobject.DoEntity;
+import org.eclipse.scout.rt.dataobject.DoEntityBuilder;
 import org.eclipse.scout.rt.dataobject.DoEntityHolder;
 import org.eclipse.scout.rt.dataobject.IDataObjectMapper;
+import org.eclipse.scout.rt.dataobject.IDoEntity;
 import org.eclipse.scout.rt.dataobject.testing.TestingDataObjectHelper;
+import org.eclipse.scout.rt.jackson.dataobject.JacksonDataObjectMapper.StreamReadConstraintsConfigProperty;
+import org.eclipse.scout.rt.jackson.dataobject.JacksonDataObjectMapper.StreamWriteConstraintsConfigProperty;
 import org.eclipse.scout.rt.jackson.dataobject.fixture.ITestBaseEntityDo;
 import org.eclipse.scout.rt.jackson.dataobject.fixture.TestComplexEntityDo;
 import org.eclipse.scout.rt.jackson.dataobject.fixture.TestCustomImplementedEntityDo;
@@ -40,11 +49,15 @@ import org.eclipse.scout.rt.platform.Platform;
 import org.eclipse.scout.rt.platform.exception.PlatformException;
 import org.eclipse.scout.rt.platform.util.Assertions.AssertionException;
 import org.eclipse.scout.rt.platform.util.CloneUtility;
+import org.eclipse.scout.rt.platform.util.CollectionUtility;
 import org.eclipse.scout.rt.testing.platform.BeanTestingHelper;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.StreamReadConstraints;
+import com.fasterxml.jackson.core.StreamReadConstraints.Builder;
+import com.fasterxml.jackson.core.StreamWriteConstraints;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 
 /**
@@ -209,5 +222,183 @@ public class JacksonDataObjectMapperTest {
     assertNull(m_mapper.getObjectMapper().getDeserializationConfig().getDefaultTyper(null));
     assertNull(m_mapper.getObjectMapper().getDeserializationConfig().getDefaultTyper(TypeFactory.defaultInstance().constructType(DoEntity.class)));
     assertNull(m_mapper.getObjectMapper().getDeserializationConfig().getDefaultTyper(TypeFactory.defaultInstance().constructType(Object.class)));
+  }
+
+  protected final String m_longStringValue = IntStream.range(1, 10_0000).mapToObj(Integer::toString).collect(Collectors.joining());
+
+  /**
+   * NOTE: This test case just covers the current behavior of Jackson. This behavior is not enforced or verified by the
+   * Scout data object mapper and may change in a future release of Scout using a newer version of Jackson.
+   */
+  @Test
+  public void testStreamReadConstraints_maxStringLength() {
+    // Jackson does not check string length for very short input streams
+    assertEquals("12345", runTestStreamReadConstraints(b -> b.maxStringLength(3), "{\"attribute\" : \"12345\"}").get("attribute"));
+
+    // Jackson checks string length correctly only for longer input streams
+    assertThrows(PlatformException.class, () -> runTestStreamReadConstraints(b -> b.maxStringLength(3), "{\"attribute\" : \"" + m_longStringValue + "\"}"));
+  }
+
+  /**
+   * NOTE: This test case just covers the current behavior of Jackson. This behavior is not enforced or verified by the
+   * Scout data object mapper and may change in a future release of Scout using a newer version of Jackson.
+   */
+  @Test
+  public void testStreamReadConstraints_maxDocumentLength() {
+    // Jackson does not check document length for very short input streams
+    assertEquals("12345", runTestStreamReadConstraints(b -> b.maxDocumentLength(3), "{\"attribute\" : \"12345\"}").get("attribute"));
+
+    // Jackson checks document length correctly only for longer input streams
+    assertThrows(PlatformException.class, () -> runTestStreamReadConstraints(b -> b.maxDocumentLength(3), "{\"attribute\" : \"" + m_longStringValue + "\"}"));
+  }
+
+  /**
+   * NOTE: This test case just covers the current behavior of Jackson. This behavior is not enforced or verified by the
+   * Scout data object mapper and may change in a future release of Scout using a newer version of Jackson.
+   */
+  @Test
+  public void testStreamReadConstraints_maxNumberLength() {
+    assertThrows(PlatformException.class, () -> runTestStreamReadConstraints(b -> b.maxNumberLength(3), "{\"attribute\" : 1234}"));
+  }
+
+  /**
+   * NOTE: This test case just covers the current behavior of Jackson. This behavior is not enforced or verified by the
+   * Scout data object mapper and may change in a future release of Scout using a newer version of Jackson.
+   */
+  @Test
+  public void testStreamReadConstraints_maxNameLength() {
+    assertThrows(PlatformException.class, () -> runTestStreamReadConstraints(b -> b.maxNameLength(8), "{\"attribute\": 1234}"));
+  }
+
+  /**
+   * NOTE: This test case just covers the current behavior of Jackson. This behavior is not enforced or verified by the
+   * Scout data object mapper and may change in a future release of Scout using a newer version of Jackson.
+   */
+  @Test
+  public void testStreamReadConstraints_maxNestingDepth() {
+    assertThrows(PlatformException.class, () -> runTestStreamReadConstraints(b -> b.maxNestingDepth(1), "{\"attribute\" : []}"));
+  }
+
+  /**
+   * NOTE: This test case just covers the current behavior of Jackson. This behavior is not enforced or verified by the
+   * Scout data object mapper and may change in a future release of Scout using a newer version of Jackson.
+   */
+  @Test
+  public void testStreamReadConstraints_maxBigIntScale() {
+    // BigDecimal scale (100001) magnitude exceeds the maximum allowed (100000)
+    BigDecimal value = new BigDecimal("1").setScale(100_001, RoundingMode.UNNECESSARY);
+
+    // reading a big integer attribute which is given as very large decimal value in JSON
+    // See com.fasterxml.jackson.core.StreamReadConstraints#validateBigIntegerScale for fixed scale limit of 100k
+    String json = m_mapper.writeValue(BEANS.get(DoEntityBuilder.class).put("bigIntegerAttribute", value).build());
+    assertThrows(PlatformException.class, () -> runTestStreamReadConstraints(b -> b.maxNumberLength(100_002), json, TestComplexEntityDo.class));
+  }
+
+  protected DoEntity runTestStreamReadConstraints(Consumer<Builder> builderConsumer, String json) {
+    return runTestStreamReadConstraints(builderConsumer, json, DoEntity.class);
+  }
+
+  protected <T> T runTestStreamReadConstraints(Consumer<Builder> builderConsumer, String json, Class<T> expectedClass) {
+    Builder builder = StreamReadConstraints.builder();
+    builderConsumer.accept(builder);
+    IBean bean = BeanTestingHelper.get().mockConfigProperty(StreamReadConstraintsConfigProperty.class, builder.build());
+    try {
+      JacksonDataObjectMapper mapper = new JacksonDataObjectMapper(); // force new instance to apply config change
+      return mapper.readValue(json, expectedClass);
+    }
+    finally {
+      BeanTestingHelper.get().unregisterBean(bean);
+    }
+  }
+
+  @Test
+  public void testParseStreamReadConstraintsProperty_defaultValues() {
+    StreamReadConstraints constraints = BEANS.get(StreamReadConstraintsConfigProperty.class).parse(CollectionUtility.emptyHashMap());
+    assertEquals(1000, constraints.getMaxNestingDepth());
+    assertEquals(-1, constraints.getMaxDocumentLength());
+    assertEquals(50_000, constraints.getMaxNameLength());
+    assertEquals(1000, constraints.getMaxNumberLength());
+    assertEquals(100_000_000, constraints.getMaxStringLength());
+  }
+
+  @Test
+  public void testParseStreamReadConstraintsProperty_invalidKey() {
+    assertThrows(PlatformException.class, () -> BEANS.get(StreamReadConstraintsConfigProperty.class).parse(Map.of("foo", "bar")));
+  }
+
+  @Test
+  public void testParseStreamReadConstraintsProperty_values() {
+    StreamReadConstraints constraints = BEANS.get(StreamReadConstraintsConfigProperty.class).parse(Map.of(
+        StreamReadConstraintsConfigProperty.MAX_NESTING_DEPTH, "1",
+        StreamReadConstraintsConfigProperty.MAX_DOCUMENT_LENGTH, "2",
+        StreamReadConstraintsConfigProperty.MAX_NAME_LENGTH, "3",
+        StreamReadConstraintsConfigProperty.MAX_NUMBER_LENGTH, "4",
+        StreamReadConstraintsConfigProperty.MAX_STRING_LENGTH, "5"));
+
+    assertEquals(1, constraints.getMaxNestingDepth());
+    assertEquals(2, constraints.getMaxDocumentLength());
+    assertEquals(3, constraints.getMaxNameLength());
+    assertEquals(4, constraints.getMaxNumberLength());
+    assertEquals(5, constraints.getMaxStringLength());
+  }
+
+  @Test
+  public void testParseStreamReadConstraintsProperty_incompleteValues() {
+    StreamReadConstraints constraints = BEANS.get(StreamReadConstraintsConfigProperty.class).parse(Map.of(
+        StreamReadConstraintsConfigProperty.MAX_NESTING_DEPTH, "1",
+        StreamReadConstraintsConfigProperty.MAX_DOCUMENT_LENGTH, "2",
+        StreamReadConstraintsConfigProperty.MAX_NAME_LENGTH, "3"));
+
+    assertEquals(1, constraints.getMaxNestingDepth());
+    assertEquals(2, constraints.getMaxDocumentLength());
+    assertEquals(3, constraints.getMaxNameLength());
+    assertEquals(1000, constraints.getMaxNumberLength());
+    assertEquals(100_000_000, constraints.getMaxStringLength());
+  }
+
+  /**
+   * NOTE: This test case just covers the current behavior of Jackson. This behavior is not enforced or verified by the
+   * Scout data object mapper and may change in a future release of Scout using a newer version of Jackson.
+   */
+  @Test
+  public void testStreamWriteConstraints_maxNestingDepth() {
+    IDoEntity entity = BEANS.get(DoEntityBuilder.class).put("attribute", "a").build();
+    assertEquals("{\"attribute\":\"a\"}", runTestStreamWriteConstraints(b -> b.maxNestingDepth(1), entity));
+
+    IDoEntity nestedEntity = BEANS.get(DoEntityBuilder.class).put("attribute", BEANS.get(DoEntityBuilder.class).put("attribute", "a").build()).build();
+    assertThrows(PlatformException.class, () -> runTestStreamWriteConstraints(b -> b.maxNestingDepth(1), nestedEntity));
+
+    IDoEntity listEntity = BEANS.get(DoEntityBuilder.class).putList("attribute", "a", "b").build();
+    assertThrows(PlatformException.class, () -> runTestStreamWriteConstraints(b -> b.maxNestingDepth(1), listEntity));
+  }
+
+  protected String runTestStreamWriteConstraints(Consumer<StreamWriteConstraints.Builder> builderConsumer, IDoEntity entity) {
+    StreamWriteConstraints.Builder builder = StreamWriteConstraints.builder();
+    builderConsumer.accept(builder);
+    IBean bean = BeanTestingHelper.get().mockConfigProperty(StreamWriteConstraintsConfigProperty.class, builder.build());
+    try {
+      JacksonDataObjectMapper mapper = new JacksonDataObjectMapper(); // force new instance to apply config change
+      return mapper.writeValue(entity);
+    }
+    finally {
+      BeanTestingHelper.get().unregisterBean(bean);
+    }
+  }
+
+  @Test
+  public void testParseStreamWriteConstraintsProperty_defaultValues() {
+    StreamWriteConstraints constraints = BEANS.get(StreamWriteConstraintsConfigProperty.class).parse(CollectionUtility.emptyHashMap());
+    assertEquals(1000, constraints.getMaxNestingDepth());
+  }
+
+  @Test
+  public void testParseStreamWriteConstraintsProperty_invalidKey() {
+    assertThrows(PlatformException.class, () -> BEANS.get(StreamWriteConstraintsConfigProperty.class).parse(Map.of("foo", "bar")));
+  }
+
+  @Test
+  public void testParseStreamWriteConstraintsProperty_values() {
+    StreamWriteConstraints constraints = BEANS.get(StreamWriteConstraintsConfigProperty.class).parse(Map.of(StreamReadConstraintsConfigProperty.MAX_NESTING_DEPTH, "1"));
+    assertEquals(1, constraints.getMaxNestingDepth());
   }
 }
