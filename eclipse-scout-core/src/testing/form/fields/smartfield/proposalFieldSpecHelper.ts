3d5def26a8a4a172261f8be5eac877e3befe1e3a
Fritz Schinkel <fritz.schinkel@bsi-software.com>
2024-04-05 10:24:07 +0200

    ProposalField: fix custom text in touch mode while error status present
    Problem: If there is an error status present on the proposal field it
    will be mirrored to the field in the touch popup. When the value of the
    field in the touch popup changes, its error status is cleared. If this
    value change happens because a custom text was entered, the error status
    of the main proposal field will not be cleared. The SmartFieldAdapter
    will not send the value to the server if there is an error status
    present on the field. Therefore, the value in the model on the server
    never changes.
    
    This problem only occurs in the touch mode when entering a custom text.
    When a lookup row is selected, the error status will always be cleared
    regardless of the touch mode. If a custom text is entered in the desktop
    mode, the error status will also be cleared.
    
    Therefore, add the error status to the properties that are copied from
    the touch popups field to the main proposal field. This leads to the
    error status being cleared for all different ways the proposal fields
    value can be changed and therefore, the new value will be sent to the
    server, where it is updated in the model.
    
    369873

---
+++ eclipse-scout-core/src/testing/form/fields/smartfield/proposalFieldSpecHelper.ts
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+import {LookupRow, ProposalChooser, ProposalField, SmartFieldPopup, SmartFieldTouchPopup} from '../../../../index';
+import {JQueryTesting} from '../../../jquery-testing';
+
+export const proposalFieldSpecHelper = {
+
+  async testProposalFieldInputs(field: ProposalField, inputs: (ProposalFieldSpecHelperInput | string)[], touchMode?: boolean, callbacks?: ProposalFieldSpecHelperCallbacks) {
+    field.touchMode = touchMode;
+    field.render();
+
+    callbacks = $.extend({
+      beforeInput: (input: ProposalFieldSpecHelperInput) => {
+      },
+      afterInput: (input: ProposalFieldSpecHelperInput) => {
+      },
+      afterSelectLookupRow: (text: string, lookupRow: LookupRow<string>) => {
+      },
+      afterAcceptCustomText: (text: string) => {
+      }
+    }, callbacks);
+
+    for (const inputOrText of inputs) {
+      const input = proposalFieldSpecHelper.ensureInput(inputOrText);
+      const {text, lookup} = input;
+
+      callbacks.beforeInput(input);
+
+      if (lookup) {
+        const lookupRow = await proposalFieldSpecHelper.selectLookupRow(field, text);
+        callbacks.afterSelectLookupRow(text, lookupRow);
+      } else {
+        await proposalFieldSpecHelper.acceptCustomText(field, text);
+        callbacks.afterAcceptCustomText(text);
+      }
+
+      callbacks.afterInput(input);
+    }
+  },
+
+  ensureInput(inputOrText: ProposalFieldSpecHelperInput | string): ProposalFieldSpecHelperInput {
+    return typeof inputOrText === 'string' ? {text: inputOrText} : inputOrText;
+  },
+
+  async acceptCustomText(field: ProposalField, text: string) {
+    if (field.touchMode) {
+      // touchMode opens a popup with a field and a done-menu
+      const popup = await proposalFieldSpecHelper.openPopup(field) as SmartFieldTouchPopup<string>;
+      popup._field.$field.val(text);
+      popup._field.acceptInput();
+      popup.doneAction.doAction();
+    } else {
+      field.$field.val(text);
+      field.acceptInput();
+    }
+  },
+
+  async selectLookupRow(field: ProposalField, text: string): Promise<LookupRow<string>> {
+    // find row for text
+    const popup = await proposalFieldSpecHelper.openPopup(field);
+    const proposalChooser = field.touchMode ? (popup as SpecSmartFieldTouchPopup<string>)._widget : (popup as SmartFieldPopup<string>).proposalChooser;
+    const table = proposalChooser.content;
+    const row = table.rows.find(r => r.cells[0].text === text);
+
+    // trigger row mousedown and mouseup
+    JQueryTesting.triggerMouseDown(row.$row);
+    JQueryTesting.triggerMouseUp(row.$row);
+
+    return row.lookupRow;
+  },
+
+  async openPopup(field: ProposalField): Promise<SmartFieldTouchPopup<string> | SmartFieldPopup<string>> {
+    field.$field.focus();
+    await field.openPopup(true);
+    const popup = field.popup;
+    popup.animateRemoval = false;
+    return popup;
+  }
+};
+
+export type ProposalFieldSpecHelperInput = {
+  text: string;
+  lookup?: boolean;
+};
+
+export type ProposalFieldSpecHelperCallbacks = {
+  beforeInput?: (input: ProposalFieldSpecHelperInput) => void;
+  afterInput?: (input: ProposalFieldSpecHelperInput) => void;
+  afterSelectLookupRow?: (text: string, lookupRow: LookupRow<string>) => void;
+  afterAcceptCustomText?: (text: string) => void;
+};
+
+export class SpecProposalField extends ProposalField {
+  override acceptInput(sync?: boolean): JQuery.Promise<void> | void {
+    this._acceptInputEnabled = true; // accept all inputs, no need for a timeout
+    return super.acceptInput(sync);
+  }
+}
+
+export class SpecSmartFieldTouchPopup<TValue> extends SmartFieldTouchPopup<TValue> {
+  declare _widget: ProposalChooser<TValue, any, any>;
+}
