c3b4d24f06f8b2745e745157d56f271f33c40dc6
Fritz Schinkel <fritz.schinkel@bsi-software.com>
2024-04-05 10:24:07 +0200

    ProposalField: fix custom text in touch mode while error status present
    Problem: If there is an error status present on the proposal field it
    will be mirrored to the field in the touch popup. When the value of the
    field in the touch popup changes, its error status is cleared. If this
    value change happens because a custom text was entered, the error status
    of the main proposal field will not be cleared. The SmartFieldAdapter
    will not send the value to the server if there is an error status
    present on the field. Therefore, the value in the model on the server
    never changes.
    
    This problem only occurs in the touch mode when entering a custom text.
    When a lookup row is selected, the error status will always be cleared
    regardless of the touch mode. If a custom text is entered in the desktop
    mode, the error status will also be cleared.
    
    Therefore, add the error status to the properties that are copied from
    the touch popups field to the main proposal field. This leads to the
    error status being cleared for all different ways the proposal fields
    value can be changed and therefore, the new value will be sent to the
    server, where it is updated in the model.
    
    369873

--- eclipse-scout-core/src/testing/index.ts
+++ eclipse-scout-core/src/testing/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -24,6 +24,7 @@ export * from './table/TableSpecHelper';
 export * from './form/FormSpecHelper';
 export * from './form/fields/CloneSpecHelper';
 export * from './form/fields/beanfield/TestBeanField';
+export * from './form/fields/smartfield/proposalFieldSpecHelper';
 export * from './form/fields/tabbox/TabBoxSpecHelper';
 export * from './desktop/outline/OutlineSpecHelper';
 export * from './lookup/DummyLookupCall';
---
+++ eclipse-scout-core/test/form/fields/smartfield/ProposalFieldAdapterSpec.ts
@@ -0,0 +1,281 @@
+/*
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+import {LookupRow, ModelAdapterSendOptions, ProposalFieldAdapter, scout, SmartFieldAcceptInputEvent, StaticLookupCall, Status} from '../../../../src/index';
+import {proposalFieldSpecHelper, ProposalFieldSpecHelperCallbacks, ProposalFieldSpecHelperInput, SpecProposalField} from '../../../../src/testing';
+
+describe('ProposalFieldAdapter', () => {
+
+  let session: SandboxSession, field: SpecProposalField, modelAdapter: SpecProposalFieldAdapter;
+
+  beforeEach(() => {
+    setFixtures(sandbox());
+    session = sandboxSession();
+    field = scout.create(SpecProposalField, {parent: session.desktop});
+    linkWidgetAndAdapter(field, SpecProposalFieldAdapter);
+    modelAdapter = field.modelAdapter as SpecProposalFieldAdapter;
+    field.setLookupCall(scout.create(StaticLookupCall<string>, {
+      session: field.session,
+      data: [
+        ['foo', 'foo'],
+        ['bar', 'bar']
+      ]
+    }));
+  });
+
+  describe('acceptInput-event contains correct data', () => {
+    let spy;
+
+    beforeEach(() => {
+      spy = spyOn(modelAdapter, '_send');
+    });
+
+    // some > foo > bar
+
+    it('write \'some\' > write \'foo\' > write \'bar\' (touchMode: false, withErrorStatus: false)', async () => {
+      await testProposalFieldInputs(
+        ['some', 'foo', 'bar'],
+        false,
+        false);
+    });
+
+    it('write \'some\' > write \'foo\' > write \'bar\' (touchMode: true, withErrorStatus: false)', async () => {
+      await testProposalFieldInputs(
+        ['some', 'foo', 'bar'],
+        true,
+        false);
+    });
+
+    it('write \'some\' > write \'foo\' > write \'bar\' (touchMode: false, withErrorStatus: true)', async () => {
+      await testProposalFieldInputs(
+        ['some', 'foo', 'bar'],
+        false,
+        true);
+    });
+
+    it('write \'some\' > write \'foo\' > write \'bar\' (touchMode: true, withErrorStatus: true)', async () => {
+      await testProposalFieldInputs(
+        ['some', 'foo', 'bar'],
+        true,
+        true);
+    });
+
+    // foo > some > bar
+
+    it('write \'foo\' > write \'some\' > write \'bar\' (touchMode: false, withErrorStatus: false)', async () => {
+      await testProposalFieldInputs(
+        ['foo', 'some', 'bar'],
+        false,
+        false);
+    });
+
+    it('write \'foo\' > write \'some\' > write \'bar\' (touchMode: true, withErrorStatus: false)', async () => {
+      await testProposalFieldInputs(
+        ['foo', 'some', 'bar'],
+        true,
+        false);
+    });
+
+    it('write \'foo\' > write \'some\' > write \'bar\' (touchMode: false, withErrorStatus: true)', async () => {
+      await testProposalFieldInputs(
+        ['foo', 'some', 'bar'],
+        false,
+        true);
+    });
+
+    it('write \'foo\' > write \'some\' > write \'bar\' (touchMode: true, withErrorStatus: true)', async () => {
+      await testProposalFieldInputs(
+        ['foo', 'some', 'bar'],
+        true,
+        true);
+    });
+
+    // foo > bar > some
+
+    it('write \'foo\' > write \'bar\' > write \'some\' (touchMode: false, withErrorStatus: false)', async () => {
+      await testProposalFieldInputs(
+        ['foo', 'bar', 'some'],
+        false,
+        false);
+    });
+
+    it('write \'foo\' > write \'bar\' > write \'some\' (touchMode: true, withErrorStatus: false)', async () => {
+      await testProposalFieldInputs(
+        ['foo', 'bar', 'some'],
+        true,
+        false);
+    });
+
+    it('write \'foo\' > write \'bar\' > write \'some\' (touchMode: false, withErrorStatus: true)', async () => {
+      await testProposalFieldInputs(
+        ['foo', 'bar', 'some'],
+        false,
+        true);
+    });
+
+    it('write \'foo\' > write \'bar\' > write \'some\' (touchMode: true, withErrorStatus: true)', async () => {
+      await testProposalFieldInputs(
+        ['foo', 'bar', 'some'],
+        true,
+        true);
+    });
+
+    // foo > bar > some > thing
+
+    it('lookup \'foo\' > lookup \'bar\' > write \'some\' > write \'thing\' (touchMode: false, withErrorStatus: false)', async () => {
+      await testProposalFieldInputs(
+        [{text: 'foo', lookup: true}, {text: 'bar', lookup: true}, 'some', 'thing'],
+        false,
+        false);
+    });
+
+    it('lookup \'foo\' > lookup \'bar\' > write \'some\' > write \'thing\' (touchMode: true, withErrorStatus: false)', async () => {
+      await testProposalFieldInputs(
+        [{text: 'foo', lookup: true}, {text: 'bar', lookup: true}, 'some', 'thing'],
+        true,
+        false);
+    });
+
+    it('lookup \'foo\' > lookup \'bar\' > write \'some\' > write \'thing\' (touchMode: false, withErrorStatus: true)', async () => {
+      await testProposalFieldInputs(
+        [{text: 'foo', lookup: true}, {text: 'bar', lookup: true}, 'some', 'thing'],
+        false,
+        true);
+    });
+
+    it('lookup \'foo\' > lookup \'bar\' > write \'some\' > write \'thing\' (touchMode: true, withErrorStatus: true)', async () => {
+      await testProposalFieldInputs(
+        [{text: 'foo', lookup: true}, {text: 'bar', lookup: true}, 'some', 'thing'],
+        true,
+        true);
+    });
+
+    // some > thing > foo > bar
+
+    it('write \'some\' > write \'thing\' > lookup \'foo\' > lookup \'bar\' (touchMode: false, withErrorStatus: false)', async () => {
+      await testProposalFieldInputs(
+        ['some', 'thing', {text: 'foo', lookup: true}, {text: 'bar', lookup: true}],
+        false,
+        false);
+    });
+
+    it('write \'some\' > write \'thing\' > lookup \'foo\' > lookup \'bar\' (touchMode: true, withErrorStatus: false)', async () => {
+      await testProposalFieldInputs(
+        ['some', 'thing', {text: 'foo', lookup: true}, {text: 'bar', lookup: true}],
+        true,
+        false);
+    });
+
+    it('write \'some\' > write \'thing\' > lookup \'foo\' > lookup \'bar\' (touchMode: false, withErrorStatus: true)', async () => {
+      await testProposalFieldInputs(
+        ['some', 'thing', {text: 'foo', lookup: true}, {text: 'bar', lookup: true}],
+        false,
+        true);
+    });
+
+    it('write \'some\' > write \'thing\' > lookup \'foo\' > lookup \'bar\' (touchMode: true, withErrorStatus: true)', async () => {
+      await testProposalFieldInputs(
+        ['some', 'thing', {text: 'foo', lookup: true}, {text: 'bar', lookup: true}],
+        true,
+        true);
+    });
+
+    // foo > bar > foo > bar
+
+    it('lookup \'foo\' > lookup \'bar\' > write \'foo\' > write \'bar\' (touchMode: false, withErrorStatus: false)', async () => {
+      await testProposalFieldInputs(
+        [{text: 'foo', lookup: true}, {text: 'bar', lookup: true}, 'foo', 'bar'],
+        false,
+        false);
+    });
+
+    it('lookup \'foo\' > lookup \'bar\' > write \'foo\' > write \'bar\' (touchMode: true, withErrorStatus: false)', async () => {
+      await testProposalFieldInputs(
+        [{text: 'foo', lookup: true}, {text: 'bar', lookup: true}, 'foo', 'bar'],
+        true,
+        false);
+    });
+
+    it('lookup \'foo\' > lookup \'bar\' > write \'foo\' > write \'bar\' (touchMode: false, withErrorStatus: true)', async () => {
+      await testProposalFieldInputs(
+        [{text: 'foo', lookup: true}, {text: 'bar', lookup: true}, 'foo', 'bar'],
+        false,
+        true);
+    });
+
+    it('lookup \'foo\' > lookup \'bar\' > write \'foo\' > write \'bar\' (touchMode: true, withErrorStatus: true)', async () => {
+      await testProposalFieldInputs(
+        [{text: 'foo', lookup: true}, {text: 'bar', lookup: true}, 'foo', 'bar'],
+        true,
+        true);
+    });
+
+    it('write \'foo\' > write \'bar\' > lookup \'foo\' > lookup \'bar\' (touchMode: false, withErrorStatus: false)', async () => {
+      await testProposalFieldInputs(
+        ['foo', 'bar', {text: 'foo', lookup: true}, {text: 'bar', lookup: true}],
+        false,
+        false);
+    });
+
+    it('write \'foo\' > write \'bar\' > lookup \'foo\' > lookup \'bar\' (touchMode: true, withErrorStatus: false)', async () => {
+      await testProposalFieldInputs(
+        ['foo', 'bar', {text: 'foo', lookup: true}, {text: 'bar', lookup: true}],
+        true,
+        false);
+    });
+
+    it('write \'foo\' > write \'bar\' > lookup \'foo\' > lookup \'bar\' (touchMode: false, withErrorStatus: true)', async () => {
+      await testProposalFieldInputs(
+        ['foo', 'bar', {text: 'foo', lookup: true}, {text: 'bar', lookup: true}],
+        false,
+        true);
+    });
+
+    it('write \'foo\' > write \'bar\' > lookup \'foo\' > lookup \'bar\' (touchMode: true, withErrorStatus: true)', async () => {
+      await testProposalFieldInputs(
+        ['foo', 'bar', {text: 'foo', lookup: true}, {text: 'bar', lookup: true}],
+        true,
+        true);
+    });
+
+    async function testProposalFieldInputs(inputs: (ProposalFieldSpecHelperInput | string)[], touchMode?: boolean, withErrorStatus?: boolean) {
+      const callbacks: ProposalFieldSpecHelperCallbacks = {
+        afterInput: (input: ProposalFieldSpecHelperInput) => {
+          const {text} = input;
+          expect(field.value).toBe(text);
+          expect(field.errorStatus).toBeNull();
+        },
+        afterSelectLookupRow: (text: string, lookupRow: LookupRow<string>) => expectAcceptInputEvent(text, lookupRow),
+        afterAcceptCustomText: (text: string) => expectAcceptInputEvent(text)
+      };
+
+      if (withErrorStatus) {
+        callbacks.beforeInput = (input: ProposalFieldSpecHelperInput) => field.setErrorStatus(Status.warning('I am a WARNING!'));
+      }
+
+      await proposalFieldSpecHelper.testProposalFieldInputs(field, inputs, touchMode, callbacks);
+    }
+
+    function expectAcceptInputEvent(text: string, lookupRow?: LookupRow<string>) {
+      const displayText = text;
+      const value = text;
+      let eventData: Partial<SmartFieldAcceptInputEvent> = {displayText, value, errorStatus: null};
+      if (lookupRow) {
+        eventData = {...eventData, lookupRow};
+      }
+      expect(spy).toHaveBeenCalledWith('acceptInput', jasmine.objectContaining(eventData), jasmine.anything());
+    }
+  });
+});
+
+class SpecProposalFieldAdapter extends ProposalFieldAdapter {
+  override _send<Data extends Record<PropertyKey, any>>(type: string, data?: Data, options?: ModelAdapterSendOptions<Data>) {
+    super._send(type, data, options);
+  }
+}
