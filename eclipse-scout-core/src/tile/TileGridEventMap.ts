a2b401a90d6017a9b29a4f5567af87f1b5e4f83a
Timon Gygax <timon.gygax@bsi-software.com>
2024-05-07 11:34:08 +0200

    Use specific tile type for TileClickEvent and TileActionEvent
    374090

--- eclipse-scout-core/src/tile/TileGridEventMap.ts
+++ eclipse-scout-core/src/tile/TileGridEventMap.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -7,17 +7,19 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-import {Event, Menu, PropertyChangeEvent, Tile, TileGrid, TileGridLayoutConfig, WidgetEventMap} from '../index';
+import {Event, Menu, PropertyChangeEvent, Tile, TileAccordion, TileGrid, TileGridLayoutConfig, WidgetEventMap} from '../index';
 
 export interface TileActionEvent<T = TileGrid> extends Event<T> {
-  tile: Tile;
+  tile: TileOf<T>;
 }
 
 export interface TileClickEvent<T = TileGrid> extends Event<T> {
-  tile: Tile;
+  tile: TileOf<T>;
   mouseButton: number;
   originalEvent: JQuery.ClickEvent;
 }
+
+export type TileOf<T> = T extends TileGrid<infer TTile> | TileAccordion<infer TTile> ? TTile : Tile;
 
 export interface TileGridEventMap extends WidgetEventMap {
   'tileAction': TileActionEvent;
