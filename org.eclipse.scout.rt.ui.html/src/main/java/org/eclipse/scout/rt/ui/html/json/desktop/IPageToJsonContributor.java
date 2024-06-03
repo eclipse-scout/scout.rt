f8151508f7dbb80139bc8bd6b5405990af76fad0
Beat Schwarzentrub <bsh@bsiag.com>
2024-06-03 19:30:32 +0200

    Page: add support for PageParamDos in JS

---
+++ org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/desktop/IPageToJsonContributor.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.scout.rt.ui.html.json.desktop;
+
+import org.eclipse.scout.rt.client.ui.desktop.outline.pages.IPage;
+import org.eclipse.scout.rt.platform.ApplicationScoped;
+import org.json.JSONObject;
+
+@ApplicationScoped
+public interface IPageToJsonContributor {
+
+  void contribute(JSONObject json, IPage<?> page);
+}
