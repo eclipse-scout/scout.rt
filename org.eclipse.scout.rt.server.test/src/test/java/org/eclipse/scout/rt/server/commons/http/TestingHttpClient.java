7d03631e0dbb6b6497883843448ae75a18cc2875
Matthias Otterbach <matthias.otterbach@bsi-software.com>
2024-07-24 17:58:31 +0200

    NoHttpResponseException thrown more frequently
    Http Client 5 behavior change, OneTimeRepeatableRequestEntityProxy must
    be installed before retryRequest is called.
    
    389832

--- org.eclipse.scout.rt.server.test/src/test/java/org/eclipse/scout/rt/server/commons/http/TestingHttpClient.java
+++ org.eclipse.scout.rt.server.test/src/test/java/org/eclipse/scout/rt/server/commons/http/TestingHttpClient.java
@@ -17,19 +17,33 @@ import org.apache.hc.client5.http.config.ConnectionConfig;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.client5.http.io.HttpClientConnectionManager;
 import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
+import org.apache.hc.core5.http.ClassicHttpRequest;
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.HttpException;
+import org.apache.hc.core5.http.impl.io.HttpRequestExecutor;
+import org.apache.hc.core5.http.io.HttpClientConnection;
+import org.apache.hc.core5.http.io.HttpResponseInformationCallback;
 import org.apache.hc.core5.http.protocol.HttpContext;
 import org.eclipse.scout.rt.platform.IgnoreBean;
 import org.eclipse.scout.rt.shared.http.ApacheHttpTransportFactory;
+import org.eclipse.scout.rt.shared.http.ApacheHttpTransportFactory.ApacheHttpTransportBuilder;
 import org.eclipse.scout.rt.shared.http.DefaultHttpTransportManager;
+import org.eclipse.scout.rt.shared.http.IHttpTransportBuilder;
 import org.eclipse.scout.rt.shared.http.IHttpTransportManager;
 
 import com.google.api.client.http.HttpTransport;
 
 /**
- * HTTP Client supporting socket level interception of http requests/responses. Used to trigger and force errors and failures.
+ * HTTP Client supporting socket level interception of http requests/responses. Used to trigger and force errors and
+ * failures.
  */
 @IgnoreBean
 public class TestingHttpClient extends DefaultHttpTransportManager {
+
+  @FunctionalInterface
+  public interface IExecuteInterceptor {
+    ClassicHttpResponse execute(ClassicHttpRequest request, HttpClientConnection conn, HttpResponseInformationCallback informationCallback, HttpContext context, IExecuteInterceptor superExecuteCall) throws IOException, HttpException;
+  }
 
   /**
    * Add interception on socket level
@@ -66,12 +80,30 @@ public class TestingHttpClient extends DefaultHttpTransportManager {
     }
   }
 
+  private IExecuteInterceptor m_executeInterceptor;
+
   private Supplier<SocketWithInterception.ISocketReadInterceptor> m_socketReadInterceptor;
   private Supplier<SocketWithInterception.ISocketWriteInterceptor> m_socketWriteInterceptor;
 
   @Override
   protected HttpTransport createHttpTransport() {
     return new ApacheHttpTransportFactoryEx().newHttpTransport(this);
+  }
+
+  @Override
+  public void interceptNewHttpTransport(IHttpTransportBuilder builder) {
+    ApacheHttpTransportBuilder builder0 = (ApacheHttpTransportBuilder) builder;
+    builder0.getBuilder().setRequestExecutor(new HttpRequestExecutor() {
+      @Override
+      public ClassicHttpResponse execute(ClassicHttpRequest request, HttpClientConnection conn, HttpResponseInformationCallback informationCallback, HttpContext context) throws IOException, HttpException {
+        if (m_executeInterceptor != null) {
+          return m_executeInterceptor.execute(request, conn, informationCallback, context, (request0, conn0, informationCallback0, context0, interceptor) -> super.execute(request0, conn0, informationCallback0, context0));
+        }
+        else {
+          return super.execute(request, conn, informationCallback, context);
+        }
+      }
+    });
   }
 
   public TestingHttpClient withSocketReadInterceptor(Supplier<SocketWithInterception.ISocketReadInterceptor> socketReadInterceptor) {
@@ -86,5 +118,10 @@ public class TestingHttpClient extends DefaultHttpTransportManager {
 
   public void stop() {
     removeHttpTransport();
+  }
+
+  public TestingHttpClient withExecuteInterceptor(IExecuteInterceptor executeInterceptor) {
+    m_executeInterceptor = executeInterceptor;
+    return this;
   }
 }
