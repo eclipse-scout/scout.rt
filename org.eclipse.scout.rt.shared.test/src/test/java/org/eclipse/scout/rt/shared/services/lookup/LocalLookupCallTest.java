eclipse-scout/scout.rt

commit 251c05954edee6e1a6fc992320b56a3f964a2af8

LocalLookupCall: make sure returned lookup rows are unique
Make sure the returned lookup rows of getDataByText are unique for
hierarchical lookup calls, in particular when the searched text matches
for a parent and its children.

org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/services/lookup/LocalLookupCall.java
@@ -15,6 +15,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -134,23 +135,23 @@ public class LocalLookupCall<T> extends LookupCall<T> {
    */
   @Override
   public List<? extends ILookupRow<T>> getDataByText() {
-    List<ILookupRow<T>> list = new ArrayList<>();
+    Map<T, ILookupRow<T>> list = new HashMap<>();
     Pattern p = createSearchPattern(getText());
     List<? extends ILookupRow<T>> lookupRows = createLookupRowsFiltered();
     for (ILookupRow<T> row : lookupRows) {
       if (row.getText() != null && p.matcher(row.getText().toLowerCase()).matches()) {
-        list.add(row);
+        list.put(row.getKey(), row);
       }
     }
     if (isHierarchicalLookup()) {
       Map<T, Set<ILookupRow<T>>> nodeMap = createNodeMap(lookupRows);
       List<ILookupRow<T>> children = new ArrayList<>();
-      for (ILookupRow<T> res : list) {
+      for (ILookupRow<T> res : list.values()) {
         collectChildrenRec(nodeMap, res.getKey(), children);
       }
-      list.addAll(children);
+      list.putAll(children.stream().collect(Collectors.toMap(ILookupRow::getKey, Function.identity())));
     }
-    return list;
+    return new ArrayList<>(list.values());
   }
 
   /**

commit 66195351f06cf6ac42d934798d9e8af14d96d0bc

LocalLookupCall: fix order of getDataByText result
getDataByText does not return the results by insertion order anymore.
Compared to getDataByAll, which still does it correctly.

The bug was introduced with 251c05954edee6e1a6fc992320b56a3f964a2af8

org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/services/lookup/LocalLookupCall.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -12,6 +12,7 @@ package org.eclipse.scout.rt.shared.services.lookup;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -135,7 +136,7 @@ public class LocalLookupCall<T> extends LookupCall<T> {
    */
   @Override
   public List<? extends ILookupRow<T>> getDataByText() {
-    Map<T, ILookupRow<T>> list = new HashMap<>();
+    Map<T, ILookupRow<T>> list = new LinkedHashMap<>();
     Pattern p = createSearchPattern(getText());
     List<? extends ILookupRow<T>> lookupRows = createLookupRowsFiltered();
     for (ILookupRow<T> row : lookupRows) {

org.eclipse.scout.rt.shared.test/src/test/java/org/eclipse/scout/rt/shared/services/lookup/LocalLookupCallTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -131,6 +131,17 @@ public class LocalLookupCallTest {
   }
 
   @Test
+  public void testGetDataByTexOrder() {
+    P_LocalLookupCall lc = new P_LocalLookupCall();
+    lc.setWildcard("*");
+    lc.setText("*");
+    List<? extends ILookupRow<Integer>> rows = lc.getDataByText();
+    assertEquals("lorem", rows.get(0).getText());
+    assertEquals("ipsum", rows.get(1).getText());
+    assertEquals("dolor", rows.get(2).getText());
+  }
+
+  @Test
   public void testGetDataByKey() {
     runGetDataByKey(0, null);
     runGetDataByKey(1, ROW10_KEY);
@@ -155,7 +166,6 @@ public class LocalLookupCallTest {
     runGetDataByRec(1, ROW30_KEY);
     runGetDataByRec(0, ROW11_KEY);
     runGetDataByRec(0, 799999);
-
   }
 
   @Test