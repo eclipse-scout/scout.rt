98a50efbf79588e5db1ceb20800fa52a78494fd0
Fritz Schinkel <fritz.schinkel@bsi-software.com>
2024-08-08 12:26:26 +0200

    Move MetaResource to scout.rt
    Move MetaResource, MetaVersionInfoDo and IApiVersion to scout.rt.
    Implementations of IApiVersion need to be moved to rest.server module.

---
+++ org.eclipse.scout.rt.api.data/src/main/java/org/eclipse/scout/rt/api/data/meta/MetaVersionInfoDo.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.scout.rt.api.data.meta;
+
+import java.util.Date;
+
+import jakarta.annotation.Generated;
+
+import org.eclipse.scout.rt.dataobject.DoEntity;
+import org.eclipse.scout.rt.dataobject.DoValue;
+import org.eclipse.scout.rt.dataobject.TypeName;
+
+@TypeName("scout.MetaVersionInfo")
+public class MetaVersionInfoDo extends DoEntity {
+
+  public DoValue<String> apiVersion() {
+    return doValue("apiVersion");
+  }
+
+  public DoValue<String> applicationName() {
+    return doValue("applicationName");
+  }
+
+  public DoValue<String> applicationVersion() {
+    return doValue("applicationVersion");
+  }
+
+  public DoValue<Date> buildDate() {
+    return doValue("buildDate");
+  }
+
+  /* **************************************************************************
+   * GENERATED CONVENIENCE METHODS
+   * *************************************************************************/
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public MetaVersionInfoDo withApiVersion(String apiVersion) {
+    apiVersion().set(apiVersion);
+    return this;
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public String getApiVersion() {
+    return apiVersion().get();
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public MetaVersionInfoDo withApplicationName(String applicationName) {
+    applicationName().set(applicationName);
+    return this;
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public String getApplicationName() {
+    return applicationName().get();
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public MetaVersionInfoDo withApplicationVersion(String applicationVersion) {
+    applicationVersion().set(applicationVersion);
+    return this;
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public String getApplicationVersion() {
+    return applicationVersion().get();
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public MetaVersionInfoDo withBuildDate(Date buildDate) {
+    buildDate().set(buildDate);
+    return this;
+  }
+
+  @Generated("DoConvenienceMethodsGenerator")
+  public Date getBuildDate() {
+    return buildDate().get();
+  }
+}
