513bbead3aa95af3f247a41ba4806213ad5cb708
Beat Schwarzentrub <bsh@bsiag.com>
2024-08-28 09:44:26 +0200

    ComboMenu: fix prefSize when child actions have a custom layout
    A ComboMenu may contain a FormFieldMenu which in turn contains a
    FormField, e.g. a Button. When the MenuBar is layouted initially, it
    reads the preferred size of each menu item before they are actually
    layouted. The NullLayout just reads the current CSS size from the DOM.
    This is not necessarily correct if the inner layout (e.g. a Button)
    relies on its layout to report the correct preferred size. To fix this,
    we explicitly set a ColumnLayout to the ComboMenu that has a different
    implementation of the preferredLayoutSize() method.
    
    389822

--- eclipse-scout-core/src/menu/ComboMenu.ts
+++ eclipse-scout-core/src/menu/ComboMenu.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -7,7 +7,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-import {EventHandler, HtmlComponent, Menu, PropertyChangeEvent, widgets} from '../index';
+import {ColumnLayout, EventHandler, HtmlComponent, Menu, PropertyChangeEvent, widgets} from '../index';
 
 export class ComboMenu extends Menu {
   protected _childVisibleChangeHandler: EventHandler<PropertyChangeEvent<boolean>>;
@@ -24,6 +24,7 @@ export class ComboMenu extends Menu {
     }
     this.$container.unfocusable();
     this.htmlComp = HtmlComponent.install(this.$container, this.session);
+    this.htmlComp.setLayout(new ColumnLayout());
   }
 
   protected override _renderProperties() {
