52890167f0b48740c495cc30823ec1b774af132e
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2024-09-03 09:56:59 +0200

    TileButton: support labelVisible
    Tile buttons did not respect the label visible property.
    The same applies to regular buttons, but since they are used frequently
    changing their behaviour would likely break existing code (see also
    TODO in Button.ts).
    
    393740

--- eclipse-scout-core/src/tile/fields/button/TileButton.ts
+++ eclipse-scout-core/src/tile/fields/button/TileButton.ts
@@ -85,4 +85,9 @@ export class TileButton extends Button {
       tooltips.uninstall(this.$container);
     }
   }
+
+  protected override _renderLabelVisible() {
+    super._renderLabelVisible();
+    this._renderChildVisible(this.$buttonLabel, this.labelVisible);
+  }
 }
