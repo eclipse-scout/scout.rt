7a80eb8453f695ff9db9dfa850feebbe2e484c43
Paolo Bazzi <paolo.bazzi@bsi-software.com>
2024-09-04 15:18:43 +0200

    HTTP client connection manager pools metrics
    Add OpenTelemetry metrics for HTTP client connection pool:
    - active/idle connections
    - max connections
    
    Co-authored-by: Bruno Koeferli <bruno.koeferli@bsiag.com>
    
    393561
    
    (cherry picked from commit ab97e46f21e4f3f4900f0318ae0d0182ddba213f)
    
    ab97e46f 393561 HTTP client connection manager pools metrics
    
    Co-authored-by: Paolo Bazzi <paolo.bazzi@bsi-software.com>

--- org.eclipse.scout.rt.rest.jersey.test/src/test/java/org/eclipse/scout/rt/rest/jersey/client/ScoutApacheConnectorTest.java
+++ org.eclipse.scout.rt.rest.jersey.test/src/test/java/org/eclipse/scout/rt/rest/jersey/client/ScoutApacheConnectorTest.java
@@ -11,8 +11,7 @@ package org.eclipse.scout.rt.rest.jersey.client;
 
 import static org.eclipse.scout.rt.rest.jersey.EchoServletParameters.*;
 import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -22,6 +21,7 @@ import java.util.UUID;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -49,6 +49,7 @@ import org.eclipse.scout.rt.dataobject.DoEntityBuilder;
 import org.eclipse.scout.rt.dataobject.IDataObjectMapper;
 import org.eclipse.scout.rt.dataobject.IDoEntity;
 import org.eclipse.scout.rt.platform.BEANS;
+import org.eclipse.scout.rt.platform.BeanMetaData;
 import org.eclipse.scout.rt.platform.IBean;
 import org.eclipse.scout.rt.platform.context.CorrelationId;
 import org.eclipse.scout.rt.platform.context.RunContexts;
@@ -65,6 +66,7 @@ import org.eclipse.scout.rt.rest.jersey.client.ScoutApacheConnector.RestHttpTran
 import org.eclipse.scout.rt.rest.jersey.client.ScoutApacheConnector.RestHttpTransportMaxConnectionsPerRouteProperty;
 import org.eclipse.scout.rt.rest.jersey.client.ScoutApacheConnector.RestHttpTransportMaxConnectionsTotalProperty;
 import org.eclipse.scout.rt.rest.jersey.client.ScoutApacheConnector.RestHttpTransportValidateAfterInactivityProperty;
+import org.eclipse.scout.rt.shared.http.HttpClientMetricsHelper;
 import org.eclipse.scout.rt.testing.platform.BeanTestingHelper;
 import org.eclipse.scout.rt.testing.platform.runner.PlatformTestRunner;
 import org.glassfish.jersey.client.ClientProperties;
@@ -72,7 +74,10 @@ import org.glassfish.jersey.client.ClientRequest;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
+
+import io.opentelemetry.api.metrics.Meter;
 
 @RunWith(PlatformTestRunner.class)
 public class ScoutApacheConnectorTest {
@@ -533,6 +538,49 @@ public class ScoutApacheConnectorTest {
     when(config.getProperty(RestClientProperties.VALIDATE_CONNECTION_AFTER_INACTIVITY)).thenReturn(300);
     ScoutApacheConnector connector = new ScoutApacheConnector(client, config);
     assertEquals(300, connector.getValidateAfterInactivityMillis(config));
+  }
+
+  @Test
+  public void testMetricsDisabled() {
+    Client client = Mockito.mock(Client.class);
+    Configuration config = Mockito.mock(Configuration.class);
+    when(config.getProperty(RestClientProperties.OTEL_HTTP_CLIENT_NAME)).thenReturn(null);
+    List<IBean<?>> beans = new ArrayList<>();
+    try {
+      HttpClientMetricsHelper mock = Mockito.mock(HttpClientMetricsHelper.class);
+      beans.add(BEANS.get(BeanTestingHelper.class).registerBean(new BeanMetaData(HttpClientMetricsHelper.class, mock)));
+      new ScoutApacheConnector(client, config);
+      verify(mock, never()).initMetrics(any(Meter.class), any(), any(), any(), any());
+    }
+    finally {
+      beans.forEach(BeanTestingHelper.get()::unregisterBean);
+    }
+  }
+
+  @Test
+  public void testMetricsEnabled() {
+    Client client = Mockito.mock(Client.class);
+    Configuration config = Mockito.mock(Configuration.class);
+    when(config.getProperty(RestClientProperties.OTEL_HTTP_CLIENT_NAME)).thenReturn("mock-http-client-name");
+    when(config.getProperty(RestClientProperties.MAX_CONNECTIONS_TOTAL)).thenReturn(123);
+
+    List<IBean<?>> beans = new ArrayList<>();
+    try {
+      HttpClientMetricsHelper mock = Mockito.mock(HttpClientMetricsHelper.class);
+      @SuppressWarnings("unchecked") ArgumentCaptor<Supplier<Integer>> idleCaptor = ArgumentCaptor.forClass(Supplier.class);
+      @SuppressWarnings("unchecked") ArgumentCaptor<Supplier<Integer>> activeCaptor = ArgumentCaptor.forClass(Supplier.class);
+      @SuppressWarnings("unchecked") ArgumentCaptor<Supplier<Integer>> maxCaptor = ArgumentCaptor.forClass(Supplier.class);
+      beans.add(BEANS.get(BeanTestingHelper.class).registerBean(new BeanMetaData(HttpClientMetricsHelper.class, mock)));
+      new ScoutApacheConnector(client, config);
+
+      verify(mock, only()).initMetrics(any(Meter.class), eq("mock-http-client-name"), idleCaptor.capture(), activeCaptor.capture(), maxCaptor.capture());
+      assertEquals(Integer.valueOf(0), idleCaptor.getValue().get());
+      assertEquals(Integer.valueOf(0), activeCaptor.getValue().get());
+      assertEquals(Integer.valueOf(123), maxCaptor.getValue().get());
+    }
+    finally {
+      beans.forEach(BeanTestingHelper.get()::unregisterBean);
+    }
   }
 
   protected JerseyTestRestClientHelper newHelper(BiConsumer<JerseyTestRestClientHelper, ClientBuilder> clientBuilderCustomizer) {
