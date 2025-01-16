2ede0c46f30eac34119d403278bd30a9163a2ebc
Ralph Wortha <steiner.ralph@gmail.com>
2025-01-16 17:16:52 +0100

    Use scout config properties and allow override of driver initialization
     - SeleniumDriver as ApplicationScoped bean to allow customization
     - Replace System.out and System.err calls with logger
     - Allow use of remote driver

--- org.eclipse.scout.rt.ui.html.selenium/src/main/java/org/eclipse/scout/rt/ui/html/selenium/junit/RetryOnFailureRule.java
+++ org.eclipse.scout.rt.ui.html.selenium/src/main/java/org/eclipse/scout/rt/ui/html/selenium/junit/RetryOnFailureRule.java
@@ -12,8 +12,11 @@ package org.eclipse.scout.rt.ui.html.selenium.junit;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RetryOnFailureRule implements TestRule {
+  private static final Logger LOG = LoggerFactory.getLogger(RetryOnFailureRule.class);
 
   public RetryOnFailureRule(AbstractSeleniumTest test) {
   }
@@ -39,17 +42,17 @@ public class RetryOnFailureRule implements TestRule {
           try {
             base.evaluate();
             if (i > 0) {
-              System.out.println("@@@ Test succeeded (after " + (i == 1 ? "1 retry" : i + " retries") + ")");
+              LOG.info("@@@ Test succeeded (after {})", (i == 1 ? "1 retry" : i + " retries"));
             }
             return; // success
           }
           catch (Throwable t) {
             if (i >= retryCount) {
               // This was the last attempt -> give up and fail
-              System.out.println("@@@ Test failed (" + (i + 1) + "/" + (retryCount + 1) + ") --> giving up!");
+              LOG.info("@@@ Test failed ({}/{}) --> giving up!", (i + 1), (retryCount + 1));
               throw t;
             }
-            System.out.println("@@@ Test failed (" + (i + 1) + "/" + (retryCount + 1) + ") --> retrying " + (retryCount - i) + " more time" + (retryCount - i > 1 ? "s" : "") + "...");
+            LOG.info("@@@ Test failed ({}/{}) --> retrying {} more time{}...", (i + 1), (retryCount + 1), (retryCount - i), (retryCount - i > 1 ? "s" : ""));
           }
         }
       }
