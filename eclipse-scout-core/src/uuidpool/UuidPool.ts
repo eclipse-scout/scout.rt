03477089efdcad473626c9f6b51d4ca701ad8c18
Beat Schwarzentrub <bsh@bsiag.com>
2025-01-16 13:41:05 +0100

    Improve jsdoc
    399264

--- eclipse-scout-core/src/uuidpool/UuidPool.ts
+++ eclipse-scout-core/src/uuidpool/UuidPool.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -106,6 +106,7 @@ export class UuidPool extends Widget implements UuidPoolModel {
    * Otherwise, a new instance is created.
    *
    * @param session Session object providing a desktop. If this is omitted, the first session of the app is used.
+   * If the app does not have any active sessions (e.g. during unit testing), this argument is mandatory.
    */
   static get(session?: Session): UuidPool {
     session = session || App.get().sessions[0];
