6877d93b4034876fb1146b47cd1d16c294299ea6
Matthias Villiger <mvi@bsi-software.com>
2025-02-13 14:37:31 +0100

    TableUpdateBuffer: only process existing rows
    410096, 323009, 392964

--- eclipse-scout-core/test/table/TableUpdateBufferSpec.ts
+++ eclipse-scout-core/test/table/TableUpdateBufferSpec.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -8,14 +8,26 @@
  * SPDX-License-Identifier: EPL-2.0
  */
 import {TableSpecHelper} from '../../src/testing/index';
+import {App, ChildModelOf, ErrorHandler, scout, SmartColumn, StaticLookupCall} from '../../src/index';
 
 describe('TableUpdateBuffer', () => {
-  let session: SandboxSession, helper: TableSpecHelper;
+  let session: SandboxSession, helper: TableSpecHelper, origErrorHandler: ErrorHandler, errHandlerSpy: jasmine.Spy;
 
   beforeEach(() => {
     setFixtures(sandbox());
     session = sandboxSession();
     helper = new TableSpecHelper(session);
+    jasmine.clock().install();
+
+    origErrorHandler = App.get().errorHandler;
+    const newErrorHandler = scout.create(ErrorHandler);
+    errHandlerSpy = spyOn(newErrorHandler, 'handle').and.callThrough();
+    App.get().errorHandler = newErrorHandler;
+  });
+
+  afterEach(() => {
+    jasmine.clock().uninstall();
+    App.get().errorHandler = origErrorHandler;
   });
 
   it('buffers updateRow calls and processes them when all promises resolve', done => {
@@ -42,6 +54,34 @@ describe('TableUpdateBuffer', () => {
       done();
     });
     deferred.resolve();
+    jasmine.clock().tick(500);
+  });
+
+  it('ignores removed rows', done => {
+    class TableUpdateBufferSpecLookupCall extends StaticLookupCall<number> {
+      protected override _data(): any[] {
+        return [
+          [1, 'Foo'],
+          [2, 'Bar'],
+          [3, 'Baz']
+        ];
+      }
+    }
+
+    let columnModel = helper.createModelColumn('smart', SmartColumn<number>) as ChildModelOf<SmartColumn<number>>;
+    columnModel.lookupCall = TableUpdateBufferSpecLookupCall;
+    let table = helper.createTable(helper.createModel([columnModel], helper.createModelRows([columnModel], 0)));
+    table.on('propertyChange:loading', e => {
+      if (e.oldValue && !e.newValue) { // change to loading=false after the last promise is resolved
+        expect(errHandlerSpy.calls.count()).toEqual(0);
+        done();
+      }
+    });
+
+    table.insertRows([{cells: [1]}, {cells: [2]}, {cells: [3]}]);
+    table.render();
+    table.deleteRow(table.rows[0]);
+    jasmine.clock().tick(500); // here the LookupCall is invoked and therefore TableUpdateBuffer#process
   });
 
   it('prevents rendering viewport while buffering', done => {
@@ -72,5 +112,6 @@ describe('TableUpdateBuffer', () => {
       done();
     });
     deferred.resolve();
+    jasmine.clock().tick(500);
   });
 });
