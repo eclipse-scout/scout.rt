0087be249a75765e7c8d18c54319fd81748b88d8
Adrian Egloff <adrian.egloff@bsi-software.com>
2025-02-21 10:51:36 +0100

    Bump Eclipse Scout version to 25.2
    - replace 25.1-SNAPSHOT
    - replace '>=25.1.0-snapshot <25.1.0' and '25.1.0-snapshot'
    - replace git branch-names
    - replace maven properties
    - replace other tokens: '2510', '25.1', '25-1', '25_1'
    - add new type-versions

--- org.eclipse.scout.rt.dataobject/src/main/java/org/eclipse/scout/rt/dataobject/ScoutTypeVersions.java
+++ org.eclipse.scout.rt.dataobject/src/main/java/org/eclipse/scout/rt/dataobject/ScoutTypeVersions.java
@@ -44,4 +44,21 @@ public class ScoutTypeVersions {
    */
   public static final class Scout_25_1_001 extends AbstractTypeVersion {
   }
+
+  //  ___  _____  ___
+  // |__ \| ____||__ \
+  //    ) | |__     ) |
+  //   / /|___ \   / /
+  //  / /_ ___) | / /_
+  // |____|____(_)____|
+
+  /**
+   * Baseline version for all new data objects created in 25.2.
+   * <p>
+   * As soon as the initial build of this major release was published, a new type version must always be created for
+   * data objects requiring a migration handler. A new type version is created by incrementing the counter by 1 and
+   * providing a suffix.
+   */
+  public static final class Scout_25_2_001 extends AbstractTypeVersion {
+  }
 }
