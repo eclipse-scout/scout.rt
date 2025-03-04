02c8f0091313b4b90e7a4561156ac2fbed831d2c
David Wlazlo <david.wlazlo@bsi-software.com>
2025-03-04 12:26:29 +0100

    Improve formatting of DateTimePeriodFormatter
    In order to prevent confusion with time instants, the
    DateTimePeriodFormatter should format time periods in a format which is
    distinct from the format of local (date) times.
    
    Previously, the format was "hh:mm:ss.zzz" (e.g. 12:34:56) which was
    confused with the corresponding time of day. The new format makes this
    distinction clear by adding time units (e.g. 12h 34m 56s).
    
    389916

--- org.eclipse.scout.rt.platform.test/src/test/java/org/eclipse/scout/rt/platform/util/date/DateTimePeriodFormatterTest.java
+++ org.eclipse.scout.rt.platform.test/src/test/java/org/eclipse/scout/rt/platform/util/date/DateTimePeriodFormatterTest.java
@@ -13,6 +13,7 @@ import static org.junit.Assert.*;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.Duration;
 import java.util.Date;
 import java.util.Locale;
 
@@ -30,17 +31,61 @@ public class DateTimePeriodFormatterTest {
 
   protected DateTimePeriodFormatter m_formatter = BEANS.get(DateTimePeriodFormatter.class);
 
+  @Test
+  public void testFormatDuration() {
+    // null and zero
+    assertNull(m_formatter.formatDuration(null));
+    assertEquals("0s", m_formatter.formatDuration(Duration.ZERO));
+
+    // negative value
+    assertEquals("0s", m_formatter.formatDuration(Duration.ofDays(-1L)));
+
+    // simple values
+    assertEquals("1 " + TEXTS.get("Day") + " 0h 00m 00s", m_formatter.formatDuration(Duration.ofDays(1L)));
+    assertEquals("3 " + TEXTS.get("Days") + " 15h 42m 54s", m_formatter.formatDuration(Duration.ofDays(3L)
+        .plusHours(15L).plusMinutes(42L).plusSeconds(54L)));
+    assertEquals("11 " + TEXTS.get("Days") + " 11h 11m 11s", m_formatter.formatDuration(Duration.ofDays(11L)
+        .plusHours(11L).plusMinutes(11L).plusSeconds(11L)));
+
+    // ignore millis part
+    assertEquals("0s", m_formatter.formatDuration(Duration.ofMillis(123)));
+    assertEquals("0s", m_formatter.formatDuration(Duration.ofMillis(999)));
+  }
+
+  @Test
+  public void testFormatDurationPadding() {
+    assertEquals("1s", m_formatter.formatDuration(Duration.ofSeconds(1)));
+    assertEquals("1m 00s", m_formatter.formatDuration(Duration.ofMinutes(1)));
+    assertEquals("1h 00m 00s", m_formatter.formatDuration(Duration.ofHours(1)));
+    assertEquals("1 " + TEXTS.get("Day") + " 0h 00m 00s", m_formatter.formatDuration(Duration.ofDays(1L)));
+
+    assertEquals("1m 01s", m_formatter.formatDuration(Duration.ofMinutes(1).plusSeconds(1)));
+
+    assertEquals("1h 00m 01s", m_formatter.formatDuration(Duration.ofHours(1).plusSeconds(1)));
+    assertEquals("1h 01m 01s", m_formatter.formatDuration(Duration.ofHours(1).plusMinutes(1).plusSeconds(1)));
+    assertEquals("1h 01m 00s", m_formatter.formatDuration(Duration.ofHours(1).plusMinutes(1)));
+
+    assertEquals("1 " + TEXTS.get("Day") + " 0h 00m 00s", m_formatter.formatDuration(Duration.ofDays(1L).plusHours(0).plusMinutes(0).plusSeconds(0)));
+    assertEquals("1 " + TEXTS.get("Day") + " 0h 00m 01s", m_formatter.formatDuration(Duration.ofDays(1L).plusHours(0).plusMinutes(0).plusSeconds(1)));
+    assertEquals("1 " + TEXTS.get("Day") + " 0h 01m 00s", m_formatter.formatDuration(Duration.ofDays(1L).plusHours(0).plusMinutes(1).plusSeconds(0)));
+    assertEquals("1 " + TEXTS.get("Day") + " 0h 01m 01s", m_formatter.formatDuration(Duration.ofDays(1L).plusHours(0).plusMinutes(1).plusSeconds(1)));
+    assertEquals("1 " + TEXTS.get("Day") + " 1h 00m 00s", m_formatter.formatDuration(Duration.ofDays(1L).plusHours(1).plusMinutes(0).plusSeconds(0)));
+    assertEquals("1 " + TEXTS.get("Day") + " 1h 00m 01s", m_formatter.formatDuration(Duration.ofDays(1L).plusHours(1).plusMinutes(0).plusSeconds(1)));
+    assertEquals("1 " + TEXTS.get("Day") + " 1h 01m 00s", m_formatter.formatDuration(Duration.ofDays(1L).plusHours(1).plusMinutes(1).plusSeconds(0)));
+    assertEquals("1 " + TEXTS.get("Day") + " 1h 01m 01s", m_formatter.formatDuration(Duration.ofDays(1L).plusHours(1).plusMinutes(1).plusSeconds(1)));
+  }
+
   /**
    * test cases for conversion of special dates
    */
   @Test
   public void testFormatTimePeriod1() {
     assertNull(m_formatter.formatTimePeriod(null));
-    assertEquals("00:00:00", m_formatter.formatTimePeriod(BigDecimal.ZERO));
-    assertEquals("1 " + TEXTS.get("Day") + " 00:00:00", m_formatter.formatTimePeriod(BigDecimal.ONE));
-    assertEquals("00:00:00", m_formatter.formatTimePeriod(BigDecimal.valueOf(-3.654789D)));
-    assertEquals("3 " + TEXTS.get("Days") + " 15:42:54", m_formatter.formatTimePeriod(BigDecimal.valueOf(3.65479D)));
-    assertEquals("11 " + TEXTS.get("Days") + " 11:11:11", m_formatter.formatTimePeriod(BigDecimal.valueOf(11.4661D)));
+    assertEquals("0s", m_formatter.formatTimePeriod(BigDecimal.ZERO));
+    assertEquals("1 " + TEXTS.get("Day") + " 0h 00m 00s", m_formatter.formatTimePeriod(BigDecimal.ONE));
+    assertEquals("0s", m_formatter.formatTimePeriod(BigDecimal.valueOf(-3.654789D)));
+    assertEquals("3 " + TEXTS.get("Days") + " 15h 42m 54s", m_formatter.formatTimePeriod(BigDecimal.valueOf(3.65479D)));
+    assertEquals("11 " + TEXTS.get("Days") + " 11h 11m 11s", m_formatter.formatTimePeriod(BigDecimal.valueOf(11.4661D)));
   }
 
   @Test
@@ -49,7 +94,7 @@ public class DateTimePeriodFormatterTest {
     BigDecimal day = BigDecimal.valueOf(86400L).setScale(100, RoundingMode.HALF_UP);
     BigDecimal d = value.divide(day, RoundingMode.HALF_UP);
     String result = m_formatter.formatTimePeriod(d);
-    assertEquals("Formatted Time", "00:00:11", result);
+    assertEquals("Formatted Time", "11s", result);
   }
 
   @Test
@@ -57,7 +102,7 @@ public class DateTimePeriodFormatterTest {
     Object o = 5L;
     BigDecimal d = TypeCastUtility.castValue(o, BigDecimal.class);
     String result = m_formatter.formatTimePeriod(d);
-    assertEquals("Formatted Time", "5 " + TEXTS.get("Days") + " 00:00:00", result);
+    assertEquals("Formatted Time", "5 " + TEXTS.get("Days") + " 0h 00m 00s", result);
   }
 
   @Test
@@ -65,11 +110,13 @@ public class DateTimePeriodFormatterTest {
     char decimalSeparator = BEANS.get(NumberFormatProvider.class).getNumberInstance(NlsLocale.get()).getDecimalFormatSymbols().getDecimalSeparator();
 
     assertNull(m_formatter.formatTimePeriodOfMs(null));
-    assertEquals("00:00:00" + decimalSeparator + "000", m_formatter.formatTimePeriodOfMs(-1L));
-    assertEquals("00:00:00" + decimalSeparator + "005", m_formatter.formatTimePeriodOfMs(5L));
-    assertEquals("00:00:02" + decimalSeparator + "001", m_formatter.formatTimePeriodOfMs(2001L));
-    assertEquals("00:09:20" + decimalSeparator + "000", m_formatter.formatTimePeriodOfMs(560000L));
-    assertEquals("00:00:00" + decimalSeparator + "999", m_formatter.formatTimePeriodOfMs(999L)); // Test rounding.
+    assertEquals("0" + decimalSeparator + "000s", m_formatter.formatTimePeriodOfMs(-1L));
+    assertEquals("0" + decimalSeparator + "050s", m_formatter.formatTimePeriodOfMs(50L));
+    assertEquals("2" + decimalSeparator + "001s", m_formatter.formatTimePeriodOfMs(2_001L));
+    assertEquals("9m 20" + decimalSeparator + "000s", m_formatter.formatTimePeriodOfMs(560_000L));
+    assertEquals("1h 09m 20" + decimalSeparator + "000s", m_formatter.formatTimePeriodOfMs(4_160_000L));
+    assertEquals("2 " + TEXTS.get("Days") + " 1h 09m 20" + decimalSeparator + "000s", m_formatter.formatTimePeriodOfMs(176_960_000L));
+    assertEquals("0" + decimalSeparator + "999s", m_formatter.formatTimePeriodOfMs(999L)); // Test rounding.
   }
 
   @Test
