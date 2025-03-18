91e5eb8fb780e1b4c61769eb0d6bcbf2777706a8
Laura Toedtli <laura.toedtli@bsi-software.com>
2025-03-18 13:28:53 +0100

    Graceful exit in case of error during startup
    407124

--- org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/healthcheck/PlatformHealthChecker.java
+++ org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/healthcheck/PlatformHealthChecker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -10,23 +10,18 @@
 package org.eclipse.scout.rt.server.commons.healthcheck;
 
 import org.eclipse.scout.rt.platform.IPlatform.State;
-import org.eclipse.scout.rt.platform.IPlatformListener;
-import org.eclipse.scout.rt.platform.Order;
-import org.eclipse.scout.rt.platform.PlatformEvent;
+import org.eclipse.scout.rt.platform.Platform;
+import org.eclipse.scout.rt.platform.util.ObjectUtility;
 
-@Order(PlatformHealthChecker.ORDER) // ensure to be the last platform listener
-public class PlatformHealthChecker extends AbstractHealthChecker implements IPlatformListener {
-  public static final int ORDER = 100_000;
-
-  private volatile boolean m_platformStarted = false;
+public class PlatformHealthChecker extends AbstractHealthChecker {
 
   @Override
   protected boolean execCheckHealth(HealthCheckCategoryId category) throws Exception {
-    return m_platformStarted;
+    return Platform.get().getState() == State.PlatformStarted;
   }
 
   @Override
-  public void stateChanged(PlatformEvent event) {
-    m_platformStarted = event.getState() == State.PlatformStarted;
+  public boolean acceptCategory(HealthCheckCategoryId category) {
+    return ObjectUtility.isOneOf(category, Startup.ID, Liveness.ID);
   }
 }
