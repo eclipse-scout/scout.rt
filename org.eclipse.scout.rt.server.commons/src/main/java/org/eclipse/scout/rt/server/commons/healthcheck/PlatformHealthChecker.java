45ebe91d3293a17a55915edeab7a75848b1ebafe
Laura Toedtli <laura.toedtli@bsi-software.com>
2025-03-18 13:28:53 +0100

    Graceful exit in case of error during startup
    407124

--- org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/healthcheck/PlatformHealthChecker.java
+++ org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/healthcheck/PlatformHealthChecker.java
@@ -11,11 +11,17 @@ package org.eclipse.scout.rt.server.commons.healthcheck;
 
 import org.eclipse.scout.rt.platform.IPlatform.State;
 import org.eclipse.scout.rt.platform.Platform;
+import org.eclipse.scout.rt.platform.util.ObjectUtility;
 
 public class PlatformHealthChecker extends AbstractHealthChecker {
 
   @Override
   protected boolean execCheckHealth(HealthCheckCategoryId category) throws Exception {
     return Platform.get().getState() == State.PlatformStarted;
+  }
+
+  @Override
+  public boolean acceptCategory(HealthCheckCategoryId category) {
+    return ObjectUtility.isOneOf(category, Startup.ID, Liveness.ID);
   }
 }
