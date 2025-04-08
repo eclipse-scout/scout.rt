cb8d611c1f601075e85d2b300850cf5531c09f30
Nicola Steger <nicola.steger@bsi-software.com>
2025-04-08 12:45:29 +0200

    GroupBox: Pull up setBodyGrid to IGroupBox
    412569

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/form/fields/groupbox/AbstractGroupBox.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/form/fields/groupbox/AbstractGroupBox.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -516,6 +516,7 @@ public abstract class AbstractGroupBox extends AbstractCompositeField implements
     return CollectionUtility.arrayList(m_systemButtons);
   }
 
+  @Override
   public void setBodyGrid(IGroupBoxBodyGrid bodyGrid) {
     m_bodyGrid = bodyGrid;
   }
