ebf7c5cfb00e35bf8e5c0df9afde43d2427baa43
Paolo Bazzi <paolo.bazzi@bsi-software.com>
2025-04-14 16:59:42 +0200

    Introduce simple type wrapper impls for Date, Long, Integer, Boolean
    This wrappers may be used for simple parts of ICompositeId which
    are not used separately in API or persistence outside the composite id.
    
    411383

---
+++ org.eclipse.scout.rt.dataobject/src/main/java/org/eclipse/scout/rt/dataobject/id/DateCompositePartId.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.scout.rt.dataobject.id;
+
+import java.util.Date;
+
+/**
+ * Simple wrapper implementation wrapping a {@link Date} value.
+ * <p>
+ * <b>Note:</b> Use this class to wrap a component value of an {@link ICompositeId} which is only used within
+ * the composite id and is not directly referenced in APIs or persisted data objects. This class doesn't have
+ * a distinct {@link IdTypeName} and may therefore not be addressed in specific data object migrations.
+ */
+@IdSignature(false)
+public final class DateCompositePartId extends AbstractDateId {
+  private static final long serialVersionUID = 1L;
+
+  private DateCompositePartId(Date id) {
+    super(id);
+  }
+
+  public static DateCompositePartId of(Date value) {
+    if (value == null) {
+      return null;
+    }
+    return new DateCompositePartId(value);
+  }
+}
