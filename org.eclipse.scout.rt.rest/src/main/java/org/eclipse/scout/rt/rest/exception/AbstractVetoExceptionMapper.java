6db3629bafe5a247a424330fdf91ad9593bd2e44
Roger Stebler <Roger.Stebler@bsi-software.com>
2025-05-06 13:50:19 +0200

    Hide correlation ID for VetoExceptions thrown by REST requests
    The correlation ID is relevant to trace and debug unexpected errors.
    VetoExceptions usually are expected errors that should not need tracing.
    
    419475

--- org.eclipse.scout.rt.rest/src/main/java/org/eclipse/scout/rt/rest/exception/AbstractVetoExceptionMapper.java
+++ org.eclipse.scout.rt.rest/src/main/java/org/eclipse/scout/rt/rest/exception/AbstractVetoExceptionMapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -33,6 +33,7 @@ public abstract class AbstractVetoExceptionMapper<E extends VetoException> exten
         .withTitle(exception.getStatus().getTitle())
         .withMessage(exception.getStatus().getBody())
         .withErrorCode(exception.getStatus().getCode())
-        .withSeverity(exception.getStatus().getSeverity());
+        .withSeverity(exception.getStatus().getSeverity())
+        .withCorrelationId(false);
   }
 }
