aa8d0f05d3884c9a1cc8351e78cf3fb3e9127775
Lukas Bosshart <lukas.bosshart@bsi-software.com>
2025-05-07 16:28:59 +0200

    Don't remove cell sortCode on SmartColumn without codeType
    - Cell.sortCode can be initialized from CellModel
    - smartColumns with lookupCall instead of codeType should still support sorting by sortCode
    
    397681

--- eclipse-scout-core/src/table/columns/SmartColumn.ts
+++ eclipse-scout-core/src/table/columns/SmartColumn.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -44,7 +44,7 @@ export class SmartColumn<TValue> extends LookupCallColumn<TValue> {
 
   protected _calculateCellSortCode(cell: Cell<TValue>): number {
     if (!this.codeType) {
-      return null;
+      return cell.sortCode;
     }
     let codeType = codes.get(this.codeType);
     if (!codeType) {
