1adeb2bedd38019adf395d512fa48f66989da6b8
Matthias Otterbach <matthias.otterbach@bsi-software.com>
2025-05-28 11:03:08 +0200

    Removed default/additional description for @MaintenanceMode
    321464

--- org.eclipse.scout.rt.rest/src/main/java/org/eclipse/scout/rt/rest/maintenance/MaintenanceMode.java
+++ org.eclipse.scout.rt.rest/src/main/java/org/eclipse/scout/rt/rest/maintenance/MaintenanceMode.java
@@ -19,11 +19,15 @@ import org.eclipse.scout.rt.rest.IRestResource;
 /**
  * <p>Annotate a {@link IRestResource} to disable access if application is in maintenance mode ({@link MaintenanceModeFilter} is installed for annotated types/methods).</p>
  * <p>If annotation is used an implementation of {@link IMaintenanceModeService} must be provided (could be as simple as a bean holding a boolean attribute to enable/disable maintenance mode)</p>
- * <p>Annotation may be used on types and their methods, if it is provided on both of them the method's annotation is the relevant one (allowing to disable a resource type in general for maintenance mode and enable a specific method on the type).</p>
+ * <p>Annotation may be used on types and their methods, if it is provided on both of them the method's annotation is the relevant one (allowing to disable a resource type in general for maintenance mode and enable a specific method on the
+ * type).</p>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface MaintenanceMode {
 
-  boolean disabled() default true;
+  /**
+   * @return true to disable access to this type/method in maintenance mode; false to ignore maintenance mode resp. always allow access
+   */
+  boolean disabled();
 }
