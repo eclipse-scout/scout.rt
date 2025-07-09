4e8601c73c5c23a068feb77d06a2693a2ebf8e66
Laura Toedtli <laura.toedtli@bsi-software.com>
2025-07-09 16:34:10 +0200

    Add title to ITreeNode
    Title gives you the possibility to differentiate between
    the cell text (especially when it contains html content) and what
    the cell text represents
    
    319634

--- eclipse-scout-core/src/tree/TreeNode.ts
+++ eclipse-scout-core/src/tree/TreeNode.ts
@@ -32,6 +32,7 @@ export class TreeNode implements TreeNodeModel, ObjectWithType, FilterElement {
   parentNode: TreeNode;
   session: Session;
   text: string;
+  plainTextTitle: string;
   tooltipText: string;
   foregroundColor: string;
   backgroundColor: string;
@@ -87,6 +88,7 @@ export class TreeNode implements TreeNodeModel, ObjectWithType, FilterElement {
     this.rendered = false;
     this.session = null;
     this.text = null;
+    this.plainTextTitle = null;
 
     this._loadChildrenPromise = null;
   }
@@ -226,6 +228,10 @@ export class TreeNode implements TreeNodeModel, ObjectWithType, FilterElement {
 
   setText(text: string) {
     this.text = text;
+  }
+
+  setPlainTextTitle(plainTextTitle: string) {
+    this.plainTextTitle = plainTextTitle;
   }
 
   /**
