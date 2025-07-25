3ffba2ff0584a35d1abe8ddaabf89cb4de3c5889
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2025-07-25 08:54:07 +0200

    Don't use disabled copy overlay
    The overlay prevents a multiline string field to be scrolled when
    using Firefox. Since the Firefox bug has finally been fixed some years
    ago, the copy overlay is not necessary anymore and will be removed
    with 25.2.
    
    Firefox bug: https://bugzilla.mozilla.org/show_bug.cgi?id=253870
    Original ticket: 163273
    
    423418

--- eclipse-scout-core/src/util/Device.ts
+++ eclipse-scout-core/src/util/Device.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -511,7 +511,7 @@ export class Device implements DeviceModel, ObjectWithType {
   }
 
   supportsCopyFromDisabledInputFields(): boolean {
-    return Device.Browser.FIREFOX !== this.browser;
+    return true; // Every browser supports it now. The disabledCopyOverlay will be removed with 25.2.
   }
 
   /**
