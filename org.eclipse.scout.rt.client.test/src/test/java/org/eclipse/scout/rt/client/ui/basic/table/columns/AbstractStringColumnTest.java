69fd3b2aae322d05d652d92d0be7e2ee46e7b4fe
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2025-08-19 16:59:07 +0200

    StringColumn: truncate html content correctly
    Currently, if a column has a max length and htmlEnabled set to true,
    the html content may be truncated incorrectly resulting in UI errors.
    This happens because the Scout JS table just concatenates the table rows
    together and if the cells contain html tags especially <div> that are
    not closed, the rows won't be built correctly.
    
    Now, only the text inside the html content will be truncated preserving
    the html tags.
    
    Also: add ellipsis if text is truncated in a string column.
    
    426350

--- org.eclipse.scout.rt.client.test/src/test/java/org/eclipse/scout/rt/client/ui/basic/table/columns/AbstractStringColumnTest.java
+++ org.eclipse.scout.rt.client.test/src/test/java/org/eclipse/scout/rt/client/ui/basic/table/columns/AbstractStringColumnTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -55,7 +55,7 @@ public class AbstractStringColumnTest {
     TestStringColumn col = table.getTestStringColumn();
     col.setMaxLength(1);
     table.addRowByArray(new Object[]{"text"});
-    assertEquals("t", table.getCell(0, 0).getValue());
+    assertEquals("t…", table.getCell(0, 0).getValue());
   }
 
   public class TestTable extends AbstractTable {
