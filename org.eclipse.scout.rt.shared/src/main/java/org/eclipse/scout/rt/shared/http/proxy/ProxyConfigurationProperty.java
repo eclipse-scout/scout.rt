ca2138731ea215861061f6d78afbbb795a61f39f
Matthias Otterbach <matthias.otterbach@bsi-software.com>
2025-09-02 09:41:56 +0200

    Add additional proxy type support for ConfigurableProxySelector
    400169

--- org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/http/proxy/ProxyConfigurationProperty.java
+++ org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/http/proxy/ProxyConfigurationProperty.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -8,6 +8,8 @@
  * SPDX-License-Identifier: EPL-2.0
  */
 package org.eclipse.scout.rt.shared.http.proxy;
+
+import java.net.Proxy;
 
 import org.eclipse.scout.rt.platform.config.AbstractStringListConfigProperty;
 
@@ -21,11 +23,16 @@ public class ProxyConfigurationProperty extends AbstractStringListConfigProperty
   @Override
   @SuppressWarnings("findbugs:VA_FORMAT_STRING_USES_NEWLINE")
   public String description() {
-    return String.format("Configure proxies for the '%s'. If an URI matches a pattern the corresponding proxy will be used.\n"
-        + "By default no proxy is used.\n"
-        + "The property value is of the format REGEXP_FOR_URI=PROXY_HOST:PROXY_PORT\n"
-        + "Example:\n"
-        + "scout.http.proxyPatterns[0]=.*\\.example.com(:\\d+)?=127.0.0.1:8888\n"
-        + "scout.http.proxyPatterns[1]=.*\\.example.org(:\\d+)?=proxy.company.com", ConfigurableProxySelector.class.getSimpleName());
+    return String.format("""
+            Configure proxies for the '%s'. If an URI matches a pattern the corresponding proxy will be used.
+            By default no proxy is used.
+            The property value is of the format REGEXP_FOR_URI=PROXY_HOST:PROXY_PORT
+            Alternatively also a proxy type can be defined (must be a type of the enum %s) using this format REGEXP_FOR_URI=PROXY_TYPE=PROXY_HOST:PROXY_PORT
+            Example:
+            scout.http.proxyPatterns[0]=.*\\.example.com(:\\d+)?=127.0.0.1:8888
+            scout.http.proxyPatterns[1]=.*\\.example.org(:\\d+)?=proxy.example.com:8080\
+            scout.http.proxyPatterns[2]=.*\\.example.net(:\\d+)?=SOCKS=proxy.example.com:1080""",
+        ConfigurableProxySelector.class.getSimpleName(),
+        Proxy.Type.class.getName());
   }
 }
