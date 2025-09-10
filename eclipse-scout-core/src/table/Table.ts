6a371475043b4fe9dc4b15f609fbbdaf57fea654
Beat Schwarzentrub <bsh@bsiag.com>
2025-09-10 16:06:28 +0200

    Table: check column type before calling type-specific methods
    The pure existence of a property "backgroundEffect" does not mean that
    _renderBackgroundEffect() can be called. Instead, check if the column
    is instanceof NumberColumn.

--- eclipse-scout-core/src/table/Table.ts
+++ eclipse-scout-core/src/table/Table.ts
@@ -2970,7 +2970,10 @@ export class Table extends Widget implements TableModel, Filterable<TableRow> {
    * Meaning: Recalculates the min / max values and renders the background effect again.
    */
   protected _updateBackgroundEffect() {
-    this.columns.forEach((column: NumberColumn) => {
+    this.columns.forEach(column => {
+      if (!(column instanceof NumberColumn)) {
+        return;
+      }
       if (!column.backgroundEffect) {
         return;
       }
@@ -2982,7 +2985,10 @@ export class Table extends Widget implements TableModel, Filterable<TableRow> {
    * Recalculates the values necessary for the background effect of every column, if column.backgroundEffect is set
    */
   protected _calculateValuesForBackgroundEffect() {
-    this.columns.forEach((column: NumberColumn) => {
+    this.columns.forEach(column => {
+      if (!(column instanceof NumberColumn)) {
+        return;
+      }
       if (!column.backgroundEffect) {
         return;
       }
@@ -5549,7 +5555,10 @@ export class Table extends Widget implements TableModel, Filterable<TableRow> {
    * Renders the background effect of every column, if column.backgroundEffect is set
    */
   protected _renderBackgroundEffect() {
-    this.columns.forEach((column: NumberColumn) => {
+    this.columns.forEach(column => {
+      if (!(column instanceof NumberColumn)) {
+        return;
+      }
       if (!column.backgroundEffect) {
         return;
       }
