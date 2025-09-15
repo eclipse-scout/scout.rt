40b12b66ca5d0ac5696304883b3cd0fdb6a2c600
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2025-09-15 15:27:33 +0200

    HybridManager: make session optional
    To make it more convenient to use, session is now optional, as it is
    already the case for other singletons.
    
    430200

--- eclipse-scout-core/src/desktop/hybrid/HybridManager.ts
+++ eclipse-scout-core/src/desktop/hybrid/HybridManager.ts
@@ -8,7 +8,7 @@
  * SPDX-License-Identifier: EPL-2.0
  */
 import {
-  Event, EventHandler, EventListener, EventMapOf, Form, HybridActionEvent, HybridManagerEventMap, HybridManagerWidgetAddEvent, HybridManagerWidgetRemoveEvent, InitModelOf, ObjectOrChildModel, Session, UuidPool, Widget
+  App, Event, EventHandler, EventListener, EventMapOf, Form, HybridActionEvent, HybridManagerEventMap, HybridManagerWidgetAddEvent, HybridManagerWidgetRemoveEvent, InitModelOf, ObjectOrChildModel, scout, Session, UuidPool, Widget
 } from '../../index';
 
 /**
@@ -29,9 +29,12 @@ export class HybridManager extends Widget {
 
   // static helpers
 
-  static get(session: Session, wait?: false): HybridManager;
-  static get(session: Session, wait: true): JQuery.Promise<HybridManager>;
-  static get(session: Session, wait?: boolean): HybridManager | JQuery.Promise<HybridManager> {
+  static get(session?: Session, wait?: false): HybridManager;
+  static get(session?: Session, wait?: true): JQuery.Promise<HybridManager>;
+  static get(session?: Session, wait?: boolean): HybridManager | JQuery.Promise<HybridManager> {
+    session = session || App.get().sessions[0];
+    scout.assertParameter('session', session);
+
     const findHybridManager = () => session.desktop.addOns.find(addOn => addOn instanceof HybridManager) as HybridManager;
 
     if (!wait) {
