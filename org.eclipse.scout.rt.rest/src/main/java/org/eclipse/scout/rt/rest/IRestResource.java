4d92947ba88e2603b1704311f7ce4ab6143b0adf
Lukas Bosshart <lukas.bosshart@bsi-software.com>
2025-09-23 14:29:04 +0200

    IRestResource: Content type text/html with charset UTF-8
    - Added constant in IRestResource for html endpoints to include the
    default charset UTF-8.
    - Security: Including a charset is best practice.
     Guarantees that clients interpret the payload as UTF-8.
     Reduces the risk of XSS.
    
    #434062

--- org.eclipse.scout.rt.rest/src/main/java/org/eclipse/scout/rt/rest/IRestResource.java
+++ org.eclipse.scout.rt.rest/src/main/java/org/eclipse/scout/rt/rest/IRestResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -9,6 +9,9 @@
  */
 package org.eclipse.scout.rt.rest;
 
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
 import org.eclipse.scout.rt.platform.Bean;
 
 /**
@@ -16,4 +19,10 @@ import org.eclipse.scout.rt.platform.Bean;
  */
 @Bean
 public interface IRestResource {
+
+  /**
+   * When returning responses with content type text/html, it is best practice to include a charset.
+   * Use this constant in annotation {@link Produces} instead of {@link MediaType#TEXT_HTML}.
+   */
+  String TEXT_HTML_UTF8 = "text/html;charset=UTF-8";
 }
