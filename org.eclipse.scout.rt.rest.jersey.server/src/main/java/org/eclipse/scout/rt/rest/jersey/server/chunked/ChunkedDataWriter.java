229d81be7128c90d59d5ea9c6c23704200bbccb5
Paolo Bazzi <paolo.bazzi@bsi-software.com>
2025-10-22 17:50:24 +0200

    Support for chunked data for REST resources and REST clients
    Added support for chunked data resources and resource clients as
    Scout API using Jersey ChunkedInput and ChunkedOutput internally.
    
    Co-authored-by: matthiaso <matthias.otterbach@bsi-software.com>
    Co-authored-by: bschwarzent <beat.schwarzentrub@bsi-software.com>
    
    423389

---
+++ org.eclipse.scout.rt.rest.jersey.server/src/main/java/org/eclipse/scout/rt/rest/jersey/server/chunked/ChunkedDataWriter.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.scout.rt.rest.jersey.server.chunked;
+
+import static org.eclipse.scout.rt.platform.util.Assertions.assertNull;
+
+import java.io.IOException;
+
+import jakarta.ws.rs.core.GenericType;
+
+import org.eclipse.scout.rt.platform.util.StringUtility;
+import org.eclipse.scout.rt.rest.chunked.IChunkedDataWriter;
+import org.glassfish.jersey.server.ChunkedOutput;
+
+/**
+ * Scout REST API wrapper around Jersey {@link ChunkedOutput} instance.
+ * <p>
+ * See documentation
+ * <a href="https://eclipse-ee4j.github.io/jersey.github.io/documentation/latest/user-guide.html#chunked-output">Jersey chunked output</a>
+ */
+public class ChunkedDataWriter<T> implements IChunkedDataWriter<T> {
+
+  protected ChunkedOutput<T> m_output;
+
+  @Override
+  public void init(Class<T> type, String delimiter, int queueCapacity) {
+    assertNull(m_output, "already initialized");
+
+    // Use delimiter specified by org.glassfish.jersey.client.ChunkedInput.parser (e.g. "\r\n") if no custom delimiter was specified.
+    // Providing a null or empty delimiter to ChunkedOutput leads to no delimiter at all which cannot be parsed by ChunkedInput later on.
+    // See https://stackoverflow.com/questions/40429196/chunkedoutput-response-from-jersey-rest-services
+    byte[] delimiterBytes = (StringUtility.isNullOrEmpty(delimiter) ? "\r\n" : delimiter).getBytes();
+
+    m_output = ChunkedOutput.<T>builder(type)
+        .chunkDelimiter(delimiterBytes)
+        .queueCapacity(queueCapacity)
+        .build();
+  }
+
+  @Override
+  public GenericType<T> toEntity() {
+    return m_output;
+  }
+
+  @Override
+  public void write(T chunk) throws IOException {
+    m_output.write(chunk);
+  }
+
+  @Override
+  public boolean isClosed() {
+    return m_output.isClosed();
+  }
+
+  @Override
+  public void close() throws IOException {
+    m_output.close();
+  }
+}
