ed376f7edac6b890c20dfb3c7c4d6b175cffb74b
Roger Stebler <Roger.Stebler@bsi-software.com>
2025-11-05 14:34:22 +0100

    Avoid stale IdCodec in tests
    Ensure that tests which replace the IdCodec use a fresh instance of the
    used DataObjectMappers.
    
    This avoids scenarios where AbstractIdCodecSerializer retains the
    replaced IdCodec due to internal caching in the DataObjectMapper,
    potentially affecting subsequent tests.
    
    423389

--- org.eclipse.scout.rt.rest.jersey.test/src/test/java/org/eclipse/scout/rt/rest/jersey/server/IdSignatureRestFilterTest.java
+++ org.eclipse.scout.rt.rest.jersey.test/src/test/java/org/eclipse/scout/rt/rest/jersey/server/IdSignatureRestFilterTest.java
@@ -13,6 +13,7 @@ import static org.eclipse.scout.rt.platform.util.CollectionUtility.*;
 import static org.junit.Assert.*;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.ws.rs.client.ClientRequestFilter;
@@ -20,10 +21,14 @@ import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.client.Invocation.Builder;
 import jakarta.ws.rs.core.MediaType;
 
+import org.eclipse.scout.rt.dataobject.IDataObjectMapper;
 import org.eclipse.scout.rt.dataobject.IIdSignatureDataObjectMapper;
 import org.eclipse.scout.rt.dataobject.IPrettyPrintDataObjectMapper;
 import org.eclipse.scout.rt.dataobject.fixture.FixtureIntegerId;
 import org.eclipse.scout.rt.dataobject.id.IdCodec;
+import org.eclipse.scout.rt.jackson.dataobject.JacksonDataObjectMapper;
+import org.eclipse.scout.rt.jackson.dataobject.JacksonIdSignatureDataObjectMapper;
+import org.eclipse.scout.rt.jackson.dataobject.JacksonPrettyPrintDataObjectMapper;
 import org.eclipse.scout.rt.platform.BEANS;
 import org.eclipse.scout.rt.platform.BeanMetaData;
 import org.eclipse.scout.rt.platform.IBean;
@@ -44,17 +49,22 @@ import org.junit.runner.RunWith;
 @RunWith(PlatformTestRunner.class)
 public class IdSignatureRestFilterTest {
 
-  protected static IBean<?> s_bean;
+  protected static List<IBean<?>> s_beans = new ArrayList<>();
 
   @BeforeClass
   public static void beforeClass() {
-    s_bean = BeanTestingHelper.get().registerBean(new BeanMetaData(P_IdCodec.class).withReplace(true));
+    s_beans.add(BeanTestingHelper.get().registerBean(new BeanMetaData(P_IdCodec.class).withReplace(true)));
     BEANS.get(JerseyTestApplication.class).ensureStarted();
+
+    // create new instance of data object mapper because of the serializer cache. Otherwise, the replaced codec would still be used after the test
+    s_beans.add(BeanTestingHelper.get().registerBean(new BeanMetaData(IDataObjectMapper.class, new JacksonDataObjectMapper()).withApplicationScoped(true)));
+    s_beans.add(BeanTestingHelper.get().registerBean(new BeanMetaData(IPrettyPrintDataObjectMapper.class, new JacksonPrettyPrintDataObjectMapper()).withApplicationScoped(true)));
+    s_beans.add(BeanTestingHelper.get().registerBean(new BeanMetaData(IIdSignatureDataObjectMapper.class, new JacksonIdSignatureDataObjectMapper()).withApplicationScoped(true)));
   }
 
   @AfterClass
   public static void afterClass() {
-    BeanTestingHelper.get().unregisterBean(s_bean);
+    BeanTestingHelper.get().unregisterBeans(s_beans);
   }
 
   protected Builder request(ClientRequestFilter... filters) {
--- org.eclipse.scout.rt.server.test/src/test/java/org/eclipse/scout/rt/server/servicetunnel/ServiceTunnelIdSignatureTest.java
+++ org.eclipse.scout.rt.server.test/src/test/java/org/eclipse/scout/rt/server/servicetunnel/ServiceTunnelIdSignatureTest.java
@@ -24,10 +24,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.eclipse.scout.rt.dataobject.DataObjectHolder;
 import org.eclipse.scout.rt.dataobject.DoEntity;
 import org.eclipse.scout.rt.dataobject.DoEntityHolder;
+import org.eclipse.scout.rt.dataobject.IDataObjectMapper;
 import org.eclipse.scout.rt.dataobject.IDoEntity;
+import org.eclipse.scout.rt.dataobject.IIdSignatureDataObjectMapper;
 import org.eclipse.scout.rt.dataobject.fixture.FixtureIntegerId;
 import org.eclipse.scout.rt.dataobject.id.IdCodec;
 import org.eclipse.scout.rt.dataobject.id.IdCodec.IdCodecFlag;
+import org.eclipse.scout.rt.jackson.dataobject.JacksonDataObjectMapper;
+import org.eclipse.scout.rt.jackson.dataobject.JacksonIdSignatureDataObjectMapper;
 import org.eclipse.scout.rt.platform.BEANS;
 import org.eclipse.scout.rt.platform.BeanMetaData;
 import org.eclipse.scout.rt.platform.IBean;
@@ -68,6 +72,10 @@ public class ServiceTunnelIdSignatureTest {
         new BeanMetaData(P_IdCodec.class).withReplace(true)
     );
     BEANS.get(TestingRestClientConfigFactory.class).setupMockConnectorProvider();
+
+    // create new instance of data object mapper because of the serializer cache. Otherwise, the replaced codec would still be used after the test
+    s_beans.add(BeanTestingHelper.get().registerBean(new BeanMetaData(IDataObjectMapper.class, new JacksonDataObjectMapper()).withApplicationScoped(true)));
+    s_beans.add(BeanTestingHelper.get().registerBean(new BeanMetaData(IIdSignatureDataObjectMapper.class, new JacksonIdSignatureDataObjectMapper()).withApplicationScoped(true)));
   }
 
   @AfterClass
