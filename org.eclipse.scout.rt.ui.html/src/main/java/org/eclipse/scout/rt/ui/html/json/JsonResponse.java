91ee40ca6b1fb2549782343c01e649e32af46c9b
Niklas Sidler <niklas.sidler@bsi-software.com>
2025-11-17 16:22:26 +0100

    MalwareScanner: Add support for RemoteSystemUnavailableException
    When the MalwareScanner is not available a
    RemoteSystemUnavailableException can be thrown to adress this case.
    This allows a more detailed error handling.
    
    422721

--- org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/JsonResponse.java
+++ org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/json/JsonResponse.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -37,6 +37,7 @@ public class JsonResponse {
   public static final int ERR_UNSAFE_UPLOAD = 30;
   public static final int ERR_REJECTED_UPLOAD = 31;
   public static final int ERR_VERSION_MISMATCH = 40;
+  public static final int ERR_REMOTE_SYSTEM_UNAVAILABLE = 50;
 
   public static final String PROP_SEQUENCE_NO = "#";
   public static final String PROP_COMBINED = "combined";
