6353e60c758b57e21542368b5644573c5a93fe3f
Beat Schwarzentrub <bsh@bsiag.com>
2025-11-25 14:32:39 +0100

    Cleanup imports

--- eclipse-scout-core/src/switch/SwitchAdapter.ts
+++ eclipse-scout-core/src/switch/SwitchAdapter.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -7,7 +7,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-import {ModelAdapter} from '../session/ModelAdapter';
+import {ModelAdapter} from '../index';
 
 export class SwitchAdapter extends ModelAdapter {
 
