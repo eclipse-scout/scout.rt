738280e73311d554ad5bdd0a4ea28cc2e7b6d12d
Roger Stebler <Roger.Stebler@bsi-software.com>
2025-12-05 10:13:39 +0100

    Property for minimum password length
    Add new config property 'scout.passwordPolicy.minLength' which controls
    the minimum length for passwords checked by the password policy.
    
    441787

--- org.eclipse.scout.rt.server.test/src/test/java/org/eclipse/scout/rt/server/services/common/pwd/DefaultPasswordPolicyTest.java
+++ org.eclipse.scout.rt.server.test/src/test/java/org/eclipse/scout/rt/server/services/common/pwd/DefaultPasswordPolicyTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -9,12 +9,24 @@
  */
 package org.eclipse.scout.rt.server.services.common.pwd;
 
+import org.eclipse.scout.rt.platform.BEANS;
+import org.eclipse.scout.rt.platform.IBean;
+import org.eclipse.scout.rt.platform.config.PlatformConfigProperties.PasswordPolicyMinLengthProperty;
 import org.eclipse.scout.rt.platform.exception.VetoException;
 import org.eclipse.scout.rt.platform.security.PasswordPolicy;
+import org.eclipse.scout.rt.testing.platform.BeanTestingHelper;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class DefaultPasswordPolicyTest {
+
+  protected IBean<?> m_minLengthPropertyMockBean;
+
+  @After
+  public void after() {
+    BEANS.get(BeanTestingHelper.class).unregisterBean(m_minLengthPropertyMockBean);
+  }
 
   @Test
   public void testPolicy() {
@@ -32,6 +44,10 @@ public class DefaultPasswordPolicyTest {
 
     policy.check("uid", "12Characters_".toCharArray(), -1);
     policy.check(null, "12Characters_".toCharArray(), -1);
+
+    setMinPasswordLength(20);
+    assertVetoException(policy, "uname", "13Characters_".toCharArray(), -1);
+    policy.check("uname", "20CharacterPassword_".toCharArray(), -1);
   }
 
   private void assertVetoException(PasswordPolicy policy, String userName, char[] newPassword, int historyIndex) {
@@ -43,5 +59,10 @@ public class DefaultPasswordPolicyTest {
       hasException = true;
     }
     Assert.assertTrue(hasException);
+  }
+
+  protected void setMinPasswordLength(Integer minLength) {
+    BEANS.get(BeanTestingHelper.class).unregisterBean(m_minLengthPropertyMockBean);
+    m_minLengthPropertyMockBean = BEANS.get(BeanTestingHelper.class).mockConfigProperty(PasswordPolicyMinLengthProperty.class, minLength);
   }
 }
