9294d4eff998e677b89ea927d8c51bbd5401daae
Roger Stebler <Roger.Stebler@bsi-software.com>
2026-01-08 09:41:27 +0100

    Correction for UserId in AccessControlService of HelloJs/HelloWorld app
    423389

--- scout-hellojs-app/src/main/resources/archetype-resources/__rootArtifactId__.core/src/main/java/core/AccessControlService.java
+++ scout-hellojs-app/src/main/resources/archetype-resources/__rootArtifactId__.core/src/main/java/core/AccessControlService.java
@@ -1,11 +1,17 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.core;
+
 import org.eclipse.scout.rt.security.AbstractAccessControlService;
 import org.eclipse.scout.rt.security.IPermissionCollection;
+import org.eclipse.scout.rt.shared.user.UserId;
 
 public class AccessControlService extends AbstractAccessControlService<String> {
 
   @Override
   protected String getCurrentUserCacheKey() {
-    return Users.CURRENT.get();
+    return UserId.CURRENT.get();
   }
 
   @Override
