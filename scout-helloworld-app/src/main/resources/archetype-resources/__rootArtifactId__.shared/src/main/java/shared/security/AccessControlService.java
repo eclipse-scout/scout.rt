7bb58105537e9c82d05d33d5b605c9204c1c1233
Roger Stebler <Roger.Stebler@bsi-software.com>
2026-01-08 16:04:03 +0100

    Another correction for UserId in the HelloWorld app
    423389

--- scout-helloworld-app/src/main/resources/archetype-resources/__rootArtifactId__.shared/src/main/java/shared/security/AccessControlService.java
+++ scout-helloworld-app/src/main/resources/archetype-resources/__rootArtifactId__.shared/src/main/java/shared/security/AccessControlService.java
@@ -6,11 +6,10 @@ package ${package}.shared.security;
 import org.eclipse.scout.rt.security.AbstractAccessControlService;
 import org.eclipse.scout.rt.security.IAccessControlService;
 import org.eclipse.scout.rt.security.IPermissionCollection;
-import org.eclipse.scout.rt.shared.ISession;
 import org.eclipse.scout.rt.shared.user.UserId;
 
 /**
- * {@link IAccessControlService} service that uses {@link ISession#getUserId()} as internal cache key required by
+ * {@link IAccessControlService} service that uses {@link UserId#CURRENT} as internal cache key required by
  * {@link AbstractAccessControlService} implementation.
  * <p>
  * Replace this service at server side to load permission collection. It is <b>not</b> required to implement
