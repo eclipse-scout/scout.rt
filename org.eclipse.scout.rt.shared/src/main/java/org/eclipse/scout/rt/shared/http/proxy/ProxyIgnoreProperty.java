a0a8c9c3ed23c8340810196cb6fa4afd6ac0ce36
Roger Stebler <roger.stebler@bsi-software.com>
2026-01-21 10:44:39 +0100

    Correction for proxy selector example
    400169
    
    Cherry-pick of afb7d09c5cfe0f1efdb3964bcf9fd4596334f7af

--- org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/http/proxy/ProxyIgnoreProperty.java
+++ org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/http/proxy/ProxyIgnoreProperty.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -23,7 +23,7 @@ public class ProxyIgnoreProperty extends AbstractStringListConfigProperty {
     return "Configure the proxy ignore list for the ConfigurableProxySelector. If an URI matches the pattern no proxy connection is used.\n"
         + "By default no proxy is configured.\n"
         + "Example:\n"
-        + "scout.http.ignoreProxyPatterns[0]=https?://localhost(?::\\d+)?(?:/.*)?\n"
+        + "scout.http.ignoreProxyPatterns[0]=https?://localhost(?::\\\\d+)?(?:/.*)?\n"
         + "scout.http.ignoreProxyPatterns[1]=...";
   }
 }
