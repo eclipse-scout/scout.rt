1a9be529ae27d3fe79cfeb7a5d9ccbdd3e673173
Roger Stebler <roger.stebler@bsi-software.com>
2026-01-21 10:44:39 +0100

    Correction for proxy selector example
    400169
    
    Cherry-pick of afb7d09c5cfe0f1efdb3964bcf9fd4596334f7af

--- org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/http/proxy/ProxyConfigurationProperty.java
+++ org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/http/proxy/ProxyConfigurationProperty.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -27,8 +27,8 @@ public class ProxyConfigurationProperty extends AbstractStringListConfigProperty
         + "The property value is of the format REGEXP_FOR_URI=PROXY_HOST:PROXY_PORT\n"
         + "Alternatively also a proxy type can be defined (must be a type of the enum " + Proxy.Type.class.getName() + ") using this format REGEXP_FOR_URI=PROXY_TYPE=PROXY_HOST:PROXY_PORT\n"
         + "Example:\n"
-        + "scout.http.proxyPatterns[0]=.*\\.example.com(:\\d+)?=127.0.0.1:8888\n"
-        + "scout.http.proxyPatterns[1]=.*\\.example.org(:\\d+)?=proxy.example.com:8080\n"
-        + "scout.http.proxyPatterns[2]=.*\\.example.net(:\\d+)?=SOCKS=proxy.example.com:1080";
+        + "scout.http.proxyPatterns[0]=.*\\\\.example\\\\.com(:\\\\d+)?=127.0.0.1:8888\n"
+        + "scout.http.proxyPatterns[1]=.*\\\\.example\\\\.org(:\\\\d+)?=proxy.example.com:8080\n"
+        + "scout.http.proxyPatterns[2]=.*\\\\.example\\\\.net(:\\\\d+)?=SOCKS=proxy.example.com:1080";
   }
 }
