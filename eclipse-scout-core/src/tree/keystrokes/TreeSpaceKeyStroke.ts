fe9124e317c27d339853acfce6d771d6f4061ce5
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2026-02-03 13:03:58 +0100

    Tree: fix toggling checked node with autoCheckChildren
    With autoCheckChildren enabled: if a node is disabled, clicking a parent
    node will leave the parent node partly-checked because the disabled
    node could not be checked. A second click on the parent should now
    uncheck the node and its children. This does not happen and the parent
    is still partly-checked.
    
    443405

--- eclipse-scout-core/src/tree/keystrokes/TreeSpaceKeyStroke.ts
+++ eclipse-scout-core/src/tree/keystrokes/TreeSpaceKeyStroke.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -25,9 +25,6 @@ export class TreeSpaceKeyStroke extends KeyStroke {
   }
 
   override handle(event: JQuery.KeyboardEventBase) {
-    let selectedNodes = this.field.selectedNodes.filter(node => node.enabled);
-    // Toggle checked state to 'true', except if every node is already checked
-    let checked = selectedNodes.some(node => !node.checked);
-    selectedNodes.forEach(node => this.field.checkNode(node, checked));
+    this.field.toggleChecked(this.field.selectedNodes);
   }
 }
