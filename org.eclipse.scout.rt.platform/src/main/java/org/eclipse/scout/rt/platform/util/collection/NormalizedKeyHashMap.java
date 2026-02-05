0472a2dcce8f6bfdcac9d298a6f6ae117593f6f5
Laura Toedtli <laura.toedtli@bsi-software.com>
2026-02-05 11:43:04 +0100

    Introduce maps which normalize keys
    387153

---
+++ org.eclipse.scout.rt.platform/src/main/java/org/eclipse/scout/rt/platform/util/collection/NormalizedKeyHashMap.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.scout.rt.platform.util.collection;
+
+import java.util.HashMap;
+import java.util.function.Function;
+
+/**
+ * Map that normalizes keys before get and put. Useful if keys might be of different subclasses of K.
+ *
+ * @param <K>
+ *     the type of keys maintained by this map
+ * @param <V>
+ *     the type of mapped values
+ */
+public class NormalizedKeyHashMap<K, V> extends HashMap<K, V> {
+  private static final long serialVersionUID = 1L;
+
+  private final Function<K, K> m_normalizeKeyFunction;
+
+  public NormalizedKeyHashMap(Function<K, K> normalizeKeyFunction) {
+    m_normalizeKeyFunction = normalizeKeyFunction;
+  }
+
+  @Override
+  public V get(Object key) {
+    return super.get(normalize((K) key));
+  }
+
+  @Override
+  public V put(K key, V value) {
+    return super.put(normalize(key), value);
+  }
+
+  public K normalize(K key) {
+    return m_normalizeKeyFunction.apply(key);
+  }
+}
