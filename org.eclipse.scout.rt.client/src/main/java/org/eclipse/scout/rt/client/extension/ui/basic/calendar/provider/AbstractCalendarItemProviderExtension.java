d3327035502db780d4a3ad52b4324fc7037f127f
Niklas Sidler <niklas.sidler@bsi-software.com>
2026-02-10 13:38:34 +0100

    Calendar: improve drag n drop of components
    Calendar components can be marked as draggable by the backend. This
    allows to reschedule components by drag and drop. This change
    improves the stability of this feature.
    
    448762

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/extension/ui/basic/calendar/provider/AbstractCalendarItemProviderExtension.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/extension/ui/basic/calendar/provider/AbstractCalendarItemProviderExtension.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -44,8 +44,8 @@ public abstract class AbstractCalendarItemProviderExtension<OWNER extends Abstra
   }
 
   @Override
-  public void execItemMoved(CalendarItemProviderItemMovedChain chain, ICalendarItem item, Date fromDate, Date toDate) {
-    chain.execItemMoved(item, fromDate, toDate);
+  public void execItemMoved(CalendarItemProviderItemMovedChain chain, ICalendarItem item, Date fromDate, Date toDate, String resourceId) {
+    chain.execItemMoved(item, fromDate, toDate, resourceId);
   }
 
   @Override
