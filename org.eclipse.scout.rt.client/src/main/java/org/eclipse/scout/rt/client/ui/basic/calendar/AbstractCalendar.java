879dc7c4d657862b8ec5d0583e623cd79abc0f34
Niklas Sidler <niklas.sidler@bsi-software.com>
2026-02-19 15:43:09 +0100

    feat: propagate resourceId of moved component to ui server

--- org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/basic/calendar/AbstractCalendar.java
+++ org.eclipse.scout.rt.client/src/main/java/org/eclipse/scout/rt/client/ui/basic/calendar/AbstractCalendar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -1048,12 +1048,12 @@ public abstract class AbstractCalendar extends AbstractWidget implements ICalend
     }
 
     @Override
-    public void fireComponentMoveFromUI(CalendarComponent comp, Date fromDate, Date toDate) {
+    public void fireComponentMoveFromUI(CalendarComponent comp, Date fromDate, Date toDate, String resourceId) {
       try {
         pushUIProcessor();
         comp = resolveComponent(comp);
         if (comp != null) {
-          comp.getProvider().onItemMoved(comp.getItem(), fromDate, toDate);
+          comp.getProvider().onItemMoved(comp.getItem(), fromDate, toDate, resourceId);
         }
         fireCalendarComponentAction();
       }
