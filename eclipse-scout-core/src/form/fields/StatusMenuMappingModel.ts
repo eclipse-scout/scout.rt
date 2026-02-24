1e3f27e1acbbebe6be9a81ad33750e28fa68f4e5
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2026-02-24 08:38:20 +0100

    FieldStatus: opening tooltip with menus fails if context menu is open
    Use case:
    A field has status menu mappings to display some menus in tooltip.
    1. Click on the field status to open the context menu
    2. Click into the field to open the tooltip with menus
    -> An error occurs because the menus are still rendered in the context
    menu due to the remove animation.
    
    Solution is to close the context menu immediately when a tooltip needs
    to be shown.
    
    439853

--- eclipse-scout-core/src/form/fields/StatusMenuMappingModel.ts
+++ eclipse-scout-core/src/form/fields/StatusMenuMappingModel.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -10,7 +10,22 @@
 import {Menu, ObjectOrChildModel, StatusSeverity, WidgetModel} from '../../index';
 
 export interface StatusMenuMappingModel extends WidgetModel {
+  /**
+   * Configures the codes the status should have to display the menu.
+   *
+   * If no codes are defined, the menu is shown for every code.
+   */
   codes?: number[];
+  /**
+   * Configures the severities the status should have to display the menu.
+   *
+   * If no severities are defined, the menu is shown for every severity.
+   */
   severities?: StatusSeverity[];
-  menu?: ObjectOrChildModel<Menu>;
+  /**
+   * Configures the menu that should be shown.
+   *
+   * If an id is provided, the menu is resolved automatically using the {@link WidgetModel.parent}.
+   */
+  menu?: ObjectOrChildModel<Menu> | string;
 }
