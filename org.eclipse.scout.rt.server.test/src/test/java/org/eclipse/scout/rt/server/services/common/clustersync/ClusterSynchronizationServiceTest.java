971671a00abef683b73c93e054df98b9435d6a8d
Roger Stebler <Roger.Stebler@bsi-software.com>
2026-03-11 17:33:29 +0100

    Add per-cache statistics for InvalidateCacheNotification
    Store cluster synchronization status information using a message
    type and optional identifier key. For cache invalidation messages,
    the cache ID is used as identifier, allowing status information to
    be queried per cache via getInvalidationStatusInfo(String).
    
    214038

--- org.eclipse.scout.rt.server.test/src/test/java/org/eclipse/scout/rt/server/services/common/clustersync/ClusterSynchronizationServiceTest.java
+++ org.eclipse.scout.rt.server.test/src/test/java/org/eclipse/scout/rt/server/services/common/clustersync/ClusterSynchronizationServiceTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -30,6 +30,7 @@ import org.eclipse.scout.rt.platform.IBeanInstanceProducer;
 import org.eclipse.scout.rt.platform.IgnoreBean;
 import org.eclipse.scout.rt.platform.Replace;
 import org.eclipse.scout.rt.platform.cache.AllCacheEntryFilter;
+import org.eclipse.scout.rt.platform.cache.ICacheEntryFilter;
 import org.eclipse.scout.rt.platform.cache.InvalidateCacheNotification;
 import org.eclipse.scout.rt.platform.transaction.ITransaction;
 import org.eclipse.scout.rt.server.mom.IClusterMomDestinations;
@@ -54,8 +55,10 @@ import org.mockito.ArgumentCaptor;
 public class ClusterSynchronizationServiceTest {
   private static final NodeId TEST_NODE = NodeId.of("node");
   private static final String TEST_USER = "user";
+  private static final String TEST_CACHE_ID = "cacheId";
 
   private ClusterNotificationMessage m_message;
+  private ClusterNotificationMessage m_cacheInvalidationMessage;
   private ClusterSynchronizationService m_svc = null;
 
   private IMomImplementor m_nullMomImplementorSpy;
@@ -71,6 +74,7 @@ public class ClusterSynchronizationServiceTest {
 
     ClusterNotificationProperties testProps = new ClusterNotificationProperties(TEST_NODE, TEST_USER);
     m_message = new ClusterNotificationMessage("notification", testProps);
+    m_cacheInvalidationMessage = new ClusterNotificationMessage(new InvalidateCacheNotification(TEST_CACHE_ID, mock(ICacheEntryFilter.class)), testProps);
 
     m_svc = new ClusterSynchronizationService();
     m_svc.enable();
@@ -86,15 +90,60 @@ public class ClusterSynchronizationServiceTest {
    */
   @SuppressWarnings("unchecked")
   @Test
-  public void testReveiveInfoUpdated() {
+  public void testReceiveInfoUpdated() {
     IMessage<IClusterNotificationMessage> momMsg = mock(IMessage.class);
     when(momMsg.getTransferObject()).thenReturn(m_message);
     m_svc.onMessage(momMsg);
 
-    IClusterNodeStatusInfo nodeInfo = m_svc.getStatusInfo();
-    assertEquals(1, nodeInfo.getReceivedMessageCount());
-    assertEquals(0, nodeInfo.getSentMessageCount());
-    assertEquals(TEST_NODE, nodeInfo.getLastChangedOriginNodeId());
+    IClusterNodeStatusInfo statusInfo = m_svc.getStatusInfo();
+    assertEquals(1, statusInfo.getReceivedMessageCount());
+    assertEquals(0, statusInfo.getSentMessageCount());
+    assertEquals(TEST_NODE, statusInfo.getLastChangedOriginNodeId());
+    assertEquals(TEST_USER, statusInfo.getLastChangedUserId());
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void testStatusForMessageType() {
+    IMessage<IClusterNotificationMessage> momMsg = mock(IMessage.class);
+    when(momMsg.getTransferObject()).thenReturn(m_message);
+    m_svc.onMessage(momMsg);
+
+    IClusterNodeStatusInfo status = m_svc.getStatusInfo(String.class);
+    assertEquals(1, status.getReceivedMessageCount());
+    assertEquals(0, status.getSentMessageCount());
+    assertEquals(TEST_NODE, status.getLastChangedOriginNodeId());
+    assertEquals(TEST_USER, status.getLastChangedUserId());
+
+    assertEmptyStatusInfo(m_svc.getStatusInfo(Long.class));
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void testCacheInvalidationStatusWithoutCacheId() {
+    IMessage<IClusterNotificationMessage> momMsg = mock(IMessage.class);
+    when(momMsg.getTransferObject()).thenReturn(m_cacheInvalidationMessage);
+    m_svc.onMessage(momMsg);
+
+    // status for invalidation notification requires a cache id - getCacheInvalidationStatusInfo should be used
+    IClusterNodeStatusInfo status = m_svc.getStatusInfo(InvalidateCacheNotification.class);
+    assertEmptyStatusInfo(status);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void testCacheInvalidationStatus() {
+    IMessage<IClusterNotificationMessage> momMsg = mock(IMessage.class);
+    when(momMsg.getTransferObject()).thenReturn(m_cacheInvalidationMessage);
+    m_svc.onMessage(momMsg);
+
+    IClusterNodeStatusInfo status = m_svc.getCacheInvalidationStatusInfo(TEST_CACHE_ID);
+    assertEquals(1, status.getReceivedMessageCount());
+    assertEquals(0, status.getSentMessageCount());
+    assertEquals(TEST_NODE, status.getLastChangedOriginNodeId());
+    assertEquals(TEST_USER, status.getLastChangedUserId());
+
+    assertEmptyStatusInfo(m_svc.getCacheInvalidationStatusInfo("notExistingCacheId"));
   }
 
   /**
@@ -192,7 +241,7 @@ public class ClusterSynchronizationServiceTest {
 
   private void assertNoMessageSent() {
     verify(m_nullMomImplementorSpy, never()).publish(eq(IClusterMomDestinations.CLUSTER_NOTIFICATION_TOPIC), any(IClusterNotificationMessage.class), any(PublishInput.class));
-    assertEmptyNodeInfo(m_svc.getStatusInfo());
+    assertEmptyStatusInfo(m_svc.getStatusInfo());
   }
 
   private void assertSingleMessageSent() {
@@ -204,7 +253,7 @@ public class ClusterSynchronizationServiceTest {
     assertEquals("default", statusInfo.getLastChangedUserId());
   }
 
-  private void assertEmptyNodeInfo(IClusterNodeStatusInfo status) {
+  private void assertEmptyStatusInfo(IClusterNodeStatusInfo status) {
     assertEquals(0, status.getReceivedMessageCount());
     assertEquals(0, status.getSentMessageCount());
     assertNull(status.getLastChangedOriginNodeId());
