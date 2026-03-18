e1749949aa86f4822b69362d1fd699f19ccc972a
Beat Schwarzentrub <bsh@bsiag.com>
2026-03-18 14:03:11 +0100

    Add @TypeVersion to UiPreferencesDo
    To support the migration of a persisted UiPreferencesDo in the future,
    an initial @TypeVersion was added.
    
    454123

--- org.eclipse.scout.rt.api.data/src/main/java/org/eclipse/scout/rt/api/data/prefs/UiPreferencesDo.java
+++ org.eclipse.scout.rt.api.data/src/main/java/org/eclipse/scout/rt/api/data/prefs/UiPreferencesDo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -17,9 +17,12 @@ import jakarta.annotation.Generated;
 import org.eclipse.scout.rt.api.data.table.TableClientUiPreferencesDo;
 import org.eclipse.scout.rt.dataobject.DoEntity;
 import org.eclipse.scout.rt.dataobject.DoList;
+import org.eclipse.scout.rt.dataobject.ScoutTypeVersions.Scout_25_2_002__uiPreferences;
 import org.eclipse.scout.rt.dataobject.TypeName;
+import org.eclipse.scout.rt.dataobject.TypeVersion;
 
 @TypeName("scout.UiPreferences")
+@TypeVersion(Scout_25_2_002__uiPreferences.class)
 public class UiPreferencesDo extends DoEntity {
 
   public DoList<TableClientUiPreferencesDo> tablePreferences() {
