362008bd6f6ce7e5fec3356e73edeb1db9a3fc3b
Matthias Villiger <mvi@bsi-software.com>
2026-04-01 09:39:01 +0200

    Use ConfigurableContentSecurityPolicy for dynamic resources.
    Retain insertion order for IHttpResponseInterceptors on HttpCacheObject
    and BinaryResourceHolder. This prevents random behavior if multiple
    interceptors of the same BinaryResource change the same aspect of the
    response (e.g. the same HTTP header).
    
    This is especially important for the interceptor adding the default CSP
    for dynamic resources (see DynamicResourceLoader#loadResource). It must
    be called first (for the HttpCacheObject) to allow the interceptors
    registered in the BinaryResourceHolder to customize it.
    
    453833

--- org.eclipse.scout.rt.ui.html.test/src/test/java/org/eclipse/scout/rt/ui/html/cache/HttpCacheObjectTest.java
+++ org.eclipse.scout.rt.ui.html.test/src/test/java/org/eclipse/scout/rt/ui/html/cache/HttpCacheObjectTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -9,11 +9,21 @@
  */
 package org.eclipse.scout.rt.ui.html.cache;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doAnswer;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.eclipse.scout.rt.platform.holders.StringHolder;
 import org.eclipse.scout.rt.platform.resource.BinaryResources;
 import org.eclipse.scout.rt.platform.util.Assertions;
+import org.eclipse.scout.rt.security.csp.ContentSecurityPolicy;
 import org.eclipse.scout.rt.server.commons.servlet.cache.HttpCacheKey;
 import org.eclipse.scout.rt.server.commons.servlet.cache.HttpCacheObject;
+import org.eclipse.scout.rt.server.commons.servlet.cache.IHttpResponseInterceptor;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class HttpCacheObjectTest {
 
@@ -35,5 +45,28 @@ public class HttpCacheObjectTest {
   @Test
   public void testOkOk() {
     new HttpCacheObject(new HttpCacheKey(null), BinaryResources.create().build());
+  }
+
+  /**
+   * Tests insertion order stability of IHttpResponseInterceptor executions.
+   */
+  @Test
+  public void testInterceptorOrder() {
+    HttpCacheObject httpCacheObject = new HttpCacheObject(new HttpCacheKey(null), BinaryResources.create().build());
+    IHttpResponseInterceptor i1 = (req, resp) -> resp.setHeader(ContentSecurityPolicy.HTTP_HEADER, "1");
+    IHttpResponseInterceptor i2 = (req, resp) -> resp.setHeader(ContentSecurityPolicy.HTTP_HEADER, "2");
+    IHttpResponseInterceptor i3 = (req, resp) -> resp.setHeader(ContentSecurityPolicy.HTTP_HEADER, "3");
+    httpCacheObject.addHttpResponseInterceptor(i1);
+    httpCacheObject.addHttpResponseInterceptor(i2);
+    httpCacheObject.addHttpResponseInterceptor(i3);
+
+    HttpServletResponse resp = Mockito.mock(HttpServletResponse.class);
+    StringHolder result = new StringHolder();
+    doAnswer(a -> {
+      result.setValue(a.getArgument(1));
+      return null;
+    }).when(resp).setHeader(anyString(), anyString());
+    httpCacheObject.applyHttpResponseInterceptors(null, resp);
+    assertEquals("3", result.getValue());
   }
 }
