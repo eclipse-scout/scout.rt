1e7a7afb18f8840356b1c95d587e1f983c82f38a
Matthias Villiger <mvi@bsi-software.com>
2026-04-01 09:39:01 +0200

    Use ConfigurableContentSecurityPolicy for dynamic resources.
    Retain insertion order for IHttpResponseInterceptors on HttpCacheObject
    and BinaryResourceHolder. This prevents random behavior if multiple
    interceptors of the same BinaryResource change the same aspect of the
    response (e.g. the same HTTP header).
    
    This is especially important for the interceptor adding the default CSP
    for dynamic resources (see DynamicResourceLoader#loadResource). It must
    be called first (for the HttpCacheObject) to allow the interceptors
    registered in the BinaryResourceHolder to customize it.
    
    453833

--- org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/res/BinaryResourceHolder.java
+++ org.eclipse.scout.rt.ui.html/src/main/java/org/eclipse/scout/rt/ui/html/res/BinaryResourceHolder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -9,7 +9,7 @@
  */
 package org.eclipse.scout.rt.ui.html.res;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import org.eclipse.scout.rt.platform.resource.BinaryResource;
@@ -21,14 +21,14 @@ import org.eclipse.scout.rt.server.commons.servlet.cache.IHttpResponseIntercepto
 public class BinaryResourceHolder {
 
   private final BinaryResource m_binaryResource;
-  private final Set<IHttpResponseInterceptor> m_httpResponseInterceptors = new HashSet<>();
+  private final Set<IHttpResponseInterceptor> m_httpResponseInterceptors = new LinkedHashSet<>();
 
   public BinaryResourceHolder(BinaryResource binaryResource) {
     m_binaryResource = binaryResource;
   }
 
   /**
-   * @return the provided binary resource (may be <code>null</code>)
+   * @return the provided binary resource or {@code null}.
    */
   public BinaryResource get() {
     return m_binaryResource;
@@ -45,7 +45,7 @@ public class BinaryResourceHolder {
   /**
    * @return live set of associated {@link IHttpResponseInterceptor} (although it is recommended to use the
    * {@link #addHttpResponseInterceptor(IHttpResponseInterceptor)} and
-   * {@link #removeHttpResponseInterceptor(IHttpResponseInterceptor)} methods). Never <code>null</code>.
+   * {@link #removeHttpResponseInterceptor(IHttpResponseInterceptor)} methods). Never {@code null}.
    */
   public Set<IHttpResponseInterceptor> getHttpResponseInterceptors() {
     return m_httpResponseInterceptors;
