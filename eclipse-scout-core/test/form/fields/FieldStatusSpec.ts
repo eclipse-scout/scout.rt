eclipse-scout/scout.rt

commit 811f99656ecf58e445596554b3f1451eef046a40

aria: make field status accessible
The field status now gets the proper aria roles and can be accessed
using tab.

Also:
- Support keyboard navigation for menus in tooltip
  Because a field status can open a tooltip with menus, the menus need
  to support keyboard-navigation
- Menu: support selected state
  A context menu uses the css class 'selected' during keyboard
  navigation to highlight the current menu item. This breaks two things:
  - The regular selected state cannot be visualized correctly.
  - If a tooltip has a hierarchical menu, the menu is opened as context
    menu popup. Closing it again with ESC does not focus the parent menu
    item in the tooltip because the selected state is removed by the
    menu item.

Changes:
- The menu item now uses the class 'focused' instead of 'selected' to
  for the keyboard navigation.
- The class 'selected' now properly visualizes the selected state of a
  menu item. The state is now also correctly synchronized between a
  context menu item and the original menu item.
- If the context menu item only toggles a sub menu or a form popup, the
  selected state must not be synchronized (existing behavior). The logic
  in Menu.ts had to be changed slightly to support this case properly.
- The tooltip now better visualizes the different menu item states with
  different severities

394500

eclipse-scout-core/src/form/fields/FieldStatus.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -8,8 +8,8 @@
  * SPDX-License-Identifier: EPL-2.0
  */
 import {
-  aria, arrays, ContextMenuPopup, EventHandler, FieldStatusEventMap, FieldStatusModel, FormField, FormFieldStatusPosition, HierarchyChangeEvent, HtmlComponent, Menu, PropertyChangeEvent, scout, Status, StatusOrModel, strings, Tooltip,
-  Widget
+  aria, arrays, ContextMenuPopup, EventHandler, FieldStatusEventMap, FieldStatusExecKeyStroke, FieldStatusModel, FormField, FormFieldStatusPosition, HierarchyChangeEvent, HtmlComponent, KeyStrokeContext, Menu, PropertyChangeEvent, scout,
+  Status, StatusOrModel, strings, Tooltip, Widget
 } from '../../index';
 
 export class FieldStatus extends Widget implements FieldStatusModel {
@@ -37,16 +37,33 @@ export class FieldStatus extends Widget implements FieldStatusModel {
     this.updating = false;
     this.autoRemove = true;
     this.position = FormField.StatusPosition.DEFAULT;
+    this.inheritAccessibility = false;
+    this.preventInitialFocus = true;
+    this.preventClickFocus = true;
+    this.menus = [];
 
     this._parents = [];
     this._parentPropertyChangeListener = this._onParentPropertyChange.bind(this);
     this._parentHierarchyChangeListener = this._onParentHierarchyChange.bind(this);
   }
 
+  protected override _createKeyStrokeContext(): KeyStrokeContext {
+    return new KeyStrokeContext();
+  }
+
+  protected override _initKeyStrokeContext() {
+    super._initKeyStrokeContext();
+    this.keyStrokeContext.registerKeyStroke(new FieldStatusExecKeyStroke(this));
+  }
+
   protected override _render() {
     this.$container = this.$parent.appendSpan('status')
       .on('mousedown', this._onStatusMouseDown.bind(this));
     this.htmlComp = HtmlComponent.install(this.$container, this.session);
+    aria.role(this.$container, 'button');
+    aria.hasPopup(this.$container, 'menu');
+    aria.expanded(this.$container, false);
+    this._updateVisibility();
   }
 
   protected override _remove() {
@@ -65,6 +82,16 @@ export class FieldStatus extends Widget implements FieldStatusModel {
   protected override _renderProperties() {
     super._renderProperties();
     this._renderPosition();
+  }
+
+  protected override _renderEnabled() {
+    super._renderEnabled();
+    this._updateTabbable();
+  }
+
+  protected _updateTabbable() {
+    let hasMenus = !!this.menus.length;
+    this.$container.setTabbable(hasMenus && this.enabledComputed);
   }
 
   update(status: StatusOrModel, menus: Menu | Menu[], autoRemove: boolean, showStatus?: boolean) {
@@ -93,6 +120,30 @@ export class FieldStatus extends Widget implements FieldStatusModel {
     if (!this.updating) {
       this._updatePopup();
     }
+    this._updateAriaLabel();
+    this._updateVisibility();
+  }
+
+  protected _updateVisibility() {
+    let invisible = !this.menus.length && !this.status;
+    if (invisible && this.isFocused()) {
+      this.session.focusManager.focusNextTabbable(this.getFocusableElement());
+    }
+    this.$container.toggleClass('invisible', invisible);
+  }
+
+  protected _updateAriaLabel() {
+    let hasMenus = this.menus.length > 0;
+
+    let label = this.session.text('ui.MoreInformation');
+    if (hasMenus) {
+      label = this.session.text('ui.MoreActions');
+    } else if (this.status?.isWarning()) {
+      label = this.session.text('ui.Warning');
+    } else if (this.status?.isError()) {
+      label = this.session.text('ui.ErrorMessage');
+    }
+    aria.label(this.$container, label);
   }
 
   setPosition(position: FormFieldStatusPosition) {
@@ -119,6 +170,9 @@ export class FieldStatus extends Widget implements FieldStatusModel {
     if (!this.updating) {
       this._updatePopup();
     }
+    this._updateAriaLabel();
+    this._updateVisibility();
+    this._updateTabbable();
   }
 
   setAutoRemove(autoRemove: boolean) {
@@ -145,7 +199,7 @@ export class FieldStatus extends Widget implements FieldStatusModel {
     if (!this._requiresTooltip()) {
       this.hideTooltip();
     }
-    if (arrays.empty(this.menus)) {
+    if (!this.menus.length) {
       this.hideContextMenu();
     }
     if (showStatus === true) {
@@ -159,7 +213,7 @@ export class FieldStatus extends Widget implements FieldStatusModel {
     if (!this.status || !this.rendered) {
       return false;
     }
-    if (arrays.empty(this.menus) && !strings.hasText(this.status.message)) {
+    if (!this.menus.length && !strings.hasText(this.status.message)) {
       return false;
     }
     return true;
@@ -197,10 +251,14 @@ export class FieldStatus extends Widget implements FieldStatusModel {
       this.tooltip.render();
       aria.role(this.tooltip.$content, 'alert');
       this.$container.addClass('selected');
+      aria.expanded(this.$container, true);
+      aria.linkElementWithControls(this.$container, this.tooltip.$container);
       this.tooltip.one('destroy', () => {
         this.tooltip = null;
         if (this.$container) {
           this.$container.removeClass('selected');
+          aria.expanded(this.$container, false);
+          aria.removeControls(this.$container);
         }
       });
     }
@@ -234,10 +292,14 @@ export class FieldStatus extends Widget implements FieldStatusModel {
     });
     this.contextMenu.open();
     this.$container.addClass('selected');
+    aria.expanded(this.$container, true);
+    aria.linkElementWithControls(this.$container, this.contextMenu.$container);
     this.contextMenu.one('destroy', () => {
       this.contextMenu = null;
       if (this.$container) {
         this.$container.removeClass('selected');
+        aria.expanded(this.$container, false);
+        aria.removeControls(this.$container);
       }
     });
   }
@@ -288,7 +350,23 @@ export class FieldStatus extends Widget implements FieldStatusModel {
   protected _onStatusMouseDown(event: JQuery.MouseDownEvent) {
     let statusDownEvent = this.trigger('statusMouseDown', event);
     if (!statusDownEvent.defaultPrevented) {
-      this.togglePopup();
+      this.doAction();
+    }
+  }
+
+  doAction() {
+    this.togglePopup();
+
+    // Ensure the user can use keyboard to select the menus inside the tooltip.
+    // Ideally, a tooltip would always be a focus context if it had menus, but some status tooltips will be opened during field input.
+    // In that case we do not want the tooltip to take the focus away from the input
+    // -> Only do it when the user explicitly requested the opening of the tooltip.
+    let withFocusContext = this.menus.length > 0;
+    this.tooltip?.setWithFocusContext(withFocusContext);
+
+    // Remove 'alert' role to prevent a screen reader from reading it twice if it gains focus
+    if (withFocusContext) {
+      aria.role(this.tooltip?.$content, null);
     }
   }
 

eclipse-scout-core/test/form/fields/FieldStatusSpec.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -7,7 +7,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-import {GroupBox, Menu, scout, SmartField, Status, StringField} from '../../../src/index';
+import {GroupBox, Menu, scout, SmartField, Status, StringField, ValueField} from '../../../src/index';
 import {FormSpecHelper, JQueryTesting} from '../../../src/testing/index';
 
 describe('FieldStatus', () => {
@@ -22,6 +22,92 @@ describe('FieldStatus', () => {
 
   afterEach(() => {
     jasmine.clock().uninstall();
+  });
+
+  describe('visibility', () => {
+    it('is invisible if it has no menus and no status', () => {
+      let model = helper.createFieldModel(StringField);
+      let formField = new StringField();
+      formField.init(model);
+      formField.render();
+      expect(formField.fieldStatus.$container).toHaveClass('invisible');
+
+      formField.setErrorStatus({
+        severity: Status.Severity.ERROR,
+        message: 'foo'
+      });
+      expect(formField.fieldStatus.$container).not.toHaveClass('invisible');
+
+      formField.setErrorStatus(null);
+      expect(formField.fieldStatus.$container).toHaveClass('invisible');
+
+      formField.setTooltipText('text');
+      expect(formField.fieldStatus.$container).not.toHaveClass('invisible');
+
+      formField.setTooltipText(null);
+      expect(formField.fieldStatus.$container).toHaveClass('invisible');
+
+      formField.setMenus([{objectType: Menu}]);
+      expect(formField.fieldStatus.$container).not.toHaveClass('invisible');
+
+      formField.setMenus(null);
+      expect(formField.fieldStatus.$container).toHaveClass('invisible');
+    });
+
+    it('is invisible if menusVisible is false', () => {
+      let model = helper.createFieldModel(StringField);
+      let formField = new StringField();
+      formField.init(model);
+      formField.render();
+      expect(formField.fieldStatus.$container).toHaveClass('invisible');
+
+      formField.setMenus([{objectType: Menu}]);
+      expect(formField.fieldStatus.$container).not.toHaveClass('invisible');
+
+      formField.setMenusVisible(false);
+      expect(formField.fieldStatus.$container).toHaveClass('invisible');
+
+      formField.setMenusVisible(true);
+      expect(formField.fieldStatus.$container).not.toHaveClass('invisible');
+    });
+
+    it('is invisible if it has no visible menus', () => {
+      let model = helper.createFieldModel(StringField);
+      let formField = new StringField();
+      formField.init(model);
+      formField.render();
+      expect(formField.fieldStatus.$container).toHaveClass('invisible');
+
+      formField.setMenus([{objectType: Menu, menuTypes: [ValueField.MenuType.NotNull]}]);
+      expect(formField.fieldStatus.$container).toHaveClass('invisible');
+
+      formField.setValue('value');
+      expect(formField.fieldStatus.$container).not.toHaveClass('invisible');
+
+      formField.setValue(null);
+      expect(formField.fieldStatus.$container).toHaveClass('invisible');
+    });
+
+    it('focuses next focusable element if it gets invisible while focused', () => {
+      let groupBox = scout.create(GroupBox, {
+        parent: session.desktop,
+        fields: [{
+          objectType: StringField,
+          menus: [{objectType: Menu, menuTypes: [ValueField.MenuType.NotNull]}],
+          value: 'hi'
+        }, {
+          objectType: StringField
+        }]
+      });
+      groupBox.render();
+      groupBox.fields[0].fieldStatus.focus();
+      expect(groupBox.fields[0].fieldStatus.$container).not.toHaveClass('invisible');
+      expect(groupBox.fields[0].fieldStatus.get$Focusable()).toBeFocused();
+
+      (groupBox.fields[0] as StringField).setValue(null);
+      expect(groupBox.fields[0].fieldStatus.get$Focusable()).toHaveClass('invisible');
+      expect(groupBox.fields[1].get$Focusable()).toBeFocused();
+    });
   });
 
   describe('parent changes visibility', () => {
@@ -177,6 +263,56 @@ describe('FieldStatus', () => {
       });
       formField.setErrorStatus(status1);
       expect(formField.fieldStatus.tooltip.$content).toHaveAttr('role', 'alert');
+    });
+
+    it('toggles expanded and controls attributes of tooltip', () => {
+      let model = helper.createFieldModel();
+      let formField = new StringField();
+      formField.init(model);
+      formField.render();
+      expect(formField.fieldStatus.$container).toHaveAttr('role', 'button');
+      expect(formField.fieldStatus.$container).toHaveAttr('aria-haspopup', 'menu');
+      expect(formField.fieldStatus.$container).toHaveAttr('aria-expanded', 'false');
+      expect(formField.fieldStatus.$container).not.toHaveAttr('aria-controls');
+
+      formField.setTooltipText('hi there');
+      expect(formField.fieldStatus.$container).not.toHaveAttr('aria-controls');
+      expect(formField.fieldStatus.$container).toHaveAttr('aria-expanded', 'false');
+
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.$container).toHaveAttr('aria-controls', formField.fieldStatus.tooltip.$container.attr('id'));
+      expect(formField.fieldStatus.$container).toHaveAttr('aria-expanded', 'true');
+
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.$container).not.toHaveAttr('aria-controls');
+      expect(formField.fieldStatus.$container).toHaveAttr('aria-expanded', 'false');
+    });
+
+    it('toggles expanded and controls attributes of menu', () => {
+      let model = helper.createFieldModel();
+      let formField = new StringField();
+      formField.init(model);
+      formField.render();
+      expect(formField.fieldStatus.$container).toHaveAttr('role', 'button');
+      expect(formField.fieldStatus.$container).toHaveAttr('aria-haspopup', 'menu');
+      expect(formField.fieldStatus.$container).toHaveAttr('aria-expanded', 'false');
+      expect(formField.fieldStatus.$container).not.toHaveAttr('aria-controls');
+
+      formField.setMenus([{objectType: Menu}]);
+      expect(formField.fieldStatus.$container).not.toHaveAttr('aria-controls');
+      expect(formField.fieldStatus.$container).toHaveAttr('aria-expanded', 'false');
+
+      session.desktop.one('popupOpen', event => {
+        event.popup.animateOpening = false;
+      });
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.$container).toHaveAttr('aria-controls', formField.fieldStatus.contextMenu.$container.attr('id'));
+      expect(formField.fieldStatus.$container).toHaveAttr('aria-expanded', 'true');
+
+      formField.fieldStatus.contextMenu.animateRemoval = false;
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.$container).not.toHaveAttr('aria-controls');
+      expect(formField.fieldStatus.$container).toHaveAttr('aria-expanded', 'false');
     });
   });
 });

eclipse-scout-core/src/form/FormMenu.ts
@@ -116,7 +116,7 @@ export class FormMenu extends Menu implements FormMenuModel {
 
   protected _setSelected(selected: boolean) {
     this._setProperty('selected', selected);
-    if (this.popupStyle === FormMenu.PopupStyle.MOBILE && this._doActionTogglesPopup()) {
+    if (this.popupStyle === FormMenu.PopupStyle.MOBILE && this.form) {
       // Mobile Popup can be rendered even if menu is not. This is useful if a tool form menu should be opened while the desktop bench is open instead of the outline
       // Open will be called in renderSelected again but won't do anything
       if (this.selected) {
@@ -142,7 +142,15 @@ export class FormMenu extends Menu implements FormMenuModel {
     // (either by an exception if its already open, or it may be rendered into the wrong menu).
     // To prevent that, we ensure the other popup is really closed before opening the new one.
     this._closeOtherPopupsForSameMenu();
-    return super._canOpenPopup();
+    if (!super._canOpenPopup()) {
+      return false;
+    }
+
+    // Recheck if opening is still possible (maybe destroying the popup changed that, e.g. form was set to null)
+    if (!this.form) {
+      return false;
+    }
+    return true;
   }
 
   protected _closeOtherPopupsForSameMenu() {
@@ -209,7 +217,7 @@ export class FormMenu extends Menu implements FormMenuModel {
   }
 
   protected override _doActionTogglesPopup(): boolean {
-    return !!this.form;
+    return true;
   }
 
   override updateAriaRole() {

eclipse-scout-core/src/form/fields/FieldStatusExecKeyStroke.ts
@@ -1,0 +1,26 @@
+/*
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+import {keys, KeyStroke} from '../../index';
+import {FieldStatus} from './FieldStatus';
+
+export class FieldStatusExecKeyStroke extends KeyStroke {
+  declare field: FieldStatus;
+
+  constructor(fieldStatus: FieldStatus) {
+    super();
+    this.field = fieldStatus;
+    this.which = [keys.SPACE, keys.ENTER];
+    this.stopPropagation = true;
+  }
+
+  override handle(event: JQuery.KeyboardEventBase) {
+    this.field.doAction();
+  }
+}

commit a9686f41c66ca9cadeee9c8dc62091907949821c

FieldStatus: menus are enabled even though field is disabled
If a field is disabled, the menus should be disabled as well, unless
they have inheritAccessibility set to false. This broke with commit
811f99656ecf58e445596554b3f1451eef046a40.

Before that commit, the field status was accessible even if it was
disabled -> it ignored the enabled property completely, which was
confusing. This is why inheritAccessibility was set to false on the
field status and the css rules and click handlers were adjusted to
respect the enabled property. Unfortunately, the child menus are now
always enabled because their parent (field status if a context menu is
open) is always enabled.

To fix it, the field status now passes the enabled state of its parent
to its children instead of its own state when the enabled state of its
children is computed.

Note: a regular menu has a slight different semantic: it is disabled, if
every child menu is disabled and enabled, if a child menu is enabled.
This would be a good behavior for field status as well, but the field
status is not a menu and that behavior is implemented only for menus
(see Menu.ts#recomputeEnabled)

455532

eclipse-scout-core/src/form/fields/FieldStatus.ts
@@ -93,6 +93,12 @@ export class FieldStatus extends Widget implements FieldStatusModel {
     this.$container.setTabbable(hasMenus && this.enabledComputed && !Device.get().supportsOnlyTouch());
   }
 
+  protected override _updateEnabledComputed(enabledComputed: boolean, enabledComputedForChildren?: boolean) {
+    // The enabled state of the field status is irrelevant for its child menus -> always pass the state of the field
+    // This is because the field status should be enabled even if the parent field is disabled (inheritAccessibility is false), but the actual menus should be disabled
+    super._updateEnabledComputed(enabledComputed, this.parent.enabledComputed);
+  }
+
   update(status: StatusOrModel, menus: Menu | Menu[], autoRemove: boolean, showStatus?: boolean) {
     this.updating = true;
     this.setStatus(status);
@@ -289,6 +295,7 @@ export class FieldStatus extends Widget implements FieldStatusModel {
       aria.expanded(this.$container, true);
       aria.linkElementWithControls(this.$container, this.tooltip.$container);
       this._updateParentListeners();
+      this.recomputeEnabled(); // triggers _updateEnabledComputed
       this.tooltip.one('destroy', () => {
         this.tooltip = null;
         this._removeParentListeners();
@@ -336,6 +343,7 @@ export class FieldStatus extends Widget implements FieldStatusModel {
     this.$container.addClass('selected');
     aria.expanded(this.$container, true);
     aria.linkElementWithControls(this.$container, this.contextMenu.$container);
+    this.recomputeEnabled(); // triggers _updateEnabledComputed
     this.contextMenu.one('destroy', () => {
       this.contextMenu = null;
       if (this.$container) {

eclipse-scout-core/test/form/fields/FieldStatusSpec.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -107,6 +107,170 @@ describe('FieldStatus', () => {
       (groupBox.fields[0] as StringField).setValue(null);
       expect(groupBox.fields[0].fieldStatus.get$Focusable()).toHaveClass('invisible');
       expect(groupBox.fields[1].get$Focusable()).toBeFocused();
+    });
+  });
+
+  describe('enabled', () => {
+    it('is enabled but children disabled even if parent is disabled', () => {
+      let formField = scout.create(StringField, {
+        parent: session.desktop,
+        enabled: false,
+        menus: [{
+          objectType: Menu,
+          childActions: [{objectType: Menu}]
+        }]
+      });
+      formField.render();
+      expect(formField.enabledComputed).toBe(false);
+      expect(formField.fieldStatus.enabledComputed).toBe(true);
+      expect(formField.menus[0].enabledComputed).toBe(false);
+      expect(formField.menus[0].childActions[0].enabledComputed).toBe(false);
+
+      // open menu
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.contextMenu.menuItems[0].enabledComputed).toBe(false);
+      expect(formField.fieldStatus.contextMenu.menuItems[0].childActions[0].enabledComputed).toBe(false);
+
+      // close menu
+      formField.fieldStatus.contextMenu.animateRemoval = false;
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.contextMenu).toBe(null);
+
+      formField.setEnabled(true);
+      expect(formField.enabledComputed).toBe(true);
+      expect(formField.fieldStatus.enabledComputed).toBe(true);
+      expect(formField.menus[0].enabledComputed).toBe(true);
+      expect(formField.menus[0].childActions[0].enabledComputed).toBe(true);
+
+      // open menu
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.contextMenu.menuItems[0].enabledComputed).toBe(true);
+      expect(formField.fieldStatus.contextMenu.menuItems[0].childActions[0].enabledComputed).toBe(true);
+
+      // close menu
+      formField.fieldStatus.contextMenu.animateRemoval = false;
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.contextMenu).toBe(null);
+
+      formField.setEnabled(false);
+      expect(formField.enabledComputed).toBe(false);
+      expect(formField.fieldStatus.enabledComputed).toBe(true);
+      expect(formField.menus[0].enabledComputed).toBe(false);
+      expect(formField.menus[0].childActions[0].enabledComputed).toBe(false);
+
+      // open menu
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.contextMenu.menuItems[0].enabledComputed).toBe(false);
+      expect(formField.fieldStatus.contextMenu.menuItems[0].childActions[0].enabledComputed).toBe(false);
+
+      formField.insertMenu({objectType: Menu});
+      expect(formField.menus[1].enabledComputed).toBe(false);
+      expect(formField.enabledComputed).toBe(false);
+      expect(formField.fieldStatus.enabledComputed).toBe(true);
+
+      // close menu
+      formField.fieldStatus.contextMenu.animateRemoval = false;
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.contextMenu).toBe(null);
+
+      // open menu
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.contextMenu.menuItems[1].enabledComputed).toBe(false);
+
+      // close menu
+      formField.fieldStatus.contextMenu.animateRemoval = false;
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.contextMenu).toBe(null);
+
+      formField.insertMenu({objectType: Menu, inheritAccessibility: false});
+      expect(formField.menus[2].enabledComputed).toBe(true);
+      expect(formField.enabledComputed).toBe(false);
+      expect(formField.fieldStatus.enabledComputed).toBe(true);
+
+      // open menu
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.contextMenu.menuItems[1].enabledComputed).toBe(false);
+      expect(formField.fieldStatus.contextMenu.menuItems[2].enabledComputed).toBe(true);
+    });
+
+    it('is enabled but children disabled even if parent is disabled and menus are shown in tooltip', () => {
+      let formField = scout.create(StringField, {
+        parent: session.desktop,
+        enabled: false,
+        tooltipText: 'hi',
+        menus: [{
+          objectType: Menu,
+          childActions: [{objectType: Menu}]
+        }]
+      });
+      formField.render();
+      expect(formField.enabledComputed).toBe(false);
+      expect(formField.fieldStatus.enabledComputed).toBe(true);
+      expect(formField.menus[0].enabledComputed).toBe(false);
+      expect(formField.menus[0].childActions[0].enabledComputed).toBe(false);
+
+      // open tooltip
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.tooltip.menus[0].enabledComputed).toBe(false);
+      expect(formField.fieldStatus.tooltip.menus[0].childActions[0].enabledComputed).toBe(false);
+
+      // close tooltip
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.tooltip).toBe(null);
+
+      formField.setEnabled(true);
+      expect(formField.enabledComputed).toBe(true);
+      expect(formField.fieldStatus.enabledComputed).toBe(true);
+      expect(formField.menus[0].enabledComputed).toBe(true);
+      expect(formField.menus[0].childActions[0].enabledComputed).toBe(true);
+
+      // open tooltip
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.tooltip.menus[0].enabledComputed).toBe(true);
+      expect(formField.fieldStatus.tooltip.menus[0].childActions[0].enabledComputed).toBe(true);
+
+      // close tooltip
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.tooltip).toBe(null);
+
+      formField.setEnabled(false);
+      expect(formField.enabledComputed).toBe(false);
+      expect(formField.fieldStatus.enabledComputed).toBe(true);
+      expect(formField.menus[0].enabledComputed).toBe(false);
+      expect(formField.menus[0].childActions[0].enabledComputed).toBe(false);
+
+      // open tooltip
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.tooltip.menus[0].enabledComputed).toBe(false);
+      expect(formField.fieldStatus.tooltip.menus[0].childActions[0].enabledComputed).toBe(false);
+
+      // close tooltip
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.tooltip).toBe(null);
+
+      formField.insertMenu({objectType: Menu});
+      expect(formField.menus[1].enabledComputed).toBe(false);
+      expect(formField.enabledComputed).toBe(false);
+      expect(formField.fieldStatus.enabledComputed).toBe(true);
+
+      // open tooltip
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.tooltip.menus[1].enabledComputed).toBe(false);
+
+      // close tooltip
+      formField.fieldStatus.tooltip.animateRemoval = false;
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.tooltip).toBe(null);
+
+      formField.insertMenu({objectType: Menu, inheritAccessibility: false});
+      expect(formField.menus[2].enabledComputed).toBe(true);
+      expect(formField.enabledComputed).toBe(false);
+      expect(formField.fieldStatus.enabledComputed).toBe(true);
+
+      // open tooltip
+      formField.fieldStatus.doAction();
+      expect(formField.fieldStatus.tooltip.menus[1].enabledComputed).toBe(false);
+      expect(formField.fieldStatus.tooltip.menus[2].enabledComputed).toBe(true);
     });
   });
 