d7a96354a0c92839513cdcb4b1faa4b53156aa3d
Fritz Schinkel <fritz.schinkel@bsi-software.com>
2026-08-03 10:40:56 +0200

    Inject relevant style sheets into PopupWindow
    The PopupWindow may display content that depends on dynamically injected
    style sheets (e.g. by jquery-scout). These style sheets are not reused
    from the main window or injected dynamically by the browser. The
    PopupWindow now ensures that all relevant style sheets (marked with
    'relevantForPopupWindow') are injected into the popup before any data is
    rendered. Additionally, all relevant style sheets that are injected into
    the main window while the popup is opened are injected into the popup
    window as well.
    
    463702

--- eclipse-scout-core/src/desktop/DesktopFormController.ts
+++ eclipse-scout-core/src/desktop/DesktopFormController.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -125,6 +125,7 @@ export class DesktopFormController extends FormController implements DesktopForm
       // error assertion
       throw new Error('Neither property \'formId\' nor \'popupWindow\' exists on data parameter');
     }
+    // noinspection JSIgnoredPromiseFromCall
     popupWindow._onReady();
   }
 
