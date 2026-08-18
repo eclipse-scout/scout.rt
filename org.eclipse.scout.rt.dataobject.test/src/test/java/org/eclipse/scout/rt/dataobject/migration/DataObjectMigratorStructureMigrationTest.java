46042339fa898e641666db4026178a021647a71d
Ralph Wortha <ralph.wortha@bsi-software.com>
2026-08-18 09:16:53 +0200

    Cleanup static beans in tests
    473674

--- org.eclipse.scout.rt.dataobject.test/src/test/java/org/eclipse/scout/rt/dataobject/migration/DataObjectMigratorStructureMigrationTest.java
+++ org.eclipse.scout.rt.dataobject.test/src/test/java/org/eclipse/scout/rt/dataobject/migration/DataObjectMigratorStructureMigrationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -91,6 +91,7 @@ public class DataObjectMigratorStructureMigrationTest {
   @AfterClass
   public static void afterClass() {
     BEANS.get(BeanTestingHelper.class).unregisterBeans(TEST_BEANS);
+    TEST_BEANS.clear();
   }
 
   /**
