8a8d5f4fff9c2d92f2b035199635ef19918aca2f
Beat Schwarzentrub <bsh@bsiag.com>
2026-08-19 08:12:40 +0200

    Table: fix filter if table text filter is removed and added again
    The previous change 095758a89c06924de8332c38e4da87ec5ec633f2 correctly
    updated the filterText property when the table text filter was removed
    programmatically. However, when such a filter is added, the property
    was not updated, only the input field. It was also not initialized
    according to an existing filter.
    
    The property filterText and the content of _$textFilter should always be
    kept in sync with the TableTextUserFilter.
    
    472907

--- eclipse-scout-core/src/table/TableFooterModel.ts
+++ eclipse-scout-core/src/table/TableFooterModel.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -11,5 +11,4 @@ import {Table, WidgetModel} from '../index';
 
 export interface TableFooterModel extends WidgetModel {
   table?: Table;
-  filterText?: string;
 }
