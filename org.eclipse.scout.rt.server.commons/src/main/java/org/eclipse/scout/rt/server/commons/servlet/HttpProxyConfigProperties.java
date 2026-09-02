dfcb41d1707ae887b14961f45a2b9d92d7806939
Paolo Bazzi <paolo.bazzi@bsi-software.com>
2026-09-02 14:32:27 +0200

    Application: Add HTTP connection idle timeout, improve stream/async TO
    - Added new low-level HTTP connection timeout for HTTP/HTTPS default 6m
    - Improved default value for HTTP2 stream timeout, new value 6 min
    - Improved default value for HTTP async context timeout, new value 6 min
    
    475330

--- org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/servlet/HttpProxyConfigProperties.java
+++ org.eclipse.scout.rt.server.commons/src/main/java/org/eclipse/scout/rt/server/commons/servlet/HttpProxyConfigProperties.java
@@ -11,6 +11,9 @@ package org.eclipse.scout.rt.server.commons.servlet;
 
 import java.util.concurrent.TimeUnit;
 
+import jakarta.servlet.AsyncContext;
+
+import org.eclipse.scout.rt.platform.Platform;
 import org.eclipse.scout.rt.platform.config.AbstractClassConfigProperty;
 import org.eclipse.scout.rt.platform.config.AbstractLongConfigProperty;
 import org.eclipse.scout.rt.shared.http.async.AbstractAsyncHttpClientManager;
@@ -39,6 +42,14 @@ public final class HttpProxyConfigProperties {
     }
   }
 
+  /**
+   * Sets the timeout (in milliseconds) for the {@link AsyncContext} used by the {@link HttpProxy} when forwarding requests.
+   * <p>
+   * <b>Note:</b> Keep this value smaller or equals to {@link org.eclipse.scout.rt.app.ApplicationProperties.ScoutApplicationStreamIdleTimeoutProperty}
+   * to ensure the async context can be closed before the underlying connection is terminated.
+   *
+   * @see AsyncContext#setTimeout(long)
+   */
   public static class HttpProxyAsyncTimeoutConfigProperty extends AbstractLongConfigProperty {
 
     @Override
@@ -48,12 +59,13 @@ public final class HttpProxyConfigProperties {
 
     @Override
     public String description() {
-      return "Timeout for async servlet contexts.";
+      return "Timeout in milliseconds for async servlet contexts. Default: " + getDefaultValue() + ".";
     }
 
     @Override
     public Long getDefaultValue() {
-      return TimeUnit.HOURS.toMillis(1);
+      // Do not use a short timeout in DEV mode to allow longer debugging sessions
+      return TimeUnit.MINUTES.toMillis(Platform.get().inDevelopmentMode() ? 60 : 6);
     }
   }
 }
