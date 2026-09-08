18bca9a224466578641da79c4cd4b92c58639abb
Andi Bur <andi.bur@gmail.com>
2026-09-08 09:17:42 +0200

    ChunkedDataWriter: fetch and iterate within the same transaction
    Chunked data transfer is used for large amounts of data, such as those
    loaded from a database. In this case, it is absolutely necessary that
    the execution of the SQL statement and the iteration over the ResultSet
    take place within the same transaction.
    
    This change modifies the signature of toResponse from an Iterator<T> to
    a Supplier<Iterator<T>>, allowing to create and iterate in the same
    RunContext.
    
    474697
    
    (cherry picked from commit 93fd4878c2f9e2beec2a39d53261279d1ea21d3d)
    (cherry picked from commit d4e9dfedce55c1a98d5e081b478b6c561e1eac64)

---
+++ org.eclipse.scout.rt.rest.jersey.test/src/test/java/org/eclipse/scout/rt/rest/jersey/server/chunked/ChunkedDataWriterTest.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.scout.rt.rest.jersey.server.chunked;
+
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import jakarta.ws.rs.core.Response;
+
+import org.eclipse.scout.rt.platform.BEANS;
+import org.eclipse.scout.rt.platform.BeanMetaData;
+import org.eclipse.scout.rt.platform.IBean;
+import org.eclipse.scout.rt.platform.IgnoreBean;
+import org.eclipse.scout.rt.platform.Replace;
+import org.eclipse.scout.rt.platform.context.RunContext;
+import org.eclipse.scout.rt.platform.context.RunContexts.RunContextFactory;
+import org.eclipse.scout.rt.platform.transaction.ITransaction;
+import org.eclipse.scout.rt.platform.transaction.TransactionCancelledError;
+import org.eclipse.scout.rt.platform.transaction.TransactionScope;
+import org.eclipse.scout.rt.platform.util.CollectionUtility;
+import org.eclipse.scout.rt.platform.util.SleepUtil;
+import org.eclipse.scout.rt.platform.util.concurrent.ThreadInterruptedError;
+import org.eclipse.scout.rt.rest.chunked.IChunkedDataWriter;
+import org.eclipse.scout.rt.testing.platform.BeanTestingHelper;
+import org.eclipse.scout.rt.testing.platform.runner.JUnitExceptionHandler;
+import org.eclipse.scout.rt.testing.platform.runner.PlatformTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(PlatformTestRunner.class)
+public class ChunkedDataWriterTest {
+
+  private FixtureIterator m_iterator;
+
+  @Test
+  public void testToResponse() {
+    @SuppressWarnings("resource")
+    IChunkedDataWriter<String> writer = IChunkedDataWriter.create(String.class, "\r\n", 100);
+    Response res = writer.toResponse(() -> fetchData());
+    assertEquals(Response.Status.OK.getStatusCode(), res.getStatus());
+    assertTrue(m_iterator.waitForCompletionAndVerifyTxn());
+  }
+
+  @Test
+  public void testToResponse_supplierThrowsUncheckedException() throws Exception {
+    @SuppressWarnings("resource")
+    IChunkedDataWriter<String> writer = IChunkedDataWriter.create(String.class, "\r\n", 100);
+
+    JUnitExceptionHandler exceptionHandler = BEANS.get(JUnitExceptionHandler.class);
+    exceptionHandler.ignoreExceptionOnce(RuntimeException.class, () -> {
+      assertThrows(RuntimeException.class,
+          () -> writer.toResponse(() -> {
+            throw new RuntimeException("by intention");
+          }));
+      // The exception above is thrown in a job and finally handled by the JUnitExceptionHandler.
+      // But because it involves another job, we must give it some time to be processed.
+      SleepUtil.sleepSafe(100, TimeUnit.MILLISECONDS);
+    });
+
+    exceptionHandler.ignoreExceptionOnce(TransactionCancelledError.class, () -> {
+      assertThrows(TransactionCancelledError.class,
+          () -> writer.toResponse(() -> {
+            throw new TransactionCancelledError("by intention");
+          }));
+      // same applies here (see comment above)
+      SleepUtil.sleepSafe(100, TimeUnit.MILLISECONDS);
+    });
+  }
+
+  @Test
+  public void testToResponse_differentTransactions() {
+    IBean<Object> fixtureRunCtxFactoryBean = BeanTestingHelper.get().registerBean(new BeanMetaData(RequiresNewFixtureRunContextFactory.class));
+    try {
+      @SuppressWarnings("resource")
+      IChunkedDataWriter<String> writer = IChunkedDataWriter.create(String.class, "\r\n", 100);
+      FixtureIterator iterator = fetchData();
+      Response res = writer.toResponse(() -> iterator);
+      assertEquals(Response.Status.OK.getStatusCode(), res.getStatus());
+      assertFalse(iterator.waitForCompletionAndVerifyTxn());
+    }
+    finally {
+      BeanTestingHelper.get().unregisterBean(fixtureRunCtxFactoryBean);
+    }
+  }
+
+  protected FixtureIterator fetchData() {
+    m_iterator = new FixtureIterator();
+    return m_iterator;
+  }
+
+  protected static class FixtureIterator implements Iterator<String> {
+    final ITransaction m_creatingTxn;
+    final Iterator<String> m_delegate;
+    final CountDownLatch m_completedLatch;
+    private volatile boolean m_iteratesInCreatingTxn = true;
+
+    public FixtureIterator() {
+      m_delegate = CollectionUtility.arrayList("a", "b", "c").iterator();
+      m_creatingTxn = ITransaction.CURRENT.get();
+      m_completedLatch = new CountDownLatch(1);
+    }
+
+    @Override
+    public boolean hasNext() {
+      checkTxn();
+      boolean hasNext = m_delegate.hasNext();
+      if (!hasNext) {
+        m_completedLatch.countDown();
+      }
+      return hasNext;
+    }
+
+    @Override
+    public String next() {
+      checkTxn();
+      return m_delegate.next();
+    }
+
+    private void checkTxn() {
+      if (m_creatingTxn != ITransaction.CURRENT.get()) {
+        m_iteratesInCreatingTxn = false;
+      }
+    }
+
+    public boolean waitForCompletionAndVerifyTxn() {
+      try {
+        return m_completedLatch.await(5, TimeUnit.SECONDS) && m_iteratesInCreatingTxn;
+      }
+      catch (InterruptedException e) {
+        throw new ThreadInterruptedError("Interrupted", e);
+      }
+    }
+  }
+
+  @Replace
+  @IgnoreBean
+  private static class RequiresNewFixtureRunContextFactory extends RunContextFactory {
+
+    @Override
+    public RunContext empty() {
+      return super.empty()
+          .withTransactionScope(TransactionScope.REQUIRES_NEW);
+    }
+
+    @Override
+    public RunContext copyCurrent() {
+      return super.copyCurrent()
+          .withTransactionScope(TransactionScope.REQUIRES_NEW);
+    }
+  }
+}
